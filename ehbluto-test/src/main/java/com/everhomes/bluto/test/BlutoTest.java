a92c20b033344f023521f89eaceba32127847fd5
Kelven Yang <kelveny@gmail.com>
2015-01-26 17:13:32 -0800

    Add secret key decoding example

---
+++ ehbluto-test/src/main/java/com/everhomes/bluto/test/BlutoTest.java
@@ -0,0 +1,35 @@
+package com.everhomes.bluto.test;
+
+import java.io.UnsupportedEncodingException;
+
+import com.everhomes.tool.BlutoAccessor;
+import com.everhomes.tool.BlutoMixer;
+
+class Key extends BlutoMixer {
+    public void keyRun() {
+        qZRTHhNg();AtazmCxA();mrpGu414();lbw82Zp0();qZRTHhNg();AtazmCxA();xjGpVroQ();mHw3Q5Dk();
+        mssooUYb();mrpGu414();xjGpVroQ();mrpGu414();AtazmCxA();xjGpVroQ();mHw3Q5Dk();mssooUYb();
+        mrpGu414();AtazmCxA();mrpGu414();lbw82Zp0();qZRTHhNg();AtazmCxA();xjGpVroQ();mHw3Q5Dk();
+        mssooUYb();mrpGu414();lbw82Zp0();AtazmCxA();mrpGu414();lbw82Zp0();qZRTHhNg();AtazmCxA();
+        bpcYSnXe();mssooUYb();mrpGu414();qZRTHhNg();mrpGu414();AtazmCxA();xjGpVroQ();mHw3Q5Dk();
+        mssooUYb();mrpGu414();lbw82Zp0();xjGpVroQ();mrpGu414();AtazmCxA();mHw3Q5Dk();mssooUYb();
+        mrpGu414();mrpGu414();xjGpVroQ();mssooUYb();mrpGu414();lbw82Zp0();qZRTHhNg();AtazmCxA();
+        mrpGu414();qZRTHhNg();bpcYSnXe();mHw3Q5Dk();mssooUYb();mrpGu414();lbw82Zp0();mrpGu414();
+        lbw82Zp0();AtazmCxA();mHw3Q5Dk();mssooUYb();mrpGu414();lbw82Zp0();qZRTHhNg();mrpGu414();
+        AtazmCxA();xjGpVroQ();mHw3Q5Dk();mssooUYb();mrpGu414();    }
+}
+
+public class BlutoTest {
+    public static void main(String[] args) {
+        BlutoAccessor accessor = new BlutoAccessor();
+        
+        Key key = new Key();
+        String decodedKey;
+        try {
+            decodedKey = new String(accessor.get(key), "UTF-8");
+            System.out.println("Decoded key: " + decodedKey);
+        } catch (UnsupportedEncodingException e) {
+        }
+    }
+}
+
