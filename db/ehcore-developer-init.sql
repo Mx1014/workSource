b7189cad8764426922774a0a0bf775c9ede6ae37
Kelven Yang <kelveny@gmail.com>
2015-03-26 20:00:25 -0700

    Add parent pom for building all server components with one command

---
+++ db/ehcore-developer-init.sql
@@ -0,0 +1,9 @@
+#
+# This file contains initial data set for a typical developer setup with DB+redis configuration
+#
+INSERT INTO `eh_servers` (`id`, `address_uri`, `server_type`, `status`, `config_tag`, `description`) 
+    VALUES(1, 'jdbc:mysql://ehcore:ehcore@db-master:3306/ehcore?characterEncoding=UTF-8', 0, 1, 'default', 'default DB');
+INSERT INTO `eh_servers` (`id`, `address_uri`, `address_port`, `server_type`, `status`, `config_tag`, `description`) 
+    VALUES(2, 'redis-server', 6379, 1, 1, 'RedisStorage', 'Redis Storage');
+INSERT INTO `eh_servers` (`id`, `address_uri`, `address_port`, `server_type`, `status`, `config_tag`, `description`) 
+    VALUES(3, 'redis-server', 6379, 2, 1, 'RedisCache', 'Redis Cache');
