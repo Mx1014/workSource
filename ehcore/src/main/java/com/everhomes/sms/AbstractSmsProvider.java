0a916e77a907a913ea44ea8b6568d982fe0ccdf6
Elian Wan <elians.wan@everhomes.com>
2015-04-09 11:38:54 +0800

    update convert method

--- ehcore/src/main/java/com/everhomes/sms/AbstractSmsProvider.java
+++ ehcore/src/main/java/com/everhomes/sms/AbstractSmsProvider.java
@@ -1,10 +1,13 @@
 package com.everhomes.sms;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.everhomes.cache.CacheProvider;
+import com.everhomes.constants.ErrorCodes;
 import com.everhomes.db.DbProvider;
+import com.everhomes.util.RuntimeErrorException;
 
 public abstract class AbstractSmsProvider implements SmsProvider {
 
@@ -18,17 +21,20 @@ public abstract class AbstractSmsProvider implements SmsProvider {
 
 	@Override
 	public void sendSms(String phoneNumber, String text) {
-		doSend(phoneNumber,convert(text));
+		doSend(phoneNumber, convert(text));
 	}
 
 	@Override
 	public void sendSms(String[] phoneNumbers, String text) throws Exception {
 		doSend(phoneNumbers, convert(text));
 	}
-	
-	private String convert(String text)
-	{
-		return null;
+
+	private String convert(String text) {
+		if (StringUtils.isEmpty(text)) {
+			throw RuntimeErrorException.errorWith(ErrorCodes.SCOPE_GENERAL,
+					ErrorCodes.ERROR_GENERAL_EXCEPTION, "message is empty");
+		}
+		return text.replace(" ", "%20").replace("=", "%3D");
 	}
 
 	protected abstract void doSend(String phoneNumber, String text);
