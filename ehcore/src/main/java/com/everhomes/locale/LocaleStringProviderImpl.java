33603211ff50b0acd6eb4cf5be53ac4aca10b4f9
Kelven Yang <kelveny@gmail.com>
2015-04-21 18:08:47 -0700

    Clear i18n cache upon startup

--- ehcore/src/main/java/com/everhomes/locale/LocaleStringProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/locale/LocaleStringProviderImpl.java
@@ -1,10 +1,14 @@
 package com.everhomes.locale;
 
+import javax.annotation.PostConstruct;
+
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
+import com.everhomes.cache.CacheAccessor;
+import com.everhomes.cache.CacheProvider;
 import com.everhomes.db.AccessSpec;
 import com.everhomes.db.DbProvider;
 import com.everhomes.server.schema.Tables;
@@ -16,6 +20,15 @@ public class LocaleStringProviderImpl implements LocaleStringProvider {
     
     @Autowired
     private DbProvider dbProvider;
+    
+    @Autowired
+    private CacheProvider cacheProvice;
+    
+    @PostConstruct
+    public void setup() {
+        CacheAccessor accessor = this.cacheProvice.getCacheAccessor("LocaleStringFind");
+        accessor.clear();
+    }
     
     @Cacheable(value="LocaleStringFind", key="{#scope, #code, #locale}")
     @Override
