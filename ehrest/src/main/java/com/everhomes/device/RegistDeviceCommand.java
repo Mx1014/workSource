b4ad72a4f85f590123337ee81ebd8ba7f3e15439
janson <gandancing@gmail.com>
2015-04-22 15:03:58 +0800

    support for device registing

---
+++ ehrest/src/main/java/com/everhomes/device/RegistDeviceCommand.java
@@ -0,0 +1,79 @@
+package com.everhomes.device;
+
+import javax.validation.constraints.NotNull;
+
+public class RegistDeviceCommand {
+    @NotNull
+    private String deviceId;
+    
+    @NotNull
+    private String platform;
+    
+    private String product;
+    
+    private String brand;
+    
+    private String deviceModel;
+    
+    private String systemVersion;
+    
+    private String meta;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
+    }
+
+    public String getSystemVersion() {
+        return systemVersion;
+    }
+
+    public void setSystemVersion(String systemVersion) {
+        this.systemVersion = systemVersion;
+    }
+
+    public String getMeta() {
+        return meta;
+    }
+
+    public void setMeta(String meta) {
+        this.meta = meta;
+    }
+    
+    
+}
