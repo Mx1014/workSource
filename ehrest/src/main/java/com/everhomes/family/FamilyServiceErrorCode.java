b90a1259ba1b7d66511e7caf95be9e3accacfa42
liuxiaochun <shane.liu@zuolin.com>
2015-04-27 11:42:16 +0800

    user join family

--- ehrest/src/main/java/com/everhomes/family/FamilyServiceErrorCode.java
+++ ehrest/src/main/java/com/everhomes/family/FamilyServiceErrorCode.java
@@ -2,5 +2,8 @@ package com.everhomes.family;
 
 public class FamilyServiceErrorCode {
     static final String SCOPE = "family";
+    
+    //用户已经加入该家庭。
+    static final int ERROR_USER_FAMILY_EXIST = 10001;
 
 }
