7f804a54ab752c0d9788958bfb64aca57c3e019a
Elian Wan <elians.wan@everhomes.com>
2015-05-16 15:51:42 +0800

    add status check

---
+++ ehcore/src/main/java/com/everhomes/util/StatusChecker.java
@@ -0,0 +1,17 @@
+package com.everhomes.util;
+
+import com.everhomes.poll.ProcessStatus;
+
+public class StatusChecker {
+    public static ProcessStatus getProcessStatus(Long startTime, Long endTime) {
+        long current = DateHelper.currentGMTTime().getTime();
+        if (startTime == null) {
+            if (current <= endTime.longValue())
+                return ProcessStatus.UNKNOWN;
+            return ProcessStatus.END;
+        }
+        if (current <= startTime)
+            return ProcessStatus.UNDERWAY;
+        return ProcessStatus.END;
+    }
+}
