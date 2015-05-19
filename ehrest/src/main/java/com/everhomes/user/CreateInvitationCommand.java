073a10439bb8b35525511828ef57c088c1a41691
Elian Wan <elians.wan@everhomes.com>
2015-05-19 17:03:22 +0800

    update invitation code

--- ehrest/src/main/java/com/everhomes/user/CreateInvitationCommand.java
+++ ehrest/src/main/java/com/everhomes/user/CreateInvitationCommand.java
@@ -8,11 +8,16 @@ import com.everhomes.util.StringHelper;
  * 获取邀请码
  * 
  * @author elians
- * <ul>
- * <li>inviteType:邀请类型.支持类型1.SMS,2.wechat,3.friend_circle,4.weibo,5.phone</li>
- * <li>targetEntityType:实体类型</li>
- * <li>targetEntityId:实体ID</li>
- * </ul>
+ *         <ul>
+ *         <li>
+ *         inviteType:邀请类型.支持类型1.SMS,2.wechat,3.friend_circle,4.weibo,5.phone</li>
+ *         <li>targetEntityType:实体类型</li>
+ *         <li>targetEntityId:实体ID</li>
+ *         <li>identifiers:用户标识，手机或者邮箱，用;分割</li>
+ *         <li>communityId:小区ID</li>
+ *         <li>buildingNum:楼栋号</li>
+ *         <li>aptNumber:公寓号</li>
+ *         </ul>
  *
  */
 public class CreateInvitationCommand {
@@ -23,6 +28,15 @@ public class CreateInvitationCommand {
     private String targetEntityType;
     @NotNull
     private Long targetEntityId;
+
+    private String identifiers;
+    
+    private Long communityId;
+    
+    
+    private String buildingNum;
+
+    private String aptNumber;
 
     public String getInviteType() {
         return inviteType;
@@ -47,8 +61,41 @@ public class CreateInvitationCommand {
     public void setTargetEntityId(Long targetEntityId) {
         this.targetEntityId = targetEntityId;
     }
+
+    public String getIdentifiers() {
+        return identifiers;
+    }
+
+    public void setIdentifiers(String identifiers) {
+        this.identifiers = identifiers;
+    }
     
     
+
+    public Long getCommunityId() {
+        return communityId;
+    }
+
+    public void setCommunityId(Long communityId) {
+        this.communityId = communityId;
+    }
+
+    public String getBuildingNum() {
+        return buildingNum;
+    }
+
+    public void setBuildingNum(String buildingNum) {
+        this.buildingNum = buildingNum;
+    }
+
+    public String getAptNumber() {
+        return aptNumber;
+    }
+
+    public void setAptNumber(String aptNumber) {
+        this.aptNumber = aptNumber;
+    }
+
     @Override
     public String toString() {
         return StringHelper.toJsonString(this);
