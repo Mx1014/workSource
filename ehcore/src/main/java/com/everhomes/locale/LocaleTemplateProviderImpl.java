3e55df3843b410384f4b9efcba8766827911879e
LiangQishi <qishi.liang@zuolin.com>
2015-05-22 08:16:28 +0800

    add notification implements for group

---
+++ ehcore/src/main/java/com/everhomes/locale/LocaleTemplateProviderImpl.java
@@ -0,0 +1,88 @@
+package com.everhomes.locale;
+
+import javax.annotation.PostConstruct;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.stereotype.Component;
+
+import com.everhomes.cache.CacheAccessor;
+import com.everhomes.cache.CacheProvider;
+import com.everhomes.db.AccessSpec;
+import com.everhomes.db.DaoAction;
+import com.everhomes.db.DaoHelper;
+import com.everhomes.db.DbProvider;
+import com.everhomes.server.schema.Tables;
+import com.everhomes.server.schema.tables.daos.EhLocaleTemplatesDao;
+import com.everhomes.server.schema.tables.pojos.EhGroups;
+import com.everhomes.server.schema.tables.pojos.EhLocaleTemplates;
+import com.everhomes.server.schema.tables.records.EhLocaleTemplatesRecord;
+import com.everhomes.util.ConvertHelper;
+
+@Component
+public class LocaleTemplateProviderImpl implements LocaleTemplateProvider {
+    
+    @Autowired
+    private DbProvider dbProvider;
+    
+    @Autowired
+    private CacheProvider cacheProvice;
+    
+    @PostConstruct
+    public void setup() {
+        CacheAccessor accessor = this.cacheProvice.getCacheAccessor("LocaleStringFind");
+        accessor.clear();
+    }
+    
+    @Cacheable(value="LocaleTemplateById", key="#id")
+    @Override
+    public LocaleTemplate findLocaleTemplateById(long id) {
+        DSLContext context = this.dbProvider.getDslContext(AccessSpec.readOnly());
+        EhLocaleTemplatesDao dao = new EhLocaleTemplatesDao(context.configuration());
+        EhLocaleTemplates record = dao.findById(id);
+            
+        return ConvertHelper.convert(record, LocaleTemplate.class);
+    }
+    
+    @Cacheable(value="LocaleTemplateByScope", key="{#scope, #code, #locale}")
+    @Override
+    public LocaleTemplate findLocaleTemplateByScope(String scope, int code, String locale) {
+        DSLContext context = this.dbProvider.getDslContext(AccessSpec.readOnly());
+        EhLocaleTemplatesRecord record = (EhLocaleTemplatesRecord)context.select().from(Tables.EH_LOCALE_TEMPLATES)
+            .where(Tables.EH_LOCALE_TEMPLATES.SCOPE.like(scope))
+            .and(Tables.EH_LOCALE_TEMPLATES.CODE.eq(code))
+            .and(Tables.EH_LOCALE_TEMPLATES.LOCALE.like(locale))
+            .fetchAny();
+            
+        return ConvertHelper.convert(record, LocaleTemplate.class);
+    }
+    
+    @Caching(evict={ @CacheEvict(value="LocaleTemplateById", key="#template.id"),
+        @CacheEvict(value="LocaleTemplateByScope", key="{#template.scope, #template.code, #template.locale}")})
+    @Override
+    public void updateLocaleTemplate(LocaleTemplate template) {
+        assert(template.getId() != null);
+        
+        DSLContext context = this.dbProvider.getDslContext(AccessSpec.readWriteWith(EhGroups.class));
+        EhLocaleTemplatesDao dao = new EhLocaleTemplatesDao(context.configuration());
+        dao.update(template);
+        
+        DaoHelper.publishDaoAction(DaoAction.MODIFY, EhLocaleTemplates.class, template.getId());
+    }
+
+    @Caching(evict={ @CacheEvict(value="LocaleTemplateById", key="#template.id"),
+        @CacheEvict(value="LocaleTemplateByScope", key="{#template.scope, #template.code, #template.locale}")})
+    @Override
+    public void deleteLocaleTemplate(LocaleTemplate template) {
+        assert(template.getId() != null);
+        
+        DSLContext context = this.dbProvider.getDslContext(AccessSpec.readWriteWith(EhGroups.class, template.getId()));
+        EhLocaleTemplatesDao dao = new EhLocaleTemplatesDao(context.configuration());
+        dao.delete(template);
+        
+        DaoHelper.publishDaoAction(DaoAction.MODIFY, EhLocaleTemplates.class, template.getId());
+    }
+}
