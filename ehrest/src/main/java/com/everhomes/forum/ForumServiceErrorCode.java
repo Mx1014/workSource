ddce39d7f4dcb6c434bd64b78ca1de40f25ce305
LiangQishi <qishi.liang@zuolin.com>
2015-06-04 09:36:47 +0800

    add topic comment interfaces

--- ehrest/src/main/java/com/everhomes/forum/ForumServiceErrorCode.java
+++ ehrest/src/main/java/com/everhomes/forum/ForumServiceErrorCode.java
@@ -6,4 +6,5 @@ public interface ForumServiceErrorCode {
     static final int ERROR_FORUM_NOT_EXIST = 10001;
     static final int ERROR_FORUM_VISIBLE_REGION_NOT_EXIST = 10002;
     static final int ERROR_FORUM_COMMUNITY_NOT_EXIST = 10003;
+    static final int ERROR_FORUM_TOPIC_NOT_FOUND = 10004;
 }
