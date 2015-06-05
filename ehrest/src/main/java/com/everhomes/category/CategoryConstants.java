30d593b96d6a6028b4c41b85338d7294cfdefe86
liuxiaochun <shane.liu@zuolin.com>
2015-06-05 16:09:52 +0800

    add property taskType enum

--- ehrest/src/main/java/com/everhomes/category/CategoryConstants.java
+++ ehrest/src/main/java/com/everhomes/category/CategoryConstants.java
@@ -10,5 +10,10 @@ public interface CategoryConstants {
     public static final long CATEGORY_ID_PM = 9L;
     /** 公告（物业、业委、居委、公安等） */
     public static final long CATEGORY_ID_GA_NOTICE = 3091L;
-
+    /** 物业建议 */
+    public static final long CATEGORY_ID_GA_ADVISE = 3092L;
+    /** 物业维修 */
+    public static final long CATEGORY_ID_GA_HELP = 3093L;
+    /** 物业求助 */
+    public static final long CATEGORY_ID_GA_REPAIR = 3094L;
 }
