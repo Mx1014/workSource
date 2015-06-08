3b535090fd2db4bd97d1ffc54b330afdee4358d9
Elian Wan <elians.wan@everhomes.com>
2015-06-08 14:00:58 +0800

    refactor interface

--- ehcore/src/main/java/com/everhomes/admin/user/UserAdminController.java
+++ ehcore/src/main/java/com/everhomes/admin/user/UserAdminController.java
@@ -1,6 +1,7 @@
 package com.everhomes.admin.user;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -11,14 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.everhomes.controller.ControllerBase;
 import com.everhomes.discover.RestReturn;
 import com.everhomes.rest.RestResponse;
-import com.everhomes.user.ListUserRelationResponse;
+import com.everhomes.user.ListRegisterUsersResponse;
+import com.everhomes.user.ListVerfyCodeResponse;
+import com.everhomes.user.ListVestResponse;
 import com.everhomes.user.PaginationCommand;
 import com.everhomes.user.UserAdminService;
 import com.everhomes.user.UserIdentifier;
+import com.everhomes.user.UserIdentifierDTO;
 import com.everhomes.user.UserInfo;
+import com.everhomes.util.ConvertHelper;
+import com.everhomes.util.RequireAuthentication;
 import com.everhomes.util.Tuple;
 
 @RestController
+@RequireAuthentication(true)
 @RequestMapping("/admin/user")
 public class UserAdminController extends ControllerBase {
 
@@ -26,19 +33,21 @@ public class UserAdminController extends ControllerBase {
     private UserAdminService userAdminService;
 
     @RequestMapping("listVerifyCode")
-    @RestReturn(ListUserRelationResponse.class)
+    @RestReturn(ListVerfyCodeResponse.class)
     public RestResponse listVerifyCode(@Valid PaginationCommand cmd) {
-        ListUserRelationResponse<List<UserIdentifier>> object = new ListUserRelationResponse<List<UserIdentifier>>();
+        // check acl
+        ListVerfyCodeResponse object = new ListVerfyCodeResponse();
         Tuple<Long, List<UserIdentifier>> result = userAdminService.listVerifyCode(cmd);
-        object.setValues(result.second());
+        object.setValues(result.second().stream().map(r -> ConvertHelper.convert(r, UserIdentifierDTO.class))
+                .collect(Collectors.toList()));
         object.setNextAnchor(result.first());
         return new RestResponse(object);
     }
 
     @RequestMapping("listVest")
-    @RestReturn(ListUserRelationResponse.class)
+    @RestReturn(ListVestResponse.class)
     public RestResponse listVest(@Valid PaginationCommand cmd) {
-        ListUserRelationResponse<List<UserInfo>> object = new ListUserRelationResponse<List<UserInfo>>();
+        ListVestResponse object = new ListVestResponse();
         Tuple<Long, List<UserInfo>> result = userAdminService.listVets(cmd);
         object.setValues(result.second());
         object.setNextAnchor(result.first());
@@ -46,9 +55,9 @@ public class UserAdminController extends ControllerBase {
     }
 
     @RequestMapping("listRegisterUsers")
-    @RestReturn(ListUserRelationResponse.class)
+    @RestReturn(ListRegisterUsersResponse.class)
     public RestResponse listRegisterUsers(@Valid PaginationCommand cmd) {
-        ListUserRelationResponse<List<UserInfo>> object = new ListUserRelationResponse<List<UserInfo>>();
+        ListRegisterUsersResponse object = new ListRegisterUsersResponse();
         Tuple<Long, List<UserInfo>> result = userAdminService.listRegisterUsers(cmd);
         object.setValues(result.second());
         object.setNextAnchor(result.first());
