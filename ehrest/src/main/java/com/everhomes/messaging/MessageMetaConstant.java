b3628a423c03c758ae9dc01a7b8f46005a7e723a
janson <gandancing@gmail.com>
2015-06-16 15:12:30 +0800

    for push and meta

---
+++ ehrest/src/main/java/com/everhomes/messaging/MessageMetaConstant.java
@@ -0,0 +1,5 @@
+package com.everhomes.messaging;
+
+public interface MessageMetaConstant {
+
+}
---
+++ ehrest/src/main/java/com/everhomes/messaging/PushJumpObject.java
@@ -0,0 +1,20 @@
+package com.everhomes.messaging;
+
+public class PushJumpObject {
+    private String jump;
+    private Long id;
+    
+    public String getJump() {
+        return jump;
+    }
+    public void setJump(String jump) {
+        this.jump = jump;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
