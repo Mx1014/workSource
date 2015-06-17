5580633dff8b228d728de4ced6dc917da8328278
janson <gandancing@gmail.com>
2015-06-17 14:57:48 +0800

    add senderName in message meta

--- ehrest/src/main/java/com/everhomes/messaging/MessageMetaConstant.java
+++ ehrest/src/main/java/com/everhomes/messaging/MessageMetaConstant.java
@@ -7,4 +7,5 @@ public interface MessageMetaConstant {
     public static final String META_OBJECT = "meta-object";
     public static final String INCLUDE = "include";
     public static final String EXCLUDE = "exclude";
+    public static final String SENDER_NAME = "sender-name";
 }
