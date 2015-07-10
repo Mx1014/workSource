9da3847cb3221f742f62a9e929d7ee1891267ffa
LiangQishi <qishi.liang@zuolin.com>
2015-07-10 16:14:33 +0800

    the mapper can not be converted by ConvertHelper

--- ehcore/src/main/java/com/everhomes/organization/EhOrganizationRecordMapper.java
+++ ehcore/src/main/java/com/everhomes/organization/EhOrganizationRecordMapper.java
@@ -1,15 +1,29 @@
 // @formatter:off
 package com.everhomes.organization;
 
+import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.RecordMapper;
 
+import com.everhomes.server.schema.tables.records.EhOrganizationsRecord;
 import com.everhomes.util.ConvertHelper;
 
-public class EhOrganizationRecordMapper implements RecordMapper<Record, Organization> {    
+public class EhOrganizationRecordMapper implements RecordMapper<Record, EhOrganizationsRecord> {    
     @SuppressWarnings("unchecked")
     @Override
-    public Organization map(Record r) {
-        return ConvertHelper.convert(r, Organization.class);
+    public EhOrganizationsRecord map(Record r) {
+        EhOrganizationsRecord record =  ConvertHelper.convert(r, EhOrganizationsRecord.class);
+        
+        EhOrganizationsRecord organization = new EhOrganizationsRecord();
+        organization.setId(r.getValue((Field<Long>)r.field("id")));
+        organization.setParentId(r.getValue((Field<Long>)r.field("parent_id")));
+        organization.setOrganizationType(r.getValue((Field<String>)r.field("organization_type")));
+        organization.setName(r.getValue((Field<String>)r.field("name")));
+        organization.setAddressId(r.getValue((Field<Long>)r.field("address_id")));
+        organization.setPath(r.getValue((Field<String>)r.field("path")));
+        organization.setLevel(r.getValue((Field<Integer>)r.field("level")));
+        organization.setStatus(r.getValue((Field<Byte>)r.field("status")));
+        
+        return organization;
     }
 }
