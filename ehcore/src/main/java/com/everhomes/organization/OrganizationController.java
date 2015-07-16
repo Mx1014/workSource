9925dddae63e24f57ff57e2c31bafc673125ea55
liujinwen <278810263@qq.com>
2015-07-16 13:48:44 +0800

    change /organization to /org

--- ehcore/src/main/java/com/everhomes/organization/OrganizationController.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationController.java
@@ -31,7 +31,7 @@ import com.everhomes.user.UserTokenCommand;
 import com.everhomes.user.UserTokenCommandResponse;
 
 @RestController
-@RequestMapping("/organization")
+@RequestMapping("/org")
 public class OrganizationController extends ControllerBase {
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationController.class);
 
