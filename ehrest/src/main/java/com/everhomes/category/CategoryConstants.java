64420bdbae4baa5ffe49e30e7ccf9fcffe91d357
LiangQishi <qishi.liang@zuolin.com>
2015-07-22 08:59:14 +0800

    add constant relate to used, rental, free-stuff, lost and found

--- ehrest/src/main/java/com/everhomes/category/CategoryConstants.java
+++ ehrest/src/main/java/com/everhomes/category/CategoryConstants.java
@@ -6,6 +6,15 @@ public interface CategoryConstants {
     public static final long Interest = 10001L;
     public static final long ApplicationTool = 10002L;
     
+    /** 帖子 */
+    public static final long CATEGORY_ID_TOPIC = 1L;
+    /** 兴趣 */
+    public static final long CATEGORY_ID_INTEREST = 2L;
+    /** 商家与服务 */
+    public static final long CATEGORY_ID_SERVICE = 3L;
+    /** 活动 */
+    public static final long CATEGORY_ID_ACTIVITY = 4L;
+    
     /** 公告（物业、业委、居委、公安等） */
     public static final long CATEGORY_ID_NOTICE = 1003L;
     /** 报修（物业、业委、居委、公安等）*/
@@ -14,4 +23,12 @@ public interface CategoryConstants {
     public static final long CATEGORY_ID_CONSULT_APPEAL = 1005L;
     /** 投诉与建议（物业、业委、居委、公安等） */
     public static final long CATEGORY_ID_COMPLAINT_ADVICE = 1006L;
+    /** 二手和租售 */
+    public static final long CATEGORY_ID_USED_AND_RENTAL = 1007L;
+    /** 免费物品 */
+    public static final long CATEGORY_ID_FREE_STUFF = 1008L;
+    /** 失物招领 */
+    public static final long CATEGORY_ID_LOST_AND_FOUND = 1009L;
+    /** 帖子/活动 */
+    public static final long CATEGORY_ID_TOPIC_ACTIVITY = 1010L;
 }
