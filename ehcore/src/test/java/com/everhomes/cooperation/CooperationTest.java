231cb06348fa17aa0326d6a6400ca922ad473b29
wuhan <han.wu@zuolin.com>
2015-07-30 11:24:46 +0800

    update :add new cooperation @RequireAuthentication false

---
+++ ehcore/src/test/java/com/everhomes/cooperation/CooperationTest.java
@@ -0,0 +1,31 @@
+package com.everhomes.cooperation;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.everhomes.junit.CoreServerTestCase;
+
+public class CooperationTest extends CoreServerTestCase {
+	@Autowired
+	CooperationService cooperationService;
+
+	@Test
+	public void testNewCooperation() {
+		NewCooperationCommand cmd = new NewCooperationCommand();
+		cmd.setApplicantName("user");
+		cmd.setApplicantOccupation("CTO");
+		cmd.setApplicantPhone("0755110");
+		cmd.setAreaName("nanshan");
+		cmd.setProvinceName("guangdong"); 
+		cmd.setAddress("addresstest");
+		cmd.setName("testName");
+		cmd.setCooperationType("PM");
+		cmd.setContactToken("13322215114");
+		cmd.setCityName("cityName");
+		cmd.setApplicantEmail("2222#$@adsf@3");
+		cmd.setCommunityNames("南山家园,南山佳苑,南山之家,南山尼玛");
+		System.out.println(cmd.toString());
+		cooperationService.newCooperation(cmd);
+	}
+
+}
