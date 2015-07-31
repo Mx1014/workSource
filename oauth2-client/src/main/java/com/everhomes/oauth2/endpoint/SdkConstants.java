982bd30dbff2f50404430f8688b3347424703008
janson <gandancing@gmail.com>
2015-07-31 17:32:55 +0800

    change api key

--- oauth2-client/src/main/java/com/everhomes/oauth2/endpoint/SdkConstants.java
+++ oauth2-client/src/main/java/com/everhomes/oauth2/endpoint/SdkConstants.java
@@ -1,16 +1,16 @@
 package com.everhomes.oauth2.endpoint;
 
 public interface SdkConstants {
-    public static final String API_KEY = "b86ddb3b-ac77-4a65-ae03-7e8482a3db70";
+    public static final String API_KEY = "d80e06ca-3766-11e5-b18f-b083fe4e159f";
 
     // demostration only, it should be kept in a safe place
-    public static final String API_SECRET = "2-0cDFNOq-zPzYGtdS8xxqnkR8PRgNhpHcWoku6Ob49NdBw8D9-Q72MLsCidI43IKhP1D_43ujSFbatGPWuVBQ";
+    public static final String API_SECRET = "g1JOZUM3BYzWpZD5Q7p3z+i/z0nj2TcokTFx2ic53FCMRIKbMhSUCi7fSu9ZklFCZ9tlj68unxur9qmOji4tNg==";
 
-    public static final String API_AUTHORIZE_SERVICE_URI = "http://10.1.1.92:8080/oauth2/authorize";
+    public static final String API_AUTHORIZE_SERVICE_URI = "http://10.1.1.98:8080/oauth2/authorize";
 
-    public static final String API_TOKEN_SERVICE_URI = "http://10.1.1.92:8080/oauth2/token";
+    public static final String API_TOKEN_SERVICE_URI = "http://10.1.1.98:8080/oauth2/token";
 
-    public static final String API_OAUTH2API_URI = "http://10.1.1.92:8080/oauth2api";
+    public static final String API_OAUTH2API_URI = "http://10.1.1.98:8080/oauth2api";
 
-    public static final String CLIENT_REDIRECT_URI = "http://10.1.1.92:7788/redirect";
+    public static final String CLIENT_REDIRECT_URI = "http://10.1.1.98:7788/redirect";
 }
