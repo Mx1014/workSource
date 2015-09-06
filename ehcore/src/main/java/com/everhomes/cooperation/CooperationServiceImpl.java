e357670371a90ad2fff817245c34cd26c3b5ec18
wuhan <han.wu@zuolin.com>
2015-09-06 17:01:45 +0800

    add cooperation admin

--- ehcore/src/main/java/com/everhomes/cooperation/CooperationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/cooperation/CooperationServiceImpl.java
@@ -1,12 +1,23 @@
 package com.everhomes.cooperation;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.everhomes.banner.admin.ListBannersAdminCommandResponse;
+import com.everhomes.configuration.ConfigurationProvider;
+import com.everhomes.cooperation.admin.CooperationDTO;
+import com.everhomes.cooperation.admin.ListCooperationAdminCommand;
+import com.everhomes.cooperation.admin.ListCooperationAdminCommandResponse;
+import com.everhomes.core.AppConfig;
+import com.everhomes.user.User;
+import com.everhomes.user.UserContext;
 import com.everhomes.util.ConvertHelper;
- 
+import com.everhomes.util.PaginationHelper;
 
 @Component
 public class CooperationServiceImpl implements CooperationService {
@@ -15,15 +26,51 @@ public class CooperationServiceImpl implements CooperationService {
 			.getLogger(CooperationServiceImpl.class);
 	@Autowired
 	CooperationProvider cooperationProvider;
+	@Autowired
+	ConfigurationProvider configurationProvider;
 
 	@Override
 	public void newCooperation(NewCooperationCommand cmd) {
 		// TODO Auto-generated method stub
 		// 类型强制为电话
 		cmd.setContactType((byte) 0);
-		LOGGER.debug("new Cooperation begin insert , "+cmd.toString()+"");
-		CooperationRequests cooperationRequests = ConvertHelper.convert(cmd,CooperationRequests.class);
+		LOGGER.debug("new Cooperation begin insert , " + cmd.toString() + "");
+		CooperationRequests cooperationRequests = ConvertHelper.convert(cmd,
+				CooperationRequests.class);
 		cooperationProvider.newCooperation(cooperationRequests);
 	}
 
+	@Override
+	public ListCooperationAdminCommandResponse listCooperation(
+			ListCooperationAdminCommand cmd) {
+//		User user = UserContext.current().getUser();
+//		long userId = user.getId();
+		if (cmd.getKeyword() == null)
+			cmd.setKeyword("");
+		if (cmd.getCooperationType() == null)
+			return null;
+		final long pageSize = cmd.getPageSize() == null ? this.configurationProvider
+				.getIntValue("pagination.page.size",
+						AppConfig.DEFAULT_PAGINATION_PAGE_SIZE) : cmd
+				.getPageSize();
+		long pageOffset = cmd.getPageOffset() == null ? 1L : cmd
+				.getPageOffset();
+		long offset = PaginationHelper.offsetFromPageOffset(pageOffset,
+				pageSize);
+		List<CooperationDTO> result = cooperationProvider
+				.listCooperation(cmd.getKeyword(), cmd.getCooperationType(),
+						offset, pageSize)
+				.stream()
+				.map((CooperationRequests r) -> {
+					CooperationDTO dto = ConvertHelper.convert(r,
+							CooperationDTO.class);
+					return dto;
+				}).collect(Collectors.toList());
+		ListCooperationAdminCommandResponse response = new ListCooperationAdminCommandResponse();
+		if (result != null && result.size() >= pageSize) {
+			response.setNextPageOffset((int) pageOffset + 1);
+		}
+		response.setRequests(result);
+		return response;
+	}
 }
