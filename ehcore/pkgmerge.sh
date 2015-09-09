35e3b68e01419b9d11f1aa1350a6ef03acbdf037
janson <gandancing@gmail.com>
2015-09-09 16:09:24 +0800

    delete verbose

--- ehcore/pkgmerge.sh
+++ ehcore/pkgmerge.sh
@@ -3,9 +3,9 @@
 mkdir merge
 cd merge
 
-tar -zxvf ../output/ehcore.tar.gz
+tar -zvf ../output/ehcore.tar.gz
 cd ehcore
-tar -zxvf ../../output/jar.tar.gz
+tar -zvf ../../output/jar.tar.gz
 jar -cvfM0 ehcore.war ./
 mv ehcore.war ../../output
 cd ../../
