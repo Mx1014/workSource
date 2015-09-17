e9fb30b7ed3b063998c73ee2344bccac25586e14
wuhan <han.wu@zuolin.com>
2015-09-17 12:23:51 +0800

    techpark Mysql sql script

---
+++ db/techpark-punch-init.sql
@@ -0,0 +1,36 @@
+ 
+USE `ehcore`;
+
+/*Table structure for table `eh_punch_logs` */
+
+DROP TABLE IF EXISTS `eh_punch_logs`;
+
+CREATE TABLE `eh_punch_logs` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `company_id` bigint(20) DEFAULT NULL COMMENT 'compay id',
+  `user_id` bigint(20) DEFAULT NULL COMMENT 'user''s id',
+  `check_time` datetime DEFAULT NULL COMMENT 'user check time',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `eh_punch_rules` */
+
+DROP TABLE IF EXISTS `eh_punch_rules`;
+
+CREATE TABLE `eh_punch_rules` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `company_id` bigint(20) NOT NULL COMMENT 'rule company id',
+  `check_type` tinyint(4) DEFAULT NULL COMMENT '0:standard ;  1:flextime',
+  `punch_times` int(1) DEFAULT NULL COMMENT 'how many times everyday emloyee need punched. 2 times or 4 times',
+  `start_time` time DEFAULT NULL COMMENT 'type=0:work start time ;1:work start earlist time',
+  `end_time` time DEFAULT NULL COMMENT 'type=0:work end time ;1:work start latest time',
+  `noon_end_time` time DEFAULT NULL,
+  `afternoon_start_time` time DEFAULT NULL,
+  `work_time` time DEFAULT NULL COMMENT 'how long must I work',
+  `late_time` time DEFAULT NULL COMMENT 'how long can I be late',
+  `create_uid` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_uid` bigint(20) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
