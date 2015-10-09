0cd373155b1597a3c3cd6d0c465c4bd4b469454e
wuhan <han.wu@zuolin.com>
2015-10-09 14:40:12 +0800

    punch tables modify

--- db/techpark-schema.sql
+++ db/techpark-schema.sql
@@ -2,8 +2,6 @@
 USE `ehcore`;
 
 
-USE `ehcore`;
-
 
 
 /*Table structure for table `eh_company_phone_list` */
@@ -67,7 +65,7 @@ CREATE TABLE `eh_punch_day_logs` (
   `status` TINYINT NOT NULL DEFAULT 1 COMMENT 'NORMAL(0)BELATE(1)LEAVEEARLY(2)UNPUNCH(3)BLANDLE(4)ABSENCE(5)SICK(6)EXCHANGE(7)',
 	`creator_uid` bigint,
 	`create_time` datetime,
-	'view_flag'TINYINT NOT NULL DEFAULT 1 COMMENT 'is view(0) not view(1)',
+	`view_flag` TINYINT NOT NULL DEFAULT 1 COMMENT 'is view(0) not view(1)',
  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -128,7 +126,7 @@ CREATE TABLE `eh_punch_exception_requests` (
 	`create_time` datetime,
   `operator_uid` bigint,
   `operate_time` datetime,
-	'view_flag'TINYINT NOT NULL DEFAULT 1 COMMENT 'is view(0) not view(1)',
+	`view_flag` TINYINT NOT NULL DEFAULT 1 COMMENT 'is view(0) not view(1)',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -146,7 +144,7 @@ CREATE TABLE `eh_punch_exception_approvals` (
 	`create_time` datetime,
   `operator_uid` bigint,
   `operate_time` datetime,
-	'view_flag'TINYINT NOT NULL DEFAULT 1 COMMENT 'is view(0) not view(1)',
+	`view_flag` TINYINT NOT NULL DEFAULT 1 COMMENT 'is view(0) not view(1)',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
