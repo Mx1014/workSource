c6d63abcbb4082d2cbeaa3749c395c527bd6a02a
janson <gandancing@gmail.com>
2015-10-12 16:37:40 +0800

    add eh_push_messages and eh_push_message_results add createtime for results

--- db/ehcore-server-schema.sql
+++ db/ehcore-server-schema.sql
@@ -2532,7 +2532,7 @@ CREATE TABLE `eh_push_message_results` (
     `message_id` BIGINT NOT NULL,
     `user_id` BIGINT NOT NULL,
     `identifier_token` VARCHAR(128),
-    `create_time` datetime DEFAULT NULL
+    `send_time` datetime DEFAULT NULL
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 SET foreign_key_checks = 1;
