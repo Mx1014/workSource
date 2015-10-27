4e6e18b4a4c8822baa880c196f2ac117df3c3540
liujinwen <jinwen.liu@zuolin.com>
2015-10-27 15:49:00 +0800

    update ehrest-response 2015/10/27

--- ehrest-response/src/main/java/com/everhomes/launchpad/GetLastLaunchPadLayoutByVersionCodeRestResponse.java
+++ ehrest-response/src/main/java/com/everhomes/launchpad/GetLastLaunchPadLayoutByVersionCodeRestResponse.java
@@ -3,6 +3,7 @@
 // generated at 2015-10-21 17:44:18
 // generated at 2015-10-26 15:50:45
 // generated at 2015-10-27 13:49:25
+// generated at 2015-10-27 15:48:23
 package com.everhomes.launchpad;
 
 import com.everhomes.rest.RestResponseBase;
