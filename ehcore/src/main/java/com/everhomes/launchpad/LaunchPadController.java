18399511b516711a67d5cd0b4da6571c79568738
liujinwen <jinwen.liu@zuolin.com>
2015-11-09 20:46:27 +0800

    revert test checkout to 3.1.x

--- ehcore/src/main/java/com/everhomes/launchpad/LaunchPadController.java
+++ ehcore/src/main/java/com/everhomes/launchpad/LaunchPadController.java
@@ -40,7 +40,6 @@ public class LaunchPadController extends ControllerBase {
     @RequestMapping("getLaunchPadItems")
     @RestReturn(value=GetLaunchPadItemsCommandResponse.class)
     public RestResponse getLaunchPadItems(@Valid GetLaunchPadItemsCommand cmd,HttpServletRequest request,HttpServletResponse response) {
-        
     	
         GetLaunchPadItemsCommandResponse commandResponse = launchPadService.getLaunchPadItems(cmd,request);
         RestResponse resp =  new RestResponse(commandResponse);
