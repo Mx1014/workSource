cd4b26c9773b78a80c00c52cd8eb9efaad969f9c
liujinwen <jinwen.liu@zuolin.com>
2015-11-10 12:55:40 +0800

    update getBizSignature

--- ehrest/src/main/java/com/everhomes/user/GetSignatureCommandResponse.java
+++ ehrest/src/main/java/com/everhomes/user/GetSignatureCommandResponse.java
@@ -1,53 +1,28 @@
 package com.everhomes.user;
 
 import com.everhomes.util.StringHelper;
-
+/**
+ * <ul>
+ * 	<li>paramUrl</li>
+ * </ul>
+ * @author Administrator
+ *
+ */
 public class GetSignatureCommandResponse {
-	private Long id;
-	 private String name;
-	 private String signature;
-	 private String appKey;
-	 private Long timeStamp;
-	 private Integer randomNum;
-	 public Long getId() {
-	  return id;
-	 }
-	 public void setId(Long id) {
-	  this.id = id;
-	 }
-	 public String getName() {
-	  return name;
-	 }
-	 public void setName(String name) {
-	  this.name = name;
-	 }
-	 public String getSignature() {
-	  return signature;
-	 }
-	 public void setSignature(String signature) {
-	  this.signature = signature;
-	 }
-	 public String getAppKey() {
-	  return appKey;
-	 }
-	 public void setAppKey(String appKey) {
-	  this.appKey = appKey;
-	 }
-	 public Long getTimeStamp() {
-	  return timeStamp;
-	 }
-	 public void setTimeStamp(Long timeStamp) {
-	  this.timeStamp = timeStamp;
-	 }
-	 public Integer getRandomNum() {
-	  return randomNum;
-	 }
-	 public void setRandomNum(Integer randomNum) {
-	  this.randomNum = randomNum;
-	 }
-	 @Override
-	 public String toString() {
-	  return StringHelper.toJsonString(this);
-	 }
+
+	private String paramUrl;
+	
+	public String getParamUrl() {
+		return paramUrl;
+	}
+
+	public void setParamUrl(String paramUrl) {
+		this.paramUrl = paramUrl;
+	}
+
+	@Override
+	public String toString() {
+		return StringHelper.toJsonString(this);
+	}
 
 }
