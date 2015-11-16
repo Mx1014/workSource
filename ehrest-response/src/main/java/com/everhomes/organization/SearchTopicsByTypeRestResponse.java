c7007bf7cbee330af5bd0ef60c94b7597ffd5bcb
LiangQishi <qishi.liang@zuolin.com>
2015-11-16 17:46:03 +0800

    code gen

---
+++ ehrest-response/src/main/java/com/everhomes/organization/SearchTopicsByTypeRestResponse.java
@@ -0,0 +1,23 @@
+// @formatter:off
+// generated file: DO NOT EDIT
+package com.everhomes.organization;
+
+import com.everhomes.rest.RestResponseBase;
+
+import com.everhomes.organization.SearchTopicsByTypeResponse;
+
+public class SearchTopicsByTypeRestResponse extends RestResponseBase {
+
+    private SearchTopicsByTypeResponse response;
+
+    public SearchTopicsByTypeRestResponse () {
+    }
+
+    public SearchTopicsByTypeResponse getResponse() {
+        return response;
+    }
+
+    public void setResponse(SearchTopicsByTypeResponse response) {
+        this.response = response;
+    }
+}
