53ff2b250859bfeb2649d30214c490be7a9efc5d
LiangQishi <qishi.liang@zuolin.com>
2015-11-20 09:53:04 +0800

    change the api name

--- ehcore/src/main/java/com/everhomes/namespace/NamespaceResourceController.java
+++ ehcore/src/main/java/com/everhomes/namespace/NamespaceResourceController.java
@@ -1,11 +1,6 @@
 // @formatter:off
 package com.everhomes.namespace;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -17,21 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.everhomes.constants.ErrorCodes;
 import com.everhomes.controller.ControllerBase;
 import com.everhomes.discover.RestReturn;
-import com.everhomes.organization.ListOrganizationCommunityCommand;
 import com.everhomes.organization.ListOrganizationCommunityCommandResponse;
-import com.everhomes.organization.ListOrganizationCommunityV2CommandResponse;
-import com.everhomes.region.ListRegionCommand;
-import com.everhomes.region.Region;
-import com.everhomes.region.RegionAdminStatus;
-import com.everhomes.region.RegionDTO;
-import com.everhomes.region.RegionProvider;
-import com.everhomes.region.RegionScope;
 import com.everhomes.rest.RestResponse;
-import com.everhomes.util.ConvertHelper;
-import com.everhomes.util.EtagHelper;
-import com.everhomes.util.SortOrder;
-import com.everhomes.util.Tuple;
-import com.everhomes.util.RequireAuthentication;
 
 /**
  * Namespace resource REST API controller
@@ -51,7 +33,7 @@ public class NamespaceResourceController extends ControllerBase {
      * <p>根据命名空间下列出机构所管辖的小区信息</p>
      */
     //checked
-    @RequestMapping("listOrganizationCommunitiesV2")
+    @RequestMapping("listCommunityByNamespace")
     @RestReturn(value=ListOrganizationCommunityCommandResponse.class)
     public RestResponse listCommunityByNamespace(@Valid ListCommunityByNamespaceCommand cmd) {
         ListCommunityByNamespaceCommandResponse commandResponse = namespaceResourceService.listCommunityByNamespace(cmd);
