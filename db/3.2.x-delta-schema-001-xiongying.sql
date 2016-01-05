721da5d226142846e5e42f5079a82e5c3d9b28b4
ying.xiong <ying.xiong@zuolin.com>
2016-01-05 15:47:06 +0800

    modify conf schema

---
+++ db/3.2.x-delta-schema-001-xiongying.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `eh_conf_account_categories` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_invoices` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_orders` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_order_account_map` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_order_account_map` ADD COLUMN `conf_account_namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_source_accounts` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_warning_contacts` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+
+ALTER TABLE `eh_conf_accounts` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_account_histories` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_conferences` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_enterprises` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_reservations` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
