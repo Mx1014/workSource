ba90fc184ad17a37fa3276bba31ca5f31bc0d056
yanshaofan <sf.yan@zuolin.com>
2016-01-15 17:43:26 +0800

    sql

---
+++ db/3.3.x-delta-schema-002-yanshaofan.sql
@@ -0,0 +1,14 @@
+#
+# 组织架构和角色关系表
+#
+DROP TABLE IF EXISTS `eh_organization_role_map`;
+CREATE TABLE `eh_organization_role_map`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `owner_type` varchar(64),
+    `owner_id` BIGINT,
+    `role_id` BIGINT NOT NULL,
+    `private_flag` TINYINT(4) NOT NULL DEFAULT 0 COMMENT '0: public, 1: private',
+    `status` TINYINT(4) NOT NULL DEFAULT '2' COMMENT '0: inactive, 2: active',
+    `create_time` DATETIME,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
