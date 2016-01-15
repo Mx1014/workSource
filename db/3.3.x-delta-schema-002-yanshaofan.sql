d0785546443eda68c9cb14966100171c08084b57
LiangQishi <qishi.liang@zuolin.com>
2016-01-15 19:03:00 +0800

    change the details

--- db/3.3.x-delta-schema-002-yanshaofan.sql
+++ db/3.3.x-delta-schema-002-yanshaofan.sql
@@ -1,14 +1,15 @@
 #
-# 组织架构和角色关系表
+# member of global parition
+# the relationship between owner and role, owner can be organization, enterprise, etc
 #
 DROP TABLE IF EXISTS `eh_organization_role_map`;
 CREATE TABLE `eh_organization_role_map`(
-    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL,
     `owner_type` VARCHAR(64),
-    `owner_id` BIGINT,
-    `role_id` BIGINT NOT NULL,
-    `private_flag` TINYINT(4) NOT NULL DEFAULT 0 COMMENT '0: public, 1: private',
-    `status` TINYINT(4) NOT NULL DEFAULT '2' COMMENT '0: inactive, 2: active',
+    `owner_id` BIGINT NOT NULL DEFAULT 0,
+    `role_id` BIGINT NOT NULL DEFAULT 0,
+    `private_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: public, 1: private',
+    `status` TINYINT NOT NULL DEFAULT 2 COMMENT '0: inactive, 2: active',
     `create_time` DATETIME,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
