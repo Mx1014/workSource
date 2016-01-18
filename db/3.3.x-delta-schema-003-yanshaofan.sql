b4af938ff91eb6455bfcb56a875c14340709706b
yanshaofan <sf.yan@zuolin.com>
2016-01-18 20:42:00 +0800

    sql

--- db/3.3.x-delta-schema-003-yanshaofan.sql
+++ db/3.3.x-delta-schema-003-yanshaofan.sql
@@ -11,11 +11,12 @@ ALTER TABLE `eh_enterprise_details` ADD COLUMN `post_uri` VARCHAR(128) COMMENT '
 #
 # eh_organization_members
 #
-ALTER TABLE `eh_organization_members` ADD COLUMN `employee_no` BIGINT NOT NULL DEFAULT 0 COMMENT '';
-ALTER TABLE `eh_organization_members` ADD COLUMN `gender` TINYINT NOT NULL DEFAULT '0' COMMENT '0: undisclosured, 1: male, 2: female';
+ALTER TABLE `eh_organization_members` ADD COLUMN `employee_no` BIGINT COMMENT '';
+ALTER TABLE `eh_organization_members` ADD COLUMN `gender` TINYINT DEFAULT '0' COMMENT '0: undisclosured, 1: male, 2: female';
 
 #
 # eh_organizations
 #
 ALTER TABLE `eh_organizations` ADD COLUMN `create_time` DATETIME COMMENT '';
 ALTER TABLE `eh_organizations` ADD COLUMN `update_time` DATETIME COMMENT '';
+ALTER TABLE `eh_organizations` ADD COLUMN `parent_enterprise_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'directly under the company';
