4cf9bba13ff5d3c8c013ab4346dae9e24ff496db
yanshaofan <sf.yan@zuolin.com>
2016-01-18 17:06:47 +0800

    sql

---
+++ db/3.3.x-delta-schema-003-yanshaofan.sql
@@ -0,0 +1,21 @@
+#
+# eh_enterprise_details
+#
+ALTER TABLE `eh_enterprise_details` ADD COLUMN `display_name` VARCHAR(64)  COMMENT ''; 
+ALTER TABLE `eh_enterprise_details` ADD COLUMN `contactor` VARCHAR(64)  COMMENT ''; 
+ALTER TABLE `eh_enterprise_details` ADD COLUMN `member_count` BIGINT DEFAULT 0 COMMENT '';
+ALTER TABLE `eh_enterprise_details` ADD COLUMN `checkin_date` DATETIME COMMENT 'checkin date';
+ALTER TABLE `eh_enterprise_details` ADD COLUMN `avatar` VARCHAR(128) COMMENT '';
+ALTER TABLE `eh_enterprise_details` ADD COLUMN `post_uri` VARCHAR(128) COMMENT '';
+
+#
+# eh_organization_members
+#
+ALTER TABLE `eh_organization_members` ADD COLUMN `employee_no` BIGINT NOT NULL DEFAULT 0 COMMENT '';
+ALTER TABLE `eh_organization_members` ADD COLUMN `gender` TINYINT NOT NULL DEFAULT '0' COMMENT '0: undisclosured, 1: male, 2: female';
+
+#
+# eh_organizations
+#
+ALTER TABLE `eh_organizations` ADD COLUMN `create_time` DATETIME COMMENT '';
+ALTER TABLE `eh_organizations` ADD COLUMN `update_time` DATETIME COMMENT '';
