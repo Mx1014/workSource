cd38554ebe552083900d39bf15117790f21d6f08
yanshaofan <sf.yan@zuolin.com>
2016-01-26 14:08:25 +0800

    sql

--- db/3.3.x-delta-schema-003-yanshaofan.sql
+++ db/3.3.x-delta-schema-003-yanshaofan.sql
@@ -40,7 +40,6 @@ ALTER TABLE `eh_organization_members` ADD COLUMN `string_tag5` VARCHAR(128);
 ALTER TABLE `eh_organizations` ADD COLUMN `create_time` DATETIME COMMENT '';
 ALTER TABLE `eh_organizations` ADD COLUMN `update_time` DATETIME COMMENT '';
 ALTER TABLE `eh_organizations` ADD COLUMN `directly_enterprise_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'directly under the company';
-ALTER TABLE `eh_organizations` ADD COLUMN `namespace_id` INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE `eh_organizations` ADD COLUMN `integral_tag1` BIGINT;
 ALTER TABLE `eh_organizations` ADD COLUMN `integral_tag2` BIGINT;
 ALTER TABLE `eh_organizations` ADD COLUMN `integral_tag3` BIGINT;
@@ -51,3 +50,9 @@ ALTER TABLE `eh_organizations` ADD COLUMN `string_tag2` VARCHAR(128);
 ALTER TABLE `eh_organizations` ADD COLUMN `string_tag3` VARCHAR(128);
 ALTER TABLE `eh_organizations` ADD COLUMN `string_tag4` VARCHAR(128);
 ALTER TABLE `eh_organizations` ADD COLUMN `string_tag5` VARCHAR(128);
+
+ALTER TABLE `eh_organization_members` DROP `group_id`;
+ALTER TABLE `eh_organization_members` ADD COLUMN `group_id` BIGINT DEFAULT 0 COMMENT 'refer to the organization id';
+ALTER TABLE `eh_organizations` ADD COLUMN `namespace_id` INTEGER NOT NULL DEFAULT 0 AFTER directly_enterprise_id;
+
+
