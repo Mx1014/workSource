27e2821faf4592fd32ac11df6d03bc3132cb6d12
ying.xiong <ying.xiong@zuolin.com>
2016-01-28 15:47:35 +0800

    modify

--- db/3.2.x-delta-schema-001-xiongying.sql
+++ db/3.2.x-delta-schema-001-xiongying.sql
@@ -4,7 +4,6 @@ ALTER TABLE `eh_conf_orders` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT 
 ALTER TABLE `eh_conf_order_account_map` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
 ALTER TABLE `eh_conf_order_account_map` ADD COLUMN `conf_account_namespace_id` int(11) NOT NULL DEFAULT '0';
 ALTER TABLE `eh_conf_source_accounts` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
-ALTER TABLE `eh_warning_contacts` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
 
 ALTER TABLE `eh_conf_accounts` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
 ALTER TABLE `eh_conf_account_histories` ADD COLUMN `namespace_id` int(11) NOT NULL DEFAULT '0';
