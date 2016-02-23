733de7fb9578b63588fe8eccf3e4923934ffbcd5
ying.xiong <ying.xiong@zuolin.com>
2016-02-23 19:08:20 +0800

    add sql

--- db/3.2.x-delta-data-003-xiongying.sql
+++ db/3.2.x-delta-data-003-xiongying.sql
@@ -1,4 +1,5 @@
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'videoConf', '10006', 'zh_CN', '左邻');
+INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'videoConf', '10007', 'zh_CN', '现在还没有召开会议');
 
 INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'videoconf.notification', 1, 'zh_CN', '会议邀请，邮件主题', '您被${userName}邀请出席左邻视频会议 ${confName}');
 
