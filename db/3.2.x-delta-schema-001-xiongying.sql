f6630b4b31609938534b9b6c23ae88b4e028eb4a
ying.xiong <ying.xiong@zuolin.com>
2016-02-24 19:13:27 +0800

    modify schema

--- db/3.2.x-delta-schema-001-xiongying.sql
+++ db/3.2.x-delta-schema-001-xiongying.sql
@@ -18,5 +18,5 @@ INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'video
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'videoConf', '10004', 'zh_CN', '用户数大于账户数');
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'videoConf', '10005', 'zh_CN', '账号在3个月内已分配过用户');
 
-ALTER TABLE `eh_conf_conferences` ADD COLUMN `meeting_no` bigint(20) NOT NULL DEFAULT '0' COMMENT 'the meeting no from 3rd conference provider';
-ALTER TABLE `eh_conf_conferences` CHANGE `conf_id` `conference_id` bigint(20) NOT NULL DEFAULT '0';
+ALTER TABLE `eh_conf_conferences` ADD COLUMN `conference_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'the conference id from 3rd conference provider';
+ALTER TABLE `eh_conf_conferences` CHANGE `conf_id` `meeting_no` bigint(20) NOT NULL DEFAULT '0'COMMENT 'the meeting no from 3rd conference provider';
