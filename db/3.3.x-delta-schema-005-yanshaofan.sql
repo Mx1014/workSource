effa5c8d0d48df9ad9b340f23800395c33da416c
yanshaofan <sf.yan@zuolin.com>
2016-02-25 19:29:16 +0800

    organization data sql

--- db/3.3.x-delta-schema-005-yanshaofan.sql
+++ db/3.3.x-delta-schema-005-yanshaofan.sql
@@ -51,15 +51,19 @@ SELECT  (@organization_details_id := @organization_details_id + 1),`id`,`descrip
 # update organization data
 #
 
-UPDATE `eh_organizations` SET path = CONCAT('/',id),directly_enterprise_id = id,`update_time`= now(), group_type='ENTERPRISE' WHERE path = CONCAT('/',name);
+UPDATE `eh_organizations` SET path = CONCAT('/',id),0,`update_time`= now(), group_type='ENTERPRISE' WHERE path = CONCAT('/',name);
 
 UPDATE `eh_organizations` SET path = CONCAT('/',parent_id,'/',id),directly_enterprise_id = parent_id,`update_time`= now(),group_type='DEPARTMENT' WHERE path = CONCAT('/',parent_id,'/',name);
 
-UPDATE `eh_organizations` SET directly_enterprise_id = id WHERE path LIKE CONCAT('/',id,'/%');
+UPDATE `eh_organizations` SET directly_enterprise_id = 1000001 WHERE path LIKE CONCAT('/',1000001,'/%');
 
 UPDATE `eh_organizations` SET path = (SELECT b.path1 FROM (SELECT id,a.path,CONCAT('/',a.directly_enterprise_id,'/',(SELECT id FROM `eh_organizations` WHERE name = substring_index(path1,'/',1) AND parent_id = a.directly_enterprise_id limit 1),'/',a.id) path1 FROM (SELECT id,parent_id,directly_enterprise_id,path,substring_index(path, '/', -2) path1 FROM `eh_organizations` WHERE substring_index(path, '/', -1) IN (SELECT name FROM `eh_organizations`)) a
-) b WHERE b.id= id limit 1),`update_time`= now(),group_type='DEPARTMENT' WHERE id IN (SELECT id FROM (WHERE id,parent_id,directly_enterprise_id,path,substring_index(path, '/', -2) path1 FROM `eh_organizations` WHERE substring_index(path, '/', -1) IN (SELECT name FROM `eh_organizations`)) a)
+) b WHERE b.id= id limit 1),`update_time`= now(),group_type='DEPARTMENT' WHERE id IN (SELECT id FROM (SELECT id,parent_id,directly_enterprise_id,path,substring_index(path, '/', -2) path1 FROM `eh_organizations` WHERE substring_index(path, '/', -1) IN (SELECT name FROM `eh_organizations`)) a);
 
+UPDATE `eh_organizations` SET group_type='ENTERPRISE' WHERE parent_id = 0;
+UPDATE `eh_organizations` SET group_type='DEPARTMENT' WHERE group_type IS NULL;
+
+UPDATE `eh_organizations` SET directly_enterprise_id = substring_index(substring_index(path,'/',2),'/',-1) WHERE group_type = 'DEPARTMENT';
 
 #
 # merge department and organization data
@@ -95,7 +99,7 @@ SELECT  @organization_addresses_id + `id`, `enterprise_id`,`address_id`,`status`
 #
 INSERT INTO `eh_organization_attachments`
 (`id`,`organization_id`,`content_type`,`content_uri`,`creator_uid`,`create_time`)
-SELECT @organization_attachments_id + `id`,`organization_id`,`content_type`,`content_uri`,`creator_uid`,`create_time`  FROM `eh_organization_attachments`;
+SELECT @organization_attachments_id + `id`,`organization_id`,`content_type`,`content_uri`,`creator_uid`,`create_time`  FROM `eh_enterprise_attachments`;
 
 #
 # move enterprise community requests information
@@ -113,6 +117,5 @@ INSERT INTO `eh_preferential_rules` (`id`,`owner_type`,`owner_id`,`start_time`,`
 
 INSERT INTO `eh_organization_role_map` (`id`,`owner_type`,`owner_id`,`role_id`,`private_flag`,`status`,`create_time`)
 SELECT  (@organization_role_map_id := @organization_role_map_id + 1),'EhOrganizations',0,`id`,0,2,now() FROM `eh_acl_roles` WHERE `app_id` = 32;
-
 
 
