b825818f48c884bc972fc24b3ec0939720ea8ef2
LiangQishi <qishi.liang@zuolin.com>
2016-03-02 10:40:08 +0800

    add two tables: eh_promotion_activity_triggers, eh_promotion_activities

--- db/3.3.x-delta-schema-006-liangqishi.sql
+++ db/3.3.x-delta-schema-006-liangqishi.sql
@@ -18,3 +18,47 @@ CREATE TABLE `eh_scenes`(
 ALTER TABLE `eh_launch_pad_layouts` ADD COLUMN `scene` VARCHAR(64) NOT NULL DEFAULT 'default';
 ALTER TABLE `eh_launch_pad_items` ADD COLUMN `scene` VARCHAR(64) NOT NULL DEFAULT 'default';
 ALTER TABLE `eh_banners` ADD COLUMN `scene` VARCHAR(64) NOT NULL DEFAULT 'default';
+
+#
+# member of global parition
+# shared among namespaces, no application module specific information
+#
+DROP TABLE IF EXISTS `eh_promotion_activity_triggers`;
+CREATE TABLE `eh_promotion_activity_triggers`(
+    `id` BIGINT NOT NULL,
+    `namespace_id` INTEGER NOT NULL DEFAULT 0,
+    `title` VARCHAR(512) NOT NULL DEFAULT 0 COMMENT 'the title of the activity',
+	`trigger_type` TINYINT NOT NULL DEFAULT 1 COMMENT '0: none, 1: trigger at start time, 2: trigger by register, 3: trigger by biz cost',
+	`trigger_data` VARCHAR(1024) COMMENT 'json format, the parameters which help trigger type to decine when to start the activity',
+	`start_time` DATETIME,
+    `end_time` DATETIME,
+	`scope_code` TINYINT NOT NULL DEFAULT 0 COMMENT '0: all, 1: community, 2: city, 3: user',
+    `scope_id` BIGINT,
+	`action_type` TINYINT NOT NULL DEFAULT 0 COMMENT 'according to document',
+    `action_data` TEXT COMMENT 'the parameters depend on item_type, json format',
+	`valid_count` INTEGER NOT NULL DEFAULT 0 COMMENT '0: unlimit, 1: only once, others',
+    `creator_uid` BIGINT NOT NULL DEFAULT 0 COMMENT 'record creator user id',
+    `create_time` DATETIME,
+    
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+#
+# member of global parition
+# shared among namespaces, no application module specific information
+#
+DROP TABLE IF EXISTS `eh_promotion_activities`;
+CREATE TABLE `eh_promotion_activities`(
+    `id` BIGINT NOT NULL,
+    `namespace_id` INTEGER NOT NULL DEFAULT 0,
+	`trigger_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'refer to the id of eh_promotion_activity_triggers',
+    `title` VARCHAR(512) NOT NULL DEFAULT 0 COMMENT 'the title of the activity',
+	`scope_code` TINYINT NOT NULL DEFAULT 0 COMMENT '0: all, 1: community, 2: city, 3: user',
+    `scope_id` BIGINT,
+	`action_type` TINYINT NOT NULL DEFAULT 0 COMMENT 'according to document',
+    `action_data` TEXT COMMENT 'the parameters depend on item_type, json format',    
+	`valid_count` INTEGER NOT NULL DEFAULT 0 COMMENT '0: unlimit, 1: only once, others',
+    `create_time` DATETIME,
+    
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
