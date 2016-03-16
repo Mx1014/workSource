b07fc4a11c93f2de2a50e976ee8df352c89fe191
yanshaofan <sf.yan@zuolin.com>
2016-03-16 10:19:14 +0800

    sql

--- db/3.3.x-delta-schema-007-yanshaofan.sql
+++ db/3.3.x-delta-schema-007-yanshaofan.sql
@@ -1,3 +1,5 @@
 
 ALTER TABLE `eh_organization_tasks` ADD COLUMN `task_category` VARCHAR(128) COMMENT '1:PUBLIC_AREA 2:PRIVATE_OWNER';
 
+
+ALTER TABLE `eh_organization_members` DROP COLUMN `namespace_id`;
