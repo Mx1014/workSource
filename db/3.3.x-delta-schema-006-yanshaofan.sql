a49cb700c589ec1a9afa0e771ba7f7ac5e9062f2
juan.xu <juan.xu@zuolin.com>
2016-03-17 09:40:28 +0800

    sql

--- db/3.3.x-delta-schema-006-yanshaofan.sql
+++ db/3.3.x-delta-schema-006-yanshaofan.sql
@@ -2,3 +2,4 @@ set @organization_member_id = 2000000;
 
 update `eh_organization_members` em set `integral_tag4` = (select `integral_tag1` from `eh_enterprise_contacts` where id = em.id - @organization_member_id);
 
+ALTER TABLE `eh_organization_members` MODIFY COLUMN `namespace_id` INTEGER DEFAULT 0;
