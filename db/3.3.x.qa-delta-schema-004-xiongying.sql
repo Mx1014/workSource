38844692396c09725f4bb675134554076ce79586
ying.xiong <ying.xiong@zuolin.com>
2016-03-23 15:33:22 +0800

    modify schema

--- db/3.3.x.qa-delta-schema-004-xiongying.sql
+++ db/3.3.x.qa-delta-schema-004-xiongying.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `eh_quality_inspection_task_attachments` ADD COLUMN `task_type` TINYINT NOT NULL DEFAULT 0 COMMENT '0: none, 1: verify task, 2: rectify task';
+ALTER TABLE `eh_quality_inspection_task_attachments` CHANGE `task_id` `record_id` bigint(20) NOT NULL DEFAULT '0'COMMENT 'refernece to the id of eh_quality_inspection_task_records';
