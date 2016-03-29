59c7a7ea87ff311d699508be2b2c12c049632915
ying.xiong <ying.xiong@zuolin.com>
2016-03-29 17:54:06 +0800

    modify schema

--- db/3.3.x-delta-schema-001-xiongying.sql
+++ db/3.3.x-delta-schema-001-xiongying.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `eh_conf_orders` ADD COLUMN `buyer_name` VARCHAR(128);
 ALTER TABLE `eh_conf_orders` ADD COLUMN `buyer_contact` VARCHAR(128);
-ALTER TABLE `eh_conf_orders` ADD COLUMN `extend_flag` TINYINT NOT NULL DEFAULT 0 COMMENT 'buy new accounts or extend accounts 0: new accounts, 1: extend account';
 ALTER TABLE `eh_conf_orders` ADD COLUMN `vendor_type` TINYINT NOT NULL DEFAULT 0 COMMENT 'vendor type 0: none, 1: Alipay, 2: Wechat';
 
 ALTER TABLE `eh_conf_account_categories` ADD COLUMN `online_flag` TINYINT NOT NULL DEFAULT 0 COMMENT 'is visible online 0: no, 1: yes';
+ALTER TABLE `eh_conf_account_categories` CHANGE `channel_type` `mutiple_num` int not null default 0 comment 'the limit value of mutiple buy channel';
