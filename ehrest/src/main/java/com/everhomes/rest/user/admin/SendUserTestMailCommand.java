afde2c708006446a97f286f94d229875146c3250
LiangQishi <qishi.liang@zuolin.com>
2016-04-01 18:23:47 +0800

    implement send mail function

---
+++ ehrest/src/main/java/com/everhomes/rest/user/admin/SendUserTestMailCommand.java
@@ -0,0 +1,80 @@
+package com.everhomes.rest.user.admin;
+
+import com.everhomes.util.StringHelper;
+
+public class SendUserTestMailCommand {
+	private Integer namespaceId;
+    
+	private String from;
+	
+	private String to;
+	
+	private String subject;
+	
+	private String body;
+	
+	private String attachment1;
+	
+	private String attachment2;
+
+    public Integer getNamespaceId() {
+        return namespaceId;
+    }
+
+    public void setNamespaceId(Integer namespaceId) {
+        this.namespaceId = namespaceId;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAttachment1() {
+        return attachment1;
+    }
+
+    public void setAttachment1(String attachment1) {
+        this.attachment1 = attachment1;
+    }
+
+    public String getAttachment2() {
+        return attachment2;
+    }
+
+    public void setAttachment2(String attachment2) {
+        this.attachment2 = attachment2;
+    }
+
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
+}
