73f73b54e33ec0ac0481aa75256658757b34e7c1
LiangQishi <qishi.liang@zuolin.com>
2016-04-05 11:40:00 +0800

    move the acount info of sending mail to configurations

---
+++ db/3.3.x-delta-data-002-liangqishi.sql
@@ -0,0 +1,5 @@
+INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (0, 'mail.smtp.address', 'smtp.mxhichina.com', '访问邮件SMTP服务器的地址');
+INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (0, 'mail.smtp.port', '25', '访问邮件SMTP服务器的端口号');
+INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (0, 'mail.smtp.account', '', '系统邮件发送者帐号');
+INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (0, 'mail.smtp.passwod', '', '系统邮件发送者帐号密码');
+	
