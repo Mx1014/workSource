c72179220d4d16528f64bebe8ab26be64ca0682d
LiangQishi <qishi.liang@zuolin.com>
2016-04-06 15:03:26 +0800

    implement getUserOpPromotionsByScene

---
+++ ehrest/src/main/java/com/everhomes/rest/scheduler/ScheduleServiceErrorCode.java
@@ -0,0 +1,7 @@
+package com.everhomes.rest.scheduler;
+
+public interface ScheduleServiceErrorCode {
+    static final String SCOPE = "schedule";
+    
+    static final int ERROR_SCHEDULE_JOB_FAILED = 10001;
+}
