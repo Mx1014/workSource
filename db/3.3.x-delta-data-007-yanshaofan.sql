457fec95f7659dc826f463fdc25d38150ecf2189
yanshaofan <sf.yan@zuolin.com>
2016-04-06 11:35:08 +0800

    sql

--- db/3.3.x-delta-data-007-yanshaofan.sql
+++ db/3.3.x-delta-data-007-yanshaofan.sql
@@ -1,11 +1,11 @@
 
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 11, 'zh_CN','接受任务时回复的帖子消息', '该任务已由${targetUName}（${targetUToken}）确认，将会很快联系您。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 12, 'zh_CN', '新发布一条任务短信消息','您有一个新的任务，请尽快处理。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 13, 'zh_CN', '处理任务时发送的短信消息','${operatorUName}已分配给你一个物业任务，请尽快联系业主（${createUName}）处理。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 14, 'zh_CN', '处理任务时回复的帖子消息','${operatorUName}（${operatorUToken}）已将该任务指派给${targetUName}处理，请等待。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 15, 'zh_CN', '任务被拒绝收到的短信消息','该任务已被${targetUName}（${targetUToken}）拒绝，请重新分配');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 16, 'zh_CN', '任务已完成后的短信消息','您的服务已完成，滑动去查看详情');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 17, 'zh_CN', '任务已完成后回复的帖子消息','该服务已由${operatorUName}完成，稍后我们会将进行回访');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 10, 'zh_CN','接受任务时回复的帖子消息', '该任务已由${targetUName}（${targetUToken}）确认，将会很快联系您。');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 11, 'zh_CN', '新发布一条任务短信消息','您有一个新的任务，请尽快处理。');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 12, 'zh_CN', '处理任务时发送的短信消息','${operatorUName}已分配给你一个物业任务，请尽快联系业主（${createUName}）处理。');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 13, 'zh_CN', '处理任务时回复的帖子消息','${operatorUName}（${operatorUToken}）已将该任务指派给${targetUName}处理，请等待。');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 14, 'zh_CN', '任务被拒绝收到的短信消息','该任务已被${targetUName}（${targetUToken}）拒绝，请重新分配');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 15, 'zh_CN', '任务已完成后的短信消息','您的服务已完成，滑动去查看详情');
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 16, 'zh_CN', '任务已完成后回复的帖子消息','该服务已由${operatorUName}完成，稍后我们会将进行回访');
 
 delete from eh_launch_pad_items where id = 10006; 
 INSERT INTO `eh_launch_pad_items`(`id`, `scene_type`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`) 
