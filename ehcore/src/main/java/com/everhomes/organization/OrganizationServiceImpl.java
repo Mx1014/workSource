226bdd1836bbeeb7616732f8ed1eee11a8a3195c
yanshaofan <sf.yan@zuolin.com>
2016-04-07 13:53:17 +0800

    task

--- ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
@@ -5351,6 +5351,7 @@ public class OrganizationServiceImpl implements OrganizationService {
 	    	if(OrganizationTaskStatus.fromCode(task.getTaskStatus()) == OrganizationTaskStatus.UNPROCESSED
 	    			&& (task.getTargetId().equals(user.getId())
 	    			|| ((StringUtils.isEmpty(task.getTargetId()) || task.getTargetId() == 0)))){
+	    		task.setTargetId(user.getId());
 	    		task.setTaskStatus(OrganizationTaskStatus.PROCESSING.getCode());
 	    		task.setOperateTime(new Timestamp(DateHelper.currentGMTTime().getTime()));
 	    		task.setOperatorUid(user.getId());
