1aae1db7819f9178d462b8513b09256499dce285
ying.xiong <ying.xiong@zuolin.com>
2016-04-07 14:44:37 +0800

    add quality realm

--- ehrest/src/main/java/com/everhomes/rest/version/VersionRealmType.java
+++ ehrest/src/main/java/com/everhomes/rest/version/VersionRealmType.java
@@ -24,7 +24,7 @@ package com.everhomes.rest.version;
  * <li>IOS_LONGGANG("iOS_Longgang"): 龙岗智慧社区iOS版APP</li>
  * <li>ANDROID_HAIAN("Android_Haian"): 海岸城Android版APP</li>
  * <li>IOS_HAIAN("iOS_Haian"): 海岸城iOS版APP</li>
- * <li>H5_QUALITY("H5_Quality"): 品质核查H5</li>
+ * <li>H5_QUALITY("quality"): 品质核查H5</li>
  * </ul>
  */
 public enum VersionRealmType {
