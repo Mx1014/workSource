272e4381d21da99435ef262642a14747900c13bf
ying.xiong <ying.xiong@zuolin.com>
2016-04-08 17:03:08 +0800

    use autowired in QualityInspectionScheduleJob

--- ehcore/src/main/java/com/everhomes/scheduler/QualityInspectionScheduleJob.java
+++ ehcore/src/main/java/com/everhomes/scheduler/QualityInspectionScheduleJob.java
@@ -10,6 +10,7 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.everhomes.bootstrap.PlatformContext;
 import com.everhomes.quality.QualityInspectionStandards;
@@ -18,9 +19,19 @@ import com.everhomes.quality.QualityService;
 import com.everhomes.repeat.RepeatService;
 import com.everhomes.util.DateHelper;
 
+@Component
 public class QualityInspectionScheduleJob implements ScheduleJob {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(QualityInspectionScheduleJob.class);
+	
+	@Autowired
+	private QualityProvider qualityProvider;
+	
+	@Autowired
+	private QualityService qualityService;
+	
+	@Autowired
+	private RepeatService repeatService;
 	
 	
 	@Override
@@ -28,10 +39,6 @@ public class QualityInspectionScheduleJob implements ScheduleJob {
 		if(LOGGER.isInfoEnabled()) {
 			LOGGER.info("QualityInspectionScheduleJob" + new Timestamp(DateHelper.currentGMTTime().getTime()));
 		}
-		
-		QualityService qualityService = PlatformContext.getComponent(QualityService.class);
-		QualityProvider qualityProvider = PlatformContext.getComponent(QualityProvider.class);
-		RepeatService repeatService = PlatformContext.getComponent(RepeatService.class);
 		
 		List<QualityInspectionStandards> activeStandards = qualityProvider.listActiveStandards();
 		
