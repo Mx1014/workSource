dba75544df7a87ba233200e0a5e681a6362066fa
yanshaofan <sf.yan@zuolin.com>
2016-04-14 14:21:58 +0800

    send sms

--- db/3.3.x-delta-data-010-yanshaofan.sql
+++ db/3.3.x-delta-data-010-yanshaofan.sql
@@ -1,10 +1,6 @@
-
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 10, 'zh_CN','接受任务时回复的帖子消息', '该任务已由${targetUName}（${targetUToken}）确认，将会很快联系您。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 11, 'zh_CN', '新发布一条任务短信消息','您有一个新的任务，请尽快处理。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 12, 'zh_CN', '处理任务时发送的短信消息','${operatorUName}给你分配了一个任务，请直接联系用户${targetUName}（电话${targetUToken}），帮他处理该问题。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 13, 'zh_CN', '处理任务时回复的帖子消息','${operatorUName}（${operatorUToken}）已将该任务指派给${targetUName}处理，请等待。');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 14, 'zh_CN', '任务被拒绝收到的短信消息','该任务已被${targetUName}（${targetUToken}）拒绝，请重新分配');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 15, 'zh_CN', '任务已完成后的短信消息','您的服务已完成');
-INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`) VALUES( 'organization.notification', 16, 'zh_CN', '任务已完成后回复的帖子消息','该服务已由${operatorUName}完成，稍后我们会将进行回访');
-
-
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) VALUES( 'sms.default', 6, 'zh_CN', '处理任务时发送的短信','${operatorUName}给你分配了一个任务，请直接联系用户${targetUName}（电话${targetUToken}），帮他处理该问题。', 0);
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) VALUES( 'sms.default.yzx', 6, 'zh_CN', '处理任务时发送的短信-左邻',22716, 0);
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) VALUES( 'sms.default.yzx', 6, 'zh_CN', '处理任务时发送的短信-科技园',22717, 1000000);
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) VALUES( 'sms.default.yzx', 6, 'zh_CN', '处理任务时发送的短信-讯美',22718, 999999);
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) VALUES( 'sms.default.yzx', 6, 'zh_CN', '处理任务时发送的短信-金隅嘉业',22719, 999995);
+INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) VALUES( 'sms.default.yzx', 6, 'zh_CN', '处理任务时发送的短信-海岸城',22720, 999993);
