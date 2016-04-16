4a74f37a0fd06d8ab6e501706ae78b9d9cb3dd47
LiangQishi <qishi.liang@zuolin.com>
2016-04-16 14:57:55 +0800

    fix bug: check if the community id is null before building the searching condition

--- ehcore/src/main/java/com/everhomes/search/PostSearcherImpl.java
+++ ehcore/src/main/java/com/everhomes/search/PostSearcherImpl.java
@@ -278,25 +278,27 @@ public class PostSearcherImpl extends AbstractElasticSearch implements PostSearc
     private FilterBuilder getForumFilter(SearchTopicCommand cmd) {
         FilterBuilder fb = null;
         if(cmd.getSearchFlag() != null && cmd.getSearchFlag() == 1) {
-            GetNearbyCommunitiesByIdCommand nearCmd = new GetNearbyCommunitiesByIdCommand();
-            nearCmd.setId(cmd.getCommunityId());
-            Community community = communityProvider.findCommunityById(cmd.getCommunityId());
-            Long forumId = ForumConstants.SYSTEM_FORUM;
-            if(community != null) {
-                forumId = community.getDefaultForumId();
-            }
-            List<CommunityDTO> coms = communityService.getNearbyCommunityById(nearCmd);
-            List<Long> comIds = new ArrayList<Long>();
-            for(CommunityDTO cDTO : coms) {
-                comIds.add(cDTO.getId());
-                }
-            
             FilterBuilder comFilter = null;
-            if(comIds.size() > 0) {
-                FilterBuilder comIn = boolInFilter("visibleRegionId", comIds);
-                FilterBuilder comType = FilterBuilders.termFilter("visibleRegionType", (long)VisibleRegionType.COMMUNITY.getCode());
-                FilterBuilder comForum = FilterBuilders.termFilter("forumId", forumId);
-                comFilter = FilterBuilders.boolFilter().must(comIn, comType, comForum);
+            if(cmd.getCommunityId() != null) {
+                GetNearbyCommunitiesByIdCommand nearCmd = new GetNearbyCommunitiesByIdCommand();
+                nearCmd.setId(cmd.getCommunityId());
+                Community community = communityProvider.findCommunityById(cmd.getCommunityId());
+                Long forumId = ForumConstants.SYSTEM_FORUM;
+                if(community != null) {
+                    forumId = community.getDefaultForumId();
+                }
+                List<CommunityDTO> coms = communityService.getNearbyCommunityById(nearCmd);
+                List<Long> comIds = new ArrayList<Long>();
+                for(CommunityDTO cDTO : coms) {
+                    comIds.add(cDTO.getId());
+                    }
+                
+                if(comIds.size() > 0) {
+                    FilterBuilder comIn = boolInFilter("visibleRegionId", comIds);
+                    FilterBuilder comType = FilterBuilders.termFilter("visibleRegionType", (long)VisibleRegionType.COMMUNITY.getCode());
+                    FilterBuilder comForum = FilterBuilders.termFilter("forumId", forumId);
+                    comFilter = FilterBuilders.boolFilter().must(comIn, comType, comForum);
+                }
             }
             
             List<GroupDTO> groups = groupService.listUserRelatedGroups();
