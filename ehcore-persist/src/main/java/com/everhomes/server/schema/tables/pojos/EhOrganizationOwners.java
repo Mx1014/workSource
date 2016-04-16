df7f599f309bb5ee8c2561e893d567429f262cc5
xujuan <juan.xu@zuolin.com>
2016-04-16 14:49:34 +0800

    add column community_id to eh_organization_owners

--- ehcore-persist/src/main/java/com/everhomes/server/schema/tables/pojos/EhOrganizationOwners.java
+++ ehcore-persist/src/main/java/com/everhomes/server/schema/tables/pojos/EhOrganizationOwners.java
@@ -11,7 +11,7 @@ package com.everhomes.server.schema.tables.pojos;
 @java.lang.SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class EhOrganizationOwners implements java.io.Serializable {
 
-	private static final long serialVersionUID = 1964327138;
+	private static final long serialVersionUID = 2053877924;
 
 	private java.lang.Long     id;
 	private java.lang.Long     organizationId;
@@ -24,6 +24,7 @@ public class EhOrganizationOwners implements java.io.Serializable {
 	private java.lang.Long     creatorUid;
 	private java.sql.Timestamp createTime;
 	private java.lang.Integer  namespaceId;
+	private java.lang.Long     communityId;
 
 	public EhOrganizationOwners() {}
 
@@ -38,7 +39,8 @@ public class EhOrganizationOwners implements java.io.Serializable {
 		java.lang.String   address,
 		java.lang.Long     creatorUid,
 		java.sql.Timestamp createTime,
-		java.lang.Integer  namespaceId
+		java.lang.Integer  namespaceId,
+		java.lang.Long     communityId
 	) {
 		this.id = id;
 		this.organizationId = organizationId;
@@ -51,6 +53,7 @@ public class EhOrganizationOwners implements java.io.Serializable {
 		this.creatorUid = creatorUid;
 		this.createTime = createTime;
 		this.namespaceId = namespaceId;
+		this.communityId = communityId;
 	}
 
 	public java.lang.Long getId() {
@@ -139,5 +142,13 @@ public class EhOrganizationOwners implements java.io.Serializable {
 
 	public void setNamespaceId(java.lang.Integer namespaceId) {
 		this.namespaceId = namespaceId;
+	}
+
+	public java.lang.Long getCommunityId() {
+		return this.communityId;
+	}
+
+	public void setCommunityId(java.lang.Long communityId) {
+		this.communityId = communityId;
 	}
 }
