b8dac2cf43f1efe09ec7f149db2f72cc08d3979f
LiangQishi <qishi.liang@zuolin.com>
2016-04-18 20:50:27 +0800

    fix bug: sourceAccountName is missing

--- ehrest/src/main/java/com/everhomes/rest/videoconf/CancelVideoConfCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/videoconf/CancelVideoConfCommand.java
@@ -6,11 +6,14 @@ import com.everhomes.util.StringHelper;
 /**
  * <ul>
  *  <li>confId: 会议id</li>
+ *  <li>sourceAccountName: 源账号</li>
  * </ul>
  */
 public class CancelVideoConfCommand {
 
 	private Long confId;
+	
+	private String sourceAccountName;
 	
 	public Long getConfId() {
 		return confId;
@@ -20,7 +23,15 @@ public class CancelVideoConfCommand {
 		this.confId = confId;
 	}
 
-	@Override
+	public String getSourceAccountName() {
+        return sourceAccountName;
+    }
+
+    public void setSourceAccountName(String sourceAccountName) {
+        this.sourceAccountName = sourceAccountName;
+    }
+
+    @Override
     public String toString() {
         return StringHelper.toJsonString(this);
     }
