2594e5eb4fe730dedfc34c415186d85212d0ef50
LiangQishi <qishi.liang@zuolin.com>
2016-04-21 19:47:04 +0800

    add alias name for scene token

--- ehrest/src/main/java/com/everhomes/rest/ui/user/SceneDTO.java
+++ ehrest/src/main/java/com/everhomes/rest/ui/user/SceneDTO.java
@@ -9,6 +9,7 @@ import com.everhomes.util.StringHelper;
  * <li>entityType: 实体类型，{@link com.everhomes.rest.user.UserCurrentEntityType}</li>
  * <li>entityContent: 实体内容，会根据具体实体类型有不同的JSON结构</li>
  * <li>name: 显示名称</li>
+ * <li>aliasName: 别名</li>
  * <li>avatar: 显示头像URI</li>
  * <li>avatarUrl: 显示头像URL</li>
  * </ul>
@@ -18,6 +19,7 @@ public class SceneDTO {
     private String entityType;
     private String entityContent;
     private String name;
+    private String aliasName;
     private String avatar;
     private String avatarUrl;
 
@@ -51,6 +53,14 @@ public class SceneDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAliasName() {
+        return aliasName;
+    }
+
+    public void setAliasName(String aliasName) {
+        this.aliasName = aliasName;
     }
 
     public String getAvatar() {
