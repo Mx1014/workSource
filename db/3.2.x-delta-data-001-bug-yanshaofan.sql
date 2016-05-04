c3d43e3b9fd3b395432c6830ef5a76ab17e48c44
yanshaofan <sf.yan@zuolin.com>
2016-05-04 11:44:11 +0800

    topic data

--- db/3.2.x-delta-data-001-bug-yanshaofan.sql
+++ db/3.2.x-delta-data-001-bug-yanshaofan.sql
@@ -8,6 +8,6 @@ UPDATE `eh_organization_members` SET `status` = 0 WHERE `organization_id` = 1783
 #
 #20160504
 #
-update `eh_forum_posts` set `forum_id` = 178622 where `creator_uid` in (select id from eh_users where namespace_id=999999); 
-update `eh_forum_posts` set `forum_id` = 176520 where `creator_uid` in (select id from eh_users where namespace_id=1000000); 
+update `eh_forum_posts` set `forum_id` = 178622 where `creator_uid` in (select id from eh_users where namespace_id=999999) and `forum_id` = 1; 
+update `eh_forum_posts` set `forum_id` = 176520 where `creator_uid` in (select id from eh_users where namespace_id=1000000) and `forum_id` = 1; 
 
