d4debc49644455101fc780259dbf0c0cd392a2fd
yanshaofan <sf.yan@zuolin.com>
2016-05-05 17:07:35 +0800

    bug

--- ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
@@ -459,8 +459,13 @@ public class OrganizationServiceImpl implements OrganizationService {
 	public void updateChildrenOrganization(UpdateOrganizationsCommand cmd) {
 		//先判断，后台管理员才能创建。状态直接设为正常
 		
+		if(null == cmd.getNaviFlag()){
+			cmd.setNaviFlag((byte)1);
+		}
+		
 		Organization parOrg = this.checkOrganization(cmd.getId());
 		
+		parOrg.setShowFlag(cmd.getNaviFlag());
 		parOrg.setName(cmd.getName());
 		
 		if(OrganizationGroupType.fromCode(parOrg.getGroupType()) == OrganizationGroupType.ENTERPRISE){
