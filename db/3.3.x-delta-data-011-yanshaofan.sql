858e8e91db8895a83c01364e64b061583584dba9
yanshaofan <sf.yan@zuolin.com>
2016-05-11 09:59:47 +0800

    sql

---
+++ db/3.3.x-delta-data-011-yanshaofan.sql
@@ -0,0 +1,10 @@
+#
+#20160511
+#INSERT INTO `eh_namespace_resources`(`id`, `namespace_id`,`resource_type`, `resource_id`, `create_time`) VALUES( 4, 999993,'COMMUNITY', , now());
+#深业物业小区有 风临左岸，岭秀名苑，深发花园，深业花园
+#科技园园区有 科技园
+#讯美  讯美科技广场
+
+
+
+
