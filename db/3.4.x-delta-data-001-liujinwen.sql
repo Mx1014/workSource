014929067d1bb3ca8f9d1d032363a891555c505c
liujinwen <jinwen.liu@zuolin.com>
2016-05-23 17:11:21 +0800

    add sql for /user/getBizSignature

---
+++ db/3.4.x-delta-data-001-liujinwen.sql
@@ -0,0 +1,3 @@
+-- user/getBizSignature
+INSERT INTO `eh_apps` (`creator_uid`,`app_key`,`secret_key`,`name`,`description`,`status`,`create_time`,`update_uid`,`update_time`) VALUES ('1', '44952417-b120-4f41-885f-0c1110c6aece', 'gPVzYlvWb1z7zPZkQTFX1W92xqnKgBL3yy5aL/foPRdOuGmXeKvQ5lH1zEV8XqeN5zEACaOCeoWh6ezPv31kaw==', 'sign app', 'getBizsignature interface use it', '1', '2016-03-23 00:00:00', null, null);
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`) VALUES ('sign.appKey','44952417-b120-4f41-885f-0c1110c6aece','sign.appKey',0);
