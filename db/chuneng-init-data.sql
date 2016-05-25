01da0cb0a907b378652f4a3a382bc50c4ae678ef
ying.xiong <ying.xiong@zuolin.com>
2016-05-25 10:36:45 +0800

    add banner scene type

--- db/chuneng-init-data.sql
+++ db/chuneng-init-data.sql
@@ -63,8 +63,10 @@ INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `des
 INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`) VALUES(999990, 'sms.default.yzx', 6, 'zh_CN', '任务2-储能', '23387');
 
 
-INSERT INTO `eh_banners` (`id`, `namespace_id`, `appId`, `banner_location`, `banner_group`, `scope_code`, `scope_id`, `name`, `vendor_tag`, `poster_path`, `action_type`, `action_data`, `start_time`, `end_time`, `status`, `order`, `creator_uid`, `create_time`, `delete_time`) 
-	VALUES(85, 999990, 0, '/home', 'Default', 0, 0, 'chuneng', 'chuneng','cs://1/image/aW1hZ2UvTVRvMVlqZ3lORGswTldSak1qZGlPVFJoTmpFMU16VXdOR1kxTkRBeFlUaGtOQQ','0','',NULL,NULL,'2','1','0','2015-06-30 16:01:45',NULL);	
+INSERT INTO `eh_banners` (`id`, `namespace_id`, `appId`, `banner_location`, `banner_group`, `scope_code`, `scope_id`, `name`, `vendor_tag`, `poster_path`, `action_type`, `action_data`, `start_time`, `end_time`, `status`, `order`, `creator_uid`, `create_time`, `delete_time`, `scene_type`) 
+	VALUES(85, 999990, 0, '/home', 'Default', 0, 0, 'chuneng', 'chuneng','cs://1/image/aW1hZ2UvTVRvMVlqZ3lORGswTldSak1qZGlPVFJoTmpFMU16VXdOR1kxTkRBeFlUaGtOQQ','0','',NULL,NULL,'2','1','0','2015-06-30 16:01:45',NULL, 'park_tourist');	
+INSERT INTO `eh_banners` (`id`, `namespace_id`, `appId`, `banner_location`, `banner_group`, `scope_code`, `scope_id`, `name`, `vendor_tag`, `poster_path`, `action_type`, `action_data`, `start_time`, `end_time`, `status`, `order`, `creator_uid`, `create_time`, `delete_time`, `scene_type`) 
+	VALUES(88, 999990, 0, '/home', 'Default', 0, 0, 'chuneng', 'chuneng','cs://1/image/aW1hZ2UvTVRvMVlqZ3lORGswTldSak1qZGlPVFJoTmpFMU16VXdOR1kxTkRBeFlUaGtOQQ','0','',NULL,NULL,'2','1','0','2015-06-30 16:01:45',NULL, 'pm_admin');	
 
 -- 园区管理员    
 INSERT INTO `eh_launch_pad_layouts` (`id`, `namespace_id`, `name`, `layout_json`, `version_code`, `min_version_code`, `status`, `create_time`, `scene_type`) 
