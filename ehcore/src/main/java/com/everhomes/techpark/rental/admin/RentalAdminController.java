04b2037bba6532ae90dc58e0fafde40c9c0682fe
tangtong <tong.tang@zuolin.com>
2016-06-01 10:11:02 +0800

    添加后台controller

---
+++ ehcore/src/main/java/com/everhomes/techpark/rental/admin/RentalAdminController.java
@@ -0,0 +1,16 @@
+package com.everhomes.techpark.rental.admin;
+
+import com.everhomes.controller.ControllerBase;
+import com.everhomes.rest.RestResponse;
+
+public class RentalAdminController extends ControllerBase {
+	
+	
+	public RestResponse setDefaultRules(){
+		
+		return new RestResponse();
+	}
+	
+	
+	
+}
