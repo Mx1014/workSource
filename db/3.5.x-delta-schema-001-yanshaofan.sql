f8fee180c155aa30239ecac92620411206c28058
yanshaofan <sf.yan@zuolin.com>
2016-06-04 17:33:40 +0800

    add user item

--- db/3.5.x-delta-schema-001-yanshaofan.sql
+++ db/3.5.x-delta-schema-001-yanshaofan.sql
@@ -6,6 +6,8 @@ DROP TABLE IF EXISTS `eh_user_launch_pad_items`;
 CREATE TABLE `eh_user_launch_pad_items`(
     `id` BIGINT NOT NULL,
     `item_id` BIGINT NOT NULL,
+    `owner_type` varchar(64) NOT NULL COMMENT 'community, organization',
+    `owner_id` tinyint(4) NOT NULL,
     `user_id` tinyint(4) NOT NULL,
     `apply_policy` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: default, 1: override, 2: revert 3:customized',
     `display_flag` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'default display on the pad, 0: hide, 1:display',
