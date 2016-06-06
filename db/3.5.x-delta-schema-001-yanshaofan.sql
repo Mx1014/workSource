3c8dfe7aefc5852d285cd6486485c85d8a5092b5
yanshaofan <sf.yan@zuolin.com>
2016-06-06 15:02:43 +0800

    sql

--- db/3.5.x-delta-schema-001-yanshaofan.sql
+++ db/3.5.x-delta-schema-001-yanshaofan.sql
@@ -12,5 +12,6 @@ CREATE TABLE `eh_user_launch_pad_items`(
     `apply_policy` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: default, 1: override, 2: revert 3:customized',
     `display_flag` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'default display on the pad, 0: hide, 1:display',
     `default_order` int(11) NOT NULL DEFAULT '0',
+    `scene_type` varchar(64) NOT NULL DEFAULT 'default',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
