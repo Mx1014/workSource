778d0f2cf87eac4c072423d67029b1590db38d7f
yanshaofan <sf.yan@zuolin.com>
2016-06-06 16:01:57 +0800

    add item api

--- ehrest/src/main/java/com/everhomes/rest/ui/user/LaunchPadItemSort.java
+++ ehrest/src/main/java/com/everhomes/rest/ui/user/LaunchPadItemSort.java
@@ -1,8 +1,32 @@
 package com.everhomes.rest.ui.user;
 
+/**
+ * <ul>
+ *  <li>id: item ID</li>
+ *  <li>defaultOrder: 排序号</li>
+ * </ul>
+ */
 public class LaunchPadItemSort {
 
 	private Long id;
 	
-	private 
+	private Integer defaultOrder;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getDefaultOrder() {
+		return defaultOrder;
+	}
+
+	public void setDefaultOrder(Integer defaultOrder) {
+		this.defaultOrder = defaultOrder;
+	}
+	
+	
 }
