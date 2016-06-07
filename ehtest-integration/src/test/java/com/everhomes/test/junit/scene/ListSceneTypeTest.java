b9f5d55c4a0299c262314a53dffba1dfaef790b3
LiangQishi <qishi.liang@zuolin.com>
2016-06-07 22:20:05 +0800

    change the method name

--- ehtest-integration/src/test/java/com/everhomes/test/junit/scene/ListSceneTypeTest.java
+++ ehtest-integration/src/test/java/com/everhomes/test/junit/scene/ListSceneTypeTest.java
@@ -5,7 +5,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.everhomes.rest.ui.user.UserListUserRelatedScenesRestResponse;
+import com.everhomes.rest.scene.ListSceneTypesCommand;
+import com.everhomes.rest.scene.SceneTypeInfoDTO;
+import com.everhomes.rest.scene.admin.SceneListSceneTypesRestResponse;
+import com.everhomes.rest.ui.user.SceneType;
 import com.everhomes.test.core.base.BaseLoginAuthTestCase;
 import com.everhomes.util.StringHelper;
 
@@ -16,22 +19,47 @@ public class ListSceneTypeTest extends BaseLoginAuthTestCase {
     }
     
     @Test
-    public void testFamilyScenes() {
+    public void testListSceneTypes() {
         Integer namespaceId = 0;
-        String userIdentifier = "12900000001";
+        String userIdentifier = "12900000001"; // 管理员帐号
         String plainTexPassword = "123456";
-        // 登录时不传namepsace，默认为左邻域空间
         logon(null, userIdentifier, plainTexPassword);
         
+        ListSceneTypesCommand cmd = new ListSceneTypesCommand();
+        cmd.setNamespaceId(0);
+        
         String commandRelativeUri = "/admin/scene/listSceneTypes";
-        UserListUserRelatedScenesRestResponse response = httpClientService.restGet(commandRelativeUri, null, 
-            UserListUserRelatedScenesRestResponse.class, context);
+        SceneListSceneTypesRestResponse response = httpClientService.restGet(commandRelativeUri, cmd, 
+            SceneListSceneTypesRestResponse.class, context);
         
         assertNotNull("The reponse of may not be null", response);
         assertTrue("The user scenes should be get from server, response=" + 
             StringHelper.toJsonString(response), httpClientService.isReponseSuccess(response));
         assertNotNull(response.getResponse());
-        assertEquals(2, response.getResponse().size());
+        assertEquals(6, response.getResponse().size());
+        int flag = 0;
+        for(SceneTypeInfoDTO scene : response.getResponse()) {
+            SceneType sceneType = SceneType.fromCode(scene.getName());
+            if(SceneType.DEFAULT == sceneType) {
+                flag = flag | 0x01;
+            }
+            if(SceneType.PM_ADMIN == sceneType) {
+                flag = flag | 0x02;
+            }
+            if(SceneType.FAMILY == sceneType) {
+                flag = flag | 0x04;
+            }
+            if(SceneType.PARK_TOURIST == sceneType) {
+                flag = flag | 0x04;
+            }
+            if(SceneType.ENTERPRISE == sceneType) {
+                flag = flag | 0x04;
+            }
+            if(SceneType.ENTERPRISE_NOAUTH == sceneType) {
+                flag = flag | 0x04;
+            }
+        }
+        assertEquals(0x3F, flag);
     }
     
     @After
