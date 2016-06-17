e018ace3b88c4f3a41362ac026581c7384c7b5bc
janson <gandancing@gmail.com>
2016-06-17 19:55:31 +0800

    support for remote opening door

--- ehcore/src/main/java/com/everhomes/aclink/DoorAccessServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/aclink/DoorAccessServiceImpl.java
@@ -1429,10 +1429,12 @@ public class DoorAccessServiceImpl implements DoorAccessService {
         Long lastTick = updateDoorAccessLastTick(resp.getId());
         //generate a time message
         //if( (lastTick+5*60*1000) < System.currentTimeMillis() ) {
-            return msgGenerator.generateTimeMessage(resp.getId());
+            //return msgGenerator.generateTimeMessage(resp.getId());
         //}
         
         //return msgGenerator.generateWebSocketMessage(resp.getId());
+        
+        return null;
     }
     
     @Override
