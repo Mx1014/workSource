b26a3865699f8fa67397872590f54ec13588e240
ying.xiong <ying.xiong@zuolin.com>
2016-06-30 10:29:21 +0800

    modify namespace id

--- db/3.4.x.sfyan-delta-data-release.sql
+++ db/3.4.x.sfyan-delta-data-release.sql
@@ -1,5 +1,5 @@
 INSERT INTO `eh_locale_templates`(`scope`, `code`,`locale`, `description`, `text`, `namespace_id`) 
-    VALUES( 'user.notification', 2, 'zh_CN', '注册天数描述', '我已加入左邻“${days}”天', 0);
+    VALUES( 'user.notification', 2, 'zh_CN', '注册天数描述', '我已加入Ibase“${days}”天', 999989);
 
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'hottag', '10001', 'zh_CN', '该标签已是热门标签');
 
