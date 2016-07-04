2430f37962d98ae5286a64d337cd709eb894d877
yanshaofan <sf.yan@zuolin.com>
2016-07-04 17:28:07 +0800

    biz item sql

--- db/3.4.x.sfyan-delta-data-migrate.sql
+++ db/3.4.x.sfyan-delta-data-migrate.sql
@@ -22,15 +22,46 @@ UPDATE `eh_launch_pad_layouts` SET `layout_json` = '{"versionCode":"2015111401",
 SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
 SET @order_id = 50;
 INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
-SELECT (@item_id = @item_id + 1),0,'0','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','pm_admin' FROM `eh_businesses`;
+SELECT (@item_id = @item_id + 1),0,'0','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','pm_admin' FROM `eh_businesses` WHERE target_type = 1;
 
 SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
 SET @order_id = 50;
 INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
-SELECT (@item_id = @item_id + 1),0,'0','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','park_tourist' FROM `eh_businesses`;
+SELECT (@item_id = @item_id + 1),0,'0','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','park_tourist' FROM `eh_businesses` WHERE target_type = 1;
 
 SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
 SET @order_id = 50;
 INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
-SELECT (@item_id = @item_id + 1),0,'0','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','default' FROM `eh_businesses`;
+SELECT (@item_id = @item_id + 1),0,'0','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','default' FROM `eh_businesses` WHERE target_type = 1;
+
+SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+SET @order_id = 50;
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
+SELECT (@item_id = @item_id + 1),0,'1000000','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','pm_admin' FROM `eh_businesses` WHERE target_type = 1;
+
+SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+SET @order_id = 50;
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
+SELECT (@item_id = @item_id + 1),0,'1000000','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','park_tourist' FROM `eh_businesses` WHERE target_type = 1;
+
+SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+SET @order_id = 50;
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
+SELECT (@item_id = @item_id + 1),0,'1000000','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','default' FROM `eh_businesses` WHERE target_type = 1;
+
+SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+SET @order_id = 50;
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
+SELECT (@item_id = @item_id + 1),0,'999999','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','pm_admin' FROM `eh_businesses` WHERE target_type = 1;
+
+SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+SET @order_id = 50;
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
+SELECT (@item_id = @item_id + 1),0,'999999','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','park_tourist' FROM `eh_businesses` WHERE target_type = 1;
+
+SET @item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+SET @order_id = 50;
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`) 
+SELECT (@item_id = @item_id + 1),0,'999999','0','0','/home','Bizs',`display_name`,`name`,`logo_uri`,'1','1','14','',(@order_id = @order_id + 1),'0','1','0','','0',NULL,'biz',`id`,'1','default' FROM `eh_businesses` WHERE target_type = 1;
+
 
