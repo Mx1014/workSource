1dcf65a08d3564779d428af982dba9ecf3909c0a
sunwen <wen.sun@zuolin.com>
2016-07-07 19:04:36 +0800

    fix payment card test

--- ehtest-integration/src/test/java/com/everhomes/test/junit/payment/SetCardPasswordTest.java
+++ ehtest-integration/src/test/java/com/everhomes/test/junit/payment/SetCardPasswordTest.java
@@ -2,6 +2,8 @@
 package com.everhomes.test.junit.payment;
 
 
+import java.io.File;
+
 import org.jooq.DSLContext;
 import org.jooq.SelectQuery;
 import org.junit.After;
@@ -9,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.everhomes.rest.RestResponse;
+import com.everhomes.rest.device.CreateCertCommand;
 import com.everhomes.rest.payment.SetCardPasswordCommand;
 import com.everhomes.server.schema.Tables;
 import com.everhomes.server.schema.tables.pojos.EhPaymentCards;
@@ -18,12 +21,43 @@ import com.everhomes.util.ConvertHelper;
 import com.everhomes.util.StringHelper;
 
 public class SetCardPasswordTest extends BaseLoginAuthTestCase {
+	
+	private static final String[] certNames = {"taotaogu.keystore","taotaogu.pin3.crt","taotaogu.server.cer","taotaogu.client.pfx"};
+	private static final String[] certPass = {"jxd,123456,123456","","","123456"};
+
     @Before
     public void setUp() {
         super.setUp();
+        
+    }
+   @Test
+    public void testCreateCert() {
+        
+        String userIdentifier = "13265549907";
+        String plainTexPassword = "123456";
+        Integer namespaceId = 999990;
+        // 登录时不传namepsace，默认为左邻域空间
+        logon(namespaceId, userIdentifier, plainTexPassword);
+        for(int i=0;i<certNames.length;i++){
+        	CreateCertCommand cmd = new CreateCertCommand();
+            cmd.setCertKey("");
+            cmd.setCertPass(certPass[i]);
+            cmd.setCertType(2);
+            cmd.setName(certNames[i]);
+            String faileName = "data/json/paymentcard/jxd.keystore";
+            String filePath = dbProvider.getAbsolutePathFromClassPath(faileName);
+            String commandRelativeUri = "/pusher/createCert";
+           // RestResponse response = httpClientService.restPost(commandRelativeUri, cmd, RestResponse.class,context);
+            RestResponse response = httpClientService.postFile(commandRelativeUri, cmd,new File(filePath), RestResponse.class);
+
+            assertNotNull("The reponse of updateCardRechargeOrder may not be null", response);
+            assertTrue("SetCardPassword, response=" + 
+                StringHelper.toJsonString(response), httpClientService.isReponseSuccess(response));
+        }
+        
     }
     
-    @Test
+    //@Test
     public void testSetCardPassword() {
         
     	String ownerType = "community";
