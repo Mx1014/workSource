5f9fec5dbc09e48db7edfa8c7cd366b0c301aa4c
LiangQishi <qishi.liang@zuolin.com>
2016-07-12 10:46:01 +0800

    merge news module

---
+++ ehnextgen-api/src/main/java/com/everhomes/search/SearchProvider.java
@@ -0,0 +1,20 @@
+// @formatter:off
+package com.everhomes.search;
+
+import com.alibaba.fastjson.JSONArray;
+
+public interface SearchProvider {
+
+	String insertOrUpdate(String type, String id, String json);
+
+	String getById(String type, String id);
+
+	String deleteById(String type, String id);
+
+	String bulk(String type, String json);
+
+	JSONArray query(String type, String json, String fields);
+
+	String clearType(String type);
+
+}
