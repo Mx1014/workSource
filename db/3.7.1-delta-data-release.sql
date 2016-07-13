478a2f6f4f33ae8f6c467eabc2b49de3deed89c5
tangtong <tong.tang@zuolin.com>
2016-07-13 17:39:40 +0800

    add 3.7.1 release data

---
+++ db/3.7.1-delta-data-release.sql
@@ -0,0 +1,5 @@
+delete from eh_launch_pad_items
+where target_type='biz'
+and scope_code = 0;
+
+INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'https://biz.zuolin.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=https://biz.zuolin.com%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14477417463124576784%3F_k%3Dzlbiz#sign_suffix', 'business url');
