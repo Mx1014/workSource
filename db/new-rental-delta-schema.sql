f6b826c2db6325ab8153f4aea3f9c2588ae487fb
wuhan <han.wu@zuolin.com>
2016-07-20 10:43:23 +0800

    modify db

--- db/new-rental-delta-schema.sql
+++ db/new-rental-delta-schema.sql
@@ -2,9 +2,9 @@
 -- 保存一个公司的一个场所图标的默认设置
 -- 
 -- 
-DROP TABLE IF EXISTS eh_rental_default_rules;
+DROP TABLE IF EXISTS eh_rentalv2_default_rules;
 
-CREATE TABLE `eh_rental_default_rules` (
+CREATE TABLE `eh_rentalv2_default_rules` (
 `id` BIGINT(20)    COMMENT 'id',
 `owner_type` VARCHAR(255)  COMMENT 'owner type : community ; organization',
 `owner_id` BIGINT(20)    COMMENT 'community id or organization id',
@@ -34,7 +34,7 @@ CREATE TABLE `eh_rental_default_rules` (
 `need_pay` TINYINT(4)    COMMENT '是否需要支付 1是 0否', 
 `workday_price` DECIMAL(10,2)  COMMENT '工作日价格',
 `weekend_price` DECIMAL(10,2)  COMMENT '周末价格',
-`site_counts` DOUBLE  COMMENT '可预约个数',
+`resource_counts` DOUBLE  COMMENT '可预约个数',
 `begin_date` DATE  COMMENT '开始日期',
 `end_date` DATE  COMMENT '结束日期',
 `open_weekday` VARCHAR(7)   COMMENT '7位二进制，0000000每一位表示星期7123456',
@@ -48,12 +48,12 @@ CREATE TABLE `eh_rental_default_rules` (
 -- 
 -- 对于按小时预定的场所默认设置，保存时间段
 -- 
-DROP TABLE IF EXISTS eh_rental_time_interval;
+DROP TABLE IF EXISTS eh_rentalv2_time_interval;
 
-CREATE TABLE `eh_rental_time_interval` (
+CREATE TABLE `eh_rentalv2_time_interval` (
 `id` BIGINT(20)    COMMENT '',
 `owner_id` BIGINT(20)    COMMENT '',
-`owner_type` VARCHAR(255)  COMMENT '"default_rule","site_rule"',
+`owner_type` VARCHAR(255)  COMMENT '"default_rule","resource_rule"',
 `begin_time` DOUBLE  COMMENT '开始时间-24小时制',
 `end_time` DOUBLE  COMMENT '结束时间-24小时制',
   PRIMARY KEY (`id`)
@@ -63,12 +63,12 @@ CREATE TABLE `eh_rental_time_interval` (
 -- 
 -- 保存默认设置的关闭时间
 -- 
-DROP TABLE IF EXISTS eh_rental_close_dates;
+DROP TABLE IF EXISTS eh_rentalv2_close_dates;
 
-CREATE TABLE `eh_rental_close_dates` (
+CREATE TABLE `eh_rentalv2_close_dates` (
 `id` BIGINT(20)    COMMENT '',
 `owner_id` BIGINT(20)    COMMENT '',
-`owner_type` VARCHAR(255)  COMMENT '"default_rule","site_rule"',
+`owner_type` VARCHAR(255)  COMMENT '"default_rule","resource_rule"',
 `close_date` DATE  COMMENT '',
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
@@ -78,13 +78,13 @@ CREATE TABLE `eh_rental_close_dates` (
 -- 
 -- 场所表
 -- 
-DROP TABLE IF EXISTS eh_rental_sites;
+DROP TABLE IF EXISTS eh_rentalv2_resources;
 
-CREATE TABLE `eh_rental_sites` (
+CREATE TABLE `eh_rentalv2_resources` (
 `id` BIGINT(20)   COMMENT 'id',
 `parent_id` BIGINT(20)  COMMENT '',
-`site_name` VARCHAR(127)   COMMENT '名称：',
-`site_type2` TINYINT(4)   COMMENT '',
+`resource_name` VARCHAR(127)   COMMENT '名称：',
+`resource_type2` TINYINT(4)   COMMENT '',
 `building_name` VARCHAR(128)   COMMENT '',
 `building_id` BIGINT(20)   COMMENT '',
 `address` VARCHAR(192)   COMMENT '地址',
@@ -136,13 +136,13 @@ CREATE TABLE `eh_rental_sites` (
 -- 
 -- 场所和归属园区的关联表
 -- 
-DROP TABLE IF EXISTS eh_rental_site_owners;
+DROP TABLE IF EXISTS eh_rentalv2_resource_ranges;
 
-CREATE TABLE `eh_rental_site_owners` (
+CREATE TABLE `eh_rentalv2_resource_ranges` (
 `id` BIGINT(20)    COMMENT 'id',
 `owner_type` VARCHAR(255)  COMMENT 'owner type : community ; organization',
 `owner_id` BIGINT(20)    COMMENT 'community id or organization id',
-`rental_site_id` BIGINT(20)  COMMENT 'rental_site id',
+`rental_resource_id` BIGINT(20)  COMMENT 'rental_resource id',
 
  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
@@ -152,12 +152,12 @@ CREATE TABLE `eh_rental_site_owners` (
 -- 
 -- 保存场所详情图片
 -- 
-DROP TABLE IF EXISTS eh_rental_site_pics;
+DROP TABLE IF EXISTS eh_rentalv2_resource_pics;
 
-CREATE TABLE `eh_rental_site_pics` (
+CREATE TABLE `eh_rentalv2_resource_pics` (
 `id` BIGINT(20)    COMMENT '',
 `owner_id` BIGINT(20)    COMMENT '',
-`owner_type` VARCHAR(255)  COMMENT '"eh_rental_sites"',
+`owner_type` VARCHAR(255)  COMMENT '"eh_rental_resources"',
 `uri` VARCHAR(1024)  COMMENT '',
  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
@@ -166,11 +166,11 @@ CREATE TABLE `eh_rental_site_pics` (
 -- 
 -- 场所设定好的单元格表
 -- 
-DROP TABLE IF EXISTS eh_rental_site_rules;
+DROP TABLE IF EXISTS eh_rentalv2_cells;
 
-CREATE TABLE `eh_rental_site_rules` (
+CREATE TABLE `eh_rentalv2_cells` (
 `id` BIGINT(20)    COMMENT 'id',
-`rental_site_id` BIGINT(20)    COMMENT 'rental_site id',
+`rental_resource_id` BIGINT(20)    COMMENT 'rental_resource id',
 `rental_type` TINYINT(4)   COMMENT '0: as hour:min 1-as half day 2-as day 3-支持晚上的半天',
 `amorpm` TINYINT(4)   COMMENT '0:am 1:pm 2:night',
 `rental_step` INT(11)   COMMENT 'how many time_step must be rental every time',
@@ -179,7 +179,7 @@ CREATE TABLE `eh_rental_site_rules` (
 `counts` DOUBLE   COMMENT '共多少个',
 `unit` DOUBLE   COMMENT '是否支持0.5个',
 `price` DECIMAL(10,2)   COMMENT '折后价',
-`site_rental_date` DATE   COMMENT 'which day',
+`resource_rental_date` DATE   COMMENT 'which day',
 `status` TINYINT(4)   COMMENT 'unuse 0:open 1:closed',
 `creator_uid` BIGINT(20)   COMMENT '',
 `create_time` DATETIME   COMMENT '',
@@ -192,9 +192,9 @@ CREATE TABLE `eh_rental_site_rules` (
 `multi_unit` TINYINT(4)    COMMENT '是否允许预约多个场所 1是 0否',
 `multi_time_interval` TINYINT(4)    COMMENT '是否允许预约多个时段 1是 0否',
 `resource_type_id` BIGINT(20)    COMMENT '广场图标id',
-`site_number` VARCHAR(100)  COMMENT '场所号',
-`halfsite_price` DECIMAL(10,2)   COMMENT '半场折后价',
-`halfsite_original_price` DECIMAL(10,2)   COMMENT '半场原价（如果不为null则price为打折价）',
+`resource_number` VARCHAR(100)  COMMENT '场所号',
+`halfresource_price` DECIMAL(10,2)   COMMENT '半场折后价',
+`halfresource_original_price` DECIMAL(10,2)   COMMENT '半场原价（如果不为null则price为打折价）',
  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
 ;
@@ -202,12 +202,12 @@ CREATE TABLE `eh_rental_site_rules` (
 -- 
 -- 保存场所附件设置
 -- 
-DROP TABLE IF EXISTS eh_rental_config_attachments;
+DROP TABLE IF EXISTS eh_rentalv2_config_attachments;
 
-CREATE TABLE `eh_rental_config_attachments` (
+CREATE TABLE `eh_rentalv2_config_attachments` (
 `id` BIGINT(20)    COMMENT '',
 `owner_id` BIGINT(20)    COMMENT '',
-`owner_type` VARCHAR(255)  COMMENT '"default_rule","site_rule"',
+`owner_type` VARCHAR(255)  COMMENT '"default_rule","resource_rule"',
 `attachment_type` TINYINT(4)    COMMENT '',
 `content` TEXT  COMMENT '根据type，这里可能是文本或者附件url',
 `must_options` TINYINT(4)    COMMENT '0 非必须 1 必选',
@@ -218,11 +218,11 @@ PRIMARY KEY (`id`)
 -- 
 -- 物品表
 -- 
-DROP TABLE IF EXISTS eh_rental_site_items;
+DROP TABLE IF EXISTS eh_rentalv2_items;
 
-CREATE TABLE `eh_rental_site_items` (
+CREATE TABLE `eh_rentalv2_items` (
 `id` BIGINT(20)    COMMENT 'id',
-`rental_site_id` BIGINT(20)    COMMENT 'rental_site id',
+`rental_resource_id` BIGINT(20)    COMMENT 'rental_resource id',
 `name` VARCHAR(128)   COMMENT '',
 `price` DECIMAL(10,2)   COMMENT '',
 `counts` INT(11)   COMMENT 'item count',
@@ -242,19 +242,19 @@ CREATE TABLE `eh_rental_site_items` (
 -- 
 -- 订单主表
 -- 
-DROP TABLE IF EXISTS eh_rental_bills;
+DROP TABLE IF EXISTS eh_rentalv2_orders;
 
-CREATE TABLE `eh_rental_bills` (
+CREATE TABLE `eh_rentalv2_orders` (
 `id` BIGINT(20) NOT NULL  COMMENT 'id',
-`bill_number` VARCHAR(20) NOT NULL COMMENT '订单编号',
-`rental_site_id` BIGINT(20) NOT NULL  COMMENT 'id',
+`order_no` VARCHAR(20) NOT NULL COMMENT '订单编号',
+`rental_resource_id` BIGINT(20) NOT NULL  COMMENT 'id',
 `rental_uid` BIGINT(20) NULL  COMMENT 'rental user id',
 `rental_date` DATE NULL  COMMENT '使用日期',
 `start_time` DATETIME NULL  COMMENT '使用开始时间',
 `end_time` DATETIME NULL  COMMENT '使用结束时间',
 `rental_count` DOUBLE NULL  COMMENT '预约数',
 `pay_total_money` DECIMAL(10,2) NULL  COMMENT '总价',
-`site_total_money` DECIMAL(10,2) NULL  COMMENT '',
+`resource_total_money` DECIMAL(10,2) NULL  COMMENT '',
 `reserve_money` DECIMAL(10,2) NULL  COMMENT '',
 `reserve_time` DATETIME NULL  COMMENT 'reserve time',
 `pay_start_time` DATETIME NULL  COMMENT '',
@@ -269,15 +269,18 @@ CREATE TABLE `eh_rental_bills` (
 `create_time` DATETIME NULL  COMMENT '下单时间',
 `operator_uid` BIGINT(20) NULL  COMMENT '',
 `operate_time` DATETIME NULL  COMMENT '',
-`site_name` VARCHAR(255) NULL  COMMENT '名称',
+`resource_name` VARCHAR(255) NULL  COMMENT '名称',
 `use_detail` VARCHAR(255) NULL  COMMENT '使用时间',
-`vendor_type` VARCHAR(255) NULL  COMMENT '支付方式,10001-支付宝，10002-微信',
+`vendor_type` VARCHAR(255) NULL  COMMENT '支付方式,10001-支付宝，10002-微信 --多次支付怎木办，估计产品都没想清楚',
 `resource_type_id` BIGINT(20)    COMMENT '广场图标id',
 `organization_id` BIGINT(20)    COMMENT '所属公司的ID', 
 `spec` VARCHAR(255)   COMMENT '规格',
 `address` VARCHAR(192)   COMMENT '地址',
 `longitude` DOUBLE  COMMENT '地址经度',
 `latitude` DOUBLE  COMMENT '地址纬度',
+`contact_phonenum` VARCHAR(20)   COMMENT '咨询电话',
+`introduction` TEXT   COMMENT '详情',
+`notice` TEXT   COMMENT '',
  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
 ;
@@ -285,12 +288,12 @@ CREATE TABLE `eh_rental_bills` (
 -- 
 -- 订单分表-场所订单表
 -- 
-DROP TABLE IF EXISTS eh_rental_sites_bills;
+DROP TABLE IF EXISTS eh_rentalv2_resource_orders;
 
-CREATE TABLE `eh_rental_sites_bills` (
+CREATE TABLE `eh_rentalv2_resource_orders` (
 `id` BIGINT(20) NOT NULL  COMMENT 'id',
-`rental_bill_id` BIGINT(20) NULL  COMMENT '',
-`rental_site_rule_id` BIGINT(20) NULL  COMMENT '',
+`rental_order_id` BIGINT(20) NULL  COMMENT '',
+`rental_resource_rule_id` BIGINT(20) NULL  COMMENT '',
 `rental_count` DOUBLE NULL  COMMENT '',
 `total_money` DECIMAL(10,2) NULL  COMMENT '',
 `creator_uid` BIGINT(20) NULL  COMMENT '',
@@ -301,40 +304,24 @@ CREATE TABLE `eh_rental_sites_bills` (
 `begin_time` DATETIME   COMMENT '开始时间 对于按时间定',
 `end_time` DATETIME   COMMENT '结束时间 对于按时间定',
 `price` DECIMAL(10,2)   COMMENT '折后价',
-`site_rental_date` DATE   COMMENT 'which day',
+`resource_rental_date` DATE   COMMENT 'which day',
 `amorpm` TINYINT(4)   COMMENT '0:am 1:pm 2:night',
 `rental_step` INT(11)   COMMENT 'how many time_step must be rental every time',
 
  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
 ;
-
--- 
--- 订单分表-场所订单-对应资源编号表
--- 
-DROP TABLE IF EXISTS eh_rental_sites_bill_numbers;
-
-CREATE TABLE `eh_rental_sites_bill_numbers` (
-`id` BIGINT(20) NOT NULL  COMMENT 'id',
-`rental_site_bill_id` BIGINT(20) NULL  COMMENT 'fk',
-`rental_site_rule_id` BIGINT(20) NULL  COMMENT '场所单元格id',
-`site_number` INT  COMMENT '场所编号',
-`rental_count` DOUBLE NULL  COMMENT '0.5为半个1为全个',
-
- PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4	
-;
-
+ 
 
 -- 
 -- 订单分表-物品订单表
 -- 
-DROP TABLE IF EXISTS eh_rental_items_bills;
+DROP TABLE IF EXISTS eh_rentalv2_items_orders;
 
-CREATE TABLE `eh_rental_items_bills` (
+CREATE TABLE `eh_rentalv2_items_orders` (
 `id` BIGINT(20) NOT NULL  COMMENT 'id',
-`rental_bill_id` BIGINT(20) NULL  COMMENT '',
-`rental_site_item_id` BIGINT(20) NULL  COMMENT '',
+`rental_order_id` BIGINT(20) NULL  COMMENT '',
+`rental_resource_item_id` BIGINT(20) NULL  COMMENT '',
 `rental_count` INT(11) NULL  COMMENT '',
 `total_money` DECIMAL(10,2) NULL  COMMENT '',
 `creator_uid` BIGINT(20) NULL  COMMENT '',
@@ -355,11 +342,11 @@ CREATE TABLE `eh_rental_items_bills` (
 -- 
 -- 订单附件表
 -- 
-DROP TABLE IF EXISTS eh_rental_bill_attachments;
+DROP TABLE IF EXISTS eh_rentalv2_order_attachments;
 
-CREATE TABLE `eh_rental_bill_attachments` (
+CREATE TABLE `eh_rentalv2_order_attachments` (
 `id` BIGINT(20) NOT NULL  COMMENT 'id',
-`rental_bill_id` BIGINT(20) NULL  COMMENT '',
+`rental_order_id` BIGINT(20) NULL  COMMENT '',
 `attachment_type` TINYINT(4) NULL  COMMENT '0:文本 1:车牌 2:显示内容 3：附件链接',
 `content` VARCHAR(500) NULL  COMMENT '',
 `file_path` VARCHAR(500) NULL  COMMENT '',
@@ -375,14 +362,15 @@ CREATE TABLE `eh_rental_bill_attachments` (
 
 
 -- 
--- 订单-支付关联表
+-- 订单-支付关联表 
+-- 订单可能多次支付
 -- 
-DROP TABLE IF EXISTS eh_rental_bill_paybill_map;
+DROP TABLE IF EXISTS eh_rentalv2_order_payorder_map;
 
-CREATE TABLE `eh_rental_bill_paybill_map` (
+CREATE TABLE `eh_rentalv2_order_payorder_map` (
 `id` BIGINT(20) NOT NULL  COMMENT 'id',
-`rental_bill_id` BIGINT(20) NULL  COMMENT '',
-`online_pay_bill_id` BIGINT(20) NULL  COMMENT '',
+`order_id` BIGINT(20) NULL  COMMENT '',
+`order_no` BIGINT(20) NULL  COMMENT '',
 `creator_uid` BIGINT(20) NULL  COMMENT '',
 `create_time` DATETIME NULL  COMMENT '',
 `operator_uid` BIGINT(20) NULL  COMMENT '',
@@ -397,11 +385,11 @@ CREATE TABLE `eh_rental_bill_paybill_map` (
 -- 
 -- 订单-退款表
 -- 
-DROP TABLE IF EXISTS eh_rental_refund_orders;
+DROP TABLE IF EXISTS eh_rentalv2_refund_orders;
 
-CREATE TABLE `eh_rental_refund_orders` (
+CREATE TABLE `eh_rentalv2_refund_orders` (
 `id` BIGINT(20)  COMMENT 'id',
-`rental_bill_id` BIGINT(20)   COMMENT '订单id',
+`order_id` BIGINT(20)   COMMENT '订单id',
 `refund_order_no` BIGINT(20)   COMMENT '退款的refoundOrderNo-服务端退款时候生成',
 `resource_type_id` BIGINT(20)    COMMENT '广场图标id',
 `order_no` BIGINT(20)   COMMENT '支付的orderno-下单时候生成',
@@ -423,9 +411,9 @@ CREATE TABLE `eh_rental_refund_orders` (
 -- 
 -- 资源类型表
 -- 
-DROP TABLE IF EXISTS eh_rental_resource_types;
+DROP TABLE IF EXISTS eh_rentalv2_resource_types;
 
-CREATE TABLE `eh_rental_resource_types` (
+CREATE TABLE `eh_rentalv2_resource_types` (
 `id` BIGINT(20)    COMMENT 'id',
 `name` VARCHAR(50)   COMMENT '名称',
 `page_type` TINYINT(4)    COMMENT '预定展示0代表默认页面DefaultType, 1代表定制页面CustomType',
