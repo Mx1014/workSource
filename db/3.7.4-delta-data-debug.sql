3d403c10afe9000c09dac4f3296d9f0a6c5b788d
LiangQishi <qishi.liang@zuolin.com>
2016-07-20 15:54:34 +0800

    merge 3.7.1 sql to 3.7.4

--- db/3.7.4-delta-data-debug.sql
+++ db/3.7.4-delta-data-debug.sql
@@ -1,6 +1,4 @@
-delete from eh_launch_pad_items
-where target_type='biz'
-and scope_code = 0;
+delete from eh_launch_pad_items where target_type='biz' and scope_code = 0;
 
 INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'https://biz-beta.zuolin.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=https://biz-beta.zuolin.com%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14616591393750460212%3F_k%3Dzlbiz#sign_suffix', 'business url');
 
