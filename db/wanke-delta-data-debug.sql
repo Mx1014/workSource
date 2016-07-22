f61317d4548e554dba16d80e6f03b9339ecb7ba3
sunwen <wen.sun@zuolin.com>
2016-07-22 14:36:31 +0800

    wanke table schema

---
+++ db/wanke-delta-data-debug.sql
@@ -0,0 +1,6 @@
+INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) 
+    VALUES (86, 'wanke.mashen.url', 'https://api.open.imasheng.com/openapi', 'the url for wanke.mashen', 0, NULL);
+INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) 
+    VALUES (87, 'wanke.mashen.appId', '15725632', 'the appId of wanke.mashen', 0, NULL);
+INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) 
+    VALUES (88, 'wanke.mashen.appSecret', 'b4b994d5ed0a9990', 'the appSecret of wanke.mashen', 0, NULL);
