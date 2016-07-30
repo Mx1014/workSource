88ade311e8c2ce00c2e85bd2c415b254f60b4609
tangtong <tong.tang@zuolin.com>
2016-07-30 13:48:32 +0800

    update biz url

--- db/3.7.4-delta-data-debug.sql
+++ db/3.7.4-delta-data-debug.sql
@@ -1,9 +1,13 @@
 -- 删除电商相关图标
 delete from eh_launch_pad_items where target_type='biz' and scope_code = 0;
 
--- 配置电商链接
+-- 电商链接配置alpha环境
 INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.realm', 'biz', 'business realm');
-INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'https://biz-beta.zuolin.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=https://biz-beta.zuolin.com%2Fnar%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14477417463124576784%3F_k%3Dzlbiz#sign_suffix', 'business url');
+INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'http://biz-alpha.lab.everhomes.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=http://biz-alpha.lab.everhomes.com%2Fnar%2Fbiz%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14646085111657370488%3F_k%3Dzlbiz#sign_suffix', 'business url');
+
+-- 电商链接配置beta环境
+-- INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.realm', 'biz', 'business realm');
+-- INSERT INTO `ehcore`.`eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'http://biz-beta.zuolin.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=http://biz-beta.zuolin.com%2Fnar%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14646085111657370488%3F_k%3Dzlbiz#sign_suffix', 'business url');
 
 -- group下增加错误提示
 INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('group', '10030', 'zh_CN', '您不是群主，无权操作');
