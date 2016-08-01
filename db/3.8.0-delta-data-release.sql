4e2c64e725f8bd43cf58f21a1830affab033cce7
liujinwen <jinwen.liu@zuolin.com>
2016-08-01 18:22:33 +0800

    modify business.url

--- db/3.8.0-delta-data-release.sql
+++ db/3.8.0-delta-data-release.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO `eh_configurations` (`name`, `value`, `description`) VALUES ('business.realm', 'biz', 'business realm');
-INSERT INTO `eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'https://biz.zuolin.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=https://biz.zuolin.com%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14477417463124576784%3F_k%3Dzlbiz#sign_suffix', 'business url');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`) VALUES ('business.url', 'https://biz.zuolin.com/zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=https://biz.zuolin.com%2Fnar%2Fbiz%2Fweb%2Fapp%2Fuser%2Findex.html%23%2Fstore%2Fdetails%2F14477417463124576784%3F_k%3Dzlbiz#sign_suffix', 'business url');
 
 INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('group', '10030', 'zh_CN', '您不是群主，无权操作');
 
