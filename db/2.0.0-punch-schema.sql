b4061dc512514b5d3db3cb2743adf05542301975
wuhan <han.wu@zuolin.com>
2016-08-03 19:04:27 +0800

    modify schema

--- db/2.0.0-punch-schema.sql
+++ db/2.0.0-punch-schema.sql
@@ -4,12 +4,12 @@
 -- DROP TABLE IF EXISTS `eh_punch_time_rules`;
 CREATE TABLE `eh_punch_time_rules` (
   `id` BIGINT COMMENT 'id',
-  `name` VARCHAR (128) COMMENT 'time rule name ',
   `owner_type` VARCHAR (128) COMMENT 'owner resource(user/organization) type',
-  `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id',
-  `start_early_time` TIME  COMMENT 'how early can i arrive',
-  `start_late_time` TIME  COMMENT 'how late can i arrive ',
-  `work_time` TIME  COMMENT 'how long do i must be work',
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
+  `name` VARCHAR (128) COMMENT 'time rule name ',
+  `start_early_time` TIME COMMENT 'how early can i arrive',
+  `start_late_time` TIME COMMENT 'how late can i arrive ',
+  `work_time` TIME COMMENT 'how long do i must be work',
   `noon_leave_time` TIME ,
   `afternoon_arrive_time` TIME ,
   `punch_times_per_day` TINYINT NOT NULL DEFAULT 2 COMMENT 'how many times should punch everyday :2/4',
@@ -26,12 +26,10 @@ CREATE TABLE `eh_punch_time_rules` (
 -- 具体打卡地点
 -- 
 
-ALTER TABLE `eh_punch_rules` ADD COLUMN `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type';
-ALTER TABLE `eh_punch_rules` ADD COLUMN `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id';
-ALTER TABLE `eh_punch_rules` ADD COLUMN `location_rule_id` BIGINT  COMMENT 'fk:eh_punch_geopoints id';
-ALTER TABLE `eh_punch_rules` ADD COLUMN `description` VARCHAR(256);
-ALTER TABLE `eh_punch_rules` ADD KEY `fk_eh_punch_location_rules` (`location_rule_id`);
-ALTER TABLE `eh_punch_rules` ADD CONSTRAINT `fk_eh_punch_geopoints_ibfk_1` FOREIGN KEY (`location_rule_id`) REFERENCES `eh_punch_location_rules`(`id`);
+ALTER TABLE `eh_punch_geopoints` ADD COLUMN `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type';
+ALTER TABLE `eh_punch_geopoints` ADD COLUMN `owner_id` BIGINT COMMENT 'owner resource(user/organization) id';
+ALTER TABLE `eh_punch_geopoints` ADD COLUMN `location_rule_id` BIGINT COMMENT 'fk:eh_punch_geopoints id';
+ALTER TABLE `eh_punch_geopoints` ADD COLUMN `description` VARCHAR(256);
 
 
 -- 
@@ -41,11 +39,12 @@ ALTER TABLE `eh_punch_rules` ADD CONSTRAINT `fk_eh_punch_geopoints_ibfk_1` FOREI
 CREATE TABLE `eh_punch_location_rules` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
-  `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id',
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
   `name` VARCHAR(128) COMMENT 'location rule name ',
   `description` VARCHAR(256) ,
   `creator_uid` BIGINT ,
   `create_time` DATETIME ,
+  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -57,15 +56,14 @@ CREATE TABLE `eh_punch_location_rules` (
 CREATE TABLE `eh_punch_wifis` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
-  `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id',
-  `wifi_rule_id` BIGINT  COMMENT 'fk:eh_punch_wifi_rules id', 
-  `ssid` VARCHAR(64)  COMMENT 'wifi ssid', 
-  `mac_address` VARCHAR(32)  COMMENT 'wifi mac address', 
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
+  `wifi_rule_id` BIGINT COMMENT 'fk:eh_punch_wifi_rules id', 
+  `ssid` VARCHAR(64) COMMENT 'wifi ssid', 
+  `mac_address` VARCHAR(32) COMMENT 'wifi mac address', 
   `creator_uid` BIGINT ,
   `create_time` DATETIME ,
+  
   PRIMARY KEY (`id`),
-  KEY `fk_eh_punch_wifi_rules` (`wifi_rule_id`),
-  CONSTRAINT `fk_eh_punch_wifis_ibfk_1` FOREIGN KEY (`wifi_rule_id`) REFERENCES `eh_punch_wifi_rules` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -81,6 +79,7 @@ CREATE TABLE `eh_punch_wifi_rules` (
   `description` VARCHAR (256),
   `creator_uid` BIGINT,
   `create_time` DATETIME,
+  
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ;
 
@@ -92,31 +91,31 @@ CREATE TABLE `eh_punch_wifi_rules` (
 CREATE TABLE `eh_punch_holidays` (
   `id` BIGINT NOT NULL COMMENT 'id',
   `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
-  `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id',
-  `scheduling_rule_id` BIGINT  COMMENT 'fk:eh_punch_scheduling_rules id', 
-  `status` TINYINT  COMMENT 'its holiday or workday:0-workday ; 1-holiday',
-  `rule_date` DATE  COMMENT 'date',
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
+  `workday_rule_id` BIGINT COMMENT 'fk:eh_punch_workday_rules id', 
+  `status` TINYINT COMMENT 'its holiday or workday:0-workday ; 1-holiday',
+  `rule_date` DATE COMMENT 'date',
   `creator_uid` BIGINT ,
   `create_time` DATETIME ,
+  
   PRIMARY KEY (`id`),
-  KEY `fk_eh_punch_scheduling_rules` (`scheduling_rule_id`),
-  CONSTRAINT `fk_eh_punch_holidays_ibfk_1` FOREIGN KEY (`scheduling_rule_id`) REFERENCES `eh_punch_scheduling_rules` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 -- 
 -- 打卡排班表
 -- 
--- DROP TABLE IF EXISTS `eh_punch_scheduling_rules`;
-CREATE TABLE `eh_punch_scheduling_rules` (
+-- DROP TABLE IF EXISTS `eh_punch_workday_rules`;
+CREATE TABLE `eh_punch_workday_rules` (
   `id` BIGINT NOT NULL COMMENT 'id', 
   `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
-  `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id',
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
   `name` VARCHAR(128) COMMENT 'wifi rule name ',
   `description` VARCHAR(256) ,
   `work_week_dates` VARCHAR(8) DEFAULT '0000000' COMMENT '7位，从左至右每一位表示星期7123456,值:0-关闭 1-开放 example:周12345上班[0111110]',
   `creator_uid` BIGINT ,
   `create_time` DATETIME , 
+  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -128,22 +127,18 @@ DROP TABLE IF EXISTS `eh_punch_rules`;
 CREATE TABLE `eh_punch_rules` (
   `id` BIGINT NOT NULL COMMENT 'id', 
   `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
-  `owner_id` BIGINT  COMMENT 'owner resource(user/organization) id',
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
   `name` VARCHAR(128) COMMENT 'wifi rule name ',
   `time_rule_id` BIGINT COMMENT 'fk:eh_punch_time_rules id',
   `location_rule_id` BIGINT COMMENT 'fk:eh_punch_geopoints id',
   `wifi_rule_id` BIGINT COMMENT 'fk:eh_punch_wifi_rules id',
   `scheduling_rule_id` BIGINT COMMENT 'fk:eh_punch_scheduling_rules id',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `operator_uid` BIGINT ,
+  `operate_time` DATETIME ,
   
   PRIMARY KEY (`id`),
-  KEY `fk_eh_punch_time_rules` (`time_rule_id`),
-  CONSTRAINT `fk_eh_punch_rules_ibfk_1` FOREIGN KEY (`time_rule_id`) REFERENCES `eh_punch_time_rules` (`id`),
-  KEY `fk_eh_punch_location_rules` (`location_rule_id`),
-  CONSTRAINT `fk_eh_punch_rules_ibfk_2` FOREIGN KEY (`location_rule_id`) REFERENCES `eh_punch_location_rules` (`id`),
-  KEY `fk_eh_punch_wifi_rules` (`wifi_rule_id`),
-  CONSTRAINT `fk_eh_punch_rules_ibfk_3` FOREIGN KEY (`wifi_rule_id`) REFERENCES `eh_punch_wifi_rules` (`id`),
-  KEY `fk_eh_punch_scheduling_rules` (`scheduling_rule_id`),
-  CONSTRAINT `fk_eh_punch_rules_ibfk_4` FOREIGN KEY (`scheduling_rule_id`) REFERENCES `eh_punch_scheduling_rules` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ;
 
 
@@ -160,5 +155,6 @@ CREATE TABLE `eh_punch_rule_owner_map` (
   `description` VARCHAR (256),
   `creator_uid` BIGINT,
   `create_time` DATETIME,
+  
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ;
