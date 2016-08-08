8791a163207d518b93f4fa5f03d81805b40c1f46
sunwen <wen.sun@zuolin.com>
2016-08-08 19:03:17 +0800

    parking recharge

--- ehrest/src/main/java/com/everhomes/rest/parking/ParkingRechargeRateStatus.java
+++ ehrest/src/main/java/com/everhomes/rest/parking/ParkingRechargeRateStatus.java
@@ -2,10 +2,10 @@
 package com.everhomes.rest.parking;
 
 /**
- * <ul>停车充值订单状态
+ * <ul>停车充值费率状态
  * <li>INACTIVE(0): 无效</li>
- * <li>WAITING(1): 未支付</li>
- * <li>ACTIVE(2): 已支付</li>
+ * <li>WAITING(1): wait</li>
+ * <li>ACTIVE(2): 有效</li>
  * </ul>
  */
 public enum ParkingRechargeRateStatus {
