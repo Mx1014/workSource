95fb87b8a0eab822e8346877900707bec4280652
ying.xiong <ying.xiong@zuolin.com>
2016-08-10 16:50:01 +0800

    update categoryid into yellowpage table

--- db/yellowpage-delta-data-release.sql
+++ db/yellowpage-delta-data-release.sql
@@ -3,10 +3,11 @@ INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('yel
 INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('yellowPage', '10003', 'zh_CN', '该服务联盟企业记录不存在');
 
 -- 富文本default数据
-SET @rich_texts_id = (SELECT MAX(id) FROM `eh_rich_texts`);
 INSERT INTO `eh_rich_texts` (`id`, `namespace_id`, `owner_id`, `owner_type`, `resource_type`, `content_type`, `content`, `create_time`)
-    VALUES ((@rich_texts_id := @rich_texts_id + 1), '0', '0', 'DEFAULT', 'about', 'richtext', '', UTC_TIMESTAMP());
+    VALUES (1, '0', '0', 'DEFAULT', 'about', 'richtext', '', UTC_TIMESTAMP());
 INSERT INTO `eh_rich_texts` (`id`, `namespace_id`, `owner_id`, `owner_type`, `resource_type`, `content_type`, `content`, `create_time`)
-    VALUES ((@rich_texts_id := @rich_texts_id + 1), '0', '0', 'DEFAULT', 'introduction', 'richtext', '', UTC_TIMESTAMP());
+    VALUES (2, '0', '0', 'DEFAULT', 'introduction', 'richtext', '', UTC_TIMESTAMP());
 INSERT INTO `eh_rich_texts` (`id`, `namespace_id`, `owner_id`, `owner_type`, `resource_type`, `content_type`, `content`, `create_time`)
-    VALUES ((@rich_texts_id := @rich_texts_id + 1), '0', '0', 'DEFAULT', 'agreement', 'link', '', UTC_TIMESTAMP());
+    VALUES (3, '0', '0', 'DEFAULT', 'agreement', 'link', '', UTC_TIMESTAMP());
+    
+update eh_yellow_pages a set integral_tag2 = (select id from eh_categories where name = a.string_tag3 and namespace_id=(select namespace_id from eh_communities where id = a.owner_id) and parent_id = 100001);
