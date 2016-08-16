a64f86b7ac6f13207a39c64b9d054beb1dd12b15
wuhan <han.wu@zuolin.com>
2016-08-16 17:30:41 +0800

    table schema

--- db/3.8.2-delta-schema.sql
+++ db/3.8.2-delta-schema.sql
@@ -122,23 +122,24 @@ CREATE TABLE `eh_punch_workday_rules` (
 -- 
 -- 打卡总规则表
 -- 
--- DROP TABLE IF EXISTS `eh_punch_rules`;
--- CREATE TABLE `eh_punch_rules` (
---  `id` BIGINT NOT NULL COMMENT 'id', 
-  -- `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
-  -- `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
-  -- `name` VARCHAR(128) COMMENT 'wifi rule name ',
-  -- `time_rule_id` BIGINT COMMENT 'fk:eh_punch_time_rules id',
-  -- `location_rule_id` BIGINT COMMENT 'fk:eh_punch_geopoints id',
-  -- `wifi_rule_id` BIGINT COMMENT 'fk:eh_punch_wifi_rules id',
-  -- `workday_rule_id` BIGINT COMMENT 'fk:eh_punch_workday_rules id',
-  -- `creator_uid` BIGINT,
-  -- `create_time` DATETIME,
-  -- `operator_uid` BIGINT ,
-  -- `operate_time` DATETIME ,
+DROP TABLE IF EXISTS `eh_punch_rules`;
+CREATE TABLE `eh_punch_rules` (
+  `id` BIGINT NOT NULL COMMENT 'id', 
+  `owner_type` VARCHAR(128) COMMENT 'owner resource(user/organization) type',
+  `owner_id` BIGINT COMMENT 'owner resource(user/organization) id',
+  `name` VARCHAR(128) COMMENT 'wifi rule name ',
+  `time_rule_id` BIGINT COMMENT 'fk:eh_punch_time_rules id',
+  `location_rule_id` BIGINT COMMENT 'fk:eh_punch_geopoints id',
+  `wifi_rule_id` BIGINT COMMENT 'fk:eh_punch_wifi_rules id',
+  `workday_rule_id` BIGINT COMMENT 'fk:eh_punch_workday_rules id',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `operator_uid` BIGINT ,
+  `operate_time` DATETIME ,
   
-  -- PRIMARY KEY (`id`)
--- ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 ;
+  PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 ;
+ 
 
 
 -- 
