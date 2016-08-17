ccd9e6a6e59249ab883ec20c5727324953c91d69
sunwen <wen.sun@zuolin.com>
2016-08-17 10:10:39 +0800

    update repair schema

--- db/repair-delta-schema.sql
+++ db/repair-delta-schema.sql
@@ -1,19 +1,17 @@
 
--- DROP TABLE IF EXISTS `eh_community_tasks`;
-CREATE TABLE `eh_community_tasks` (
+DROP TABLE IF EXISTS `eh_community_tasks`;
+CREATE TABLE `eh_pm_tasks` (
 	`id` BIGINT NOT NULL COMMENT 'id of the record',
 	`namespace_id` INTEGER NOT NULL DEFAULT 0,
 	`owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the standard, community, etc',
 	`owner_id` BIGINT NOT NULL DEFAULT 0,
 
 	`category_id` BIGINT NOT NULL DEFAULT 0,
-	`category_name` VARCHAR(128) NOT NULL DEFAULT '',
-	`child_category_id` BIGINT NOT NULL DEFAULT 0,
-	`child_category_name` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'the name of category',
+
 	`address` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'detail address',
 	`content` TEXT NOT NULL COMMENT 'content data',
 	`status` TINYINT NOT NULL DEFAULT 1 COMMENT '0: inactive 1: wating, 2: allocated 3: completed 4: closed',
-	`evaluate_score` INT COMMENT 'evaluate score',
+	`star` TINYINT COMMENT 'evaluate score',
 
 	`unprocessed_time` DATETIME,
 	`processing_time` DATETIME,
@@ -21,40 +19,41 @@ CREATE TABLE `eh_community_tasks` (
 	`closed_time` DATETIME,
 
 	`creator_uid` BIGINT NOT NULL DEFAULT 0,
-    `create_time` DATETIME,
-    `delete_uid` BIGINT NOT NULL DEFAULT 0,
-    `delete_time` DATETIME,
+  `create_time` DATETIME,
+  `delete_uid` BIGINT NOT NULL DEFAULT 0,
+  `delete_time` DATETIME,
 	
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
--- DROP TABLE IF EXISTS `eh_community_task_logs`;
-CREATE TABLE `eh_community_task_logs` (
+ DROP TABLE IF EXISTS `eh_community_task_logs`;
+CREATE TABLE `eh_pm_task_logs` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0,
   `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the standard, community, etc',
   `owner_id` BIGINT NOT NULL DEFAULT 0,
-  `task_id` BIGINT NOT NULL,
+  `task_id` BIGINT NOT NULL DEFAULT 0,
 
   `content` TEXT NOT NULL  COMMENT 'content data',
   `status` TINYINT NOT NULL DEFAULT 1 COMMENT '0: inactive 1: wating, 2: allocated 3: completed 4: closed',
-    `target_id` BIGINT NOT NULL COMMENT 'target user id if target_type is a user',
+  `task_type` VARCHAR(32) COMMENT 'task type assigned by organization',
+  `target_id` BIGINT NOT NULL DEFAULT COMMENT 'target user id if target_type is a user',
 
   `operator_uid` BIGINT NOT NULL DEFAULT 0,
-    `operator_time` DATETIME,
+  `operator_time` DATETIME,
 
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP TABLE IF EXISTS `eh_community_task_attachments`;
-CREATE TABLE `eh_community_task_attachments` (
+ DROP TABLE IF EXISTS `eh_community_task_attachments`;
+CREATE TABLE `eh_pm_task_attachments` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
-  `owner_id` BIGINT NOT NULL COMMENT 'owner id, e.g comment_id',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'owner id, e.g comment_id',
   `content_type` VARCHAR(32) COMMENT 'attachment object content type',
   `content_uri` VARCHAR(1024) COMMENT 'attachment object link info on storage',
-  `creator_uid` BIGINT NOT NULL,
-  `create_time` DATETIME NOT NULL,
+  `creator_uid` BIGINT NOT NULL DEFAULT 0,
+  `create_time` DATETIME,
   
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
