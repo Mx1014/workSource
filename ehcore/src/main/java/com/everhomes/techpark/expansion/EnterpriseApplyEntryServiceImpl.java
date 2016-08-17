b489329a48ffa6c81dbffe599c909d3e8da2af8b
wuhan <han.wu@zuolin.com>
2016-08-17 10:07:51 +0800

    fix bug : apply entry

--- ehcore/src/main/java/com/everhomes/techpark/expansion/EnterpriseApplyEntryServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/techpark/expansion/EnterpriseApplyEntryServiceImpl.java
@@ -61,13 +61,11 @@ import com.everhomes.rest.techpark.expansion.ListEnterpriseDetailResponse;
 import com.everhomes.rest.techpark.expansion.UpdateApplyEntryStatusCommand;
 import com.everhomes.rest.techpark.expansion.UpdateLeasePromotionCommand;
 import com.everhomes.rest.techpark.expansion.UpdateLeasePromotionStatusCommand;
-import com.everhomes.rest.user.IdentifierType;
 import com.everhomes.server.schema.Tables;
 import com.everhomes.settings.PaginationConfigHelper;
 import com.everhomes.sms.SmsProvider;
 import com.everhomes.user.User;
 import com.everhomes.user.UserContext;
-import com.everhomes.user.UserIdentifier;
 import com.everhomes.user.UserProvider;
 import com.everhomes.util.ConvertHelper;
 import com.everhomes.util.RuntimeErrorException;
@@ -295,62 +293,48 @@ public class EnterpriseApplyEntryServiceImpl implements EnterpriseApplyEntryServ
 //		SimpleDateFormat datetimeSF = new SimpleDateFormat("MM-dd HH:mm");
 //        sendApplyEntrySmsToManager(phoneNumber, cmd.getApplyUserName(),cmd.getContactPhone(), datetimeSF.format(new Date()), 
 //                lp.getRentPosition(), cmd.getAreaSize()+"平米", cmd.getEnterpriseName(), cmd.getDescription(), cmd.getNamespaceId());
-        // 根据source type来区分
+        // 根据apply type来区分
         String phoneNumber = null;
         String location = null;
-        ApplyEntrySourceType sourceType = ApplyEntrySourceType.fromType(cmd.getSourceType());
-        if(sourceType != null && cmd.getSourceId() != null) {
-        	//modify by wuhan 2016-8-17 园区入驻2.2全部变成了给管理员发短信(其实是没有for_rent的类型了)
-          Building building = this.communityProvider.findBuildingById(cmd.getSourceId());
-          if(building != null) {
-          	OrganizationMember member = organizationProvider.findOrganizationMemberById(building.getManagerUid());
-
-              if(null != member) {
-                  phoneNumber = member.getContactToken();
-              }
-              location = building.getName();
-          } else {
-              if(LOGGER.isWarnEnabled()) {
-                  LOGGER.warn("Building not found, builingId={}, cmd={}", cmd.getSourceId(), cmd);
-              }
-          }
-//            switch(sourceType) {
-//            case FOR_RENT:
-//                LeasePromotion lp = this.enterpriseApplyEntryProvider.getLeasePromotionById(cmd.getSourceId());
-//                if(lp != null) {
-//                    phoneNumber = lp.getContactPhone();
-//                    location = lp.getRentPosition();
-//                } else {
-//                    if(LOGGER.isWarnEnabled()) {
-//                        LOGGER.warn("Lease promotion not found, promotionId={}, cmd={}", cmd.getSourceId(), cmd);
-//                    }
-//                }
-//                break;
-//            case BUILDING:
-//                Building building = this.communityProvider.findBuildingById(cmd.getSourceId());
-//                if(building != null) {
-//                	OrganizationMember member = organizationProvider.findOrganizationMemberById(building.getManagerUid());
-//
-//                    if(null != member) {
-//                        phoneNumber = member.getContactToken();
-//                    }
-//                    location = building.getName();
-//                } else {
-//                    if(LOGGER.isWarnEnabled()) {
-//                        LOGGER.warn("Building not found, builingId={}, cmd={}", cmd.getSourceId(), cmd);
-//                    }
-//                }
-//                break;
-//            default:
-//                if(LOGGER.isWarnEnabled()) {
-//                    LOGGER.warn("Apply entry source type not supported, sourceType={}, cmd={}", sourceType, cmd);
-//                }
-//                break;
-//            }
+        ApplyEntryApplyType applyType = ApplyEntryApplyType.fromType(cmd.getApplyType());
+        if(applyType != null && cmd.getSourceId() != null) {
+			//modify by wuhan 2016-8-17 园区入驻2.2全部变成了给管理员发短信(其实是没有for_rent的类型了)
+			  Building building = this.communityProvider.findBuildingById(cmd.getSourceId());
+			  if(building != null) {
+			  	OrganizationMember member = organizationProvider.findOrganizationMemberById(building.getManagerUid());
+			
+			      if(null != member) {
+			          phoneNumber = member.getContactToken();
+			      }
+			      location = building.getName();
+			  } else {
+			      if(LOGGER.isWarnEnabled()) {
+			          LOGGER.warn("Building not found, builingId={}, cmd={}", cmd.getSourceId(), cmd);
+			      }
+			  }
 
 	  		SimpleDateFormat datetimeSF = new SimpleDateFormat("MM-dd HH:mm");
-	  		sendApplyEntrySmsToManager(phoneNumber, cmd.getApplyUserName(),cmd.getContactPhone(), datetimeSF.format(new Date()), 
-	  				location, cmd.getAreaSize()+"平米", cmd.getEnterpriseName(), cmd.getDescription(), cmd.getNamespaceId(),"看楼");
+	  		
+            switch(applyType) {
+            case APPLY:
+            	sendApplyEntrySmsToManager(phoneNumber, cmd.getApplyUserName(),cmd.getContactPhone(), datetimeSF.format(new Date()), 
+    	  				location, cmd.getAreaSize()+"平米", cmd.getEnterpriseName(), cmd.getDescription(), cmd.getNamespaceId(),"看楼");
+                break;
+            case RENEW:
+            	sendApplyEntrySmsToManager(phoneNumber, cmd.getApplyUserName(),cmd.getContactPhone(), datetimeSF.format(new Date()), 
+    	  				location, cmd.getAreaSize()+"平米", cmd.getEnterpriseName(), cmd.getDescription(), cmd.getNamespaceId(),"续租");
+                break;
+            case EXPANSION:
+            	sendApplyEntrySmsToManager(phoneNumber, cmd.getApplyUserName(),cmd.getContactPhone(), datetimeSF.format(new Date()), 
+    	  				location, cmd.getAreaSize()+"平米", cmd.getEnterpriseName(), cmd.getDescription(), cmd.getNamespaceId(),"扩租");
+                break;
+            default:
+                if(LOGGER.isWarnEnabled()) {
+                    LOGGER.warn("Apply entry source type not supported, applyType={}, cmd={}", applyType, cmd);
+                }
+                break;
+            }
+
         }
 
 		return true;
