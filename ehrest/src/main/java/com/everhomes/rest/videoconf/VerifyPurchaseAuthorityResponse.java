a7e90256f7ae1734fb331ae853ff14203f4c57a6
ying.xiong <ying.xiong@zuolin.com>
2016-08-26 17:43:36 +0800

    change name

--- ehrest/src/main/java/com/everhomes/rest/videoconf/VerifyPurchaseAuthorityResponse.java
+++ ehrest/src/main/java/com/everhomes/rest/videoconf/VerifyPurchaseAuthorityResponse.java
@@ -5,7 +5,7 @@ import com.everhomes.util.StringHelper;
 /**
  * <ul>
  *  <li>purchaseAuthority: 是否具有购买权限 </li>
- *  <li>isAllTrial: 是否全为试用账号 true: 全为测试账号  false：有非测试账号 </li>
+ *  <li>allTrial: 是否全为试用账号 true: 全为测试账号  false：有非测试账号 </li>
  *  <li>enterpriseActiveAccountCount: 企业有效账号数</li>
  *  <li>enterpriseAccountCount: 企业账号数</li>
  * </ul>
@@ -15,7 +15,7 @@ public class VerifyPurchaseAuthorityResponse {
 	
 	private boolean purchaseAuthority;
 	
-	private boolean isAllTrial;
+	private boolean allTrial;
 	
 	private int enterpriseActiveAccountCount;
 	
@@ -30,11 +30,11 @@ public class VerifyPurchaseAuthorityResponse {
 	}
 
 	public boolean isAllTrial() {
-		return isAllTrial;
+		return allTrial;
 	}
 
-	public void setAllTrial(boolean isAllTrial) {
-		this.isAllTrial = isAllTrial;
+	public void setAllTrial(boolean allTrial) {
+		this.allTrial = allTrial;
 	}
 
 	public int getEnterpriseActiveAccountCount() {
