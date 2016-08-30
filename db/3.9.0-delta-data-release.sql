6805f3937c34cbce43f17a6f63a12da9d93b01f4
sunwen <wen.sun@zuolin.com>
2016-08-30 10:12:28 +0800

    add init data

--- db/3.9.0-delta-data-release.sql
+++ db/3.9.0-delta-data-release.sql
@@ -88,6 +88,14 @@ INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description
 INSERT INTO `eh_categories` (`id`, `parent_id`, `link_id`, `name`, `path`, `default_order`, `status`, `create_time`, `delete_time`, `logo_uri`, `description`, `namespace_id`) 
 VALUES ('5', '0', '0', '任务', '任务', '0', '2', '2015-09-28 06:09:03', NULL, NULL, NULL, '999991');
 
+SET @eh_locale_strings_id = (SELECT MAX(id) FROM `eh_locale_strings`);
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) 
+	VALUES ((@eh_locale_strings_id := @eh_locale_strings_id + 1), 'pmtask', '10001', 'zh_CN', '任务分类已存在');
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) 
+	VALUES ((@eh_locale_strings_id := @eh_locale_strings_id + 1), 'pmtask', '10002', 'zh_CN', '服务类型不存在');
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) 
+	VALUES ((@eh_locale_strings_id := @eh_locale_strings_id + 1), 'pmtask', '10003', 'zh_CN', '目标用户不存在');
+
 update eh_service_alliances set type = 3 where namespace_id = 999999
 
 
