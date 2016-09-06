e9752da00fd2e88a6731c74d6161ccbdc62e515d
janson <gandancing@gmail.com>
2016-09-06 17:57:12 +0800

    fix for develop push

--- ehnextgen-messaging/src/main/java/com/everhomes/pusher/PusherServiceImpl.java
+++ ehnextgen-messaging/src/main/java/com/everhomes/pusher/PusherServiceImpl.java
@@ -433,23 +433,23 @@ public class PusherServiceImpl implements PusherService, ApnsServiceFactory {
     
     @Override
     public void pushServiceTest(PushMessageCommand cmd) {
-//        ApnsService service =
-//                APNS.newService()
-//                .withCert("/tmp/apns_appstore.p12", "zuolin")
-//                .withAppleDestination(true)
-//                .build();
+        ApnsService service =
+                APNS.newService()
+                .withCert("/tmp/apns_appstore.p12", "zuolin")
+                .withAppleDestination(false)
+                .build();
         
 //        ApnsService service = getApnsService("namespace:1000000");
-//        
-//        String payload = APNS.newPayload().alertBody(cmd.getMessage() + Double.valueOf(Math.random()) ).build();
-//        String token = cmd.getDeviceId();
-//        service.push(token, payload);
-//        
-//        Map<String, Date> inactiveDevices = service.getInactiveDevices();
-//        for (String deviceToken : inactiveDevices.keySet()) {
-//            Date inactiveAsOf = inactiveDevices.get(deviceToken);
-//            LOGGER.info("date=" + inactiveAsOf + " deviceToken=" + deviceToken);
-//        }
+        
+        String payload = APNS.newPayload().alertBody(cmd.getMessage() + Double.valueOf(Math.random()) ).build();
+        String token = cmd.getDeviceId();
+        service.push(token, payload);
+        
+        Map<String, Date> inactiveDevices = service.getInactiveDevices();
+        for (String deviceToken : inactiveDevices.keySet()) {
+            Date inactiveAsOf = inactiveDevices.get(deviceToken);
+            LOGGER.info("date=" + inactiveAsOf + " deviceToken=" + deviceToken);
+        }
         
         //http://www.concretepage.com/java/jdk-8/java-8-completablefuture-example
 //        List<Integer> list = Arrays.asList(10,20,30,40);
@@ -462,12 +462,12 @@ public class PusherServiceImpl implements PusherService, ApnsServiceFactory {
 //        list.stream().map(s->CompletableFuture.supplyAsync(()->s+s))
 //                .map(f->f.whenComplete((result,error)->System.out.println(result+" Error:"+error))).count();
         
-        Map<String, Long> deviceMap = new HashMap<String, Long>();
-        deviceMap.put("7e978fbb0d127671e30b4704414c7bdf272b06d066fccde8a2f309bcfa110393", 0l);
-        deviceMap.put("frompython_195870_xiaoxiao2", 0l);
-        deviceMap = requestDevices(deviceMap);
-        
-        LOGGER.info("all device is: " + deviceMap);
+//        Map<String, Long> deviceMap = new HashMap<String, Long>();
+//        deviceMap.put("7e978fbb0d127671e30b4704414c7bdf272b06d066fccde8a2f309bcfa110393", 0l);
+//        deviceMap.put("frompython_195870_xiaoxiao2", 0l);
+//        deviceMap = requestDevices(deviceMap);
+//        
+//        LOGGER.info("all device is: " + deviceMap);
     }
     
 //    private static int getNumber(int a){
