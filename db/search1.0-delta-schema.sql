03d9633b43e6f23a67d15fc495a41a9c8ce57e78
ying.xiong <ying.xiong@zuolin.com>
2016-09-08 10:23:34 +0800

    add schema

---
+++ db/search1.0-delta-schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `eh_search_types`;
+CREATE TABLE `eh_search_types` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the type, community, enterprise, etc',
+  `owner_id` BIGINT NOT NULL DEFAULT 0,
+  `name` VARCHAR(128) NOT NULL DEFAULT '',
+  `content_type` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'search content type',
+  `status` TINYINT NOT NULL DEFAULT '0' COMMENT '0: inactive, 1: active', 
+  `create_time` DATETIME,
+  `delete_time` DATETIME,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
