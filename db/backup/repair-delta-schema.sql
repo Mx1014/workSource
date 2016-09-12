d66bdbf6af79f7b3128870c11a1d11b6cfcbb3b6
sunwen <wen.sun@zuolin.com>
2016-09-12 11:57:15 +0800

    add wuye repair column

--- db/backup/repair-delta-schema.sql
+++ db/backup/repair-delta-schema.sql
@@ -69,6 +69,7 @@ CREATE TABLE `eh_pm_task_logs` (
   `target_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'target user id if target_type is a user',
 
   `operator_uid` BIGINT NOT NULL DEFAULT 0,
+  `name` VARCHAR(64) COMMENT 'the name of user',
   `operator_time` DATETIME,
 
     PRIMARY KEY (`id`)
--- db/ehcore-server-schema.sql
+++ db/ehcore-server-schema.sql
@@ -3882,6 +3882,7 @@ CREATE TABLE `eh_pm_task_logs` (
   `target_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'target user id if target_type is a user',
 
   `operator_uid` BIGINT NOT NULL DEFAULT 0,
+  `name` VARCHAR(64) COMMENT 'the name of user',
   `operator_time` DATETIME,
 
   PRIMARY KEY (`id`)
