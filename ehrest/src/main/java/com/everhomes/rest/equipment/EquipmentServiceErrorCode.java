c5619d496422e0cd5b4cb771f4ed7ebeb57ea0d5
ying.xiong <ying.xiong@zuolin.com>
2016-09-13 16:58:37 +0800

    modify verifyEquipmentLocation

--- ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentServiceErrorCode.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentServiceErrorCode.java
@@ -23,4 +23,6 @@ public interface EquipmentServiceErrorCode {
     static final int ERROR_CREATE_EXCEL = 10016;  //生成excel信息有问题
     static final int ERROR_DOWNLOAD_EXCEL = 10017;  //下载excel信息有问题
     static final int ERROR_EQUIPMENT_TASK_INACTIVE = 10018;//任务已失效
+    
+    static final int ERROR_EQUIPMENT_TASK_QRCODE = 10019;//二维码和任务设备不对应
 }
--- ehrest/src/main/java/com/everhomes/rest/equipment/VerifyEquipmentLocationCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/VerifyEquipmentLocationCommand.java
@@ -4,6 +4,7 @@ import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
+ *  <li>equipmentId: 设备id</li>
  *  <li>qrCodeToken: 设备二维码token</li>
  *  <li>longitude: 经度</li>
  *  <li>latitude: 纬度</li>
@@ -11,12 +12,21 @@ import com.everhomes.util.StringHelper;
  */
 public class VerifyEquipmentLocationCommand {
 	
+	private Long equipmentId;
+	
 	private String qrCodeToken;
 	
 	private Double longitude;
 	
     private Double latitude;
     
+	public Long getEquipmentId() {
+		return equipmentId;
+	}
+
+	public void setEquipmentId(Long equipmentId) {
+		this.equipmentId = equipmentId;
+	}
 
 	public String getQrCodeToken() {
 		return qrCodeToken;
