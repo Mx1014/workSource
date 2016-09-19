f5e651b9c8344ef85069c72ee827150c019b85d9
xq.tian <xq.tian@zuolin.com>
2016-09-19 17:21:22 +0800

    merge 3.9.2

--- ehnextgen-api/src/main/java/com/everhomes/locale/LocaleStringProvider.java
+++ ehnextgen-api/src/main/java/com/everhomes/locale/LocaleStringProvider.java
@@ -1,15 +1,14 @@
 // @formatter:off
 package com.everhomes.locale;
 
-import java.util.List;
-
 import com.everhomes.rest.locale.LocaleTemplateDTO;
+
+import java.util.List;
 
 public interface LocaleStringProvider {
     LocaleString find(String scope, String code, String locale);
-<<<<<<< HEAD
+
     LocaleString findByText(String scope, String text, String locale);
-=======
 
 	List<LocaleTemplateDTO> listLocaleTemplate(int from, int pageSize, Integer namespaceId, String scope, Integer code,
 			String keyword);
@@ -17,5 +16,4 @@ public interface LocaleStringProvider {
 	LocaleTemplate findTemplateById(Long id);
 
 	void updateLocaleTemplate(LocaleTemplate template);
->>>>>>> 3.9.2
 }
