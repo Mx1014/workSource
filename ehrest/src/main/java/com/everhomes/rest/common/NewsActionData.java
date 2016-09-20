a379b6e9987e49af218830494efd0595b63b51d0
wuhan <han.wu@zuolin.com>
2016-09-20 16:52:43 +0800

    modfiy type

--- ehrest/src/main/java/com/everhomes/rest/common/NewsActionData.java
+++ ehrest/src/main/java/com/everhomes/rest/common/NewsActionData.java
@@ -16,7 +16,9 @@ public class NewsActionData implements Serializable  {
 	 */
 	private static final long serialVersionUID = -5152885247968357254L;
 
-    private Long categoryId;
+    private Long categoryId; 
+    
+    private String timeWidgetStyle ;
 	 
 
 	@Override
@@ -32,6 +34,16 @@ public class NewsActionData implements Serializable  {
 
 	public void setCategoryId(Long categoryId) {
 		this.categoryId = categoryId;
+	}
+
+
+	public String getTimeWidgetStyle() {
+		return timeWidgetStyle;
+	}
+
+
+	public void setTimeWidgetStyle(String timeWidgetStyle) {
+		this.timeWidgetStyle = timeWidgetStyle;
 	}
  
 }
