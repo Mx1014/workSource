635e16e8b4a6645b32f6a882a72acdb9c270b9a6
sunwen <wen.sun@zuolin.com>
2016-09-23 14:25:37 +0800

    add parking coloumn

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -2,3 +2,4 @@ ALTER TABLE eh_parking_lots ADD COLUMN `max_request_num` INTEGER NOT NULL DEFAUL
 
 ALTER TABLE eh_parking_recharge_orders ADD COLUMN `recharge_type` TINYINT NOT NULL DEFAULT 0 COMMENT '1: monthly, 2: temporary';
 
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN `order_token` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'it may be from 3rd system';
