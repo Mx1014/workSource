18a8493dbdcb33fdf95f452e26ba1f303d4f9a13
yanshaofan <sf.yan@zuolin.com>
2016-10-11 20:36:59 +0800

    add menu sql

--- db/3.10.0-delta-data-release.sql
+++ db/3.10.0-delta-data-release.sql
@@ -11,3 +11,17 @@ INSERT INTO `eh_acls` (`id`,`owner_type`,`grant_type`,`privilege_id`,`role_id`,`
 -- added by sfyan 20161011
 UPDATE `eh_web_menu_scopes` SET `menu_name` = '创业活动', `apply_policy` = 1 WHERE `owner_type` = 'EhNamespaces' AND `owner_id` = 999987 AND `menu_id` = 11100;
 UPDATE `eh_web_menu_scopes` SET `menu_name` = '路演直播', `apply_policy` = 1 WHERE `owner_type` = 'EhNamespaces' AND `owner_id` = 999987 AND `menu_id` = 11400;
+
+-- 增加日活统计菜单 added by sfyan 20161011
+set @web_menu_privilege_id = (SELECT MAX(id) FROM `eh_web_menu_privileges`);
+INSERT INTO `eh_acl_privileges` (`id`,`app_id`,`name`,`description`,`tag`)
+VALUES (716,0,'日活统计','日活统计 全部功能',null);
+
+INSERT INTO `eh_web_menus` (`id`,`name`,`parent_id`,`icon_url`,`data_type`,`leaf_flag`,`status`,`path`,`type`,`sort_num`)
+VALUES (49800,'日活统计',40000,null,'dailyActive--dailyActive',0,2,'/40000/49800','park',459);
+
+INSERT INTO `eh_web_menu_privileges` (`id`,`privilege_id`,`menu_id`,`name`,`show_flag`,`status`,`discription`,`sort_num`)
+VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1),716,49800,'日活统计',1,1,'日活统计  全部权限',16);
+
+set @acl_id = (SELECT MAX(id) FROM `eh_acls`);
+INSERT INTO `eh_acls` (`id`,`owner_type`,`grant_type`,`privilege_id`,`role_id`,`order_seq`,`creator_uid`,`create_time`) VALUES((@acl_id := @acl_id + 1), 'EhOrganizations', 1, 716, 1001,0,1,now());
