566a1fe5dc20bfa4ad067693f12e7a9f9db8b639
yanshaofan <sf.yan@zuolin.com>
2016-10-11 20:18:39 +0800

    add menu sql

--- db/3.10.0-delta-data-release.sql
+++ db/3.10.0-delta-data-release.sql
@@ -2,3 +2,8 @@
 INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (0, 'active.count','6-10','active count ');
 INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (999987, 'active.count','6-10','active count ');
 INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (1000000, 'active.count','6-10','active count ');
+
+-- added by sfyan 20161011
+set @acl_id = (SELECT MAX(id) FROM `eh_acls`);
+INSERT INTO `eh_acls` (`id`,`owner_type`,`grant_type`,`privilege_id`,`role_id`,`order_seq`,`creator_uid`,`create_time`) VALUES((@acl_id := @acl_id + 1), 'EhOrganizations', 1, 315, 1001,0,1,now());
+INSERT INTO `eh_acls` (`id`,`owner_type`,`grant_type`,`privilege_id`,`role_id`,`order_seq`,`creator_uid`,`create_time`) VALUES((@acl_id := @acl_id + 1), 'EhOrganizations', 1, 315, 1002,0,1,now());
