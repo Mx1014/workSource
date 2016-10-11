55686ebae9d0f00f0608623df53ae2e32c1cb40a
wuhan <han.wu@zuolin.com>
2016-10-11 20:17:37 +0800

    modify name

--- db/3.10.0-init-data.sql
+++
@@ -1,4 +0,0 @@
--- added by wh 2016-10-11 设置活跃用户数的区间
-INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (0, 'active.count','6-10','active count ');
-INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (999987, 'active.count','6-10','active count ');
-INSERT INTO `eh_configurations`(`namespace_id`, `name`, `value`, `description`) VALUES (1000000, 'active.count','6-10','active count ');
