e3342180b938ce64b109e30352185246e97d0032
sunwen <wen.sun@zuolin.com>
2016-10-12 10:33:28 +0800

    parking

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -6,3 +6,16 @@ ALTER TABLE eh_parking_recharge_orders ADD COLUMN `recharge_type` TINYINT NOT NU
 
 ALTER TABLE eh_parking_recharge_orders ADD COLUMN `order_token` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'it may be from 3rd system';
 
+
+-- add by wh 20161011
+-- 用户活动率计算表
+-- DROP TABLE IF EXISTS `eh_stat_active_users`;
+CREATE TABLE `eh_stat_active_users` (
+  `id` BIGINT NOT NULL,
+  `stat_date` DATE COMMENT '统计日期',
+  `namespace_id` INTEGER NOT NULL DEFAULT '0', 
+  `active_count` INTEGER NOT NULL DEFAULT '0' COMMENT '活动人数',
+  `total_count` INTEGER NOT NULL DEFAULT '0' COMMENT '总人数-当天的', 
+  `create_time` DATETIME,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
