fa46cf48bb732375b2e33de15f372d4f9c5810d0
janson <gandancing@gmail.com>
2016-10-12 15:19:04 +0800

    fix keyword empty

--- ehnextgen-messaging/src/main/java/com/everhomes/user/UserImpersonationProviderImpl.java
+++ ehnextgen-messaging/src/main/java/com/everhomes/user/UserImpersonationProviderImpl.java
@@ -227,7 +227,7 @@ public class UserImpersonationProviderImpl implements UserImpersonationProvider 
             if(namespaceId != null) {
                 cond = cond.and(Tables.EH_USERS.NAMESPACE_ID.eq(namespaceId));
             }
-            if(!StringUtils.isEmpty(keyword)){
+            if(keyword != null && !StringUtils.isEmpty(keyword)){
                  Condition cond1 = Tables.EH_USER_IDENTIFIERS.IDENTIFIER_TOKEN.like(keyword + "%");
                  cond1 = cond1.or(Tables.EH_USERS.NICK_NAME.like(keyword+"%"));
                  cond = cond.and(cond1);
