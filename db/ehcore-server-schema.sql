6a832dc9546a7bd84b19594b979650ca14e19da9
tangtong <tong.tang@zuolin.com>
2016-10-12 14:08:20 +0800

    merge from 3.10.0

--- db/ehcore-server-schema.sql
+++ db/ehcore-server-schema.sql
@@ -6284,8 +6284,24 @@ CREATE TABLE `eh_source_account` (
   `status` TINYINT COMMENT '0-available 1-occupied',
   `occupy_account_id` BIGINT,
   `conf_id` INTEGER,
+  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- add by wh 20161011
+-- 用户活动率计算表
+DROP TABLE IF EXISTS `eh_stat_active_users`;
+CREATE TABLE `eh_stat_active_users` (
+  `id` BIGINT NOT NULL,
+  `stat_date` DATE COMMENT '统计日期',
+  `namespace_id` INTEGER NOT NULL DEFAULT '0', 
+  `active_count` INTEGER NOT NULL DEFAULT '0' COMMENT '活动人数',
+  `total_count` INTEGER NOT NULL DEFAULT '0' COMMENT '总人数-当天的', 
+  `create_time` DATETIME,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 
 -- 订单交易流水表
@@ -6307,6 +6323,7 @@ CREATE TABLE `eh_stat_orders` (
   `shop_type` TINYINT NOT NULL DEFAULT 0 COMMENT '1-platform shop,2-self shop',
   `order_time` DATETIME,
   `create_time` DATETIME,
+  
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
