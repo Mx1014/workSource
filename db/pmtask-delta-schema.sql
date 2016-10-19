14b587e35afb8079e8bc0aed7f5f1cc60bf0ad03
sunwen <wen.sun@zuolin.com>
2016-10-19 11:03:14 +0800

    pmtask

--- db/pmtask-delta-schema.sql
+++ db/pmtask-delta-schema.sql
@@ -6,9 +6,23 @@ ALTER TABLE eh_pm_tasks ADD COLUMN `organization_id` BIGINT NOT NULL DEFAULT 0;
 ALTER TABLE eh_pm_tasks ADD COLUMN `requestor_name` VARCHAR(64) COMMENT 'the name of requestor';
 ALTER TABLE eh_pm_tasks ADD COLUMN `requestor_phone` VARCHAR(64) COMMENT 'the phone of requestor';
 ALTER TABLE eh_pm_tasks ADD COLUMN `address_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'address id';
-ALTER TABLE eh_pm_tasks ADD COLUMN `revisit_status` TINYINT NOT NULL DEFAULT 0 COMMENT '0: not 1: callbacked ';
 ALTER TABLE eh_pm_tasks ADD COLUMN `revisit_content` TEXT COMMENT 'revisit content';
 ALTER TABLE eh_pm_tasks ADD COLUMN `revisit_time` DATETIME;
+
+CREATE TABLE `eh_pm_task_targets` (
+  `id` bigint(20) NOT NULL,
+  `owner_type` varchar(64) NOT NULL,
+  `owner_id` bigint(20) DEFAULT NULL,
+  `target_type` varchar(64) NOT NULL COMMENT 'target object(user/group) type',
+  `target_id` bigint(20) DEFAULT NULL COMMENT 'target object(user/group) id',
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) 
+	VALUES ('313', 'pmtask.notification', '8', 'zh_CN', '任务操作模版', '${operatorName} ${operatorPhone} 已回访该任务', '0');
+
 
 
 ALTER TABLE eh_parking_lots ADD COLUMN `max_request_num` INTEGER NOT NULL DEFAULT 1 COMMENT 'the max num of the request card';
