b30081e968511325f54bb6878fb720743c47ab05
sunwen <wen.sun@zuolin.com>
2016-10-19 11:27:26 +0800

    pmtask

--- db/pmtask-delta-schema.sql
+++ db/pmtask-delta-schema.sql
@@ -16,6 +16,8 @@ CREATE TABLE `eh_pm_task_targets` (
   `target_type` varchar(64) NOT NULL COMMENT 'target object(user/group) type',
   `target_id` bigint(20) DEFAULT NULL COMMENT 'target object(user/group) id',
   `role_id` BIGINT NOT NULL DEFAULT 0,
+  `status` TINYINT NOT NULL DEFAULT 2 COMMENT '0: inactive, 2: active',
+
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
