d50604102d819b3ae20934076baaef5c8e1dae32
LiangQishi <qishi.liang@zuolin.com>
2016-10-25 15:58:25 +0800

    move the customer-manage-1.1 sql to backup

--- db/3.11.0-delta-data-debug.sql
+++ db/3.11.0-delta-data-debug.sql
@@ -1,18 +1,4 @@
---
--- 服务联盟4.5.0   add by xq.tian  2016/10/18
---
-SET @locale_string_id = (SELECT MAX(id) FROM `eh_locale_strings`);
-INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@locale_string_id := @locale_string_id + 1), 'serviceAlliance.category.display', '1', 'zh_CN', '列表-介绍');
-INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@locale_string_id := @locale_string_id + 1), 'serviceAlliance.category.display', '2', 'zh_CN', '列表-大图');
-
---
--- 服务联盟详情页面配置   add by xq.tian  2016/10/19
---
-SET @configuration_id = (SELECT MAX(id) FROM `eh_configurations`);
-INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES ((@configuration_id := @configuration_id + 1), 'serviceAlliance.serviceDetail.url', 'http://core.zuolin.com/service-alliance/index.html#/service_detail/%s/%s?_k=%s', '服务联盟详情页面URL', '0', NULL);
-
-
--- merge from customer-manage-1.1-delta-data-release.sql by lqs 20161025
+-- merge from customer-manage-1.1-delta-data-debug.sql by lqs 20161025
 --
 -- 车辆停车类型     add by xq.tian 2016/10/11
 --
@@ -38,5 +24,3 @@ INSERT INTO `eh_organization_owner_type` (`id`, `namespace_id`, `name`, `display
 SET @locale_string_id = (SELECT MAX (id) FROM `eh_locale_strings`);
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`)
   VALUES ((@locale_string_id := @locale_string_id + 1), 'pm', '18001', 'zh_CN', '该记录已经处于未认证状态');
-  
-  
--- db/3.11.0-delta-schema.sql
+++ db/3.11.0-delta-schema.sql
@@ -26,3 +26,22 @@ CREATE TABLE `eh_pm_task_targets` (
 
 INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) 
 	VALUES ('313', 'pmtask.notification', '8', 'zh_CN', '任务操作模版', '${operatorName} ${operatorPhone} 已回访该任务', '0');
+
+
+-- merge from customer-manage-1.1-delta-schema.sql 20161025 by lqs
+--
+-- 车辆停车类型     add by xq.tian 2016/10/11
+--
+CREATE TABLE `eh_parking_card_categories` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INT NOT NULL DEFAULT '0',
+  `owner_type` VARCHAR(32) DEFAULT NULL,
+  `owner_id` BIGINT DEFAULT NULL,
+  `card_type` TINYINT NOT NULL COMMENT '1. temp, 2. month, 3. free ,etc.',
+  `category_name` VARCHAR(64) NOT NULL COMMENT 'name of category',
+  `status` TINYINT NOT NULL COMMENT '1: normal, 0: delete',
+  `creator_uid` BIGINT DEFAULT NULL,
+  `create_time` DATETIME DEFAULT NULL,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;	
