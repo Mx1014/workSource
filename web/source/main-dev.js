e4087b3a3f09289cf3fa00770399a8cd2a3220aa
Kelven Yang <kelveny@gmail.com>
2016-10-31 19:29:29 -0700

    Add React-based API portal implementation

---
+++ web/source/main-dev.js
@@ -0,0 +1,71 @@
+/**
+ *
+ *  Standard React-Wdiget bootstrap main sequence
+ *
+ *  @Author Kelven Yang
+ *
+ */
+
+// React
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+
+// Redux
+import {applyMiddleware, combineReducers, compose} from 'redux'
+import thunk from 'redux-thunk'
+
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore, routerMiddleware} from 'react-router-redux'
+
+import {bindLocaleLoader} from 'widget-common-util/locale'
+import {createStore, apiDispatcherMiddleware} from 'widget-redux-util/redux-enhancer'
+import {actionInterceptor} from 'widget-redux-util/action-interceptor'
+
+import {loadLocaleResource} from './locale/resource-loader'
+
+import createLogger from 'redux-logger'
+import DevTools from './dev-tools'
+
+import Root from './root'
+import reducers from './reducers'
+
+//
+// Create store and launch (Standard)
+//
+bindLocaleLoader(loadLocaleResource)
+
+export const store = createStore(
+    reducers,
+    {},
+    compose(
+        applyMiddleware(
+            thunk,
+            actionInterceptor.getMiddleware(),
+            apiDispatcherMiddleware,
+            routerMiddleware(browserHistory),        // allow navigation through React action
+            createLogger()
+        ),
+        DevTools.instrument()
+    )
+);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+history.listen(location=> { console.log('go to location ' + location.pathname); });
+
+/*
+render(
+    <div>
+        <Root store={store} history={history} />
+        <DevTools store={store} />
+    </div>,
+    document.getElementById('root')
+);
+*/
+
+render(
+    <div style={{width: '100%', height: '100%'}}>
+        <Root store={store} history={history} />
+    </div>,
+    document.getElementById('root')
+);
+
