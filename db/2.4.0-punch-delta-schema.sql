0c00a135d47993df02178f034253d8418d084ab1
wuhan <han.wu@zuolin.com>
2016-11-09 11:39:07 +0800

    fix bug

--- db/2.4.0-punch-delta-schema.sql
+++ db/2.4.0-punch-delta-schema.sql
@@ -4,6 +4,10 @@
 ALTER TABLE `eh_approval_requests` ADD COLUMN `effective_date` DATE COMMENT 'when the request approval effective';
 ALTER TABLE `eh_approval_requests` ADD COLUMN `hour_length` DOUBLE COMMENT 'how long (hours) does the request effective'; 
 
+-- added by wh 2016-11-09 打卡计算结果还要对异常的设备进行计算
+
+ALTER TABLE `eh_punch_day_logs` ADD COLUMN `device_change_flag` TINYINT DEFAULT 0 COMMENT '0- unchange 1-changed' ;
+
 -- 3.11.0
 -- merge from pmtask-delta-schema.sql
 ALTER TABLE eh_pm_tasks ADD COLUMN `task_category_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'task category id';
