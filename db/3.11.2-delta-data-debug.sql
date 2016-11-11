7ddf08c048565de913969f7fdb2c15dad170e5ce
wuhan <han.wu@zuolin.com>
2016-11-11 15:35:27 +0800

    merge rental 2.2

--- db/3.11.2-delta-data-debug.sql
+++ db/3.11.2-delta-data-debug.sql
@@ -1,4 +1,12 @@
 
+-- 发短信模板 
+
+SET @id = (SELECT MAX(id) FROM eh_locale_templates) +1;
+INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) VALUES(@id := @id + 1,'sms.default.yzx','12','zh_CN','付费预约成功 独占资源','31934','1000000');
+INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) VALUES(@id := @id + 1,'sms.default.yzx','13','zh_CN','付费预约成功 非独占资源，不需要选择资源编号','32008','1000000');
+INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) VALUES(@id := @id + 1,'sms.default.yzx','14','zh_CN','付费预约成功 非独占资源，需要选择资源编号','32009','1000000');
+
+
 -- 修改之前审批发送消息模板，add by wh, 20161018
 UPDATE `eh_locale_templates` SET TEXT = '${creatorName}提交了请假申请，请假时间：${time}，请及时进行处理。' WHERE scope = 'approval.notification' AND CODE = 11;
 UPDATE `eh_locale_templates` SET TEXT = '${creatorName}提交了请假申请，请假时间：${time}，${approver}已同意，请及时进行处理。' WHERE scope = 'approval.notification' AND CODE = 12;
