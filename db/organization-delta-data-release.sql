5986121d6b2c0ab46436066cf532b4c372499ff7
yanshaofan <sf.yan@zuolin.com>
2016-11-11 15:19:21 +0800

    init sql

---
+++ db/organization-delta-data-release.sql
@@ -0,0 +1,4 @@
+
+
+-- by sfyan 获取电商平台的店铺信息接口
+INSERT INTO `eh_configurations` (`namespace_id`, `name`, `value`, `description`) VALUES (0, 'get.businesses.info.api', 'zl-ec/rest/openapi/shop/listByCondition', '获取店铺信息');
