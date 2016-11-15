d3fa9c57f844f355d42ab3071e6c289e0a4ac372
xq.tian <xq.tian@zuolin.com>
2016-11-15 16:45:50 +0800

    fix bug: energy statistic

--- db/3.11.2-delta-data-debug.sql
+++ db/3.11.2-delta-data-debug.sql
@@ -112,7 +112,7 @@ INSERT INTO `eh_energy_meter_default_settings` (`id`, `namespace_id`, `meter_typ
 --
 SET @launch_pad_item_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
 INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`)
-VALUES(@launch_pad_item_id := @launch_pad_item_id + 1, 999992, 0, 0, 0, '/home', 'Bizs', 'Energy', '能耗管理', 'cs://1/image/aW1hZ2UvTVRwaVl6ZGtPVFE0TURZd1pUZzRZekppTTJNMVl6QmlPVFprTWpWbFpHRXlNUQ', 1, 1, 13, '{"url":"http://beta.zuolin.com/energy-management/index.html?hideNavigationBar=1#/energy_management#sign_suffix"}', 0, 0, 1, 1, NULL, 0, NULL, NULL, NULL, 0, 'pm_admin');
+VALUES(@launch_pad_item_id := @launch_pad_item_id + 1, 999992, 0, 0, 0, '/home', 'Bizs', 'Energy', '能耗管理', 'cs://1/image/aW1hZ2UvTVRvM1pHVTNPVE16Wm1NMVlURTFabUpsTXpoaE9ERmpOamhqTmpneVpHVXlZZw', 1, 1, 13, '{"url":"http://beta.zuolin.com/energy-management/index.html?hideNavigationBar=1#/energy_management#sign_suffix"}', 0, 0, 1, 1, NULL, 0, NULL, NULL, NULL, 0, 'pm_admin');
 
 --
 -- 字符串
