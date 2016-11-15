a87c28fff9a7fb1ee3aa4ba8e182926014b08a12
sunwen <wen.sun@zuolin.com>
2016-11-15 14:19:36 +0800

    organization

--- ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
@@ -8254,8 +8254,8 @@ System.out.println();
 				CrossShardListingLocator locator = new CrossShardListingLocator();
 				locator.setAnchor(cmd.getPageAnchor());
 				List<OrganizationMember> organizationMembers = organizationProvider.listOrganizationPersonnels(cmd.getKeywords(),
-						orgCommoand, null, VisibleFlag.ALL, locator, 10000);
-				dto.setMembers(organizationMembers.stream().map(m -> ConvertHelper.convert(r, OrganizationMemberDTO.class))
+						orgCommoand, null, VisibleFlag.SHOW, locator, 10000);
+				dto.setMembers(organizationMembers.stream().map(m -> ConvertHelper.convert(m, OrganizationMemberDTO.class))
 						.collect(Collectors.toList()));
 				dto.setParentName(organization.getName());
 				return dto;
