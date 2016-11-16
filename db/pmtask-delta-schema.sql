d7ec455c3aaea57c78db87a3c0d210cc3d6c9d37
sunwen <wen.sun@zuolin.com>
2016-11-16 16:12:04 +0800

    db/pmtask-delta-schema.sql

--- db/pmtask-delta-schema.sql
+++ db/pmtask-delta-schema.sql
@@ -1,3 +1,5 @@
 
 ALTER TABLE eh_pm_tasks ADD COLUMN `operator_star` TINYINT NOT NULL DEFAULT 0 COMMENT 'task star of operator';
 ALTER TABLE eh_pm_tasks ADD COLUMN `address_type` TINYINT COMMENT '1: family , 2:organization';
+ALTER TABLE eh_pm_tasks ADD COLUMN `address_org_id` BIGINT NOT NUll DEFAULT 0 COMMENT '1: family , 2:organization';
+
