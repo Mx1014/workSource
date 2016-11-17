bf40439d16250d731c33ea667e9a82ab7c76529f
tangtong <tong.tang@zuolin.com>
2016-11-17 21:57:53 +0800

    add delta data

--- db/jindie-1.0.0-delta-data-debug.sql
+++ db/jindie-1.0.0-delta-data-debug.sql
@@ -1,3 +1,4 @@
 -- 初始化数据, add by tt, 20161117
 INSERT INTO `eh_app_namespace_mappings` (`id`, `namespace_id`, `app_key`, `community_id`) VALUES (1, 1000000, '7757a75f-b79a-42fd-896e-107f4bfedd59', 240111044331048623);
 
+INSERT INTO `eh_apps` (`id`, `creator_uid`, `app_key`, `secret_key`, `name`, `description`, `status`, `create_time`, `update_uid`, `update_time`) VALUES (5002, 1, '7757a75f-b79a-42fd-896e-107f4bfedd59', 'nM9PpqGaV2Qe5QqmNSHfWEJyvJjyo0r0f1wJgRadN9zWqcIwdU08FZYjyRSpa2vKmC/Mblh535WMKLiG/Ymr2Q==', 'jin die', 'kingdee', 1, '2016-11-09 11:49:16', NULL, NULL);
