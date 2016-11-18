486736d93024da7bb5d12482dff1adcc5cace057
wuhan <han.wu@zuolin.com>
2016-11-18 17:22:30 +0800

    modify table column

--- db/org-member-1.1-delta-schema.sql
+++ db/org-member-1.1-delta-schema.sql
@@ -2,21 +2,22 @@
 -- 用户认证记录表:记录用户加入/退出企业的log
 --
 
-CREATE TABLE `eh_organization_user_logs` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'id of the record',
-  `organization_id` BIGINT(20) NOT NULL, 
-  `user_id` BIGINT(20) NOT NULL COMMENT 'organization member target id (type user)',
-  `contact_name` VARCHAR(64) DEFAULT NULL,
-  `contact_type` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '0: mobile, 1: email',
-  `contact_token` VARCHAR(128) DEFAULT NULL COMMENT 'phone number or email address',
-  `operation_type` TINYINT(4) DEFAULT '0' COMMENT '0-退出企业 1-加入企业',
-  `operate_time` DATETIME DEFAULT NULL , 
-  `operator_uid` BIGINT(20) NOT NULL,
+CREATE TABLE `eh_organization_member_logs` (
+  `id` BIGINT(20)  COMMENT 'id of the record',
   `namespace_id` INT(11) DEFAULT '0',
+  `organization_id` BIGINT(20) , 
+  `user_id` BIGINT(20) COMMENT 'organization member target id (type user)',
+  `contact_name` VARCHAR(64) DEFAULT NULL,
+  `contact_type` TINYINT(4) DEFAULT '0' COMMENT '0: mobile, 1: email',
+  `contact_token` VARCHAR(128) COMMENT 'phone number or email address',
+  `operation_type` TINYINT(4) DEFAULT '0' COMMENT '0-退出企业 1-加入企业',
+  `request_type` TINYINT(4) DEFAULT '0' COMMENT '0-管理员操作 1-用户操作',
+  `operate_time` DATETIME , 
+  `operator_uid` BIGINT(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB AUTO_INCREMENT=2119064 DEFAULT CHARSET=utf8mb4;
 
 -- 增加字段  
 ALTER TABLE `eh_organization_members` ADD COLUMN `executive_flag` TINYINT(4) DEFAULT '0' COMMENT '0-不是高管 1-是高管';
 ALTER TABLE `eh_organization_members` ADD COLUMN `position` VARCHAR(128) DEFAULT NULL COMMENT '职位';
-ALTER TABLE `eh_organization_members` ADD COLUMN `id_number` VARCHAR(20) DEFAULT NULL COMMENT 'ID CARD NUMBER';
+ALTER TABLE `eh_organization_members` ADD COLUMN `identity_number` VARCHAR(20) DEFAULT NULL COMMENT 'ID CARD NUMBER';
