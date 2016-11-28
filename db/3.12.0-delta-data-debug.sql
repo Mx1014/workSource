996dbe13311d96f2543cecaffad141cf6ff42798
ying.xiong <ying.xiong@zuolin.com>
2016-11-28 15:15:28 +0800

    merge sqls

--- db/3.12.0-delta-data-debug.sql
+++ db/3.12.0-delta-data-debug.sql
@@ -4,3 +4,10 @@ INSERT INTO `eh_configurations` (`namespace_id`, `name`, `value`, `description`)
 
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) 
 VALUES ('300', 'organization', '600001', 'zh_CN', '通用岗位已存在');
+
+-- merge from activityentry-delta-data-release.sql by xiongying20161128
+update eh_activities a set forum_id = (select forum_id from eh_forum_posts where id = a.post_id);
+update eh_activities a set creator_tag = (select creator_tag from eh_forum_posts where id = a.post_id);
+update eh_activities a set target_tag = (select target_tag from eh_forum_posts where id = a.post_id);
+update eh_activities a set visible_region_type = (select visible_region_type from eh_forum_posts where id = a.post_id);
+update eh_activities a set visible_region_id = (select visible_region_id from eh_forum_posts where id = a.post_id);
