c2cc516f20f000dda653a01d120252fb0fd8c2fb
wuhan <han.wu@zuolin.com>
2016-12-02 16:39:25 +0800

    add column

--- db/rental-2.3-delta-schema.sql
+++ db/rental-2.3-delta-schema.sql
@@ -4,6 +4,10 @@
 ALTER TABLE `eh_rentalv2_resource_numbers` ADD COLUMN `number_group` INTEGER COMMENT '同一个groupid的两个编号资源被认为是一组资源';
 ALTER TABLE `eh_rentalv2_resource_numbers` ADD COLUMN `group_lock_flag` TINYINT COMMENT '一个资源被预约是否锁整个group,0-否,1-是';
 
+
+ALTER TABLE `eh_rentalv2_cells` ADD COLUMN `number_group` INTEGER COMMENT '同一个groupid的两个编号资源被认为是一组资源';
+ALTER TABLE `eh_rentalv2_cells` ADD COLUMN `group_lock_flag` TINYINT COMMENT '一个资源被预约是否锁整个group,0-否,1-是';
+
 ALTER TABLE `eh_rentalv2_orders` ADD COLUMN `reminder_time` DATETIME     COMMENT '消息提醒时间';
 
 ALTER TABLE `eh_rentalv2_resource_orders` ADD COLUMN `status` TINYINT  DEFAULT 0 COMMENT '状态 0-普通预定订单 1-不显示给用户的';
