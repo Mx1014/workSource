7dd991dca49eca0210d6bb3b89f6388b84cf845f
wuhan <han.wu@zuolin.com>
2016-12-02 15:08:23 +0800

    add column

--- db/rental-2.3-delta-schema.sql
+++ db/rental-2.3-delta-schema.sql
@@ -2,6 +2,7 @@
 -- added by wh 2016-11-24 增加资源编号关联性
  
 ALTER TABLE `eh_rentalv2_resource_numbers` ADD COLUMN `number_group` INTEGER COMMENT '同一个groupid的两个编号资源被认为是一组资源';
+ALTER TABLE `eh_rentalv2_resource_numbers` ADD COLUMN `group_lock_flag` INTEGER COMMENT '一个资源被预约是否锁整个group,0-否,1-是';
 
 ALTER TABLE `eh_rentalv2_orders` ADD COLUMN `reminder_time` DATETIME DEFAULT NULL COMMENT '消息提醒时间';
 
