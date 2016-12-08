35a5991800c77e0ca078958a72bb655e3f0974d7
wuhan <han.wu@zuolin.com>
2016-12-08 18:33:40 +0800

    add provider

--- ehnextgen-api/src/main/java/com/everhomes/techpark/servicehotline/ServiceConfigurationsProvider.java
+++ ehnextgen-api/src/main/java/com/everhomes/techpark/servicehotline/ServiceConfigurationsProvider.java
@@ -1,5 +1,22 @@
 package com.everhomes.techpark.servicehotline;
 
+import java.util.List;
+
+import com.everhomes.listing.ListingLocator;
+import com.everhomes.listing.ListingQueryBuilderCallback;
+
 public interface ServiceConfigurationsProvider {
+
+	Long createServiceConfiguration(ServiceConfiguration obj);
+
+	void updateServiceConfiguration(ServiceConfiguration obj);
+
+	void deleteServiceConfiguration(ServiceConfiguration obj);
+
+	ServiceConfiguration getServiceConfigurationById(Long id);
+
+	List<ServiceConfiguration> queryServiceConfigurations(
+			ListingLocator locator, int count,
+			ListingQueryBuilderCallback queryBuilderCallback);
 
 }
