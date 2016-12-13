1c380a5ac36be46de99294da159d779c93ba9226
tangtong <tong.tang@zuolin.com>
2016-12-13 10:08:22 +0800

    fix schema

--- ehnextgen-api/src/main/java/com/everhomes/openapi/TechparkSyncdataLog.java
+++ ehnextgen-api/src/main/java/com/everhomes/openapi/TechparkSyncdataLog.java
@@ -1,10 +1,10 @@
 // @formatter:off
 package com.everhomes.openapi;
 
-import com.everhomes.server.schema.tables.pojos.EhTechparkSyncdataLog;
+import com.everhomes.server.schema.tables.pojos.EhTechparkSyncdataLogs;
 import com.everhomes.util.StringHelper;
 
-public class TechparkSyncdataLog extends EhTechparkSyncdataLog {
+public class TechparkSyncdataLog extends EhTechparkSyncdataLogs {
 
 	@Override
 	public String toString() {
