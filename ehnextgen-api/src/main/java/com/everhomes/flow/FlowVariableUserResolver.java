1934e3892be3579e323d73fc03ce5a36cad2b3aa
janson <gandancing@gmail.com>
2016-12-14 19:54:59 +0800

    resolve

--- ehnextgen-api/src/main/java/com/everhomes/flow/FlowVariableUserResolver.java
+++ ehnextgen-api/src/main/java/com/everhomes/flow/FlowVariableUserResolver.java
@@ -1,5 +1,12 @@
 package com.everhomes.flow;
 
 public interface FlowVariableUserResolver {
+	public static final String APPLIER = "flow-variable-applier";
+	public static final String PREFIX_NODE_PROCESSOR = "flow-variable-prefix-node-processor";
+	public static final String CURRENT_NODE_PROCESSOR = "flow-variable-current-node-processor";
+	public static final String NEXT_NODE_PROCESSOR = "flow-variable-next-node-processor";
+	public static final String N_NODE_PROCESSOR = "flow-variable-n-node-processor";
+	public static final String SUPERVISOR = "flow-variable-supervisor";
+	
 	String variableUserResolve(FlowCaseState ctx, FlowUserSelection userSelection);
 }
