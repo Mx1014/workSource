f58c6157d2971cde304d67d7349be3b482b5a748
sunwen <wen.sun@zuolin.com>
2016-12-15 14:17:39 +0800

    parking schema

--- db/3.12.2-delta-schema.sql
+++ db/3.12.2-delta-schema.sql
@@ -598,6 +598,49 @@ ALTER TABLE `eh_rentalv2_orders` ADD COLUMN `reminder_time` DATETIME     COMMENT
 
 ALTER TABLE `eh_rentalv2_resource_orders` ADD COLUMN `status` TINYINT  DEFAULT 0 COMMENT '状态 0-普通预定订单 1-不显示给用户的';
 
+-- merge from parking-clearance-1.0-delta-schema.sql by lqs 20161215
+--
+-- 车辆放行申请人员与处理人员
+--
+-- DROP TABLE IF EXISTS `eh_parking_clearance_operators`;
+CREATE TABLE `eh_parking_clearance_operators` (
+  `id`              BIGINT  NOT NULL,
+  `namespace_id`    INTEGER NOT NULL DEFAULT 0,
+  `organization_id` BIGINT,
+  `community_id`    BIGINT,
+  `parking_lot_id`  BIGINT COMMENT 'eh_parking_lots id',
+  `operator_type`   VARCHAR(32) COMMENT 'applicant, handler',
+  `operator_id`     BIGINT,
+  `status`          TINYINT COMMENT '0: inactive, 1: waitingForApproval, 2: active',
+  `creator_uid`     BIGINT,
+  `create_time`     DATETIME,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 车辆放行记录
+--
+-- DROP TABLE IF EXISTS `eh_parking_clearance_logs`;
+CREATE TABLE `eh_parking_clearance_logs` (
+  `id`             BIGINT  NOT NULL,
+  `namespace_id`   INTEGER NOT NULL DEFAULT 0,
+  `community_id`   BIGINT,
+  `parking_lot_id` BIGINT COMMENT 'eh_parking_lots id',
+  `applicant_id`   BIGINT COMMENT 'applicant id',
+  `operator_id`    BIGINT COMMENT 'operator id',
+  `plate_number`   VARCHAR(32) COMMENT 'plate number',
+  `apply_time`     DATETIME COMMENT 'apply time',
+  `clearance_time` DATETIME COMMENT 'The time the vehicle passed',
+  `remarks`        VARCHAR(1024) COMMENT 'remarks',
+  `status`         TINYINT COMMENT '0: inactive, 1: processing, 2: completed, 3: cancelled, 4: pending',
+  `creator_uid`    BIGINT,
+  `create_time`    DATETIME,
+  `update_uid`     BIGINT,
+  `update_time`    DATETIME,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- 停车充值5.0  merge from parking-delta-schema.sql by sw 20161215
 ALTER TABLE eh_parking_lots ADD COLUMN `recharge_month_count` INT NOT NUll DEFAULT 0 COMMENT 'organization of address';
