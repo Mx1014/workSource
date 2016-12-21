c8eb464b1ebc00d209a23e1588bb167f56e74053
sunwen <wen.sun@zuolin.com>
2016-12-21 11:41:48 +0800

    update pmtask

--- ehrest/src/main/java/com/everhomes/rest/flow/FlowConstants.java
+++ ehrest/src/main/java/com/everhomes/rest/flow/FlowConstants.java
@@ -4,4 +4,7 @@ public interface FlowConstants {
     public static final String FLOW_STEP_NAME_PREFIX = "flow.stepname.";
     public static final Integer FLOW_CONFIG_VER = 0;
     public static final Integer FLOW_CONFIG_START = 1;
+    
+    // 物业报修模块id
+    public static final Long PM_TASK_MODULE = 20100L;
 }
