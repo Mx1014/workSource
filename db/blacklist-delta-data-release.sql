0c9a64ce297806986ad189c89ac9f11cf21b0106
yanshaofan <sf.yan@zuolin.com>
2016-12-22 17:35:12 +0800

    sql

--- db/blacklist-delta-data-release.sql
+++ db/blacklist-delta-data-release.sql
@@ -33,3 +33,20 @@ UPDATE `eh_locale_strings` SET `text` = '您权限不足' WHERE `scope` = 'general' A
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'blacklist', '600000', 'zh_CN', '您已被管理员禁止了此操作。');	
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'blacklist', '600020', 'zh_CN', '未查询到符合条件的对象，请查证。');	
 INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'blacklist', '600010', 'zh_CN', '黑名单已存在。');	
+
+-- 黑名单权限 
+insert into `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) values('30600','黑名单管理','30000',NULL,'react:/blacklist-management/black-list','0','2','/30000/30600','park','361','30600');
+INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
+VALUES (10090, 0, '黑名单管理 管理员', '黑名单管理 管理员权限', NULL);
+
+SET @web_menu_privilege_id = (SELECT MAX(id) FROM `eh_web_menu_privileges`);
+INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`)
+VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), 10090, 30600, '黑名单管理', 1, 1, '黑名单管理 管理员权限', 361);
+
+SET @acl_id = (SELECT MAX(id) FROM `eh_acls`);
+INSERT INTO `eh_acls` (`id`, `owner_type`, `grant_type`, `privilege_id`, `role_id`, `order_seq`, `creator_uid`, `create_time`)
+VALUES ((@acl_id := @acl_id + 1), 'EhOrganizations', 1, 10090, 1001, 0, 1, NOW());
+
+SET @eh_service_module_scopes = (SELECT MAX(id) FROM `eh_service_module_scopes`);
+INSERT INTO `eh_service_module_scopes` (`id`, `namespace_id`, `module_id`, `module_name`, `owner_type`, `owner_id`, `default_order`, `apply_policy`) 
+	VALUES ((@eh_service_module_scopes := @eh_service_module_scopes + 1), '0', '30600', '', 'EhNamespaces', '0', NULL, '2');
