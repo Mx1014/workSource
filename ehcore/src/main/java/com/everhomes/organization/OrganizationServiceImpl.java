8379dce07070ea5bbeecf6cbd6fca9db63659233
wuhan <han.wu@zuolin.com>
2016-12-29 16:40:40 +0800

    fix bug : send message

--- ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
@@ -35,6 +35,7 @@ import com.everhomes.listing.ListingLocator;
 import com.everhomes.listing.ListingQueryBuilderCallback;
 import com.everhomes.locale.LocaleStringService;
 import com.everhomes.locale.LocaleTemplateService;
+import com.everhomes.mail.MailHandler;
 import com.everhomes.messaging.MessagingService;
 import com.everhomes.module.ServiceModuleAssignment;
 import com.everhomes.module.ServiceModuleProvider;
@@ -86,6 +87,7 @@ import com.everhomes.rest.ui.user.SceneTokenDTO;
 import com.everhomes.rest.user.*;
 import com.everhomes.rest.user.admin.ImportDataResponse;
 import com.everhomes.rest.visibility.VisibleRegionType;
+import com.everhomes.rest.yellowPage.ServiceAllianceRequestNotificationTemplateCode;
 import com.everhomes.search.OrganizationSearcher;
 import com.everhomes.search.PostAdminQueryFilter;
 import com.everhomes.search.PostSearcher;
@@ -98,6 +100,7 @@ import com.everhomes.user.admin.SystemUserPrivilegeMgr;
 import com.everhomes.util.*;
 import com.everhomes.util.excel.RowResult;
 import com.everhomes.util.excel.handler.PropMrgOwnerHandler;
+
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -120,6 +123,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.*;
 import java.sql.Timestamp;
 import java.text.ParseException;
@@ -8424,17 +8428,22 @@ System.out.println();
 		String mailText = localeTemplateService.getLocaleTemplateString(VerifyMailTemplateCode.SCOPE, VerifyMailTemplateCode.TEXT_CODE, locale, map, "");
 		String mailSubject =this.localeStringService.getLocalizedString(VerifyMailTemplateCode.SCOPE,
 				VerifyMailTemplateCode.SUBJECT_CODE, RentalNotificationTemplateCode.locale, "加入企业验证邮件"); 
-		Email email = new EmailBuilder()
-	    .from(appName,account)
-	    .to(UserContext.current().getUser().getNickName(), cmd.getEmail()) 
-	    .subject(mailSubject)
-	    .text(mailText)
-	    .build();
+//		Email email = new EmailBuilder()
+//	    .from(appName,account)
+//	    .to(UserContext.current().getUser().getNickName(), cmd.getEmail()) 
+//	    .subject(mailSubject)
+//	    .text(mailText)
+//	    .build();
 		try{
 			String address = configProvider.getValue("mail.smtp.address", "smtp.mxhichina.com");
 			String passwod = configProvider.getValue("mail.smtp.passwod", "abc123!@#");
 			int port = configProvider.getIntValue("mail.smtp.port", 25);
-			new Mailer(address, port , account , passwod).sendMail(email);
+//			new Mailer(address, port , account , passwod).sendMail(email);
+			//另一种发送方式
+			String handlerName = MailHandler.MAIL_RESOLVER_PREFIX + MailHandler.HANDLER_JSMTP;
+	        MailHandler handler = PlatformContext.getComponent(handlerName);
+	          
+	        handler.sendMail(UserContext.getCurrentNamespaceId(), account,cmd.getEmail(), mailSubject, mailText);
 		}catch (Exception e){
 			LOGGER.error(e.getMessage());
 			// LOGGER.error(e.getStackTrace());
