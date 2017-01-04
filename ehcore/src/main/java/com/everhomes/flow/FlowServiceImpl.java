54283aefe349efb0c0c280fdf70bb7c47c890da3
janson <gandancing@gmail.com>
2017-01-04 19:09:17 +0800

    fix flow next error

--- ehcore/src/main/java/com/everhomes/flow/FlowServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/flow/FlowServiceImpl.java
@@ -2311,7 +2311,26 @@ public class FlowServiceImpl implements FlowService {
 		if(cmd.getFlowUserType() == null) {
 			cmd.setFlowUserType(FlowUserType.PROCESSOR.getCode());
 		}
-		List<FlowUserSelection> seles = flowUserSelectionProvider.findSelectionByBelong(flowNode.getId(), FlowEntityType.FLOW_NODE.getCode(), cmd.getFlowUserType());
+		FlowNode nextNode = null;
+		List<FlowNode> nodes = flowNodeProvider.findFlowNodesByFlowId(flowNode.getFlowMainId(), flowNode.getFlowVersion());
+		Map<Long, FlowNode> nodeMap = new HashMap<Long, FlowNode>();
+		for(int i = 0; i < nodes.size(); i++) {
+			FlowNode node = nodes.get(i);
+			if(flowNode.getId().equals(node.getId())) {
+				if((i+1) < nodes.size()) {
+					nextNode = nodes.get(i+1);
+				}
+			}
+			nodeMap.put(node.getId(), node);
+		}
+		if(btn.getFlowNodeId() != null && nodeMap.containsKey(btn.getFlowNodeId())) {
+			nextNode = nodeMap.get(btn.getFlowNodeId());
+		}
+		if(nextNode == null) {
+			throw RuntimeErrorException.errorWith(FlowServiceErrorCode.SCOPE, FlowServiceErrorCode.ERROR_FLOW_NODE_NOEXISTS, "next node not exists");
+		}
+		
+		List<FlowUserSelection> seles = flowUserSelectionProvider.findSelectionByBelong(nextNode.getId(), FlowEntityType.FLOW_NODE.getCode(), cmd.getFlowUserType());
 		if(seles != null) {
 			seles.forEach((s) -> {
 				FlowUserSelectionDTO dto = ConvertHelper.convert(s, FlowUserSelectionDTO.class);
