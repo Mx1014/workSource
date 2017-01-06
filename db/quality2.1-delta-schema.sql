01683fa4379513197ad53360e6cfde9ed35fabf5
ying.xiong <ying.xiong@zuolin.com>
2017-01-06 17:48:38 +0800

    modify schema

--- db/quality2.1-delta-schema.sql
+++ db/quality2.1-delta-schema.sql
@@ -35,7 +35,9 @@ CREATE TABLE `eh_quality_inspection_task_templates` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-alter table `eh_quality_inspection_tasks` drop column `create_uid`;
+ALTER TABLE `eh_quality_inspection_tasks` DROP COLUMN `create_uid`;
+ALTER TABLE `eh_quality_inspection_standard_group_map` ADD COLUMN `position_id` BIGINT NOT NULL DEFAULT '0' COMMENT 'refernece to the id of eh_organizations';
+
 
 -- 3.12.4
 ALTER TABLE `eh_resource_categories` ADD COLUMN `type` tinyint(4)  DEFAULT '1' COMMENT '1:分类, 2：子项目';
