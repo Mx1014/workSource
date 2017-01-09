146decbc109e0b18f8ed8cd5bba8acd6f79aaba7
ying.xiong <ying.xiong@zuolin.com>
2017-01-09 17:44:54 +0800

    add schema:

---
+++ db/equipment2.1-delta-schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `eh_equipment_inspection_categories`;
+CREATE TABLE `eh_equipment_inspection_categories` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the category, community, etc',
+  `owner_id` BIGINT NOT NULL DEFAULT 0,
+  `parent_id` BIGINT NOT NULL DEFAULT 0,
+  `name` VARCHAR(64) NOT NULL,
+  `path` VARCHAR(128),
+  `default_order` INTEGER,
+  `status` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: waiting for confirmation, 2: active',
+  `creator_uid` BIGINT NOT NULL DEFAULT 0 COMMENT 'record creator user id',
+  `create_time` DATETIME,
+  `deletor_uid` BIGINT NOT NULL DEFAULT 0 COMMENT 'record deleter user id',
+  `delete_time` DATETIME,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `eh_equipment_inspection_equipments` ADD COLUMN `inspection_category_id` BIGINT;
+ALTER TABLE `eh_equipment_inspection_standards` ADD COLUMN `inspection_category_id` BIGINT;
+ALTER TABLE `eh_equipment_inspection_tasks` ADD COLUMN `inspection_category_id` BIGINT;
