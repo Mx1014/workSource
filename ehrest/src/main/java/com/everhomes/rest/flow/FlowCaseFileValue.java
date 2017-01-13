3759eaea174b55def3fd70566a37636f8801a84b
wuhan <han.wu@zuolin.com>
2017-01-13 16:35:48 +0800

    fix bug

--- ehrest/src/main/java/com/everhomes/rest/flow/FlowCaseFileValue.java
+++ ehrest/src/main/java/com/everhomes/rest/flow/FlowCaseFileValue.java
@@ -2,9 +2,11 @@ package com.everhomes.rest.flow;
 
 import java.util.List;
 
+import com.everhomes.discover.ItemType;
 import com.everhomes.util.StringHelper;
 
 public class FlowCaseFileValue {
+	@ItemType(FlowCaseFileDTO.class)
 	private List<FlowCaseFileDTO> files;
 
 	public List<FlowCaseFileDTO> getFiles() {
