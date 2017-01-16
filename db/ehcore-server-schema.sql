5aa289590d01040e2a93bb9d8b3c901c17548373
LiangQishi <qishi.liang@zuolin.com>
2017-01-16 14:23:50 +0800

    merge some unrelease sql to backup for convenience of 4.1.2 branch

--- db/ehcore-server-schema.sql
+++ db/ehcore-server-schema.sql
@@ -8443,18 +8443,36 @@ CREATE TABLE `eh_user_behaviors`(
 --
 -- member of eh_users sharding group
 --
-DROP TABLE IF EXISTS `eh_user_blacklist`;
-CREATE TABLE `eh_user_blacklist` (
-  `id` BIGINT NOT NULL COMMENT 'id of the record',
-  `owner_uid` BIGINT NOT NULL COMMENT 'owner user id',
-  `target_type` VARCHAR(32),
-  `target_id` BIGINT,
-  `create_time` DATETIME COMMENT 'remove-deletion policy, user directly managed data',
+-- DROP TABLE IF EXISTS `eh_user_blacklist`;
+-- CREATE TABLE `eh_user_blacklist` (
+--   `id` BIGINT NOT NULL COMMENT 'id of the record',
+--   `owner_uid` BIGINT NOT NULL COMMENT 'owner user id',
+--   `target_type` VARCHAR(32),
+--   `target_id` BIGINT,
+--   `create_time` DATETIME COMMENT 'remove-deletion policy, user directly managed data',
 
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `u_eh_usr_blk_owner_target`(`owner_uid`, `target_type`, `target_id`),
-  KEY `i_eh_usr_blk_owner`(`owner_uid`),
-  KEY `i_eh_usr_blk_create_time`(`create_time`)
+--   PRIMARY KEY (`id`),
+--   UNIQUE KEY `u_eh_usr_blk_owner_target`(`owner_uid`, `target_type`, `target_id`),
+--   KEY `i_eh_usr_blk_owner`(`owner_uid`),
+--   KEY `i_eh_usr_blk_create_time`(`create_time`)
+-- ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `eh_user_blacklists`;
+CREATE TABLE `eh_user_blacklists` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT '0',
+  `scope_type` VARCHAR(64) DEFAULT NULL,
+  `scope_id` BIGINT DEFAULT NULL,
+  `owner_uid` BIGINT NOT NULL,
+  `contact_type` TINYINT NOT NULL DEFAULT '0',
+  `contact_token` varchar(128) DEFAULT '',
+  `contact_name` VARCHAR(64) DEFAULT NULL,
+  `gender` TINYINT DEFAULT '0' COMMENT '0: undisclosured, 1: male, 2: female',
+  `status` TINYINT NOT NULL DEFAULT '1' COMMENT '0: inactive, 1: confirming, 2: active',
+  `create_time` DATETIME DEFAULT NULL,
+  
+  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 
