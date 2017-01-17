c8542281980438a5ac3f719abdde8b99118256c5
xq.tian <xq.tian@zuolin.com>
2017-01-17 19:08:26 +0800

    privilege id

--- db/authorization-delta-data-release.sql
+++ db/authorization-delta-data-release.sql
@@ -1,42 +1,43 @@
 -- 
 -- 客户资料管理细分权限 add by xq.tian  2017/01/16
--- 
+--
+SELECT max(id) FROM `eh_service_module_privileges` INTO @service_module_pri_id;
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10060, '0', '添加客户', '添加客户', NULL);
+VALUES (10127, '0', '添加客户', '添加客户', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10060, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10127, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10061, '0', '批量导入及下载模版', '批量导入及下载模版', NULL);
+VALUES (10128, '0', '批量导入及下载模版', '批量导入及下载模版', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10061, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10128, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10062, '0', '导出EXCEL', '导出EXCEL', NULL);
+VALUES (10129, '0', '导出EXCEL', '导出EXCEL', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10062, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10129, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10063, '0', '查看统计信息', '查看统计信息', NULL);
+VALUES (10130, '0', '查看统计信息', '查看统计信息', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10063, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10130, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10064, '0', '修改客户资料', '修改客户资料', NULL);
+VALUES (10131, '0', '修改客户资料', '修改客户资料', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10064, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10131, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10065, '0', '查看客户资料', '查看客户资料', NULL);
+VALUES (10132, '0', '查看客户资料', '查看客户资料', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10065, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10132, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10066, '0', '管理客户资料', '管理客户资料', NULL);
+VALUES (10133, '0', '管理客户资料', '管理客户资料', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10066, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10133, NULL, '0', NOW());
 
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
-VALUES (10067, '0', '删除客户资料', '删除客户资料', NULL);
+VALUES (10134, '0', '删除客户资料', '删除客户资料', NULL);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
-VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10067, NULL, '0', NOW());
+VALUES ((@service_module_pri_id := @service_module_pri_id + 1), '37000', '0', 10134, NULL, '0', NOW());
