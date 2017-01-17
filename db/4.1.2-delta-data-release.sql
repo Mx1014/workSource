1e2e6caf05ace1b2d6eb5f8f61574cfcc6e5428d
sunwen <wen.sun@zuolin.com>
2017-01-17 15:13:59 +0800

    add init data

--- db/4.1.2-delta-data-release.sql
+++ db/4.1.2-delta-data-release.sql
@@ -167,6 +167,7 @@ INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`,
 INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`) VALUES ('pmtask.handler-999983-205250', 'flow', '', '0', NULL);
 
 INSERT INTO `eh_locale_strings` ( `scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10010', 'zh_CN', '分类不能为空！');
+INSERT INTO `eh_locale_strings` ( `scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10011', 'zh_CN', '任务已在处理中，不能取消！');
 
 update eh_launch_pad_items set action_data = '{"url":"zl://propertyrepair/create?type=user&taskCategoryId=205250&displayName=投诉建议"}' where action_type = 60 and item_label = '投诉建议' and namespace_id = 999983;
 update eh_launch_pad_items set action_data = '{\"url\":\"http://core.zuolin.com/property_service/index.html?hideNavigationBar=1&taskCategoryId=1#/my_service#sign_suffix\"}' where action_type = 14 and item_label = '报修' and namespace_id = 999983;
