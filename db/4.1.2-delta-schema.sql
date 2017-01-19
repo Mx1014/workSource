1576a6e937f7ed4099607b62d295e546a9325970
LiangQishi <qishi.liang@zuolin.com>
2017-01-19 12:04:27 +0800

    fix bug: eh_door_auth_logs already exists in 3.12.4

--- db/4.1.2-delta-schema.sql
+++ db/4.1.2-delta-schema.sql
@@ -114,18 +114,19 @@ CREATE TABLE `eh_business_promotions`(
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
+-- 在3.12.4上已经有此表 by lqs 20170119
 -- DROP TABLE IF EXISTS `eh_door_auth_logs`;
-CREATE TABLE `eh_door_auth_logs` (
-  `id` BIGINT NOT NULL COMMENT 'id of the record',
-  `door_id` BIGINT NOT NULL,
-  `user_id` BIGINT NOT NULL,
-  `is_auth` TINYINT NOT NULL DEFAULT '0',
-  `right_content` VARCHAR(1024) NOT NULL DEFAULT '0',
-  `create_time` datetime DEFAULT NULL,
-  `create_uid` BIGINT NOT NULL,
-  `discription` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- CREATE TABLE `eh_door_auth_logs` (
+--   `id` BIGINT NOT NULL COMMENT 'id of the record',
+--   `door_id` BIGINT NOT NULL,
+--   `user_id` BIGINT NOT NULL,
+--   `is_auth` TINYINT NOT NULL DEFAULT '0',
+--   `right_content` VARCHAR(1024) NOT NULL DEFAULT '0',
+--   `create_time` datetime DEFAULT NULL,
+--   `create_uid` BIGINT NOT NULL,
+--   `discription` text,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 增加机构类型
 ALTER TABLE `eh_organization_members` ADD COLUMN `group_type` varchar(64) DEFAULT NULL COMMENT 'ENTERPRISE, DEPARTMENT, GROUP, JOB_POSITION, JOB_LEVEL, MANAGER';
