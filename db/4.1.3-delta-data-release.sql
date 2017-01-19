328f9f1afe420ad8eec889054fcc6a18f457d747
LiangQishi <qishi.liang@zuolin.com>
2017-01-19 16:39:53 +0800

    merge sql from branch activity-2.0.0

---
+++ db/4.1.3-delta-data-release.sql
@@ -0,0 +1,7 @@
+-- merge from  activity-2.0.0-delta-data-release.sql by lqs 20170119
+-- 初始化数据，把官方活动的category_id全部配成1, add by tt, 20170116
+update eh_activities set category_id = 1 where official_flag = 1;
+update eh_forum_posts set category_id = 1 where official_flag = 1;
+
+
+
