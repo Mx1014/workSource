6ce093f767acff21901e2921afd528b7e018c23b
sunwen <wen.sun@zuolin.com>
2017-02-17 10:19:57 +0800

    pmtask schema

--- db/pmtask-delta-schema.sql
+++ db/pmtask-delta-schema.sql
@@ -1,1 +1,5 @@
 ALTER TABLE eh_pm_tasks ADD COLUMN `building_name` VARCHAR(128);
+
+ALTER TABLE eh_pm_task_targets ADD COLUMN `create_time` DATETIME;
+ALTER TABLE eh_pm_task_targets ADD COLUMN `creator_uid` BIGINT;
+
