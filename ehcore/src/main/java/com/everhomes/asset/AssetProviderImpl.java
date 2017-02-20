5bac0cbb9b56765bd0cb9f98d55c1d36fd1a3e74
ying.xiong <ying.xiong@zuolin.com>
2017-02-20 17:28:46 +0800

    add AssetProviderImpl

---
+++ ehcore/src/main/java/com/everhomes/asset/AssetProviderImpl.java
@@ -0,0 +1,10 @@
+package com.everhomes.asset;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/2/20.
+ */
+@Component
+public class AssetProviderImpl implements AssetProvider {
+}
