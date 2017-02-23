5fe02ebd7e3aed3eafa53df4b8d8e75534da6501
sunwen <wen.sun@zuolin.com>
2017-02-23 17:41:32 +0800

    parking

---
+++ ehcore/src/main/java/com/everhomes/parking/innospring/InnoSpringTempFee.java
@@ -0,0 +1,61 @@
+package com.everhomes.parking.innospring;
+
+/**
+ * Created by Administrator on 2017/2/23.
+ */
+public class InnoSpringTempFee {
+    private String delay_time;
+    private String elapsed_time;
+    private String entry_time;
+    private String order_no;
+    private String pay_time;
+    private String payable;
+
+    public String getDelay_time() {
+        return delay_time;
+    }
+
+    public void setDelay_time(String delay_time) {
+        this.delay_time = delay_time;
+    }
+
+    public String getElapsed_time() {
+        return elapsed_time;
+    }
+
+    public void setElapsed_time(String elapsed_time) {
+        this.elapsed_time = elapsed_time;
+    }
+
+    public String getEntry_time() {
+        return entry_time;
+    }
+
+    public void setEntry_time(String entry_time) {
+        this.entry_time = entry_time;
+    }
+
+    public String getOrder_no() {
+        return order_no;
+    }
+
+    public void setOrder_no(String order_no) {
+        this.order_no = order_no;
+    }
+
+    public String getPay_time() {
+        return pay_time;
+    }
+
+    public void setPay_time(String pay_time) {
+        this.pay_time = pay_time;
+    }
+
+    public String getPayable() {
+        return payable;
+    }
+
+    public void setPayable(String payable) {
+        this.payable = payable;
+    }
+}
