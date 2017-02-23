f13c81d60139c6652dbefef7fac095d692a4edfe
wuhan <han.wu@zuolin.com>
2017-02-23 16:46:14 +0800

    fix bug

--- db/4.3.0-delta-data-release.sql
+++ db/4.3.0-delta-data-release.sql
@@ -6,9 +6,11 @@ UPDATE eh_forum_posts SET official_flag = 1, activity_category_id = 1 WHERE id I
 SET @id := (SELECT MAX(id) FROM eh_locale_templates);
 INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) VALUES(( @id := @id +1),'expansion','3','zh_CN','园区入驻工作流详情内容','[{\"key\":\"发起人\",\"value\":\"${applyUserName}\",\"entityType\":\"list\"},{\"key\":\"联系电话\",\"value\":\"${contactPhone}\",\"entityType\":\"list\"},{\"key\":\"企业\",\"value\":\"${enterpriseName}\",\"entityType\":\"list\"},{\"key\":\"申请类型\",\"value\":\"${applyType}\",\"entityType\":\"list\"},{\"key\":\"申请来源\",\"value\":\"${sourceType}\",\"entityType\":\"list\"},{\"key\":\"备注\",\"value\":\"${description}\",\"entityType\":\"multi_line\"}]','0');
 
+SET @id := (SELECT MAX(id) FROM eh_locale_strings);
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES(( @id := @id +1),'expansion.applyType','4','zh_CN','创客申请');
 
 -- 问卷调查的错误提示， add by tt, 20170220
-select max(id) into @id from eh_locale_strings;
+SELECT MAX(id) INTO @id FROM eh_locale_strings;
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'questionnaire', '1', 'zh_CN', '问卷名称不能为空');
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'questionnaire', '2', 'zh_CN', '问卷名称不能超过50个字');
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'questionnaire', '3', 'zh_CN', '题目名称不能为空');
