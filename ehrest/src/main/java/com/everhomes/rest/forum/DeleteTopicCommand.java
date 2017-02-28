a70550bd502ae41a1d2381f6f0c24590bad3794e
yanshaofan <sf.yan@zuolin.com>
2017-02-28 16:12:37 +0800

    reset

--- ehrest/src/main/java/com/everhomes/rest/forum/DeleteTopicCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/forum/DeleteTopicCommand.java
@@ -5,46 +5,15 @@ import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
- * <li>currentOrgId: 当前所在机构</li>
- * <li>ownerType: 项目范围</li>
- * <li>ownerId: 项目id</li>
+ * <li>forumId: 论坛ID</li>
  * <li>topicId: 帖子ID</li>
  * </ul>
  */
 public class DeleteTopicCommand {
-
-    private Long currentOrgId;
-    private String ownerType;
-    private Long ownerId;
-
     private Long forumId;
     private Long topicId;
     
     public DeleteTopicCommand() {
-    }
-
-    public Long getCurrentOrgId() {
-        return currentOrgId;
-    }
-
-    public void setCurrentOrgId(Long currentOrgId) {
-        this.currentOrgId = currentOrgId;
-    }
-
-    public String getOwnerType() {
-        return ownerType;
-    }
-
-    public void setOwnerType(String ownerType) {
-        this.ownerType = ownerType;
-    }
-
-    public Long getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(Long ownerId) {
-        this.ownerId = ownerId;
     }
 
     public Long getForumId() {
