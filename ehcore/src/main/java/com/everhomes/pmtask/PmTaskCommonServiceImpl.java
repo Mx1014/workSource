c777f05a59a823408cd914b702a3fbbfd96cbc8c
sunwen <wen.sun@zuolin.com>
2017-02-28 19:04:11 +0800

    pmtask

--- ehcore/src/main/java/com/everhomes/pmtask/PmTaskCommonServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/pmtask/PmTaskCommonServiceImpl.java
@@ -38,7 +38,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionStatus;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
