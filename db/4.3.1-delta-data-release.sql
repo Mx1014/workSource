602e702749628ea16d4d613b27e9cc6839eb527a
sunwen <wen.sun@zuolin.com>
2017-03-01 11:26:02 +0800

    pmtask2.8 init data

--- db/4.3.1-delta-data-release.sql
+++ db/4.3.1-delta-data-release.sql
@@ -62,10 +62,6 @@ SET @acl_id = (SELECT MAX(id) FROM `eh_acls`);
 
 INSERT INTO `eh_acls` (`id`,`owner_type`,`grant_type`,`privilege_id`,`role_id`,`order_seq`,`creator_uid`,`create_time`, `role_type`)
 	SELECT (@acl_id := @acl_id + 1), 'EhOrganizations', 1, `id`, 1001, 0, 1, now(), 'EhAclRoles' FROM `eh_acl_privileges` WHERE id = 10138;
-	
-
-INSERT INTO `eh_locale_strings` ( `scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10012', 'zh_CN', 'û�д���Ȩ�ޣ�');
-INSERT INTO `eh_locale_strings` ( `scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10013', 'zh_CN', '�鲻�����û���Ϣ��');
 
 INSERT INTO `eh_locale_strings` ( `scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10012', 'zh_CN', '没有代发权限！');
 INSERT INTO `eh_locale_strings` ( `scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10013', 'zh_CN', '查不到该用户信息！');
