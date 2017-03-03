f8617d09074e81fd49109d62418ddf1e282e2745
wuhan <han.wu@zuolin.com>
2017-03-03 09:47:08 +0800

    fix bug

--- ehrest/src/main/java/com/everhomes/rest/rentalv2/admin/GetResourceListAdminResponse.java
+++ ehrest/src/main/java/com/everhomes/rest/rentalv2/admin/GetResourceListAdminResponse.java
@@ -11,13 +11,13 @@ import com.everhomes.util.StringHelper;
  * <ul>
  * 列出资源列表返回值(根据图标和园区)
  * <li>nextPageAnchor: 分页，下一页锚点</li>
- * <li>rentalSites: 资源列表list</li> 
+ * <li>rentalSites: 资源列表list{@link com.everhomes.rest.rentalv2.RentalSiteDTO}</li> 
  * </ul>
  */
 public class GetResourceListAdminResponse {
 	private Long nextPageAnchor;
 	
-	@ItemType(BuildingDTO.class)
+	@ItemType(RentalSiteDTO.class)
 	private List<RentalSiteDTO> rentalSites;
 	
 	@Override
