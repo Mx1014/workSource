b7b1962145f8bb5033d8a58240f78bcef39be27e
ying.xiong <ying.xiong@zuolin.com>
2017-03-13 18:37:50 +0800

    merge from sywy2.0

--- db/sywy2.0-delta-schema.sql
+++
@@ -1,9 +0,0 @@
--- 物业报修2.8 add by sw 20170306
-ALTER TABLE eh_pm_tasks ADD COLUMN `building_name` VARCHAR(128);
-ALTER TABLE eh_pm_tasks ADD COLUMN `organization_uid` BIGINT;
-
-ALTER TABLE eh_pm_task_targets ADD COLUMN `create_time` DATETIME;
-ALTER TABLE eh_pm_task_targets ADD COLUMN `creator_uid` BIGINT;
-
--- 修改dataType 长度 add by sw 20170306
-ALTER TABLE eh_web_menus MODIFY COLUMN data_type VARCHAR (256);
