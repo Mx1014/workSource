815c63981453c7885ff445dfae193800b1393f33
sunwen <wen.sun@zuolin.com>
2017-03-16 11:24:27 +0800

    rent schema

---
+++ db/rent-delta-schema.sql
@@ -0,0 +1,54 @@
+-- 物业报修2.8 add by sw 20170306
+ALTER TABLE eh_pm_tasks ADD COLUMN `building_name` VARCHAR(128);
+ALTER TABLE eh_pm_tasks ADD COLUMN `organization_uid` BIGINT;
+
+ALTER TABLE eh_pm_task_targets ADD COLUMN `create_time` DATETIME;
+ALTER TABLE eh_pm_task_targets ADD COLUMN `creator_uid` BIGINT;
+
+-- 修改dataType 长度 add by sw 20170306
+ALTER TABLE eh_web_menus MODIFY COLUMN data_type VARCHAR (256);
+
+
+-- 活动报名表添加活动报名信息等字段, add by tt, 20170227
+ALTER TABLE `eh_activities` ADD COLUMN `signup_end_time` DATETIME;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `phone` VARCHAR(32);
+ALTER TABLE `eh_activity_roster` ADD COLUMN `real_name` VARCHAR(128);
+ALTER TABLE `eh_activity_roster` ADD COLUMN `gender` TINYINT;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `community_name` VARCHAR(64);
+ALTER TABLE `eh_activity_roster` ADD COLUMN `organization_name` VARCHAR(128);
+ALTER TABLE `eh_activity_roster` ADD COLUMN `position` VARCHAR(64);
+ALTER TABLE `eh_activity_roster` ADD COLUMN `leader_flag` TINYINT;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `source_flag` TINYINT;
+
+ALTER TABLE `eh_activity_roster` DROP INDEX `u_eh_act_roster_user`;
+
+
+CREATE TABLE `eh_lease_issuers` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `community_id` BIGINT NOT NULL DEFAULT 0,
+  `issuer_contact` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'For rent',
+  `issuer_name` VARCHAR(128),
+  `enterprise_id` BIGINT COMMENT 'enterprise id',
+  
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE eh_lease_promotions ADD COLUMN `enter_time_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled';
+ALTER TABLE eh_lease_promotions ADD COLUMN `address_id` BIGINT NOT NULL DEFAULT 0;
+ALTER TABLE eh_lease_promotions ADD COLUMN `orientation` VARCHAR(128);
+ALTER TABLE eh_lease_promotions ADD COLUMN `rent_amount` DECIMAL(10,2);
+ALTER TABLE eh_lease_promotions ADD COLUMN `issuer_type` TINYINT NOT NULL DEFAULT 1 COMMENT '1: organization 2: normal_user';
+
+CREATE TABLE `eh_lease_configs` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `rent_amount_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled'
+  `issuing_lease_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled'
+  `issuer_manage_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled'
+  `park_indroduce_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled'
+  `renew_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled'
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
