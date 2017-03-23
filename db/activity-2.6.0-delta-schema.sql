06530c0776df93d3595a471f23d9c801b2e2992f
yanjun <jun.yan@zuolin.com>
2017-03-23 16:29:37 +0800

    modi all date flag

--- db/activity-2.6.0-delta-schema.sql
+++ db/activity-2.6.0-delta-schema.sql
@@ -1,2 +1,2 @@
 alter table `ehcore`.`eh_activities` 
-   add column `all_date_flag` tinyint(4) DEFAULT '0' NULL COMMENT 'whether it is an all date activity, 0 not, 1 yes' after `end_time`;
+   add column `all_day_flag` tinyint(4) DEFAULT '0' NULL COMMENT 'whether it is an all day activity, 0 not, 1 yes' after `end_time`;
