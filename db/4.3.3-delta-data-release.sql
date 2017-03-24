1cdd7afb1311118816e07638983c26148ee7febb
sunwen <wen.sun@zuolin.com>
2017-03-24 11:50:39 +0800

    add flow sms init data

---
+++ db/4.3.3-delta-data-release.sql
@@ -0,0 +1,84 @@
+-- 对接工作流短信模版 add by sw 20170324
+ INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 23, 'zh_CN', '新建月卡申请', '34956');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 24, 'zh_CN', '月卡申请驳回', '34957');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 25, 'zh_CN', '月卡申请审核通过', '34958');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 26, 'zh_CN', '月卡申请办理成功', '34959');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 35, 'zh_CN', '新建园区入驻', '39496');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 36, 'zh_CN', '园区入驻已受理', '39497');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 37, 'zh_CN', '园区入驻审核未通过', '39498');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 38, 'zh_CN', '园区入驻催办', '39499');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 39, 'zh_CN', '园区入驻办理成功', '39500');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 40, 'zh_CN', '新建资源预约', '39615');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 41, 'zh_CN', '资源预约督办', '39616');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 42, 'zh_CN', '资源预约已受理', '39617');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 43, 'zh_CN', '资源预约驳回', '39618');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 44, 'zh_CN', '资源预约催办', '39619');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 45, 'zh_CN', '资源预约办理成功', '39620');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 46, 'zh_CN', '物业报修督办受理', '39721');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 47, 'zh_CN', '物业报修待分配', '39722');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 48, 'zh_CN', '物业报修督办分配', '39723');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'sms.default.yzx', 49, 'zh_CN', '物业报修已完成', '39724');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40800', 23, 'zh_CN', '新建月卡申请', '34956');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40800', 24, 'zh_CN', '月卡申请驳回', '34957');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40800', 25, 'zh_CN', '月卡申请审核通过', '34958');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40800', 26, 'zh_CN', '月卡申请办理成功', '34959');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40100', 35, 'zh_CN', '新建园区入驻', '39496');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40100', 36, 'zh_CN', '园区入驻已受理', '39497');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40100', 37, 'zh_CN', '园区入驻审核未通过', '39498');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40100', 38, 'zh_CN', '园区入驻催办', '39499');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40100', 39, 'zh_CN', '园区入驻办理成功', '39500');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40400', 40, 'zh_CN', '新建资源预约', '39615');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40400', 41, 'zh_CN', '资源预约督办', '39616');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40400', 42, 'zh_CN', '资源预约已受理', '39617');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40400', 43, 'zh_CN', '资源预约驳回', '39618');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40400', 44, 'zh_CN', '资源预约催办', '39619');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:40400', 45, 'zh_CN', '资源预约办理成功', '39620');
+
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:20100', 46, 'zh_CN', '物业报修督办受理', '39721');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:20100', 47, 'zh_CN', '物业报修待分配', '39722');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:20100', 48, 'zh_CN', '物业报修督办分配', '39723');
+INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
+	VALUES (1000000, 'flow:20100', 49, 'zh_CN', '物业报修已完成', '39724');
