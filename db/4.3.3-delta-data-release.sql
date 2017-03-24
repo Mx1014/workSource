502238536c4455b6fd63d8de10457479281aaf7d
xq.tian <xq.tian@zuolin.com>
2017-03-24 14:18:16 +0800

    modify sql

--- db/4.3.3-delta-data-release.sql
+++ db/4.3.3-delta-data-release.sql
@@ -125,3 +125,15 @@ INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `des
 	VALUES (1000000, 'flow:20100', 48, 'zh_CN', '物业报修督办分配', '39723');
 INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `description`, `text`)
 	VALUES (1000000, 'flow:20100', 49, 'zh_CN', '物业报修已完成', '39724');
+
+
+-- oauth2client 1.0   add by xq.tian 2017/03/09
+--
+-- 门禁icon
+--
+SET @max_id = (SELECT max(id) FROM `eh_launch_pad_items`);
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`)
+VALUES ((@max_id := @max_id + 1), 999986, 0, 0, 0, '/home', 'Bizs', 'ACLINK', '门禁', 'cs://1/image/aW1hZ2UvTVRwaVl6ZGtPVFE0TURZd1pUZzRZekppTTJNMVl6QmlPVFprTWpWbFpHRXlNUQ', 1, 1, 13, '{"url":"https://core.zuolin.com/evh/oauth2cli/redirect/huanteng?serviceUrl=%2fzlapp%2fdist%2f%3fhideNavigationBar%3d1%23%2faccess-control%2flock-list-cy&hideNavigationBar=1#sign_suffix"}', 0, 0, 1, 1, NULL, 0, NULL, NULL, NULL, 0, 'default', 0, NULL, NULL);
+SET @max_id = (SELECT max(id) FROM `eh_launch_pad_items`);
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`)
+VALUES ((@max_id := @max_id + 1), 999986, 0, 0, 0, '/home', 'Bizs', 'ACLINK', '门禁', 'cs://1/image/aW1hZ2UvTVRwaVl6ZGtPVFE0TURZd1pUZzRZekppTTJNMVl6QmlPVFprTWpWbFpHRXlNUQ', 1, 1, 13, '{"url":"https://core.zuolin.com/evh/oauth2cli/redirect/huanteng?serviceUrl=%2fzlapp%2fdist%2f%3fhideNavigationBar%3d1%23%2faccess-control%2flock-list-cy&hideNavigationBar=1#sign_suffix"}', 0, 0, 1, 1, NULL, 0, NULL, NULL, NULL, 0, 'pm_admin', 0, NULL, NULL);
--- db/4.3.3-delta-schema.sql
+++ db/4.3.3-delta-schema.sql
@@ -2,7 +2,7 @@
 --
 -- oauth2 client AccessToken
 --
-DROP TABLE IF EXISTS `eh_oauth2_client_tokens`;
+-- DROP TABLE IF EXISTS `eh_oauth2_client_tokens`;
 CREATE TABLE `eh_oauth2_client_tokens` (
   `id` BIGINT NOT NULL,
   `token_string` VARCHAR(128) NOT NULL COMMENT 'token string issued to requestor',
@@ -15,11 +15,10 @@ CREATE TABLE `eh_oauth2_client_tokens` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHAR SET ='utf8mb4';
 
-
 --
 -- oauth2 servers
 --
-DROP TABLE IF EXISTS `eh_oauth2_servers`;
+-- DROP TABLE IF EXISTS `eh_oauth2_servers`;
 CREATE TABLE `eh_oauth2_servers` (
   `id` BIGINT NOT NULL,
   `vendor` VARCHAR(32) NOT NULL COMMENT 'OAuth2 server name',
@@ -32,7 +31,6 @@ CREATE TABLE `eh_oauth2_servers` (
   `scope` VARCHAR(256) NULL DEFAULT NULL COMMENT 'space-delimited scope tokens per RFC 6749',
   `authorize_url` VARCHAR(1024) NULL DEFAULT NULL COMMENT 'OAuth server provided authorize url',
   `token_url` VARCHAR(1024) NULL DEFAULT NULL COMMENT 'OAuth server provided get token url',
-#   `service_url` VARCHAR(1024) NULL DEFAULT NULL COMMENT 'OAuth server provided authorize url',
   `create_time` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHAR SET ='utf8mb4';
