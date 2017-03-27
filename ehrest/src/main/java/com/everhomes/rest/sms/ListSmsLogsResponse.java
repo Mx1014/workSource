96bf03ef646353d0955c43be297f912144d81f56
sunwen <wen.sun@zuolin.com>
2017-03-27 14:52:36 +0800

    add sms log

---
+++ ehrest/src/main/java/com/everhomes/rest/sms/ListSmsLogsResponse.java
@@ -0,0 +1,27 @@
+package com.everhomes.rest.sms;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+public class ListSmsLogsResponse {
+    private Long nextPageAnchor;
+    private List<SmsLogDTO> logs;
+
+    public Long getNextPageAnchor() {
+        return nextPageAnchor;
+    }
+
+    public void setNextPageAnchor(Long nextPageAnchor) {
+        this.nextPageAnchor = nextPageAnchor;
+    }
+
+    public List<SmsLogDTO> getLogs() {
+        return logs;
+    }
+
+    public void setLogs(List<SmsLogDTO> logs) {
+        this.logs = logs;
+    }
+}
