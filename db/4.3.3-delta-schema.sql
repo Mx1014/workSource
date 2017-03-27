44b452b2167bc6cea7222f5b213471fe80518a75
sunwen <wen.sun@zuolin.com>
2017-03-27 12:34:21 +0800

    add sms log schema

--- db/4.3.3-delta-schema.sql
+++ db/4.3.3-delta-schema.sql
@@ -52,7 +52,7 @@ CREATE TABLE `eh_sms_logs`(
   `locale` VARCHAR(16),
   `mobile` VARCHAR(128),
   `text` text,
-  `templateId` BIGINT,
+  `template_id` BIGINT,
   `variables` VARCHAR(512),
   `result` text,
   `create_time` DATETIME,
