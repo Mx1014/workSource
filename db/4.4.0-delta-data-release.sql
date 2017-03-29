f70a45905ff18311024c182e3ff2aea6ba7b3bec
sunwen <wen.sun@zuolin.com>
2017-03-29 15:10:04 +0800

    add usermanage init data

--- db/4.4.0-delta-data-release.sql
+++ db/4.4.0-delta-data-release.sql
@@ -10,3 +10,9 @@ SET @acl_id = (SELECT MAX(id) FROM `eh_acls`);
 INSERT INTO `eh_acls` (`id`, `owner_type`, `owner_id`, `grant_type`, `privilege_id`, `role_id`, `order_seq`, `creator_uid`, `create_time`)
 	VALUES ((@acl_id := @acl_id + 1), 'EhOrganizations', NULL, '1', '10155', '1001', '0', '1', UTC_TIMESTAMP());
 
+-- 用户管理 add by sw 20170329
+delete from eh_web_menus where id = 34000;
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) 
+	VALUES ('34000', '用户管理', '30000', NULL, 'user--user_management/1', '0', '2', '/30000/34000', 'park', '340', '34000');
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) 
+	VALUES ('34100', '用户管理', '30000', NULL, 'user--user_management/0', '0', '2', '/30000/34100', 'park', '340', '34000');
