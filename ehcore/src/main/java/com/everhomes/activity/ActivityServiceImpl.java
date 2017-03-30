1c1f910eaf27fc2abcedf26a79bc1dc2da088ce9
yanshaofan <sf.yan@zuolin.com>
2017-03-30 19:29:15 +0800

    video callback

--- ehcore/src/main/java/com/everhomes/activity/ActivityServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/activity/ActivityServiceImpl.java
@@ -3863,7 +3863,7 @@ public class ActivityServiceImpl implements ActivityService {
 			}else{
 				videoState = VideoState.LIVE;
 			}
-		}else if(cmd.getModule().equals("ﬁle") && !StringUtils.isEmpty(cmd.getFrom()) && cmd.getFrom().equals("record")){
+		}else if(cmd.getModule().equals("file") && !StringUtils.isEmpty(cmd.getFrom()) && cmd.getFrom().equals("record")){
 			if(cmd.getState() == -1){
 				videoState = VideoState.EXCEPTION;
 			}else if(cmd.getState() == 1){
