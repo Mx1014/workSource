b3561460c59f11337ccaab2a72d9c17a4fab092f
yanjun <jun.yan@zuolin.com>
2017-04-05 16:36:28 +0800

    merge activity-2.6.0

---
+++ db/4.4.1-delta-schema.sql
@@ -0,0 +1,3 @@
+-- 2017-04-05 16:32 add by yanjun
+alter table `ehcore`.`eh_activities` 
+   add column `all_day_flag` tinyint(4) DEFAULT '0' NULL COMMENT 'whether it is an all day activity, 0 not, 1 yes' after `end_time`;
