c00be9e08609ed35154f6754d3ebb661ad00e0f4
sunwen <wen.sun@zuolin.com>
2017-04-05 16:43:47 +0800

    add huarun rent init data

--- db/4.4.1-delta-data-release.sql
+++ db/4.4.1-delta-data-release.sql
@@ -1,2 +1,5 @@
 ﻿-- 2017-03-31 19:17 add by yanjun
 insert into `eh_locale_strings` (`scope`, `code`, `locale`, `text`) values('activity','11','zh_CN','活动时间：');
+
+-- 华润招租管理 add by sw 20170405
+UPDATE eh_launch_pad_items SET action_type = 28, action_data = '' where item_label = '看房' and namespace_id = 999985;
