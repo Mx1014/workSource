c99de7aadb1fd0eca6fe42b95c1dd3c940a9c167
ying.xiong <ying.xiong@zuolin.com>
2017-04-06 10:29:04 +0800

    add picture flag in equipments

--- db/4.4.1-delta-schema.sql
+++ db/4.4.1-delta-schema.sql
@@ -1,3 +1,7 @@
 -- 2017-04-05 16:32 add by yanjun
 alter table `eh_activities` 
    add column `all_day_flag` tinyint(4) DEFAULT '0' NULL COMMENT 'whether it is an all day activity, 0 not, 1 yes' after `end_time`;
+   
+   
+-- 设备表增加字段是否需要拍照 add by xiongying20170406
+ALTER TABLE `eh_activities` ADD COLUMN `picture_flag` tinyint(4) DEFAULT '1' NULL COMMENT 'whether need to take a picture while report equipment task, 0 not, 1 yes'
