ca3f964f7bacf37647329ff66e48310e6f4c9d90
sunwen <wen.sun@zuolin.com>
2017-04-13 19:02:15 +0800

    add aitejia init data

--- db/4.4.2-delta-data-release.sql
+++ db/4.4.2-delta-data-release.sql
@@ -1,3 +1,1 @@
--- 更新爱特家 俱乐部icon add by sw 20170413
-UPDATE eh_launch_pad_items set icon_uri = 'cs://1/image/aW1hZ2UvTVRvM04yVTRObU01TkRZd1pHUTNNMlV6WXpFek1tSmlZelV3WlRjMVl6VmpOdw' where item_label = '俱乐部' and namespace_id = 999988;
 
