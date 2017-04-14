8902b49dc7cb403b9fba737f8be66e9b08676a4d
yanjun <jun.yan@zuolin.com>
2017-04-14 17:43:07 +0800

    scope to 3

--- db/activity-bug-1.0-delta-data-release.sql
+++ db/activity-bug-1.0-delta-data-release.sql
@@ -1,5 +1,3 @@
 -- 更改华润的查询方式为当前小区或者是管理公司，具体实现是scope使用3  add by yanjun 20170114
 
-UPDATE eh_launch_pad_items SET action_data = '{"categoryId":1,"publishPrivilege":1,"livePrivilege":2,"listStyle":2,"scope":3,"style":4}' WHERE namespace_id = 999985 AND action_type = 61 AND scene_type = 'park_tourist' AND default_order = 1;
-
-UPDATE eh_launch_pad_items SET action_data = '{"categoryId":1,"publishPrivilege":1,"livePrivilege":2,"listStyle":2,"scope":3,"style":4,"title": "白领活动"}' WHERE namespace_id = 999985 AND action_type = 61 AND scene_type = 'park_tourist' AND default_order = 0;
+UPDATE eh_launch_pad_items SET action_data = REPLACE(action_data, '"scope":2', '"scope":3') WHERE namespace_id = 999985 AND action_type = 61 AND scene_type = 'park_tourist' ;
