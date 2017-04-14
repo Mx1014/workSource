fe79c115521121ecf16df75aa8d3671c6fe88a74
janson <gandancing@gmail.com>
2017-04-14 13:47:06 +0800

    update aclink

--- db/4.4.2-delta-data-release.sql
+++ db/4.4.2-delta-data-release.sql
@@ -1,1 +1,4 @@
 
+-- update 北环门禁 mac by janson
+update `eh_launch_pad_items` set `action_data`='{\"hardwareId\":\"EE:F1:50:7C:60:D5\"}' where id=1112290;
+update `eh_launch_pad_items` set `action_data`='{\"hardwareId\":\"EE:F1:50:7C:60:D5\"}' where id=1112291;
