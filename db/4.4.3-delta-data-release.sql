836690efa92389d332fbf06c0cd91b75e8e196ef
ying.xiong <ying.xiong@zuolin.com>
2017-04-17 17:50:19 +0800

    merge data

---
+++ db/4.4.3-delta-data-release.sql
@@ -0,0 +1,4 @@
+-- merge from mergeAsset by xiongying20170417
+INSERT INTO `eh_asset_vendor` (`id`, `owner_type`, `owner_id`, `name`, `vendor_name`, `status`, `namespace_id`) VALUES ('1', 'community', '240111044331055940', '科兴物业缴费', 'EBEI', '2', '999983');
+INSERT INTO `eh_asset_vendor` (`id`, `owner_type`, `owner_id`, `name`, `vendor_name`, `status`, `namespace_id`) VALUES ('2', 'community', '240111044331055035', '华润物业缴费', 'ZUOLIN', '2', '999985');
+INSERT INTO `eh_asset_vendor` (`id`, `owner_type`, `owner_id`, `name`, `vendor_name`, `status`, `namespace_id`) VALUES ('3', 'community', '240111044331055036', '华润物业缴费', 'ZUOLIN', '2', '999985');
--- db/mergeAsset-delta-data-release.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `eh_asset_vendor` (`id`, `owner_type`, `owner_id`, `name`, `vendor_name`, `status`, `namespace_id`) VALUES ('1', 'community', '240111044331055940', '科兴物业缴费', 'EBEI', '2', '999983');
