5a0cef39a179b49c5741c2d86642d70fb663eed5
yanshaofan <sf.yan@zuolin.com>
2017-04-21 16:42:17 +0800

    table sql

--- db/4.4.3-delta-schema.sql
+++ db/4.4.3-delta-schema.sql
@@ -18,3 +18,12 @@ ALTER TABLE `eh_organization_owners` MODIFY COLUMN `id_card_number` VARCHAR(32) 
 
 -- add by sw 20170418
 ALTER TABLE `eh_parking_lots` ADD COLUMN `lock_car_flag` TINYINT NOT NULL DEFAULT 0 COMMENT ' 1: support, 0: not ';
+
+
+-- 机构表里增加字段 记录操作人  2017-04-20 add by sfyan
+ALTER TABLE `eh_organizations` ADD COLUMN `creator_uid` BIGINT;
+ALTER TABLE `eh_organizations` ADD COLUMN `operator_uid` BIGINT;
+
+-- 机构人员表里增加字段 记录操作人  2017-04-20 add by sfyan
+ALTER TABLE `eh_organization_members` ADD COLUMN `creator_uid` BIGINT;
+ALTER TABLE `eh_organization_members` ADD COLUMN `operator_uid` BIGINT;
