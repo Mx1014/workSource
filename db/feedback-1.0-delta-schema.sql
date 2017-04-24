9381fe1b74a304ad13080b3eca59fc863f3ed650
yanjun <jun.yan@zuolin.com>
2017-04-24 16:41:33 +0800

    add status and handle_type to table feedback

---
+++ db/feedback-1.0-delta-schema.sql
@@ -0,0 +1,3 @@
+-- 举报管理增加“处理状态status”和“处理方式”两个字段 add by yanjun 20170424
+ALTER TABLE `eh_feedbacks` ADD COLUMN `status` TINYINT(4) DEFAULT '0' NULL COMMENT '0: does not handle, 1: verify true, 2: verify false';
+ALTER TABLE `eh_feedbacks` ADD COLUMN `handle_type` TINYINT(4) NULL COMMENT '0: none, 1 delete';
