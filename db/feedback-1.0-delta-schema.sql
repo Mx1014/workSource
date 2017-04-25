97906bb3e4982dc3c20f277760ac26fef42550d8
yanjun <jun.yan@zuolin.com>
2017-04-25 10:36:49 +0800

    add verifytype to feedback

--- db/feedback-1.0-delta-schema.sql
+++ db/feedback-1.0-delta-schema.sql
@@ -1,4 +1,5 @@
--- 举报管理增加“处理状态status”和“处理方式”两个字段 add by yanjun 20170424
-ALTER TABLE `eh_feedbacks` ADD COLUMN `status` TINYINT(4) DEFAULT '0' NULL COMMENT '0: does not handle, 1: verify true, 2: verify false';
+-- 举报管理增加“处理状态status”、“核实情况”、“处理方式”和“域空间”四个字段 add by yanjun 20170425
+ALTER TABLE `eh_feedbacks` ADD COLUMN `status` TINYINT(4) DEFAULT '0' NULL COMMENT '0: does not handle, 1: have handled';
+ALTER TABLE `eh_feedbacks` ADD COLUMN `verify_type` TINYINT(4) NULL COMMENT '0: verify false, 1: verify true';
 ALTER TABLE `eh_feedbacks` ADD COLUMN `handle_type` TINYINT(4) NULL COMMENT '0: none, 1 delete';
 ALTER TABLE `eh_feedbacks` ADD COLUMN `namespace_id` INT(11) NULL;
