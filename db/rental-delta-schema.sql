64b355b2ab3ddab2fc9c21133828cdf6178661ea
sunwen <wen.sun@zuolin.com>
2017-04-28 09:47:34 +0800

    rental

--- db/rental-delta-schema.sql
+++ db/rental-delta-schema.sql
@@ -12,9 +12,18 @@ ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag4` VARCHAR(12
 ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag5` VARCHAR(128);
 
 
-ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_start_time_flag` tinyint(4) DEFAULT NULL COMMENT '至少提前预约时间标志: 1-限制, 0-不限制';
-ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_end_time_flag` tinyint(4) DEFAULT NULL COMMENT '最多提前预约时间标志: 1-限制, 0-不限制';
-ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `org_member_workday_price` decimal(10,2) DEFAULT NULL COMMENT '企业内部工作日价格';
-ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `org_member_weekend_price` decimal(10,2) DEFAULT NULL COMMENT '企业内部节假日价格';
-ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `approving_user_workday_price` decimal(10,2) DEFAULT NULL COMMENT '外部客户工作日价格';
-ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `approving_user_weekend_price` decimal(10,2) DEFAULT NULL COMMENT '外部客户节假日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_start_time_flag` TINYINT DEFAULT 0 COMMENT '至少提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_end_time_flag` TINYINT DEFAULT 0 COMMENT '最多提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `org_member_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部工作日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `org_member_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部节假日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `approving_user_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户工作日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `approving_user_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户节假日价格';
+
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `rental_start_time_flag` TINYINT DEFAULT 0 COMMENT '至少提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `rental_end_time_flag` TINYINT DEFAULT 0 COMMENT '最多提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `org_member_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部工作日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `org_member_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部节假日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `approving_user_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户工作日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `approving_user_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户节假日价格';
+
+
