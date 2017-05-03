d2ded4106f1b5c82670573f5827e54e45e368b52
yanjun <jun.yan@zuolin.com>
2017-05-03 16:49:35 +0800

    add roster status

--- db/activity-3.0.0-delta-schema.sql
+++ db/activity-3.0.0-delta-schema.sql
@@ -1,4 +1,5 @@
 -- 活动增加支付相关字段 add by yanjun 20170502
 ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_flag` TINYINT(4) DEFAULT '0' NULL COMMENT '0: no pay, 1:have pay';
+ALTER TABLE `eh_activity_roster` ADD COLUMN `status` TINYINT(4) DEFAULT '2' NULL COMMENT '0: cancel, 1: reject, 2:normal';
 ALTER TABLE `eh_activities` ADD COLUMN `charge_flag` TINYINT(4) DEFAULT '0' NULL COMMENT '0: no charge, 1: charge'; 
 ALTER TABLE `eh_activities` ADD COLUMN `charge_price` INT(11) NULL COMMENT 'charge_price';
