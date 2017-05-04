09c4e997b4cccc9616d263242887d30803916375
sunwen <wen.sun@zuolin.com>
2017-05-04 18:47:41 +0800

    rental schema

--- db/rental-delta-schema.sql
+++ db/rental-delta-schema.sql
@@ -5,12 +5,6 @@ ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag3` VARCHAR(1
 ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag4` VARCHAR(128);
 ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag5` VARCHAR(128);
 
-ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag1` VARCHAR(128);
-ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag2` VARCHAR(128);
-ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag3` VARCHAR(128);
-ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag4` VARCHAR(128);
-ALTER TABLE `eh_rentalv2_order_attachments`  ADD COLUMN `string_tag5` VARCHAR(128);
-
 
 ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_start_time_flag` TINYINT DEFAULT 0 COMMENT '至少提前预约时间标志: 1-限制, 0-不限制';
 ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_end_time_flag` TINYINT DEFAULT 0 COMMENT '最多提前预约时间标志: 1-限制, 0-不限制';
