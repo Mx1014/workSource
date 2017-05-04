9190a1a429e595adf672e0790eb79b943a33e075
yanjun <jun.yan@zuolin.com>
2017-05-04 12:03:21 +0800

    fix common

--- ehrest/src/main/java/com/everhomes/rest/user/FeedbackDTO.java
+++ ehrest/src/main/java/com/everhomes/rest/user/FeedbackDTO.java
@@ -21,8 +21,7 @@ import com.everhomes.util.StringHelper;
  *   <li>forumId: 如果举报类型是post的话，返回post对应的forumId</li>
  *   <li>targetStatus: 目标的状态， 0-已删除、1-待确认、2-正常。参考{@link com.everhomes.rest.forum.PostStatus}</li>
  *   <li>proofResourceUri:图片链接</li>
- *   <li>contentCategory:内容类别：0-其它、1-产品bug、2-产品改进、3-版本问题;11-敏感信息、12-版权问题、13
- *         -暴力色情、14-诈骗和虚假信息、15-骚扰；16-谣言、17-恶意营销、18-诱导分享；19-政治。参考{@link com.everhomes.rest.user.FeedbackContentCategoryType}</li>
+ *   <li>contentCategory:内容类别。 参考{@link com.everhomes.rest.user.FeedbackContentCategoryType}</li>
  *   <li>contentCategoryText: contentCategory对应的内容</li>
  *   <li>status: 处理状态  0-未处理,</li>
  *   <li>verifyType: 核实情况  0-不属实， 1-属实</li>
