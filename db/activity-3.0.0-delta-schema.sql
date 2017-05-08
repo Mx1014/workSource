d87d11b8c6b490fd215495235bbd78474b7ec2a2
yanjun <jun.yan@zuolin.com>
2017-05-08 20:22:39 +0800

    add order_create_time

--- db/activity-3.0.0-delta-schema.sql
+++ db/activity-3.0.0-delta-schema.sql
@@ -1,6 +1,7 @@
 -- 活动增加支付相关字段 add by yanjun 20170502
 ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_flag` TINYINT(4) DEFAULT '0' NULL COMMENT '0: no pay, 1:have pay, 2:tobepay, 3:refund';
 ALTER TABLE `eh_activity_roster` ADD COLUMN `order_token` VARCHAR(128) NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `order_create_time` DATETIME NULL;
 ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_time` DATETIME NULL;
 ALTER TABLE `eh_activity_roster` ADD COLUMN `refund_time` DATETIME NULL;
 ALTER TABLE `eh_activity_roster` ADD COLUMN `order_no` BIGINT(20) NULL;
