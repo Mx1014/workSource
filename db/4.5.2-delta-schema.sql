15d6797a0b86f2f162a82525f8ce846ba77a61a1
ying.xiong <ying.xiong@zuolin.com>
2017-05-09 18:14:46 +0800

    merge data

--- db/4.5.2-delta-schema.sql
+++ db/4.5.2-delta-schema.sql
@@ -75,3 +75,15 @@ CREATE TABLE `eh_approval_range_statistics` (
   `create_time` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 ;
+
+-- merge from shenye201704 by xiongying20170509
+ALTER TABLE eh_equipment_inspection_task_logs ADD COLUMN `inspection_category_id` BIGINT; 
+ALTER TABLE eh_equipment_inspection_task_logs ADD COLUMN `community_id` BIGINT; 
+ALTER TABLE eh_equipment_inspection_task_logs ADD COLUMN `namespace_id` INTEGER; 
+
+ALTER TABLE eh_equipment_inspection_item_results ADD COLUMN `create_time` DATETIME; 
+ALTER TABLE eh_equipment_inspection_item_results ADD COLUMN `community_id` BIGINT; 
+ALTER TABLE eh_equipment_inspection_item_results ADD COLUMN `standard_id` BIGINT; 
+ALTER TABLE eh_equipment_inspection_item_results ADD COLUMN `equipment_id` BIGINT; 
+ALTER TABLE eh_equipment_inspection_item_results ADD COLUMN `inspection_category_id` BIGINT; 
+ALTER TABLE eh_equipment_inspection_item_results ADD COLUMN `namespace_id` INTEGER; 
