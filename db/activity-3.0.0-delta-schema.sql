da58e53686946f9bb1958e4417c38008c4844814
yanjun <jun.yan@zuolin.com>
2017-05-09 14:05:37 +0800

    save order detail 3

--- db/activity-3.0.0-delta-schema.sql
+++ db/activity-3.0.0-delta-schema.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_flag` TINYINT(4) DEFAULT '0' NULL COMMENT '0: no pay, 1:have pay, 2:tobepay, 3:refund';
 ALTER TABLE `eh_activity_roster` ADD COLUMN `order_no` BIGINT(20) NULL;
 ALTER TABLE `eh_activity_roster` ADD COLUMN `order_start_time` DATETIME NULL;
-ALTER TABLE `eh_activity_roster` ADD COLUMN `vendor_type` TINYINT(4) NULL COMMENT '10001: alipay, 10002: wechatpay';
+ALTER TABLE `eh_activity_roster` ADD COLUMN `vendor_type` VARCHAR(32) NULL COMMENT '10001: alipay, 10002: wechatpay';
 ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_amount` INT(11) NULL;
 ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_time` DATETIME NULL;
 ALTER TABLE `eh_activity_roster` ADD COLUMN `refund_order_no` BIGINT(20) NULL;
