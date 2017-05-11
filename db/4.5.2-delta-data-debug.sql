1a0d9d08eb979f91f4a683964498dfdcc0327812
xq.tian <xq.tian@zuolin.com>
2017-05-11 10:53:58 +0800

    update some sql

--- db/4.5.2-delta-data-debug.sql
+++ db/4.5.2-delta-data-debug.sql
@@ -1,9 +1,9 @@
 -- 修改审批字符串模板 add by xq.tian  2017/04/17
-UPDATE `eh_locale_templates` SET `text` = '${userName}申请加入“${groupName}”(理由:${reason}),是否同意?'
+UPDATE `eh_locale_templates` SET `text` = '${userName}申请加入“${groupName}”（理由：${reason}），是否同意？'
 WHERE `scope` = 'group.notification' AND `code` = 44 AND `namespace_id` = 0;
-UPDATE `eh_locale_templates` SET `text` = '${userName}${description}申请加入公司“${enterpriseName}”,是否同意?'
+UPDATE `eh_locale_templates` SET `text` = '${userName}${description}申请加入公司“${enterpriseName}”，是否同意？'
 WHERE `scope` = 'enterprise.notification' AND `code` = 7 AND `namespace_id` = 0;
-UPDATE `eh_locale_templates` SET `text` = '您的家人${userName}申请加入${address},是否同意?'
+UPDATE `eh_locale_templates` SET `text` = '您的家人${userName}申请加入${address}，是否同意？'
 WHERE `scope` = 'family.notification' AND `code` = 2 AND `namespace_id` = 0;
 
 -- 修改原来的“系统小助手” 名称为“系统消息”，并且修改成新的头像
