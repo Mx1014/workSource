d606d8cefcc6fe5bd57dd173c00c671994b4475d
yanjun <jun.yan@zuolin.com>
2017-05-12 14:37:46 +0800

    fix meger msg2.0 bug

--- ehcore/src/main/java/com/everhomes/sequence/SequenceServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/sequence/SequenceServiceImpl.java
@@ -1300,6 +1300,8 @@ public class SequenceServiceImpl implements SequenceService {
 
         syncTableSequence(null, EhImportFileTasks.class, Tables.EH_IMPORT_FILE_TASKS.getName(), (dbContext) -> {
             return dbContext.select(Tables.EH_IMPORT_FILE_TASKS.ID.max()).from(Tables.EH_IMPORT_FILE_TASKS).fetchOne().value1();
+        });
+            
         syncTableSequence(null, EhUserNotificationSettings.class, Tables.EH_USER_NOTIFICATION_SETTINGS.getName(), (dbContext) -> {
             return dbContext.select(Tables.EH_USER_NOTIFICATION_SETTINGS.ID.max()).from(Tables.EH_USER_NOTIFICATION_SETTINGS).fetchOne().value1();
         });
