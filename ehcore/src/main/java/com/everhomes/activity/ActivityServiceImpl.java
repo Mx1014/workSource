a3296519db3f44f716ec5bd4310e5ed1904ca3e1
yanjun <jun.yan@zuolin.com>
2017-05-12 17:27:24 +0800

    fix bug

--- ehcore/src/main/java/com/everhomes/activity/ActivityServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/activity/ActivityServiceImpl.java
@@ -1149,7 +1149,7 @@ public class ActivityServiceImpl implements ActivityService {
 		ActivityRoster roster = activityProvider.findRosterByUidAndActivityId(activity.getId(), userId, ActivityRosterStatus.NORMAL.getCode());
 		
 		//只有需要支付并已经支付的才需要退款
-		if(activity.getChargeFlag() != ActivityChargeFlag.CHARGE.getCode() || roster.getPayFlag() != ActivityRosterPayFlag.PAY.getCode()){
+		if(activity.getChargeFlag() == null || activity.getChargeFlag().byteValue() == ActivityChargeFlag.UNCHARGE.getCode() || roster.getPayFlag() == null || roster.getPayFlag().byteValue() != ActivityRosterPayFlag.PAY.getCode()){
 			return;
 		}
 		
