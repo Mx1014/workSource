17ee45a9ca1c581c0012d959b92be411c9ee3018
sunwen <wen.sun@zuolin.com>
2017-05-17 09:28:19 +0800

    rent

--- db/rent-delta-schema.sql
+++ db/rent-delta-schema.sql
@@ -1,18 +1,4 @@
 
-CREATE TABLE `eh_lease_issuers` (
-  `id` BIGINT NOT NULL COMMENT 'id of the record',
-  `namespace_id` INTEGER NOT NULL DEFAULT 0,
-  `community_id` BIGINT NOT NULL DEFAULT 0,
-  `issuer_contact` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'For rent',
-  `issuer_name` VARCHAR(128),
-  `enterprise_id` BIGINT COMMENT 'enterprise id',
-  `creator_uid` BIGINT,
-  `create_time` DATETIME,
-  `status` TINYINT COMMENT '0: inactive, 2: active',
-  
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
-
 ALTER TABLE `eh_buildings` ADD COLUMN `general_form_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'id of eh_general_form';
 ALTER TABLE `eh_buildings` ADD COLUMN `custom_form_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: not add custom field, 1: add custom field';
 
@@ -27,11 +13,42 @@ CREATE TABLE `eh_lease_configs` (
   `owner_type` VARCHAR(32) COMMENT 'owner type, e.g EhCommunities',
   `owner_id` BIGINT COMMENT 'owner id, e.g eh_communities id',  
   `config_name` VARCHAR(128),
-  `config_value` TINYINT NOT NULL DEFAULT 0 COMMENT '0: disabled, 1: enabled',  
-  `title` VARCHAR(128),
-  
+  `config_value` VARCHAR(128),  
+
   `create_time` DATETIME,
   `creator_uid` BIGINT,
   
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `eh_general_form_vals` (
+	`id` BIGINT NOT NULL COMMENT 'id of the record',
+	`namespace_id` INT NOT NULL DEFAULT '0',
+	`organization_id` BIGINT NOT NULL DEFAULT '0',
+	`owner_id` BIGINT NOT NULL,
+	`owner_type` VARCHAR (64) NOT NULL,
+	`module_id` BIGINT DEFAULT NULL COMMENT 'the module id',
+	`module_type` VARCHAR (64) DEFAULT NULL,
+
+    `source_id` BIGINT NOT NULL,
+	`source_type` VARCHAR (64) NOT NULL,
+
+	`form_origin_id` BIGINT DEFAULT NULL,
+	`form_version` BIGINT DEFAULT NULL,
+	`field_name` VARCHAR (128) DEFAULT NULL,
+	`field_type` VARCHAR (128) DEFAULT NULL,
+	`field_value` text,
+	`create_time` datetime DEFAULT NULL COMMENT 'record create time',
+
+    `string_tag1` VARCHAR (128) DEFAULT NULL,
+	`string_tag2` VARCHAR (128) DEFAULT NULL,
+	`string_tag3` VARCHAR (128) DEFAULT NULL,
+	`string_tag4` VARCHAR (128) DEFAULT NULL,
+	`string_tag5` VARCHAR (128) DEFAULT NULL,
+	`integral_tag1` BIGINT (20) DEFAULT '0',
+	`integral_tag2` BIGINT (20) DEFAULT '0',
+	`integral_tag3` BIGINT (20) DEFAULT '0',
+	`integral_tag4` BIGINT (20) DEFAULT '0',
+	`integral_tag5` BIGINT (20) DEFAULT '0',
+	PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
