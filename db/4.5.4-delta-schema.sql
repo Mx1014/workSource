de925c36f25721b7a4bf56f851c2412faee7a9cd
dengs <shuang.deng@zuolin.com>
2017-05-22 17:12:24 +0800

    merge schema sql

--- db/4.5.4-delta-schema.sql
+++ db/4.5.4-delta-schema.sql
@@ -158,3 +158,6 @@ CREATE TABLE `eh_express_order_logs` (
   
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+ -- add by dengs 20170511 添加服务联盟描述信息折叠高度
+ALTER TABLE `eh_service_alliances` ADD COLUMN `description_height` INT NULL DEFAULT '2' COMMENT '0:not collapse , N: collapse N lines';
