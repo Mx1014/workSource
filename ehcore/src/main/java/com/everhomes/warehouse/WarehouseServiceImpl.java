5f3d8eb656eadbc15b4dee90e9d6b7ca7006a34d
ying.xiong <ying.xiong@zuolin.com>
2017-05-23 13:38:01 +0800

    fix updatestock

--- ehcore/src/main/java/com/everhomes/warehouse/WarehouseServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/warehouse/WarehouseServiceImpl.java
@@ -545,7 +545,7 @@ public class WarehouseServiceImpl implements WarehouseService {
 
             //申请下面的都出库了则申请也出库
             List<WarehouseRequestMaterials> requestMaterials = warehouseProvider.listWarehouseRequestMaterials(cmd.getRequestId(), cmd.getOwnerType(), cmd.getOwnerId());
-            if(requestMaterials == null || requestMaterials.size() == 0) {
+            if(requestMaterials != null || requestMaterials.size() > 0) {
                 WarehouseRequests request = warehouseProvider.findWarehouseRequests(cmd.getRequestId(), cmd.getOwnerType(), cmd.getOwnerId());
                 request.setDeliveryFlag(DeliveryFlag.YES.getCode());
                 request.setUpdateTime(current);
