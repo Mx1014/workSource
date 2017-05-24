a87003c3a27724e42d2f567c195d1dd9e9c9e10a
dengs <shuang.deng@zuolin.com>
2017-05-24 13:57:57 +0800

    servicealliance2.5 sql modfiy

--- db/serviceAlliance-v2.5-delta-schema.sql
+++ db/serviceAlliance-v2.5-delta-schema.sql
@@ -1,3 +1,3 @@
 -- 服务联盟	增加排序和是否显示在app端的字段 by dengs, 20170523
-ALTER TABLE `eh_service_alliances` ADD COLUMN `show_or_hide` TINYINT NOT NULL DEFAULT '1' COMMENT '0:hide,1show';
+ALTER TABLE `eh_service_alliances` ADD COLUMN `show_flag` TINYINT NOT NULL DEFAULT '1' COMMENT '0:hide,1show';
 ALTER TABLE `eh_service_alliances` ADD COLUMN `sort_order` BIGINT COMMENT 'order of the service alliance';
