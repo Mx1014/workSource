d56711abc32e62fa5b3aea7dce943a3b212bb6b2
dengs <shuang.deng@zuolin.com>
2017-05-25 09:53:07 +0800

    serviceAlliance2.5 fix complie error

--- ehrest/src/main/java/com/everhomes/rest/yellowPage/UpdateServiceAllianceEnterpriseDefaultOrderCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/yellowPage/UpdateServiceAllianceEnterpriseDefaultOrderCommand.java
@@ -3,6 +3,7 @@ package com.everhomes.rest.yellowPage;
 
 import java.util.List;
 
+import com.everhomes.discover.ItemType;
 import com.everhomes.util.StringHelper;
 /**
  * 
@@ -15,6 +16,7 @@ import com.everhomes.util.StringHelper;
  */
 public class UpdateServiceAllianceEnterpriseDefaultOrderCommand {
 	
+	@ItemType(ServiceAllianceDTO.class)
 	private List<ServiceAllianceDTO> values;
 	public List<ServiceAllianceDTO> getValues() {
 		return values;
