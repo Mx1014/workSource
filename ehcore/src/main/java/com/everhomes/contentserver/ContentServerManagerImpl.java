808c8c1c5ca8924b3cc85cac7373fa072ba6cba4
xq.tian <xq.tian@zuolin.com>
2017-05-25 11:28:05 +0800

    add tick message debug log

--- ehcore/src/main/java/com/everhomes/contentserver/ContentServerManagerImpl.java
+++ ehcore/src/main/java/com/everhomes/contentserver/ContentServerManagerImpl.java
@@ -187,7 +187,7 @@ public class ContentServerManagerImpl implements ContentServerMananger {
 
         ContentServerResource resource = contentServerProvider.findByResourceId(resourceId);
         if (resource == null) {
-            LOGGER.error("Resource not found, orginResourceId=" + orginResourceId 
+            LOGGER.error("Resource not found, orginResourceId=" + orginResourceId
                 + ", decodeResourceId=" + resourceId + ", loginToken=" + login);
             throw RuntimeErrorException.errorWith(ErrorCodes.SCOPE_GENERAL, ErrorCodes.ERROR_INVALID_PARAMETER,
                     "Resource file not found");
--- ehcore/src/main/java/com/everhomes/flow/FlowTimeoutAction.java
+++ ehcore/src/main/java/com/everhomes/flow/FlowTimeoutAction.java
@@ -7,31 +7,36 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.everhomes.rest.flow.FlowStatusType;
-
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class FlowTimeoutAction implements Runnable {
-	private static final Logger log = LoggerFactory.getLogger(FlowTimeoutAction.class);
-	
-	@Autowired
-	FlowTimeoutProvider flowTimeoutProvider;
-	
-	@Autowired
-	FlowTimeoutService flowTimeoutService;
-	
-	private Long timeoutId;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlowTimeoutAction.class);
 
-	@Override
-	public void run() {
-		if(flowTimeoutProvider.deleteIfValid(timeoutId)) {
-			//delete ok, means we take it's owner
-			FlowTimeout ft = flowTimeoutProvider.getFlowTimeoutById(timeoutId);
-			flowTimeoutService.processTimeout(ft);
-		}
-	}
-	
-	public FlowTimeoutAction(final String timeoutId) {
-		this.timeoutId = Long.parseLong(timeoutId);
-	}
+    @Autowired
+    FlowTimeoutProvider flowTimeoutProvider;
+
+    @Autowired
+    FlowTimeoutService flowTimeoutService;
+
+    private Long timeoutId;
+
+    @Override
+    public void run() {
+        if (flowTimeoutProvider.deleteIfValid(timeoutId)) {
+            FlowTimeout ft = flowTimeoutProvider.getFlowTimeoutById(timeoutId);
+
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("FlowTimeoutAction.run success ft = {}", ft);
+            }
+
+            //delete ok, means we take it's owner
+            flowTimeoutService.processTimeout(ft);
+        } else {
+            LOGGER.warn("FlowTimeoutAction.run failure timeoutId = {}", timeoutId);
+        }
+    }
+
+    public FlowTimeoutAction(final String timeoutId) {
+        this.timeoutId = Long.parseLong(timeoutId);
+    }
 }
