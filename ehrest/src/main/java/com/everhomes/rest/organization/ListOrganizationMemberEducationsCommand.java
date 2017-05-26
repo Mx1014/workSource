48483f73ced16a986fc9026bef57c2066f73118b
nan.rong <nan.rong@zuolin.com>
2017-05-26 21:52:34 +0800

    a little change, id->detailId

--- ehrest/src/main/java/com/everhomes/rest/organization/ListOrganizationMemberEducationsCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/organization/ListOrganizationMemberEducationsCommand.java
@@ -4,23 +4,23 @@ import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
- * <li>id: 员工编号(Education.detail_id),注意不是教育信息的id</li>
+ * <li>detailId: 员工编号(Education.detail_id),注意不是教育信息的id</li>
  * </ul>
  */
 public class ListOrganizationMemberEducationsCommand {
 
 
-    private Long id;
+    private Long detailId;
 
     public ListOrganizationMemberEducationsCommand() {
     }
 
-    public Long getId() {
-        return id;
+    public Long getDetailId() {
+        return detailId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setDetailId(Long detailId) {
+        this.detailId = detailId;
     }
 
     @Override
