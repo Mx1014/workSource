a8bf5686c7668e8b6972021b336efa6d698ac3e3
wuhan <han.wu@zuolin.com>
2017-05-27 17:01:57 +0800

    merge rental 2.6

--- db/4.5.5-delta-schema.sql
+++ db/4.5.5-delta-schema.sql
@@ -141,18 +141,48 @@ CREATE TABLE `eh_warehouse_request_materials` (
 
 -- 单位表 
 CREATE TABLE `eh_warehouse_units` (
-  `id` bigint(20) NOT NULL,
-  `namespace_id` int(11) NOT NULL DEFAULT '0',
-  `owner_type` varchar(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the category, enterprise, etc',
-  `owner_id` bigint(20) NOT NULL DEFAULT '0',
-  `name` varchar(64) NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: disabled, 1: waiting for confirmation, 2: active',
-  `creator_uid` bigint(20) NOT NULL DEFAULT '0' COMMENT 'record creator user id',
-  `create_time` datetime DEFAULT NULL,
-  `deletor_uid` bigint(20) NOT NULL DEFAULT '0' COMMENT 'record deleter user id',
-  `delete_time` datetime DEFAULT NULL,
+  `id` BIGINT(20) NOT NULL,
+  `namespace_id` INT(11) NOT NULL DEFAULT '0',
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the category, enterprise, etc',
+  `owner_id` BIGINT(20) NOT NULL DEFAULT '0',
+  `name` VARCHAR(64) NOT NULL,
+  `status` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '0: disabled, 1: waiting for confirmation, 2: active',
+  `creator_uid` BIGINT(20) NOT NULL DEFAULT '0' COMMENT 'record creator user id',
+  `create_time` DATETIME DEFAULT NULL,
+  `deletor_uid` BIGINT(20) NOT NULL DEFAULT '0' COMMENT 'record deleter user id',
+  `delete_time` DATETIME DEFAULT NULL,
 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 
-alter table eh_import_file_tasks modify column result longtext;
+ALTER TABLE eh_import_file_tasks MODIFY COLUMN result LONGTEXT;
+
+
+ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag1` VARCHAR(128);
+ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag2` VARCHAR(128);
+ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag3` VARCHAR(128);
+ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag4` VARCHAR(128);
+ALTER TABLE `eh_rentalv2_config_attachments`  ADD COLUMN `string_tag5` VARCHAR(128);
+
+
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_start_time_flag` TINYINT DEFAULT 0 COMMENT '至少提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `rental_end_time_flag` TINYINT DEFAULT 0 COMMENT '最多提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `org_member_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部工作日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `org_member_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部节假日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `approving_user_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户工作日价格';
+ALTER TABLE `eh_rentalv2_default_rules`  ADD COLUMN `approving_user_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户节假日价格';
+
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `rental_start_time_flag` TINYINT DEFAULT 0 COMMENT '至少提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `rental_end_time_flag` TINYINT DEFAULT 0 COMMENT '最多提前预约时间标志: 1-限制, 0-不限制';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `org_member_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部工作日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `org_member_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '企业内部节假日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `approving_user_workday_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户工作日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `approving_user_weekend_price` DECIMAL(10,2) DEFAULT NULL COMMENT '外部客户节假日价格';
+ALTER TABLE `eh_rentalv2_resources`  ADD COLUMN `default_order` BIGINT NOT NULL DEFAULT 0 COMMENT 'order';
+
+ALTER TABLE `eh_rentalv2_cells`  ADD COLUMN `org_member_original_price` DECIMAL(10,2) DEFAULT NULL COMMENT '原价-如果打折则有(企业内部价)';
+ALTER TABLE `eh_rentalv2_cells`  ADD COLUMN `org_member_price` DECIMAL(10,2) DEFAULT NULL COMMENT '实际价格-打折则为折后价(企业内部价)';
+ALTER TABLE `eh_rentalv2_cells`  ADD COLUMN `approving_user_original_price` DECIMAL(10,2) DEFAULT NULL COMMENT '原价-如果打折则有（外部客户价）';
+ALTER TABLE `eh_rentalv2_cells`  ADD COLUMN `approving_user_price` DECIMAL(10,2) DEFAULT NULL COMMENT '实际价格-打折则为折后价（外部客户价）';
+
+ALTER TABLE `eh_rentalv2_orders`  ADD COLUMN `flow_case_id` BIGINT DEFAULT NULL COMMENT 'id of the flow_case';
