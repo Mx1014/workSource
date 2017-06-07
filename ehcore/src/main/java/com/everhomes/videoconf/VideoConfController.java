045256db68311e3a23d37c82a40d5263e2401a91
wuhan <han.wu@zuolin.com>
2017-06-07 19:11:59 +0800

    some test operate

--- ehcore/src/main/java/com/everhomes/videoconf/VideoConfController.java
+++ ehcore/src/main/java/com/everhomes/videoconf/VideoConfController.java
@@ -1439,6 +1439,8 @@ public class VideoConfController  extends ControllerBase{
 	@RestReturn(CheckVideoConfTrialAccountResponse.class)
 	public RestResponse checkVideoConfTrialAccount(CheckVideoConfTrialAccountCommand cmd){
 		CheckVideoConfTrialAccountResponse resp = videoConfService.checkVideoConfTrialAccount(cmd);
+		//TODO : 给王蓉蓉测试用,记得删
+		resp.setTrialFlag((byte) 2);
 		RestResponse response = new RestResponse(resp);
 		response.setErrorCode(ErrorCodes.SUCCESS);
 		response.setErrorDescription("OK");
