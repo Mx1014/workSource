cbb75f06d7993477ea114df86805d40632242324
dengs <shuang.deng@zuolin.com>
2017-06-08 20:29:22 +0800

    ServiceAlliance fix issue 10974

--- db/4.6.0-delta-data-release.sql
+++ db/4.6.0-delta-data-release.sql
@@ -75,4 +75,15 @@ INSERT INTO `eh_locale_templates`(`namespace_id`, `scope`, `code`,`locale`, `des
 	VALUES (999983, 'flow:40400', 33, 'zh_CN', '催办短信', '38832');
 INSERT INTO `eh_locale_templates` (`scope`, `code`, `locale`, `description`, `text`, `namespace_id`)
 	VALUES ('sms.default.yzx', '50', 'zh_CN', '正中会-预成功', '47318', '999983');
+
+-- 服务联盟bug 10974 by dengs,20170608
+update eh_locale_templates SET text='您收到一条${categoryName}:${serviceAllianceName}的申请 
+ 提交者信息：
+ 预订人：${creatorName} 
+ 手机号：${creatorMobile} 
+ 公司名称：${creatorOrganization} 
+ 
+ 提交的信息：
+ ${notemessage} 
+ 您可以登录管理后台查看详情' WHERE scope='serviceAlliance.request.notification' AND `code`=1;
 	
