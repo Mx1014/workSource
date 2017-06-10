ce997fe5daa4606019d9d50a6e84ca258612ea12
sunwen <wen.sun@zuolin.com>
2017-06-10 14:15:23 +0800

    rent

--- db/rent-delta-schema.sql
+++ db/rent-delta-schema.sql
@@ -9,6 +9,8 @@ ALTER TABLE `eh_buildings` ADD COLUMN `default_order` BIGINT NOT NULL;
 
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `longitude` DOUBLE DEFAULT NULL;
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `latitude` DOUBLE DEFAULT NULL;
+ALTER TABLE `eh_lease_promotions` ADD COLUMN `address` VARCHAR(512);
+
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `general_form_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'id of eh_general_form';
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `custom_form_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: not add custom field, 1: add custom field';
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `default_order` BIGINT NOT NULL DEFAULT 0;
