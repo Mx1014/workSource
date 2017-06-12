904f7f2d487af4be649aa15b5ecc3d0c3135b620
wuhan <han.wu@zuolin.com>
2017-06-12 15:27:16 +0800

    merge approval 1.2

--- db/4.6.1-delta-data-debug.sql
+++ db/4.6.1-delta-data-debug.sql
@@ -1,5 +1,5 @@
 -- 错误提示，add by tt, 20170527
-select max(id) into @id from `eh_locale_strings`;
+SELECT MAX(id) INTO @id FROM `eh_locale_strings`;
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'talent', '1', 'zh_CN', '本功能仅对企业管理员开放，如有需要请联系企业管理员');
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'talent', '2', 'zh_CN', '分类名称不能重复');
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'talent', '3', 'zh_CN', '导入失败，请检查数据是否按要求填写');
@@ -7,7 +7,7 @@ INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id:=@id+1, 'parameters.error', '10003', 'zh_CN', '参数不能为空');
 
 -- 默认头像，add by tt, 20170527
-select max(id) into @id from `eh_configurations`;
+SELECT MAX(id) INTO @id FROM `eh_configurations`;
 INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES (@id:=@id+1, 'talent.male.uri', 'cs://1/image/aW1hZ2UvTVRwaE1tRmpOalEwWVRJME1UZ3pOalUwT0RKa09HRTNZV0ZtWkdabFpHTmtOdw', 'talen default male uri', 0, NULL);
 INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES (@id:=@id+1, 'talent.female.uri', 'cs://1/image/aW1hZ2UvTVRwa04yRTNaR1ZrWlRreU1XWmhabVV3WkdZd05qQTFZamxqTURSa1pHSmlNQQ', 'talen default female uri', 0, NULL);
 
@@ -15,19 +15,19 @@ INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespac
 INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) VALUES (40730, '企业人才', 40000, NULL, 'react:/enterprise-management/talent-list', 1, 2, '/40730/70100', 'park', 498, 40730);
 
 -- 权限，add by tt, 20170527
-select max(id) into @pri_id from `eh_acl_privileges`;
+SELECT MAX(id) INTO @pri_id FROM `eh_acl_privileges`;
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`) VALUES (@pri_id:=@pri_id+1, 0, '企业人才 管理员', '企业人才 业务模块权限', NULL);
 
 -- 菜单关联权限，add by tt, 20170527
-select max(id) into @id from `eh_web_menu_privileges`;
+SELECT MAX(id) INTO @id FROM `eh_web_menu_privileges`;
 INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`) VALUES (@id+1, @pri_id, 40730, '企业人才', 1, 1, '企业人才 全部权限', 714);
 
 -- 权限赋予超管，add by tt, 20170527
-select max(id) into @id from `eh_acls`;
+SELECT MAX(id) INTO @id FROM `eh_acls`;
 INSERT INTO `eh_acls` (`id`, `owner_type`, `owner_id`, `grant_type`, `privilege_id`, `role_id`, `order_seq`, `creator_uid`, `create_time`, `namespace_id`, `role_type`, `scope`, `integral_tag1`, `integral_tag2`, `integral_tag3`, `integral_tag4`, `integral_tag5`, `comment_tag1`, `comment_tag2`, `comment_tag3`, `comment_tag4`, `comment_tag5`) VALUES (@id+1, 'EhOrganizations', NULL, 1, @pri_id, 1001, 0, 1, '2017-06-05 09:41:07', 0, 'EhAclRoles', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- 显示菜单，add by tt, 20170527
-select max(id) into @id from `eh_web_menu_scopes`;
+SELECT MAX(id) INTO @id FROM `eh_web_menu_scopes`;
 INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@id+1, 40730, '', 'EhNamespaces', 999990, 2);
 
 -- 不同环境执行不同的语句！！！！
@@ -53,22 +53,22 @@ INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `o
 
 
 -- 导出无数据提示，add by tt, 20170522
-select max(id) into @id from `eh_locale_strings`;
+SELECT MAX(id) INTO @id FROM `eh_locale_strings`;
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES (@id+1, 'organization', '800000', 'zh_CN', '无数据！');
 
 -- 初始化organization表中是否设置了企业管理员标记，add by tt, 20170522
-update eh_organizations t1
-set t1.set_admin_flag = 1
-where t1.`status` = 2
-and t1.parent_id = 0
-and t1.group_type = 'ENTERPRISE'
-and exists (
-	select 1 
-	from eh_acl_role_assignments t2
-	where t2.owner_type = 'EhOrganizations'
-	and t2.owner_id = t1.id
-	and t2.target_type = 'EhUsers'
-	and t2.role_id = 1005
+UPDATE eh_organizations t1
+SET t1.set_admin_flag = 1
+WHERE t1.`status` = 2
+AND t1.parent_id = 0
+AND t1.group_type = 'ENTERPRISE'
+AND EXISTS (
+	SELECT 1 
+	FROM eh_acl_role_assignments t2
+	WHERE t2.owner_type = 'EhOrganizations'
+	AND t2.owner_id = t1.id
+	AND t2.target_type = 'EhUsers'
+	AND t2.role_id = 1005
 );
 
 
@@ -76,4 +76,100 @@ and exists (
 UPDATE `eh_service_alliances` SET `default_order` = `id`;
 
 -- 将服务联盟下的菜单机构管理改为服务管理 by dengs,20170524
-update `eh_web_menus` SET `name`='服务管理' WHERE `id` = 40520;
+UPDATE `eh_web_menus` SET `name`='服务管理' WHERE `id` = 40520;
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) 
+VALUES('51000','审批管理','50000',NULL,'react:/approval-management/approval-list/51000','0','2','/50000/51000','park','591','51000');
+-- INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) 
+-- VALUES('51001','审批列表','51000',NULL,'react:/approval-management/approval-list/51000','1','2','/50000/51000/51001','park','591','51000');
+-- INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) 
+-- VALUES('51002','申请记录','51000',NULL,'apply_record','1','2','/50000/51000/51002','park','591','51000'); 
+
+-- 给科兴加
+SET @id = (SELECT MAX(id) FROM eh_web_menu_scopes);
+INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES((@id:=@id+1),'51000','','EhNamespaces','999983','2');
+-- 给深业加
+SET @id = (SELECT MAX(id) FROM eh_web_menu_scopes);
+INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES((@id:=@id+1),'51000','','EhNamespaces','999992','2');
+SET @id = (SELECT MAX(id) FROM eh_web_menu_scopes);
+INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES((@id:=@id+1),'50900','','EhNamespaces','999992','2');
+-- INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES((@id:=@id+1),'51001','','EhNamespaces','999983','2');
+-- INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES((@id:=@id+1),'51002','','EhNamespaces','999983','2');
+
+-- 加module
+INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`) 
+VALUES('51000','审批管理','50000','/50000/51000','0','2','2','0','2017-05-19 11:50:20');
+
+INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`) 
+VALUES (10137, 0, '审批管理', '审批管理 全部权限', NULL);
+
+SET @web_menu_privilege_id = (SELECT MAX(id) FROM `eh_web_menu_privileges`);
+INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`) 
+VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), 10137, 51000, '审批管理', 1, 1, '审批管理  全部权限', 202); 
+-- INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`) 
+-- VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), 10137, 51001, '审批列表', 1, 1, '审批列表  全部权限', 202); 
+-- INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`) 
+-- VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), 10137, 51002, '申请记录', 1, 1, '申请记录  全部权限', 202); 
+
+SET @acl_id = (SELECT MAX(id) FROM `eh_acls`);
+INSERT INTO `eh_acls` (`id`, `namespace_id`, `owner_type`, `owner_id`, `grant_type`, `privilege_id`, `role_id`, `role_type`, `order_seq`, `creator_uid`, `create_time`)
+VALUES ((@acl_id := @acl_id + 1), 0, 'EhOrganizations', NULL, 1, 10137, 1005, 'EhAclRoles', 0, 1, NOW()); 
+INSERT INTO `eh_acls` (`id`, `namespace_id`, `owner_type`, `owner_id`, `grant_type`, `privilege_id`, `role_id`, `role_type`, `order_seq`, `creator_uid`, `create_time`)
+VALUES ((@acl_id := @acl_id + 1), 0, 'EhOrganizations', NULL, 1, 10137, 1001, 'EhAclRoles', 0, 1, NOW()); 
+
+SET @eh_service_module_privileges_id = (SELECT MAX(id) FROM `eh_service_module_privileges`);
+INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) 
+VALUES ((@eh_service_module_privileges_id := @eh_service_module_privileges_id + 1), '51000', '1', '10137', NULL, '0', UTC_TIMESTAMP());
+SET @eh_service_module_scopes_id = (SELECT MAX(id) FROM `eh_service_module_scopes`);
+INSERT INTO `eh_service_module_scopes` (`id`, `namespace_id`, `module_id`, `module_name`, `owner_type`, `owner_id`, `default_order`, `apply_policy`)
+VALUES ((@eh_service_module_scopes_id := @eh_service_module_scopes_id + 1), 999983, 51000, '表单管理', 'EhNamespaces', 999983, NULL, 2);
+SET @eh_service_module_scopes_id = (SELECT MAX(id) FROM `eh_service_module_scopes`);
+INSERT INTO `eh_service_module_scopes` (`id`, `namespace_id`, `module_id`, `module_name`, `owner_type`, `owner_id`, `default_order`, `apply_policy`)
+VALUES ((@eh_service_module_scopes_id := @eh_service_module_scopes_id + 1), 999983, 51000, '表单管理', 'EhNamespaces', 999992, NULL, 2);
+
+SET @eh_launch_pad_items_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`, `more_order`, `alias_icon_uri`)
+ VALUES((@eh_launch_pad_items_id:=@eh_launch_pad_items_id+1),'999983','0','0','0','/home','Bizs','审批','审批','cs://1/image/aW1hZ2UvTVRwak5qVTVZV1EyWkdVM1l6TTBaR1k1Tm1RMk56STVaVGt3TldGbU1HVmlOQQ','1','1','65','','4','0','1','1','','0',NULL,NULL,NULL,'0','pm_admin','0',NULL,NULL,'0',NULL);
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`, `more_order`, `alias_icon_uri`) 
+VALUES((@eh_launch_pad_items_id:=@eh_launch_pad_items_id+1),'999983','0','0','0','/home','Bizs','审批','审批','cs://1/image/aW1hZ2UvTVRwak5qVTVZV1EyWkdVM1l6TTBaR1k1Tm1RMk56STVaVGt3TldGbU1HVmlOQQ','1','1','65','','4','0','1','1','','0',NULL,NULL,NULL,'0','park_tourist','0',NULL,NULL,'0',NULL);
+SET @eh_launch_pad_items_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`, `more_order`, `alias_icon_uri`)
+ VALUES((@eh_launch_pad_items_id:=@eh_launch_pad_items_id+1),'999992','0','0','0','/home','Bizs','审批','审批','cs://1/image/aW1hZ2UvTVRwak5qVTVZV1EyWkdVM1l6TTBaR1k1Tm1RMk56STVaVGt3TldGbU1HVmlOQQ','1','1','65','','4','0','1','1','','0',NULL,NULL,NULL,'0','pm_admin','0',NULL,NULL,'0',NULL);
+INSERT INTO `eh_launch_pad_items` (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`, `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`, `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`, `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`, `more_order`, `alias_icon_uri`) 
+VALUES((@eh_launch_pad_items_id:=@eh_launch_pad_items_id+1),'999992','0','0','0','/home','Bizs','审批','审批','cs://1/image/aW1hZ2UvTVRwak5qVTVZV1EyWkdVM1l6TTBaR1k1Tm1RMk56STVaVGt3TldGbU1HVmlOQQ','1','1','65','','4','0','1','1','','0',NULL,NULL,NULL,'0','park_tourist','0',NULL,NULL,'0',NULL);
+
+
+-- 给科兴加打卡考勤审批工作流
+
+SET @id = (SELECT MAX(id) FROM eh_web_menu_scopes);
+INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES((@id:=@id+1),'50634','','EhNamespaces','999992','2');
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`) 
+VALUES('50634','审批工作流设置','50630',NULL,'react:/working-flow/flow-list/punch/50600','0','2','/50000/50600/50650/50634','park','557','50600');
+
+SET @web_menu_privilege_id = (SELECT MAX(id) FROM `eh_web_menu_privileges`);
+INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`) 
+VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), 10039, 50634, '考勤管理', 1, 1, '考勤管理 全部权限', 710); 
+
+
+-- 打卡审批工作流的title，add by wh, 20170502
+SET @id =(SELECT MAX(id) FROM eh_locale_strings); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow.title','1','zh_CN','请假申请'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow.title','2','zh_CN','异常申请'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow.title','3','zh_CN','加班申请');
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','creator','zh_CN','发起人'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','contact','zh_CN','联系电话'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','requestDate','zh_CN','申请日期'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','overtimeLength','zh_CN','加班时长'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','punchDetail','zh_CN','打卡详情'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','requestReson','zh_CN','申请理由'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','category','zh_CN','请假类型'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','absentTime','zh_CN','请假时间'); 
+INSERT INTO `eh_locale_strings` (`id`,`scope`,`code`,`locale`,`text`)VALUES ((@id:=@id+1),'approval.flow','absentLength','zh_CN','请假时长'); 
+
+
+-- 打卡审批工作流的内容，add by wh, 20170502
+SET @id =(SELECT MAX(id) FROM eh_locale_templates); 
+INSERT INTO `eh_locale_templates` (`id`,`scope`,`code`,`locale`,`description`,`text`,`namespace_id`)VALUES ((@id:=@id+1),'approval.flow.context','1','zh_CN','请假申请的内容','请假类型：${absentCategory}\n请假时间${beginTime}至${endTime}',0); 
+INSERT INTO `eh_locale_templates` (`id`,`scope`,`code`,`locale`,`description`,`text`,`namespace_id`)VALUES ((@id:=@id+1),'approval.flow.context','2','zh_CN','异常申请的内容','异常日期：${exceptionDate}\n打卡详情：${punchDetail}',0); 
+INSERT INTO `eh_locale_templates` (`id`,`scope`,`code`,`locale`,`description`,`text`,`namespace_id`)VALUES ((@id:=@id+1),'approval.flow.context','3','zh_CN','加班申请的内容','加班日期：${overTimeDate}\n加班时长：${timeLength}',0); 
+INSERT INTO `eh_locale_templates` (`id`,`scope`,`code`,`locale`,`description`,`text`,`namespace_id`)VALUES ((@id:=@id+1),'approval.flow','1','zh_CN','请假申请的时间','${beginTime}至${endTime}',0); 
+
--- db/4.6.1-delta-schema.sql
+++ db/4.6.1-delta-schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE `eh_talents` (
   `operator_uid` BIGINT,
 
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- 人才分类表，add by tt, 20170511
 -- DROP TABLE IF EXISTS `eh_talent_categories`;
@@ -41,7 +41,7 @@ CREATE TABLE `eh_talent_categories` (
   `operator_uid` BIGINT,
 
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- 查询历史记录表，add by tt, 20170511
 -- DROP TABLE IF EXISTS `eh_talent_query_histories`;
@@ -58,7 +58,7 @@ CREATE TABLE `eh_talent_query_histories` (
   `operator_uid` BIGINT,
 
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- 添加是否设置了管理员标记，add by tt, 20170522
 ALTER TABLE `eh_organizations` ADD COLUMN `set_admin_flag` TINYINT DEFAULT 0;
@@ -69,3 +69,217 @@ ALTER TABLE `eh_organization_community_requests` ADD INDEX `community_id` (`comm
 -- 服务联盟	增加排序和是否显示在app端的字段 by dengs, 20170523
 ALTER TABLE `eh_service_alliances` ADD COLUMN `display_flag` TINYINT NOT NULL DEFAULT '1' COMMENT '0:hide,1:display';
 ALTER TABLE `eh_service_alliances` CHANGE COLUMN `default_order` `default_order` BIGINT COMMENT 'default value is id';
+
+ 
+-- 自寄服务地址表，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_service_addresses`;
+CREATE TABLE `eh_express_service_addresses` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'community',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `name` VARCHAR(128) COMMENT 'the name of express service address',
+  `status` TINYINT(4) NOT NULL COMMENT '0. inactive, 1. waiting for approval, 2. active',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `update_time` DATETIME,
+  `operator_uid` BIGINT,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 快递公司表，左邻配一套全局的，各园区在此选择，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_companies`;
+CREATE TABLE `eh_express_companies` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'community',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `parent_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'parent id, the id of express company under zuolin',
+  `name` VARCHAR(128) COMMENT 'the name of express company name',
+  `logo` VARCHAR(512),
+  `status` TINYINT(4) NOT NULL COMMENT '0. inactive, 1. waiting for approval, 2. active',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `update_time` DATETIME,
+  `operator_uid` BIGINT,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 快递员表，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_users`;
+CREATE TABLE `eh_express_users` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'community',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `service_address_id` BIGINT,
+  `express_company_id` BIGINT,
+  `organization_id` BIGINT COMMENT 'the id of organization',
+  `organization_member_id` BIGINT COMMENT 'the id of organization member',
+  `user_id` BIGINT,
+  `status` TINYINT(4) NOT NULL COMMENT '0. inactive, 1. waiting for approval, 2. active',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `update_time` DATETIME,
+  `operator_uid` BIGINT,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 快递地址表，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_addresses`;
+CREATE TABLE `eh_express_addresses` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'community',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `user_name` VARCHAR(128),
+  `organization_id` BIGINT,
+  `organization_name` VARCHAR(128),
+  `phone` VARCHAR(16),
+  `province_id` BIGINT,
+  `city_id` BIGINT,
+  `county_id` BIGINT,
+  `province` VARCHAR(64),
+  `city` VARCHAR(64),
+  `county` VARCHAR(64),
+  `detail_address` VARCHAR(512),
+  `default_flag` TINYINT COMMENT '0. false, 1 true',
+  `category` TINYINT COMMENT '1. send address, 2. receive address',
+  `status` TINYINT NOT NULL COMMENT '0. inactive, 1. waiting for approval, 2. active',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `update_time` DATETIME,
+  `operator_uid` BIGINT,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 快递订单表，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_orders`;
+CREATE TABLE `eh_express_orders` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'community',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `order_no` VARCHAR(64) COMMENT 'order number',
+  `bill_no` VARCHAR(64) COMMENT 'bill number',
+  `send_name` VARCHAR(128),
+  `send_phone` VARCHAR(16),
+  `send_organization` VARCHAR(128),
+  `send_province` VARCHAR(64),
+  `send_city` VARCHAR(64),
+  `send_county` VARCHAR(64),
+  `send_detail_address` VARCHAR(512),
+  `receive_name` VARCHAR(128),
+  `receive_phone` VARCHAR(16),
+  `receive_organization` VARCHAR(128),
+  `receive_province` VARCHAR(64),
+  `receive_city` VARCHAR(64),
+  `receive_county` VARCHAR(64),
+  `receive_detail_address` VARCHAR(512),
+  `service_address_id` BIGINT COMMENT 'service address id',
+  `express_company_id` BIGINT COMMENT 'express company id',
+  `send_type` TINYINT COMMENT '1. standard express',
+  `send_mode` TINYINT COMMENT '1. self send',
+  `pay_type` TINYINT COMMENT '1. cash',
+  `pay_summary` DECIMAL(10,2) COMMENT 'pay money',
+  `internal` VARCHAR(256) COMMENT 'internal things',
+  `insured_price` DECIMAL(10,2) COMMENT 'insured price',
+  `status` TINYINT NOT NULL COMMENT '1. waiting for pay, 2. paid, 3. printed, 4. cancelled',
+  `paid_flag` TINYINT COMMENT 'whether the user has pushed the pay button, 0. false, 1 true',
+  `print_time` DATETIME,
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `update_time` DATETIME,
+  `operator_uid` BIGINT,
+
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `order_no` (`order_no`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 快递查询历史表，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_query_histories`;
+CREATE TABLE `eh_express_query_histories` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `express_company_id` BIGINT COMMENT 'express company id',
+  `bill_no` VARCHAR(64) COMMENT 'bill number',
+  `status` TINYINT(4) NOT NULL COMMENT '0. inactive, 1. waiting for approval, 2. active',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `update_time` DATETIME,
+  `operator_uid` BIGINT,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 快递订单日志表，add by tt, 20170413
+-- DROP TABLE IF EXISTS `eh_express_order_logs`;
+CREATE TABLE `eh_express_order_logs` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'community',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `order_id` BIGINT,
+  `action` VARCHAR(64),
+  `remark` VARCHAR(512),
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- add by dengs 20170511 添加服务联盟描述信息折叠高度
+ALTER TABLE `eh_service_alliances` ADD COLUMN `description_height` INT NULL DEFAULT '2' COMMENT '0:not collapse , N: collapse N lines';
+
+
+-- merge from activity-3.0.0 20170522  start  
+-- 活动增加支付相关字段 add by yanjun 20170502
+ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_flag` TINYINT(4) DEFAULT '0' NULL COMMENT '0: no pay, 1:have pay, 2:refund';
+ALTER TABLE `eh_activity_roster` ADD COLUMN `order_no` BIGINT(20) NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `order_start_time` DATETIME NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `order_expire_time` DATETIME NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `vendor_type` VARCHAR(32) NULL COMMENT '10001: alipay, 10002: wechatpay';
+ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_amount` DECIMAL(10, 2) NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `pay_time` DATETIME NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `refund_order_no` BIGINT(20) NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `refund_amount` DECIMAL(10, 2) NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `refund_time` DATETIME NULL;
+ALTER TABLE `eh_activity_roster` ADD COLUMN `status` TINYINT(4) DEFAULT '2' NULL COMMENT '0: cancel, 1: reject, 2:normal';
+ALTER TABLE `eh_activity_roster` ADD COLUMN `organization_id` BIGINT(20) NULL;
+
+ALTER TABLE `eh_activities` ADD COLUMN `charge_flag` TINYINT(4) DEFAULT '0' NULL COMMENT '0: no charge, 1: charge';
+ALTER TABLE `eh_activities` ADD COLUMN `charge_price` DECIMAL(10, 2) NULL COMMENT 'charge_price';
+
+
+-- 订单过期时间的设置表  add by yanjun 20170502
+CREATE TABLE `eh_roster_order_settings` (
+  `id` BIGINT(20) NOT NULL,
+  `namespace_id` INT(11) NOT NULL COMMENT 'namespace id',
+  `time` BIGINT(20) DEFAULT NULL COMMENT 'millisecond',
+  `create_time` DATETIME DEFAULT NULL,
+  `creator_uid` BIGINT(20) DEFAULT NULL,
+  `update_time` DATETIME DEFAULT NULL,
+  `operator_uid` BIGINT(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+-- 增加取消时间，用于统计  add by yanjun 20170519
+ALTER TABLE `eh_activity_roster` ADD COLUMN `cancel_time` DATETIME NULL;
+
+-- merge from activity-3.0.0 20170522  end
+
+-- 给工作流评价项增加允许输入评价内容flag字段  add by xq.tian  2017/05/12
+ALTER TABLE `eh_flow_evaluate_items` ADD COLUMN `input_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: false, 1: true, input evaluate content flag';
+
+-- 给工作流评价增加评价内容字段  add by xq.tian  2017/05/12
+ALTER TABLE `eh_flow_evaluates` ADD COLUMN `content` VARCHAR(1024) DEFAULT NULL COMMENT 'evaluate content';
+
+-- 给工作流按钮增加文字及图片是否必填字段  add by xq.tian  2017/05/12
+ALTER TABLE `eh_flow_buttons` ADD COLUMN `subject_required_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: false, 1: true, subject required flag';
+
+-- 给flowCase增加公司字段，以便在普通公司下按照公司进行搜索  add by xq.tian  2017/05/12
+ALTER TABLE `eh_flow_cases` ADD COLUMN `organization_id` BIGINT DEFAULT NULL COMMENT 'the same as eh_flows organization_id';
