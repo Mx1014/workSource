1495603efb18cc861a2eedabc0676bd04e118497
dengs <shuang.deng@zuolin.com>
2017-06-15 19:25:20 +0800

    print1.0 schema

---
+++ db/print1.0-delta-data-release.sql
@@ -0,0 +1,2 @@
+-- by dengs,司印服务器ip地址 20170615
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`) VALUES ('print.siyin.serverUrl', 'http://siyin.zuolin.com:8119', '司印服务器ip地址', '0', NULL);
