15d11e1ae0340b0f3f11e96d7adbb49b2d35b1bd
sunwen <wen.sun@zuolin.com>
2017-06-16 13:55:55 +0800

    rent

--- db/rent-delta-schema.sql
+++ db/rent-delta-schema.sql
@@ -3,6 +3,7 @@ ALTER TABLE `eh_lease_configs` ADD COLUMN `display_order_str` VARCHAR(128);
 
 
 ALTER TABLE `eh_buildings` ADD COLUMN `general_form_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'id of eh_general_form';
+ALTER TABLE `eh_buildings` ADD COLUMN `form_version` BIGINT NOT NULL DEFAULT 0 COMMENT 'id of eh_general_form';
 ALTER TABLE `eh_buildings` ADD COLUMN `custom_form_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: not add custom field, 1: add custom field';
 ALTER TABLE `eh_buildings` ADD COLUMN `default_order` BIGINT NOT NULL;
 ALTER TABLE `eh_buildings` ADD COLUMN `manager_name` VARCHAR(128);
@@ -13,9 +14,9 @@ ALTER TABLE `eh_lease_promotions` ADD COLUMN `latitude` DOUBLE DEFAULT NULL;
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `address` VARCHAR(512);
 
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `general_form_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'id of eh_general_form';
+ALTER TABLE `eh_lease_promotions` ADD COLUMN `form_version` BIGINT NOT NULL DEFAULT 0 COMMENT 'id of eh_general_form';
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `custom_form_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: not add custom field, 1: add custom field';
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `default_order` BIGINT NOT NULL DEFAULT 0;
-
 
 CREATE TABLE `eh_lease_form_requests` (
 	`id` BIGINT NOT NULL COMMENT 'id of the record',
