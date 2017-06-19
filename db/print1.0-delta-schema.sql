91a550c74787b3f29e1ca28df5c40fd82e0eb8f5
dengs <shuang.deng@zuolin.com>
2017-06-19 11:39:15 +0800

    print1.0 print schema

--- db/print1.0-delta-schema.sql
+++ db/print1.0-delta-schema.sql
@@ -1,6 +1,6 @@
 -- 打印设置表 , add by dengs, 20170615
--- DROP TABLE IF EXISTS `eh_print_settings`;
-CREATE TABLE `eh_print_settings` (
+-- DROP TABLE IF EXISTS `eh_siyin_print_settings`;
+CREATE TABLE `eh_siyin_print_settings` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
   `owner_type` VARCHAR(64) NOT NULL COMMENT 'community',
@@ -23,8 +23,8 @@ CREATE TABLE `eh_print_settings` (
 
 
 -- 打印机表 , add by dengs, 20170615
--- DROP TABLE IF EXISTS `eh_print_printers`;
-CREATE TABLE `eh_print_printers` (
+-- DROP TABLE IF EXISTS `eh_siyin_print_printers`;
+CREATE TABLE `eh_siyin_print_printers` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
   `owner_type` VARCHAR(64) NOT NULL COMMENT 'community',
@@ -44,18 +44,18 @@ CREATE TABLE `eh_print_printers` (
 
 
 -- 打印订单表 , add by dengs, 20170615
--- DROP TABLE IF EXISTS `eh_print_orders`;
-CREATE TABLE `eh_print_orders` (
+-- DROP TABLE IF EXISTS `eh_siyin_print_orders`;
+CREATE TABLE `eh_siyin_print_orders` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
   `owner_type` VARCHAR(64) NOT NULL COMMENT 'community',
   `owner_id` BIGINT NOT NULL DEFAULT 0,
-  `job_type` VARCHAR(128) COMMENT '',
+  `order_no` BIGINT COMMENT 'order number',
+  `job_type` VARCHAR(128) COMMENT 'siyin returned,PRINT(1),COPY(2),SCAN(3)',
   `print_document_name` VARCHAR(256) COMMENT 'print document name',
   `detail` TEXT COMMENT 'print/copy/scan details',
   `email` VARCHAR(128) COMMENT '',
-  `order_total_fee` DECIMAL(10,2) COMMENT 'order price',
-  `order_no` BIGINT COMMENT 'order number',
+  `order_total_amount` DECIMAL(10,2) COMMENT 'order price',
   `order_type` VARCHAR(128) COMMENT 'order type: print(1)',
   `order_body` VARCHAR(128) COMMENT 'order body: print(1)',
   `order_subject` VARCHAR(256) COMMENT 'print order',
@@ -71,8 +71,8 @@ CREATE TABLE `eh_print_orders` (
 
 
 -- 打印记录表 , add by dengs, 20170615
--- DROP TABLE IF EXISTS `eh_print_records`;
-CREATE TABLE `eh_print_records` (
+-- DROP TABLE IF EXISTS `eh_siyin_print_records`;
+CREATE TABLE `eh_siyin_print_records` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
   `owner_type` VARCHAR(64) NOT NULL COMMENT 'community',
@@ -87,10 +87,10 @@ CREATE TABLE `eh_print_records` (
   `client_mac` VARCHAR(128) COMMENT 'siyin returned, documents sended source commputer mac',
   `driver_name` VARCHAR(128) COMMENT 'siyin returned, driver name',
   `job_type` VARCHAR(128) COMMENT 'siyin returned,PRINT(1),COPY(2),SCAN(3)',
-  `job_in_time` VARCHAR(128) COMMENT 'siyin returned, job start time',
-  `job_out_time` VARCHAR(128) COMMENT 'siyin returned, job end time',
-  `document_name` VARCHAR(128) COMMENT 'siyin returned, print document name',
-  `printer_name` VARCHAR(128) COMMENT 'siyin returned, printer name',
+  `start_time` VARCHAR(128) COMMENT 'siyin returned, job start time',
+  `end_time` VARCHAR(128) COMMENT 'siyin returned, job end time',
+  `document_name` VARCHAR(512) COMMENT 'siyin returned, print document name',
+  `printer_name` VARCHAR(256) COMMENT 'siyin returned, printer name',
   `paper_size` TINYINT COMMENT 'siyin returned, paper size, A3(3),A4(4),A5(5),A6(6)',
   `duplex` TINYINT COMMENT 'siyin returned, 1,one surface,2:double surface',
   `copy_count` INT COMMENT 'siyin returned, copy count',
@@ -100,7 +100,7 @@ CREATE TABLE `eh_print_records` (
   `page_count` INT COMMENT '',
   `color_page_count` INT COMMENT '',
   `mono_page_count` INT COMMENT '',
-  `status` VARCHAR(128) COMMENT '0:INACTIVE,2:ACTIVE',
+  `status` TINYINT COMMENT '0:INACTIVE,2:ACTIVE',
   `creator_uid` BIGINT COMMENT 'creator/initiator id',
   `create_time` DATETIME,
   `operator_uid` BIGINT,
@@ -110,8 +110,8 @@ CREATE TABLE `eh_print_records` (
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- 打印用户邮箱表,可以考虑加到eh_users或者eh_user_identifiers表，但是数据太多，连表查询效率低 , add by dengs, 20170615
--- DROP TABLE IF EXISTS `eh_print_emails`;
-CREATE TABLE `eh_print_emails` (
+-- DROP TABLE IF EXISTS `eh_siyin_print_emails`;
+CREATE TABLE `eh_siyin_print_emails` (
   `id` BIGINT NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
   `owner_type` VARCHAR(64) NOT NULL COMMENT 'community',
