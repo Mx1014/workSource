6ca89df089f508fe58bf3ac0e1e360bc7fc24fd1
yanshaofan <sf.yan@zuolin.com>
2017-06-21 14:06:12 +0800

    sql

--- db/portal-admin-delta-schema.sql
+++ db/portal-admin-delta-schema.sql
@@ -1,5 +1,5 @@
 
-ALTER TABLE `eh_service_modules` ADD `action_data` text;
+ALTER TABLE `eh_service_modules` ADD `instance_config` text;
 ALTER TABLE `eh_service_modules` ADD `update_time` datetime DEFAULT NULL;
 ALTER TABLE `eh_service_modules` ADD `operator_uid` bigint(20) NOT NULL;
 ALTER TABLE `eh_service_modules` ADD `creator_uid` bigint(20) NOT NULL;
@@ -16,7 +16,7 @@ CREATE TABLE `eh_service_module_apps` (
   `namespace_id` int(11) NOT NULL DEFAULT '0',
   `name` varchar(64) DEFAULT NULL,
   `module_id` bigint(20) NOT NULL,
-  `data` text COMMENT '应用入口需要的配置参数',
+  `instance_config` text COMMENT '应用入口需要的配置参数',
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
@@ -32,8 +32,8 @@ CREATE TABLE `eh_portal_home_pages` (
   `namespace_id` int(11) NOT NULL DEFAULT '0',
   `label` varchar(64) DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL,
-  `type` tinyint(4) COMMENT '门户、模块应用、我 是那种类型',
-  `data` text ,
+  `type` tinyint(4) NOT NULL,
+  `instance_config` text ,
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
@@ -65,12 +65,12 @@ CREATE TABLE `eh_portal_item_groups` (
   `namespace_id` int(11) NOT NULL DEFAULT '0',
   `label` varchar(64) DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL  COMMENT 'item_group_${id}，对应eh_launch_pad_layouts里面的layout_json里面item_group的 groups[x].instanceConfig.itemGroup 和 eh_launch_pad_items里的item_group',
-  `separatorFlag` varchar(64) DEFAULT NULL,
-  `separatorHeight` varchar(64) DEFAULT NULL,
+  `separatorFlag` tinyint(4) DEFAULT 0,
+  `separatorHeight` decimal(10,2) DEFAULT NULL,
   `widget` varchar(64) DEFAULT NULL,
   `style` varchar(64) DEFAULT NULL,
-  `instanceConfig` text COMMENT '参数配置',
-  `defaultOrder` bigint(20) NOT NULL,
+  `instance_config` text COMMENT '参数配置',
+  `defaultOrder` int(11) NOT NULL DEFAULT '0',
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
@@ -84,7 +84,7 @@ CREATE TABLE `eh_portal_item_groups` (
 CREATE TABLE `eh_portal_items` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` int(11) NOT NULL DEFAULT '0',
-  `label` varchar(64) DEFAULT NULL
+  `label` varchar(64) DEFAULT NULL,
   `item_location` varchar(2048) DEFAULT NULL COMMENT '/eh_portal_item_groups  name 对应eh_launch_pad_items里的item_location',
   `group_name` varchar(64) DEFAULT NULL  COMMENT 'eh_portal_item_groups  name 对应eh_launch_pad_layouts里面的layout_json里面item_group的 groups[x].instanceConfig.itemGroup 和 eh_launch_pad_items里的item_group',
   `name` varchar(64) DEFAULT NULL  COMMENT 'item_${id}',
@@ -128,7 +128,7 @@ CREATE TABLE `eh_portal_layout_templates` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` int(11) NOT NULL DEFAULT '0',
   `label` varchar(64) DEFAULT NULL,
-  `item_group_json` text COMMENT '模板json',
+  `template_json` text COMMENT '模板json',
   `show_uri` varchar(64),
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '',
   `create_time` datetime DEFAULT NULL,
