30d9b47e77f9c4dce686df32b78ef04829f8aca0
sunwen <wen.sun@zuolin.com>
2017-06-28 16:30:17 +0800

    parking

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -1,6 +1,7 @@
 
 ALTER TABLE eh_parking_recharge_orders ADD COLUMN `parking_time` INT DEFAULT NULL COMMENT 'parking-time';
 ALTER TABLE eh_parking_recharge_orders ADD COLUMN `error_description` TEXT DEFAULT NULL COMMENT 'error description';
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN `refund_time` datetime DEFAULT NULL COMMENT 'refund time';
 
 ALTER TABLE eh_parking_lots ADD COLUMN `contact` VARCHAR(128) DEFAULT NULL COMMENT 'service contact';
 
