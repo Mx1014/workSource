3d15d7e467412b90606283be60757ce1ba0d834c
dengs <shuang.deng@zuolin.com>
2017-06-29 17:22:45 +0800

    print1.0 modify

--- ehcore/src/main/java/com/everhomes/print/SiyinPrintServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/print/SiyinPrintServiceImpl.java
@@ -314,7 +314,7 @@ public class SiyinPrintServiceImpl implements SiyinPrintService {
         valueOperations.set(key, identifierToken, timeout, unit);
         
         //将通知登陆的接口返回给前端
-        String logonURL = configurationProvider.getValue(PrintErrorCode.PRINT_INFORM_URL, "") + identifierToken;
+        String logonURL = configurationProvider.getValue(PrintErrorCode.PRINT_INFORM_URL, "");
         GetPrintLogonUrlResponse response = new GetPrintLogonUrlResponse();
         response.setLogonURL(logonURL);
         response.setIdentifierToken(identifierToken);
