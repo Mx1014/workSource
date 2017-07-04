b92eb5dbc90b63f878847fc5f52410172ee0624d
wuhan <wuhan419@gmail.com>
2017-07-04 16:45:46 +0800

    add some column to employee val

--- ehrest/src/main/java/com/everhomes/rest/salary/SalaryPeriodEmployeeEntityDTO.java
+++ ehrest/src/main/java/com/everhomes/rest/salary/SalaryPeriodEmployeeEntityDTO.java
@@ -8,6 +8,11 @@ package com.everhomes.rest.salary;
  * <li>salaryValue: 字段项值</li>
  * <li>isFormula: 是否为公式 0-否 1-是</li>
  * <li>entityType: entity类型 0-文本 1-数值</li>
+ * <li>editableFlag: 是否可编辑(对文本类):0-否   1-是</li>
+ * <li>numberType: 数值类型:0-普通数值 1-计算公式</li>
+ * <li>needCheck: 是否需要核算:0-否 1-是</li>
+ * <li>defaultOrder: 排序</li>
+ * <li>visibleFlag: 是否展示到工资条:0-否 1-是</li>
  * </ul>
  * */
 public class SalaryPeriodEmployeeEntityDTO {
@@ -18,6 +23,60 @@ public class SalaryPeriodEmployeeEntityDTO {
     private String salaryValue;
 	private Byte isFormula;
 	private Byte entityType;
+	private Byte editableFlag;
+	private Byte numberType;
+	private Byte needCheck;
+	private Integer defaultOrder;
+	private Byte visibleFlag;
+
+	public Integer getDefaultOrder() {
+		return defaultOrder;
+	}
+
+	public void setDefaultOrder(Integer defaultOrder) {
+		this.defaultOrder = defaultOrder;
+	}
+
+	public Byte getEditableFlag() {
+		return editableFlag;
+	}
+
+	public void setEditableFlag(Byte editableFlag) {
+		this.editableFlag = editableFlag;
+	}
+
+	public String getGroupEntityName() {
+		return groupEntityName;
+	}
+
+	public void setGroupEntityName(String groupEntityName) {
+		this.groupEntityName = groupEntityName;
+	}
+
+	public Byte getNeedCheck() {
+		return needCheck;
+	}
+
+	public void setNeedCheck(Byte needCheck) {
+		this.needCheck = needCheck;
+	}
+
+	public Byte getNumberType() {
+		return numberType;
+	}
+
+	public void setNumberType(Byte numberType) {
+		this.numberType = numberType;
+	}
+
+	public Byte getVisibleFlag() {
+		return visibleFlag;
+	}
+
+	public void setVisibleFlag(Byte visibleFlag) {
+		this.visibleFlag = visibleFlag;
+	}
+
 	public Long getSalaryEmployeeId() {
 		return salaryEmployeeId;
 	}
