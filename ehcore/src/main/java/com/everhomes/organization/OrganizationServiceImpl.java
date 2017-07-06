f56ed1fb3140d3f959356619ad614dc8b0628afe
xq.tian <xq.tian@zuolin.com>
2017-07-06 17:33:56 +0800

    fix message

--- ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
@@ -4950,7 +4950,8 @@ public class OrganizationServiceImpl implements OrganizationService {
             member.setApplyDescription(cmd.getContactDescription());
 
             /**创建企业级的member/detail/user_organiztion记录**/
-            createOrganiztionMemberWithDetailAndUserOrganization(member, cmd.getOrganizationId());
+            OrganizationMember tempMember = createOrganiztionMemberWithDetailAndUserOrganization(member, cmd.getOrganizationId());
+            member.setId(tempMember.getId());
 
             return member;
         });
@@ -12260,7 +12261,6 @@ public class OrganizationServiceImpl implements OrganizationService {
             organizationMember.setOrganizationId(hiddenDirectId);
             organizationMember.setDetailId(new_detail_id);
             organizationProvider.createOrganizationMember(organizationMember);
-            _organizationMember.setId(organizationMember.getId());// add by xq.tian 2017/07/05
         }
         return organizationMember;// add by xq.tian 2017/07/05
     }
