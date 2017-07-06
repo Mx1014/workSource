f36de648ad0f9e5877f74feb5d3adf4e3143cf9e
ryan <nan.rong@zuolin.com>
2017-07-06 17:12:06 +0800

    getSalaryGroup interface fixed

--- ehnextgen-api/src/main/java/com/everhomes/salary/SalaryGroupEntity.java
+++ ehnextgen-api/src/main/java/com/everhomes/salary/SalaryGroupEntity.java
@@ -8,12 +8,25 @@ public class SalaryGroupEntity extends EhSalaryGroupEntities {
 	
 	private static final long serialVersionUID = -4759610341287325926L;
 
-	public SalaryGroupEntity(){
+	private Byte defaultFlag;
 
+	public SalaryGroupEntity(){
 	}
 
 	public SalaryGroupEntity(String name){
 		this.setName(name);
+	}
+
+	public Byte getDefaultFlag() {
+		return defaultFlag;
+	}
+
+	public void setDefaultFlag(Byte defaultFlag) {
+		this.defaultFlag = defaultFlag;
+	}
+
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
 	}
 
 	@Override
