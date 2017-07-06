b3388d3aca558ccdd366ce4cfa6e6de8c91797ea
yanshaofan <sf.yan@zuolin.com>
2017-07-06 21:44:43 +0800

    bug

--- ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationServiceImpl.java
@@ -6624,22 +6624,23 @@ public class OrganizationServiceImpl implements OrganizationService {
             member.setTargetType(OrganizationMemberTargetType.USER.getCode());
             organizationProvider.updateOrganizationMember(member);
 
-            OrganizationMemberDetails detail = organizationProvider.findOrganizationMemberDetailsByDetailId(member.getDetailId());
-            detail.setTargetId(member.getTargetId());
-            detail.setTargetType(member.getTargetType());
-            organizationProvider.updateOrganizationMemberDetails(detail,detail.getId());
+            if(null != member.getDetailId()){
+                OrganizationMemberDetails detail = organizationProvider.findOrganizationMemberDetailsByDetailId(member.getDetailId());
+                detail.setTargetId(member.getTargetId());
+                detail.setTargetType(member.getTargetType());
+                organizationProvider.updateOrganizationMemberDetails(detail,detail.getId());
 
-            User user = userProvider.findUserById(member.getTargetId());
-            user.setNickName(detail.getContactName());
-            if (StringUtils.isEmpty(user.getAvatar())) {
-                user.setAvatar(detail.getAvatar());
+                User user = userProvider.findUserById(member.getTargetId());
+                user.setNickName(detail.getContactName());
+                if (StringUtils.isEmpty(user.getAvatar())) {
+                    user.setAvatar(detail.getAvatar());
+                }
+                if (StringUtils.isEmpty(user.getAvatar())) {
+                    user.setAvatar(configurationProvider.getValue(UserContext.getCurrentNamespaceId(), "user.avatar.undisclosured.url", ""));
+                }
+
+                userProvider.updateUser(user);
             }
-
-            if (StringUtils.isEmpty(user.getAvatar())) {
-                user.setAvatar(configurationProvider.getValue(UserContext.getCurrentNamespaceId(), "user.avatar.undisclosured.url", ""));
-            }
-
-            userProvider.updateUser(user);
             return null;
         });
     }
