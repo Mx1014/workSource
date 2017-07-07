4220b209f10da745813795e55acb9abd171a4b4e
xq.tian <xq.tian@zuolin.com>
2017-07-07 17:48:38 +0800

    join identifier token

--- ehcore/src/main/java/com/everhomes/community/CommunityServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/community/CommunityServiceImpl.java
@@ -1459,6 +1459,8 @@ public class CommunityServiceImpl implements CommunityService {
                     String keyword = "%" + cmd.getUserInfoKeyword() + "%";
                     query.addJoin(Tables.EH_USERS, JoinType.JOIN, Tables.EH_GROUP_MEMBERS.MEMBER_ID.eq(Tables.EH_USERS.ID));
                     query.addConditions(Tables.EH_USERS.NICK_NAME.like(keyword));
+                    query.addJoin(Tables.EH_USER_IDENTIFIERS, JoinType.JOIN, Tables.EH_USER_IDENTIFIERS.OWNER_UID.eq(Tables.EH_USERS.ID));
+                    query.addConditions(Tables.EH_USER_IDENTIFIERS.IDENTIFIER_TOKEN.like(keyword));
                 }
                 if (StringUtils.isNotBlank(cmd.getCommunityKeyword())) {
                     String keyword = "%" + cmd.getCommunityKeyword() + "%";
