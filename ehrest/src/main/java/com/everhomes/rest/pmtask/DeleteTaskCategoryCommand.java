441423b2f3913c152b70ce2f986db03d3dd04a76
ying.xiong <ying.xiong@zuolin.com>
2017-07-11 14:04:51 +0800

    add privilege in pmtask

--- ehrest/src/main/java/com/everhomes/rest/pmtask/DeleteTaskCategoryCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/pmtask/DeleteTaskCategoryCommand.java
@@ -6,11 +6,15 @@ import com.everhomes.util.StringHelper;
  * <ul>
  * <li>namspaceId: 域空间</li>
  * <li>id: 类型id</li>
+ * <li>parentId: 父类型ID</li>
+ * <li>currentOrgId: 当前机构id</li>
  * </ul>
  */
 public class DeleteTaskCategoryCommand {
 	private Long id;
+	private Long parentId;
     private Integer namespaceId;
+	private  Long currentOrgId;
 	public Long getId() {
 		return id;
 	}
@@ -23,7 +27,23 @@ public class DeleteTaskCategoryCommand {
 	public void setNamespaceId(Integer namespaceId) {
 		this.namespaceId = namespaceId;
 	}
-    
+
+	public Long getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+
+	public Long getCurrentOrgId() {
+		return currentOrgId;
+	}
+
+	public void setCurrentOrgId(Long currentOrgId) {
+		this.currentOrgId = currentOrgId;
+	}
+
 	@Override
 	public String toString() {
 		return StringHelper.toJsonString(this);
