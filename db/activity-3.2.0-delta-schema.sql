e44353a0745fb14072bccd1742609933c720547b
yanjun <jun.yan@zuolin.com>
2017-07-13 13:12:12 +0800

    fix refund

--- db/activity-3.2.0-delta-schema.sql
+++ db/activity-3.2.0-delta-schema.sql
@@ -2,3 +2,6 @@
 -- 增加活动是否支持微信报名标识  add by yanjun 20170627
 ALTER TABLE `eh_roster_order_settings`  ADD COLUMN `wechat_signup` TINYINT(4) DEFAULT '0' NULL COMMENT 'is support wechat signup 0:no, 1:yes';
 ALTER TABLE `eh_activities` ADD COLUMN `wechat_signup` TINYINT(4) DEFAULT '0' NULL COMMENT 'is support wechat signup 0:no, 1:yes';
+
+-- 增加订单类型  add by yanjun 20170713
+ALTER TABLE `eh_activity_roster` ADD COLUMN `orderType` VARCHAR(128) NULL COMMENT 'orderType';
