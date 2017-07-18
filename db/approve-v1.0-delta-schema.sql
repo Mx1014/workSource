f11ebf4842d0bc4766deb9dbb2170642db7fc425
siting <st.zheng@zuolin.com>
2017-07-18 14:03:38 +0800

    创建表

---
+++ db/approve-v1.0-delta-schema.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE `eh_community_approve` (
+  `id` bigint(20) NOT NULL,
+  `namespace_id` int(11) NOT NULL,
+  `organization_id` bigint(20) NOT NULL,
+  `owner_id` bigint(20) NOT NULL,
+  `owner_type` varchar(64) NOT NULL,
+  `module_id` bigint(20) DEFAULT NULL,
+  `module_type` varchar(64) DEFAULT NULL,
+  `approve_name` varchar(64) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `form_id` bigint(20) DEFAULT NULL,
+  `flow_id` bigint(20) DEFAULT NULL,
+  `flow_version` int(11) DEFAULT '0',
+  `update_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
