e765942e1bdcc8926b3c346b29e0058b532b3cca
sunwen <wen.sun@zuolin.com>
2017-07-19 11:31:13 +0800

    parking

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -1,4 +1,4 @@
 
 ALTER TABLE eh_parking_clearance_logs ADD COLUMN `log_token` VARCHAR(1024) DEFAULT NULL COMMENT 'The info from third';
-
+ALTER TABLE eh_parking_clearance_logs ADD COLUMN `log_json` TEXT DEFAULT NULL COMMENT 'The info from third';
 
