3d7d309edcabe4a1ab013d93791fa9945a9fe91e
zhengsiting <siting1@leju.com>
2017-07-31 17:36:25 +0800

    fix

--- db/4.7.4-delta-data-release.sql
+++ db/4.7.4-delta-data-release.sql
@@ -1,9 +1,9 @@
--- 添加审批
+-- 添加审批 by zt.zheng
 INSERT INTO `ehcore`.`eh_community_approve` (`id`, `namespace_id`, `organization_id`, `owner_id`, `owner_type`, `module_id`, `module_type`, `project_id`,
  `approve_name`, `status`, `form_origin_id`, `form_version`, `update_time`, `create_time`) VALUES ('1', '999983', '1008900', '240111044331055940', 'community',
  '41600', 'EhCommunityApprove', '0', '审批测试', '2', '0', '0', '2017-07-31 08:52:50', '2017-01-19 20:29:29');
 
--- 添加菜单 -- 按照产品要求，添加菜单到左邻域
+-- 添加菜单 -- 按照产品要求，添加菜单到左邻域 by zt.zheng
 
 INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`,`category`)
 VALUES (41600, '园区审批', '40000', NULL, NULL, '1', '2', '/40000/41600', 'park', '499', 41600,'module');
@@ -18,12 +18,12 @@ INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, 
 VALUES (41630, '申请记录', '41600', NULL, 'react:/park-approval/apply-record/41600', '0', '2', '/40000/41600/41630', 'park', '502', NULL,'module');
 
 
--- 添加权限
+-- 添加权限 by zt.zheng
 set @eh_acl_privileges_id = (select max(id) from eh_acl_privileges);
 INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`)
 VALUES ((@eh_acl_privileges_id := @eh_acl_privileges_id+1), 0, '园区审批', '园区审批 全部权限', NULL);
 
--- 菜单对应的权限
+-- 菜单对应的权限 by zt.zheng
 SET @web_menu_privilege_id = (SELECT MAX(id) FROM `eh_web_menu_privileges`);
 INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `discription`, `sort_num`)
 VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), @eh_acl_privileges_id, 41400, '园区审批', 1, 1, '园区审批  全部权限', 499);
@@ -37,12 +37,12 @@ VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), @eh_acl_privileg
 INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `show_flag`, `status`, `申请记录`, `sort_num`)
 VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), @eh_acl_privileges_id, 41430, '申请记录', 1, 1, '打印价格  全部权限', 502);
 
--- 角色对应的菜单权限
+-- 角色对应的菜单权限 by zt.zheng
 SET @acl_id = (SELECT MAX(id) FROM `eh_acls`);
 INSERT INTO `eh_acls` (`id`, `namespace_id`, `owner_type`, `owner_id`, `grant_type`, `privilege_id`, `role_id`, `role_type`, `order_seq`, `creator_uid`, `create_time`)
 VALUES ((@acl_id := @acl_id + 1), 999983, 'EhOrganizations', NULL, 1, @eh_acl_privileges_id, 1001, 'EhAclRoles', 0, 1, NOW());
 
--- 菜单的范围 --0域不加scope
+-- 菜单的范围 --0域不加scope by zt.zheng
  SET @menu_scope_id = (SELECT MAX(id) FROM `eh_web_menu_scopes`);
  INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`)
  VALUES ((@menu_scope_id := @menu_scope_id + 1), 41600, '', 'EhNamespaces', 999983, 2);
@@ -53,23 +53,23 @@ VALUES ((@acl_id := @acl_id + 1), 999983, 'EhOrganizations', NULL, 1, @eh_acl_pr
  INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`)
  VALUES ((@menu_scope_id := @menu_scope_id + 1), 41630, '', 'EhNamespaces', 999983, 2);
 
--- 模块
+-- 模块 by zt.zheng
 INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`)
 VALUES (41600, '园区审批', 40000, '/40000/41600', 0, 2, 2, 0, UTC_TIMESTAMP());
 
--- 模块权限
+-- 模块权限 by zt.zheng
 SET @eh_service_module_privileges_id = (SELECT MAX(id) FROM `eh_service_module_privileges`);
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
 VALUES ((@eh_service_module_privileges_id := @eh_service_module_privileges_id + 1), 41600, 1, @eh_acl_privileges_id, NULL, '0', UTC_TIMESTAMP());
 
--- 模块权限范围
+-- 模块权限范围 by zt.zheng
 SET @eh_service_module_scopes_id = (SELECT MAX(id) FROM `eh_service_module_scopes`);
 INSERT INTO `eh_service_module_scopes` (`id`, `namespace_id`, `module_id`, `module_name`, `owner_type`, `owner_id`, `default_order`, `apply_policy`)
 VALUES ((@eh_service_module_scopes_id := @eh_service_module_scopes_id + 1), 999983, 41600, '园区审批', NULL, NULL, NULL, 2);
 
--- by dengs 添加菜单 end
 
--- 服务广场 -- 添加到左邻域
+
+-- 服务广场 -- 添加到左邻域 by zt.zheng
 
  INSERT INTO `eh_launch_pad_items`
 (`id`, `namespace_id`, `app_id`, `scope_code`, `scope_id`, `item_location`, `item_group`, `item_name`, `item_label`,
