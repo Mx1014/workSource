e17cf1d32862e9eca670b531188ffca264e0e01e
sunwen <wen.sun@zuolin.com>
2017-08-03 17:44:49 +0800

    rent

--- db/rent-delta-schema.sql
+++ db/rent-delta-schema.sql
@@ -11,11 +11,21 @@ ALTER TABLE eh_buildings DROP COLUMN fire_control_description;
 ALTER TABLE `eh_lease_promotion_attachments` ADD COLUMN `owner_type` VARCHAR(128) NOT NULL AFTER `id`;
 ALTER TABLE `eh_lease_promotion_attachments` CHANGE COLUMN `lease_id` `owner_id` BIGINT NOT NULL AFTER `id`;
 
+CREATE TABLE `eh_lease_promotion_communities` (
+  `id` bigint(20) NOT NULL COMMENT 'id of the record',
+  `lease_promotion_id` bigint(20) NOT NULL COMMENT 'lease promotion id',
+  `community_id` bigint(20) NOT NULL COMMENT 'community id',
+  `creator_uid` bigint(20) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `eh_lease_buildings` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` int(11) NOT NULL DEFAULT '0',
   `community_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'refering to eh_communities',
+  `building_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'refering to eh_buildings',
+
   `name` varchar(128) NOT NULL DEFAULT '' COMMENT 'building name',
   `alias_name` varchar(128) DEFAULT NULL,
   `manager_name` varchar(128) DEFAULT NULL,
