baaf6cabd10a9353820f71415f13215121e605f5
ying.xiong <ying.xiong@zuolin.com>
2017-08-03 16:15:00 +0800

    add field apis

---
+++ ehcore/src/main/java/com/everhomes/field/FieldController.java
@@ -0,0 +1,118 @@
+package com.everhomes.field;
+
+import com.everhomes.constants.ErrorCodes;
+import com.everhomes.controller.ControllerBase;
+import com.everhomes.discover.RestReturn;
+import com.everhomes.rest.RestResponse;
+import com.everhomes.rest.field.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * Created by ying.xiong on 2017/7/31.
+ */
+
+@RestController
+@RequestMapping("/field")
+public class FieldController extends ControllerBase {
+
+
+    /**
+     * <b>URL: /field/updateFields</b>
+     * <p>更新域空间模块字段</p>
+     * @return {@link String}
+     */
+    @RequestMapping("updateFields")
+    @RestReturn(value=String.class)
+    public RestResponse updateFields(@Valid UpdateFieldsCommand cmd) {
+        RestResponse res = new RestResponse();
+        res.setErrorCode(ErrorCodes.SUCCESS);
+        res.setErrorDescription("OK");
+        return res;
+    }
+
+    /**
+     * <b>URL: /field/listFields</b>
+     * <p>获取域空间模块字段</p>
+     * @return {@link FieldDTO}
+     */
+    @RequestMapping("listFields")
+    @RestReturn(value=FieldDTO.class, collection = true)
+    public RestResponse listFields(@Valid ListFieldCommand cmd) {
+        RestResponse res = new RestResponse();
+        res.setErrorCode(ErrorCodes.SUCCESS);
+        res.setErrorDescription("OK");
+        return res;
+    }
+
+    /**
+     * <b>URL: /field/updateFieldGroups</b>
+     * <p>更新域空间模块字段组</p>
+     * @return {@link String}
+     */
+    @RequestMapping("updateFieldGroups")
+    @RestReturn(value=String.class)
+    public RestResponse updateFieldGroups(@Valid UpdateFieldGroupsCommand cmd) {
+        RestResponse res = new RestResponse();
+        res.setErrorCode(ErrorCodes.SUCCESS);
+        res.setErrorDescription("OK");
+        return res;
+    }
+
+    /**
+     * <b>URL: /field/listFieldGroups</b>
+     * <p>获取域空间模块字段组</p>
+     * @return {@link FieldGroupDTO}
+     */
+    @RequestMapping("listFieldGroups")
+    @RestReturn(value=FieldGroupDTO.class, collection = true)
+    public RestResponse listFieldGroups(@Valid ListFieldGroupCommand cmd) {
+        RestResponse res = new RestResponse();
+        res.setErrorCode(ErrorCodes.SUCCESS);
+        res.setErrorDescription("OK");
+        return res;
+    }
+
+    /**
+     * <b>URL: /field/updateFieldItems</b>
+     * <p>更新域空间模块字段选择项</p>
+     * @return {@link String}
+     */
+    @RequestMapping("updateFieldItems")
+    @RestReturn(value=String.class)
+    public RestResponse updateFieldItems(@Valid UpdateFieldItemsCommand cmd) {
+        RestResponse res = new RestResponse();
+        res.setErrorCode(ErrorCodes.SUCCESS);
+        res.setErrorDescription("OK");
+        return res;
+    }
+
+    /**
+     * <b>URL: /field/listFieldItems</b>
+     * <p>获取域空间模块字段选择项</p>
+     * @return {@link FieldItemDTO}
+     */
+    @RequestMapping("listFieldItems")
+    @RestReturn(value=FieldItemDTO.class, collection = true)
+    public RestResponse listFieldItems(@Valid ListFieldItemCommand cmd) {
+        RestResponse res = new RestResponse();
+        res.setErrorCode(ErrorCodes.SUCCESS);
+        res.setErrorDescription("OK");
+        return res;
+    }
+
+    /**
+     * <b>URL: /field/listFieldStatistics</b>
+     * <p>列出特定字段统计信息</p>
+     */
+    @RequestMapping("listFieldStatistics")
+    @RestReturn(value = FieldStatisticDTO.class)
+    public RestResponse listFieldStatistics(@Valid ListFieldStatisticsCommand cmd) {
+        RestResponse response = new RestResponse();
+        response.setErrorCode(ErrorCodes.SUCCESS);
+        response.setErrorDescription("OK");
+        return response;
+    }
+}
---
+++ ehrest/src/main/java/com/everhomes/rest/field/FieldParamType.java
@@ -0,0 +1,47 @@
+package com.everhomes.rest.field;
+
+/**
+ * <ul>
+ *     <li>text: 单行文本框</li>
+ *     <li>multiText: 多行文本框</li>
+ *     <li>select: 单选列表</li>
+ *     <li>radio: 单选框</li>
+ *     <li>checkbox: 多选框</li>
+ *     <li>datetime: 时间</li>
+ *     <li>file: 文件</li>
+ *     <li>image: 图片</li>
+ * </ul>
+ * Created by ying.xiong on 2017/8/1.
+ */
+public enum FieldParamType {
+
+    TEXT("text"), MULTI_TEXT("multiText"), SELECT("select"), RADIO("radio"), CHECKBOX("checkbox"),DATETIME("datetime"),
+    FILE("file"), IMAGE("image");
+
+    private String name;
+
+    private FieldParamType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static FieldParamType fromName(String name) {
+        if(name != null) {
+            FieldParamType[] values = FieldParamType.values();
+            for(FieldParamType value : values) {
+                if(value.equals(name)) {
+                    return value;
+                }
+            }
+        }
+
+        return null;
+    }
+}
