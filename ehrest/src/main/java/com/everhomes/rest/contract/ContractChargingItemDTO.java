3f7528554d419dc41e7d0c868540577534e69d12
ying.xiong <ying.xiong@zuolin.com>
2017-08-03 11:57:53 +0800

    add dto

---
+++ ehrest/src/main/java/com/everhomes/rest/contract/ContractChargingItemDTO.java
@@ -0,0 +1,85 @@
+package com.everhomes.rest.contract;
+
+import com.everhomes.discover.ItemType;
+
+import java.util.List;
+
+/**
+ * <ul>
+ *     <li>chargingItemId: 收费项id</li>
+ *     <li>chargingStandardId: 收费标准id</li>
+ *     <li>lateFeeStandardId: 滞纳金标准id</li>
+ *     <li>chargingAmountValue: 收费金额参数</li>
+ *     <li>chargingStartTime: 起记日期</li>
+ *     <li>chargingExpiredTime: 截止日期</li>
+ *     <li>apartments: 计价条款适用资产列表</li>
+ * </ul>
+ * Created by ying.xiong on 2017/8/3.
+ */
+public class ContractChargingItemDTO {
+
+    private Long chargingItemId;
+    private Long chargingStandardId;
+    private Long lateFeeStandardId;
+    private Long chargingAmountValue;
+    private Long chargingStartTime;
+    private Long chargingExpiredTime;
+    @ItemType(BuildingApartmentDTO.class)
+    private List<BuildingApartmentDTO> apartments;
+
+    public List<BuildingApartmentDTO> getApartments() {
+        return apartments;
+    }
+
+    public void setApartments(List<BuildingApartmentDTO> apartments) {
+        this.apartments = apartments;
+    }
+
+    public Long getChargingAmountValue() {
+        return chargingAmountValue;
+    }
+
+    public void setChargingAmountValue(Long chargingAmountValue) {
+        this.chargingAmountValue = chargingAmountValue;
+    }
+
+    public Long getChargingExpiredTime() {
+        return chargingExpiredTime;
+    }
+
+    public void setChargingExpiredTime(Long chargingExpiredTime) {
+        this.chargingExpiredTime = chargingExpiredTime;
+    }
+
+    public Long getChargingItemId() {
+        return chargingItemId;
+    }
+
+    public void setChargingItemId(Long chargingItemId) {
+        this.chargingItemId = chargingItemId;
+    }
+
+    public Long getChargingStandardId() {
+        return chargingStandardId;
+    }
+
+    public void setChargingStandardId(Long chargingStandardId) {
+        this.chargingStandardId = chargingStandardId;
+    }
+
+    public Long getChargingStartTime() {
+        return chargingStartTime;
+    }
+
+    public void setChargingStartTime(Long chargingStartTime) {
+        this.chargingStartTime = chargingStartTime;
+    }
+
+    public Long getLateFeeStandardId() {
+        return lateFeeStandardId;
+    }
+
+    public void setLateFeeStandardId(Long lateFeeStandardId) {
+        this.lateFeeStandardId = lateFeeStandardId;
+    }
+}
