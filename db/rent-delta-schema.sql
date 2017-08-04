2c918d603a6eac16d114076dedb1beafa65206c9
sunwen <wen.sun@zuolin.com>
2017-08-04 15:37:21 +0800

    rent

--- db/rent-delta-schema.sql
+++ db/rent-delta-schema.sql
@@ -14,6 +14,10 @@ ALTER TABLE `eh_lease_promotion_attachments` CHANGE COLUMN `lease_id` `owner_id`
 ALTER TABLE `eh_lease_promotions` DROP COLUMN community_id;
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `building_name` VARCHAR(512) DEFAULT NULL AFTER `building_id`;
 ALTER TABLE `eh_lease_promotions` ADD COLUMN `building_type` tinyint DEFAULT NULL AFTER `building_id`;
+ALTER TABLE `eh_lease_promotions` DROP COLUMN `subject`;
+ALTER TABLE `eh_lease_promotions` ADD COLUMN `update_uid` bigint(20) DEFAULT NULL AFTER `update_time`;
+ALTER TABLE `eh_lease_promotions` ADD COLUMN `apartment_name` varchar(128) DEFAULT NULL AFTER `address_id`;
+
 
 CREATE TABLE `eh_lease_promotion_communities` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
