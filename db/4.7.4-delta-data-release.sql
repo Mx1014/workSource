ed8295b45db14b70545ff58e76198645545af401
siting <st.zheng@zuolin.com>
2017-08-07 14:00:29 +0800

    创梦云

--- db/4.7.4-delta-data-release.sql
+++ db/4.7.4-delta-data-release.sql
@@ -1,7 +1,7 @@
 -- 添加审批 by zt.zheng
 INSERT INTO `eh_community_approve` (`id`, `namespace_id`, `organization_id`, `owner_id`, `owner_type`, `module_id`, `module_type`, `project_id`,
- `approve_name`, `status`, `form_origin_id`, `form_version`, `update_time`, `create_time`) VALUES ('1', '999983', '1008900', '240111044331055940', 'community',
- '41600', 'EhCommunityApprove', '0', '审批测试', '1', '0', '0', NOW(), NOW());
+ `approve_name`, `status`, `form_origin_id`, `form_version`, `update_time`, `create_time`) VALUES ('1', '999977', '1008900', '240111044331055940', 'community',
+ '41600', 'EhCommunityApprove', '0', '委托找房', '1', '0', '0', NOW(), NOW());
 
 -- 添加菜单 -- 按照产品要求，添加菜单到左邻域 by zt.zheng
 
@@ -40,18 +40,18 @@ VALUES ((@web_menu_privilege_id := @web_menu_privilege_id + 1), @eh_acl_privileg
 -- 角色对应的菜单权限 by zt.zheng
 SET @acl_id = (SELECT MAX(id) FROM `eh_acls`);
 INSERT INTO `eh_acls` (`id`, `namespace_id`, `owner_type`, `owner_id`, `grant_type`, `privilege_id`, `role_id`, `role_type`, `order_seq`, `creator_uid`, `create_time`)
-VALUES ((@acl_id := @acl_id + 1), 999983, 'EhOrganizations', NULL, 1, @eh_acl_privileges_id, 1001, 'EhAclRoles', 0, 1, NOW());
+VALUES ((@acl_id := @acl_id + 1), 999977, 'EhOrganizations', NULL, 1, @eh_acl_privileges_id, 1001, 'EhAclRoles', 0, 1, NOW());
 
 -- 菜单的范围 --0域不加scope by zt.zheng
  SET @menu_scope_id = (SELECT MAX(id) FROM `eh_web_menu_scopes`);
  INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`)
- VALUES ((@menu_scope_id := @menu_scope_id + 1), 41600, '', 'EhNamespaces', 999983, 2);
+ VALUES ((@menu_scope_id := @menu_scope_id + 1), 41600, '', 'EhNamespaces', 999977, 2);
  INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`)
- VALUES ((@menu_scope_id := @menu_scope_id + 1), 41610, '', 'EhNamespaces', 999983, 2);
+ VALUES ((@menu_scope_id := @menu_scope_id + 1), 41610, '', 'EhNamespaces', 999977, 2);
  INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`)
- VALUES ((@menu_scope_id := @menu_scope_id + 1), 41620, '', 'EhNamespaces', 999983, 2);
+ VALUES ((@menu_scope_id := @menu_scope_id + 1), 41620, '', 'EhNamespaces', 999977, 2);
  INSERT INTO `eh_web_menu_scopes` (`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`)
- VALUES ((@menu_scope_id := @menu_scope_id + 1), 41630, '', 'EhNamespaces', 999983, 2);
+ VALUES ((@menu_scope_id := @menu_scope_id + 1), 41630, '', 'EhNamespaces', 999977, 2);
 
 -- 模块 by zt.zheng
 INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`)
@@ -65,7 +65,7 @@ VALUES ((@eh_service_module_privileges_id := @eh_service_module_privileges_id + 
 -- 模块权限范围 by zt.zheng
 SET @eh_service_module_scopes_id = (SELECT MAX(id) FROM `eh_service_module_scopes`);
 INSERT INTO `eh_service_module_scopes` (`id`, `namespace_id`, `module_id`, `module_name`, `owner_type`, `owner_id`, `default_order`, `apply_policy`)
-VALUES ((@eh_service_module_scopes_id := @eh_service_module_scopes_id + 1), 999983, 41600, '园区审批', NULL, NULL, NULL, 2);
+VALUES ((@eh_service_module_scopes_id := @eh_service_module_scopes_id + 1), 999977, 41600, '园区审批', NULL, NULL, NULL, 2);
 
 
 
@@ -76,8 +76,8 @@ SET @eh_launch_pad_items_id = (SELECT MAX(id) FROM `eh_launch_pad_items`);
  `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`,
 `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`,
 `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`, `more_order`, `alias_icon_uri`)
-VALUES ((@eh_launch_pad_items_id := @eh_launch_pad_items_id + 1), 999983, 0, 0, 0, '/home', 'Bizs', '园区审批测试', '园区审批测试',
-'', 1, 1, 60, '{"url":"zl://form/create?sourceType=COMMUNITY_APPROVE&sourceId=1&ownerType=EhcommunityApprove&ownerId=1008900&displayName=审批测试&metaObject="}', 3, 0,
+VALUES ((@eh_launch_pad_items_id := @eh_launch_pad_items_id + 1), 999977, 0, 0, 0, '/home', 'Bizs', '委托找房', '委托找房',
+'', 1, 1, 60, '{"url":"zl://form/create?sourceType=COMMUNITY_APPROVE&sourceId=1&ownerType=EhcommunityApprove&ownerId=1008900&displayName=委托找房&metaObject="}', 3, 0,
  1, 1, '', 0, NULL, NULL, NULL, 0,
  'park_tourist', 1, NULL, NULL, 0, NULL);
 
@@ -86,8 +86,8 @@ VALUES ((@eh_launch_pad_items_id := @eh_launch_pad_items_id + 1), 999983, 0, 0, 
  `icon_uri`, `item_width`, `item_height`, `action_type`, `action_data`, `default_order`, `apply_policy`,
 `min_version`, `display_flag`, `display_layout`, `bgcolor`, `tag`, `target_type`, `target_id`, `delete_flag`,
 `scene_type`, `scale_type`, `service_categry_id`, `selected_icon_uri`, `more_order`, `alias_icon_uri`)
-VALUES ((@eh_launch_pad_items_id := @eh_launch_pad_items_id + 1), 999983, 0, 0, 0, '/home', 'Bizs', '园区审批测试', '园区审批测试',
-'', 1, 1, 60, '{"url":"zl://form/create?sourceType=COMMUNITY_APPROVE&sourceId=1&ownerType=EhcommunityApprove&ownerId=1008900&displayName=审批测试&metaObject="}', 3, 0,
+VALUES ((@eh_launch_pad_items_id := @eh_launch_pad_items_id + 1), 999977, 0, 0, 0, '/home', 'Bizs', '委托找房', '委托找房',
+'', 1, 1, 60, '{"url":"zl://form/create?sourceType=COMMUNITY_APPROVE&sourceId=1&ownerType=EhcommunityApprove&ownerId=1008900&displayName=委托找房&metaObject="}', 3, 0,
  1, 1, '', 0, NULL, NULL, NULL, 0,
  'pm_admin', 1, NULL, NULL, 0, NULL);
 
