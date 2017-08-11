c2e45bfb382bae9b97f3f426a0b763561b0c1b88
nan.rong <nan.rong@zuolin.com>
2017-08-11 18:57:14 +0800

    start

--- ehcore/src/main/java/com/everhomes/profile/ProfileController.java
+++ ehcore/src/main/java/com/everhomes/profile/ProfileController.java
@@ -1,7 +1,16 @@
 package com.everhomes.profile;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * Created by M on 2017/7/21.
+ * 2017.08.11
+ * 1.通讯录与人事现在合并为同一块内容
+ * 2.将人事从组织架构模块分离出来
  */
+
+@RestController
+@RequestMapping("/profile")
 public class ProfileController {
+
 }
