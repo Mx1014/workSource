4b6d02e5d7600b768405e3a6828d9a4e2952bef0
nan.rong <nan.rong@zuolin.com>
2017-08-14 12:02:12 +0800

    command added

--- ehcore/src/main/java/com/everhomes/profile/ProfileServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/profile/ProfileServiceImpl.java
@@ -2,7 +2,9 @@ package com.everhomes.profile;
 
 import com.everhomes.rest.profile.AddProfileContactsPersonnelCommand;
 import com.everhomes.rest.profile.AddProfileContactsPersonnelResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProfileServiceImpl implements ProfileService {
 
     @Override
--- ehrest/src/main/java/com/everhomes/rest/profile/AddProfileContactsPersonnelCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/profile/AddProfileContactsPersonnelCommand.java
@@ -1,9 +1,46 @@
 package com.everhomes.rest.profile;
 
+import com.everhomes.discover.ItemType;
 import com.everhomes.util.StringHelper;
 
-public class AddProfileContactsPersonnelCommand {
+import java.util.List;
 
+/**
+ * <ul>
+ * <li>contactName: 成员姓名</li>
+ * <li>contactEnName: 成员英文名</li>
+ * <li>gender: 成员性别</li>
+ * <li>areaCode: 区号</li>
+ * <li>contactToken: 联系号码</li>
+ * <li>contactShortToken: 手机短号</li>
+ * <li>email: 成员邮箱</li>
+ * <li>departmentIds: 选择的部门</li>
+ * <li>jobPositionIds: 选择的职位</li>
+ * <li>visibleFlag: 隐私设置: 0-显示, 1-隐藏</li>
+ * </ul>
+ */
+public class AddProfileContactsPersonnelCommand {
+    private String contactName;
+
+    private String contactEnName;
+
+    private Byte gender;
+
+    private String areaCode;
+
+    private String contactToken;
+
+    private String contactShortToken;
+
+    private String email;
+
+    @ItemType(Long.class)
+    private List<Long> departmentIds;
+
+    @ItemType(Long.class)
+    private List<Long> jobPositionIds;
+
+    private Byte visibleFlag;
 
     @Override
     public String toString() {
