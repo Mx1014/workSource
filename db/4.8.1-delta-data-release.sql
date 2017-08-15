8eee5db572074f865e2fb0e10ffd33e7e99d9406
xq.tian <xq.tian@zuolin.com>
2017-08-15 11:34:07 +0800

    merge from flow 1.7

--- db/4.8.1-delta-data-release.sql
+++ db/4.8.1-delta-data-release.sql
@@ -12,3 +12,10 @@ VALUES (@eh_service_alliance_jump_id + 1, '审批', 2,'zl://approval/create?appr
 
 -- 更改菜单 by.st.zheng
 UPDATE `eh_web_menus` SET `data_type`='react:/approval-management/approval-list/40500/EhOrganizations' WHERE `id`='40541' and name='审批列表';
+
+--
+-- 用户输入内容变量 add by xq.tian  2017/08/05
+--
+SELECT MAX(id) FROM `eh_flow_variables` INTO @flow_variables_id;
+INSERT INTO `eh_flow_variables` (`id`, `namespace_id`, `owner_id`, `owner_type`, `module_id`, `module_type`, `name`, `label`, `var_type`, `script_type`, `script_cls`, `status`)
+VALUES ((@flow_variables_id := @flow_variables_id + 1), 0, 0, '', 0, '', 'text_button_msg_input_content', '文本备注内容', 'text_button_msg', 'bean_id', 'flow-variable-text-button-msg-user-input-content', 1);
