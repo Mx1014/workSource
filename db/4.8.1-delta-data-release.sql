d91e52714a19fcff60ad3307bda1f8c75e224a74
yanjun <jun.yan@zuolin.com>
2017-08-16 21:12:28 +0800

    fix old data

--- db/4.8.1-delta-data-release.sql
+++ db/4.8.1-delta-data-release.sql
@@ -48,3 +48,19 @@ INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES
 
 -- 活动分享页app信息栏-volgo更新为“工作中的生活态度”  add by yanjun 20170815
 UPDATE eh_app_urls set description = '工作中的生活态度' where namespace_id = 1;
+
+-- 更新老数据，以前如果一个地址有人加入后新建group时取的是0域空间。现在要跟新过来，使用实际的域空间  add by yanjun 20170816 
+update eh_groups set namespace_id = 999994 where id =   179181 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999994 where id =   179184 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999994 where id =   179185 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999994 where id =   179187 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999994 where id =   179218 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999988 where id =  1004270 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1007151 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1009351 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1010196 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1020489 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1021332 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1041837 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999975 where id =  1041838 and namespace_id =   0 ;
+update eh_groups set namespace_id = 999992 where id =  1041989 and namespace_id =   0 ;
