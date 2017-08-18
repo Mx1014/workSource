740706239a59512ede660579ba906da207cd2ce0
yanjun <jun.yan@zuolin.com>
2017-08-18 10:57:15 +0800

    add comment

--- ehcore/src/main/java/com/everhomes/activity/ActivityPortalPublishHandler.java
+++ ehcore/src/main/java/com/everhomes/activity/ActivityPortalPublishHandler.java
@@ -21,14 +21,17 @@ public class ActivityPortalPublishHandler implements PortalPublishHandler {
 
 
 	/**
-	 * 发布具体模块的内容
+	 * * 发布具体模块的内容
 	 * 注：instanceConfig就是跟web端协商好的json字符串，需要解析后添加或者修改（数据是否有id来判断添加或者修改）到对应的业务表里面去,
 	 * 如果是对应的配置项要添加到业务表，则会生成id，需要把生成的id字段回填到instanceConfig，再次发布的时候能通过id找到对应的业务数据，
 	 * 然后进行修改，如果是修改，则通过id直接找到对应业务数据修改即可
+	 * @param namespaceId
 	 * @param instanceConfig 具体模块配置的参数
-	 * @return instanceConfig 把json对象里面个个实体需要的id补充返回
+	 * @return
 	 */
 	public String publish(Integer namespaceId, String instanceConfig){
+
+		LOGGER.info("ActivityPortalPublishHandler publish start namespaceId = {}, instanceConfig = {}", namespaceId, instanceConfig);
 
 		ActivityEntryConfigulation config = ConvertHelper.convert(instanceConfig, ActivityEntryConfigulation.class);
 
@@ -55,16 +58,21 @@ public class ActivityPortalPublishHandler implements PortalPublishHandler {
 		//新增、更新内容分类
 		updateContentCategory(config, activityCategory,  maxEntryId, namespaceId);
 
+		LOGGER.info("ActivityPortalPublishHandler publish end instanceConfig = {}", StringHelper.toJsonString(config));
+
 		return StringHelper.toJsonString(config);
 	}
 
 	/**
 	 * 处理成服务广场item需要的actionData
 	 * 注：把instanceConfig中复杂的数据处理成服务广场需要的actionData字符串
+	 * @param namespaceId
 	 * @param instanceConfig
 	 * @return
 	 */
 	public String getItemActionData(Integer namespaceId, String instanceConfig){
+
+		LOGGER.info("ActivityPortalPublishHandler getItemActionData start namespaceId = {}, instanceConfig = {}", namespaceId, instanceConfig);
 
 		ActivityEntryConfigulation config = (ActivityEntryConfigulation)StringHelper.fromJsonString(instanceConfig, ActivityEntryConfigulation.class);
 
@@ -74,16 +82,22 @@ public class ActivityPortalPublishHandler implements PortalPublishHandler {
 
 		actionData.setTitle(config.getName());
 
+		LOGGER.info("ActivityPortalPublishHandler getItemActionData end actionData = {}", StringHelper.toJsonString(actionData));
+
 		return StringHelper.toJsonString(actionData);
 	}
 
 	/**
-	 * 根据服务广场的actionData获取业务应用的instanceConfig
+	 *  根据服务广场的actionData获取业务应用的instanceConfig
 	 * 注：通过actionData找到对应的所有需要配置的数据组装成之前跟web端人员协商好的instanceConfig，后面用于给web端人员解析展示对应到页面的各个配置
+	 * @param namespaceId
 	 * @param actionData
 	 * @return
 	 */
 	public String getAppInstanceConfig(Integer namespaceId, String actionData){
+
+		LOGGER.info("ActivityPortalPublishHandler getAppInstanceConfig start namespaceId = {}, actionData = {}", namespaceId, actionData);
+
 		ActivityActionData actionDataObj = (ActivityActionData)StringHelper.fromJsonString(actionData, ActivityActionData.class);
 
 		ActivityEntryConfigulation config = (ActivityEntryConfigulation)StringHelper.fromJsonString(actionData, ActivityEntryConfigulation.class);
@@ -116,14 +130,18 @@ public class ActivityPortalPublishHandler implements PortalPublishHandler {
 
 		config.setCategoryDTOList(categoryDTOList);
 
+		LOGGER.info("ActivityPortalPublishHandler getAppInstanceConfig end config = {}", StringHelper.toJsonString(config));
+
 		return StringHelper.toJsonString(config);
 	}
-
 
 
 	/**
 	 * 新增或者更新活动入口
 	 * @param config
+	 * @param maxEntryId
+	 * @param namespaceId
+	 * @return
 	 */
 	private ActivityCategories updateEntry(ActivityEntryConfigulation config, Long maxEntryId, Integer namespaceId){
 
