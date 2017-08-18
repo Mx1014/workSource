605027cf4dd0d30383366d943f03b7718ff7bf60
nan.rong <nan.rong@zuolin.com>
2017-08-18 15:31:49 +0800

    table fixed

--- db/profile-2.0-delta-schema.sql
+++ db/profile-2.0-delta-schema.sql
@@ -30,3 +30,18 @@ CREATE TABLE `eh_profile_dismiss_employees` (
 ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
 
 ALTER TABLE eh_organization_member_details CHANGE dimission_time dismiss_time DATE;
+ALTER TABLE eh_organization_member_details ADD COLUMN procreative DATE COMMENT '生育状况';
+ALTER TABLE eh_organization_member_details ADD COLUMN ethnicity VARCHAR(128) COMMENT '民族';
+ALTER TABLE eh_organization_member_details ADD COLUMN id_type VARCHAR(64) COMMENT '证件类型';
+ALTER TABLE eh_organization_member_details ADD COLUMN id_expirt_date DATE COMMENT '证件有效期';
+ALTER TABLE eh_organization_member_details ADD COLUMN education VARCHAR(64) COMMENT '学历';
+ALTER TABLE eh_organization_member_details ADD COLUMN graduation_school VARCHAR(256) COMMENT '毕业学校';
+ALTER TABLE eh_organization_member_details ADD COLUMN graduation_time DATE COMMENT '毕业时间';
+ALTER TABLE eh_organization_member_details ADD COLUMN emergency_relationship VARCHAR(128) COMMENT '紧急联系人关系';
+ALTER TABLE eh_organization_member_details ADD COLUMN work_email VARCHAR(64) COMMENT '工作邮箱';
+ALTER TABLE eh_organization_member_details ADD COLUMN work_place BIGINT COMMENT '工作地点';
+ALTER TABLE eh_organization_member_details ADD COLUMN contract_party BIGINT COMMENT '合同主体';
+ALTER TABLE eh_organization_member_details ADD COLUMN work_start_time DATE COMMENT '参加工作日期';
+ALTER TABLE eh_organization_member_details ADD COLUMN contract_start_time DATE COMMENT '合同开始日期';
+ALTER TABLE eh_organization_member_details ADD COLUMN contract_end_time DATE COMMENT '合同终止日期';
+ALTER TABLE eh_organization_member_details ADD COLUMN reg_residence_type VARCHAR(64) COMMENT '户籍类型';
