367f1c5b321525f4eabd490bb8cdef7ae4357e27
nan.rong <nan.rong@zuolin.com>
2017-08-22 16:38:21 +0800

    import fixed

---
+++ ehrest/src/main/java/com/everhomes/rest/archives/ImportArchivesContactsDTO.java
@@ -0,0 +1,106 @@
+package com.everhomes.rest.archives;
+
+import com.everhomes.util.StringHelper;
+
+/**
+ * <ul>
+ * <li>contactName: 姓名</li>
+ * <li>contactEnName: 英文名</li>
+ * <li>gender: 性别</li>
+ * <li>contactToken: 手机</li>
+ * <li>contactShortToken: 短号</li>
+ * <li>email: 工作邮箱</li>
+ * <li>department: 部门</li>
+ * <li>jobPosition: 职务</li>
+ * </ul>
+ */
+public class ImportArchivesContactsDTO {
+
+    private String contactName;
+
+    private String contactEnName;
+
+    private String gender;
+
+    private String contactToken;
+
+    private String contactShortToken;
+
+    private String email;
+
+    private String department;
+
+    private String jobPosition;
+
+    public ImportArchivesContactsDTO() {
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEnName() {
+        return contactEnName;
+    }
+
+    public void setContactEnName(String contactEnName) {
+        this.contactEnName = contactEnName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getContactToken() {
+        return contactToken;
+    }
+
+    public void setContactToken(String contactToken) {
+        this.contactToken = contactToken;
+    }
+
+    public String getContactShortToken() {
+        return contactShortToken;
+    }
+
+    public void setContactShortToken(String contactShortToken) {
+        this.contactShortToken = contactShortToken;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getJobPosition() {
+        return jobPosition;
+    }
+
+    public void setJobPosition(String jobPosition) {
+        this.jobPosition = jobPosition;
+    }
+
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
+}
