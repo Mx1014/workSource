02163f6a22a0f8f05c2555fce83b2454ae7f776c
lei.lv <lei.lv@zuolin.com>
2017-08-24 17:18:14 +0800

    fix

--- db/address-1.0-delta-schema.sql
+++ db/address-1.0-delta-schema.sql
@@ -16,6 +16,8 @@ CREATE TABLE `eh_namespace_masks` (
   `id` bigint(20) NOT NULL,
   `namespace_id` int(11) NOT NULL,
   `item_name` varchar(64) NOT NULL,
-  `imageType` tinyint(4) DEFAULT NULL,
+  `image_type` tinyint(4) DEFAULT NULL,
+  `tips` varchar(255) DEFAULT NULL,
+  `scene_type` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
