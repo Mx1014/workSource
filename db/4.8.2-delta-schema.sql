cd3a3612f3570e6d78ef015777c5486afd797c73
yanjun <jun.yan@zuolin.com>
2017-08-25 14:39:48 +0800

    add poll repeat flag

--- db/4.8.2-delta-schema.sql
+++ db/4.8.2-delta-schema.sql
@@ -221,3 +221,6 @@ CREATE TABLE `eh_portal_launch_pad_mappings` (
   `creator_uid` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 临时增加功能：可以投票增加"重复投票"选项和"投票间隔时间"   add by yanjun 20170825
+ALTER TABLE `eh_polls` ADD COLUMN `repeat_flag`  tinyint(4) NULL,  ADD COLUMN `repeat_period`  tinyint(4) NULL;
