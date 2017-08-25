b1d2b23a1202a1dc5afeb577f94568ac94eea27c
yanjun <jun.yan@zuolin.com>
2017-08-25 14:52:37 +0800

    fix schema

--- db/4.8.2-delta-schema.sql
+++ db/4.8.2-delta-schema.sql
@@ -223,4 +223,4 @@ CREATE TABLE `eh_portal_launch_pad_mappings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 临时增加功能：可以投票增加"重复投票"选项和"投票间隔时间"   add by yanjun 20170825
-ALTER TABLE `eh_polls` ADD COLUMN `repeat_flag`  tinyint(4) NULL,  ADD COLUMN `repeat_period`  tinyint(4) NULL;
+ALTER TABLE `eh_polls` ADD COLUMN `repeat_flag`  tinyint(4) NULL COMMENT 'is support repeat poll. 0-no, 1-yes', ADD COLUMN `repeat_period`  int(11) NULL COMMENT 'repeat_period,  day';
