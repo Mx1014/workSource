55175580b02cd6fdd2f6c59bfb72cc1ad8805cb4
dengs <shuang.deng@zuolin.com>
2017-08-30 18:26:38 +0800

    fix

--- db/express-2.0-delta-data-debug.sql
+++ db/express-2.0-delta-data-debug.sql
@@ -1,5 +1,5 @@
-update eh_configurations SET `value`='http://pay-beta.zuolin.com/EDS_PAY/rest/pay_common/payInfo_record/save_payInfo_record' WHERE `name` = 'guomao.payserver.url' AND namespace_id = 999968;
+update eh_configurations SET `value`='http://pay-beta.zuolin.com/EDS_PAY/rest/pay_common/payInfo_record/save_payInfo_record' WHERE `name` = 'guomao.payserver.url' AND namespace_id = 0;
 set @eh_configurations_id = (select Max(id) from eh_configurations);
 INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES ((@eh_configurations_id:=@eh_configurations_id+1), 
 'debug.flag', 'true', '国贸 debug flag', 0, NULL);
-update eh_configurations SET `value`='http://pay-beta.zuolin.com/EDS_PAY/rest/pay_common/payInfo_record/createWechatJsPayOrder' WHERE `name` = 'guomao.official.accounts.payserver.url' AND namespace_id = 999968;
+update eh_configurations SET `value`='http://pay-beta.zuolin.com/EDS_PAY/rest/pay_common/payInfo_record/createWechatJsPayOrder' WHERE `name` = 'guomao.official.accounts.payserver.url' AND namespace_id = 0;
