e1e7a9e49c2f1c965b2ec4813214dbbafc069b8e
wentian <wentian.wang@zuolin.com>
2017-09-01 11:43:47 +0800

    bigdecimal to string in an inappropriate way

--- ehcore/src/main/java/com/everhomes/asset/AssetServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/asset/AssetServiceImpl.java
@@ -906,7 +906,7 @@ public class AssetServiceImpl implements AssetService {
         HashMap<String,String> map = new HashMap();
         for(int i = 0; i < variableIdAndValueList.size(); i++){
             VariableIdAndValue variableIdAndValue = variableIdAndValueList.get(i);
-            map.put((String)variableIdAndValue.getVariableId(),(String)variableIdAndValue.getVariableValue());
+            map.put((String)variableIdAndValue.getVariableId(),((BigDecimal)variableIdAndValue.getVariableValue()).toString());
         }
         for(Map.Entry<String,String> entry : map.entrySet()){
             formula = formula.replace(entry.getKey(),entry.getValue());
