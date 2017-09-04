7fcfff19196de830ca75f0a82ed18ed6f45ebb7c
xq.tian <xq.tian@zuolin.com>
2017-09-04 19:04:06 +0800

    merge from stat-2.0.1

--- db/4.9.0-delta-schema.sql
+++ db/4.9.0-delta-schema.sql
@@ -115,3 +115,9 @@ CREATE TABLE `eh_community_building_geos` (
 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 运营统计新增场景和组件字段  add by xq.tian  2017/09/01
+--
+ALTER TABLE eh_stat_event_portal_statistics ADD COLUMN `scene_type` VARCHAR(64) COMMENT 'default, pm_admin, park_tourist...';
+ALTER TABLE eh_stat_event_portal_statistics ADD COLUMN `widget` VARCHAR(64) COMMENT 'Navigator, Banner, OPPush...';
