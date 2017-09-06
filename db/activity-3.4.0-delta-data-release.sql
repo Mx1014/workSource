68c4697f165b2d4c8da0c8b10746c4ec030e0799
yanjun <jun.yan@zuolin.com>
2017-09-06 13:43:38 +0800

    add web menus

--- db/activity-3.4.0-delta-data-release.sql
+++ db/activity-3.4.0-delta-data-release.sql
@@ -18,3 +18,9 @@ INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES
 	((@id := @id + 1), 'activity', '25', 'zh_CN', '报名信息存在，现已更新');
 
 UPDATE eh_locale_strings SET text = '呃，报名信息已经存在，可在报名详情页修改该用户信息' WHERE scope = 'activity' AND code = 10031;
+
+
+-- 管理后台增加菜单 add by yanjun 20170906
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('7000000', '园区服务模块管理', '0', null, null, '1', '2', '/7000000', 'zuolin', '20', '10600', '1', 'project', 'classify');
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('7100000', '活动模块', '7000000', NULL, 'activity-application', '1', '2', '/7000000/7100000', 'zuolin', '1', '10600', '2', 'project', 'module');
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('7110000', '活动模块', '7100000', NULL, 'activity-application', '1', '2', '/7000000/7100000/7110000', 'zuolin', '1', '10600', '3', 'project', 'page');
