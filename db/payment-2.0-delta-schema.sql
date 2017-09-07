73fd86bbc8ea60b4bec74897ab92af58adf02e15
yanjun <jun.yan@zuolin.com>
2017-09-07 19:20:49 +0800

    fix schema

--- db/payment-2.0-delta-schema.sql
+++ db/payment-2.0-delta-schema.sql
@@ -35,8 +35,8 @@ DROP TABLE IF EXISTS `eh_payment_service_configs`;
 CREATE TABLE `eh_payment_service_configs` (
 	`id` BIGINT NOT NULL,
 	`name` VARCHAR(256) DEFAULT NULL COMMENT '服务名称',
-	`type` VARCHAR(128) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
-	`namespace_id` INTEGER DEFAULT NULL, 
+	`order_type` VARCHAR(64) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
+	`namespace_id` INTEGER DEFAULT NULL,
 	`owner_type` VARCHAR(64)DEFAULT NULL,
 	`owner_id` BIGINT DEFAULT NULL,
 	`resource_type` VARCHAR(64) DEFAULT NULL,
@@ -55,23 +55,25 @@ CREATE TABLE `eh_payment_service_configs` (
 DROP TABLE IF EXISTS `eh_payment_order_records`;
 CREATE TABLE `eh_payment_order_records` (
 	`id` BIGINT NOT NULL,
-	`service_id` BIGINT NOT NULL COMMENT '业务服务类型,eh_payment_service.id',
+	`service_config_id` BIGINT NOT NULL COMMENT '业务服务类型,eh_payment_service.id',
+	`order_type` VARCHAR(64) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
 	`order_id` BIGINT NOT NULL COMMENT '业务订单编号',
 	`payment_order_id` BIGINT NOT NULL COMMENT '支付系统支付单号',
-	`create_time` datetime NOT NULL,
 	`order_commit_url` VARCHAR(1024) DEFAULT NULL COMMENT '支付接口',
 	`order_commit_token` VARCHAR(1024) DEFAULT NULL COMMENT '支付token',
 	`order_commit_nonce` VARCHAR(128) DEFAULT NULL COMMENT '支付随机数',
 	`order_commit_timestamp` BIGINT DEFAULT NULL COMMENT '支付时间戳',
 	`pay_info` TEXT COMMENT '微信公众号支付,扫码支付等支付信息',
+	`create_time` datetime NOT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `u_payment_order_id` (`payment_order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+DROP TABLE IF EXISTS `eh_payment_types`;
 CREATE TABLE `eh_payment_types` (
 	`id` BIGINT NOT NULL,
-	`type` VARCHAR(128) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
-	`namespace_id` INTEGER DEFAULT NULL, 
+	`order_type` VARCHAR(64) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
+	`namespace_id` INTEGER DEFAULT NULL,
 	`owner_type` VARCHAR(64)DEFAULT NULL,
 	`owner_id` BIGINT DEFAULT NULL,
 	`resource_type` VARCHAR(64) DEFAULT NULL,
