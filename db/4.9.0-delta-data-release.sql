93d1cbcaf4c18d7e798b9e67c4f9a23106b965c8
yanshaofan <sf.yan@zuolin.com>
2017-09-08 17:38:09 +0800

    sql

--- db/4.9.0-delta-data-release.sql
+++ db/4.9.0-delta-data-release.sql
@@ -1519,6 +1519,7 @@ insert into `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `le
 UPDATE `eh_web_menus` SET `module_id` = 31000 WHERE id = 31000;
 UPDATE `eh_web_menus` SET `module_id` = 32000 WHERE id = 32000;
 update `eh_service_modules` set `update_time` = now();
+DELETE FROM eh_service_module_scopes WHERE module_id in (70000,70100,70200);
 
 UPDATE `eh_launch_pad_items` SET `action_data` = '{"displayType":"grid","type":212481,"parentId":212481}' WHERE id in (119001,119002) and namespace_id = 999971;
 
