38bd19ba8e3e834329e5b75984bb4950745b16ea
ying.xiong <ying.xiong@zuolin.com>
2017-09-11 17:12:46 +0800

    add layout in contractdetaildto

--- ehcore/src/main/java/com/everhomes/contract/ZJContractHandler.java
+++ ehcore/src/main/java/com/everhomes/contract/ZJContractHandler.java
@@ -244,6 +244,7 @@ public class ZJContractHandler implements ContractService{
         ContractDetailDTO dto = new ContractDetailDTO();
         dto.setPartyAId(0L);
         dto.setContractNumber(zjContract.getContractNum());
+        dto.setLayout(zjContract.getLayout());
         //张江高科合同名和合同编号一样
         dto.setName(zjContract.getContractNum());
         dto.setCustomerName(zjContract.getLessee());
