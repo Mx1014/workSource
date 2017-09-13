9c0f4c7ed488fd7ed1fd4719ddad8a7570494cb0
janson <gandancing@gmail.com>
2017-09-13 13:52:41 +0800

    add checksum for update time

--- ehcore/src/main/java/com/everhomes/aclink/CmdUtil.java
+++ ehcore/src/main/java/com/everhomes/aclink/CmdUtil.java
@@ -166,6 +166,9 @@ public class CmdUtil {
         System.arraycopy(curTimeBytes, 0, dataArr, extTimeBytes.length, curTimeBytes.length);
         try {
             dataArr = addPaddingTo16Bytes(dataArr);
+            byte[] chk = DataUtil.shortToByteArray(CmdUtil.getCheckSum(dataArr, 14));
+            dataArr[14] = chk[0];
+            dataArr[15] = chk[1];
             
             LOGGER.info("updateTime dataArr = " + StringHelper.toHexString(dataArr));
             
@@ -353,9 +356,12 @@ public class CmdUtil {
         return null;
     }
 
-    public static short getCheckSum(byte[] data) {
+    public static short getCheckSum(byte[] data, int len) {
+        if(len <= 0 || len > data.length) {
+            len = data.length;
+        }
         short sum = 0;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < len; i++) {
             sum += (data[i] & 0xFF);
         }
         return sum;
