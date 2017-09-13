e5a06a5a568a18cdb951521f58103231c28b144a
yanshaofan <sf.yan@zuolin.com>
2017-09-13 15:28:28 +0800

    sql

--- db/4.9.1-delta-data-release.sql
+++ db/4.9.1-delta-data-release.sql
@@ -6,3 +6,6 @@ UPDATE `eh_namespace_masks` SET `id`='2', `namespace_id`='999971', `item_name`='
 -- 把有管理员权限的用户，在eh_organization_members表里面标识成 manager add by sfyan 20170911
 update `eh_organization_members` eom set `member_group` = '';
 update `eh_organization_members` eom set `member_group` = ifnull((select 'manager' from eh_acls where `owner_type` = 'EhOrganizations' and `owner_id` = eom.organization_id and role_type = 'EhUsers' and role_id = eom.target_id and privilege_id in (10,15)  limit 1), '') where group_type = 'ENTERPRISE' and target_type = 'USER' and status = 3;
+
+-- 用户注册已存在提示 add by sfyan 20170913 
+INSERT INTO `eh_locale_strings`(`scope`, `code`,`locale`, `text`) VALUES( 'user', '100019', 'zh_CN', '手机号已被注册！');
