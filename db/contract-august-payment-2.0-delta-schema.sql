064ba43a3819430dfed51efc07bc6e4f24a3bcaf
wentian <wentian.wang@zuolin.com>
2017-09-13 20:46:52 +0800

    for persist and response

---
+++ db/contract-august-payment-2.0-delta-schema.sql
@@ -0,0 +1,129 @@
+-- 用途:请求左邻支付系统接口,需要使用到如下4个字段
+-- 支付分配的账号信息,accountId/systemId/appKey/secretKey
+-- system_id园区系统填1,电商系统填2
+DROP TABLE IF EXISTS `eh_payment_accounts`;
+CREATE TABLE `eh_payment_accounts` (
+	`id` BIGINT NOT NULL,
+	`name` VARCHAR(128) DEFAULT NULL,
+	`account_id` BIGINT DEFAULT NULL,
+	`system_id` INTEGER DEFAULT NULL,
+	`app_key` VARCHAR(256) DEFAULT NULL,
+	`secret_key` VARCHAR(1024) DEFAULT NULL,
+	`create_time` datetime NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 用途:为每个owner生成一个唯一的id,作为暴露给支付系统的用户id,用户创建会员
+-- ownerType为普通用户/企业/商家等,ownerId填对应的owner编号
+-- id为支付系统的会员bizUserId,paymentUserId为支付系统User表的id
+DROP TABLE IF EXISTS `eh_payment_users`;
+CREATE TABLE `eh_payment_users` (
+	`id` BIGINT NOT NULL,
+	`owner_type` VARCHAR(64) NOT NULL,
+	`owner_id` BIGINT NOT NULL,
+	`payment_user_type` INTEGER NOT NULL COMMENT '1-普通会员,2-企业会员',
+	`payment_user_id` BIGINT NOT NULL,
+	`create_time` datetime NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `i_owner` (`owner_type`,`owner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 用途:根据业务需求,设置不同分账规则
+-- 将id传给支付系统的createOrder接口的orderRemark1参数,后续根据业务需求查账单数据用到
+-- 按namespaceId/owner/resource三个维度,满足不同业务需求设置不同分账规则
+DROP TABLE IF EXISTS `eh_payment_service_configs`;
+CREATE TABLE `eh_payment_service_configs` (
+	`id` BIGINT NOT NULL,
+	`name` VARCHAR(256) DEFAULT NULL COMMENT '服务名称',
+	`order_type` VARCHAR(64) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
+	`namespace_id` INTEGER DEFAULT NULL,
+	`owner_type` VARCHAR(64)DEFAULT NULL,
+	`owner_id` BIGINT DEFAULT NULL,
+	`resource_type` VARCHAR(64) DEFAULT NULL,
+	`resource_id` BIGINT DEFAULT NULL,
+	`payment_split_rule_id` BIGINT NOT NULL,
+	`payment_user_type` INTEGER NOT NULL COMMENT '1-普通会员,2-企业会员',
+	`payment_user_id` BIGINT NOT NULL,
+	`create_time` datetime NOT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 用途:记录业务订单/业务类型/支付单关联关系,便于支付成功回调接口通过支付单号找到业务订单及后续特定业务订单处理
+-- 记录这4个参数值,用于重新支付:order_commit_url,order_commit_token,order_commit_nonce,order_commit_timestamp
+-- 记录微信公众号支付,扫码支付等支付信息: pay_info
+DROP TABLE IF EXISTS `eh_payment_order_records`;
+CREATE TABLE `eh_payment_order_records` (
+	`id` BIGINT NOT NULL,
+	`service_config_id` BIGINT NOT NULL COMMENT '业务服务类型,eh_payment_service.id',
+	`order_type` VARCHAR(64) NOT NULL COMMENT '服务类型,填parking/rentalOrder等',
+	`order_id` BIGINT NOT NULL COMMENT '业务订单编号',
+	`payment_order_id` BIGINT NOT NULL COMMENT '支付系统支付单号',
+	`order_commit_url` VARCHAR(1024) DEFAULT NULL COMMENT '支付接口',
+	`order_commit_token` VARCHAR(1024) DEFAULT NULL COMMENT '支付token',
+	`order_commit_nonce` VARCHAR(128) DEFAULT NULL COMMENT '支付随机数',
+	`order_commit_timestamp` BIGINT DEFAULT NULL COMMENT '支付时间戳',
+	`pay_info` TEXT COMMENT '微信公众号支付,扫码支付等支付信息',
+	`create_time` datetime NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `u_payment_order_id` (`payment_order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `eh_payment_types`;
+CREATE TABLE `eh_payment_types` (
+	`id` BIGINT NOT NULL,
+	`order_type` VARCHAR(64) DEFAULT NULL COMMENT '服务类型,填parking/rentalOrder等',
+	`namespace_id` INTEGER DEFAULT NULL,
+	`owner_type` VARCHAR(64)DEFAULT NULL,
+	`owner_id` BIGINT DEFAULT NULL,
+	`resource_type` VARCHAR(64) DEFAULT NULL,
+	`resource_id` BIGINT DEFAULT NULL,
+	`payment_type` INTEGER DEFAULT NULL COMMENT '支付方式',
+	`payment_name` VARCHAR(128) DEFAULT NULL COMMENT '支付方式名称',
+	`payment_logo` VARCHAR(512) DEFAULT NULL COMMENT '支付方式logo',
+	`paymentParams` VARCHAR(512) DEFAULT NULL COMMENT '支付方式附加信息',
+	`create_time` datetime NOT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `eh_payment_order`;
+CREATE TABLE `eh_payment_order` (
+  `id` bigint(20) NOT NULL COMMENT 'id of the record',
+  `uuid` varchar(36) NOT NULL,
+  `payer_name` varchar(128) DEFAULT NULL,
+  `payer_type` varchar(32) DEFAULT NULL,
+  `uid` bigint(20) DEFAULT NULL,
+  `contract_id` varchar(2048) DEFAULT NULL,
+  `namespace_id` integer DEFAULT NULL,
+  `bill_ids` varchar(2048) DEFAULT NULL,
+  `client_app_name` varchar(128) DEFAULT NULL,
+  `u_name` varchar(64) DEFAULT NULL,
+  `family_id` bigint(20) DEFAULT NULL,
+  `organization_name` varchar(128) DEFAULT NULL,
+  `organization_id` bigint DEFAULT NULL,
+  `community_id` varchar(64) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL COMMENT 'remove-deletion policy, user directly managed data',
+  `phone` varchar(32) DEFAULT NULL,
+  `real_name` varchar(128) DEFAULT NULL,
+  `gender` tinyint(4) DEFAULT NULL,
+  `position` varchar(64) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `pay_flag` tinyint(4) DEFAULT '0' COMMENT '0: have not paid, 1:have paid',
+  `order_no` bigint(20) DEFAULT NULL,
+  `order_type` varchar(20) DEFAULT NULL,
+  `order_start_time` datetime DEFAULT NULL,
+  `order_expire_time` datetime DEFAULT NULL,
+  `payment_type` varchar(32) DEFAULT NULL COMMENT '10001: alipay, 10002: wechatpay',
+  `pay_amount` decimal(10,2) DEFAULT NULL,
+  `paid_time` datetime DEFAULT NULL,
+  `refund_order_no` bigint(20) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '2' COMMENT '0: cancel, 1: reject, 2:normal',
+  `cancel_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_eh_asset_order_uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
