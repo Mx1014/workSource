cdfa8fa76815630cac59ec3fb3773f38fd5359e9
lei.lv <lei.lv@zuolin.com>
2017-09-14 18:24:07 +0800

    fix

--- db/4.9.1-delta-data-release.sql
+++ db/4.9.1-delta-data-release.sql
@@ -1,7 +1,7 @@
 -- 蒙版提示信息修改 by lei.lv
 UPDATE `eh_namespace_masks` SET `id`='1', `namespace_id`='999971', `item_name`='人才公寓', `image_type`='3', `tips`='快速切换至人才公寓首页', `scene_type`='park_tourist' WHERE (`id`='1');
 UPDATE `eh_namespace_masks` SET `id`='2', `namespace_id`='999971', `item_name`='园区服务', `image_type`='3', `tips`='快速切换至园区首页', `scene_type`='default' WHERE (`id`='2');
-
+update `eh_configurations` SET value = 0 WHERE namespace_id = 999971 AND name = 'mask.key';
 
 -- 把有管理员权限的用户，在eh_organization_members表里面标识成 manager add by sfyan 20170911
 update `eh_organization_members` eom set `member_group` = '';
