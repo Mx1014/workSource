3fb4dd8606690d3672ffae67ef1bb426d208bd38
lei.lv <lei.lv@zuolin.com>
2017-09-15 11:59:51 +0800

    fix

--- db/4.9.1-delta-schema.sql
+++ db/4.9.1-delta-schema.sql
@@ -1,2 +1,4 @@
 -- 增加联系人职位 add by xiongying 20170914
 ALTER TABLE eh_enterprise_customers ADD COLUMN contact_position VARCHAR(64);
+-- 增加域空间资源排序 add by lvlei 20170915
+ALTER TABLE eh_namespace_resources ADD COLUMN default_order int(11) DEFAULT 0;
