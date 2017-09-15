94f1d6bba66b1be2b386735bf482595988ff695a
ying.xiong <ying.xiong@zuolin.com>
2017-09-15 17:08:59 +0800

    add log

--- ehcore/src/main/java/com/everhomes/pmtask/ZJGKHandoverTaskHandler.java
+++ ehcore/src/main/java/com/everhomes/pmtask/ZJGKHandoverTaskHandler.java
@@ -9,6 +9,7 @@ import com.everhomes.category.CategoryProvider;
 import com.everhomes.community.Community;
 import com.everhomes.community.CommunityProvider;
 import com.everhomes.configuration.ConfigurationProvider;
+import com.everhomes.constants.ErrorCodes;
 import com.everhomes.contentserver.ContentServerService;
 import com.everhomes.entity.EntityType;
 import com.everhomes.flow.FlowEventLog;
@@ -21,7 +22,10 @@ import com.everhomes.rest.general_approval.PostApprovalFormTextValue;
 import com.everhomes.rest.pmtask.PmTaskAttachmentDTO;
 import com.everhomes.rest.pmtask.PmTaskAttachmentType;
 import com.everhomes.util.ConvertHelper;
+import com.everhomes.util.RuntimeErrorException;
 import com.everhomes.util.SignatureHelper;
+import com.everhomes.util.StringHelper;
+import org.apache.http.protocol.HTTP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -71,22 +75,28 @@ public class ZJGKHandoverTaskHandler implements HandoverTaskHandler {
 
     @Override
     public void handoverTaskToTrd(PmTask task) {
+        LOGGER.info("ZJGKHandoverTaskHandler handoverTaskToTrd:");
         String url = configProvider.getValue(task.getNamespaceId(), "shenzhou.host.url", "");
         Map<String, String> params = generateParams(task);
+        String jsonStr = postToShenzhou(params, url+CREATE_TASK, null);
+        ZjgkJsonEntity entity = JSONObject.parseObject(jsonStr,new TypeReference<ZjgkJsonEntity>(){});
 
-        ZjgkJsonEntity entity = new ZjgkJsonEntity();
-
-
-        String jsonStr = null;
+    }
+    private String postToShenzhou(Map<String, String> params, String url, Map<String, String> headers) {
+        String json = null;
         try {
-            jsonStr = HttpUtils.post(url+CREATE_TASK, params, 20, "UTF-8");
-            //向张江认证。
-            entity = JSONObject.parseObject(jsonStr,new TypeReference<ZjgkJsonEntity>(){});
+            Long beforeRequest = System.currentTimeMillis();
+            json = HttpUtils.postJson(url, StringHelper.toJsonString(params), 30, HTTP.UTF_8);
+            Long afterRequest = System.currentTimeMillis();
+            if(LOGGER.isDebugEnabled()) {
+                LOGGER.debug("request shenzhou url: {}, json: {}, total elapse: {}", url, json, afterRequest-beforeRequest);
+            }
         } catch (Exception e) {
-            e.printStackTrace();
-            entity.setErrorDescription("请求失败");
+            LOGGER.error("sync from shenzhou request error, param={}", params, e);
+            throw RuntimeErrorException.errorWith(ErrorCodes.SCOPE_GENERAL, ErrorCodes.ERROR_GENERAL_EXCEPTION,
+                    "sync from shenzhou request error.");
         }
-
+        return json;
     }
 
 
