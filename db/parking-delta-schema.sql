86016c901800fb72d009809852111f673e84de19
sunwen <wen.sun@zuolin.com>
2017-09-18 15:15:09 +0800

    add parking schema

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -1,8 +1,29 @@
 
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN original_price decimal(10,2) DEFAULT NULL;
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN card_request_id bigint(20) DEFAULT NULL;
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN invoice_type bigint(4) DEFAULT NULL;
+
+ALTER TABLE eh_parking_card_requests ADD COLUMN card_type VARCHAR(64) DEFAULT NULL;
+ALTER TABLE eh_parking_card_requests ADD COLUMN address_id bigint(20) DEFAULT NULL;
+ALTER TABLE eh_parking_card_requests ADD COLUMN invoice_type bigint(4) DEFAULT NULL;
+
+ALTER TABLE eh_parking_lots CHANGE COLUMN `expired_recharge_json` recharge_json varchar(1024) DEFAULT NULL;
+
+ALTER TABLE eh_parking_flow ADD COLUMN card_type_tip_flag tinyint(4) NOT NULL DEFAULT '0' COMMENT '1: support , 0: not';
+ALTER TABLE eh_parking_flow ADD COLUMN card_type_tip text;
 
 
+CREATE TABLE `eh_parking_invoice_types` (
+  `id` bigint(20) NOT NULL COMMENT 'id of the record',
+  `namespace_id` int(11) NOT NULL DEFAULT '0',
+  `owner_type` varchar(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the standard, community, etc',
+  `owner_id` bigint(20) NOT NULL DEFAULT '0',
+  `parking_lot_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'reference to id of eh_parking_lots',
+  `name` varchar(64) DEFAULT NULL COMMENT 'the name of plate owner',
+  `status` tinyint(4) DEFAULT NULL COMMENT '0: inactive, 1: queueing, 2: notified, 3: issued',
 
-
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
