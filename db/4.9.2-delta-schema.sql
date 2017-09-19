5f9f02258bf1e60a12e8712b2f1030ace1719193
ying.xiong <ying.xiong@zuolin.com>
2017-09-19 17:47:29 +0800

    add schema

--- db/4.9.2-delta-schema.sql
+++ db/4.9.2-delta-schema.sql
@@ -1,2 +1,6 @@
 -- 增加域空间左上角显示场景名称的配置项
 ALTER TABLE eh_namespace_details ADD COLUMN name_type tinyint(4) DEFAULT 0;
+
+-- fix 15631 & 15636 add by xiongying20170919
+ALTER TABLE eh_organizations ADD COLUMN website VARCHAR(256);
+ALTER TABLE eh_organizations ADD COLUMN unified_social_credit_code VARCHAR(256);
