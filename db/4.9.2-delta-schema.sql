1037fa9f39752df191b76622b6a1df3aa8324381
wuhan <han.wu@zuolin.com>
2017-09-19 16:59:13 +0800

    add sql

--- db/4.9.2-delta-schema.sql
+++ db/4.9.2-delta-schema.sql
@@ -1,2 +1,5 @@
 -- 增加域空间左上角显示场景名称的配置项
-ALTER TABLE eh_namespace_details ADD COLUMN name_type tinyint(4) DEFAULT 0;
+ALTER TABLE eh_namespace_details ADD COLUMN name_type TINYINT(4) DEFAULT 0;
+
+-- 增加考勤统计字段
+ALTER TABLE eh_punch_statistics ADD COLUMN exception_day_count INT  DEFAULT NULL COMMENT '异常天数',
