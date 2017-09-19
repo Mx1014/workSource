37e8c5c3c449bea25074533f4ef36e478f4a364e
wuhan <han.wu@zuolin.com>
2017-09-19 17:02:45 +0800

    fix bug

--- db/4.9.2-delta-schema.sql
+++ db/4.9.2-delta-schema.sql
@@ -2,4 +2,4 @@
 ALTER TABLE eh_namespace_details ADD COLUMN name_type TINYINT(4) DEFAULT 0;
 
 -- 增加考勤统计字段
-ALTER TABLE eh_punch_statistics ADD COLUMN exception_day_count INT  DEFAULT NULL COMMENT '异常天数',
+ALTER TABLE eh_punch_statistics ADD COLUMN exception_day_count INT  DEFAULT NULL COMMENT '异常天数';
