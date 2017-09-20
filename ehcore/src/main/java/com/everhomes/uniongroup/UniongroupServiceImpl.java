a790d2ece2109b28a867759514cd4d2bc5873d0e
lei.lv <lei.lv@zuolin.com>
2017-09-20 15:26:11 +0800

    fix

--- ehcore/src/main/java/com/everhomes/uniongroup/UniongroupServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/uniongroup/UniongroupServiceImpl.java
@@ -48,7 +48,7 @@ public class UniongroupServiceImpl implements UniongroupService {
     @Override
     public void saveUniongroupConfigures(SaveUniongroupConfiguresCommand cmd) {
         Integer namespaceId = UserContext.getCurrentNamespaceId();
-        this.dbProvider.execute((TransactionStatus status) -> {
+//        this.dbProvider.execute((TransactionStatus status) -> {
             LOGGER.debug("saveUnion Time1 "+  System.currentTimeMillis());
 
             //已存在（即已分配薪酬组的）的部门集合
@@ -215,10 +215,10 @@ public class UniongroupServiceImpl implements UniongroupService {
                     this.uniongroupConfigureProvider.batchCreateUniongroupMemberDetail(unionDetailsList);
                 }
 
-                return null;
+//                return null;
 //            });
 //            return null;
-        });
+//        });
 
         LOGGER.debug("saveUnion Time4 "+  System.currentTimeMillis());
         //5.同步搜索引擎
