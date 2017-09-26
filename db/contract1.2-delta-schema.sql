854bc426f6eec0b0cae23362299d51069f5ea9b4
ying.xiong <ying.xiong@zuolin.com>
2017-09-26 16:49:24 +0800

    add schema

--- db/contract1.2-delta-schema.sql
+++ db/contract1.2-delta-schema.sql
@@ -4,4 +4,11 @@ ALTER TABLE `eh_var_field_group_scopes` ADD COLUMN `community_id` BIGINT COMMENT
 ALTER TABLE eh_var_fields ADD COLUMN `field_param` VARCHAR(128);
 
 ALTER TABLE `eh_buildings` ADD INDEX building_name ( `name`, `alias_name`);
+-- 4.9.2
+-- 增加域空间左上角显示场景名称的配置项
+ALTER TABLE eh_namespace_details ADD COLUMN name_type tinyint(4) DEFAULT 0;
+
+-- fix 15631 & 15636 add by xiongying20170919
+ALTER TABLE eh_organizations ADD COLUMN website VARCHAR(256);
+ALTER TABLE eh_organizations ADD COLUMN unified_social_credit_code VARCHAR(256);
 
