c83298c0bd6a73492821cfe1d91fa530243f2dbf
xujuan <juan.xu@zuolin.com>
2017-09-26 10:46:32 +0800

    modify by xujuan

--- db/init_data/kexin-init-data.sql
+++ db/init_data/kexin-init-data.sql
@@ -1718,3 +1718,5 @@ SET @eh_service_module_scopes_id = (SELECT MAX(id) FROM `eh_service_module_scope
 INSERT INTO `eh_service_module_scopes` (`id`, `namespace_id`, `module_id`, `module_name`, `owner_type`, `owner_id`, `default_order`, `apply_policy`)
 VALUES ((@eh_service_module_scopes_id := @eh_service_module_scopes_id + 1), 999983, 41500, '用户行为统计', 'EhNamespaces', 999983, NULL, 2);
 
+
+
