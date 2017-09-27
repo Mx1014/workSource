6e8ce13af53507234b061c5b0d821d91e75d3f0e
dengs <shuang.deng@zuolin.com>
2017-09-27 20:42:36 +0800

    fix

--- ehcore/src/main/java/com/everhomes/yellowPage/YellowPageServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/yellowPage/YellowPageServiceImpl.java
@@ -457,7 +457,9 @@ public class YellowPageServiceImpl implements YellowPageService {
 	@Override
 	public void updateServiceAllianceCategory(UpdateServiceAllianceCategoryCommand cmd) {
 		Integer namespaceId = UserContext.getCurrentNamespaceId();
-		
+    	if(cmd.getNamespaceId() != null){
+    		namespaceId = cmd.getNamespaceId();
+    	}
 		ServiceAllianceCategories category = new ServiceAllianceCategories();
 		ServiceAllianceCategories parent = yellowPageProvider.findCategoryById(cmd.getParentId());
 		
