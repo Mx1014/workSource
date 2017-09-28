780ed91b7288e2481a79b9174009c4e2a247d1e9
yanjun <jun.yan@zuolin.com>
2017-09-28 23:26:26 +0800

    fix

--- db/4.9.3-delta-data-release.sql
+++ db/4.9.3-delta-data-release.sql
@@ -425,8 +425,8 @@ INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('org
 
 -- 微信绑定手机页面url   add by yanjun 20170901
 SET @eh_configurations_id = (SELECT MAX(id) FROM `eh_configurations`);
-INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES((@eh_configurations_id := @eh_configurations_id + 1),'wx.bind.phone.url','/service-hub/build/#/register','微信用户绑定手机页面_默认','0',NULL);
-INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES((@eh_configurations_id := @eh_configurations_id + 1),'wx.bind.phone.url','/service-hub/build/#/register','微信用户绑定手机页面_成都孵化器','999964',NULL);
+INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES((@eh_configurations_id := @eh_configurations_id + 1),'wx.bind.phone.url','/service-hub/build/index.html#/register','微信用户绑定手机页面_默认','0',NULL);
+INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES((@eh_configurations_id := @eh_configurations_id + 1),'wx.bind.phone.url','/service-hub/build/index.html#/register','微信用户绑定手机页面_成都孵化器','999964',NULL);
 
 -- merge from incubator-1.0 成都孵化器 end by yanjun
 
