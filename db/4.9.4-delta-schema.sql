5a9542443d80aa9f0dd2efa4381a44e434093e1a
LiangQishi <qishi.liang@zuolin.com>
2017-09-29 21:43:49 +0800

    add two column for parking card

--- db/4.9.4-delta-schema.sql
+++ db/4.9.4-delta-schema.sql
@@ -4,3 +4,7 @@ ALTER TABLE `eh_addresses` ADD INDEX namespace_address ( `namespace_address_type
 -- by wentian
 ALTER TABLE `eh_payment_order_records` ADD COLUMN `order_num` varchar(255) DEFAULT NULL COMMENT '订单编号';
 ALTER TABLE `eh_payment_service_configs` modify COLUMN `payment_split_rule_id` bigint default null;
+
+-- by sun wen 20170929
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN paid_version tinyint(4) DEFAULT NULL;
+ALTER TABLE eh_rentalv2_orders ADD COLUMN paid_version tinyint(4) DEFAULT NULL;
