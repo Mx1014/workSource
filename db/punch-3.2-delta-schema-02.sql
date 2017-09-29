bfae5bb8a76bb78740946164180fbb7f695051b7
lei.lv <lei.lv@zuolin.com>
2017-09-29 11:21:11 +0800

    fox

--- db/punch-3.2-delta-schema-02.sql
+++ db/punch-3.2-delta-schema-02.sql
@@ -1,2 +1,5 @@
 -- 增加考勤统计字段
 ALTER TABLE eh_punch_statistics ADD COLUMN exception_day_count INT  DEFAULT NULL COMMENT '异常天数';
+-- 薪酬组加上版本 By lei.lv 
+ALTER TABLE eh_uniongroup_configures ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
+ALTER TABLE eh_uniongroup_member_details ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
