424c471d1f50a1e5bab99ffeeeeb82fb3732cdda
wuhan <han.wu@zuolin.com>
2017-09-30 14:16:24 +0800

    add sqls

--- db/punch-3.2-delta-schema-02.sql
+++ db/punch-3.2-delta-schema-02.sql
@@ -1,5 +1,16 @@
--- 增加考勤统计字段
-ALTER TABLE eh_punch_statistics ADD COLUMN exception_day_count INT  DEFAULT NULL COMMENT '异常天数';
--- 薪酬组加上版本 By lei.lv 
-ALTER TABLE eh_uniongroup_configures ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
-ALTER TABLE eh_uniongroup_member_details ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
+-- 同步数据时为企业添加门牌时太慢，添加索引 add by xiongying20170922
+ALTER TABLE `eh_addresses` ADD INDEX namespace_address ( `namespace_address_type`, `namespace_address_token`);
+
+-- by wentian
+ALTER TABLE `eh_payment_order_records` ADD COLUMN `order_num` VARCHAR(255) DEFAULT NULL COMMENT '订单编号';
+ALTER TABLE `eh_payment_service_configs` MODIFY COLUMN `payment_split_rule_id` BIGINT DEFAULT NULL;
+
+
+
+
+-- end of 支付2.0 init data by wentian
+
+-- by sun wen 20170929
+ALTER TABLE eh_parking_recharge_orders ADD COLUMN paid_version TINYINT(4) DEFAULT NULL;
+ALTER TABLE eh_rentalv2_orders ADD COLUMN paid_version TINYINT(4) DEFAULT NULL;
+
