f13929a32c22cba25bfc0cb0556fbed58c49f14b
wuhan <han.wu@zuolin.com>
2017-09-30 14:58:36 +0800

    add sql

---
+++ db/punch-3.2-delta-schema.sql
@@ -0,0 +1,5 @@
+
+
+-- 薪酬组加上版本 By lei.lv 
+ALTER TABLE eh_uniongroup_configures ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
+ALTER TABLE eh_uniongroup_member_details ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
