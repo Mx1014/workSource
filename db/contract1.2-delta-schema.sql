6456d600e5e52a48d543b984c64a89f3a6172316
ying.xiong <ying.xiong@zuolin.com>
2017-10-10 14:46:35 +0800

    modify schema

--- db/contract1.2-delta-schema.sql
+++ db/contract1.2-delta-schema.sql
@@ -28,6 +28,40 @@ CREATE TABLE `eh_address_attachments` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;		
 			
+CREATE TABLE `eh_contract_charging_changes` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace of owner resource, redundant info to quick namespace related queries',
+  `contract_id` BIGINT NOT NULL COMMENT 'id of eh_contracts',
+  `charging_item_id` BIGINT COMMENT '收费项',
+  `change_type` TINYINT COMMENT '1: 调租; 2: 免租',
+  `change_method` TINYINT COMMENT '1: 按金额递增; 2: 按金额递减; 3: 按比例递增; 4: 按比例递减',
+  `change_period` INTEGER,
+  `period_unit` TINYINT COMMENT '1: 天; 2: 月; 3: 年',
+  `change_range` DECIMAL(10,2),
+  `change_start_time` DATETIME,
+  `change_expired_time` DATETIME,
+  `remark` VARCHAR(1024),
+  `status` TINYINT NOT NULL DEFAULT 1 COMMENT '0: inactive; 1: waiting for approval; 2: active',
+  `create_uid` BIGINT NOT NULL DEFAULT 0,
+  `create_time` DATETIME,
+  `operator_uid` BIGINT,
+  `update_time` DATETIME,
+  `delete_uid` BIGINT,
+  `delete_time` DATETIME,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `eh_contract_charging_change_addresses` (
+  `id` bigint(20) NOT NULL COMMENT 'id of the record',
+  `namespace_id` int(11) NOT NULL DEFAULT '0' COMMENT 'namespace of owner resource, redundant info to quick namespace related queries',
+  `charging_change_id` bigint(20) NOT NULL COMMENT 'id of eh_contract_charging_changes',
+  `address_id` bigint(20) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '0: inactive; 1: waiting for approval; 2: active',
+  `create_uid` bigint(20) NOT NULL DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 -- 4.9.2
 -- 增加域空间左上角显示场景名称的配置项
