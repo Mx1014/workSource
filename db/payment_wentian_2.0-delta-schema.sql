0369959e873985a5bf6d9e0e0065c75e0356cc72
wentian <wentian.wang@zuolin.com>
2017-10-13 14:24:19 +0800

    sql incorrectness fixed

--- db/payment_wentian_2.0-delta-schema.sql
+++ db/payment_wentian_2.0-delta-schema.sql
@@ -1,23 +1,24 @@
 ALTER TABLE `eh_payment_charging_standards` ADD COLUMN `instruction` VARCHAR(1024) DEFAULT NULL COMMENT '说明';
 ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `due_day` INTEGER DEFAULT NULL COMMENT '最晚还款日，距离账单日的距离，单位可以为月 '
 -- 缴费2.0新增公式表，因为一个收费标准对应多个公式，故提取出来
+DROP TABLE IF EXISTS `eh_payment_formula`;
 CREATE TABLE `eh_payment_formula` (
-  `id` BIGINT NOT NULL,
-  `charging_standard_id` BIGINT DEFAULT NULL,
-  `name` VARCHAR(10),
-  `constraint_variable_identifer` VARCHAR(255) DEFAULT NULL,
-  `start_constraint` TINYINT COMMENT '1:大于；2：大于等于；3：小于；4：小于等于',
-  `start_num` DECIMAL(10,2) DEFAULT '0.00',
-  `end_constraint` TINYINT COMMENT '1:大于；2：大于等于；3：小于；4：小于等于',
-  `end_num` DECIMAL(10,2) DEFAULT '0.00',
-  `variables_json_string` VARCHAR(2048) DEFAULT NULL COMMENT 'json strings of variables injected for a particular formula',
-  `formula` VARCHAR(1024),
-  `formula_json` VARCHAR(2048),
-  `formula_type` TINYINT COMMENT '1: fixed fee; 2: normal formula; 3: gradient varied on variable price; 4: gradients varied functions on each variable section',
-  `price_unit_type` TINYINT COMMENT '1:日单价; 2:月单价; 3:季单价; 4:年单价',
-  `creator_uid` BIGINT,
-  `create_time` DATETIME,
-  `operator_uid` BIGINT,
-  `update_time` DATETIME,
+  `id` bigint(20) NOT NULL,
+  `charging_standard_id` bigint(20) DEFAULT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `constraint_variable_identifer` varchar(255) DEFAULT NULL,
+  `start_constraint` tinyint(4) DEFAULT NULL COMMENT '1:大于；2：大于等于；3：小于；4：小于等于',
+  `start_num` decimal(10,2) DEFAULT '0.00',
+  `end_constraint` tinyint(4) DEFAULT NULL COMMENT '1:大于；2：大于等于；3：小于；4：小于等于',
+  `end_num` decimal(10,2) DEFAULT '0.00',
+  `variables_json_string` varchar(2048) DEFAULT NULL COMMENT 'json strings of variables injected for a particular formula',
+  `formula` varchar(1024) DEFAULT NULL,
+  `formula_json` varchar(2048) DEFAULT NULL,
+  `formula_type` tinyint(4) DEFAULT NULL COMMENT '1: fixed fee; 2: normal formula; 3: gradient varied on variable price; 4: gradients varied functions on each variable section',
+  `price_unit_type` tinyint(4) DEFAULT NULL COMMENT '1:日单价; 2:月单价; 3:季单价; 4:年单价',
+  `creator_uid` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `operator_uid` bigint(20) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='收费标准公式表';
