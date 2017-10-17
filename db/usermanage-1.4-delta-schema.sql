9e36fd9bba2efc39c8d77fe6820c72574a3d7e8a
yanjun <jun.yan@zuolin.com>
2017-10-17 14:07:25 +0800

    fix schame

--- db/usermanage-1.4-delta-schema.sql
+++ db/usermanage-1.4-delta-schema.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `eh_organization_member_details`ADD COLUMN `executive_tag`  tinyint(4) NULL, ADD COLUMN `position_tag`  varchar(128) NULL;
+ALTER TABLE `eh_user_organizations` ADD COLUMN `executive_tag`  tinyint(4) NULL, ADD COLUMN `position_tag`  varchar(128) NULL;
