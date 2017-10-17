6403d630d9600d0edeca5ba28b5a955df1765943
ryan <nan.rong@zuolin.com>
2017-10-17 21:01:16 +0800

    table added

--- db/approval-1.6-delta-schema.sql
+++ db/approval-1.6-delta-schema.sql
@@ -6,3 +6,40 @@ ALTER TABLE `eh_general_approvals` ADD COLUMN `modify_flag` TINYINT DEFAULT 1 CO
 ALTER TABLE `eh_general_approvals` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMENT 'whether the approval can be deleted from desk, 0: no, 1: yes';
 ALTER TABLE `eh_general_approvals` ADD COLUMN `icon_uri` VARCHAR(1024) COMMENT 'the avatar of the approval';
 ALTER TABLE `eh_general_approvals` ADD COLUMN `approval_type` VARCHAR(64) DEFAULT 'CUSTOMIZE' COMMENT 'the type of the approval';
+
+DROP TABLE IF EXISTS `eh_general_approval_templates`;
+CREATE TABLE `eh_general_approval_templates` (
+	`id` BIGINT NOT NULL COMMENT 'id of the record',
+	`namespace_id` INTEGER NOT NULL DEFAULT 0,
+	`owner_id` BIGINT,
+	`owner_type` VARCHAR(64),
+	`organization_id` BIGINT NOT NULL DEFAULT 0,
+	`module_id` BIGINT DEFAULT 0 COMMENT 'the module id',
+	`module_type` VARCHAR(64),
+	`project_id` BIGINT NOT NULL DEFAULT 0,
+  `project_type` VARCHAR(64),
+  `form_template_id` bigint(20) NOT NULL DEFAULT 0 COMMENT 'The id of the template form',
+  `support_type` TINYINT NOT NULL DEFAULT 0 COMMENT 'APP:0, WEB:1, APP_WEB: 2',
+  `approval_name` VARCHAR(128) NOT NULL,
+  `status` TINYINT NOT NULL COMMENT 'invalid, config, running',
+  `update_time` DATETIME DEFAULT NULL COMMENT 'last update time',
+  `create_time` DATETIME COMMENT 'record create time',
+	PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4;
+
+DROP TABLE IF EXISTS `eh_general_form_templates`;
+CREATE TABLE `eh_general_form_templates` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `organization_id` BIGINT NOT NULL DEFAULT '0',
+  `owner_id` BIGINT NOT NULL,
+  `owner_type` VARCHAR(64) NOT NULL,
+  `module_id` BIGINT DEFAULT 0 COMMENT 'the module id',
+  `module_type` VARCHAR(64),
+  `form_name` VARCHAR(64) NOT NULL,
+  `template_type` VARCHAR(128) NOT NULL COMMENT 'the type of template text',
+  `template_text` TEXT COMMENT 'json 存放表单字段',
+  `update_time` DATETIME DEFAULT NULL COMMENT 'last update time',
+  `create_time` DATETIME DEFAULT NULL COMMENT 'record create time',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
