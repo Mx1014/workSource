6b91421ad09ede354cea9186fdf1ef86ce3fb15f
wuhan <han.wu@zuolin.com>
2017-10-18 18:48:58 +0800

    add version code 2 punchrule

--- db/punch-3.2-delta-schema.sql
+++ db/punch-3.2-delta-schema.sql
@@ -3,3 +3,6 @@
 -- 薪酬组加上版本 By lei.lv 
 ALTER TABLE eh_uniongroup_configures ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
 ALTER TABLE eh_uniongroup_member_details ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
+
+-- 打卡加入当前生效版本号
+ALTER TABLE eh_punch_rules ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '当前生效版本号';
