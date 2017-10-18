232f662aa1c53682d951cdcfe5508fa95f6eec93
ryan <nan.rong@zuolin.com>
2017-10-18 21:22:03 +0800

    sql changed

--- db/approval-1.6-delta-schema.sql
+++ db/approval-1.6-delta-schema.sql
@@ -5,7 +5,7 @@ ALTER TABLE `eh_general_forms` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMEN
 ALTER TABLE `eh_general_approvals` ADD COLUMN `modify_flag` TINYINT DEFAULT 1 COMMENT 'whether the approval can be modified from desk, 0: no, 1: yes';
 ALTER TABLE `eh_general_approvals` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMENT 'whether the approval can be deleted from desk, 0: no, 1: yes';
 ALTER TABLE `eh_general_approvals` ADD COLUMN `icon_uri` VARCHAR(1024) COMMENT 'the avatar of the approval';
-ALTER TABLE `eh_general_approvals` ADD COLUMN `approval_type` VARCHAR(64) DEFAULT 'CUSTOMIZE' COMMENT 'the type of the approval';
+-- ALTER TABLE `eh_general_approvals` ADD COLUMN `approval_type` VARCHAR(64) DEFAULT 'CUSTOMIZE' COMMENT 'the type of the approval';
 
 DROP TABLE IF EXISTS `eh_general_approval_templates`;
 CREATE TABLE `eh_general_approval_templates` (
