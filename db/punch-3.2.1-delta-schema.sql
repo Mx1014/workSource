f83df5df714248ead264f3553e0a9eabd9e2fbe3
wuhan <han.wu@zuolin.com>
2017-10-19 13:53:09 +0800

    add

---
+++ db/punch-3.2.1-delta-schema.sql
@@ -0,0 +1,21 @@
+
+
+-- 薪酬组加上版本 By lei.lv 
+ALTER TABLE eh_uniongroup_configures ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
+ALTER TABLE eh_uniongroup_member_details ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '版本号';
+
+-- 打卡加入当前生效版本号
+-- ALTER TABLE eh_punch_rules ADD COLUMN `version_code` INT DEFAULT 0 COMMENT '当前生效版本号';
+
+-- 维护uniongroup现在使用的是哪个version
+-- drop TABLE `eh_uniongroup_version`;
+CREATE TABLE `eh_uniongroup_version` (
+  `id` BIGINT(20) NOT NULL COMMENT 'id of the record',
+  `namespace_id` INT(11) NOT NULL DEFAULT '0',
+  `enterprise_id` BIGINT(20) DEFAULT '0',
+  `group_type` VARCHAR(32) DEFAULT NULL COMMENT 'SalaryGroup,PunchGroup', 
+  `current_version_code` INT(11) DEFAULT '0' COMMENT '当前使用的版本号 从1开始 , 0默认是config版本', 
+  `operator_uid` BIGINT(20) DEFAULT NULL,
+  `update_time` DATETIME DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4
