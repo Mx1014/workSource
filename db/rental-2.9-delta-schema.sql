f3bbcb4cef6f67faa5c7e8146924af242c40d199
siting <st.zheng@zuolin.com>
2017-10-19 16:03:37 +0800

    update

--- db/rental-2.9-delta-schema.sql
+++ db/rental-2.9-delta-schema.sql
@@ -1,7 +1,9 @@
 
--- 增加结束时间提醒 by st.zheng
+-- 增加结束时间提醒门禁时间 by st.zheng
 ALTER TABLE `eh_rentalv2_orders`
-ADD COLUMN `reminder_end_time` DATETIME NULL DEFAULT NULL AFTER `reminder_time`;
+ADD COLUMN `reminder_end_time` DATETIME NULL DEFAULT NULL AFTER `reminder_time`,
+ADD COLUMN `auth_start_time` DATETIME NULL DEFAULT NULL AFTER `reminder_end_time`,
+ADD COLUMN `auth_end_time` DATETIME NULL DEFAULT NULL AFTER `auth_start_time`;
 -- 资源预定增加门禁 by st.zheng
 ALTER TABLE `eh_rentalv2_resources`
 ADD COLUMN `aclink_id` BIGINT(20) NULL DEFAULT NULL AFTER `default_order`;
