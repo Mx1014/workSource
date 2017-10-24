6caf7cc5671f6722d0d45d2f83a6ba23f23658ff
lei.lv <lei.lv@zuolin.com>
2017-10-24 19:27:05 +0800

    fix

--- ehcore/src/main/java/com/everhomes/organization/OrganizationProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/OrganizationProviderImpl.java
@@ -5519,7 +5519,7 @@ public class OrganizationProviderImpl implements OrganizationProvider {
 		Condition condition = t1.field("id").gt(0L);
 
 		if (null != locator && null != locator.getAnchor())
-			condition = condition.and(t1.field("id").gt(locator.getAnchor()));
+			condition = condition.and(t1.field("id").lt(locator.getAnchor()));
 
 		Organization org = findOrganizationById(listCommand.getOrganizationId());
 
@@ -5598,9 +5598,8 @@ public class OrganizationProviderImpl implements OrganizationProvider {
 			locator.setAnchor(null);
 
 		if (result.size() >= pageSize) {
-			locator.setAnchor(result.get(result.size() - 1).getId());
 			result.remove(result.size() - 1);
-//			locator.setAnchor(result.get(result.size() - 1).getId());
+			locator.setAnchor(result.get(result.size() - 1).getId());
 		}
 		return result;
 	}
