59abefb46dba7b48458d5520bf29d15ad7e5c385
yanjun <jun.yan@zuolin.com>
2017-10-25 17:26:46 +0800

    fix

--- db/forum-2.4-delta-schema.sql
+++ db/forum-2.4-delta-schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE `eh_forum_categories` (
   `entry_id` bigint(20) NOT NULL COMMENT 'entry id',
   `name` varchar(255) DEFAULT NULL,
   `activity_entry_id` bigint(20) DEFAULT '0' COMMENT 'activity entry id',
-  `comment_flag` tinyint(4) NOT NULL DEFAULT '1' COMMENT 'support comment, 0-no, 1-yes',
+  `interact_flag` tinyint(4) NOT NULL DEFAULT '1' COMMENT 'support interact, 0-no, 1-yes',
   `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -14,4 +14,4 @@ CREATE TABLE `eh_forum_categories` (
 
 ALTER TABLE `eh_forum_posts` ADD COLUMN `forum_entry_id`  bigint(20) NULL DEFAULT 0 COMMENT 'forum_category  entry_id' ;
 
-ALTER TABLE `eh_forum_posts` ADD COLUMN `comment_flag`  tinyint(4) NULL DEFAULT 1 COMMENT 'support comment, 0-no, 1-yes' ;
+ALTER TABLE `eh_forum_posts` ADD COLUMN `interact_flag`  tinyint(4) NULL DEFAULT 1 COMMENT 'support interact, 0-no, 1-yes' ;
