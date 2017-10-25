e5a860bb7ba09f10c1d63f2a873802106ba05281
wuhan <han.wu@zuolin.com>
2017-10-25 16:32:47 +0800

    fix bug

--- ehcore/src/main/java/com/everhomes/techpark/punch/PunchServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/techpark/punch/PunchServiceImpl.java
@@ -5011,8 +5011,10 @@ public class PunchServiceImpl implements PunchService {
 				for (PunchRule pr : punchRules) {
 					this.coordinationProvider.getNamedLock(CoordinationLocks.REFRESH_PUNCH_RULE.getCode()+pr.getId()).enter(() -> {
 						try {
-							
+							LOGGER.debug("begin! prid = "+pr.getId()+" thread :" + Thread.currentThread().getName());
+							Thread.sleep(6000);
 							if(!pr.getStatus().equals(PunchRuleStatus.ACTIVE.getCode())){
+								
 								Organization org = organizationProvider.findOrganizationById(pr.getOwnerId());
 								//对ptr表,psd表的处理
 								processTimeRule2Active(pr); 
