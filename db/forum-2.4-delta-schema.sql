42320eb5605a67c1fec44ef78e1833929b9dcd17
yanjun <jun.yan@zuolin.com>
2017-10-25 13:43:59 +0800

    fix

--- db/forum-2.4-delta-schema.sql
+++ db/forum-2.4-delta-schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `eh_forum_categories` (
-`id`  bigint(20) NOT NULL ,
-`uuid`  varchar(128) NOT NULL ,
-`namespace_id`  int(11) NOT NULL ,
-`forum_id`  bigint(20) NOT NULL COMMENT 'forum id' ,
-`entry_id`  bigint(20) NOT NULL COMMENT 'entry id' ,
-`name`  varchar(255) NULL ,
-`activity_entry_id`  bigint(20) NULL DEFAULT 0 COMMENT 'activity entry id' ,
-`comment_flag`  tinyint(4) NOT NULL DEFAULT 1 COMMENT 'support comment, 0-no, 1-yes',
-`create_time`  datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP ,
-`update_time`  datetime NULL DEFAULT '' ON UPDATE CURRENT_TIMESTAMP ,
-PRIMARY KEY (`id`)
-);
+  `id` bigint(20) NOT NULL,
+  `uuid` varchar(128) NOT NULL,
+  `namespace_id` int(11) NOT NULL,
+  `forum_id` bigint(20) NOT NULL COMMENT 'forum id',
+  `entry_id` bigint(20) NOT NULL COMMENT 'entry id',
+  `name` varchar(255) DEFAULT NULL,
+  `activity_entry_id` bigint(20) DEFAULT '0' COMMENT 'activity entry id',
+  `comment_flag` tinyint(4) NOT NULL DEFAULT '1' COMMENT 'support comment, 0-no, 1-yes',
+  `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `eh_forum_posts` ADD COLUMN `forum_entry_id`  bigint(20) NULL DEFAULT 0 COMMENT 'forum_category  entry_id' ;
