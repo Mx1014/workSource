99a3ab42f1a4bad1b5e3d43f9f9f53c06408e386
siting <st.zheng@zuolin.com>
2017-10-26 20:46:16 +0800

    update

--- db/search/news.sh
+++ db/search/news.sh
@@ -6,91 +6,102 @@ curl -XDELETE http://$ELASTIC/$INDEX/_mapping/news
 
 curl -XPUT "http://$ELASTIC/$INDEX/_mapping/news" -d '
 {
-    "news": {
-        "properties": {
-            "id": {
-                "type": "long"
-            },
-            "namespaceId": {
-                "type": "integer"
-            },
-            "ownerType": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "ownerId": {
-                "type": "long"
-            },
-            "title": {
-                "type": "string",
-                "index_analyzer": "ansj_index",
-                "search_analyzer": "ansj_query",
-                "similarity": "BM25",
-                "store": "yes"
-            },
-            "author": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "coverUri": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "contentType": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "content": {
-                "type": "string",
-                "index_analyzer": "ansj_index",
-                "search_analyzer": "ansj_query",
-                "similarity": "BM25",
-                "store": "yes"
-            },
-            "contentAbstract": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "sourceDesc": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "sourceUrl": {
-                "type": "string",
-                "index": "not_analyzed"
-            },
-            "childCount": {
-                "type": "long"
-            },
-            "forwardCount": {
-                "type": "long"
-            },
-            "likeCount": {
-                "type": "long"
-            },
-            "viewCount": {
-                "type": "long"
-            },
-            "publishTime": {
-                "type": "date"
-            },
-            "topIndex": {
-                "type": "long"
-            },
-            "topFlag": {
-                "type": "byte"
-            },
-            "status": {
-                "type": "byte"
-            },
-            "creatorUid": {
-                "type": "long"
-            },
-            "createTime": {
-                "type": "date"
+     "news" : {
+            "properties" : {
+              "author" : {
+                "type" : "string"
+              },
+              "categoryId" : {
+                "type" : "long"
+              },
+              "childCount" : {
+                "type" : "long"
+              },
+              "communityIds" : {
+                "type" : "long"
+              },
+              "content" : {
+                "type" : "string"
+              },
+              "contentAbstract" : {
+                "type" : "string"
+              },
+              "contentType" : {
+                "type" : "string"
+              },
+              "coverUri" : {
+                "type" : "string"
+              },
+              "createTime" : {
+                "type" : "long"
+              },
+              "creatorUid" : {
+                "type" : "long"
+              },
+              "deleteTime" : {
+                "type" : "long"
+              },
+              "deleterUid" : {
+                "type" : "long"
+              },
+              "description" : {
+                "type" : "string"
+              },
+              "forwardCount" : {
+                "type" : "long"
+              },
+              "id" : {
+                "type" : "long"
+              },
+              "likeCount" : {
+                "type" : "long"
+              },
+              "namespaceId" : {
+                "type" : "long"
+              },
+              "ownerId" : {
+                "type" : "long"
+              },
+              "ownerType" : {
+                "type" : "string"
+              },
+              "phone" : {
+                "type" : "string"
+              },
+              "publishTime" : {
+                "type" : "long"
+              },
+              "sourceDesc" : {
+                "type" : "string"
+              },
+              "sourceUrl" : {
+                "type" : "string"
+              },
+              "status" : {
+                "type" : "long"
+              },
+              "tag" : {
+                "type" : "string",
+                "index" : "not_analyzed"
+              },
+              "title" : {
+                "type" : "string"
+              },
+              "topFlag" : {
+                "type" : "long"
+              },
+              "topIndex" : {
+                "type" : "long"
+              },
+              "viewCount" : {
+                "type" : "long"
+              },
+              "visibleType" : {
+                "type" : "string"
+              }
             }
-        }
-    }
+          }
+
 }
 '
 ~                                                                                                                                                                                                                                                                             
