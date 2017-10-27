b8fee80e41ad4530275bf1cb4a3421b9671bcf83
ying.xiong <ying.xiong@zuolin.com>
2017-10-27 16:08:06 +0800

    add component

--- ehcore/src/main/java/com/everhomes/organization/pm/DefaultChargingItemProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/organization/pm/DefaultChargingItemProviderImpl.java
@@ -1,10 +1,13 @@
 package com.everhomes.organization.pm;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 /**
  * Created by ying.xiong on 2017/10/27.
  */
+@Component
 public class DefaultChargingItemProviderImpl implements DefaultChargingItemProvider {
     @Override
     public List<DefaultChargingItemProperty> findByItemId(Long defaultChargingItemId) {
