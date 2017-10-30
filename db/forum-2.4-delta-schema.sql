cbdb71e719d33d12530d11745297b516c5b2e84f
yanjun <jun.yan@zuolin.com>
2017-10-30 10:17:10 +0800

    fix

--- db/forum-2.4-delta-schema.sql
+++ db/forum-2.4-delta-schema.sql
@@ -12,6 +12,18 @@ CREATE TABLE `eh_forum_categories` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `eh_forum_posts` ADD COLUMN `forum_entry_id`  bigint(20) NULL DEFAULT 0 COMMENT 'forum_category  entry_id' ;
+-- 是否支持评论功能
+CREATE TABLE `eh_interact_settings` (
+  `id` bigint(20) NOT NULL,
+  `uuid` varchar(128) NOT NULL,
+  `namespace_id` int(11) NOT NULL,
+  `forum_id` bigint(20) NOT NULL,
+  `type` varchar(32) NOT NULL COMMENT 'forum, activity, announcement',
+  `entry_id` bigint(20) DEFAULT NULL,
+  `interact_flag` tinyint(4) NOT NULL COMMENT 'support interact, 0-no, 1-yes',
+  `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `eh_forum_posts` ADD COLUMN `interact_flag`  tinyint(4) NOT NULL DEFAULT 1 COMMENT 'support interact, 0-no, 1-yes' ;
+ALTER TABLE `eh_forum_posts` ADD COLUMN `forum_entry_id`  bigint(20) NULL DEFAULT 0 COMMENT 'forum_category  entry_id' ;
