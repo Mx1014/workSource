22cf4449331f96cb5c1d2be0377b7582d3218447
wuhan <han.wu@zuolin.com>
2017-10-31 17:58:50 +0800

    add code

--- ehrest/src/main/java/com/everhomes/rest/general_approval/PostApprovalFormAbnormalPunchValue.java
+++ ehrest/src/main/java/com/everhomes/rest/general_approval/PostApprovalFormAbnormalPunchValue.java
@@ -4,15 +4,25 @@ import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
- * <li>abnormalDate: 异常日期</li>
+ * <li>abnormalDate: 异常日期 YYYY-MM-DD</li>
  * <li>abnormalItem: 异常班次</li>
+ * <li>abnormalReason: 异常事由</li>
+ * <li>punchIntervalNo: 异常是第几个班次</li>
+ * <li>punchType: 异常是上班/下班</li>
  * </ul>
  */
 public class PostApprovalFormAbnormalPunchValue {
 
 	private String abnormalDate;
-
+	
 	private String abnormalItem;
+
+	private String abnormalReason;
+	
+	private Integer punchIntervalNo;
+	
+	private Byte punchType;
+	
 
 	public String getAbnormalDate() {
 		return abnormalDate;
@@ -33,6 +43,30 @@ public class PostApprovalFormAbnormalPunchValue {
 	@Override
 	public String toString() {
 		return StringHelper.toJsonString(this);
+	}
+
+	public String getAbnormalReason() {
+		return abnormalReason;
+	}
+
+	public void setAbnormalReason(String abnormalReason) {
+		this.abnormalReason = abnormalReason;
+	}
+
+	public Integer getPunchIntervalNo() {
+		return punchIntervalNo;
+	}
+
+	public void setPunchIntervalNo(Integer punchIntervalNo) {
+		this.punchIntervalNo = punchIntervalNo;
+	}
+
+	public Byte getPunchType() {
+		return punchType;
+	}
+
+	public void setPunchType(Byte punchType) {
+		this.punchType = punchType;
 	}
 
 }
