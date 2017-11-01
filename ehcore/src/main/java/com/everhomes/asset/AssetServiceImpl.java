532bbaf2b425c2ad2194852b8e11d3f7a22cbe64
wentian <wentian.wang@zuolin.com>
2017-11-01 14:09:36 +0800

    xx

--- ehcore/src/main/java/com/everhomes/asset/AssetServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/asset/AssetServiceImpl.java
@@ -1074,7 +1074,7 @@ public class AssetServiceImpl implements AssetService {
                             newBill.setChargeStatus((byte)0);
                         }
                         try{
-                            x_v = sdf_dateStrD.parse(newBill.getDateStrEnd());
+                            x_v = sdf_dateStrD.parse(newBill.getDateStrDue());
                             if(today.compareTo(x_v)!=-1){
                                 newBill.setNextSwitch((byte)1);
                             }else{
