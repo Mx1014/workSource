2ab486af23bf9b76be7bb7092a378461b4f51959
wentian <wentian.wang@zuolin.com>
2017-11-01 16:19:10 +0800

    save

--- db/4.10.4-delta-schema.sql
+++ db/4.10.4-delta-schema.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `eh_payment_bills` ADD COLUMN `next_switch` TINYINT DEFAULT 0 COMMENT '下一次switch的值';
-ALTER TABLE `eh_payment_contract_receiver` ADD COLUMN ``
+ALTER TABLE `eh_payment_contract_receiver` ADD COLUMN `in_work` TINYINT DEFAULT 0 COMMENT '0:工作完成；1：正在生成';
+ALTER TABLE `eh_payment_contract_receiver` ADD COLUMN `is_recorder` TINYINT DEFAULT 1 COMMENT '0：合同状态记录者，不保存计价数据；1：不是合同状态记录者';
