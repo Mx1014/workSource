c6ea4fd8986fa169ba3995590d5686b0320b991d
wentian <wentian.wang@zuolin.com>
2017-11-02 17:36:18 +0800

    merged with assetv2 settting

--- ehnextgen-api/src/main/java/com/everhomes/asset/AssetErrorCodes.java
+++ ehnextgen-api/src/main/java/com/everhomes/asset/AssetErrorCodes.java
@@ -9,4 +9,6 @@ public class AssetErrorCodes {
     static final String SCOPE = "assetv2";
 
     static final int ERROR_IN_GENERATING = 10001;
+    static final int NO_COMMUNITY_CHOSE = 10002;
+    static final int FAIL_IN_GENERATION = 10003;
 }
