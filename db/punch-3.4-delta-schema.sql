f2d631c1e9fbce892a4667c36c66c8e450d1685a
wuhan <han.wu@zuolin.com>
2017-11-02 20:55:19 +0800

    fix bug

--- db/punch-3.4-delta-schema.sql
+++ db/punch-3.4-delta-schema.sql
@@ -4,3 +4,5 @@ ALTER TABLE `eh_punch_exception_requests` ADD COLUMN `end_time` DATETIME COMMENT
 ALTER TABLE `eh_punch_exception_requests` ADD COLUMN `approval_attribute` VARCHAR(128) COMMENT 'DEFAULT,CUSTOMIZE';
 
 ALTER TABLE `eh_punch_exception_approvals` ADD COLUMN `punch_type` TINYINT DEFAULT 2 COMMENT ' 0- 上班打卡 ; 1- 下班打卡';  
+
+ALTER TABLE `eh_punch_logs` ADD COLUMN `approval_status` TINYINT DEFAULT NULL COMMENT '校正后的打卡状态 0-正常';
