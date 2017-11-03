11cd38099fe403c1c8e0e6e313094b963b58c5c3
yanjun <jun.yan@zuolin.com>
2017-11-03 15:56:37 +0800

    fix

--- db/club-3.2-delta-schema.sql
+++ db/club-3.2-delta-schema.sql
@@ -7,3 +7,22 @@ CREATE TABLE `eh_guild_types` (
   `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `eh_guild_applies` (
+  `id` bigint(20) NOT NULL,
+  `uuid` varchar(128) NOT NULL,
+  `namespace_id` int(11) NOT NULL,
+  `group_id` bigint(22) NOT NULL,
+  `applicant_uid` bigint(22) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(18) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `organization_name` varchar(255) DEFAULT NULL,
+  `registered_capital` varchar(255) DEFAULT NULL,
+  `industry` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `update_uid` bigint(22) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-applying,1-reject,2-agree',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
