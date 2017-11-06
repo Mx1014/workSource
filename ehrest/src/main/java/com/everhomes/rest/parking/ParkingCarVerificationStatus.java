39db66be4566b0026fac0f4015cec4aae40c9fa9
wen.sun <wen.sun@zuolin.com>
2017-11-06 11:33:07 +0800

    parking

--- ehrest/src/main/java/com/everhomes/rest/parking/ParkingCarVerificationStatus.java
+++ ehrest/src/main/java/com/everhomes/rest/parking/ParkingCarVerificationStatus.java
@@ -3,13 +3,15 @@ package com.everhomes.rest.parking;
 
 /**
  * <ul>停车充值 卡状态
- * <li>FAILED(1): 认证失败</li>
- * <li>AUDITING(2): 审核中</li>
- * <li>SUCCEED(3): 已认证</li>
+ * <li>FAILED(0): 认证失败</li>
+ * <li>AUDITING(1): 审核中</li>
+ * <li>SUCCEED(2): 已认证</li>
+ * <li>INACTIVE(3): 无效</li>
+ * <li>UN_AUTHORIZED(4): 未认证</li>
  * </ul>
  */
 public enum ParkingCarVerificationStatus {
-    FAILED((byte)0), AUDITING((byte)1), SUCCEED((byte)2), INACTIVE((byte)3);
+    FAILED((byte)0), AUDITING((byte)1), SUCCEED((byte)2), INACTIVE((byte)3), UN_AUTHORIZED((byte)4);
 
     private byte code;
 
