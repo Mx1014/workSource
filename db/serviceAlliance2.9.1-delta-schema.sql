4e895c1360cef88475239af109071408e1d3c55d
dengs <shuang.deng@zuolin.com>
2017-11-06 16:53:18 +0800

    serviceAlliance2.9.2 api

---
+++ db/serviceAlliance2.9.1-delta-schema.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `eh_service_alliances` ADD COLUMN `online_service_uid` BIGINT COMMENT 'online service user id';
+ALTER TABLE `eh_service_alliances` ADD COLUMN `online_service_uname` varchar(64) COMMENT 'online service user name';
