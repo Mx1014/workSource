50fc4d45c92d0d2fe7d3c1865fc6ffa8a9113ced
Ryan <nan.rong@zuolin.com>
2017-11-08 14:35:14 +0800

    approval route added

--- ehrest/src/main/java/com/everhomes/rest/techpark/punch/CheckAbnormalStatusResponse.java
+++ ehrest/src/main/java/com/everhomes/rest/techpark/punch/CheckAbnormalStatusResponse.java
@@ -3,14 +3,16 @@ package com.everhomes.rest.techpark.punch;
 import com.everhomes.util.StringHelper;
 
 /**
-* <ul>  
-* <li>abnormalStatus：DELETED(0)删除状态,INVALID(1)无效状态,RUNNING(2)运行状态 {@link com.everhomes.rest.general_approval.GeneralApprovalStatus}</li>
-* </ul>
-*/
+ * <ul>
+ * <li>abnormalStatus: DELETED(0)删除状态,INVALID(1)无效状态,RUNNING(2)运行状态 {@link com.everhomes.rest.general_approval.GeneralApprovalStatus}</li>
+ * <li>approvalRoute: 打卡异常审批路由（开启时返回）</li>
+ * </ul>
+ */
 public class CheckAbnormalStatusResponse {
-	private Byte abnormalStatus; 
-	
-	
+    private Byte abnormalStatus;
+
+    private String approvalRoute;
+
 
     @Override
     public String toString() {
@@ -18,18 +20,20 @@ public class CheckAbnormalStatusResponse {
     }
 
 
-
-	public Byte getAbnormalStatus() {
-		return abnormalStatus;
-	}
-
+    public Byte getAbnormalStatus() {
+        return abnormalStatus;
+    }
 
 
-	public void setAbnormalStatus(Byte abnormalStatus) {
-		this.abnormalStatus = abnormalStatus;
-	}
+    public void setAbnormalStatus(Byte abnormalStatus) {
+        this.abnormalStatus = abnormalStatus;
+    }
 
- 
+    public String getApprovalRoute() {
+        return approvalRoute;
+    }
 
-  
+    public void setApprovalRoute(String approvalRoute) {
+        this.approvalRoute = approvalRoute;
+    }
 }
