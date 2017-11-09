3b145ee6beab0553051244c3c38430b063d24bee
Ryan <nan.rong@zuolin.com>
2017-11-09 10:51:58 +0800

    sql fixed for 85

--- db/approval-1.6-delta-data-debug.sql
+++ db/approval-1.6-delta-data-debug.sql
@@ -20,10 +20,11 @@ INSERT INTO `eh_general_approval_templates`(`id`, `namespace_id`, `owner_id`, `o
 -- ended by R
 
 -- added for 10.1.10.85
-INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (1926, 52000, '', 'EhNamespaces', 999975, 2);
-INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (10891, 52010, '审批记录', 'EhNamespaces', 999975, 2);
-INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (10892, 52020, '规则设置', 'EhNamespaces', 999975, 2);
-INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (10893, 52030, '表单管理', 'EhNamespaces', 999975, 2);
+SET @scope_id = (SELECT MAX(id) from `eh_web_menu_scopes`)
+INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52000, '', 'EhNamespaces', 999975, 2);
+INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52010, '审批记录', 'EhNamespaces', 999975, 2);
+INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52020, '规则设置', 'EhNamespaces', 999975, 2);
+INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52030, '表单管理', 'EhNamespaces', 999975, 2);
 
 SET @category_id = (SELECT MAX(id) FROM `eh_configurations`);
 INSERT INTO `eh_approval_categories` (`id`, `namespace_id`, `owner_type`, `owner_id`, `approval_type`, `category_name`, `status`, `creator_uid`, `create_time`, `update_time`, `operator_uid`) VALUES (@category_id := @category_id + 1, '0', NULL, '0', '1', '调休', '2', '212500', '2017-11-03 09:00:05', '2017-11-03 09:00:05', '212500');
