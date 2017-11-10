2c29114fa06a9423d56e25cf6589137a2fad6cc8
dengs <shuang.deng@zuolin.com>
2017-11-10 17:36:38 +0800

    fix

--- db/news-temp-delta-data-release.sql
+++ db/news-temp-delta-data-release.sql
@@ -3,6 +3,7 @@ update eh_news_categories SET entry_id = 1 WHERE id in (2);
 
 --添加菜单：通知公告----
 set @menu_id=13000;
+set @namespace_id = 999975;
 -- 设置菜单
 INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES (@menu_id, '通知公告', '10000', NULL, 'news_management/1', '0', '2', CONCAT('/10000/',@menu_id), 'park', '181', '10800', '2', NULL, 'module');
 -- 设置菜单权限
@@ -18,7 +19,6 @@ INSERT INTO `eh_web_menu_privileges` (`id`, `privilege_id`, `menu_id`, `name`, `
 update eh_web_menus SET data_type = 'news_management/0' WHERE id = 10800;
 
 -- 荣超股份的配置通知公告到app和后台
-set @namespace_id = 999975;
 INSERT INTO `eh_news_categories` (`id`, `owner_type`, `owner_id`, `parent_id`, `name`, `path`, `default_order`, `status`, `creator_uid`, `create_time`, `delete_uid`, `delete_time`, `namespace_id`, `logo_uri`, `entry_id`) VALUES ('4', '', '0', '0', '通知公告', NULL, NULL, '0', '1', now(), '0', NULL, @namespace_id, '', '1');
 update eh_launch_pad_layouts SET version_code = '2017111001',layout_json='{\"versionCode\":\"2017111001\",\"layoutName\":\"ServiceMarketLayout\",\"displayName\":\"服务市场\",\"groups\":[{\"groupName\":\"\",\"widget\":\"Banners\",\"instanceConfig\":{\"itemGroup\":\"Default\"},\"style\":\"Default\",\"defaultOrder\":1},{\"groupName\":\"\",\"widget\":\"Bulletins\",\"instanceConfig\":{\"itemGroup\":\"Default\"},\"style\":\"Default\",\"defaultOrder\":3},{\"groupName\":\"商家服务\",\"widget\":\"Navigator\",\"instanceConfig\":{\"itemGroup\":\"Bizs\",\"paddingTop\":1,\"paddingLeft\":1,\"paddingBottom\":1,\"paddingRight\":1},\"style\":\"Default\",\"defaultOrder\":5},{\"groupName\":\"\",\"widget\":\"News\",\"instanceConfig\":{\"itemGroup\":\"Default\",\"categoryId\":4,\"timeWidgetStyle\":\"datetime\"},\"style\":\"Default\",\"defaultOrder\":5}]}' WHERE namespace_id = '999975';
 -- 菜单范围添加
