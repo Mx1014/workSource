e406154bb6e8aab73044bd567317c70eddbc6bd7
jiarui <rui.jia@zuolin.com>
2017-11-10 19:27:07 +0800

    modify updateEquipmentStandardCommand add repeatType items equipmentIds

--- ehrest/src/main/java/com/everhomes/rest/equipment/UpdateEquipmentStandardCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/UpdateEquipmentStandardCommand.java
@@ -1,12 +1,11 @@
 package com.everhomes.rest.equipment;
 
-import java.util.List;
-
-import javax.validation.constraints.NotNull;
-
 import com.everhomes.discover.ItemType;
 import com.everhomes.rest.repeat.RepeatSettingsDTO;
 import com.everhomes.util.StringHelper;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * <ul>
@@ -16,15 +15,18 @@ import com.everhomes.util.StringHelper;
  *  <li>targetId: 标准所属项目id</li>
  *  <li>targetType: 标准所属项目类型</li>
  *  <li>name: 标准名称</li>
- *  <li>standardNumber: 标准编号</li>
+ *  <li>repeatType: 0:no repeat 1: by day 2:by week 3: by month 4:year</li>
+ *  <li>standardNumber: 标准编号 { 物业巡检V3.2删除}</li>
  *  <li>standardSource: 标准来源</li>
- *  <li>description: 具体内容</li>
- *  <li>remarks: 备注</li>
+ *  <li>description: 具体内容 { 物业巡检V3.2删除}</li>
+ *  <li>remarks: 备注 { 物业巡检V3.2删除}</li>
  *  <li>standardType: 标准类别 参考{@link com.everhomes.rest.equipment.StandardType}</li>
- *  <li>repeat: 执行周期 参考{@link com.everhomes.rest.repeat.RepeatSettingsDTO}</li>
+ *  <li>repeat: 执行周期 { 物业巡检V3.2删除} 参考{@link com.everhomes.rest.repeat.RepeatSettingsDTO}</li>
  *  <li>templateId: 巡检项模板id</li>
- *  <li>reviewExpiredDays: 审批过期时间限制（天）</li>
+ *  <li>reviewExpiredDays: 审批过期时间限制（天） { 物业巡检V3.2删除}</li>
  *  <li>inspectionCategoryId: 巡检对象类型id</li>
+ *  <li>items: 标准关联的巡检项目 参考{@link com.everhomes.rest.equipment.InspectionItemDTO}</li>
+ *  <li>equipmentsIds: 标准关联的设备id列表 </li>
  * </ul>
  */
 public class UpdateEquipmentStandardCommand {
@@ -37,34 +39,69 @@ public class UpdateEquipmentStandardCommand {
 	@NotNull
 	private String ownerType;
 
+	public Byte getRepeatType() {
+		return repeatType;
+	}
+
+	public void setRepeatType(Byte repeatType) {
+		this.repeatType = repeatType;
+	}
+
+	public List<InspectionItemDTO> getItems() {
+		return items;
+	}
+
+	public void setItems(List<InspectionItemDTO> items) {
+		this.items = items;
+	}
+
+	public List<Long> getEquipmentsIds() {
+		return equipmentsIds;
+	}
+
+	public void setEquipmentsIds(List<Long> equipmentsIds) {
+		this.equipmentsIds = equipmentsIds;
+	}
+
 	@NotNull
+
 	private Long targetId;
 
 	@NotNull
 	private String targetType;
 
 	private String name;
-	
-	private String standardNumber;
-	
+
+	private  Byte repeatType ;
+
 	private String standardSource;
-	
+
+	@Deprecated
+	private String standardNumber;
+	@Deprecated
 	private String description;
-	
+	@Deprecated
 	private String remarks;
-	
+	@Deprecated
 	private Byte standardType;
-	
+	@Deprecated
 	private RepeatSettingsDTO repeat;
 	
 	private Long templateId;
-	
+	@Deprecated
 	private Integer reviewExpiredDays;
 	
 	private Long inspectionCategoryId;
 	
+	@Deprecated
 	@ItemType(StandardGroupDTO.class)
 	private List<StandardGroupDTO> group;
+
+	@ItemType(InspectionItemDTO.class)
+	private  List<InspectionItemDTO>  items;
+
+	@ItemType(Long.class)
+	private List<Long> equipmentsIds;
 
 	public Long getTargetId() {
 		return targetId;
