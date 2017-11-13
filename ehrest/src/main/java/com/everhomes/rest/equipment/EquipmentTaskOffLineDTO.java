cac03af3aff992f541d6a4cbb564b795aac99ef4
jiarui <rui.jia@zuolin.com>
2017-11-13 16:09:47 +0800

    modify updateEquipmentStandards  API

---
+++ ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentTaskOffLineDTO.java
@@ -0,0 +1,9 @@
+package com.everhomes.rest.equipment;
+
+
+import java.util.List;
+
+public class EquipmentTaskOffLineDTO {
+    private  Long   TaskId;
+    private List<EquipmentStandardRelationOfflineDTO> equipmentStandardRelations;
+}
