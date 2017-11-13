b425b64592e3a2142c93f79d973804769eb64970
jiarui <rui.jia@zuolin.com>
2017-11-13 16:12:00 +0800

    modify updateEquipmentStandards  API

--- ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentTaskOffLineDTO.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentTaskOffLineDTO.java
@@ -1,9 +1,37 @@
 package com.everhomes.rest.equipment;
 
 
-import java.util.List;
+import com.everhomes.util.StringHelper;
 
+import java.util.List;
+/**
+ * <ul>
+ *  <li>taskId: 任务id</li>
+ *  <li>EquipmentStandardRelationOfflineDTO: 离线巡检对象关联DTO 参考{@link com.everhomes.rest.equipment.EquipmentStandardRelationOfflineDTO}</li>
+ * </ul>
+ */
 public class EquipmentTaskOffLineDTO {
     private  Long   TaskId;
     private List<EquipmentStandardRelationOfflineDTO> equipmentStandardRelations;
+
+    public Long getTaskId() {
+        return TaskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        TaskId = taskId;
+    }
+
+    public List<EquipmentStandardRelationOfflineDTO> getEquipmentStandardRelations() {
+        return equipmentStandardRelations;
+    }
+
+    public void setEquipmentStandardRelations(List<EquipmentStandardRelationOfflineDTO> equipmentStandardRelations) {
+        this.equipmentStandardRelations = equipmentStandardRelations;
+    }
+
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
 }
