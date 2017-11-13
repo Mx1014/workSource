813026a6c0aabaa3ce6c1cbeebc91113f2de2da8
jiarui <rui.jia@zuolin.com>
2017-11-13 18:07:30 +0800

    modify updateEquipmentStandards  API

--- ehrest/src/main/java/com/everhomes/rest/equipment/DeleteEquipmentPlanCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/DeleteEquipmentPlanCommand.java
@@ -1,15 +1,46 @@
 package com.everhomes.rest.equipment;
 
 
+import com.everhomes.util.StringHelper;
+
 /**
  * <ul>
- *  <li>id: 巡检计划id</li>
- *  <li>ownerId: 计划所属的主体id</li>
- *  <li>ownerType: 计划所属的主体，参考{@link com.everhomes.rest.quality.OwnerType}</li>
+ * <li>id: 巡检计划id</li>
+ * <li>ownerId: 计划所属的主体id</li>
+ * <li>ownerType: 计划所属的主体，参考{@link com.everhomes.rest.quality.OwnerType}</li>
  * </ul>
  */
 public class DeleteEquipmentPlanCommand {
-    private  Long  id;
+    private Long id;
+    private String ownerType;
+    private Long ownerId;
 
-    private  Long  ownerId;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwnerType() {
+        return ownerType;
+    }
+
+    public void setOwnerType(String ownerType) {
+        this.ownerType = ownerType;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
 }
