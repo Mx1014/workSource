64809471ff78b0dedbd91e7b866704babe16c3e6
jiarui <rui.jia@zuolin.com>
2017-11-13 17:51:34 +0800

    modify updateEquipmentStandards  API

---
+++ ehrest/src/main/java/com/everhomes/rest/equipment/DeleteEquipmentPlanCommand.java
@@ -0,0 +1,12 @@
+package com.everhomes.rest.equipment;
+
+
+/**
+ * <ul>
+ *  <li>id: 巡检计划id</li>
+ *  <li>ownerId: 计划所属的主体id</li>
+ *  <li>ownerType: 计划所属的主体，参考{@link com.everhomes.rest.quality.OwnerType}</li>
+ * </ul>
+ */
+public class DeleteEquipmentPlanCommand {
+}
