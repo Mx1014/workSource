98df814cd9654808bc5255e45c4ce9a7d5c97880
yanjun <jun.yan@zuolin.com>
2017-11-14 16:56:12 +0800

    fix

--- db/club-3.2-delta-data-release.sql
+++ db/club-3.2-delta-data-release.sql
@@ -4,7 +4,9 @@ INSERT INTO `ehcore`.`eh_configurations` (`id`, `name`, `value`, `description`, 
 
 INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('10760', '行业协会', '10000', NULL, 'groups/guild', '0', '2', '/10000/10760', 'park', '180', '10750', '2', NULL, 'module');
 INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('10761', '行业协会管理', '10760', NULL, 'groups_management/guild', '0', '2', '/10000/10760/10761', 'park', '181', '10750', '3', NULL, 'module');
-INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('10762', '审核行业协会', '10760', NULL, 'audit_groups/guild', '0', '2', '/10000/10760/10762', 'park', '182', '10750', '3', NULL, 'module');
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`) VALUES ('10762', '审核行业协会', '10760', NULL, 'club-audit/audit-list', '0', '2', '/10000/10760/10762', 'park', '182', '10750', '3', NULL, 'module');
+
+UPDATE eh_web_menus SET data_type = 'react:/club-audit/audit-list/1' where id = 10752;
 
 INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`, `instance_config`, `action_type`, `update_time`, `operator_uid`, `creator_uid`, `description`, `multiple_flag`) VALUES ('10760', '行业协会', '10000', '/10000/10760', '1', '2', '2', '0', NOW(), NULL, '38', NOW(), '0', '0', '0', '0');
 
