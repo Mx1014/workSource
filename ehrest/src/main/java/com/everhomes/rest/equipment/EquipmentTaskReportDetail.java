656fa2f47c6c93c1f11fd12a812aa0a66b235bd2
jiarui <rui.jia@zuolin.com>
2017-11-14 11:21:51 +0800

    modify updateEquipmentStandards  API

--- ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentTaskReportDetail.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentTaskReportDetail.java
@@ -9,12 +9,9 @@ import java.util.List;
 
 /**
  * <ul>
- *  <li>taskId: 任务id</li>
- *  <li>verificationResult: 上报结果  参考{@link com.everhomes.rest.equipment.EquipmentTaskResult}</li>
  *  <li>attachments: 上报内容图片   对应设备id   参考{@link  com.everhomes.rest.equipment.Attachment}</li>
+ *  <li>message: 上报信息</li>
  *  <li>itemResults: 设备参数 参考{@link com.everhomes.rest.equipment.InspectionItemResult}</li>
- *  <li>ownerId: 设备所属的主体id</li>
- *  <li>ownerType: 设备所属的主体，参考{@link com.everhomes.rest.quality.OwnerType}</li>
  * </ul>
  */
 public class EquipmentTaskReportDetail {
