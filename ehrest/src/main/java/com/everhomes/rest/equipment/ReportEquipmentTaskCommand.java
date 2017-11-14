185adad617bf4d1781dbc07cf0fe9a61aea63e4e
jiarui <rui.jia@zuolin.com>
2017-11-14 08:36:27 +0800

    modify updateEquipmentStandards  API

--- ehrest/src/main/java/com/everhomes/rest/equipment/ReportEquipmentTaskCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/ReportEquipmentTaskCommand.java
@@ -10,10 +10,9 @@ import java.util.List;
  * <ul>
  *  <li>taskId: 任务id</li>
  *  <li>verificationResult: 上报结果  参考{@link com.everhomes.rest.equipment.EquipmentTaskResult}</li>
- *  <li>attachments: 上报内容图片  上报内容文字 对应设备id   参考{@link  com.everhomes.rest.equipment.Attachment}</li>
- *  <li>itemResults: 设备参数 参考{@link com.everhomes.rest.equipment.InspectionItemResult}</li>
- *  <li>ownerId: 设备所属的主体id</li>
- *  <li>ownerType: 设备所属的主体，参考{@link com.everhomes.rest.quality.OwnerType}</li>
+ *  <li>ownerId: 任务所属的主体id</li>
+ *  <li>ownerType: 任务所属的主体，参考{@link com.everhomes.rest.quality.OwnerType}</li>
+ *  <li>equipmentTaskReportDetails: 任务下设备检查细项 {@link com.everhomes.rest.equipment.EquipmentTaskReportDetail}</li>
  * </ul>
  */
 public class ReportEquipmentTaskCommand {
@@ -28,14 +27,20 @@ public class ReportEquipmentTaskCommand {
 	
 	@NotNull
 	private String ownerType;
-	
+
+	@ItemType(EquipmentTaskReportDetail.class)
+	private  List<EquipmentTaskReportDetail>  equipmentTaskReportDetails;
+
+
+	@Deprecated
 	@NotNull
 	@ItemType(Attachment.class)
     private List<Attachment> attachments;
 
 	@Deprecated
 	private String message;
-	
+
+	@Deprecated
 	@ItemType(InspectionItemResult.class)
     private List<InspectionItemResult> itemResults; 
 	
