a0a79a9b9de2ce57018d844a1baee28712972ca1
nan.rong <nan.rong@zuolin.com>
2017-11-16 17:31:52 +0800

    bug fixed

--- ehcore/src/main/java/com/everhomes/archives/ArchivesServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/archives/ArchivesServiceImpl.java
@@ -2166,7 +2166,8 @@ public class ArchivesServiceImpl implements ArchivesService {
                 "    5、部门：上下级部门间用‘/'隔开，且从最上级部门开始，例如\"左邻/深圳研发中心/研发部\"；部门若为空，则自动将成员添加到选择的目录下；\n" +
                 "    6、手机：支持国内、国际手机号（国内手机号直接输入手机号即可；国际手机号必须包含加号以及国家地区码，格式示例：“+85259****24”）；\n" +
                 "    7、合同公司：合同公司若为空，将默认使用公司全称\n" +
-                "    8、若要删除某行信息，请右键行号，选择删除", (short) 18, (short) 4480);
+                "    8、若要删除某行信息，请右键行号，选择删除\n" +
+                "    9、注意日期格式为 xxxx-xx-xx", (short) 18, (short) 4480);
         excelUtils.setNeedSequenceColumn(false);
         excelUtils.setNeedTitleRemark(true);
     }
