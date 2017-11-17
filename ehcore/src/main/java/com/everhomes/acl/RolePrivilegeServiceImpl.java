66bbff88739f28e7f5ef4f7fec0aa4c7fa11af9f
lei.lv <lei.lv@zuolin.com>
2017-11-17 19:09:36 +0800

    fix

--- ehcore/src/main/java/com/everhomes/acl/RolePrivilegeServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/acl/RolePrivilegeServiceImpl.java
@@ -2817,8 +2817,8 @@ public class RolePrivilegeServiceImpl implements RolePrivilegeService {
 
 		if(null == modules){
 			LOGGER.error("This user has not been added to the administrator list.");
-			throw RuntimeErrorException.errorWith(PrivilegeServiceErrorCode.SCOPE, PrivilegeServiceErrorCode.ERROR_ADMINISTRATORS_LIST_NONEXISTS,
-					"This user has not been added to the administrator list.");
+//			throw RuntimeErrorException.errorWith(PrivilegeServiceErrorCode.SCOPE, PrivilegeServiceErrorCode.ERROR_ADMINISTRATORS_LIST_NONEXISTS,
+//					"This user has not been added to the administrator list.");
 		}
 
 		List<Authorization> authorizations = authorizationProvider.listManageAuthorizationsByTarget(cmd.getOwnerType(), cmd.getOwnerId(), cmd.getTargetType(), cmd.getTargetId(), EntityType.SERVICE_MODULE.getCode(), null);
