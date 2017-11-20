89e85c41c38fb022aa1e51f874d7802c13de4193
siting <st.zheng@zuolin.com>
2017-11-20 18:19:20 +0800

    pmtask-3.0.0

--- ehcore/src/main/java/com/everhomes/pmtask/PmtaskFlowModuleListener.java
+++ ehcore/src/main/java/com/everhomes/pmtask/PmtaskFlowModuleListener.java
@@ -319,7 +319,8 @@ public class PmtaskFlowModuleListener implements FlowModuleListener {
 		}
 		JSONObject jo = JSONObject.parseObject(JSONObject.toJSONString(dto));
 		jo.put("formUrl",processFormURL(EntityType.PM_TASK.getCode(),task.getId(),cmd.getOwnerType(),cmd.getOwnerId(),"费用确认"));
-		flowCase.setCustomObject(JSONObject.toJSONString(dto));
+
+		flowCase.setCustomObject(jo.toJSONString());
 
 		return entities;
 	}
