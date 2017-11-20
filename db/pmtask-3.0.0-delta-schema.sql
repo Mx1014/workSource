c93e43b0bb161945a0ca879439068e23447e75d0
siting <st.zheng@zuolin.com>
2017-11-20 18:45:25 +0800

    pmtask-3.0.0

--- db/pmtask-3.0.0-delta-schema.sql
+++ db/pmtask-3.0.0-delta-schema.sql
@@ -6,4 +6,4 @@ ALTER TABLE eh_flows ADD COLUMN `form_update_time` DATETIME COMMENT 'form update
 ALTER TABLE eh_flow_event_logs ADD COLUMN `extra` TEXT COMMENT 'extra data, json format';
 
 ALTER TABLE eh_flow_condition_expressions ADD COLUMN `variable_extra1` VARCHAR(256) COMMENT 'variable 1 extra';
-ALTER TABLE eh_flow_condition_expressions ADD 
+ALTER TABLE eh_flow_condition_expressions ADD COLUMN `variable_extra2` VARCHAR(256) COMMENT 'variable 2 extra';
