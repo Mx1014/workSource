16b34758a466367f7684bd19c68c4aa386154644
wen.sun <wen.sun@zuolin.com>
2017-11-21 15:46:31 +0800

    relocation

---
+++ ehrest/src/main/java/com/everhomes/rest/relocation/RelocationCommonStatus.java
@@ -0,0 +1,36 @@
+// @formatter:off
+package com.everhomes.rest.relocation;
+
+/**
+ * <ul>物品搬迁申请状态
+ * <li>CANCELED(0): 已取消</li>
+ * <li>COMPLETED(1): 处理中</li>
+ * <li>PROCESSING(2): 已完成</li>
+ * </ul>
+ */
+public enum RelocationRequestStatus {
+	CANCELED((byte)0), COMPLETED((byte)1), PROCESSING((byte)2);
+
+    private byte code;
+
+    private RelocationRequestStatus(byte code) {
+        this.code = code;
+    }
+    
+    public byte getCode() {
+        return this.code;
+    }
+    
+    public static RelocationRequestStatus fromCode(Byte code) {
+        if(code != null) {
+            RelocationRequestStatus[] values = RelocationRequestStatus.values();
+            for(RelocationRequestStatus value : values) {
+                if(value.code == code.byteValue()) {
+                    return value;
+                }
+            }
+        }
+        
+        return null;
+    }
+}
