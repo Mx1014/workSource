746687cf022bcf57c520026a04b7ef0d9888cace
wen.sun <wen.sun@zuolin.com>
2017-11-21 15:07:05 +0800

    relocation

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -2,8 +2,6 @@
 CREATE TABLE `eh_relocation_requests` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0,
-  `owner_type` varchar(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the standard, community, etc',
-  `owner_id` bigint(20) NOT NULL DEFAULT '0',
   `request_no` varchar(128) NOT NULL,
   `requestor_enterprise_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'the id of organization where the requestor is in',
   `requestor_entperise_name` varchar(64) DEFAULT NULL COMMENT 'the enterprise name of requestor',
@@ -24,8 +22,6 @@ CREATE TABLE `eh_relocation_requests` (
 CREATE TABLE `eh_relocation_request_items` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL DEFAULT 0,
-  `owner_type` varchar(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the standard, community, etc',
-  `owner_id` bigint(20) NOT NULL DEFAULT '0',
   `item_name` varchar(64) DEFAULT NULL COMMENT 'the name of item',
   `item_quantity` int(11) DEFAULT 0 COMMENT 'the quantity of item',
   `status` tinyint(4) DEFAULT NULL COMMENT '0: inactive, 1: , 2: active',
