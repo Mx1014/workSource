32cb81d4cecf94965cefa4d3a12bf73ca35447b1
yanjun <jun.yan@zuolin.com>
2017-11-22 23:33:50 +0800

    add sort num to service type

--- db/forum-2.6-delta-schema.sql
+++ db/forum-2.6-delta-schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE `eh_forum_service_types` (
   `id` bigint(20) NOT NULL,
   `namespace_id` int(11) NOT NULL DEFAULT '0',
   `service_type` varchar(255) NOT NULL,
+  `sort_num` int(11) NOT NULL DEFAULT '0',
   `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
