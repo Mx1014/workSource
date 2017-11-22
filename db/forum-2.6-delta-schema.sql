ab18a563f3567019e4d0d77e77f72057e96305b0
yanjun <jun.yan@zuolin.com>
2017-11-22 20:22:17 +0800

    fix

---
+++ db/forum-2.6-delta-schema.sql
@@ -0,0 +1,8 @@
+-- 论坛发布类型  add by yanjun 20171122
+CREATE TABLE `eh_forum_service_types` (
+  `id` bigint(20) NOT NULL,
+  `namespace_id` int(11) NOT NULL DEFAULT '0',
+  `service_type` varchar(255) NOT NULL,
+  `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
