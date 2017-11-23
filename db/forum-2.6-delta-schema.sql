a26a140deb7a9b5aa3dfac7e7c72b9379f167d44
yanjun <jun.yan@zuolin.com>
2017-11-23 19:40:16 +0800

    fix

--- db/forum-2.6-delta-schema.sql
+++ db/forum-2.6-delta-schema.sql
@@ -8,3 +8,6 @@ CREATE TABLE `eh_forum_service_types` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- 热门标签，增加模块类型，1-论坛、2-活动、3-俱乐部...等等  add by yanjun 20171123
+ALTER TABLE `eh_hot_tags`
+ADD COLUMN `module_type`  tinyint(4) NULL DEFAULT 1 COMMENT 'module type, 1-forum 2-activity...........';
