759366df1fd1249cf1afc0b685b8b62d8651268e
wen.sun <wen.sun@zuolin.com>
2017-11-23 17:02:00 +0800

    pmtask

---
+++ ehrest/src/main/java/com/everhomes/rest/pmtask/PmTaskAppType.java
@@ -0,0 +1,16 @@
+package com.everhomes.rest.pmtask;
+
+/**
+ *
+ * <ul>
+ * </ul>
+ */
+public interface PmTaskAppType {
+
+    //报修多入口id
+    long REPAIR_ID = 6;
+    //投诉建议多入口id
+    long SUGGESTION_ID = 9;
+
+
+}
