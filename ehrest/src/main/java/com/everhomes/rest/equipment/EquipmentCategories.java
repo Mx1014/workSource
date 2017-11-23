3cb39dfba71c7ce5971ba95b1866fdd45bc5c3b1
jiarui <rui.jia@zuolin.com>
2017-11-23 08:58:02 +0800

    fix eq categories

--- ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentCategories.java
+++ ehrest/src/main/java/com/everhomes/rest/equipment/EquipmentCategories.java
@@ -3,9 +3,12 @@ package com.everhomes.rest.equipment;
 import com.everhomes.util.StringHelper;
 
 public enum EquipmentCategories {
-    FIRE_CONTROL(200887L, "消防"), STRONG(200888L, "强电"), WEAK(200889L, "弱电"), ELEVATOR(202224L, "电梯"),
+    /*FIRE_CONTROL(200887L, "消防"), STRONG(200888L, "强电"), WEAK(200889L, "弱电"), ELEVATOR(202224L, "电梯"),
     AIR_CONDITIONER(202222L, "空调"), WATER(202223L, "给排水"), ROOM(203014L, "空置房"),TE(203015L,"装修"),SAFE(203016L,"安保"),
-    DAILY_CHECK(203017L,"日常工作检查"),COMMON_CHECK(203018L,"公共设施检查"),WEEEKDAY(203019L,"周末值班"),SAFTY_CHECK(203020L,"安全检查"),OTHERS(200890L,"其他");
+    DAILY_CHECK(203017L,"日常工作检查"),COMMON_CHECK(203018L,"公共设施检查"),WEEEKDAY(203019L,"周末值班"),SAFTY_CHECK(203020L,"安全检查"),OTHERS(200890L,"其他");*/
+    FIRE_CONTROL(1L, "消防"), STRONG(2L, "强电"), WEAK(3L, "弱电"), ELEVATOR(4L, "电梯"),
+    AIR_CONDITIONER(5L, "空调"), WATER(6L, "给排水"), ROOM(7L, "空置房"),TE(8L,"装修"),SAFE(9L,"安保"),
+    DAILY_CHECK(10L,"日常工作检查"),COMMON_CHECK(11L,"公共设施检查"),WEEEKDAY(12L,"周末值班"),SAFTY_CHECK(13L,"安全检查"),OTHERS(14L,"其他");
 
     private Long code;
     private String name;
