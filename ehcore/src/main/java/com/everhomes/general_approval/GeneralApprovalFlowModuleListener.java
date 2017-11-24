af942aa63dba548f0e8a8f9bb0741ea887f3e7c2
nan.rong <nan.rong@zuolin.com>
2017-11-24 16:21:57 +0800

    content fixed

--- ehcore/src/main/java/com/everhomes/general_approval/GeneralApprovalFlowModuleListener.java
+++ ehcore/src/main/java/com/everhomes/general_approval/GeneralApprovalFlowModuleListener.java
@@ -140,7 +140,7 @@ public class GeneralApprovalFlowModuleListener implements FlowModuleListener {
                 break;
             String key = entities.get(i).getKey();
             //  将"null"屏蔽为空字符串
-            String value = entities.get(i).getValue() != null ? entities.get(i).getValue() : "无";
+            String value = StringUtils.isEmpty(entities.get(i).getValue()) ? "无" : entities.get(i).getValue() ;
             content += (key + " : " + value + "\n");
         }
         content = content.substring(0,content.length()-1);
