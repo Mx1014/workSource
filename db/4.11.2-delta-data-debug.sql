b1c6d4521c33fb63da066bcd3f57d58be0932ac7
nan.rong <nan.rong@zuolin.com>
2017-11-28 09:53:15 +0800

    sql update

--- db/4.11.2-delta-data-debug.sql
+++ db/4.11.2-delta-data-debug.sql
@@ -1,4 +1,4 @@
--- added by R for approval-1.6 start.
+-- added by nan.rong for approval-1.6 start.
 SET @scope_id = (SELECT MAX(id) from `eh_web_menu_scopes`);
 INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52000, '', 'EhNamespaces', 999975, 2);
 INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52010, '审批记录', 'EhNamespaces', 999975, 2);
@@ -9,7 +9,7 @@ INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `ow
 INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52010, '审批记录', 'EhNamespaces', 1, 2);
 INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52020, '规则设置', 'EhNamespaces', 1, 2);
 INSERT INTO `eh_web_menu_scopes`(`id`, `menu_id`, `menu_name`, `owner_type`, `owner_id`, `apply_policy`) VALUES (@scope_id := @scope_id +1, 52030, '表单管理', 'EhNamespaces', 1, 2);
--- added by R end.
+-- added by nan.rong end.
 
 
 -- club 3.2 by yanjun 201711211617
