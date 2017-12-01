2cb7f41e19ac6931b0a9f6f76062141e33d567a0
lei.lv <lei.lv@zuolin.com>
2017-12-01 18:08:35 +0800

    fix

--- ehcore/src/main/java/com/everhomes/module/ServiceModuleServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/module/ServiceModuleServiceImpl.java
@@ -953,8 +953,10 @@ public class ServiceModuleServiceImpl implements ServiceModuleService {
                         //给一级模块设置APPS
                         // 这里因为运营后台的不完善，暂时使用reflectionServiceModuleApps代替真正的serviceModuleApps。原来的代码为serviceModuleAppProvider.listServiceModuleAppsByModuleIds(UserContext.getCurrentNamespaceId(), moduleIds);
                         List<ServiceModuleAppDTO> apps = serviceModuleProvider.listReflectionServiceModuleAppsByModuleIds(UserContext.getCurrentNamespaceId(), moduleIds);
-                        current.setServiceModuleApps(apps);
-                        LOGGER.debug(current.getName()+ "分类下一共有"+moduleIds.toString() +"的模块和"+ apps.size() + "个应用");
+                        if(apps != null){
+                            current.setServiceModuleApps(apps);
+                            LOGGER.debug(current.getName()+ "分类下一共有"+moduleIds.toString() +"的模块和"+ apps.size() + "个应用");
+                        }
                     }
                 }
                 results.add(current);
