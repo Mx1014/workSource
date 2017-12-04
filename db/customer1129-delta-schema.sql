df8265fae2deeeb2926f5bd515ade88ed14f52f8
ying.xiong <ying.xiong@zuolin.com>
2017-12-04 13:53:43 +0800

    add schema

--- db/customer1129-delta-schema.sql
+++ db/customer1129-delta-schema.sql
@@ -7,12 +7,21 @@ ALTER TABLE `eh_customer_commercials` ADD COLUMN `shareholder_name` VARCHAR(256)
 ALTER TABLE `eh_customer_commercials` ADD COLUMN `actual_capital_injection_situation` VARCHAR(256) COMMENT '实际注资情况';
 ALTER TABLE `eh_customer_commercials` ADD COLUMN `shareholding_situation` VARCHAR(256) COMMENT '股权占比情况';
 
-CREATE TABLE `eh_service_module_exclude_privileges` (
+CREATE TABLE `eh_service_module_functions` (
+  `id` BIGINT NOT NULL,
+  `module_id` BIGINT NOT NULL DEFAULT 0,
+  `privilege_id` BIGINT NOT NULL DEFAULT 0,
+  `explain` VARCHAR(64) NOT NULL DEFAULT 0,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `eh_service_module_exclude_functions` (
   `id` BIGINT NOT NULL,
   `namespace_id` INTEGER NOT NULL DEFAULT 0,
   `community_id` BIGINT,
   `module_id` BIGINT NOT NULL DEFAULT 0,
-  `privilege_id` BIGINT NOT NULL DEFAULT 0,
+  `function_id` BIGINT NOT NULL DEFAULT 0,
   
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
