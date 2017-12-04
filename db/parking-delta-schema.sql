5b3e65aa54eefd33d9acc4e662ca20368854deb5
wen.sun <wen.sun@zuolin.com>
2017-12-04 14:28:22 +0800

    relocation

--- db/parking-delta-schema.sql
+++ db/parking-delta-schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE `eh_relocation_requests` (
   `flow_case_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'flow case id',
   `cancel_time` datetime DEFAULT NULL,
   `cancel_uid` bigint(20) NOT NULL DEFAULT '0' COMMENT 'cancel user id',
-  `qr_id` varchar(256) DEFAULT NULL COMMENT 'id of the qr record',
+  `qr_code_url` varchar(256) DEFAULT NULL COMMENT 'url of the qr record',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
