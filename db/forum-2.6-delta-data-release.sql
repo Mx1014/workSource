60acc10914211dbebef71e706cf6e709105469fc
yanjun <jun.yan@zuolin.com>
2017-12-05 21:38:05 +0800

    fix

--- db/forum-2.6-delta-data-release.sql
+++ db/forum-2.6-delta-data-release.sql
@@ -1,10 +1,10 @@
 -- 在post表中更新模块和入口信息
 -- 活动帖子
-UPDATE eh_forum_posts set module_type = 2, module_category_id = activity_category_id where activity_category_id IS NO NULL and activity_category_id != 0;
+UPDATE eh_forum_posts set module_type = 2, module_category_id = activity_category_id where activity_category_id IS NOT NULL and activity_category_id != 0;
 -- 论坛帖子
 UPDATE eh_forum_posts set module_type = 1, module_category_id = forum_entry_id where forum_entry_id != null and category_id != 1003;
 -- 公告帖子
-UPDATE eh_forum_posts set module_type = 3 where category_id == 1003;
+UPDATE eh_forum_posts set module_type = 3 where category_id = 1003;
 
 -- 我-我的发布，按钮是否需要展示
 SET @id = (SELECT MAX(id) from eh_configurations);
