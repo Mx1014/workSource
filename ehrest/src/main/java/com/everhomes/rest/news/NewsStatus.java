efad28b7db255cab3e06fb4c0e4bb7fd87c634cb
dengs <shuang.deng@zuolin.com>
2017-12-06 13:52:53 +0800

    news1.6 api

--- ehrest/src/main/java/com/everhomes/rest/news/NewsStatus.java
+++ ehrest/src/main/java/com/everhomes/rest/news/NewsStatus.java
@@ -3,13 +3,13 @@ package com.everhomes.rest.news;
 
 /**
  * <ul>新闻状态
- * <li>INACTIVE: 失效</li>
- * <li>WAITING_FOR_CONFIRMATION: 等待审批</li>
- * <li>ACTIVE: 有效</li>
+ * <li>INACTIVE: 0 删除状态</li>
+ * <li>DRAFT: 1 草稿状态</li>
+ * <li>ACTIVE: 2 发布状态</li>
  * </ul>
  */
 public enum NewsStatus {
-	INACTIVE((byte)0), WAITING_FOR_CONFIRMATION((byte)1), ACTIVE((byte)2);
+	INACTIVE((byte)0), DRAFT((byte)1), ACTIVE((byte)2);
 	private byte code;
 	private NewsStatus(byte code) {
 		this.code = code;
