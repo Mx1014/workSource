dc1509f441df42f1760e88275d9da06a618215e9
ying.xiong <ying.xiong@zuolin.com>
2017-12-06 10:47:20 +0800

    fix schema

--- db/customercd-delta-schema.sql
+++ db/customercd-delta-schema.sql
@@ -1,40 +1,40 @@
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TEXT COMMENT '创始人介绍';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` DATETIME COMMENT '企业成立时间';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '企业登记注册类型';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '企业所属技术领域';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '企业纳税人类型';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '是否愿意与创业导师建立辅导关系';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '是否高新技术企业';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '企业主要负责人创业特征';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` TINYINT COMMENT '企业主要负责人是否为连续创业者';
-ALTER TABLE `eh_enterprise_customers` ADD COLUMN `business_value` DECIMAL(10,2) COMMENT '获天使或风险投资总金额（万元）';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `founder_introduce` TEXT COMMENT '创始人介绍';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `founding_time` DATETIME COMMENT '企业成立时间';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `registration_type` BIGINT COMMENT '企业登记注册类型';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `technical_field` BIGINT COMMENT '企业所属技术领域';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `taxpayer_type` BIGINT COMMENT '企业纳税人类型';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `relation_willing` BIGINT COMMENT '是否愿意与创业导师建立辅导关系';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `high_and_new_tech` BIGINT COMMENT '是否高新技术企业';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `entrepreneurial_characteristics` BIGINT COMMENT '企业主要负责人创业特征';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `serial_entrepreneur` BIGINT COMMENT '企业主要负责人是否为连续创业者';
+ALTER TABLE `eh_enterprise_customers` ADD COLUMN `risk_investment_amount` DECIMAL(10,2) COMMENT '获天使或风险投资总金额（万元）';
 
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '从业人员总人数';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '大专';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '本科';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '硕士';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '博士';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '留学人员';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '千人计划人数';
-ALTER TABLE `eh_customer_talents` ADD COLUMN `business_value` INTEGER COMMENT '吸纳应届大学毕业生';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `total_employees` INTEGER COMMENT '从业人员总人数';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `junior_colleges` INTEGER COMMENT '大专';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `undergraduates` INTEGER COMMENT '本科';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `masters` INTEGER COMMENT '硕士';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `doctors` INTEGER COMMENT '博士';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `overseas` INTEGER COMMENT '留学人员';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `thousand_talents_program` INTEGER COMMENT '千人计划人数';
+ALTER TABLE `eh_customer_talents` ADD COLUMN `fresh_graduates` INTEGER COMMENT '吸纳应届大学毕业生';
 
-ALTER TABLE `eh_customer_trademarks` ADD COLUMN `business_value` INTEGER COMMENT '知识产权申请总数';
-ALTER TABLE `eh_customer_trademarks` ADD COLUMN `business_value` INTEGER COMMENT '申请发明专利数';
-ALTER TABLE `eh_customer_trademarks` ADD COLUMN `business_value` INTEGER COMMENT '知识产权授权总数';
-ALTER TABLE `eh_customer_trademarks` ADD COLUMN `business_value` INTEGER COMMENT '授权发明专利数
-';
-ALTER TABLE `eh_customer_patents` ADD COLUMN `business_value` INTEGER COMMENT '拥有有效知识产权总数';
-ALTER TABLE `eh_customer_patents` ADD COLUMN `business_value` INTEGER COMMENT '发明专利';
-ALTER TABLE `eh_customer_patents` ADD COLUMN `business_value` INTEGER COMMENT '软件著作权';
-ALTER TABLE `eh_customer_patents` ADD COLUMN `business_value` INTEGER COMMENT '集成电路布图';
+ALTER TABLE `eh_customer_trademarks` ADD COLUMN `applications` INTEGER COMMENT '知识产权申请总数';
+ALTER TABLE `eh_customer_trademarks` ADD COLUMN `patent_applications` INTEGER COMMENT '申请发明专利数';
+ALTER TABLE `eh_customer_trademarks` ADD COLUMN `authorizations` INTEGER COMMENT '知识产权授权总数';
+ALTER TABLE `eh_customer_trademarks` ADD COLUMN `patent_authorizations` INTEGER COMMENT '授权发明专利数';
 
-ALTER TABLE `eh_customer_certificates` ADD COLUMN `business_value` INTEGER COMMENT '购买国外专利';
-ALTER TABLE `eh_customer_certificates` ADD COLUMN `business_value` INTEGER COMMENT '技术合同交易数量';
-ALTER TABLE `eh_customer_certificates` ADD COLUMN `business_value` INTEGER COMMENT '技术合同交易总金额（万元）';
-ALTER TABLE `eh_customer_certificates` ADD COLUMN `business_value` INTEGER COMMENT '当年承担国家级科技计划项目数';
-ALTER TABLE `eh_customer_certificates` ADD COLUMN `business_value` INTEGER COMMENT '当年获得省级以上奖励';
+ALTER TABLE `eh_customer_patents` ADD COLUMN `effective_intellectual_properties` INTEGER COMMENT '拥有有效知识产权总数';
+ALTER TABLE `eh_customer_patents` ADD COLUMN `patents` INTEGER COMMENT '发明专利';
+ALTER TABLE `eh_customer_patents` ADD COLUMN `software_copyrights` INTEGER COMMENT '软件著作权';
+ALTER TABLE `eh_customer_patents` ADD COLUMN `ic_layout` INTEGER COMMENT '集成电路布图';
 
-ALTER TABLE `eh_customer_apply_projects` ADD COLUMN `business_value` TEXT COMMENT '主要项目介绍';
+ALTER TABLE `eh_customer_certificates` ADD COLUMN `buy_patents` INTEGER COMMENT '购买国外专利';
+ALTER TABLE `eh_customer_certificates` ADD COLUMN `technology_contracts` INTEGER COMMENT '技术合同交易数量';
+ALTER TABLE `eh_customer_certificates` ADD COLUMN `technology_contract_amount` INTEGER COMMENT '技术合同交易总金额（万元）';
+ALTER TABLE `eh_customer_certificates` ADD COLUMN `national_projects` INTEGER COMMENT '当年承担国家级科技计划项目数';
+ALTER TABLE `eh_customer_certificates` ADD COLUMN `provincial_awards` INTEGER COMMENT '当年获得省级以上奖励';
+
+ALTER TABLE `eh_customer_apply_projects` ADD COLUMN `description` TEXT COMMENT '主要项目介绍';
 
 -- 入驻信息
 CREATE TABLE `eh_customer_entry_infos` (
