34fe92047fb647ad0b053130fc544bc8ffff2a75
jiarui <rui.jia@zuolin.com>
2017-12-06 17:33:53 +0800

    for stadards privilege   12/6

--- db/eq_privilege-delta-schema.sql
+++ db/eq_privilege-delta-schema.sql
@@ -1,11 +1,19 @@
-CREATE TABLE `eh_equipment_module_community_map` (
-  `id` bigint(20) NOT NULL,
-  `standard_id` bigint(20) NOT NULL,
-  `target_type` varchar(255) DEFAULT NULL,
-  `target_id` bigint(20) DEFAULT NULL COMMENT 'community id ',
-  `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+CREATE TABLE `eh_equipment_modle_community_map` (
+  `id`          BIGINT(20) NOT NULL,
+  `standard_id` BIGINT(20) DEFAULT 0 NOT NULL,
+  `template_id` BIGINT(20) DEFAULT 0 NOT NULL,
+  `target_type` VARCHAR(255) DEFAULT NULL,
+  `target_id`   BIGINT(20)   DEFAULT NULL COMMENT 'community id ',
+  `create_time` DATETIME     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 ALTER TABLE `eh_equipment_inspection_standards`
-ADD COLUMN `refer_id`  bigint(20) NULL;
+  ADD COLUMN `refer_id` BIGINT(20) NULL;
+
+
+ALTER TABLE `eh_equipment_inspection_templates`
+  ADD COLUMN `target_type` VARCHAR(32) NOT NULL DEFAULT '',
+  ADD COLUMN `target_id` BIGINT(20) NOT NULL DEFAULT 0 ;
