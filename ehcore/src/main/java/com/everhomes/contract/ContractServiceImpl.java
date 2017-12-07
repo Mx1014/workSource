b2b871b7daa90f20a145b53ceb761c4bca4c61e2
ying.xiong <ying.xiong@zuolin.com>
2017-12-07 10:34:23 +0800

    fix contract message

--- ehcore/src/main/java/com/everhomes/contract/ContractServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/contract/ContractServiceImpl.java
@@ -248,20 +248,20 @@ public class ContractServiceImpl implements ContractService {
     	if(RunningFlag.fromCode(scheduleProvider.getRunningFlag()) == RunningFlag.TRUE){
 	    	//使用tryEnter()防止分布式部署重复执行
 	    	coordinationProvider.getNamedLock(CoordinationLocks.CONTRACT_SCHEDULE.getCode()).tryEnter(()->{
-	    		sendMessageToBackTwoMonthsOrganizations();
-	        	sendMessageToBackOneMonthOrganizations();
-	        	sendMessageToNewOrganizations();
+	    		sendMessageToBackTwoMonthsOrganizations(1000000);
+	        	sendMessageToBackOneMonthOrganizations(1000000);
+	        	sendMessageToNewOrganizations(1000000);
 	    	});
     	}
     }
 
-	private void sendMessageToBackTwoMonthsOrganizations() {
+	private void sendMessageToBackTwoMonthsOrganizations(Integer namespaceId) {
 		Timestamp now = getCurrentDate();
 		Timestamp lastNow = getNextNow(now);
 		long offset = 60*ONE_DAY_MS;
 		Timestamp minValue = new Timestamp(lastNow.getTime()+offset);
 		Timestamp maxValue = new Timestamp(now.getTime()+offset);
-		List<Contract> contractList = contractProvider.listContractsByEndDateRange(minValue, maxValue);
+		List<Contract> contractList = contractProvider.listContractsByEndDateRange(minValue, maxValue, namespaceId);
 		if (contractList == null || contractList.isEmpty()) {
 			return;
 		}
@@ -296,13 +296,13 @@ public class ContractServiceImpl implements ContractService {
 		}
 	}
 	
-	private void sendMessageToBackOneMonthOrganizations() {
+	private void sendMessageToBackOneMonthOrganizations(Integer namespaceId) {
 		Timestamp now = getCurrentDate();
 		Timestamp lastNow = getNextNow(now);
 		long offset = 30*ONE_DAY_MS;
 		Timestamp minValue = new Timestamp(lastNow.getTime()+offset);
 		Timestamp maxValue = new Timestamp(now.getTime()+offset);
-		List<Contract> contractList = contractProvider.listContractsByEndDateRange(minValue, maxValue);
+		List<Contract> contractList = contractProvider.listContractsByEndDateRange(minValue, maxValue, namespaceId);
 		if (contractList == null || contractList.isEmpty()) {
 			return;
 		}
@@ -337,10 +337,10 @@ public class ContractServiceImpl implements ContractService {
 		}
 	}
 
-	private void sendMessageToNewOrganizations() {
+	private void sendMessageToNewOrganizations(Integer namespaceId) {
 		Timestamp now = getCurrentDate();
 		Timestamp lastNow = getNextNow(now);
-		List<Contract> contractList = contractProvider.listContractsByCreateDateRange(lastNow, now);
+		List<Contract> contractList = contractProvider.listContractsByCreateDateRange(lastNow, now, namespaceId);
 		if (contractList == null || contractList.isEmpty()) {
 			return;
 		}
