328b6b453d67574b910d41ac1a36c4d42133cabe
dengs <shuang.deng@zuolin.com>
2017-12-07 11:39:26 +0800

    reset code

--- ehcore/src/main/java/com/everhomes/portal/ServiceAlliancePortalPublishHandler.java
+++ ehcore/src/main/java/com/everhomes/portal/ServiceAlliancePortalPublishHandler.java
@@ -1,22 +1,13 @@
 package com.everhomes.portal;
 
-import com.everhomes.acl.WebMenuPrivilegeProvider;
-import com.everhomes.acl.WebMenuScope;
 import com.everhomes.community.Community;
 import com.everhomes.community.CommunityProvider;
-import com.everhomes.configuration.ConfigurationProvider;
-import com.everhomes.listing.CrossShardListingLocator;
-import com.everhomes.organization.Organization;
-import com.everhomes.organization.OrganizationProvider;
 import com.everhomes.rest.common.ServiceAllianceActionData;
 import com.everhomes.rest.common.ServiceModuleConstants;
-import com.everhomes.rest.organization.OrganizationGroupType;
 import com.everhomes.rest.portal.DetailFlag;
 import com.everhomes.rest.portal.ServiceAllianceInstanceConfig;
 import com.everhomes.rest.portal.ServiceAllianceJump;
 import com.everhomes.rest.yellowPage.DisplayFlagType;
-import com.everhomes.rest.yellowPage.ServiceAllianceBelongType;
-import com.everhomes.rest.yellowPage.ServiceAllianceOwnerType;
 import com.everhomes.rest.yellowPage.YellowPageStatus;
 import com.everhomes.user.User;
 import com.everhomes.user.UserContext;
@@ -25,17 +16,12 @@ import com.everhomes.yellowPage.ServiceAllianceCategories;
 import com.everhomes.yellowPage.ServiceAllianceSkipRule;
 import com.everhomes.yellowPage.ServiceAlliances;
 import com.everhomes.yellowPage.YellowPageProvider;
-
-import org.hamcrest.core.Is;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by sfyan on 2017/8/30.
@@ -48,19 +34,11 @@ public class ServiceAlliancePortalPublishHandler implements PortalPublishHandler
 
     @Autowired
     private CommunityProvider communityProvider;
-    
-    @Autowired
-    private OrganizationProvider organizationProvider;
 
     @Autowired
     private YellowPageProvider yellowPageProvider;
-    
-    @Autowired
-    private ConfigurationProvider configProvider;
 
-	@Autowired
-	private WebMenuPrivilegeProvider webMenuProvider;
-	
+
     @Override
     public String publish(Integer namespaceId, String instanceConfig, String itemLabel) {
         ServiceAllianceInstanceConfig serviceAllianceInstanceConfig = (ServiceAllianceInstanceConfig)StringHelper.fromJsonString(instanceConfig, ServiceAllianceInstanceConfig.class);
@@ -104,24 +82,20 @@ public class ServiceAlliancePortalPublishHandler implements PortalPublishHandler
         serviceAllianceCategories.setName(name);
         serviceAllianceCategories.setNamespaceId(namespaceId);
         serviceAllianceCategories.setParentId(0L);
-        List<Organization> organizations = organizationProvider.listEnterpriseByNamespaceIds(namespaceId, OrganizationGroupType.ENTERPRISE.getCode(), new CrossShardListingLocator(), 10);
-//        List<Community> communities = communityProvider.listCommunitiesByNamespaceId(namespaceId);
-//        if(null != communities && communities.size() > 0){
-        if(null != organizations && organizations.size() > 0){
-//            Community community = communities.get(0);
-        	Organization organization = organizations.get(0);
-            serviceAllianceCategories.setOwnerType(ServiceAllianceBelongType.ORGANAIZATION.getCode());
-            serviceAllianceCategories.setOwnerId(organization.getId());
+        List<Community> communities = communityProvider.listCommunitiesByNamespaceId(namespaceId);
+        if(null != communities && communities.size() > 0){
+            Community community = communities.get(0);
+            serviceAllianceCategories.setOwnerType("community");
+            serviceAllianceCategories.setOwnerId(community.getId());
             serviceAllianceCategories.setCreatorUid(user.getId());
             serviceAllianceCategories.setDeleteUid(user.getId());
             serviceAllianceCategories.setStatus(YellowPageStatus.ACTIVE.getCode());
-            serviceAllianceCategories.setEntryId(generateEntryId(namespaceId));
             yellowPageProvider.createServiceAllianceCategory(serviceAllianceCategories);
 
             ServiceAlliances serviceAlliances = new ServiceAlliances();
             serviceAlliances.setParentId(0L);
-            serviceAlliances.setOwnerType(ServiceAllianceBelongType.ORGANAIZATION.getCode());
-            serviceAlliances.setOwnerId(organization.getId());
+            serviceAlliances.setOwnerType("community");
+            serviceAlliances.setOwnerId(community.getId());
             serviceAlliances.setName(name);
             serviceAlliances.setDisplayName(name);
             serviceAlliances.setType(serviceAllianceCategories.getId());
@@ -137,89 +111,19 @@ public class ServiceAlliancePortalPublishHandler implements PortalPublishHandler
                 serviceAllianceSkipRule.setServiceAllianceCategoryId(serviceAllianceCategories.getId());
                 yellowPageProvider.createServiceAllianceSkipRule(serviceAllianceSkipRule);
             }
-            
-            boolean iscreateMenuScope = configProvider.getBooleanValue("portal.sa.create.scope", true);
-            if(iscreateMenuScope){
-            	createMenuScope(namespaceId,serviceAllianceCategories.getEntryId(),serviceAllianceCategories.getName());
-            }
         }else{
-            LOGGER.error("namespace not pm. namespaceId = {}", namespaceId);
+            LOGGER.error("namespace not community. namespaceId = {}", namespaceId);
         }
 
         return serviceAllianceCategories;
     }
 
-    private void createMenuScope(Integer namespaceId, Integer entryId, String name) {
-		if(entryId == null || namespaceId == null){
-			return ;
-		}
-		long parentMenuId = entryId*100+41600;
-		ArrayList<WebMenuScope> newSocpes = new ArrayList<WebMenuScope>();
-		ArrayList<Long> socpeIds = new ArrayList<Long>();
-		Map<Long, WebMenuScope> map = webMenuProvider.getWebMenuScopeMapByOwnerId("EhNamespaces", Long.valueOf(namespaceId));
-		boolean insertflag = false;
-		for (int i = 0; i < 7; i++) {
-			Long menuid = parentMenuId+i*10;
-			newSocpes.add(generteObjWebMenuScope(namespaceId,name,menuid));
-			WebMenuScope scope = map.get(Long.valueOf(menuid));
-			if(scope == null){
-				insertflag = true;
-			}else{
-				socpeIds.add(scope.getId());
-			}
-		}
-		if(insertflag){
-			webMenuProvider.deleteWebMenuScopes(socpeIds);
-			webMenuProvider.createWebMenuScopes(newSocpes);
-		}
-	}
-
-	private WebMenuScope generteObjWebMenuScope(Integer namespaceId, String name, Long menuid) {
-		WebMenuScope socpe = new WebMenuScope();
-		socpe.setMenuId(menuid);
-		socpe.setMenuName(name);
-		socpe.setOwnerId(Long.valueOf(namespaceId));
-		socpe.setOwnerType("EhNamespaces");
-		if(menuid%100 == 0){
-			socpe.setApplyPolicy((byte)1);
-		}else{
-			socpe.setApplyPolicy((byte)2);
-		}
-		return socpe;
-	}
-
-	private Integer generateEntryId(Integer namespaceId) {
-		if(namespaceId == null){
-			LOGGER.error("service alliance generateEntryId namespaceId "+namespaceId);
-			return null;
-		}
-    	 List<Integer> entryIds = yellowPageProvider.listAscEntryIds(namespaceId);
-    	 for (int i = 0; i < entryIds.size(); i++) {
-			if(entryIds.get(i)!=i+1){
-				return i+1;
-			}
-		}
-    	 int maxEntryId = configProvider.getIntValue("portal.max.entryid", 30);
-    	 if(entryIds.size() == maxEntryId){
-    		 LOGGER.error("service alliance maxEntryId exceed "+maxEntryId);
-    		 return null;
-    	 }
-    	return entryIds.size()+1;
-	}
-
-	private ServiceAllianceCategories updateServiceAlliance(Integer namespaceId, Long type, Byte detailFlag, String name){
+    private ServiceAllianceCategories updateServiceAlliance(Integer namespaceId, Long type, Byte detailFlag, String name){
         ServiceAllianceCategories serviceAllianceCategories = yellowPageProvider.findCategoryById(type);
         List<Community> communities = communityProvider.listCommunitiesByNamespaceId(namespaceId);
         if(null != communities && communities.size() > 0 && null != serviceAllianceCategories){
             Community community = communities.get(0);
             serviceAllianceCategories.setName(name);
-            if(serviceAllianceCategories.getEntryId() == null){
-            	serviceAllianceCategories.setEntryId(generateEntryId(namespaceId));
-            	 boolean iscreateMenuScope = configProvider.getBooleanValue("portal.sa.create.scope", true);
-                 if(iscreateMenuScope){
-                	 createMenuScope(namespaceId, serviceAllianceCategories.getEntryId(), name);
-                 }
-            }
             yellowPageProvider.updateServiceAllianceCategory(serviceAllianceCategories);
 
             ServiceAlliances serviceAlliances = yellowPageProvider.queryServiceAllianceTopic("community", community.getId(), type);
