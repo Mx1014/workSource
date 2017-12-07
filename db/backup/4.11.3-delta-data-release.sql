45720de26c9d8761bc0edbdce71fbf1db04ebc42
ying.xiong <ying.xiong@zuolin.com>
2017-12-07 15:57:59 +0800

    add data

--- db/backup/4.11.3-delta-data-release.sql
+++ db/backup/4.11.3-delta-data-release.sql
@@ -1752,5 +1752,34 @@ INSERT INTO `eh_punch_holidays` (`id`, `owner_type`, `owner_id`, `workday_rule_i
 INSERT INTO `eh_punch_holidays` (`id`, `owner_type`, `owner_id`, `workday_rule_id`, `status`, `rule_date`, `creator_uid`, `create_time`, `exchange_from_date`) VALUES((@id:=@id+1),NULL,NULL,NULL,'0','2018-09-29','1','2017-12-06 14:20:10','2018-10-04');
 INSERT INTO `eh_punch_holidays` (`id`, `owner_type`, `owner_id`, `workday_rule_id`, `status`, `rule_date`, `creator_uid`, `create_time`, `exchange_from_date`) VALUES((@id:=@id+1),NULL,NULL,NULL,'0','2018-09-30','1','2017-12-06 14:20:10','2018-10-05');
 
+-- 默认开启企业详情开关 by xiongying20171207
+
+DROP PROCEDURE if exists create_community_organization_detail_display;
+delimiter //
+CREATE PROCEDURE `create_community_organization_detail_display` ()
+BEGIN  
+  DECLARE ns INTEGER;
+  DECLARE communityid LONG;
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE cur CURSOR FOR select id, namespace_id from eh_communities where namespace_id > 0; 
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  OPEN cur;
+  read_loop: LOOP
+                FETCH cur INTO communityid, ns;
+                IF done THEN
+                    LEAVE read_loop;
+                END IF;
+
+        SET @display_id = IFNULL((SELECT MAX(id) FROM `eh_community_organization_detail_display`),0);
+        INSERT INTO eh_community_organization_detail_display (`id`, `namespace_id`, `community_id`, `detail_flag`, `operator_uid`, `update_time`)
+            VALUES((@display_id := @display_id + 1), ns, communityid, 1, 1, NOW());
+  END LOOP;
+  CLOSE cur;
+END
+//
+delimiter ;
+CALL create_community_organization_detail_display;
+DROP PROCEDURE if exists create_community_organization_detail_display;
+
 -- 科技园的客户类型直接为organization表 add by xiongying20171207
 update eh_contracts set customer_type = 2 where namespace_id = 1000000;
