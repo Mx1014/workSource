f254fd609e6c426487b01bde258de41de8a9fa84
yanjun <jun.yan@zuolin.com>
2017-12-07 19:57:44 +0800

    fix

--- ehcore/src/main/java/com/everhomes/wx/WXAuthController.java
+++ ehcore/src/main/java/com/everhomes/wx/WXAuthController.java
@@ -363,9 +363,9 @@ public class WXAuthController {// extends ControllerBase
         // 如果hash有"?"，则hash可能带有参数，并且确定base不会有参数，如果base还有参数的话本身就是错误的（不考虑这种情况）。
         pos = hashUrl.indexOf('?');
         if(pos != -1) {
-            hashUrl = hashUrl.substring(0, pos);
             String sourceParam = hashUrl.substring(pos);
             baseUrl = baseUrl + sourceParam;
+            hashUrl = hashUrl.substring(0, pos);
         }
 	    
 	    // 若有问号，代表着有参数
