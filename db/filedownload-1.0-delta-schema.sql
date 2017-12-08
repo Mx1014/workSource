233a2db41b6cd82351484401a900ecb9e16b8f74
yanjun <jun.yan@zuolin.com>
2017-12-08 17:14:59 +0800

    fix

---
+++ db/filedownload-1.0-delta-schema.sql
@@ -0,0 +1,10 @@
+-- 文件下载任务 add by yanjun 20171207
+CREATE TABLE `eh_file_download_jobs` (
+  `id` bigint(20) NOT NULL,
+  `file_name` varchar(255) DEFAULT NULL,
+  `size` bigint(20) DEFAULT NULL,
+  `count` bigint(20) DEFAULT NULL,
+  `uri` varchar(255) DEFAULT NULL,
+  `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
