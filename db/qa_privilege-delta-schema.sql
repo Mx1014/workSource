e50678b34592b065c18938762ed059c868781ac5
jiarui <rui.jia@zuolin.com>
2017-12-08 15:40:49 +0800

    quality privilege

---
+++ db/qa_privilege-delta-schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `eh_quality_inspection_modle_community_map` (
+  `id`          BIGINT(20) NOT NULL,
+  `standard_id` BIGINT(20) DEFAULT 0 NOT NULL,
+  `model_type`  tinyint(4) NOT NULL DEFAULT 0 COMMENT '0:standard',
+  `target_type` VARCHAR(255) DEFAULT NULL,
+  `target_id`   BIGINT(20)   DEFAULT NULL COMMENT 'community id ',
+  `create_time` DATETIME     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE `eh_quality_inspection_standards`
+  ADD COLUMN `refer_id` BIGINT(20) NULL;
