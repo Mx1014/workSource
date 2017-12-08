e09a2b007f08171e13bf435c8525c0635df215c6
wentian <wentian.wang@zuolin.com>
2017-12-08 17:23:16 +0800

    place communityId as remark3 for filtering the outputs when customer is checking their paid details

--- ehcore/src/main/java/com/everhomes/asset/PaymentServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/asset/PaymentServiceImpl.java
@@ -9,6 +9,7 @@ import com.everhomes.order.PaymentUser;
 import com.everhomes.pay.order.TransactionType;
 import com.everhomes.portal.PortalService;
 import com.everhomes.rest.asset.*;
+import com.everhomes.rest.order.OrderType;
 import com.everhomes.rest.portal.ListServiceModuleAppsCommand;
 import com.everhomes.rest.portal.ListServiceModuleAppsResponse;
 import com.everhomes.rest.user.UserInfo;
@@ -49,44 +50,48 @@ public class PaymentServiceImpl implements PaymentService {
         Long appId = res.getServiceModuleApps().get(0).getId();
         if(!userPrivilegeMgr.checkUserPrivilege(UserContext.currentUserId(), EntityType.ORGANIZATIONS.getCode(), 1000001L, 1000001L, 40078L, appId, null, 240111044331048623L)){
             throw RuntimeErrorException.errorWith(ErrorCodes.SCOPE_GENERAL,ErrorCodes.ERROR_GENERAL_EXCEPTION,"checkUserPrivilege false");
-        }else{
-            return new ListPaymentBillResp();
         }
-//        if(cmd.getNamespaceId() == null){
-//            cmd.setNamespaceId(UserContext.getCurrentNamespaceId());
-//        }
-//        cmd.setOrderType(getRealOrderType(cmd.getNamespaceId()));
-//
-//        if (cmd.getPageSize() == null) {
-//            cmd.setPageSize(21l);
-//        }else{
-//            cmd.setPageSize(cmd.getPageSize()+1l);
-//        }
-//        if(cmd.getPageAnchor() == null){
-//            cmd.setPageAnchor(0l);
-//        }
-//        if (cmd.getTransactionType() == null) {
-//            List<Integer> transactionTypes = new ArrayList<Integer>();
-//            transactionTypes.add(TransactionType.ORDERPAYMENT.getCode());
-//            transactionTypes.add(TransactionType.REFUND.getCode());
-//            cmd.setTransactionTypes(transactionTypes);
-//        }
-//        if (cmd.getSorts() == null) {
-//            ReSortCmd sortCmd = new ReSortCmd();
-//            sortCmd.setSortField("orderId");
-//            sortCmd.setSortType("desc");
-//            List<ReSortCmd> sorts = new ArrayList<ReSortCmd>();
-//            sorts.add(sortCmd);
-//            cmd.setSorts(sorts);
-//        }
-//        //payeeUserId
-//        PaymentUser paymentUser = assetProvider.findByOwner(cmd.getUserType(),cmd.getUserId());
-//        if(paymentUser == null) {
-//            return new ListPaymentBillResp(cmd.getPageAnchor(), cmd.getPageSize());
-//        }
-//        cmd.setUserId(paymentUser.getPaymentUserId());
-//        LOGGER.info("payee user payer id is = {}",paymentUser.getPaymentUserId());
-//        return remoteAccessService.listOrderPayment(cmd);
+
+        if(cmd.getNamespaceId() == null){
+            cmd.setNamespaceId(UserContext.getCurrentNamespaceId());
+        }
+        if(cmd.getOrderType()==null) return new ListPaymentBillResp(cmd.getPageAnchor(), cmd.getPageSize());
+
+        if(cmd.getOrderType().equals(OrderType.OrderTypeEnum.WUYE_CODE.getPycode())||cmd.getOrderType().equals(OrderType.OrderTypeEnum.ZJGK_RENTAL_CODE.getPycode())){
+            cmd.setOrderType(getRealOrderType(cmd.getNamespaceId()));
+            if(cmd.getOrderType()==null) return new ListPaymentBillResp(cmd.getPageAnchor(), cmd.getPageSize());
+        }
+
+        if (cmd.getPageSize() == null) {
+            cmd.setPageSize(21l);
+        }else{
+            cmd.setPageSize(cmd.getPageSize()+1l);
+        }
+        if(cmd.getPageAnchor() == null){
+            cmd.setPageAnchor(0l);
+        }
+        if (cmd.getTransactionType() == null) {
+            List<Integer> transactionTypes = new ArrayList<Integer>();
+            transactionTypes.add(TransactionType.ORDERPAYMENT.getCode());
+            transactionTypes.add(TransactionType.REFUND.getCode());
+            cmd.setTransactionTypes(transactionTypes);
+        }
+        if (cmd.getSorts() == null) {
+            ReSortCmd sortCmd = new ReSortCmd();
+            sortCmd.setSortField("orderId");
+            sortCmd.setSortType("desc");
+            List<ReSortCmd> sorts = new ArrayList<ReSortCmd>();
+            sorts.add(sortCmd);
+            cmd.setSorts(sorts);
+        }
+        //payeeUserId
+        PaymentUser paymentUser = assetProvider.findByOwner(cmd.getUserType(),cmd.getUserId());
+        if(paymentUser == null) {
+            return new ListPaymentBillResp(cmd.getPageAnchor(), cmd.getPageSize());
+        }
+        cmd.setUserId(paymentUser.getPaymentUserId());
+        LOGGER.info("payee user payer id is = {}",paymentUser.getPaymentUserId());
+        return remoteAccessService.listOrderPayment(cmd);
     }
 
     private String getRealOrderType(Integer namespaceId) {
