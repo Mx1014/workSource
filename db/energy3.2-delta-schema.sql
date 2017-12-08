ced13c15959551bec7cf721f11c8c779bedb2325
ying.xiong <ying.xiong@zuolin.com>
2017-12-08 20:08:02 +0800

    add schema

---
+++ db/energy3.2-delta-schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `eh_energy_meter_category_map`(
+  `id` BIGINT NOT NULL COMMENT 'id',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'enterprise id',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
+  `community_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'community id',
+  `category_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'category id',
+  `status` TINYINT NOT NULL DEFAULT 2 COMMENT '0: disabled, 1: waiting for confirmation, 2: active',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `operator_uid` BIGINT,
+  `operate_time` DATETIME,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
