c7611972bd144418a541b3ad72c1007c4f4210ed
yanjun <jun.yan@zuolin.com>
2017-12-10 15:44:01 +0800

    fix schame

--- db/filedownload-1.0-delta-schema.sql
+++ db/filedownload-1.0-delta-schema.sql
@@ -2,9 +2,13 @@
 CREATE TABLE `eh_file_download_jobs` (
   `id` bigint(20) NOT NULL,
   `file_name` varchar(255) DEFAULT NULL,
+  `job_class` varchar(255) DEFAULT NULL,
+  `params` text,
   `size` bigint(20) DEFAULT NULL,
   `count` bigint(20) DEFAULT NULL,
   `uri` varchar(255) DEFAULT NULL,
   `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `finish_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
