b726c454288126a9ac0aae03f508c7257476d5fc
siting <st.zheng@zuolin.com>
2017-12-11 18:18:58 +0800

    rental-3.1

---
+++ db/rental-3.1-delta-schema.sql
@@ -0,0 +1,10 @@
+-- add by st.zheng 增加起步价
+ALTER TABLE `eh_rentalv2_price_rules`
+ADD COLUMN `initiate_price` DECIMAL(10,2) NULL AFTER `weekend_price`,
+ADD COLUMN `org_member_initiate_price` DECIMAL(10,2) NULL AFTER `org_member_weekend_price`,
+ADD COLUMN `approving_user_initiate_price` DECIMAL(10,2) NULL AFTER `approving_user_weekend_price`;
+
+ALTER TABLE `eh_rentalv2_price_packages`
+ADD COLUMN `initiate_price` DECIMAL(10,2) NULL AFTER `price`,
+ADD COLUMN `org_member_initiate_price` DECIMAL(10,2) NULL AFTER `org_member_price`,
+ADD COLUMN `approving_user_initiate_price` DECIMAL(10,2) NULL AFTER `approving_user_price`;
