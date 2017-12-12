5c0dbebcbff03389b1fa565a2ac4a1f8a6c81d2c
siting <st.zheng@zuolin.com>
2017-12-12 15:34:37 +0800

    rental-3.1

--- db/rantal-3.1-delta-data-release.sql
+++ db/rantal-3.1-delta-data-release.sql
@@ -1,4 +1,3 @@
 -- 初始化为按时长定价 by st.zheng
 update `eh_rentalv2_price_rules` set `price_type` = 0;
 update `eh_rentalv2_price_packages` set `price_type` = 0;
-update `eh_rentalv2_cells` set `price_type` = 0;
