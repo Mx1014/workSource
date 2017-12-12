1003d35ad639f8079535a13db9fc226befac6845
siting <st.zheng@zuolin.com>
2017-12-12 18:18:13 +0800

    rental-3.1

--- db/rantal-3.1-delta-schema.sql
+++ db/rantal-3.1-delta-schema.sql
@@ -10,3 +10,9 @@ ADD COLUMN `price_type` TINYINT(4) NULL AFTER `rental_type`,
 ADD COLUMN `initiate_price` DECIMAL(10,2) NULL AFTER `price`,
 ADD COLUMN `org_member_initiate_price` DECIMAL(10,2) NULL AFTER `org_member_price`,
 ADD COLUMN `approving_user_initiate_price` DECIMAL(10,2) NULL AFTER `approving_user_price`;
+
+ALTER TABLE `eh_rentalv2_cells`
+ADD COLUMN `price_type` TINYINT(4) NULL AFTER `rental_type`,
+ADD COLUMN `initiate_price` DECIMAL(10,2) NULL AFTER `price`,
+ADD COLUMN `org_member_initiate_price` DECIMAL(10,2) NULL AFTER `org_member_price`,
+ADD COLUMN `approving_user_initiate_price` DECIMAL(10,2) NULL AFTER `approving_user_price`;
