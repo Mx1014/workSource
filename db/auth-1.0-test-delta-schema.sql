a4d76890993d9b1c41a9a1fc7d90b04dbf829a19
jiarui <rui.jia@zuolin.com>
2017-12-15 08:43:38 +0800

    revert specifications

--- db/auth-1.0-test-delta-schema.sql
+++ db/auth-1.0-test-delta-schema.sql
@@ -18,21 +18,6 @@ ALTER TABLE `eh_equipment_inspection_standards`
 ALTER TABLE `eh_equipment_inspection_templates`
   ADD COLUMN `refer_id`  bigint(20) NULL;
 
-UPDATE eh_service_module_privileges
-SET module_id = 20810
-WHERE module_id = 20811;
-
-
-UPDATE eh_service_modules
-SET STATUS =2
-WHERE id = 20840;
-UPDATE eh_service_modules
-SET LEVEL =4
-WHERE id = 20841;
-
-UPDATE eh_service_modules
-SET STATUS =2
-WHERE id = 20841;
 -- 物业巡检权限细化 start  by jiarui
 
 
