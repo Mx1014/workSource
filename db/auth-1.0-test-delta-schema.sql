abd1b994762df712e8e264952feb7f485071f7d5
jiarui <rui.jia@zuolin.com>
2017-12-18 17:01:20 +0800

    fix appId gen  when actionType == 14 44

--- db/auth-1.0-test-delta-schema.sql
+++ db/auth-1.0-test-delta-schema.sql
@@ -17,8 +17,10 @@ ALTER TABLE `eh_equipment_inspection_standards`
 ALTER TABLE `eh_equipment_inspection_templates`
   ADD COLUMN `refer_id`  bigint(20) NULL;
 
--- 物业巡检权限细化 start  by jiarui
-
+ALTER TABLE `eh_equipment_inspection_templates`
+  ADD COLUMN `target_type` VARCHAR(32) NOT NULL DEFAULT '',
+  ADD COLUMN `target_id` BIGINT(20) NOT NULL DEFAULT 0 ;
+-- 物业巡检权限细化 end  by  jiarui
 
 -- merge from forum2.6 by yanjun 201712121010 start
 
