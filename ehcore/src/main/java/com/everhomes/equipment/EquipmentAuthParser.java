76e5b34d8d99d5c300ed4dc9b46f504c9413b1ca
jiarui <rui.jia@zuolin.com>
2017-12-20 14:15:50 +0800

    fix  AuthParser

--- ehcore/src/main/java/com/everhomes/equipment/EquipmentAuthParser.java
+++ ehcore/src/main/java/com/everhomes/equipment/EquipmentAuthParser.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by rui.jia  2017/12/12 16 :36
@@ -18,12 +19,13 @@ public class EquipmentAuthParser implements PortalUrlParser {
     @Override
     public Long getModuleId(Integer namespaceId, String actionData, Byte actionType, String itemLabel) {
         Long res = null;
-        if (actionType == 13 || actionType == 14) {
+        if (Objects.equals(actionType, EquipmentConstant.EQUIPMENT_ACTION_TYEPEV1)
+                || Objects.equals(actionType, EquipmentConstant.EQUIPMENT_ACTION_TYEPEV2)) {
             Map<String, String> data = new Gson().fromJson(actionData, new TypeToken<HashMap<String, String>>() {}.getType());
             if (data != null && data.size() > 0 && data.get("url") != null) {
                 String url = data.get("url");
                 if (url.contains("equipment-inspection"))
-                    res = 20800L;
+                    res = EquipmentConstant.EQUIPMENT_MODULE;
             }
         }
         return res;
