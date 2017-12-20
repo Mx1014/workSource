53ec1bad82bb1896aa2c062182c298e279ebd37e
jiarui <rui.jia@zuolin.com>
2017-12-20 16:26:47 +0800

    fix  Auth

--- db/auth-1.0-test-delta-data-release.sql
+++ db/auth-1.0-test-delta-data-release.sql
@@ -1,7 +1,7 @@
+-- 菜单显示不全
 UPDATE eh_service_module_privileges
 SET module_id = 20810
 WHERE module_id = 20811;
-
 
 UPDATE eh_service_modules
 SET STATUS =2
@@ -13,3 +13,7 @@ WHERE id = 20841;
 UPDATE eh_service_modules
 SET STATUS =2
 WHERE id = 20841;
+
+-- 删除旧的里面的  privilege_id 为管理员
+DELETE  from eh_acls
+where privilege_id = 10011;
