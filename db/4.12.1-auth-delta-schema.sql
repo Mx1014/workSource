3c9c6ebc56ff14703baf54f19584e861f937d513
jiarui <rui.jia@zuolin.com>
2017-12-21 12:10:22 +0800

    fix

--- db/4.12.1-auth-delta-schema.sql
+++ db/4.12.1-auth-delta-schema.sql
@@ -28,6 +28,25 @@ ALTER TABLE `eh_equipment_inspection_templates`
   ADD COLUMN `target_id` BIGINT(20) NOT NULL DEFAULT 0 ;
 -- 物业巡检权限细化 end  by  jiarui
 
+-- 品质核查权限细化start by jiarui  20171215
+CREATE TABLE `eh_quality_inspection_model_community_map` (
+  `id`          BIGINT(20) NOT NULL,
+  `model_id` BIGINT(20) DEFAULT 0 NOT NULL,
+  `category_id` BIGINT(20) DEFAULT 0 NOT NULL,
+  `model_type`  tinyint(4) NOT NULL DEFAULT 0 COMMENT '0:standard',
+  `target_type` VARCHAR(255) DEFAULT NULL,
+  `target_id`   BIGINT(20)   DEFAULT NULL COMMENT 'community id ',
+  `create_time` DATETIME     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE `eh_quality_inspection_standards`
+  ADD COLUMN `refer_id` BIGINT(20) NULL;
+
+-- 品质核查权限细化end  by jiarui  20171215
+
 
 -- customerAuth by xiongying
 CREATE TABLE `eh_energy_meter_category_map`(
