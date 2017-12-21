1b95833ff6496e753f274aa3170ac86d3db5d67b
nan.rong <nan.rong@zuolin.com>
2017-12-21 22:27:27 +0800

    getWorkReportValItem bug fixed

--- ehcore/src/main/java/com/everhomes/workReport/WorkReportCommentHandler.java
+++ ehcore/src/main/java/com/everhomes/workReport/WorkReportCommentHandler.java
@@ -3,19 +3,26 @@ package com.everhomes.workReport;
 import com.everhomes.comment.CommentHandler;
 import com.everhomes.contentserver.ContentServerService;
 import com.everhomes.db.DbProvider;
+import com.everhomes.locale.LocaleTemplateService;
+import com.everhomes.messaging.MessagingService;
+import com.everhomes.namespace.Namespace;
+import com.everhomes.rest.app.AppConstants;
 import com.everhomes.rest.comment.*;
+import com.everhomes.rest.common.Router;
+import com.everhomes.rest.messaging.*;
+import com.everhomes.rest.workReport.WorkReportDetailsActionData;
+import com.everhomes.rest.workReport.WorkReportNotificationTemplateCode;
 import com.everhomes.user.User;
 import com.everhomes.user.UserContext;
 import com.everhomes.util.ConvertHelper;
+import com.everhomes.util.RouterBuilder;
+import com.everhomes.util.StringHelper;
 import com.everhomes.util.WebTokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionStatus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component(CommentHandler.COMMENT_OBJ_RESOLVER_PREFIX + OwnerTypeConstants.WORK_REPORT)
@@ -33,6 +40,12 @@ public class WorkReportCommentHandler implements CommentHandler {
     @Autowired
     private DbProvider dbProvider;
 
+    @Autowired
+    private LocaleTemplateService localeTemplateService;
+
+    @Autowired
+    private MessagingService messagingService;
+
     @Override
     public CommentDTO addComment(AddCommentCommand cmd) {
         OwnerTokenDTO ownerTokenDTO = WebTokenGenerator.getInstance().fromWebToken(cmd.getOwnerToken(), OwnerTokenDTO.class);
@@ -46,6 +59,7 @@ public class WorkReportCommentHandler implements CommentHandler {
                 for (WorkReportValCommentAttachment attachment : attachments)
                     workReportValProvider.createWorkReportValCommentAttachment(attachment);
 
+                //  return the comment back.
                 CommentDTO dto = new CommentDTO();
                 dto.setCreatorNickName(workReportService.fixUpUserName(comment.getCreatorUserId()));
                 dto.setCreatorAvatarUrl(workReportService.getUserAvatar(comment.getCreatorUserId()));
@@ -54,6 +68,7 @@ public class WorkReportCommentHandler implements CommentHandler {
                 dto.setContentType(comment.getContentType());
                 return dto;
             });
+            //  send message to the corresponding receiver.
             return commentDTO;
         }
         return null;
@@ -101,9 +116,9 @@ public class WorkReportCommentHandler implements CommentHandler {
         if (cmd.getPageSize() == null)
             cmd.setPageSize(20);
         List<WorkReportValComment> results = workReportValProvider.listWorkReportValComments(namespaceId, ownerTokenDto.getId(), cmd.getPageAnchor(), cmd.getPageSize());
-        if(results != null && results.size() >0) {
+        if (results != null && results.size() > 0) {
 
-            if(results.size() > cmd.getPageSize()){
+            if (results.size() > cmd.getPageSize()) {
                 results.remove(results.size() - 1);
                 nextPageAnchor = results.get(results.size() - 1).getId();
             }
@@ -129,8 +144,8 @@ public class WorkReportCommentHandler implements CommentHandler {
     private Map<Long, List<AttachmentDTO>> listWorkReportValCommentAttachments(Integer namespaceId, List<Long> commentIds) {
         Map<Long, List<AttachmentDTO>> res = new HashMap<>();
         List<WorkReportValCommentAttachment> results = workReportValProvider.listWorkReportValCommentAttachments(namespaceId, commentIds);
-        if(results != null && results.size()>0){
-            List<AttachmentDTO> attachmentDTOS = results.stream().map(r ->{
+        if (results != null && results.size() > 0) {
+            List<AttachmentDTO> attachmentDTOS = results.stream().map(r -> {
                 AttachmentDTO dto = ConvertHelper.convert(r, AttachmentDTO.class);
                 dto.setOwnerId(r.getCommentId());
                 dto.setContentUrl(contentServerService.parserUri(r.getContentUri()));
@@ -151,4 +166,77 @@ public class WorkReportCommentHandler implements CommentHandler {
             return null;
         });
     }
+
+    /*private String getMessageContent(){
+        User user = UserContext.current().getUser();
+
+        String locale = Locale.SIMPLIFIED_CHINESE.toString();
+        if (user != null) {
+            locale = user.getLocale();
+        }
+
+        // set the message
+        Map<String, String> model = new HashMap<>();
+        model.put("commentName", commentName);
+        model.put("reportName", reportName);
+        String content = "";
+        if (messageType.equals("post")) {
+            content = localeTemplateService.getLocaleTemplateString(
+                    Namespace.DEFAULT_NAMESPACE,
+                    WorkReportNotificationTemplateCode.SCOPE,
+                    WorkReportNotificationTemplateCode.POST_WORK_REPORT_VAL,
+                    locale,
+                    model,
+                    "Template Not Found"
+            );
+        } else if (messageType.equals("update")) {
+            content = localeTemplateService.getLocaleTemplateString(
+                    Namespace.DEFAULT_NAMESPACE,
+                    WorkReportNotificationTemplateCode.SCOPE,
+                    WorkReportNotificationTemplateCode.UPDATE_WORK_REPORT_VAL,
+                    locale,
+                    model,
+                    "Template Not Found"
+            );
+        }
+    }
+
+    private void sendMessageAfterCommentWorkReportVal(
+            String messageType, String commentName, String reportName, Long receiverId, Long reportId, Long reportValId) {
+
+
+
+
+        MessageDTO message = new MessageDTO();
+        message.setBodyType(MessageBodyType.TEXT.getCode());
+        message.setBody(getMessageContent());
+        message.setMetaAppId(AppConstants.APPID_DEFAULT);
+        message.setChannels(new MessageChannel(ChannelType.USER.getCode(), String.valueOf(receiverId)));
+
+        //  set the route
+        WorkReportDetailsActionData actionData = new WorkReportDetailsActionData();
+        actionData.setReportId(reportId);
+        actionData.setReportValId(reportValId);
+        String url = RouterBuilder.build(Router.WORK_REPORT_DETAILS, actionData);
+        RouterMetaObject metaObject = new RouterMetaObject();
+        metaObject.setUrl(url);
+        Map<String, String> meta = new HashMap<>();
+        meta.put(MessageMetaConstant.META_OBJECT_TYPE, MetaObjectType.MESSAGE_ROUTER.getCode());
+        if (messageType.equals("post"))
+            meta.put(MessageMetaConstant.MESSAGE_SUBJECT, "新的汇报");
+        else if (messageType.equals("update"))
+            meta.put(MessageMetaConstant.MESSAGE_SUBJECT, "汇报更新");
+        meta.put(MessageMetaConstant.META_OBJECT, StringHelper.toJsonString(metaObject));
+        message.setMeta(meta);
+
+        //  send the message
+        messagingService.routeMessage(
+                User.SYSTEM_USER_LOGIN,
+                AppConstants.APPID_MESSAGING,
+                ChannelType.USER.getCode(),
+                String.valueOf(receiverId),
+                message,
+                MessagingConstants.MSG_FLAG_STORED.getCode()
+        );
+    }*/
 }
--- ehrest/src/main/java/com/everhomes/rest/workReport/WorkReportNotificationTemplateCode.java
+++ ehrest/src/main/java/com/everhomes/rest/workReport/WorkReportNotificationTemplateCode.java
@@ -3,7 +3,15 @@ package com.everhomes.rest.workReport;
 public interface WorkReportNotificationTemplateCode {
 	String SCOPE = "work.report.notification";
 	
-	int POST_WORK_REPORT_VAL_FOR_RECEIVER = 1;	//给接收人发通知：提交工作汇报
+	int POST_WORK_REPORT_VAL = 1;	//给接收人发通知：提交工作汇报
 
-	int UPDATE_WORK_REPORT_VAL_FOR_RECEIVER = 2;   //给接收人发通知：更新工作汇报
+	int UPDATE_WORK_REPORT_VAL = 2;   //给接收人发通知：更新工作汇报
+
+	int READER_COMMENT_WORK_REPORT_VAL = 3;   //给作者发通知：读者发表了评论
+
+	int AUTHOR_REPLY_WORK_REPORT_VAL = 4;   //给读者发通知：作者发表了回复
+
+	int READER_WORK_REPORT_VAL_FOR_READER = 5;   //给读者发通知：读者在作者的汇报中发表了回复
+
+	int AUTHOR_COMMENT_WORK_REPORT_VAL   = 6;   //给接收人发通知：作者发表了评论
 }
