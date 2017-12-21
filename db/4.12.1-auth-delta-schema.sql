06edff4e3935dff8103e9e93dbbe24f5bab8795c
lei.lv <lei.lv@zuolin.com>
2017-12-21 11:54:45 +0800

    fix

--- db/4.12.1-auth-delta-schema.sql
+++ db/4.12.1-auth-delta-schema.sql
@@ -1,4 +1,4 @@
--- 仓库权限 by wentian
+﻿-- 仓库权限 by wentian
 ALTER TABLE `eh_warehouse_materials` ADD COLUMN `community_id` BIGINT DEFAULT 0 COMMENT '园区id';
 ALTER TABLE `eh_warehouses` ADD COLUMN `community_id` BIGINT DEFAULT 0 COMMENT '园区id';
 ALTER TABLE `eh_warehouse_stocks` ADD COLUMN `community_id` BIGINT DEFAULT 0 COMMENT '园区id';
@@ -6,24 +6,6 @@ ALTER TABLE `eh_warehouse_stock_logs` ADD COLUMN `community_id` BIGINT DEFAULT 0
 ALTER TABLE `eh_warehouse_request_materials` ADD COLUMN `community_id` BIGINT DEFAULT 0 COMMENT '园区id';
 ALTER TABLE `eh_warehouse_requests` ADD COLUMN `community_id` BIGINT DEFAULT 0 COMMENT '园区id';
 -- endl
--- 品质核查权限细化start by jiarui  20171215
-CREATE TABLE `eh_quality_inspection_model_community_map` (
-  `id`          BIGINT(20) NOT NULL,
-  `model_id` BIGINT(20) DEFAULT 0 NOT NULL,
-  `category_id` BIGINT(20) DEFAULT 0 NOT NULL,
-  `model_type`  tinyint(4) NOT NULL DEFAULT 0 COMMENT '0:standard',
-  `target_type` VARCHAR(255) DEFAULT NULL,
-  `target_id`   BIGINT(20)   DEFAULT NULL COMMENT 'community id ',
-  `create_time` DATETIME     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-ALTER TABLE `eh_quality_inspection_standards`
-  ADD COLUMN `refer_id` BIGINT(20) NULL;
-
--- 品质核查权限细化end  by jiarui  20171215
 
 -- 物业巡检权限细化 start  by jiarui
 CREATE TABLE `eh_equipment_model_community_map` (
@@ -42,10 +24,9 @@ ALTER TABLE `eh_equipment_inspection_standards`
   ADD COLUMN `refer_id` BIGINT(20) NULL;
 
 ALTER TABLE `eh_equipment_inspection_templates`
-  ADD COLUMN `refer_id`  bigint(20) NULL;
-
-ALTER TABLE `eh_equipment_inspection_templates`
   ADD COLUMN `target_type` VARCHAR(32) NOT NULL DEFAULT '',
   ADD COLUMN `target_id` BIGINT(20) NOT NULL DEFAULT 0 ;
 -- 物业巡检权限细化 end  by  jiarui
+
+-- merge from forum2.6 by yanjun 201712121010 start
 
