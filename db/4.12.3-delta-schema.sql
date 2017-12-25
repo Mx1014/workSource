b31addda2cf649c4ac44f2b7bb5047dc27f930ea
dengs <shuang.deng@zuolin.com>
2017-12-25 15:55:19 +0800

    sql add

--- db/4.12.3-delta-schema.sql
+++ db/4.12.3-delta-schema.sql
@@ -1,2 +1,6 @@
 -- 工作流日志加会签标志字段 add by xq.tian 2017/12/25
 ALTER TABLE eh_flow_event_logs ADD enter_log_complete_flag TINYINT NOT NULL DEFAULT 0;
+
+
+-- 意见反馈增加一个字段“业务自定义参数”  add by yanjun 20171220
+ALTER TABLE `eh_feedbacks` ADD COLUMN `target_param`  text NULL AFTER `target_id`;
