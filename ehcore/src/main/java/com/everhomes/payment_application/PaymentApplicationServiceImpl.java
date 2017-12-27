77d5ca1055be5d6a5983e8d30b8c357edef1eae1
ying.xiong <ying.xiong@zuolin.com>
2017-12-27 15:32:18 +0800

    add schema

---
+++ ehcore/src/main/java/com/everhomes/payment_application/PaymentApplicationServiceImpl.java
@@ -0,0 +1,29 @@
+package com.everhomes.payment_application;
+
+import com.everhomes.rest.payment_application.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ying.xiong on 2017/12/27.
+ */
+@Component
+public class PaymentApplicationServiceImpl implements PaymentApplicationService {
+    @Override
+    public PaymentApplicationDTO createPaymentApplication(CreatePaymentApplicationCommand cmd) {
+        return null;
+    }
+
+    @Override
+    public PaymentApplicationDTO getPaymentApplication(GetPaymentApplicationCommand cmd) {
+        return null;
+    }
+
+    @Override
+    public ListPaymentApplicationResponse listPaymentApplications(ListPaymentApplicationCommand cmd) {
+        return null;
+    }
+
+    private PaymentApplicationDTO toPaymentApplicationDTO() {
+
+    }
+}
---
+++ ehnextgen-api/src/main/java/com/everhomes/payment_application/PaymentApplicationService.java
@@ -0,0 +1,12 @@
+package com.everhomes.payment_application;
+
+import com.everhomes.rest.payment_application.*;
+
+/**
+ * Created by ying.xiong on 2017/12/27.
+ */
+public interface PaymentApplicationService {
+    PaymentApplicationDTO createPaymentApplication(CreatePaymentApplicationCommand cmd);
+    PaymentApplicationDTO getPaymentApplication(GetPaymentApplicationCommand cmd);
+    ListPaymentApplicationResponse listPaymentApplications(ListPaymentApplicationCommand cmd);
+}
