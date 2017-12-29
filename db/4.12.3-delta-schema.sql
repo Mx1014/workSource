54cbf9fa822d791f6c0efd5ddaa8563e02dc3fe9
jiarui <rui.jia@zuolin.com>
2017-12-29 09:17:52 +0800

    add category_id for quality

--- db/4.12.3-delta-schema.sql
+++ db/4.12.3-delta-schema.sql
@@ -109,7 +109,8 @@ ALTER TABLE `eh_equipment_inspection_templates`
 -- 品质核查权限细化start by jiarui  20171215
 CREATE TABLE `eh_quality_inspection_model_community_map` (
   `id`          BIGINT(20) NOT NULL,
-  `model_id` BIGINT(20) DEFAULT 0 NOT NULL,
+  `model_id` BIGINT(20) DEFAULT 0 ,
+  `category_id` BIGINT(20) DEFAULT 0 NOT NULL,
   `model_type`  tinyint(4) NOT NULL DEFAULT 0 COMMENT '0:standard',
   `target_type` VARCHAR(255) DEFAULT NULL,
   `target_id`   BIGINT(20)   DEFAULT NULL COMMENT 'community id ',
