b963659e65b51f59ff13e27bed3d0c0515cc20c1
yanjun <jun.yan@zuolin.com>
2018-01-05 16:05:59 +0800

    fix schame

--- db/portal-1.2-delta-schema.sql
+++ db/portal-1.2-delta-schema.sql
@@ -2,11 +2,12 @@
 CREATE TABLE `eh_portal_versions` (
   `id` bigint(20) NOT NULL,
   `namespace_id` int(11) NOT NULL,
-  `sync_version` int(11) NOT NULL,
-  `publish_version` int(11) NOT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `sync_version` int(11) DEFAULT NULL,
+  `publish_version` int(11) DEFAULT NULL,
   `sync_time` datetime DEFAULT NULL,
   `publish_time` datetime DEFAULT NULL,
-  `status` tinyint(4) DEFAULT NULL COMMENT '1-edit,2-publis success, 3-publish fail',
+  `status` tinyint(4) DEFAULT NULL COMMENT '0-init,1-edit,2-publis success, 3-publish fail',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
