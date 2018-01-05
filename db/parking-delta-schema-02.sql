bb882e0a11f3e87e353a5e846c8ec8d1ceb7d16b
wen.sun <wen.sun@zuolin.com>
2018-01-05 16:44:24 +0800

    parking rental

--- db/parking-delta-schema-02.sql
+++ db/parking-delta-schema-02.sql
@@ -8,9 +8,9 @@ ALTER TABLE `eh_feedbacks` ADD COLUMN `target_param`  text NULL AFTER `target_id
 -- add by st.zheng 增加起步价
 ALTER TABLE `eh_rentalv2_price_rules`
 ADD COLUMN `price_type` TINYINT(4) NULL AFTER `rental_type`,
-ADD COLUMN `initiate_price` DECIMAL(10,2) NULL AFTER `weekend_price`,
-ADD COLUMN `org_member_initiate_price` DECIMAL(10,2) NULL AFTER `org_member_weekend_price`,
-ADD COLUMN `approving_user_initiate_price` DECIMAL(10,2) NULL AFTER `approving_user_weekend_price`;
+ADD COLUMN `initiate_price` DECIMAL(10,2) NULL,
+ADD COLUMN `org_member_initiate_price` DECIMAL(10,2) NULL,
+ADD COLUMN `approving_user_initiate_price` DECIMAL(10,2) NULL;
 
 ALTER TABLE `eh_rentalv2_price_packages`
 ADD COLUMN `price_type` TINYINT(4) NULL AFTER `rental_type`,
