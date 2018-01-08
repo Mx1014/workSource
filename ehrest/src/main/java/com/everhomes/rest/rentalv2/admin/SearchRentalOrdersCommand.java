61a8b4ccf9ec04a020ed524182a2da0b352ace69
wen.sun <wen.sun@zuolin.com>
2018-01-08 19:32:52 +0800

    parking rental

---
+++ ehrest/src/main/java/com/everhomes/rest/rentalv2/admin/SearchRentalOrdersCommand.java
@@ -0,0 +1,113 @@
+package com.everhomes.rest.rentalv2.admin;
+
+import com.everhomes.util.StringHelper;
+
+/**
+ * @author sw on 2018/1/8.
+ */
+public class SearchRentalOrdersCommand {
+    private String resourceType;
+    private Long rentalSiteId ;
+    private Long resourceTypeId;
+    private Long startTime;
+    private Long endTime;
+    private String keyword;
+    private String tag1;
+    private String tag2;
+    private Byte billStatus;
+    private Long pageAnchor;
+    private Integer pageSize;
+
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
+
+    public String getTag1() {
+        return tag1;
+    }
+
+    public void setTag1(String tag1) {
+        this.tag1 = tag1;
+    }
+
+    public String getTag2() {
+        return tag2;
+    }
+
+    public void setTag2(String tag2) {
+        this.tag2 = tag2;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public Long getRentalSiteId() {
+        return rentalSiteId;
+    }
+
+    public void setRentalSiteId(Long rentalSiteId) {
+        this.rentalSiteId = rentalSiteId;
+    }
+
+    public Long getResourceTypeId() {
+        return resourceTypeId;
+    }
+
+    public void setResourceTypeId(Long resourceTypeId) {
+        this.resourceTypeId = resourceTypeId;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+    }
+
+    public Long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Long endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public Byte getBillStatus() {
+        return billStatus;
+    }
+
+    public void setBillStatus(Byte billStatus) {
+        this.billStatus = billStatus;
+    }
+
+    public Long getPageAnchor() {
+        return pageAnchor;
+    }
+
+    public void setPageAnchor(Long pageAnchor) {
+        this.pageAnchor = pageAnchor;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
