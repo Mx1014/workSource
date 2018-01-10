4b40dc8c91baf3fa8e4ac2bf7be0f045a5b706e7
jiarui <rui.jia@zuolin.com>
2018-01-10 15:29:56 +0800

    fix schedule gen tasks

--- db/equipment-3.0.3-delta-data-release.sql
+++ db/equipment-3.0.3-delta-data-release.sql
@@ -10,3 +10,13 @@ WHERE review_status IN (0, 3 ,4) OR review_result = 2;
 -- 上版bug数据修改
 
 --  标准数据增加周期类型 及关系表状态 end by jiarui 20180105
+-- 巡检任务状态统一 start by jiarui 20180105
+UPDATE eh_equipment_inspection_tasks
+SET `status` = 6
+WHERE `status` = 4 AND review_result = 2;
+
+UPDATE eh_equipment_inspection_tasks
+SET `status` = 7
+WHERE `status` = 4 AND review_result = 1;
+
+-- 巡检任务状态统一 end by jiarui 20180105
