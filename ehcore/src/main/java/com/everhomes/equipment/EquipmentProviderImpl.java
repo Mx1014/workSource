90c96500b14a59731accaac8516fcf06594c2f95
jiarui <rui.jia@zuolin.com>
2018-01-10 12:42:22 +0800

    fix schedule gen tasks

--- ehcore/src/main/java/com/everhomes/equipment/EquipmentProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/equipment/EquipmentProviderImpl.java
@@ -155,8 +155,8 @@ public class EquipmentProviderImpl implements EquipmentProvider {
 
     @PostConstruct
     public void init() {
-        //String cronExpression = configurationProvider.getValue(ConfigConstants.SCHEDULE_EQUIPMENT_TASK_TIME, "0 0 0 * * ? ");
-        String cronExpression = configurationProvider.getValue(ConfigConstants.SCHEDULE_EQUIPMENT_TASK_TIME, "0 */5 * * * ?");
+        String cronExpression = configurationProvider.getValue(ConfigConstants.SCHEDULE_EQUIPMENT_TASK_TIME, "0 0 0 * * ? ");
+//        String cronExpression = configurationProvider.getValue(ConfigConstants.SCHEDULE_EQUIPMENT_TASK_TIME, "0 */5 * * * ?");
 
         String taskServer = configurationProvider.getValue(ConfigConstants.TASK_SERVER_ADDRESS, "127.0.0.1");
         LOGGER.info("================================================taskServer: " + taskServer + ", equipmentIp: " + equipmentIp);
