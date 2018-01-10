644b46520607232167e657ed80a4c99e942de076
janson <gandancing@gmail.com>
2018-01-10 14:19:21 +0800

    better for auth2

--- oauth2-client/src/main/java/com/everhomes/oauth2/endpoint/SdkConstants.java
+++ oauth2-client/src/main/java/com/everhomes/oauth2/endpoint/SdkConstants.java
@@ -1,16 +1,19 @@
 package com.everhomes.oauth2.endpoint;
 
 public interface SdkConstants {
-    public static final String API_KEY = "d80e06ca-3766-11e5-b18f-b083fe4e159f";
+    public static final String API_KEY = "c3bbba16-7955-4725-b8e2-9aeb9a866919";
 
     // demostration only, it should be kept in a safe place
-    public static final String API_SECRET = "g1JOZUM3BYzWpZD5Q7p3z+i/z0nj2TcokTFx2ic53FCMRIKbMhSUCi7fSu9ZklFCZ9tlj68unxur9qmOji4tNg==";
+    public static final String API_SECRET = "Z136mxMc6wTMNSKl4+3shzCRsj3qEZtuCKoxUyRExinN3r0iYTUcKlCOHe+YnO9I4Fx87u1hOmr+c2kbYZBeaQ==";
 
-    public static final String API_AUTHORIZE_SERVICE_URI = "http://10.1.1.92:8080/oauth2/authorize";
+    //#oauth2_redirect
+    //http://qidixiangshan-beta.zuolin.com
+    public static final String API_AUTHORIZE_SERVICE_URI = "http://10.1.10.94/evh/oauth2/authorize";
 
-    public static final String API_TOKEN_SERVICE_URI = "http://10.1.1.92:8080/oauth2/token";
+    public static final String API_TOKEN_SERVICE_URI = "http://10.1.10.94/evh/oauth2/token";
 
-    public static final String API_OAUTH2API_URI = "http://10.1.1.92:8080/oauth2api";
+    public static final String API_OAUTH2API_URI = "http://10.1.10.94/evh/oauth2api";
 
-    public static final String CLIENT_REDIRECT_URI = "http://10.1.1.92:7788/redirect";
+    // 本地 url 地址
+    public static final String CLIENT_REDIRECT_URI = "http://10.1.110.13:7788/redirect";
 }
