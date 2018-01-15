549b6c5b4e582db22332d8c7d018ac208a250487
nan.rong <nan.rong@zuolin.com>
2018-01-15 14:50:39 +0800

    provider added

---
+++ ehcore/src/main/java/com/everhomes/filemanagement/FileManagementProviderImpl.java
@@ -0,0 +1,7 @@
+package com.everhomes.filemanagement;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileManagementProviderImpl implements FileManagementProvider{
+}
