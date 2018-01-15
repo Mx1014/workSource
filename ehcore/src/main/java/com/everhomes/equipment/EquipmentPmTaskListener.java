c291b959288bc81b7ae9c020f02507d2482e1da6
jiarui <rui.jia@zuolin.com>
2018-01-15 17:47:18 +0800

    fix

---
+++ ehcore/src/main/java/com/everhomes/equipment/EquipmentPmTaskListener.java
@@ -0,0 +1,22 @@
+package com.everhomes.equipment;
+
+import com.everhomes.pmtask.PmTask;
+import com.everhomes.pmtask.PmTaskListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by rui.jia  2018/1/15 15 :21
+ */
+@Component(PmTaskListener.PMTASK_PREFIX + EquipmentConstant.EQUIPMENT_REPAIR)
+public class EquipmentPmTaskListener implements PmTaskListener {
+    @Autowired
+    private EquipmentProvider equipmentProvider;
+    @Override
+    public void onTaskSuccess(PmTask pmTask, Long referId) {
+        //更新巡检log状态referId为设备id
+        EquipmentInspectionTasksLogs log = equipmentProvider.getMaintanceLogByEquipmentId(referId);
+        equipmentProvider.updateMaintanceInspectionLogsById(log.getId());
+
+    }
+}
