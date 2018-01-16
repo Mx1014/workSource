895f3d0d856f682218c80305304aa4072f5c58f5
jiarui <rui.jia@zuolin.com>
2018-01-16 10:15:41 +0800

    fix

--- db/opv2-2.0-delta-data-release.sql
+++ db/opv2-2.0-delta-data-release.sql
@@ -1,3 +1,26 @@
 -- 滞纳金变量 by wentian
 SET @var_id = (SELECT max(`id`) from `eh_payment_variables`);
 INSERT INTO `eh_payment_variables` (`id`, `charging_standard_id`, `charging_items_id`, `name`, `creator_uid`, `create_time`, `operator_uid`, `update_time`, `identifier`) VALUES (@var_id:=@var_id+1, NULL, '6', '欠费', '0', '2017-10-16 09:31:00', NULL, '2017-10-16 09:31:00', 'qf');
+
+--  标准数据增加周期类型及关系表状态start by jiarui 20180105
+UPDATE eh_equipment_inspection_standards
+SET repeat_type = (SELECT repeat_type FROM eh_repeat_settings WHERE id = eh_equipment_inspection_standards.repeat_setting_id)
+WHERE STATUS =2  AND repeat_type =0;
+
+-- 上版未置状态数据修改
+UPDATE eh_equipment_inspection_equipment_standard_map
+SET `status` = 0
+WHERE review_status IN (0, 3 ,4) OR review_result = 2;
+-- 上版bug数据修改
+
+--  标准数据增加周期类型 及关系表状态 end by jiarui 20180105
+-- 巡检任务状态统一 start by jiarui 20180105
+UPDATE eh_equipment_inspection_tasks
+SET `status` = 6
+WHERE `status` = 4 AND review_result = 2;
+
+UPDATE eh_equipment_inspection_tasks
+SET `status` = 7
+WHERE `status` = 4 AND review_result = 4;
+
+-- 巡检任务状态统一 end by jiarui 20180105
