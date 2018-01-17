73d330142f143f865318fabc0a9e37c231de7efd
jiarui <rui.jia@zuolin.com>
2018-01-17 11:30:08 +0800

    add schema

--- db/opv2-2.0-delta-schema.sql
+++ db/opv2-2.0-delta-schema.sql
@@ -117,4 +117,19 @@ ALTER TABLE `eh_equipment_inspection_standards`
 ALTER TABLE `eh_equipment_inspection_task_logs`
   ADD COLUMN `equipment_id`  bigint(20) NULL DEFAULT 0 ;
 
+-- 离线支持  jiarui
+ALTER TABLE `eh_quality_inspection_specifications`
+  ADD COLUMN `update_uid` BIGINT (20) NULL AFTER `status`;
+
+ALTER TABLE `eh_quality_inspection_specifications`
+  ADD COLUMN `update_time` datetime NULL ON UPDATE CURRENT_TIMESTAMP AFTER `update_uid`;
+
+ALTER TABLE `eh_quality_inspection_specifications`
+  ADD COLUMN `delete_uid` BIGINT (20) NULL AFTER `update_uid`;
+
+ALTER TABLE `eh_quality_inspection_specifications`
+  ADD COLUMN `delete_time` datetime NULL ON UPDATE CURRENT_TIMESTAMP AFTER `delete_uid`;
+
+-- 离线支持  jiarui
+
 
