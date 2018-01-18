1f6a122358bccabe8e20bdb670a0390f71511d39
ying.xiong <ying.xiong@zuolin.com>
2018-01-18 14:08:03 +0800

    add schema

--- db/sync-task-delta-schema.sql
+++ db/sync-task-delta-schema.sql
@@ -10,3 +10,5 @@ CREATE TABLE `eh_sync_data_tasks` (
   `update_time` DATETIME,
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `eh_addresses` ADD COLUMN `version` VARCHAR(32) COMMENT '版本号';
