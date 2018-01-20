297c8ef5c7d750cbfbb3c4cba44df1209482b5ee
lei.lv <lei.lv@zuolin.com>
2018-01-20 10:54:26 +0800

    fix

--- ehrest/src/main/java/com/everhomes/rest/openapi/FunctionCardDto.java
+++ ehrest/src/main/java/com/everhomes/rest/openapi/FunctionCardDto.java
@@ -5,12 +5,6 @@ import java.util.List;
 import java.util.Locale;
 
 public class FunctionCardDto {
-<<<<<<< HEAD
-    private String name;
-    private byte  type;
-    private list<string> giinfo;
-
-=======
     private FunctionCategory categoryName;
     private List<String> jsonData;
 
@@ -29,10 +23,4 @@ public class FunctionCardDto {
     public void setJsonData(List<String> jsonData) {
         this.jsonData = jsonData;
     }
->>>>>>> 26140dd856e44df0657bb940ac0b9e5c122e7806
-}
-
-public class FuctionCardCategory{
-    private String name;
-    private List<FunctionCardDto> dtos;
 }
