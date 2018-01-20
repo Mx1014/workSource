918edd5fb5cb623c29a74811e62c414b0d0545e6
jiarui <rui.jia@zuolin.com>
2018-01-20 12:01:45 +0800

    fix schedule job not process feeDoc

--- ehnextgen-api/src/main/java/com/everhomes/equipment/EquipmentProvider.java
+++ ehnextgen-api/src/main/java/com/everhomes/equipment/EquipmentProvider.java
@@ -216,7 +216,7 @@ public interface EquipmentProvider {
 
     EquipmentInspectionTasksLogs getMaintanceLogByEquipmentId(Long referId);
 
-	void updateMaintanceInspectionLogsById(Long id);
+	void updateMaintanceInspectionLogsById(Long taskLogId,Long flowCaseId);
 
     void statInMaintanceTaskCount(TasksStatData stat,Timestamp startTime, Timestamp endTime,StatTodayEquipmentTasksCommand cmd);
 
