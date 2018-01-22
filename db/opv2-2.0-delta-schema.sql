950acc0c539d2cbf78270709507fa5931b479e4d
yanjun <jun.yan@zuolin.com>
2018-01-22 14:05:34 +0800

    fix schame

--- db/opv2-2.0-delta-schema.sql
+++ db/opv2-2.0-delta-schema.sql
@@ -1,6 +1,10 @@
 -- domain 增加name, icon
-ALTER TABLE `eh_domains` ADD COLUMN `icon_uri`  varchar(255) NULL AFTER `create_time`;
+ALTER TABLE `eh_domains` ADD COLUMN `favicon_uri`  varchar(255) NULL;
 ALTER TABLE `eh_domains` ADD COLUMN `name`  varchar(255) NULL AFTER `namespace_id`;
+ALTER TABLE `eh_domains` ADD COLUMN `login_bg_uri`  varchar(255) NULL,
+ALTER TABLE `eh_domains` ADD COLUMN `login_logo_uri`  varchar(255) NULL,
+ALTER TABLE `eh_domains` ADD COLUMN `menu_logo_uri`  varchar(255) NULL,
+ALTER TABLE `eh_domains` ADD COLUMN `menu_logo_collapsed_uri`  varchar(255) NULL;
 
 ALTER TABLE `eh_web_menu_scopes` ADD COLUMN `app_id`  bigint(20) NULL COMMENT 'eh_service_module_app id';
 ALTER TABLE `eh_web_menu_scopes` DROP INDEX `u_menu_scope_owner` , ADD UNIQUE INDEX `u_menu_scope_owner` (`menu_id`, `owner_type`, `owner_id`, `app_id`) USING BTREE ;
@@ -224,14 +228,6 @@ CREATE TABLE `eh_sync_offline_tasks` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- end by xiongying
-
--- 在domain中增加一些icon
-ALTER TABLE `eh_domains`
-CHANGE COLUMN `icon_uri` `favicon_uri`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL AFTER `create_time`,
-ADD COLUMN `login_bg_uri`  varchar(255) NULL AFTER `favicon_uri`,
-ADD COLUMN `login_logo_uri`  varchar(255) NULL AFTER `login_bg_uri`,
-ADD COLUMN `menu_logo_uri`  varchar(255) NULL AFTER `login_logo_uri`,
-ADD COLUMN `menu_logo_collapsed_uri`  varchar(255) NULL AFTER `menu_logo_uri`;
 
 
 -- add by sw 20180122 vip车位
