bf5eab964993fa4b21337aff345e06721429af63
dengs <shuang.deng@zuolin.com>
2018-01-23 14:35:01 +0800

    fix

--- db/opv2-2.0-delta-data-release.sql
+++ db/opv2-2.0-delta-data-release.sql
@@ -112,3 +112,6 @@ INSERT INTO `ehcore`.`eh_configurations`(`name`, `value`, `description`, `namesp
   VALUES ('parking.dingding.hubMac', 'CC:1B:E0:E0:09:F8', NULL, 0, NULL);
 
 
+-- 删除审批这种服务联盟跳转模块 by dengs,2018/1/12
+select * from eh_service_alliance_jump_module WHERE module_url like '%zl://approval/create%' and module_name='审批';
+DELETE FROM eh_service_alliance_jump_module WHERE module_url like '%zl://approval/create%' and module_name='审批';
