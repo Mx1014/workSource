8c5799d2039b7fce5ceb7b44240321d1322bffc6
lei.lv <lei.lv@zuolin.com>
2018-01-24 17:11:08 +0800

    fix

--- ehborder/src/main/java/com/everhomes/border/WebSocketSessionProxy.java
+++ ehborder/src/main/java/com/everhomes/border/WebSocketSessionProxy.java
@@ -1,14 +1,9 @@
 package com.everhomes.border;
 
 import com.everhomes.rest.message.MessageRecordDto;
-import com.everhomes.rest.messaging.MessageDTO;
-import com.everhomes.rest.rpc.PduFrame;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -17,10 +12,10 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Component
-public class WebSocketSessionProxy{
+public class WebSocketSessionProxy {
 
     @Autowired
-    private TaskScheduler taskScheduler;
+    private ThreadPoolTaskScheduler taskScheduler;
 
     @Autowired
     private MessagePersistWorkerCopy messagePersistWorker;
@@ -29,19 +24,19 @@ public class WebSocketSessionProxy{
 
 
     @PostConstruct
-    public void setup(){
-        taskScheduler.scheduleAtFixedRate(()-> {
-            while (!queue.isEmpty()){
+    public void setup() {
+        taskScheduler.scheduleAtFixedRate(() -> {
+            while (!queue.isEmpty()) {
                 MessageRecordDto record = queue.poll();
                 this.messagePersistWorker.handleMessagePersist(record);
             }
-        }, 5*1000);
+        }, 5 * 1000);
     }
 
-    public static void sendMessage(WebSocketSession session, WebSocketMessage message){
+    public static void sendMessage(WebSocketSession session, WebSocketMessage message) {
 //        queue.offer(message);
         try {
-            synchronized (session){
+            synchronized (session) {
                 session.sendMessage(message);
             }
         } catch (IOException e) {
