e045e4c362701da2364b6790a431bd8a46308c0a
lei.lv <lei.lv@zuolin.com>
2018-01-25 11:18:04 +0800

    fix

--- db/5.1.3-delta-data-release.sql
+++ db/5.1.3-delta-data-release.sql
@@ -5,3 +5,8 @@ update eh_parking_lots SET config_json = REPLACE(config_json,'}',',"identityCard
 
 INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`) VALUES ('parking.zijing.marketid', '075500012201801220001', '紫荆车辆放行markid', 0, NULL);
 INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`) VALUES ('parking.zijing.userid', '0755000120180122000000000001', '紫荆车辆放行userid', 0, NULL);
+
+
+-- 更新所有域空间的模块类型
+update eh_service_modules set module_control_type = 'unlimit_control' where id = 41300;
+update eh_service_modules set module_control_type = 'unlimit_control' where id = 49200;
