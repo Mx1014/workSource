9ae6d534a8397f9f2147e2b9b3efb0c1b57a2905
lei.lv <lei.lv@zuolin.com>
2018-01-25 14:14:55 +0800

    fix

--- db/5.1.2-mlog-delta-schema.sql
+++ db/5.1.2-mlog-delta-schema.sql
@@ -5,18 +5,18 @@
 CREATE TABLE `eh_message_records` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` int(11) DEFAULT '0',
+  `dst_channel_token` varchar(32) DEFAULT NULL,
+  `dst_channel_type` varchar(32) DEFAULT NULL,
+  `status` varchar(32) COMMENT 'message status',
   `app_id` bigint(20) DEFAULT '1' COMMENT 'default to messaging app itself',
   `message_seq` bigint(20) DEFAULT NULL COMMENT 'message sequence id generated at server side',
   `sender_uid` bigint(20) DEFAULT NULL,
   `sender_tag` varchar(32) DEFAULT NULL COMMENT 'sender generated tag',
-  `dst_channel_type` varchar(32) DEFAULT NULL,
-  `dst_channel_token` varchar(32) DEFAULT NULL,
   `channels_info` varchar(2048) DEFAULT NULL,
   `body_type`varchar(32),
   `body` varchar(2048),
   `deliveryOption` int(2) DEFAULT '0',
   `create_time` datetime NOT NULL COMMENT 'message creation time',
-  `status` tinyint(4) DEFAULT NULL COMMENT 'message status',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
