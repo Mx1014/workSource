a05963f39f79c404aa0a3beb7b67e34fd86fa8ea
lei.lv <lei.lv@zuolin.com>
2018-01-25 18:46:41 +0800

    fix

--- db/5.1.2-mlog-delta-schema.sql
+++ db/5.1.2-mlog-delta-schema.sql
@@ -18,6 +18,7 @@ CREATE TABLE `eh_message_records` (
   `deliveryOption` int(2) DEFAULT '0',
   `create_time` datetime NOT NULL COMMENT 'message creation time',
   `session_token` varchar(128),
+  `deviceId` varchar(2048),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
