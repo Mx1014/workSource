b30cdee487a5b2a23890ba8e8683eecc96c52a5b
jiarui <rui.jia@zuolin.com>
2018-01-26 15:55:18 +0800

    add log

--- ehcore/src/main/java/com/everhomes/equipment/EquipmentStandardSearcherImpl.java
+++ ehcore/src/main/java/com/everhomes/equipment/EquipmentStandardSearcherImpl.java
@@ -235,18 +235,21 @@ public class EquipmentStandardSearcherImpl extends AbstractElasticSearch impleme
         if (maps != null && maps.size() > 0) {
             for (EquipmentModelCommunityMap map : maps) {
                 EquipmentInspectionStandards standard = equipmentProvider.findStandardById(map.getModelId());
-                //有效的才加  fix bug
-                if (cmd.getStatus() != null) {
-                    if (!Objects.equals(standard.getStatus(), cmd.getStatus())) {
-                        continue;
-                    }
-                }
-                if (cmd.getStandardType() != null) {
-                    if (!Objects.equals(standard.getStatus(), cmd.getStandardType())) {
-                        continue;
-                    }
-                }
                 if (standard != null && EquipmentStandardStatus.ACTIVE.equals(EquipmentStandardStatus.fromStatus(standard.getStatus()))) {
+                    LOGGER.info("map standard :"+standard);
+                    //有效的才加  fix bug
+                    if (cmd.getStatus() != null) {
+                        LOGGER.info("map standard :"+cmd.getStatus());
+                        if (!Objects.equals(standard.getStatus(), cmd.getStatus())) {
+                            continue;
+                        }
+                    }
+                    if (cmd.getStandardType() != null) {
+                        LOGGER.info("map standard :"+cmd.getStandardType());
+                        if (!Objects.equals(standard.getStatus(), cmd.getStandardType())) {
+                            continue;
+                        }
+                    }
                     processRepeatSetting(standard);
                     EquipmentStandardsDTO dto = ConvertHelper.convert(standard, EquipmentStandardsDTO.class);
                     dto.setDescription("");
