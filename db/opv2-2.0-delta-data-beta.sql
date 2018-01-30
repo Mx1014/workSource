a67847d43e038e491b081d5ea6a3d8cda53059dc
yanjun <jun.yan@zuolin.com>
2018-01-30 09:51:47 +0800

    fix

--- db/opv2-2.0-delta-data-beta.sql
+++ db/opv2-2.0-delta-data-beta.sql
@@ -1,2 +1,15 @@
--- 1、覆盖eh_web_menus、eh_web_menu_scopes和eh_domains，从88覆盖到新的数据库表
+-- 1、覆盖eh_web_menus、eh_service_modules和eh_domains，从88覆盖到新的数据库表
 -- 2、更新eh_domains中namespace_id等于0的数据的domain的值为服务器实际的ip
+
+-- 清理数据
+/*
+delete from `eh_portal_content_scopes`;
+delete from `eh_portal_item_categories`;
+delete from `eh_portal_item_groups`;
+delete from `eh_portal_items`;
+delete from `eh_portal_launch_pad_mappings`;
+delete from `eh_portal_layouts`;
+delete from `eh_service_module_apps`;
+delete from `eh_portal_versions`;
+delete from `eh_portal_version_users`;
+*/
