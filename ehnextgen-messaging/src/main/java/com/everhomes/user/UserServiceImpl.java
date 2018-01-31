306dda2aa4fed473b934ff2789daf0b08f32d1b0
lei.lv <lei.lv@zuolin.com>
2018-01-31 10:35:37 +0800

    fix

--- ehnextgen-messaging/src/main/java/com/everhomes/user/UserServiceImpl.java
+++ ehnextgen-messaging/src/main/java/com/everhomes/user/UserServiceImpl.java
@@ -5535,15 +5535,15 @@ public class UserServiceImpl implements UserService {
 					Integer namespaceId = Integer.valueOf(tokenParam[2]);
 					String userToken = tokenParam[3];
 					LOGGER.debug("userLoginToken = {}", userToken);
-					LoginToken logintoken = WebTokenGenerator.getInstance().fromWebToken(userToken, LoginToken.class);
-//					//todo 验证
-					UserLogin login = logonByToken(logintoken);
-					LoginToken newToken = new LoginToken(login.getUserId(), login.getLoginId(), login.getLoginInstanceNumber(), login.getImpersonationId());
-					String tokenString = WebTokenGenerator.getInstance().toWebToken(newToken);
-					WebRequestInterceptor.setCookieInResponse("token", tokenString, contextRequest, contextResponse);
+//					LoginToken logintoken = WebTokenGenerator.getInstance().fromWebToken(userToken, LoginToken.class);
+////					//todo 验证
+//					UserLogin login = logonByToken(logintoken);
+//					LoginToken newToken = new LoginToken(login.getUserId(), login.getLoginId(), login.getLoginInstanceNumber(), login.getImpersonationId());
+//					String tokenString = WebTokenGenerator.getInstance().toWebToken(newToken);
+//					WebRequestInterceptor.setCookieInResponse("token", tokenString, contextRequest, contextResponse);
 
 					Map valueMap = new HashMap();
-					valueMap.put("userLogin", GsonUtil.toJson(login));
+					valueMap.put("userLogin", GsonUtil.toJson(userToken));
 					valueMap.put("args",tokenParam[4]);
 					response.setMessage(GsonUtil.toJson(valueMap));
 					restResponse.setResponseObject(response);
