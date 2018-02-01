087c41a908dad83d060c89f7dca1ef8eba975df4
jiarui <rui.jia@zuolin.com>
2018-02-01 14:23:08 +0800

    add scope id for record

--- db/quality-offline-delta-schema.sql
+++ db/quality-offline-delta-schema.sql
@@ -12,3 +12,14 @@ ALTER TABLE `eh_quality_inspection_specifications`
   ADD COLUMN `delete_time` datetime NULL ON UPDATE CURRENT_TIMESTAMP AFTER `delete_uid`;
 
 -- 离线支持  jiarui
+-- 日志增加项目 jiarui
+
+ALTER TABLE `eh_quality_inspection_logs`
+  ADD COLUMN `scope_code`  tinyint(4) NULL DEFAULT 1 ;
+ALTER TABLE `eh_quality_inspection_logs`
+  ADD COLUMN `scope_id`  bigint(20) NULL DEFAULT 0 ;
+
+UPDATE  `eh_quality_inspection_logs`
+SET scope_id =(SELECT eh_quality_inspection_standards.target_id FROM  eh_quality_inspection_standards where eh_quality_inspection_standards.id = eh_quality_inspection_logs.target_id);
+
+-- 日志增加项目 jiarui
