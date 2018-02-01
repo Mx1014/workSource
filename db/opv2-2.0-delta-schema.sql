a5f74b4045425d57273af90f7f1fd19d041b4d94
jiarui <rui.jia@zuolin.com>
2018-02-01 15:52:10 +0800

    add schema

--- db/opv2-2.0-delta-schema.sql
+++ db/opv2-2.0-delta-schema.sql
@@ -160,9 +160,6 @@ ALTER TABLE `eh_quality_inspection_specifications`
 ALTER TABLE `eh_quality_inspection_logs`
   ADD COLUMN `scope_id`  bigint(20) NULL DEFAULT 0 ;
 
-UPDATE  `eh_quality_inspection_logs`
-SET scope_id =(SELECT eh_quality_inspection_standards.target_id FROM  eh_quality_inspection_standards where eh_quality_inspection_standards.id = eh_quality_inspection_logs.target_id);
-
 -- 日志增加项目 jiarui
 
 -- 文档管理1.0 add by nan.rong 01/12/2018
