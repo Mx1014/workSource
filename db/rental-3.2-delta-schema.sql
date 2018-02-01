747563b85497ed24da9736f8da9250e48a4790de
siting <st.zheng@zuolin.com>
2018-02-01 15:01:50 +0800

    rental

--- db/rental-3.2-delta-schema.sql
+++ db/rental-3.2-delta-schema.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `eh_rentalv2_items`
-CHANGE COLUMN `rental_resource_id` `source_id` BIGINT(20) NULL DEFAULT NULL ,
+  CHANGE COLUMN `rental_resource_id` `source_id` BIGINT(20) NULL DEFAULT NULL;
 ALTER TABLE `eh_rentalv2_items`
-ADD COLUMN `source_type` VARCHAR(45) NULL COMMENT 'DEFAULT: default_rule  RESOURCE: resource_rule' AFTER `id`;
+  ADD COLUMN `source_type` VARCHAR(45) NULL COMMENT 'DEFAULT: default_rule  RESOURCE: resource_rule' AFTER `id`;
