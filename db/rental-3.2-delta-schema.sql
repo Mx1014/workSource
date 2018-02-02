00dbd94aeb6dd7a091a918d63d566f4668681996
siting <st.zheng@zuolin.com>
2018-02-02 10:17:22 +0800

    rental

--- db/rental-3.2-delta-schema.sql
+++ db/rental-3.2-delta-schema.sql
@@ -4,3 +4,6 @@ ALTER TABLE `eh_rentalv2_items`
   ADD COLUMN `source_type` VARCHAR(45) NULL COMMENT 'DEFAULT: default_rule  RESOURCE: resource_rule' AFTER `id`;
 
 UPDATE eh_rentalv2_items set source_type = 'default_rule' where source_type IS NULL;
+
+ALTER TABLE `eh_rentalv2_config_attachments` 
+ADD COLUMN `default_order` INT NULL DEFAULT '0' AFTER `string_tag5`;
