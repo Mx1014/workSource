d0a56c05d1c330ee6030f3015be1db55f548b9cd
dengs <shuang.deng@zuolin.com>
2018-02-02 14:28:07 +0800

    merge gongweiyuding

--- db/opv2-2.0-delta-schema.sql
+++ db/opv2-2.0-delta-schema.sql
@@ -1489,3 +1489,33 @@ ALTER TABLE `eh_organization_member_details` ADD COLUMN `social_security_status`
 ALTER TABLE `eh_organization_member_details` ADD COLUMN `accumulation_fund_status` TINYINT NOT NULL DEFAULT 0 COMMENT '0-pending, 1-paying';
 
 -- 社保结束
+
+--工位预定开始 dengs
+
+-- by dengs. 工位预约添加范围 20180120
+ALTER TABLE `eh_office_cubicle_spaces` ADD COLUMN `owner_type` VARCHAR(128);
+ALTER TABLE `eh_office_cubicle_spaces` ADD COLUMN `owner_id` BIGINT;
+
+ALTER TABLE `eh_office_cubicle_categories` ADD COLUMN `position_nums` INTEGER;
+ALTER TABLE `eh_office_cubicle_categories` ADD COLUMN `name` VARCHAR(256);
+
+ALTER TABLE `eh_office_cubicle_orders` ADD COLUMN `flow_case_Id` BIGINT;
+ALTER TABLE `eh_office_cubicle_orders` ADD COLUMN `work_flow_status` TINYINT;
+ALTER TABLE `eh_office_cubicle_orders` ADD COLUMN `owner_type` VARCHAR(128);
+ALTER TABLE `eh_office_cubicle_orders` ADD COLUMN `owner_id` BIGINT;
+
+
+CREATE TABLE `eh_office_cubicle_ranges` (
+  `id` BIGINT NOT NULL,
+  `owner_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the ranges, community, etc',
+  `owner_id` BIGINT NOT NULL DEFAULT 0,
+  `space_id` BIGINT NOT NULL DEFAULT 0,
+  `creator_uid` BIGINT NOT NULL DEFAULT 0 COMMENT 'record creator user id',
+  `create_time` DATETIME,
+  `delete_uid` BIGINT NOT NULL DEFAULT 0 COMMENT 'record deleter user id',
+  `delete_time` DATETIME,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+--工位预定结束
