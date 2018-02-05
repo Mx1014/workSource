bbe2d910019ff9b9af0dbd4b03410231291d4c8e
nan.rong <nan.rong@zuolin.com>
2018-02-05 20:07:46 +0800

    addFileCatalogScopes bug fixed

--- ehcore/src/main/java/com/everhomes/filemanagement/FileManagementServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/filemanagement/FileManagementServiceImpl.java
@@ -272,7 +272,7 @@ public class FileManagementServiceImpl implements  FileManagementService{
 
         if (cmd.getScopes() == null || cmd.getScopes().size() <= 0){
             //  delete all scopes by catalogId
-            fileManagementProvider.deleteFileCatalogScopeNotInSourceIds(namespaceId, cmd.getCatalogId(), new ArrayList<>());
+            fileManagementProvider.deleteFileCatalogScopeByCatalogId(namespaceId, cmd.getCatalogId());
             return scopes;
         }
 
