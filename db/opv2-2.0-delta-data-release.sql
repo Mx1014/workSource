2d48071c38188fe779d43292bc4f9a031bf5c779
jiarui <rui.jia@zuolin.com>
2018-02-05 17:45:03 +0800

    add field dynamic form

--- db/opv2-2.0-delta-data-release.sql
+++ db/opv2-2.0-delta-data-release.sql
@@ -29,15 +29,6 @@ SET equipment_id = (SELECT  equipment_id FROM eh_equipment_inspection_tasks WHER
 -- 巡检任务状态统一 end by jiarui 20180105
 
 -- 增加经纬度动态表单  jiarui   20180122
-INSERT  INTO  `eh_var_fields` VALUES (10007, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10008, CONCAT('/',10008,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10107, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10007, CONCAT('/',10007,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10207, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10006, CONCAT('/',10006,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10307, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10005, CONCAT('/',10005,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10407, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10004, CONCAT('/',10004,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10507, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10003, CONCAT('/',10003,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10607, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10002, CONCAT('/',10002,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-INSERT  INTO  `eh_var_fields` VALUES (10707, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10001, CONCAT('/',10001,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
-
 INSERT  INTO  `eh_var_fields` VALUES (10926, 'equipment_inspection', 'geohash', '经纬度', 'Long', 10000, CONCAT('/',10000,'/'), 1, null, 2, 1, now(),null ,null,'{\"fieldParamType\": \"map\", \"length\": 32}');
 -- 增加经纬度动态表单  jiarui  20180122
 
