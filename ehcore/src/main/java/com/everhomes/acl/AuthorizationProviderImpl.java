f58ccc0f5963bfd4d803110a644501fd685c6c08
lei.lv <lei.lv@zuolin.com>
2018-02-05 17:01:14 +0800

    fix

--- ehcore/src/main/java/com/everhomes/acl/AuthorizationProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/acl/AuthorizationProviderImpl.java
@@ -33,7 +33,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import sun.tools.jconsole.Tab;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
