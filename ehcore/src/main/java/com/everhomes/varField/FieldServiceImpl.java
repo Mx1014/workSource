c8b59f53b8ccbf4a9f69b60c20074e1226114aa1
ying.xiong <ying.xiong@zuolin.com>
2018-02-06 16:00:38 +0800

    fix 23898

--- ehcore/src/main/java/com/everhomes/varField/FieldServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/varField/FieldServiceImpl.java
@@ -825,14 +825,14 @@ public class FieldServiceImpl implements FieldService {
         String fieldParam = field.getFieldParam();
         FieldParams params = (FieldParams) StringHelper.fromJsonString(fieldParam, FieldParams.class);
         //如果是select，则修改fieldName,在末尾加上Name，减去末尾的Id如果存在的话。由抽象跌入现实，拥有了名字，这是从神降格为人的过程---第六天天主波旬
-        if((params.getFieldParamType().equals("select") || params.getFieldParamType().equals("customizationSelect"))
-                && fieldName.lastIndexOf("Type") > -1) {
-            fieldName += "Name";
-        } else if((params.getFieldParamType().equals("select") || params.getFieldParamType().equals("customizationSelect")) && fieldName.lastIndexOf("Id") > -1){
+        if((params.getFieldParamType().equals("select") || params.getFieldParamType().equals("customizationSelect")) && fieldName.lastIndexOf("Id") > -1){
             if(!fieldName.equals("projectSource") && !fieldName.equals("status")){
                 fieldName = fieldName.split("Id")[0];
                 fieldName += "Name";
             }
+        } else if((params.getFieldParamType().equals("select") || params.getFieldParamType().equals("customizationSelect"))
+                && fieldName.lastIndexOf("Type") > -1) {
+            fieldName += "Name";
         }
 
         try {
