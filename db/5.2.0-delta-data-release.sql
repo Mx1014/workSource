869117cacbb4ac5f480235880c2d4f05cd3b9719
xq.tian <xq.tian@zuolin.com>
2018-02-09 15:04:32 +0800

    Modity test phone regex

--- db/5.2.0-delta-data-release.sql
+++ db/5.2.0-delta-data-release.sql
@@ -3,7 +3,7 @@ SET @configurations_id = IFNULL((SELECT MAX(id) FROM `eh_configurations`), 0);
 INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`)
   VALUES ((@configurations_id := @configurations_id + 1), 'sms.strictPhoneRegex', '^(?:13[0-9]|14[579]|15[0-3,5-9]|17[0135678]|18[0-9])\\d{8}$', '手机号正则', 0, NULL);
 INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`)
-  VALUES ((@configurations_id := @configurations_id + 1), 'sms.testPhoneRegex', '^(?:1[0125])\\d{9}$', '测试手机号正则', 0, NULL);
+  VALUES ((@configurations_id := @configurations_id + 1), 'sms.testPhoneRegex', '^(?:1[012])\\d{9}$', '测试手机号正则', 0, NULL);
 
 -- 滞纳金变量 by wentian
 SET @var_id = (SELECT max(`id`) from `eh_payment_variables`);
