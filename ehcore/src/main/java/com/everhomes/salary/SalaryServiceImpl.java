9efdd6fe7637d7237bc16466785fbda01724cf4a
wuhan <wuhan419@gmail.com>
2018-02-09 11:06:06 +0800

    get department name

--- ehcore/src/main/java/com/everhomes/salary/SalaryServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/salary/SalaryServiceImpl.java
@@ -1017,6 +1017,14 @@ public class SalaryServiceImpl implements SalaryService {
         return writeOutPut(wb);
     }
 
+    private String getDepartmentName(Long detailId) {
+        Long departmentId = organizationService.getDepartmentByDetailId(detailId);
+        Organization department = organizationProvider.findOrganizationById(departmentId);
+        if (department != null)
+            return department.getName();
+        return "";
+    }
+
     private void createSalaryDetailRow(XSSFSheet sheet, Long detailId, List<SalaryGroupEntity> groupEntities, List<SalaryEntityCategory> categories, NormalFlag isFile, String month, Long ownerId) {
 
         OrganizationMemberDetails detail = organizationProvider.findOrganizationMemberDetailsByDetailId(detailId);
@@ -1028,7 +1036,7 @@ public class SalaryServiceImpl implements SalaryService {
         row.createCell(++i).setCellValue(detail.getContactName());
         row.createCell(++i).setCellValue(detail.getContactToken());
         row.createCell(++i).setCellValue(detail.getEmployeeNo());
-        row.createCell(++i).setCellValue(detail.getDepartment());
+        row.createCell(++i).setCellValue(getDepartmentName(detail.getId()));
         row.createCell(++i).setCellValue(detail.getIdNumber());
         row.createCell(++i).setCellValue(detail.getSalaryCardNumber());
         row.createCell(++i).setCellValue("在职不在职荣楠没跟我说");
