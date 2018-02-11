56fb0d806322906999a93b626c8177f5c1559994
jiarui <rui.jia@zuolin.com>
2018-02-11 14:35:47 +0800

    add index taskLogs

--- db/equipment-3.0.3-delta-schema.sql
+++ db/equipment-3.0.3-delta-schema.sql
@@ -88,6 +88,7 @@ ALTER TABLE `eh_equipment_inspection_task_logs`
   ADD COLUMN `maintance_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: inactive 1: wating, 2: allocated 3: completed 4: closed';
 ALTER TABLE `eh_equipment_inspection_task_logs`
   ADD COLUMN `pm_task_id` bigint(20) NULL DEFAULT 0 ;
+ALTER TABLE eh_equipment_inspection_task_logs ADD INDEX eq_log_pm_task_id (pm_task_id) ;
 
 -- 设备操作记录表  by jiarui
 CREATE TABLE `eh_equipment_inspection_equipment_logs` (
