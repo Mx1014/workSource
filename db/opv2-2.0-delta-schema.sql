9b351728e5939c3d24c8223bdbed71bca7cdd477
nan.rong <nan.rong@zuolin.com>
2018-02-11 17:32:45 +0800

    api update

--- db/opv2-2.0-delta-schema.sql
+++ db/opv2-2.0-delta-schema.sql
@@ -1679,30 +1679,21 @@ CREATE TABLE `eh_general_approval_scope_map` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 UPDATE eh_general_approvals SET approval_attribute = 'CUSTOMIZE' WHERE approval_attribute IS NULL;
-ALTER TABLE eh_general_approvals MODIFY approval_attribute VARCHAR(128) NOT NULL DEFAULT 'CUSTOMIZE';
+ALTER TABLE eh_general_approvals MODIFY approval_attribute VARCHAR(128) NOT NULL DEFAULT 'CUSTOMIZE' COMMENT 'CUSTOMIZE, DEFAULT';
 
 UPDATE eh_general_approvals SET modify_flag = 1 WHERE modify_flag IS NULL;
-ALTER TABLE eh_general_approvals MODIFY modify_flag TINYINT NOT NULL DEFAULT 1;
+ALTER TABLE eh_general_approvals MODIFY modify_flag TINYINT NOT NULL DEFAULT 1 COMMENT '0: no, 1: yes';
 
 UPDATE eh_general_approvals SET delete_flag = 1 WHERE delete_flag IS NULL;
-ALTER TABLE eh_general_approvals MODIFY delete_flag TINYINT NOT NULL DEFAULT 1;
-
-UPDATE eh_general_approvals SET approval_attribute = 'CUSTOMIZE' WHERE approval_attribute IS NULL;
-ALTER TABLE eh_general_approvals MODIFY approval_attribute VARCHAR(128) NOT NULL DEFAULT 'CUSTOMIZE';
-
-UPDATE eh_general_approvals SET modify_flag = 1 WHERE modify_flag IS NULL;
-ALTER TABLE eh_general_approvals MODIFY modify_flag TINYINT NOT NULL DEFAULT 1;
-
-UPDATE eh_general_approvals SET delete_flag = 1 WHERE delete_flag IS NULL;
-ALTER TABLE eh_general_approvals MODIFY delete_flag TINYINT NOT NULL DEFAULT 1;
+ALTER TABLE eh_general_approvals MODIFY delete_flag TINYINT NOT NULL DEFAULT 1 COMMENT '0: no, 1: yes';
 
 UPDATE eh_general_forms SET form_attribute = 'CUSTOMIZE' WHERE form_attribute IS NULL;
-ALTER TABLE eh_general_forms MODIFY form_attribute VARCHAR(128) NOT NULL DEFAULT 'CUSTOMIZE';
+ALTER TABLE eh_general_forms MODIFY form_attribute VARCHAR(128) NOT NULL DEFAULT 'CUSTOMIZE' COMMENT 'CUSTOMIZE, DEFAULT';
 
 UPDATE eh_general_forms SET modify_flag = 1 WHERE modify_flag IS NULL;
-ALTER TABLE eh_general_forms MODIFY modify_flag TINYINT NOT NULL DEFAULT 1;
+ALTER TABLE eh_general_forms MODIFY modify_flag TINYINT NOT NULL DEFAULT 1 COMMENT '0: no, 1: yes';
 
 UPDATE eh_general_forms SET delete_flag = 1 WHERE delete_flag IS NULL;
-ALTER TABLE eh_general_forms MODIFY delete_flag TINYINT NOT NULL DEFAULT 1;
+ALTER TABLE eh_general_forms MODIFY delete_flag TINYINT NOT NULL DEFAULT 1 COMMENT '0: no, 1: yes';
 
 -- end by nan.rong
