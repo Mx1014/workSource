9acf338be3a061274c39fb09dcda66251a89f85e
yanjun <jun.yan@zuolin.com>
2018-02-27 13:44:53 +0800

    opv2 init data

--- db/opv2-2.0-delta-data-beta.sql
+++ db/opv2-2.0-delta-data-beta.sql
@@ -20,25 +20,31 @@ delete from `eh_portal_version_users`;
 
 /*
 
--- 增加大版本
+-- 增加大版本，版本id取域空间id
 INSERT INTO `eh_portal_versions` (`id`, `namespace_id`, `parent_id`, `date_version`, `big_version`, `minor_version`, `create_time`, `sync_time`, `publish_time`, `status`)
 SELECT id, id, NULL, '20180227', '1', '0', NOW(), NOW(), NULL, '2' from eh_namespaces;
 
--- 增加小版本
+-- 增加小版本，版本id取域空间id+100
 INSERT INTO `eh_portal_versions` (`id`, `namespace_id`, `parent_id`, `date_version`, `big_version`, `minor_version`, `create_time`, `sync_time`, `publish_time`, `status`)
 SELECT id + 100, id, id, '20180227', '1', '1', NOW(), NOW(), NULL, NULL from eh_namespaces;
 
--- 同步大版本应用
+-- 同步大版本应用，版本id取域空间id
 SET @appid = 0;
 INSERT INTO `eh_service_module_apps` (`id`, `namespace_id`, `version_id`, `origin_id`, `name`, `module_id`, `instance_config`, `status`, `action_type`, `create_time`, `update_time`, `operator_uid`, `creator_uid`, `module_control_type`, `custom_tag`, `custom_path`)
 SELECT @appid := @appid + 1, namespace_id, namespace_id, `active_app_id`,`name`, `module_id`, `instance_config`, `status`, `action_type`, NOW(), NOW(), 1, 1, `module_control_type`, `custom_tag`, `custom_path` from eh_reflection_service_module_apps;
 
--- 同步小版本应用
+-- 同步小版本应用，版本id取域空间id+100
 INSERT INTO `eh_service_module_apps` (`id`, `namespace_id`, `version_id`, `origin_id`, `name`, `module_id`, `instance_config`, `status`, `action_type`, `create_time`, `update_time`, `operator_uid`, `creator_uid`, `module_control_type`, `custom_tag`, `custom_path`)
 SELECT @appid := @appid + 1, namespace_id, namespace_id + 100,  `active_app_id`,`name`, `module_id`, `instance_config`, `status`, `action_type`, NOW(), NOW(), 1, 1, `module_control_type`, `custom_tag`, `custom_path` from eh_reflection_service_module_apps;
 
 */
 
--- 5、在运营后台添加活动或论坛的主页签应用后需要刷新应用id（选做）
+-- 5、同步数据库id
+-- /evh/admin/syncSequence
+
+-- 6、同步运营后台数据
+-- /evh/portal/syncLaunchPadData
+
+-- 7、在运营后台添加活动或论坛的主页签应用后需要刷新应用id（选做）
 -- UPDATE eh_service_module_apps set custom_tag = 1, instance_config = '{"categoryId":1,"publishPrivilege":1,"livePrivilege":0,"listStyle":2,"scope":3,"style":4,"title": "活动管理"}' where module_id = 10600 and (instance_config is NULL or instance_config not LIKE '%categoryId%');
 -- UPDATE eh_service_module_apps set custom_tag = 0, instance_config = '{"forumEntryId":0}' where module_id = 10100 and (instance_config is NULL or instance_config not LIKE '%forumEntryId%');
