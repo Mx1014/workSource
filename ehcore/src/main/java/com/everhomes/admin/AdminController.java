0c2bbbdbbd07e1dab7e63ffccbff2825e6e29038
lei.lv <lei.lv@zuolin.com>
2018-03-01 14:50:35 +0800

    fix

--- ehcore/src/main/java/com/everhomes/admin/AdminController.java
+++ ehcore/src/main/java/com/everhomes/admin/AdminController.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import com.everhomes.rest.version.VersionRealmType;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
