68edfa8e4be0f042fe41762464d2ff56149d590a
dengs <shuang.deng@zuolin.com>
2018-03-06 17:23:23 +0800

    fix

--- db/5.3.0-delta-data-beta.sql
+++ db/5.3.0-delta-data-beta.sql
@@ -48,3 +48,9 @@ SELECT @appid := @appid + 1, namespace_id, namespace_id + 100,  `active_app_id`,
 -- 7、在运营后台添加活动或论坛的主页签应用后需要刷新应用id（选做）
 -- UPDATE eh_service_module_apps set custom_tag = 1, instance_config = '{"categoryId":1,"publishPrivilege":1,"livePrivilege":0,"listStyle":2,"scope":3,"style":4,"title": "活动管理"}' where module_id = 10600 and (instance_config is NULL or instance_config not LIKE '%categoryId%');
 -- UPDATE eh_service_module_apps set custom_tag = 0, instance_config = '{"forumEntryId":0}' where module_id = 10100 and (instance_config is NULL or instance_config not LIKE '%forumEntryId%');
+
+
+-- 8、同步服务联盟和工位预定数据，以下接口请勿重复调用！！
+-- /yellowPage/syncOldForm
+-- /yellowPage/syncSARequestInfo
+-- /officecubicle/dataMigration
