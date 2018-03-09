19d587f66ee4642ecd6f196748314782f5446d8c
dengs <shuang.deng@zuolin.com>
2018-03-09 09:31:12 +0800

    fix

--- ehcore/src/main/java/com/everhomes/yellowPage/ServiceAllianceRequestInfoSearcherImpl.java
+++ ehcore/src/main/java/com/everhomes/yellowPage/ServiceAllianceRequestInfoSearcherImpl.java
@@ -247,7 +247,7 @@ public class ServiceAllianceRequestInfoSearcherImpl extends AbstractElasticSearc
             if(lists!=null && lists.size()>0) {
                 request.setCreateTime(lists.get(0).getCreateTime());
             }
-            request.setCreatorName(user.getNickName());
+            request.setCreatorName(user!=null?user.getNickName():"");
             if(organizationVal!= null && organizationVal.getFieldValue() != null){
                 PostApprovalFormTextValue organizationvalue = JSON.parseObject(organizationVal.getFieldValue(), PostApprovalFormTextValue.class);
                 if(organizationvalue != null) {
