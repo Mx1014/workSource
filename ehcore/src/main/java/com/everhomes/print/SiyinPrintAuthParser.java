88f9177edb75ba155dc1e59fc563111910612ec6
dengs <shuang.deng@zuolin.com>
2018-03-12 15:08:27 +0800

    13,14 publish module id add

--- ehcore/src/main/java/com/everhomes/print/SiyinPrintAuthParser.java
+++ ehcore/src/main/java/com/everhomes/print/SiyinPrintAuthParser.java
@@ -18,10 +18,51 @@ public class SiyinPrintAuthParser implements PortalUrlParser {
             Map<String,String> data= new Gson().fromJson(actionData,new TypeToken<HashMap<String,String>>(){}.getType());
             if(data!=null && data.size() > 0 && data.get("url")!=null){
                 String wd = data.get("url");
-                if(wd.contains("/cloud-print/build/index.htm"))
-                    return 41400L;
+                if(wd.contains("/cloud-print/build/index.htm")){
+                    return 41400L;//云打印
+                }
+                if(wd.contains("/park-introduction/index.html")){
+                	return 10200L;//园区介绍
+                }
+                if(wd.contains("/park-news-web/build/index.html"))
+                {
+                	return 10800L;//园区快讯
+                }
+                
+                if(wd.contains("/questionnaire-survey/build/index.htm"))
+                {
+                	return 41700L;//问卷调查
+                }
+                if(wd.contains("/station-booking/index.html"))
+                {
+                	return 40200L;//工位预定
+                }
+                
+                if(wd.contains("/goods-move/build/index.html"))
+                {
+                	return 49200L;//物品搬迁
+                }
+                
+                if(wd.contains("/deliver/dist/index.html"))
+                {
+                	return 40700L;//快递
+                }
+                
+                if(wd.contains("/metro_card/index.html"))
+                {
+                	return 41200L;//一卡通？
+                }
+                
+                if(wd.contains("/property-repair-web/build/index.html"))
+                {
+                	return 20100L;//物业报修
+                }
             }
         }
         return null;
     }
+    
+    public static void main(String[] args) {
+		System.out.println(new SiyinPrintAuthParser().getModuleId(null, "{\"url\":\"http://opv2-test.zuolin.com/cloud-print/build/index.html#/home#sign_suffix\"}", (byte)13, null));
+	}
 }
