8f11a37d8269253136eeafaf98752fe5241593a4
dengs <shuang.deng@zuolin.com>
2018-03-19 11:39:50 +0800

    #filemanager privilege remove

--- ehnextgen-api/src/main/java/com/everhomes/objectstorage/OsObjectProvider.java
+++ ehnextgen-api/src/main/java/com/everhomes/objectstorage/OsObjectProvider.java
@@ -2,8 +2,6 @@ package com.everhomes.objectstorage;
 
 import com.everhomes.listing.ListingLocator;
 import com.everhomes.listing.ListingQueryBuilderCallback;
-import com.everhomes.rest.acl.ProjectDTO;
-import com.everhomes.rest.address.CommunityDTO;
 import com.everhomes.rest.objectstorage.OsObjectQuery;
 
 import java.util.List;
@@ -19,7 +17,7 @@ interface OsObjectProvider {
 
     void createOsObject(OsObject obj);
 
-    List<OsObject> listOsObject(OsObjectQuery query, ListingLocator locator, ListingQueryBuilderCallback callback,List<ProjectDTO> privilegeCommunities);
+    List<OsObject> listOsObject(OsObjectQuery query, ListingLocator locator, ListingQueryBuilderCallback callback);
 
     OsObject findById(Long id);
 
