df0526ff87d8ff4a3069514c308f9c45b86c0d73
wentian <wentian.wang@zuolin.com>
2018-03-21 17:24:46 +0800

    requestId bug fixed:

--- ehnextgen-api/src/main/java/com/everhomes/warehouse/WarehouseRequestMaterials.java
+++ ehnextgen-api/src/main/java/com/everhomes/warehouse/WarehouseRequestMaterials.java
@@ -9,6 +9,18 @@ import com.everhomes.util.StringHelper;
 public class WarehouseRequestMaterials extends EhWarehouseRequestMaterials {
     private static final long serialVersionUID = 3757792319392563177L;
 
+    private Long requestId;
+
+    @Override
+    public Long getRequestId() {
+        return requestId;
+    }
+
+    @Override
+    public void setRequestId(Long requestId) {
+        this.requestId = requestId;
+    }
+
     @Override
     public String toString() {
         return StringHelper.toJsonString(this);
