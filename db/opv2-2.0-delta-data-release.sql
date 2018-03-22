717384095fe83ca88c01650976a6d6cd3ac0acb3
jiarui <rui.jia@zuolin.com>
2018-03-22 15:52:06 +0800

    fix

--- db/opv2-2.0-delta-data-release.sql
+++ db/opv2-2.0-delta-data-release.sql
@@ -66,14 +66,14 @@ SET  @id = (SELECT  MAX(id) FROM eh_version_realm);
 INSERT INTO `eh_version_realm` VALUES (@id:=@id+1, 'qualityInspection', NULL, NOW(), '0');
 
 SET  @vId = (SELECT  MAX(id) FROM eh_version_urls);
-INSERT INTO `eh_version_urls` VALUES (@vId:=@vId+1, @id, '1.0.0', 'http://{home.url}/nar/qualityInspect/offline/qualityInspection-1-0-0.zip', 'http://{home.url}/nar/qualityInspect/offline/qualityInspection-1-0-0.zip', '品质核查离线', '0', '品质核查', NOW(), NULL, '0');
+INSERT INTO `eh_version_urls` VALUES (@vId:=@vId+1, @id, '1.0.0', 'http://{home.url}/nar/qualityInspection/offline/qualityInspection-1-0-0.zip', 'http://{home.url}/nar/qualityInspection/offline/qualityInspection-1-0-0.zip', '品质核查离线', '0', '品质核查', NOW(), NULL, '0');
 
 UPDATE eh_launch_pad_items
-SET action_data = '{\"realm\":\"qualityInspection\",\"entryUrl\":\"http://{home.url}/nar/qualityInspect/build/index.html?hideNavigationBar=1#/home#sign_suffix\"}'
+SET action_data = '{\"realm\":\"qualityInspection\",\"entryUrl\":\"http://{home.url}/nar/qualityInspection/build/index.html?hideNavigationBar=1#/home#sign_suffix\"}'
 WHERE item_label LIKE '%品质%';
 
 update eh_service_module_apps
-set instance_config = '{\"realm\":\"qualityInspection\",\"entryUrl\":\"http://{home.url}/nar/qualityInspect/build/index.html?hideNavigationBar=1#/home#sign_suffix\"}'
+set instance_config = '{\"realm\":\"qualityInspection\",\"entryUrl\":\"http://{home.url}/nar/qualityInspection/build/index.html?hideNavigationBar=1#/home#sign_suffix\"}'
 where module_id = 20600;
 
 UPDATE eh_launch_pad_items
