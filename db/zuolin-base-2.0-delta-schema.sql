9cfd27cfba4ce38f100e2fcd139509f856829119
lei.lv <lei.lv@zuolin.com>
2018-03-23 17:38:48 +0800

    sql schema

---
+++ db/zuolin-base-2.0-delta-schema.sql
@@ -0,0 +1,10 @@
+ -- 应用公司项目授权表 by lei.lv
+ CREATE TABLE `eh_service_module_app_authorizations` (
+  `id` BIGINT(20) NOT NULL,
+  `namespace_id` INT(11) DEFAULT NULL,
+  `organization_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT 'organization_id',
+  `project_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT 'community_id',
+  `app_id` BIGINT(20)  NOT NULL DEFAULT '0' COMMENT 'app_id',
+  `control_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'control type',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
