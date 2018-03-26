d06e670348eeae756b9a1590c65bb9c6b1948bbe
yanjun <jun.yan@zuolin.com>
2018-03-26 11:13:39 +0800

    add zuolin schame

--- db/zuolin-base-2.0-delta-schema.sql
+++ db/zuolin-base-2.0-delta-schema.sql
@@ -8,3 +8,36 @@
   `control_type` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'control type',
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `eh_organization_apps` (
+  `id` bigint(20) NOT NULL,
+  `app_id` bigint(20) DEFAULT NULL,
+  `org_id` bigint(20) DEFAULT NULL,
+  `app_type` varchar(255) DEFAULT NULL,
+  `alias_name` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `creator_uid` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `operator_uid` bigint(20) DEFAULT NULL,
+  `operator_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `eh_organization_apps` ADD INDEX `org_app_orgid` (`org_id`) ;
+ALTER TABLE `eh_organization_apps` ADD INDEX `org_app_appid` (`app_id`) ;
+
+
+CREATE TABLE `eh_app_community_config` (
+  `id` bigint(20) NOT NULL,
+  `organization_app_id` bigint(20) DEFAULT NULL,
+  `community_id` bigint(20) DEFAULT NULL,
+  `visibilityFlag` tinyint(4) DEFAULT NULL,
+  `display_name` varchar(255) DEFAULT NULL,
+  `creator_uid` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `operator_uid` bigint(20) DEFAULT NULL,
+  `operator_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `eh_app_community_config` ADD INDEX `organization_app_id` (`organization_app_id`) ;
+ALTER TABLE `eh_app_community_config` ADD INDEX `community_id` (`community_id`) ;
