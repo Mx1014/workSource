9c624042bdbcb7f749c633aa06ace91ef373d2cf
siting <st.zheng@zuolin.com>
2018-03-29 19:51:27 +0800

    pmtask3.4

--- ehcore/src/main/java/com/everhomes/pmtask/ZhuzongPmTaskHandle.java
+++ ehcore/src/main/java/com/everhomes/pmtask/ZhuzongPmTaskHandle.java
@@ -2,19 +2,33 @@ package com.everhomes.pmtask;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.everhomes.constants.ErrorCodes;
+import com.everhomes.util.RuntimeErrorException;
+import org.apache.http.Header;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component(PmTaskHandle.PMTASK_PREFIX + PmTaskHandle.ZHUZONG)
 public class ZhuzongPmTaskHandle extends DefaultPmTaskHandle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZhuzongPmTaskHandle.class);
+
     private static final String GET_ADDRESSES = "/phoneServer/QueryHouseByPhoneNumber";
     private static final  String ACCOUNT_CODE = "sdgj";
 
@@ -28,16 +42,42 @@ public class ZhuzongPmTaskHandle extends DefaultPmTaskHandle {
         return super.getThirdAddress(req);
     }
 
+    public String postToZhuzong(JSONObject params,String method){
+        String url = configProvider.getValue("pmtask.zhuzong.url", "");
+        HttpPost httpPost = new HttpPost(url + method);
+        CloseableHttpResponse response = null;
+
+        String json = null;
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        params.keySet()
+        try {
+
+        }catch (IOException e) {
+            LOGGER.error("Pmtask request error, param={}", params.toJSONString(), e);
+            throw RuntimeErrorException.errorWith(ErrorCodes.SCOPE_GENERAL, ErrorCodes.ERROR_GENERAL_EXCEPTION,
+                    "Pmtask request error.");
+        }finally {
+            if (null != response) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    LOGGER.error("Pmtask close instream, response error, param={}", params.toJSONString(), e);
+                }
+            }
+        }
+    }
+
     public static void main(String [] args){
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://139.129.204.232:8007" + "LsInterfaceServer/phoneServer/QueryHouseByPhoneNumber");
+        HttpPost httpPost = new HttpPost("http://139.129.204.232:8007" + "/LsInterfaceServer/phoneServer/QueryHouseByPhoneNumber");
         CloseableHttpResponse response = null;
-        JSONObject object = new JSONObject();
-        object.put("AccountCode","sdgj");
-        object.put("phone","15650723221");
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("AccountCode", "sdgj"));
+        nameValuePairs.add(new BasicNameValuePair("phone", "15650723221"));
         try {
-            StringEntity stringEntity = new StringEntity(object.toJSONString(), "utf8");
-            httpPost.setEntity(stringEntity);
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+           // httpPost.setHeader("Content-Type", "application/json");
+           // httpPost.setEntity(stringEntity);
             response = httpclient.execute(httpPost);
             System.out.print(EntityUtils.toString(response.getEntity(), "utf8"));
         }catch (Exception e){
