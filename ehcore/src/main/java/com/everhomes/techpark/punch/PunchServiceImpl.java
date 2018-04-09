ce54040c4daccc5e788ef149fcde5183121c44cc
wuhan <wuhan419@gmail.com>
2018-04-09 10:04:10 +0800

    fix bug ;不知道为什么有一个地方北改回去了

--- ehcore/src/main/java/com/everhomes/techpark/punch/PunchServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/techpark/punch/PunchServiceImpl.java
@@ -7950,7 +7950,7 @@ public class PunchServiceImpl implements PunchService {
 		if (null != ptr && pdl == null) {
 //			LOGGER.debug("ptr is {},pdl is {}", StringHelper.toJsonString(ptr), StringHelper.toJsonString(pdl));
 			pdl = new PunchDayLog();
-			refreshPunchDayLog(userId, cmd.getEnterpriseId(), pdl, punCalendar, ptr, pdl);
+			refreshPunchDayLog(userId, cmd.getEnterpriseId(), null, punCalendar, ptr, pdl);
 			LOGGER.debug("pdl is {}",StringHelper.toJsonString(pdl));
 		}
 		String[] statusList =null;
