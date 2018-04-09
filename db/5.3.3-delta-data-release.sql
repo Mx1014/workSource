96868ee3ca08a2136ce7014338169608befded56
lei.lv <lei.lv@zuolin.com>
2018-04-09 10:39:18 +0800

    fix https

--- db/5.3.3-delta-data-release.sql
+++ db/5.3.3-delta-data-release.sql
@@ -3,6 +3,6 @@ SET @eh_locale_strings_id = IFNULL((SELECT MAX(id) FROM `eh_locale_strings`), 0)
 INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`)
     VALUES ((@eh_locale_strings_id := @eh_locale_strings_id + 1), 'banner', '10004', 'zh_CN', '跳转数据处理失败');
 
--- 增加扫码登录的域空间配置项、
+-- 增加扫码登录的域空间配置项
 set @c_id = (select max(id) from eh_configurations);
-INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES (@c_id:= @c_id +1, 'scanForLogonServer', 'https://web.zuolin.com', NULL, '999971', NULL);
+INSERT INTO `eh_configurations` (`id`, `name`, `value`, `description`, `namespace_id`, `display_name`) VALUES (@c_id:= @c_id +1, 'scanForLogonServer', 'http://web.zuolin.com', NULL, '999971', NULL);
