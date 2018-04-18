94ac269e2bd688286ec0f9a76aecce239aa7e3e0
yuanlei <lei.yuan@zuolin.com>
2018-04-18 10:01:04 +0800

    WebMenuServiceImpl.java

--- ehcore/src/main/java/com/everhomes/menu/WebMenuServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/menu/WebMenuServiceImpl.java
@@ -34,7 +34,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
