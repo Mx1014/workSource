6cff5ef56ec165a7208ac44d9d4be4182f96085b
siting <st.zheng@zuolin.com>
2018-04-18 15:02:11 +0800

    园区入驻3.8

--- db/5.4.0-delta-schema.sql
+++ db/5.4.0-delta-schema.sql
@@ -131,3 +131,13 @@ CREATE TABLE `eh_office_cubicle_selected_cities` (
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- end
+
+-- by zheng 招租增加园区id 和 客服id
+ALTER TABLE `eh_lease_promotions`
+ADD COLUMN `community_id` BIGINT(20) NOT NULL DEFAULT '0' AFTER `namespace_id`;
+ALTER TABLE `eh_lease_promotions`
+ADD COLUMN `contact_uid` BIGINT(20) NULL DEFAULT NULL AFTER `contact_phone`;
+ALTER TABLE `eh_lease_projects`
+ADD COLUMN `contact_uid` BIGINT(20) NULL DEFAULT NULL AFTER `contact_phone`;
+ALTER TABLE `eh_lease_buildings`
+ADD COLUMN `manager_uid` BIGINT(20) NULL DEFAULT NULL AFTER `manager_contact`;
