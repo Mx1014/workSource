0139748f29445f7e32a6d19c9c3b0ff3f766ea7f
yangcx <cx.yang@zuolin.com>
2018-04-23 09:54:43 +0800

    新增schema.sql

---
+++ ehrest/src/main/java/com/everhomes/rest/asset/zhuzong/QueryHouseByPhoneNumberDTO.java
@@ -0,0 +1,17 @@
+package com.everhomes.rest.asset.zhuzong;
+
+import java.util.List;
+
+public class QueryHouseByPhoneNumberDTO {
+	
+	private List<HouseDTO> Result;
+
+	public List<HouseDTO> getResult() {
+		return Result;
+	}
+
+	public void setResult(List<HouseDTO> result) {
+		Result = result;
+	}
+
+}
