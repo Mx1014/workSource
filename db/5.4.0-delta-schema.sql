b71d1bd70cda105623ad1620318b1217e1d2ccb6
wentian <wentian.wang@zuolin.com>
2018-04-23 17:03:37 +0800

    added change_duration_days

--- db/5.4.0-delta-schema.sql
+++ db/5.4.0-delta-schema.sql
@@ -367,3 +367,7 @@ ALTER TABLE eh_sync_data_tasks ADD COLUMN `view_flag` TINYINT NOT NULL DEFAULT 0
 ALTER TABLE eh_customer_talents ADD COLUMN `member_id` BIGINT NOT NULL DEFAULT 0 COMMENT '通讯录表中的id';
 
 -- end by: wentian
+
+
+-- 免租日期
+ALTER TABLE `eh_contract_charging_changes` ADD COLUMN `change_duration_days` INTEGER DEFAULT NULL COMMENT '变化的天数，例如免租了xx天';
