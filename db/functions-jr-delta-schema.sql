a59b243acbe7a29485d57d5c816f608a713e243b
jiarui <rui.jia@zuolin.com>
2018-04-23 19:33:19 +0800

    fix

--- db/functions-jr-delta-schema.sql
+++ db/functions-jr-delta-schema.sql
@@ -5,6 +5,10 @@ ALTER TABLE `eh_energy_meter_addresses`
 ALTER TABLE `eh_energy_meters`
   ADD COLUMN `auto_flag`  tinyint(4) NOT NULL DEFAULT 0 AFTER `status`;
 
--- 增加比例系数
+-- 增加比例系数 by jiarui
 set @id = ifnull((SELECT MAX(`id`) FROM eh_payment_variables), 0);
 INSERT INTO `eh_payment_variables` (`id`, `charging_standard_id`, `charging_items_id`, `name`, `creator_uid`, `create_time`, `operator_uid`, `update_time`, `identifier`) VALUES (@id:=@id+1, NULL, NULL , '比例系数', '0', now(), NULL, now(), 'blxs');
+
+-- 客户事件设备类型  by jiarui
+ALTER TABLE `eh_customer_events`
+  ADD COLUMN `device_type`  tinyint(4) NOT NULL DEFAULT 0 AFTER `content`;
