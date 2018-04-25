ed6992b92b9eb3c3ac80a7cfdab5c4abc21a0733
jiarui <rui.jia@zuolin.com>
2018-04-25 15:26:46 +0800

    modify trackingUid from -1 to null

--- db/functions-jr-delta-schema.sql
+++ db/functions-jr-delta-schema.sql
@@ -8,3 +8,9 @@ ALTER TABLE `eh_energy_meters`
 -- 客户事件设备类型  by jiarui
 ALTER TABLE `eh_customer_events`
   ADD COLUMN `device_type`  tinyint(4) NOT NULL DEFAULT 0 AFTER `content`;
+
+-- 客户的跟进人  by jiarui
+ALTER TABLE `eh_enterprise_customers`
+  MODIFY COLUMN `tracking_uid`  bigint(20) NULL DEFAULT NULL COMMENT 'tracking uid' AFTER `update_time`;
+
+UPDATE eh_enterprise_customers SET tracking_uid = NULL WHERE tracking_uid = '-1';
