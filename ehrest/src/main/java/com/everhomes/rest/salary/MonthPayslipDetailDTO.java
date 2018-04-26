4933df3f6f2fdfe3303629a05ef67fa8ccaa6f61
wuhan <han.wu@zuolin.com>
2018-04-26 18:12:45 +0800

    fix bug

--- ehrest/src/main/java/com/everhomes/rest/salary/MonthPayslipDetailDTO.java
+++ ehrest/src/main/java/com/everhomes/rest/salary/MonthPayslipDetailDTO.java
@@ -5,32 +5,32 @@ import com.everhomes.util.StringHelper;
 /**
  * 
  * <ul> 
- * <li>operateTime: 发放时间</li>
+ * <li>updateTime: 发放时间</li>
  * <li>payslipDetailId: 详情id</li> 
  * </ul>
  */
 public class MonthPayslipDetailDTO {
-	private Long operateTime;
+	private Long updateTime;
 	private Long payslipDetailId;
 
 	@Override
 	public String toString() {
 		return StringHelper.toJsonString(this);
 	}
-
-	public Long getOperateTime() {
-		return operateTime;
-	}
-
-	public void setOperateTime(Long operateTime) {
-		this.operateTime = operateTime;
-	}
-
+ 
 	public Long getPayslipDetailId() {
 		return payslipDetailId;
 	}
 
 	public void setPayslipDetailId(Long payslipDetailId) {
 		this.payslipDetailId = payslipDetailId;
+	}
+
+	public Long getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Long updateTime) {
+		this.updateTime = updateTime;
 	}
 }
