7088d021419f355ab04233f58a22c14beb2a7e31
jiarui <rui.jia@zuolin.com>
2018-04-28 12:39:21 +0800

    check delta sql

--- db/functions-jr-delta-data-release.sql
+++ db/functions-jr-delta-data-release.sql
@@ -42,4 +42,26 @@ INSERT INTO `eh_payment_variables` (`id`, `charging_standard_id`, `charging_item
 -- 没有生成账单以及账单任务记录时的提示语更改 by wentian 2018/4/28
 UPDATE `eh_locale_strings` SET `text` = '无数据' WHERE `scope` = 'assetv2' AND `code` = '10003';
 
+UPDATE eh_enterprise_customers SET tracking_uid = NULL WHERE tracking_uid = '-1';
+
+-- 客户增加的权限  by jiarui
+UPDATE `eh_service_module_privileges` SET  default_order = 1, `remark`='新增客户' WHERE `module_id`='21110' and  `privilege_id`='21101';
+UPDATE `eh_service_module_privileges` SET  default_order = 2,`remark`='修改客户' WHERE `module_id`='21110' and  `privilege_id`='21102';
+UPDATE `eh_service_module_privileges` SET  default_order = 4,`remark`='导入客户' WHERE `module_id`='21110' and  `privilege_id`='21103';
+UPDATE `eh_service_module_privileges` SET  default_order = 6,`remark`='同步客户' WHERE `module_id`='21110' and  `privilege_id`='21104';
+UPDATE `eh_service_module_privileges` SET  default_order = 3,`remark`='删除客户' WHERE `module_id`='21110' and  `privilege_id`='21105';
+UPDATE `eh_service_module_privileges` SET  default_order = 0 , `remark`='查看客户' WHERE `module_id`='21110' and  `privilege_id`='21106';
+UPDATE `eh_service_module_privileges` SET  default_order = 7,`remark`='查看管理信息'WHERE `module_id`='21110' and  `privilege_id`='21107';
+UPDATE `eh_service_module_privileges` SET  default_order = 8,`remark`='新增管理信息'WHERE `module_id`='21110' and  `privilege_id`='21108';
+UPDATE `eh_service_module_privileges` SET  default_order = 9,`remark`='修改管理信息'WHERE `module_id`='21110' and  `privilege_id`='21109';
+UPDATE `eh_service_module_privileges` SET  default_order = 10,`remark`='删除管理信息'WHERE `module_id`='21110' and  `privilege_id`='21110';
+UPDATE `eh_service_module_privileges` SET  default_order = 11,`remark`='导入管理信息'WHERE `module_id`='21110' and  `privilege_id`='21111';
+UPDATE `eh_service_module_privileges` SET  default_order = 12,`remark`='导出管理信息'WHERE `module_id`='21110' and  `privilege_id`='21112';
+UPDATE `eh_service_module_privileges` SET  default_order = 0,`remark`='查看' WHERE `module_id`='21120' and  `privilege_id`='21113';
+
+
+INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`) VALUES ('21114', '0', '客户管理导出权限', '客户管理 业务模块权限', NULL);
+
+set @id =(select MAX(id) from eh_service_module_privileges);
+INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) VALUES ((@id:=@id+1) ,'21110', '0', '21114', '导出客户', '5', NOW());
 
