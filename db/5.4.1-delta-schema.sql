8d1802afae4e0860a4128d9d1fca7f63dc2ca86f
jiarui <rui.jia@zuolin.com>
2018-04-28 15:53:33 +0800

    merge energy customer functions

--- db/5.4.1-delta-schema.sql
+++ db/5.4.1-delta-schema.sql
@@ -4,3 +4,19 @@ ALTER TABLE `eh_parking_lots` ADD COLUMN `order_code` BIGINT NOT NULL DEFAULT 0 
 
 -- 增加发票 by wentian
 ALTER TABLE `eh_payment_bills` ADD COLUMN `invoice_number` VARCHAR(128) COMMENT '发票编号';
+
+-- 能耗增加分摊比例  by jiarui 20180416
+ALTER TABLE `eh_energy_meter_addresses`
+  ADD COLUMN `burden_rate`  decimal(10,2) NULL DEFAULT NULL AFTER `status`;
+-- 能耗增表计是否远程自动抄表 by jiarui 20180416
+ALTER TABLE `eh_energy_meters`
+  ADD COLUMN `auto_flag`  tinyint(4) NOT NULL DEFAULT 0 AFTER `status`;
+
+-- 客户事件设备类型  by jiarui
+ALTER TABLE `eh_customer_events`
+  ADD COLUMN `device_type`  tinyint(4) NOT NULL DEFAULT 0 AFTER `content`;
+
+-- 客户的跟进人  by jiarui
+ALTER TABLE `eh_enterprise_customers`
+  MODIFY COLUMN `tracking_uid`  bigint(20) NULL DEFAULT NULL COMMENT 'tracking uid' AFTER `update_time`;
+
