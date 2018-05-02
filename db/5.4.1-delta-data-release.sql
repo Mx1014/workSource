41d58d98831962d76d4823748a398bc6afb7a6ab
siting <st.zheng@zuolin.com>
2018-05-02 14:41:08 +0800

    资源预约3.4

--- db/5.4.1-delta-data-release.sql
+++ db/5.4.1-delta-data-release.sql
@@ -151,4 +151,11 @@ INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`,
 set @id =(select MAX(id) from eh_locale_templates);
 INSERT INTO `eh_locale_templates` (`id`, `scope`, `code`, `locale`, `description`, `text`, `namespace_id`) VALUES ((@id:=@id+1), 'rental.notification', '23', 'zh_CN', '即将超时', '尊敬的用户，您预约的${useDetail}剩余使用时长：15分钟，如需延时，请前往APP进行操作，否则超时系统将继续计时计费，感谢您的使用。', '0');
 
+-- 资源预定增加统计权限 by st.zheng
+INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`, `instance_config`, `action_type`, `update_time`, `operator_uid`, `creator_uid`, `description`, `multiple_flag`, `module_control_type`) VALUES ('40430', '统计信息', '40400', '/40000/40400/40430', '1', '3', '2', '0', now(), NULL, NULL, now(), '0', '1', '1', NULL, '');
 
+set @privilege_id = (select max(id) from eh_service_module_privileges);
+INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`) VALUES (4040040430, '0', '资源预约 统计管理权限', '资源预约 统计管理权限', NULL);
+INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) VALUES (@privilege_id:=@privilege_id+1, '40430', '0', 4040040430, '统计信息权限', '0', now());
+
+update eh_rentalv2_orders set status = 7 where status = 8;
