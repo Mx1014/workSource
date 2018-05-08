6db646b7d4958401cd08643f18b4ad0ab52db170
dengs <shuang.deng@zuolin.com>
2018-05-08 15:54:01 +0800

    issue21443 api modify 2

--- ehcore/src/main/java/com/everhomes/visitorsys/VisitorSysServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/visitorsys/VisitorSysServiceImpl.java
@@ -128,7 +128,7 @@ public class VisitorSysServiceImpl implements VisitorSysService{
         ListVisitReasonsResponse response = new ListVisitReasonsResponse();
         response.setVisitorReasonList(visitorSysVisitReasons.stream().map(r->{
             BaseVisitorReasonDTO convert = new BaseVisitorReasonDTO();
-            convert.setVisitReason(r.getVisitorReason());
+            convert.setVisitReason(r.getVisitReason());
             convert.setVisitReasonId(r.getId());
             return convert;
         }).collect(Collectors.toList()));
