f2b6706028fa77e87cac35bf24600e59a908c618
ryan <nan.rong@zuolin.com>
2018-05-10 14:25:55 +0800

    sql update

--- db/approval-3.0-delta-schema.sql
+++ db/approval-3.0-delta-schema.sql
@@ -32,32 +32,12 @@ CREATE TABLE `eh_archives_operations` (
 	`id` BIGINT NOT NULL,
 	`namespace_id` INT NOT NULL DEFAULT '0',
 	`organization_id` BIGINT NOT NULL DEFAULT '0',
-  `operation_type` TINYINT NOT NULL COMMENT 'the type of operation',
-  `operation_time` DATE COMMENT 'the time to execute the operation',
   `detail_id` BIGINT NOT NULL COMMENT 'the detail id that belongs to the employee which is the change target',
-
-	`owner_type` VARCHAR(32),
-	`owner_id` BIGINT,
-	`name` VARCHAR(64) NOT NULL COMMENT 'name of the approval group',
-	`status` TINYINT NOT NULL DEFAULT 1 COMMENT '0. inactive, 1.active',
-	`group_attribute` VARCHAR(128) NOT NULL DEFAULT 'CUSTOMIZE' COMMENT 'DEFAULT, CUSTOMIZE',
-	`approval_icon` VARCHAR(1024) COMMENT 'the default icon that belongs to the group',
-	`operator_uid` BIGINT,
-	`operator_time` DATETIME,
+  `operate_type` TINYINT NOT NULL COMMENT 'the type of operation',
+  `operate_date` DATE COMMENT 'the date of executing the operation',
+  `additional_info` TEXT COMMENT 'the addition information for the operation',
+  `status` TINYINT NOT NULL DEFAULT '0' COMMENT 'pending, execution',
+  `create_time` DATETIME DEFAULT NULL COMMENT 'create time',
+  `operator_uid` BIGINT DEFAULT NULL COMMENT 'the id of the operator',
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-DROP TABLE IF EXISTS `eh_archives_configurations`;
-CREATE TABLE `eh_archives_configurations` (
-  `id` bigint(20) NOT NULL COMMENT 'id of the record',
-  `namespace_id` int(11) NOT NULL DEFAULT '0',
-  `organization_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'the id of organization',
-  `operation_type` tinyint(4) NOT NULL COMMENT 'the type of operation',
-  `operation_time` date DEFAULT NULL COMMENT 'the time to execute the operation',
-  `operation_information` text COMMENT 'information about the operation',
-  `remind_time` datetime DEFAULT NULL COMMENT 'time to send email to the corresponding member',
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'pending, execution',
-  `create_time` datetime DEFAULT NULL COMMENT 'create time',
-  `operator_uid` bigint(20) DEFAULT NULL COMMENT 'the id of the operator',
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
