d2355c8b287a3189a95fdd09767e247ae0f009a8
wuhan <han.wu@zuolin.com>
2018-05-11 14:58:51 +0800

    ISSUE28786:merge sql to 5.5.0

--- db/5.5.0-delta-schema.sql
+++ db/5.5.0-delta-schema.sql
@@ -6,32 +6,36 @@ UPDATE `eh_file_management_catalog_scopes` SET source_type = 'MEMBERDETAIL';
 
 -- issue-26471 服务热线V1.5（客服聊天记录保存和导出) by tianxiaoqiang 2018.5.8
 CREATE TABLE `eh_message_records` (
-  `id` bigint(20) NOT NULL COMMENT 'id of the record',
-  `namespace_id` int(11) DEFAULT '0',
-  `dst_channel_token` varchar(32) DEFAULT NULL,
-  `dst_channel_type` varchar(32) DEFAULT NULL,
-  `status` varchar(32) COMMENT 'message status',
-  `app_id` bigint(20) DEFAULT '1' COMMENT 'default to messaging app itself',
-  `message_seq` bigint(20) DEFAULT NULL COMMENT 'message sequence id generated at server side',
-  `sender_uid` bigint(20) DEFAULT NULL,
-  `sender_tag` varchar(32) DEFAULT NULL COMMENT 'sender generated tag',
-  `channels_info` varchar(2048) DEFAULT NULL,
-  `body_type`varchar(32),
-  `body` varchar(2048),
-  `delivery_option` int(2) DEFAULT '0',
-  `create_time` datetime NOT NULL COMMENT 'message creation time',
-  `session_token` varchar(128),
-  `device_id` varchar(2048),
-  `index_id` bigint(20),
+  `id` BIGINT(20) NOT NULL COMMENT 'id of the record',
+  `namespace_id` INT(11) DEFAULT '0',
+  `dst_channel_token` VARCHAR(32) DEFAULT NULL,
+  `dst_channel_type` VARCHAR(32) DEFAULT NULL,
+  `status` VARCHAR(32) COMMENT 'message status',
+  `app_id` BIGINT(20) DEFAULT '1' COMMENT 'default to messaging app itself',
+  `message_seq` BIGINT(20) DEFAULT NULL COMMENT 'message sequence id generated at server side',
+  `sender_uid` BIGINT(20) DEFAULT NULL,
+  `sender_tag` VARCHAR(32) DEFAULT NULL COMMENT 'sender generated tag',
+  `channels_info` VARCHAR(2048) DEFAULT NULL,
+  `body_type`VARCHAR(32),
+  `body` VARCHAR(2048),
+  `delivery_option` INT(2) DEFAULT '0',
+  `create_time` DATETIME NOT NULL COMMENT 'message creation time',
+  `session_token` VARCHAR(128),
+  `device_id` VARCHAR(2048),
+  `index_id` BIGINT(20),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 
 -- issue-26471 服务热线V1.5（客服聊天记录保存和导出) by huangmingbo 2018.5.8
 ALTER TABLE `eh_service_hotlines` ADD COLUMN `status` TINYINT NOT NULL DEFAULT '1' COMMENT '0-deleted 1-active';
 
 -- by zheng
-ALTER TABLE `eh_configurations` modify `value` varchar(1024);
+ALTER TABLE `eh_configurations` MODIFY `value` VARCHAR(1024);
 
 -- add by yanjun 增加字段长度 201805091806
-ALTER TABLE `eh_service_module_apps` MODIFY COLUMN `custom_tag`  varchar(256)  NULL DEFAULT '';
+ALTER TABLE `eh_service_module_apps` MODIFY COLUMN `custom_tag`  VARCHAR(256)  NULL DEFAULT '';
+
+-- added by wh 
+-- 增加应打卡时间给punch_log
+ALTER TABLE `eh_punch_logs` ADD should_punch_time BIGINT COMMENT '应该打卡时间(用以计算早退迟到时长)';
