21d43641bcfd58699281e6968134329e0753c5e1
dingjianmin <jm.ding@zuolin.com>
2018-05-12 18:39:13 +0800

    change sql script

--- db/testByDingjianminThree-delta-schema.sql
+++ db/testByDingjianminThree-delta-schema.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `eh_test_infos`
-ADD COLUMN `address`  varchar(255) NULL AFTER `age`,
-ADD COLUMN `sex`  bit NULL AFTER `address`;
+ALTER TABLE `eh_test_infos` ADD COLUMN `address`  varchar(255);
+ALTER TABLE `eh_test_infos` ADD COLUMN `sex`  bit;
