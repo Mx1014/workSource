8fe1a52a5e2a40993c574bc1dfd376d1ce12c9aa
dingjianmin <jm.ding@zuolin.com>
2018-05-14 10:00:59 +0800

    updateTable22 sql

--- db/testByDingjianminThree-delta-schema.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `eh_test_infos` ADD COLUMN `sex`  char(2) NULL COMMENT '性别';
-ALTER TABLE `eh_test_infos` ADD COLUMN `address`  varchar(255) NULL COMMENT '地址';
-ALTER TABLE `eh_test_infos` ADD COLUMN `birth`  date NULL COMMENT '生日' ;
