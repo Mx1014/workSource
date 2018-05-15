383f0c90524d2c68575992f08cf9208c5202a4fb
steve <cen.tang@zuolin.com>
2018-05-15 11:45:01 +0800

    add column certificate_note into eh_payment_bills

--- db/asset_payment_5.4.0-delta-schema.sql
+++ db/asset_payment_5.4.0-delta-schema.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `eh_payment_bills` ADD COLUMN `payment_type` int DEFAULT null COMMENT '账单的支付方式（0-线下缴费，1-微信支付，2-对公转账，8-支付宝支付）';
 
-ALTER TABLE `eh_payment_bills` ADD COLUMN `certificate_note`  varchar(255) DEFAULT NULL COMMENT '上传凭证图片时附加的留言';
+ALTER TABLE `eh_payment_bills` ADD COLUMN `certificate_note` varchar(255) DEFAULT NULL COMMENT '上传凭证图片时附加的留言';
