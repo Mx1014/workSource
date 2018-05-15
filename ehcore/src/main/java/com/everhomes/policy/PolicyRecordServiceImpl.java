744c40a52475e194df5048427c2e0175472c1474
shiheng.ma <shiheng.ma@zuolin.com>
2018-05-15 15:06:20 +0800

    issue-26483:生成接口对象

--- ehcore/src/main/java/com/everhomes/policy/PolicyRecordServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/policy/PolicyRecordServiceImpl.java
@@ -1,7 +1,20 @@
 package com.everhomes.policy;
 
+import com.everhomes.rest.policy.GetPolicyRecordCommand;
+import com.everhomes.rest.policy.PolicyRecordDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PolicyRecordServiceImpl implements PolicyRecordService {
+    @Override
+    public PolicyRecordDTO createPolicyRecord(PolicyRecord policyRecord) {
+        return null;
+    }
+
+    @Override
+    public List<PolicyRecordDTO> searchPolicyRecords(GetPolicyRecordCommand cmd) {
+        return null;
+    }
 }
