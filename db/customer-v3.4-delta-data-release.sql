2bc16d6666cfeb14b282cdd55aad4617d38d5921
jiarui <rui.jia@zuolin.com>
2018-05-21 10:15:51 +0800

    update energy offline version

---
+++ db/customer-v3.4-delta-data-release.sql
@@ -0,0 +1,3 @@
+update eh_version_urls set download_url = replace(download_url,'1-0-0','1-0-3'),
+  info_url = replace(info_url,'1-0-1','1-0-3'),
+  target_version = '1.0.3' where realm_id = (select id from eh_version_realm where realm = 'energyManagement');
