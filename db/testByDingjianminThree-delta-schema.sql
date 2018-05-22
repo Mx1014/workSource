2a1749d8e10a6b66f47f667d762dd126eaa92bd6
dingjianmin <jm.ding@zuolin.com>
2018-05-22 13:56:00 +0800

    update sql

--- db/testByDingjianminThree-delta-schema.sql
+++ db/testByDingjianminThree-delta-schema.sql
@@ -1,2 +1,4 @@
 -- --合同管理 添加合同的收付款规则用
 ALTER TABLE `eh_contract_params` ADD COLUMN `payorreceive_contract_type` tinyint(2) NULL COMMENT '0 收款合同 1付款合同';
+ALTER TABLE `eh_contract_params` ADD COLUMN `contract_Number_RuleJson` text NULL COMMENT '合同规则';
+ALTER TABLE `eh_contract_params` ADD COLUMN `update_Time` datetime NULL COMMENT '更新时间';
