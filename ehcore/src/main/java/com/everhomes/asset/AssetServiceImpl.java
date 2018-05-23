a442d394d15b7e6460adca4e31c54733d99b17d8
wentian <wentian.wang@zuolin.com>
2018-05-23 20:30:48 +0800

    days bet method bug fixed

--- ehcore/src/main/java/com/everhomes/asset/AssetServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/asset/AssetServiceImpl.java
@@ -1790,8 +1790,8 @@ public class AssetServiceImpl implements AssetService {
     {
         long time1 = c1.getTime();
         long time2 = c2.getTime();
-        long between_days=Math.abs(time2-time1)/(1000*3600*24);
-        return Integer.parseInt(String.valueOf(between_days) + 1);
+        Long between_days=Math.abs(time2-time1)/(1000*3600*24);
+        return between_days.intValue() + 1;
     }
 
     private void NaturalMonthHandler(List<PaymentExpectancyDTO> dtos1, FeeRules rule, List<VariableIdAndValue> variableIdAndValueList, String formula, String chargingItemName, Integer billDay, List<PaymentExpectancyDTO> dtos2, ContractProperty property) {
