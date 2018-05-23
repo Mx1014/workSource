b874622967366f0430a483332c97b7638ae6145d
liangyanlong <yanlong.liang@zuolin.com>
2018-05-23 13:33:37 +0800

    issue-30013:修改表字段名称

--- db/issue-30013-delta-schema.sql
+++ db/issue-30013-delta-schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE `eh_phone_white_list` (
 	`namespace_id` INT NOT NULL DEFAULT 0 COMMENT '域空间',
 	`phone_number` VARCHAR(128) NOT NULL COMMENT '白名单手机号码',
 	`creator_uid` BIGINT COMMENT '记录创建人userID',
-	`creator_time` DATETIME COMMENT '记录创建时间',
+	`create_time` DATETIME COMMENT '记录创建时间',
 	PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '短信白名单';
