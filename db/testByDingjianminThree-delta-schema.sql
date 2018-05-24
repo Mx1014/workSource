bd5a50f481e630ea52f5d6e0518c33a6507d21b1
dingjianmin <jm.ding@zuolin.com>
2018-05-24 17:27:08 +0800

    add comment table

--- db/testByDingjianminThree-delta-schema.sql
+++ db/testByDingjianminThree-delta-schema.sql
@@ -2,3 +2,4 @@
 ALTER TABLE `eh_contract_params` ADD COLUMN `payorreceive_contract_type` tinyint(2) NULL COMMENT '0 收款合同 1付款合同';
 ALTER TABLE `eh_contract_params` ADD COLUMN `contract_Number_RuleJson` text NULL COMMENT '合同规则';
 ALTER TABLE `eh_contract_params` ADD COLUMN `update_Time` datetime NULL COMMENT '更新时间';
+ALTER TABLE `eh_contracts` ADD COLUMN `category_id` bigint(20) DEFAULT '0' COMMENT 'activity category id';
