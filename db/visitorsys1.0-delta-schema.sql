ad05b8f68e54b6bfdb6af25166540f447070913f
dengs <shuang.deng@zuolin.com>
2018-05-24 14:22:52 +0800

    issue-21443 fix 35

--- db/visitorsys1.0-delta-schema.sql
+++ db/visitorsys1.0-delta-schema.sql
@@ -54,6 +54,22 @@ CREATE TABLE `eh_visitor_sys_visitors` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '访客管理访客表 ';
 
+-- 访客管理预约编码表 , add by dengs, 20180425
+-- DROP TABLE IF EXISTS `eh_visitor_sys_owner_code`;
+CREATE TABLE `eh_visitor_sys_owner_code` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
+  `owner_type` VARCHAR(64) NOT NULL COMMENT 'community or organization',
+  `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'ownerType为community时候，为园区id;ownerType为organization时候，为公司id',
+	`random_code` VARCHAR(16) NOT NULL UNIQUE COMMENT '随机码',
+  `creator_uid` BIGINT,
+  `create_time` DATETIME,
+  `operator_uid` BIGINT,
+  `operate_time` DATETIME,
+
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '访客管理预约编码表';
+
 -- 访客管理预约流水码表 , add by dengs, 20180425
 -- DROP TABLE IF EXISTS `eh_visitor_sys_coding`;
 CREATE TABLE `eh_visitor_sys_coding` (
@@ -127,7 +143,6 @@ CREATE TABLE `eh_visitor_sys_configurations` (
   `namespace_id` INTEGER NOT NULL DEFAULT 0 COMMENT 'namespace id',
   `owner_type` VARCHAR(64) NOT NULL COMMENT 'community or organization',
   `owner_id` BIGINT NOT NULL DEFAULT 0 COMMENT 'ownerType为community时候，为园区id;ownerType为organization时候，为公司id',
-  `random_code` VARCHAR(16) NOT NULL UNIQUE COMMENT '随机码,用于生成预约编码',
   `config_version` BIGINT COMMENT '配置版本',
   `guide_info` varchar(2048) COMMENT '指引信息',
   `owner_token` VARCHAR(32) NOT NULL UNIQUE COMMENT '自助登记二维码ownerToken地址',
