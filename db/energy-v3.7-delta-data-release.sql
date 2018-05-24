e3aa032685b7f02a98290cfe35e36e21870f5db9
jiarui <rui.jia@zuolin.com>
2018-05-24 19:22:33 +0800

    alter reading columns to big decimal (10,2)

--- db/energy-v3.7-delta-data-release.sql
+++ db/energy-v3.7-delta-data-release.sql
@@ -10,3 +10,7 @@ DELETE  FROM  eh_energy_plans WHERE  name = 'autoPlans';
 
 ALTER TABLE `eh_energy_meter_reading_logs`
   MODIFY COLUMN `reading`  decimal(10,2) NULL DEFAULT NULL AFTER `meter_id`;
+ALTER TABLE `eh_energy_meter_tasks`
+  MODIFY COLUMN `last_task_reading`  decimal(10,2) NULL DEFAULT NULL AFTER `executive_expire_time`;
+ALTER TABLE `eh_energy_meter_tasks`
+  MODIFY COLUMN `reading`  decimal(10,2) NULL DEFAULT NULL AFTER `last_task_reading`;
