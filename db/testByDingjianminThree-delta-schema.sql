6f98e983c86f8270bb07e465f4f68d140b2f0a4b
dingjianmin <jm.ding@zuolin.com>
2018-05-25 10:24:51 +0800

    update table 合同表以及规则参数表

--- db/testByDingjianminThree-delta-schema.sql
+++ db/testByDingjianminThree-delta-schema.sql
@@ -1,5 +1,6 @@
 -- --合同管理 添加合同的收付款规则用
-ALTER TABLE `eh_contract_params` ADD COLUMN `payorreceive_contract_type` tinyint(2) NULL COMMENT '0 收款合同 1付款合同';
-ALTER TABLE `eh_contract_params` ADD COLUMN `contract_Number_RuleJson` text NULL COMMENT '合同规则';
-ALTER TABLE `eh_contract_params` ADD COLUMN `update_Time` datetime NULL COMMENT '更新时间';
-ALTER TABLE `eh_contracts` ADD COLUMN `category_id` bigint(20) DEFAULT '0' COMMENT 'activity category id';
+ALTER TABLE `eh_contract_params` ADD COLUMN `payorreceive_contract_type` tinyint(2) DEFAULT '0' COMMENT '0 收款合同 1付款合同';
+ALTER TABLE `eh_contract_params` ADD COLUMN `contract_number_rulejson` text NULL COMMENT '合同规则';
+ALTER TABLE `eh_contract_params` ADD COLUMN `update_time` datetime NULL COMMENT '更新时间';
+ALTER TABLE `eh_contract_params` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'contract category id';
+ALTER TABLE `eh_contracts` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'contract category id';
