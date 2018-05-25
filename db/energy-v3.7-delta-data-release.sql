b594d7b1d43e814f93ea413e7e55ec5331d2fd21
jiarui <rui.jia@zuolin.com>
2018-05-25 10:49:26 +0800

    change  energy reading logs column  to bigDecimal(10,2)

--- db/energy-v3.7-delta-data-release.sql
+++ db/energy-v3.7-delta-data-release.sql
@@ -7,10 +7,3 @@ update eh_version_urls set download_url = replace(download_url,'1-0-1','1-0-3'),
 DELETE FROM  eh_energy_meter_tasks WHERE  plan_id in (SELECT  id from eh_energy_plans where name ='autoPlans');
 
 DELETE  FROM  eh_energy_plans WHERE  name = 'autoPlans';
-
-ALTER TABLE `eh_energy_meter_reading_logs`
-  MODIFY COLUMN `reading`  decimal(10,2) NULL DEFAULT NULL AFTER `meter_id`;
-ALTER TABLE `eh_energy_meter_tasks`
-  MODIFY COLUMN `last_task_reading`  decimal(10,2) NULL DEFAULT NULL AFTER `executive_expire_time`;
-ALTER TABLE `eh_energy_meter_tasks`
-  MODIFY COLUMN `reading`  decimal(10,2) NULL DEFAULT NULL AFTER `last_task_reading`;
