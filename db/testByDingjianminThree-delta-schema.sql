eff0f3193fe8f0975891b234a577d3c27958d09c
dingjianmin <jm.ding@zuolin.com>
2018-05-26 16:46:47 +0800

    添加工作流的多入口sql

--- db/testByDingjianminThree-delta-schema.sql
+++ db/testByDingjianminThree-delta-schema.sql
@@ -1,9 +1,13 @@
--- --合同管理 添加合同的收付款规则用
+-- --合同管理 基础设置合同规则
 ALTER TABLE `eh_contract_params` ADD COLUMN `payorreceive_contract_type` tinyint(2) DEFAULT '0' COMMENT '0 收款合同 1付款合同';
 ALTER TABLE `eh_contract_params` ADD COLUMN `contract_number_rulejson` text NULL COMMENT '合同规则';
 ALTER TABLE `eh_contract_params` ADD COLUMN `update_time` datetime NULL COMMENT '更新时间';
 ALTER TABLE `eh_contract_params` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'contract category id';
+-- --合同管理 合同多入口设置
 ALTER TABLE `eh_contracts` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'contract category id';
+-- --合同管理，表单设置，动态字段
 ALTER TABLE `eh_var_field_scopes` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'category id';
 ALTER TABLE `eh_var_field_item_scopes` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'category id';
 ALTER TABLE `eh_var_field_group_scopes` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'category id';
+-- --合同管理，工作流设置，不同入口对应不同工作流
+ALTER TABLE `eh_flows` ADD COLUMN `category_id` bigint(20) NULL COMMENT 'category id';
