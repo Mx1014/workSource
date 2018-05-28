e70b5fbe79b5b4790661470eb87ad66fc3155575
liuyilin <yilin.liu@zuolin.com>
2018-05-28 15:23:13 +0800

    fix command name

---
+++ ehrest/src/main/java/com/everhomes/rest/aclink/ListAdminAesUserKeyCommand.java
@@ -0,0 +1,27 @@
+// @formatter:off
+package com.everhomes.rest.aclink;
+
+import com.everhomes.util.StringHelper;
+
+/**
+ * <ul> 查询转发的消息
+ * <li>rightRemote: 是否有远程开门权限0无1有</li>
+ * </ul>
+ * @author janson
+ *
+ */
+public class ListAdminAesUserKeyCommand {
+	private Byte rightRemote;
+
+	public Byte getRightRemote() {
+		return rightRemote;
+	}
+
+	public void setRightRemote(Byte rightRemote) {
+		this.rightRemote = rightRemote;
+	}
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
+}
