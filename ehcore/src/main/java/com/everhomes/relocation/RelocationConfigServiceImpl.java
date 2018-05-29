7da45aee86d270694f795aaa35614ee8dc206d5d
shiheng.ma <shiheng.ma@zuolin.com>
2018-05-29 17:26:57 +0800

    issue-26467:接口实现

---
+++ ehcore/src/main/java/com/everhomes/relocation/RelocationConfigServiceImpl.java
@@ -0,0 +1,47 @@
+package com.everhomes.relocation;
+
+import com.everhomes.rest.relocation.GetRelocationConfigCommand;
+import com.everhomes.rest.relocation.RelocationConfigDTO;
+import com.everhomes.rest.relocation.SetRelocationConfigCommand;
+import com.everhomes.user.User;
+import com.everhomes.user.UserContext;
+import com.everhomes.util.ConvertHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RelocationConfigServiceImpl implements RelocationConfigService {
+
+    @Autowired
+    RelocationConfigProvider relocationConfigProvider;
+
+    @Override
+    public RelocationConfigDTO setRelocationConfig(SetRelocationConfigCommand cmd) {
+        User user = UserContext.current().getUser();
+        RelocationConfig result = relocationConfigProvider.searchRelocationConfigById(cmd.getNamespaceId(),cmd.getOwnerType(),cmd.getOwnerId(),cmd.getId());
+        if(null != result){
+            if(null != cmd.getAgreementFlag())
+                result.setAgreementFlag(cmd.getAgreementFlag());
+            if(null != cmd.getAgreementContent())
+                result.setAgreementContent(cmd.getAgreementContent());
+            if(null != cmd.getTipsFlag())
+                result.setTipsFlag(cmd.getTipsFlag());
+            if(null != cmd.getTipsContent()){
+                result.setTipsContent(cmd.getTipsContent());
+            }
+            result.setOperatorUid(user.getId());
+            relocationConfigProvider.updateRelocationConfig(result);
+        } else {
+            result = ConvertHelper.convert(cmd, RelocationConfig.class);
+            result.setCreatorUid(user.getId());
+            relocationConfigProvider.createRelocationConfig(result);
+        }
+        return ConvertHelper.convert(result,RelocationConfigDTO.class);
+    }
+
+    @Override
+    public RelocationConfigDTO searchRelocationConfigById(GetRelocationConfigCommand cmd) {
+        RelocationConfig result = this.relocationConfigProvider.searchRelocationConfigById(cmd.getNamespaceId(),cmd.getOwnerType(),cmd.getOwnerId(),cmd.getId());
+        return ConvertHelper.convert(result,RelocationConfigDTO.class);
+    }
+}
