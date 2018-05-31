9b805a450cec1528c9cd6becde3a2e19c8ebd0e3
liuyilin <yilin.liu@zuolin.com>
2018-05-31 09:29:47 +0800

    注释编码修复

--- ehborder/src/main/java/com/everhomes/controller/AclinkWebSocketState.java
+++ ehborder/src/main/java/com/everhomes/controller/AclinkWebSocketState.java
@@ -64,7 +64,7 @@ public class AclinkWebSocketState {
             
             AclinkWebSocketMessage cmd = new AclinkWebSocketMessage();
             cmd.setId(this.getId());
-            //by liuyilin 20180419 TODO 内网服务器心跳包处理
+            //by liuyilin 20180419 TODO 鍐呯綉鏈嶅姟鍣ㄩ厤瀵圭爜浣滀负uuid
             if(this.getUuid().length() == 6){
             	cmd.setType(1);
             }
