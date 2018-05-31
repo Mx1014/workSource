0f835182386d234e5f166fb9808e0abe498cb212
liangyanlong <yanlong.liang@zuolin.com>
2018-05-31 10:54:59 +0800

    issue-30635: 增加分支依赖表.

--- db/issue-30635-delta-schema-02.sql
+++ db/issue-30635-delta-schema-02.sql
@@ -247,7 +247,7 @@ CREATE TABLE `eh_policy_agent_rules` (
 ALTER TABLE `eh_enterprise_customer_admins` ADD COLUMN `namespace_id`  int NOT NULL DEFAULT 0 AFTER `create_time`;
 
 -- 文档管理1.2 start by ryan.
-ALTER TABLE `eh_file_management_catalog_scopes` ADD COLUMN `source_type` VARCHAR(64) COMMENT'the type of the source' AFTER `source_id`;
+-- ALTER TABLE `eh_file_management_catalog_scopes` ADD COLUMN `source_type` VARCHAR(64) COMMENT'the type of the source' AFTER `source_id`;
 UPDATE `eh_file_management_catalog_scopes` SET source_type = 'MEMBERDETAIL';
 -- 文档管理1.2 end by ryan.
 
