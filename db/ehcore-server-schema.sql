8d909748463fecfc5acbd296209e6c0095824a11
steve <cen.tang@zuolin.com>
2018-06-01 09:30:59 +0800

    修改asset_payment_5.4.2模块的sql脚本

--- db/ehcore-server-schema.sql
+++ db/ehcore-server-schema.sql
@@ -17962,12 +17962,5 @@ CREATE TABLE `eh_zj_syncdata_backup` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-DROP TABLE IF EXISTS `eh_payment_bill_certificate`;
 
-CREATE TABLE `eh_payment_bill_certificate` (
-  `id` bigint(20) NOT NULL,
-  `bill_id` bigint(20) NOT NULL COMMENT '该凭证记录对应的账单id',
-  `certificate_uri` varchar(255) DEFAULT NULL COMMENT '上传凭证图片的uri',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
