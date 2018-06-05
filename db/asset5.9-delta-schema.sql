92008ab00f897068d8745122da5f4559e15903a9
yangcx <cx.yang@zuolin.com>
2018-06-05 19:06:05 +0800

    去掉收款方名称字段

--- db/asset5.9-delta-schema.sql
+++ db/asset5.9-delta-schema.sql
@@ -1,7 +1,6 @@
 -- 为账单组管理增加“收款方账户”字段
-ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_account` VARCHAR(1024) COMMENT '收款方账户';
-ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_type` VARCHAR(1024) COMMENT '收款方账户类型：EhUsers/EhOrganizations';
-ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_id` VARCHAR(1024) COMMENT '收款方账户id';
+ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_type` VARCHAR(128) COMMENT '收款方账户类型：EhUsers/EhOrganizations';
+ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_id` BIGINT COMMENT '收款方账户id';
 
 
 -- 陈毅峰
