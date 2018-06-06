da7888d39db41a46871cef789c2b02b364a995f0
dengs <shuang.deng@zuolin.com>
2018-06-06 18:58:09 +0800

    issue30413 modify 3

--- db/pay3.0_parking_print-delta-schema.sql
+++ db/pay3.0_parking_print-delta-schema.sql
@@ -495,3 +495,7 @@ CREATE TABLE `eh_payment_bill_certificate` (
   `certificate_uri` varchar(255) DEFAULT NULL COMMENT '上传凭证图片的uri',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 为账单组管理增加“收款方账户”字段
+ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_type` VARCHAR(128) COMMENT '收款方账户类型：EhUsers/EhOrganizations';
+ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `biz_payee_id` BIGINT COMMENT '收款方账户id';
