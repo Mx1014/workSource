9e302f74fd088681f8cb2acca3276bd7dcf37cb7
yuanlei <lei.yuan@zuolin.com>
2018-06-07 16:08:19 +0800

    delete 9999

--- ehcore/src/main/java/com/everhomes/acl/RolePrivilegeServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/acl/RolePrivilegeServiceImpl.java
@@ -3062,8 +3062,6 @@ public class RolePrivilegeServiceImpl implements RolePrivilegeService {
 		CrossShardListingLocator locator = new CrossShardListingLocator();
 		if(cmd.getPageAnchor() != null){
 			locator.setAnchor(cmd.getPageAnchor());
-		}else{
-			locator.setAnchor(99999L);
 		}
 		Integer pageSize = 20;//默认20条
 		if(cmd.getPageSize() != null){
