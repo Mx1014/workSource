5bae12d93850440f32a5069beb75415174eba82a
st.zheng <st.zheng@zuolin.com>
2018-06-12 17:23:17 +0800

    资源预约

--- db/asset5.9-delta-data-release.sql
+++ db/asset5.9-delta-data-release.sql
@@ -1,10 +1,9 @@
 -- by st.zheng 数据迁移
-SET @id = ifnull((SELECT MAX(id) FROM `eh_rentalv2_order_record`),0);
-INSERT INTO `eh_rentalv2_order_records` (`id`,`order_no`,`biz_order_num`,pay_order_id`,`payment_order_type`,status`,`create_time`,`update_time`)
-SELECT (@id := @id + 1), order_id,order_num,payment_order_id,payment_order_type,0,create_time,create_time  FROM eh_payment_order_records where order_type = 'rentalOrder';
+SET @id = ifnull((SELECT MAX(id) FROM `eh_rentalv2_order_records`),0);
+INSERT INTO `eh_rentalv2_order_records` (`id`,`order_no`,`biz_order_num`,`pay_order_id`,`payment_order_type`,`status`,`create_time`,`update_time`)
+    SELECT (@id := @id + 1), order_id,order_num,payment_order_id,payment_order_type,0,create_time,create_time  FROM eh_payment_order_records where order_type = 'rentalOrder';
 
 update `eh_rentalv2_order_records` t1 right join `eh_rentalv2_orders` t2 on t1.`order_no` = t2.`order_no` set t1.order_id = t2.id,t1.amount = t2.pay_total_money,t1.status = IF(t2.status in (2,7,9,10,14,20),1,0) ;
-
 
 -- by cx.yang 支付回调
 INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`)
