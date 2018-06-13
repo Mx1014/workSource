7c1cddd023d9b5b5cd0d8dd0802affda0ed5cfc2
yanjun <jun.yan@zuolin.com>
2018-06-13 14:16:48 +0800

    add category

--- db/workplatform-1.0-delta-schema.sql
+++ db/workplatform-1.0-delta-schema.sql
@@ -2,6 +2,8 @@
 -- 模块对应的菜单是否需要授权, add by yanjun 20180517
 ALTER TABLE `eh_service_modules` ADD COLUMN `menu_auth_flag`  tinyint(4) NOT NULL DEFAULT 1 COMMENT 'if its menu need auth' ;
 
+ALTER TABLE `eh_service_modules` ADD COLUMN `category`  varchar(255) NULL COMMENT 'classify, module, subModule';
+
 
 -- 增加索引 add by yanjun 01806121918
 alter table eh_portal_layouts add index version_id_index(`version_id`);
