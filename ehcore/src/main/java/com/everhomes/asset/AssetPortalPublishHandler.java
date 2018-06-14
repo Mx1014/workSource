b91e0d4277f0dcc400334baee2a363161fd69cb4
wentian <wentian.wang@zuolin.com>
2018-06-14 12:11:15 +0800

    asset publish error fixed

--- ehcore/src/main/java/com/everhomes/asset/AssetPortalPublishHandler.java
+++ ehcore/src/main/java/com/everhomes/asset/AssetPortalPublishHandler.java
@@ -69,7 +69,13 @@ public class AssetPortalPublishHandler implements PortalPublishHandler{
             JSONObject ret = new JSONObject();
             JSONObject jsonObject = (JSONObject) new JSONParser().parse(instanceConfig);
             String url = (String)jsonObject.get("url");
-            String categoryId = (String)jsonObject.get("categoryId");
+            String categoryId = "";
+            try{
+                Long temp = (Long)jsonObject.get("categoryId");
+                categoryId = String.valueOf(temp);
+            }catch (Exception e){
+                categoryId = (String)jsonObject.get("categoryId");
+            }
             String[] split = url.split("\\?");
             if(split.length != 2){
                 return null;
