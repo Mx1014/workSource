5725e4c07d243642ef65cc7201efe3f4655496c9
steve <cen.tang@zuolin.com>
2018-06-14 10:56:53 +0800

    modify GetDurationParamCommand.java

--- ehrest/src/main/java/com/everhomes/rest/contract/GetDurationParamCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/contract/GetDurationParamCommand.java
@@ -5,7 +5,7 @@ import com.everhomes.util.StringHelper;
 public class GetDurationParamCommand {
 	
 	private Long endTimeByDay;
-	private Long ContractId;
+	private Long contractId;
 	
 	public Long getEndTimeByDay() {
 		return endTimeByDay;
@@ -13,13 +13,13 @@ public class GetDurationParamCommand {
 	public void setEndTimeByDay(Long endTimeByDay) {
 		this.endTimeByDay = endTimeByDay;
 	}
+	
 	public Long getContractId() {
-		return ContractId;
+		return contractId;
 	}
 	public void setContractId(Long contractId) {
-		ContractId = contractId;
+		this.contractId = contractId;
 	}
-	
 	@Override
 	public String toString() {
 		return StringHelper.toJsonString(this);
