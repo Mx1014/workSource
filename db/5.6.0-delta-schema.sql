9d8318289e591eb1ccc71309f2fe61d81d9456c8
nan.rong <nan.rong@zuolin.com>
2018-06-21 18:22:14 +0800

    issue-26754 schema fix

--- db/5.6.0-delta-schema.sql
+++ db/5.6.0-delta-schema.sql
@@ -20,6 +20,8 @@ CREATE TABLE `eh_relocation_configs` (
 -- by shiheng.ma 20180620
 alter table eh_relocation_requests add column org_owner_type_id BIGINT(20) DEFAULT NULL COMMENT '客户类型（小区场景）';
 
+
+
 -- Designer: zhiwei zhang
 -- Description: ISSUE#28363 会议管理V1.0
 
@@ -217,3 +219,21 @@ CREATE TABLE `eh_archives_operational_logs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 人事2.7 end by ryan.
+
+-- 通用脚本
+-- ADD BY 梁燕龙
+-- issue-26754 敏感词日志记录
+CREATE TABLE `eh_sensitive_filter_record` (
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL COMMENT '域空间ID',
+  `sensitive_words` VARCHAR(128) COMMENT '敏感词',
+  `module_id` BIGINT COMMENT '模块ID',
+  `community_id` BIGINT COMMENT '项目ID',
+  `creator_uid` BIGINT COMMENT '记录发布人userId' ,
+  `creator_name` VARCHAR(32) COMMENT '发布人姓名',
+  `phone` VARCHAR(128) COMMENT '发布人手机号',
+  `publish_time` DATETIME COMMENT '记录发布时间' ,
+  `text` TEXT COMMENT '文本内容',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT = '敏感词过滤日志表';
+-- END BY 梁燕龙
