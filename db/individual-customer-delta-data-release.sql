70941ab63ef661f198016fd0e42b0f4b37166e43
dingjianmin <jm.ding@zuolin.com>
2018-06-21 13:34:49 +0800

    update sql

--- db/individual-customer-delta-data-release.sql
+++ db/individual-customer-delta-data-release.sql
@@ -11,7 +11,13 @@ INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUE
 
 -- apartment owner  by dingjianmin
 SET @id = (SELECT MAX(id) from eh_locale_strings);
+
 INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'address', '20011', 'zh_CN', '门牌状态不能为空');
 INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'address', '20012', 'zh_CN', '门牌朝向输入过长');
 INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'address', '20013', 'zh_CN', '门牌状态输入格式不正确');
+
+INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'community', '10013', 'zh_CN', '项目名称不能为空');
+INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'community', '10014', 'zh_CN', '项目地址超过指定长度');
+INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'community', '10015', 'zh_CN', '省市区不能为空');
+INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'community', '10016', 'zh_CN', '项目名称已存在');
 INSERT INTO  `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@id:=@id+1), 'community', '20006', 'zh_CN', '面积格式错误');
