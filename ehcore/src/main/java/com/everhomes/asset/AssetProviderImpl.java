07df6a43e7da321fadd4fd0aceea6b258e4ae04a
wentian <wentian.wang@zuolin.com>
2018-06-21 10:35:34 +0800

    #31979 found the wrong bill group rule when calculating, now add limits on category id

--- ehcore/src/main/java/com/everhomes/asset/AssetProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/asset/AssetProviderImpl.java
@@ -2287,7 +2287,7 @@ public class AssetProviderImpl implements AssetProvider {
     }
 
     @Override
-    public PaymentBillGroupRule getBillGroupRule(Long chargingItemId, Long chargingStandardId, String ownerType, Long ownerId) {
+    public List<PaymentBillGroupRule> getBillGroupRule(Long chargingItemId, Long chargingStandardId, String ownerType, Long ownerId) {
         DSLContext context = this.dbProvider.getDslContext(AccessSpec.readOnly());
         EhPaymentBillGroupsRules t = Tables.EH_PAYMENT_BILL_GROUPS_RULES.as("t");
         List<PaymentBillGroupRule> rules = context.select()
