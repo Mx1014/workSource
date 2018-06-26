0ae600e47dc511f7ce3eac94bfcf9b11e67ce21c
shiheng.ma <shiheng.ma@zuolin.com>
2018-06-26 10:55:04 +0800

    pmtask-3.6:过滤分类中间类型

--- ehcore/src/main/java/com/everhomes/pmtask/EbeiPmTaskHandle.java
+++ ehcore/src/main/java/com/everhomes/pmtask/EbeiPmTaskHandle.java
@@ -737,6 +737,10 @@ public class EbeiPmTaskHandle extends DefaultPmTaskHandle implements Application
         ListTaskCategoriesResponse response = new ListTaskCategoriesResponse();
 
         List<CategoryDTO> childrens = listServiceType(projectId, null != cmd.getParentId() ? cmd.getParentId() : null);
+//      V3.6 过滤正中会办事的中间类型
+        if(childrens.size() == 1){
+            childrens = childrens.get(0).getChildrens();
+        }
 
         if(null == cmd.getParentId()) {
             CategoryDTO dto = createCategoryDTO();
