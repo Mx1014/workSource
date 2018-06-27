fa6e695c41aa8d0b1277ff51abba560b2237bb08
dingjianmin <jm.ding@zuolin.com>
2018-06-27 10:37:30 +0800

    update code

--- ehrest/src/main/java/com/everhomes/rest/contract/AddContractTemplateCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/contract/AddContractTemplateCommand.java
@@ -18,60 +18,14 @@ import com.everhomes.rest.general_approval.PostApprovalFormItem;
 public class AddContractTemplateCommand {
 	
 	private Long id;
-	
 	private Integer namespaceId;
 	private Long communityId;
 	private String name;
-
-
+	private Byte contracttemplateType;
+	private Long categoryId;
+    private Long orgId;
 	
 	
-	private Long managerUid;
-	private String managerName;
-
-	private String contact;
-	
-	private String address;
-	
-	private Double areaSize;
-	
-	@NotNull
-	private String geoString;
-	
-	private String description;
-	
-	private String posterUri;
 	
 
-	
-	@ItemType(AttachmentDescriptor.class)
-    private List<AttachmentDescriptor> attachments;
-
-	private String floorCount;
-	private String trafficDescription;
-
-	private String liftDescription;
-	private String pmDescription;
-	private String parkingLotDescription;
-	private String environmentalDescription;
-	private String powerDescription;
-	private String telecommunicationDescription;
-	private String airConditionDescription;
-	private String securityDescription;
-	private String fireControlDescription;
-
-	private Long generalFormId;
-	private Byte customFormFlag;
-
-	private String constructionCompany;
-	private Long entryDate;
-	private Double height;
-
-	private String buildingNumber;
-
-	@ItemType(PostApprovalFormItem.class)
-	private List<PostApprovalFormItem> formValues;
-
-	
-	
 }
