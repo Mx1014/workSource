ef7024a4667a98b67bba1953ab54b744068adf38
dingjianmin <jm.ding@zuolin.com>
2018-06-27 11:22:04 +0800

    update table

--- db/contractV3.0_2-delta-schema.sql
+++ db/contractV3.0_2-delta-schema.sql
@@ -5,13 +5,15 @@
 CREATE TABLE `eh_contract_template` (
   `id` bigint(20) NOT NULL COMMENT 'id of the record',
   `namespace_id` int(11) NOT NULL DEFAULT '0' COMMENT 'namespace of owner resource, redundant info to quick namespace related queries',
-  `community_id` bigint(20) DEFAULT NULL COMMENT '园区id',
+  `category_id` bigint(20) DEFAULT NULL COMMENT 'contract category id 用于多入口',
+  `owner_id` bigint(20) NOT NULL DEFAULT '0',
+  `owner_type` varchar(32) NOT NULL DEFAULT '' COMMENT 'the type of who own the category, community, etc',
   `name` varchar(64) NOT NULL COMMENT '合同模板名称',
   `contract_template_type` tinyint(2) DEFAULT '0' COMMENT '0 收款合同模板 1付款合同模板',
-  `category_id` bigint(20) DEFAULT NULL COMMENT 'contract category id',
   `status` tinyint(4) NOT NULL DEFAULT '2' COMMENT '0: inactive, 1: confirming, 2: active', 
-  `template_contents` text  COMMENT '模板内容',
-  `template_owner` bigint(20) NOT NULL DEFAULT '0' COMMENT '0: 通用配置, community_id ：表示归属的园区', 
+  `contents` text  COMMENT '模板内容',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '复制于哪个合同模板',
+  `version` int(11) DEFAULT '0' COMMENT '版本记录',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `creator_uid` bigint(20) NOT NULL DEFAULT '0' COMMENT 'record creator user id',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
@@ -20,6 +22,4 @@ CREATE TABLE `eh_contract_template` (
   `delete_uid` bigint(20) DEFAULT '0' COMMENT 'record deleter user id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
 
--- ehrest/src/main/java/com/everhomes/rest/contract/AddContractTemplateCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/contract/AddContractTemplateCommand.java
@@ -1,31 +1,104 @@
 package com.everhomes.rest.contract;
 
-import java.util.List;
-
 import javax.validation.constraints.NotNull;
 
-import com.everhomes.discover.ItemType;
-import com.everhomes.rest.forum.AttachmentDescriptor;
-import com.everhomes.rest.general_approval.PostApprovalFormItem;
-
 /**
- * <ul>参数:
+ * <ul>
+ * 参数:
  * <li>namespaceId: 域空间id</li>
  * <li>communityId: 园区id</li>
  * </ul>
  * Created by ying.xiong on 2017/8/17.
  */
 public class AddContractTemplateCommand {
-	
+
 	private Long id;
+	@NotNull
 	private Integer namespaceId;
-	private Long communityId;
+	@NotNull
+	private Long categoryId;
+	private Long ownerId;
+	private String ownerType;
 	private String name;
 	private Byte contracttemplateType;
-	private Long categoryId;
-    private Long orgId;
-	
-	
-	
+	private Byte status;
+	private String contents;
+	private Long parentId;
+	private Long version;
+	@NotNull
+	private Long orgId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getNamespaceId() {
+		return namespaceId;
+	}
+	public void setNamespaceId(Integer namespaceId) {
+		this.namespaceId = namespaceId;
+	}
+	public Long getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+	public Long getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+	public String getOwnerType() {
+		return ownerType;
+	}
+	public void setOwnerType(String ownerType) {
+		this.ownerType = ownerType;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Byte getContracttemplateType() {
+		return contracttemplateType;
+	}
+	public void setContracttemplateType(Byte contracttemplateType) {
+		this.contracttemplateType = contracttemplateType;
+	}
+	public Byte getStatus() {
+		return status;
+	}
+	public void setStatus(Byte status) {
+		this.status = status;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Long getParentId() {
+		return parentId;
+	}
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+	public Long getVersion() {
+		return version;
+	}
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+	public Long getOrgId() {
+		return orgId;
+	}
+	public void setOrgId(Long orgId) {
+		this.orgId = orgId;
+	}
 
+	
 }
