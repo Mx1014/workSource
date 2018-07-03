8daf1fcac6715b0f7ebec145cca5b2b2b2926851
yanjun <jun.yan@zuolin.com>
2018-07-03 11:45:38 +0800

    fix schame error

--- db/zuolin-base-2.0-delta-schema.sql
+++ db/zuolin-base-2.0-delta-schema.sql
@@ -221,23 +221,6 @@ ALTER TABLE `eh_service_module_apps` ADD COLUMN `default_app_flag`  tinyint(4) N
 -- 修改appId名字，实际为应用originId
 ALTER TABLE `eh_banners` CHANGE COLUMN `appId` `app_id`  bigint(20) NULL DEFAULT NULL;
 
--- Already delete in 5.5.1
-ALTER TABLE `eh_organization_member_details` ADD COLUMN `profile_integrity` INTEGER NOT NULL DEFAULT '0';
-ALTER TABLE eh_organization_member_details ADD COLUMN department VARCHAR(256) COMMENT '部门';
-ALTER TABLE eh_organization_member_details ADD COLUMN department_ids VARCHAR(256) COMMENT '部门Id';
-ALTER TABLE eh_organization_member_details ADD COLUMN job_position VARCHAR(256) COMMENT '岗位';
-ALTER TABLE eh_organization_member_details ADD COLUMN job_position_ids VARCHAR(256) COMMENT '岗位Id';
-ALTER TABLE eh_organization_member_details ADD COLUMN job_level VARCHAR(256) COMMENT '职级';
-ALTER TABLE eh_organization_member_details ADD COLUMN job_level_ids VARCHAR(256) COMMENT '职级Id';
--- end Janson
-
--- 园区表增加namespace_id索引 add by yanjun 20180615
-alter table eh_communities add index namespace_id_index(`namespace_id`);
-
--- fix for zuolinbase only, remove this after 5.5.2
-ALTER TABLE `eh_organization_member_details` CHANGE COLUMN `profile_integrity` `profile_integrity` VARCHAR(20) NULL DEFAULT '0' ;
--- end Janson
-
 -- 园区广场电商 add by yanjun 20180703
 CREATE TABLE `eh_community_bizs` (
   `id` bigint(20) NOT NULL,
