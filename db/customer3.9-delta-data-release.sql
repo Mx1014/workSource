642ee14a9703cb8977ffe48c50f59d38fe10e13c
jiarui <rui.jia@zuolin.com>
2018-07-03 09:43:09 +0800

    add default dynamic group and fields

--- db/customer3.9-delta-data-release.sql
+++ db/customer3.9-delta-data-release.sql
@@ -1,4 +1,4 @@
---  客户事件假如动态表单
+--  客户事件加入动态表单
 --  by jiarui 20180626
 SET  @groupId = (SELECT  max(id) FROM  eh_var_field_groups);
 INSERT INTO `eh_var_field_groups` (`id`, `module_name`, `parent_id`, `path`, `title`, `name`, `mandatory_flag`, `default_order`, `status`, `creator_uid`, `create_time`, `operator_uid`, `update_time`) VALUES (( @groupId:= @groupId+1), 'enterprise_customer', '0', concat('/',@groupId), '客户事件', '', '0', NULL, '2', '1', NOW(), NULL, NULL);
