02754e2c2a2f42d174fea7522864e09480a6fbd7
yanjun <jun.yan@zuolin.com>
2018-07-03 13:29:36 +0800

    add communityBiz

---
+++ ehcore/src/main/java/com/everhomes/launchpadbase/CommunityBizController.java
@@ -0,0 +1,91 @@
+package com.everhomes.launchpadbase;
+
+import com.everhomes.constants.ErrorCodes;
+import com.everhomes.controller.ControllerBase;
+import com.everhomes.discover.RestDoc;
+import com.everhomes.discover.RestReturn;
+import com.everhomes.launchpad.CommunityBizService;
+import com.everhomes.launchpad.LaunchPadService;
+import com.everhomes.rest.RestResponse;
+import com.everhomes.rest.launchpad.GetLaunchPadLayoutCommand;
+import com.everhomes.rest.launchpad.LaunchPadLayoutDTO;
+import com.everhomes.rest.launchpadbase.*;
+import com.everhomes.serviceModuleApp.ServiceModuleAppService;
+import com.everhomes.util.RequireAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestDoc(value="LaunchPadBase controller", site="core")
+@RestController
+@RequestMapping("/communityBiz")
+public class CommunityBizController extends ControllerBase {
+
+    @Autowired
+    private CommunityBizService communityBizService;
+
+
+    /**
+     * <b>URL: /communityBiz/createCommunityBiz</b>
+     * <p>获取运营板块信息</p>
+     */
+    @RequestMapping("createCommunityBiz")
+    @RestReturn(value=CommunityBizDTO.class)
+    @RequireAuthentication(false)
+    public RestResponse createCommunityBiz(CreateCommunityBiz cmd) {
+        CommunityBizDTO res = communityBizService.CreateCommunityBiz(cmd);
+        RestResponse response =  new RestResponse(res);
+        response.setErrorCode(ErrorCodes.SUCCESS);
+        response.setErrorDescription("OK");
+        return response;
+    }
+
+
+    /**
+     * <b>URL: /communityBiz/updateCommunityBiz</b>
+     * <p>获取运营板块信息</p>
+     */
+    @RequestMapping("updateCommunityBiz")
+    @RestReturn(value=CommunityBizDTO.class)
+    @RequireAuthentication(false)
+    public RestResponse updateCommunityBiz(updateCommunityBiz cmd) {
+        CommunityBizDTO res = communityBizService.updateCommunityBiz(cmd);
+        RestResponse response =  new RestResponse(res);
+        response.setErrorCode(ErrorCodes.SUCCESS);
+        response.setErrorDescription("OK");
+        return response;
+    }
+
+    /**
+     * <b>URL: /communityBiz/deleteCommunityBiz</b>
+     * <p>获取运营板块信息</p>
+     */
+    @RequestMapping("deleteCommunityBiz")
+    @RestReturn(value=String.class)
+    @RequireAuthentication(false)
+    public RestResponse deleteCommunityBiz(DeleteCommunityBiz cmd) {
+        communityBizService.deleteCommunityBiz(cmd);
+        RestResponse response =  new RestResponse();
+        response.setErrorCode(ErrorCodes.SUCCESS);
+        response.setErrorDescription("OK");
+        return response;
+    }
+    /**
+     * <b>URL: /communityBiz/findCommunityBizByCommunityId</b>
+     * <p>获取运营板块信息</p>
+     */
+    @RequestMapping("findCommunityBizByCommunityId")
+    @RestReturn(value=CommunityBizDTO.class)
+    @RequireAuthentication(false)
+    public RestResponse findCommunityBizByCommunityId(FindCommunityBizByCommunityId cmd) {
+        CommunityBizDTO res = communityBizService.findCommunityBizByCommunityId(cmd);
+        RestResponse response =  new RestResponse(res);
+        response.setErrorCode(ErrorCodes.SUCCESS);
+        response.setErrorDescription("OK");
+        return response;
+    }
+
+
+}
---
+++ ehrest/src/main/java/com/everhomes/rest/launchpadbase/updateCommunityBiz.java
@@ -0,0 +1,78 @@
+// @formatter:off
+package com.everhomes.rest.launchpadbase;
+
+
+import com.everhomes.util.StringHelper;
+
+/**
+ * <ul>
+ *     <li>id: id</li>
+ *     <li>communityId: communityId</li>
+ *     <li>name: name</li>
+ *     <li>bizUrl: bizUrl</li>
+ *     <li>logoUri: logoUri</li>
+ *     <li>status: 状态0-删除，1-未启用，2-启用，参考{@link com.everhomes.rest.launchpadbase.CommunityBizStatus}</li>
+ * </ul>
+ */
+public class updateCommunityBiz {
+
+    private Long id;
+    private Long communityId;
+    private String name;
+    private String bizUrl;
+    private String logoUri;
+    private Byte status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCommunityId() {
+        return communityId;
+    }
+
+    public void setCommunityId(Long communityId) {
+        this.communityId = communityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBizUrl() {
+        return bizUrl;
+    }
+
+    public void setBizUrl(String bizUrl) {
+        this.bizUrl = bizUrl;
+    }
+
+    public String getLogoUri() {
+        return logoUri;
+    }
+
+    public void setLogoUri(String logoUri) {
+        this.logoUri = logoUri;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
+}
