5ba19f4490c008bb9ecb79d3e7f9a4e23bdd5660
steve <cen.tang@zuolin.com>
2018-07-03 14:29:21 +0800

    merge contract-2.9-merge-branch into 5.6.2

--- db/5.6.2-delta-schema.sql
+++ db/5.6.2-delta-schema.sql
@@ -113,3 +113,17 @@ ALTER TABLE `eh_parking_recharge_orders` ADD COLUMN `invoice_create_time` DATETI
 ALTER TABLE eh_meeting_rooms DROP INDEX u_eh_namespace_owner_name;
 ALTER TABLE eh_meeting_rooms ADD INDEX `i_eh_namespace_owner_name` (`namespace_id` , `organization_id`, `owner_type` , `owner_id` , `name`);
 -- END
+
+-- 通用脚本
+-- ADD BY 唐岑  2018年7月3日14:27:33
+-- 合同管理V2.9 #30013
+CREATE TABLE `eh_contract_events` (
+  `id` bigint(20) NOT NULL,
+  `namespace_id` int(11) NOT NULL,
+  `contract_id` bigint(20) NOT NULL COMMENT '该日志事件对应的合同id',
+  `operator_uid` bigint(20) DEFAULT NULL COMMENT '修改人员id',
+  `opearte_time` datetime NOT NULL COMMENT '修改时间',
+  `opearte_type` tinyint(4) NOT NULL COMMENT '操作类型（1：增加，2：删除，3：修改）',
+  `content` text COMMENT '修改内容描述'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '合同日志记录表';
+-- END BY 唐岑
