f90b3960f511f4c4ff1e6beabbbc61187c1f6398
steve <cen.tang@zuolin.com>
2018-07-04 22:24:10 +0800

    issue-33012:解决只能以第一个手机号搜索的问题

--- db/search/pmowner.sh
+++ db/search/pmowner.sh
@@ -8,6 +8,7 @@ curl -XPUT "http://$ELASTIC/$INDEX/_mapping/pmowner" -d '
         "properties": {
             "contactToken":{"type":"string","index_analyzer":"ansj_index", "search_analyzer":"ansj_query", "similarity":"BM25", "store":"yes"},
             "contactName":{"type":"string","index_analyzer":"ansj_index", "search_analyzer":"ansj_query", "similarity":"BM25", "store":"yes"},
+            "contactExtraTels":{"type":"string","index_analyzer":"ansj_index", "search_analyzer":"ansj_query", "similarity":"BM25"},
             "communityId":{"type":"long"},
             "orgOwnerTypeId":{"type":"integer"}
         }
