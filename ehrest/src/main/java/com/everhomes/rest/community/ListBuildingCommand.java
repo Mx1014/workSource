0a5979e44738015204fb6f1686a3f9a0e81db697
jiarui <rui.jia@zuolin.com>
2018-07-10 10:50:47 +0800

    openapi init

--- ehrest/src/main/java/com/everhomes/rest/community/ListBuildingCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/community/ListBuildingCommand.java
@@ -20,6 +20,13 @@ public class ListBuildingCommand {
     
 	private Integer pageSize;
 
+	//以下参数为openapi所需
+	private String appKey;
+	private String signature;
+	private Long timestamp;
+	private Integer nonce;
+	private String crypto;
+
 	public Long getCommunityId() {
 		return communityId;
 	}
@@ -52,4 +59,43 @@ public class ListBuildingCommand {
 		this.namespaceId = namespaceId;
 	}
 
+	public String getAppKey() {
+		return appKey;
+	}
+
+	public void setAppKey(String appKey) {
+		this.appKey = appKey;
+	}
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getNonce() {
+		return nonce;
+	}
+
+	public void setNonce(Integer nonce) {
+		this.nonce = nonce;
+	}
+
+	public String getCrypto() {
+		return crypto;
+	}
+
+	public void setCrypto(String crypto) {
+		this.crypto = crypto;
+	}
 }
