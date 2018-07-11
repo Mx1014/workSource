67c84db22c23094fb395eb4f275e78491aef318e
nan.rong <nan.rong@zuolin.com>
2018-07-11 17:12:23 +0800

    sql update

--- db/archives-2.8-delta-schema.sql
+++ db/archives-2.8-delta-schema.sql
@@ -1,5 +1,7 @@
-RENAME TABLE eh_archives_forms TO eh_archives_form_navigation;
+DROP TABLE eh_general_form_groups;
 
+-- 此表未使用 直接重建
+DROP TABLE eh_archives_forms;
 CREATE TABLE `eh_archives_forms` (
 	`id` BIGINT NOT NULL,
 	`namespace_id` INT NOT NULL DEFAULT '0',
@@ -23,9 +25,18 @@ CREATE TABLE `eh_archives_form_vals`(
 	`field_name` VARCHAR(128) COMMENT 'the name of the field',
 	`field_type` VARCHAR(128) COMMENT 'the type of the field',
 	`field_value` TEXT COMMENT 'the value of the field',
-	`form_name` VARCHAR(64) NOT NULL COMMENT 'name of the form',
-	`static_fields` NOT NULL TEXT COMMENT 'static form fields in json format',
-	`dynamic_fields` TEXT COMMENT 'dynamic form fields in json format',
 	`create_time` DATETIME,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `eh_archives_form_groups` (
+	`id` BIGINT NOT NULL,
+	`namespace_id` INT NOT NULL DEFAULT '0',
+	`owner_type` VARCHAR(32),
+	`owner_id` BIGINT,
+	`form_id` BIGINT COMMENT 'the id of the archives form',
+	`form_groups` TEXT NOT NULL  COMMENT 'the group of the form in json format',
+	`operator_uid` BIGINT,
+	`operator_time` DATETIME,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
