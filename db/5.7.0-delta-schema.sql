32bb4072bb191d399c3aeaed53acad5e00a1c79b
huangliangming <liangming.huang@zuolin.com>
2018-07-11 14:28:40 +0800

    issue-33216-2:整理脚本到5.7.0中去

--- db/5.7.0-delta-schema.sql
+++ db/5.7.0-delta-schema.sql
@@ -7,3 +7,17 @@ ALTER TABLE eh_terminal_day_statistics ADD COLUMN average_active_user_number BIG
 ALTER TABLE eh_terminal_day_statistics ADD COLUMN average_active_user_change_rate DECIMAL(10, 2);
 
 -- ISSUE-32697 END
+
+-- 通用脚本  
+-- AUTHOR: 黄良铭
+-- REMARK: #Issue-33216 服务协议信息表
+CREATE TABLE `eh_service_agreement` (
+
+  `id` INT(11)  NOT NULL COMMENT '主键',
+  `namespace_id` INT(11) NOT NULL  COMMENT '域空间ID',
+  `agreement_content` mediumtext  COMMENT '协议内容',
+
+  PRIMARY KEY(`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '服务协议信息表';
+
+-- #Issue-33216  end
