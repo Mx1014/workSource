34fdb2cf2fa7ecd3d3aca2c8b3794d23e03d5f8b
st.zheng <st.zheng@zuolin.com>
2018-07-13 15:50:20 +0800

    园区入驻

--- ehcore/src/main/java/com/everhomes/techpark/expansion/EnterpriseApplyBuildingServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/techpark/expansion/EnterpriseApplyBuildingServiceImpl.java
@@ -385,11 +385,10 @@ public class EnterpriseApplyBuildingServiceImpl implements EnterpriseApplyBuildi
 			cmd.setNamespaceId(UserContext.getCurrentNamespaceId());
 		}
 
-		Set<Long> authCommunities = null;
+		Set<Long> authCommunities = new HashSet<>();
 		if(cmd.getCurrentPMId()!=null && cmd.getAppId()!=null && configProvider.getBooleanValue("privilege.community.checkflag", true)){
 			if (cmd.getCurrentProjectId()!=null) {
 				userPrivilegeMgr.checkUserPrivilege(UserContext.current().getUser().getId(), cmd.getCurrentPMId(), 4010040110L, cmd.getAppId(), null, cmd.getCurrentProjectId());//项目介绍权限
-				authCommunities = new HashSet<>();
 				authCommunities.add(cmd.getCurrentProjectId());
 			}else{//项目导航为全部 找出授权的项目
 				ListUserRelatedProjectByModuleCommand cmd2 = new ListUserRelatedProjectByModuleCommand();
@@ -399,8 +398,6 @@ public class EnterpriseApplyBuildingServiceImpl implements EnterpriseApplyBuildi
 				List<ProjectDTO> dtos = serviceModuleService.listUserRelatedCategoryProjectByModuleId(cmd2);
 				if (dtos!=null && dtos.size()>0)
 					authCommunities = dtos.stream().map(ProjectDTO::getProjectId).collect(Collectors.toSet());
-				else
-					authCommunities = new HashSet<>();
 			}
 		}
 
