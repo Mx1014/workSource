478eb9220428d5cdf7c374c0534229b2afca1293
liangyanlong <yanlong.liang@zuolin.com>
2018-07-16 15:16:12 +0800

    appwhitelist: 新增支付宝链接

--- db/appwhitelist-delta-data-release.sql
+++ db/appwhitelist-delta-data-release.sql
@@ -2,4 +2,6 @@
 INSERT INTO `eh_app_white_list` (`link`,`name`)
     VALUES ('weixin','微信');
 INSERT INTO `eh_app_white_list` (`link`,`name`)
-VALUES ('AliPay','支付宝');
+VALUES ('alipay','支付宝');
+INSERT INTO `eh_app_white_list` (`link`,`name`)
+VALUES ('alipays','支付宝');
