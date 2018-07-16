784cff0415ec2e32b88a3aea6e886ad684c35a36
shiheng.ma <shiheng.ma@zuolin.com>
2018-07-16 15:17:47 +0800

    pmtask-3.7:修改评价类型

--- ehcore/src/main/java/com/everhomes/pmtask/PmTaskSearchImpl.java
+++ ehcore/src/main/java/com/everhomes/pmtask/PmTaskSearchImpl.java
@@ -373,7 +373,7 @@ public class PmTaskSearchImpl extends AbstractElasticSearch implements PmTaskSea
             doc.setFlowCaseId(SearchUtils.getLongField(source.get("flowCaseId")));
             doc.setBuildingName((String)source.get("buildingName"));
             doc.setOrganizationUid(SearchUtils.getLongField(source.get("organizationUid")));
-            doc.setStar(source.get("star") == null ? null : SearchUtils.getLongField(source.get("star")).byteValue());
+            doc.setStar((String)source.get("star"));
             
             return doc;
         }catch (Exception ex) {
