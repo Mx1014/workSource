e82709ea37674fbd77418fb9e0e63ecbbbc75483
liangyanlong <yanlong.liang@zuolin.com>
2018-07-17 11:54:26 +0800

    appwhitelist: 白名单增加招商银行.

--- db/appwhitelist-delta-data-release.sql
+++ db/appwhitelist-delta-data-release.sql
@@ -5,3 +5,5 @@ INSERT INTO `eh_app_white_list` (`id`,`link`,`name`)
 VALUES (1,'alipay','支付宝');
 INSERT INTO `eh_app_white_list` (`id`,`link`,`name`)
 VALUES (2,'alipays','支付宝');
+INSERT INTO `eh_app_white_list` (`id`,`link`,`name`)
+VALUES (3,'cmbmobilebank','招商银行');
