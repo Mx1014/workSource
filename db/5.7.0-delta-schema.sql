53225ae568f5c48ffd039f21d4101cde193a50f2
yanjun <jun.yan@zuolin.com>
2018-07-17 19:06:42 +0800

    issue-26827 PC工作台V1.0（后台框架）

--- db/5.7.0-delta-schema.sql
+++ db/5.7.0-delta-schema.sql
@@ -114,3 +114,11 @@ CREATE TABLE `eh_payment_subtraction_items` (
 -- REMARK: 取消滞纳金表字段非空限制
 ALTER TABLE eh_payment_late_fine MODIFY COLUMN customer_id BIGINT COMMENT 'allows searching taking advantage of it';
 -- --------------------- SECTION END ---------------------------------------------------------
+
+-- 通用脚本
+-- ADD BY jun.yan
+-- ISSUE-32697 模块对应的菜单是否需要授权, add by yanjun 20180517
+ALTER TABLE `eh_service_modules` ADD COLUMN `menu_auth_flag`  tinyint(4) NOT NULL DEFAULT 1 COMMENT 'if its menu need auth' ;
+
+ALTER TABLE `eh_service_modules` ADD COLUMN `category`  varchar(255) NULL COMMENT 'classify, module, subModule';
+-- end
