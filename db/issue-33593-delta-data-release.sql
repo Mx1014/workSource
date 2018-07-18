5eb621654f616f157bf413689248d647a8eaf6de
liangyanlong <yanlong.liang@zuolin.com>
2018-07-18 15:30:44 +0800

    issue-33593: 报名限制人数提示文案修改.

--- db/issue-33593-delta-data-release.sql
+++ db/issue-33593-delta-data-release.sql
@@ -1,4 +1,4 @@
 INSERT INTO eh_locale_templates(`scope`, `code`,`locale`, `description`, `text`)
-VALUES( 'activity.notification', 18, 'zh_CN', '活动报名人数不足最低限制人数，活动取消', '您报名的活动“${subject}”由于未达到最低人数，已被取消。”。');
+VALUES( 'activity.notification', 18, 'zh_CN', '活动报名人数不足最低限制人数，活动取消', '您报名的活动“${subject}”由于未达到最低人数，已被取消。');
 INSERT INTO eh_locale_templates(`scope`, `code`,`locale`, `description`, `text`)
-VALUES( 'activity.notification', 19, 'zh_CN', '活动报名人数不足最低限制人数，活动取消', '您报名的活动“${subject}”由于未达到最低人数，已被取消，报名费用将在三个工作日内退回您的账户上”。');
+VALUES( 'activity.notification', 19, 'zh_CN', '活动报名人数不足最低限制人数，活动取消', '您报名的活动“${subject}”由于未达到最低人数，已被取消，报名费用将在三个工作日内退回您的账户上。');
