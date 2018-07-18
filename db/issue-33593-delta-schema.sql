38c7aa5783eb59c44571bf8c8487b43a84a1ecf5
liangyanlong <yanlong.liang@zuolin.com>
2018-07-18 16:02:40 +0800

    issue-33593: 设置取消时间.

--- db/issue-33593-delta-schema.sql
+++ db/issue-33593-delta-schema.sql
@@ -3,4 +3,6 @@
 -- 帖子和活动表增加最低限制人数
 ALTER TABLE `eh_forum_posts` ADD COLUMN `min_quantity` INT(11) COMMENT '最低限制人数';
 ALTER TABLE `eh_activities` ADD COLUMN `min_quantity` INT(11) COMMENT '最低限制人数';
+ALTER TABLE `eh_forum_posts` ADD COLUMN `cancel_time` DATETIME COMMENT '取消时间';
+ALTER TABLE `eh_activities` ADD COLUMN `cancel_time` DATETIME COMMENT '取消时间';
 -- END BY yanlong.liang
