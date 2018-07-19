43b4a38fc0525a2581cea3667b71efa457a1106a
st.zheng <st.zheng@zuolin.com>
2018-07-19 10:58:41 +0800

    rental3.6

--- db/rental-3.6-delta-schema.sql
+++ db/rental-3.6-delta-schema.sql
@@ -1,4 +1,4 @@
 -- rental-3.6 by st.zheng
 ALTER TABLE eh_rentalv2_resource_pics rename eh_rentalv2_site_resources;
-ALTER TABLE `eh_rentalv2_resource_pics`
+ALTER TABLE `eh_rentalv2_site_resources`
 ADD COLUMN `type`  varchar(64) NULL DEFAULT 'pic' AFTER `owner_type`;
