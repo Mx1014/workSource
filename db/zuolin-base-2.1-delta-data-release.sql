c3b8e25cc37c765e90755aa0fe4a60eea491e998
yanjun <jun.yan@zuolin.com>
2018-07-25 16:56:05 +0800

    merge 5.7.0

---
+++ db/zuolin-base-2.1-delta-data-release.sql
@@ -0,0 +1,52 @@
+-- 因为 5.7.0对模块和菜单重构了，这里要重新执行模块了菜单的sql。
+
+-- 客户端处理方式
+-- 默认所有是navtive应用
+update eh_service_modules set client_handler_type = 0;
+-- 内部链接
+update eh_service_modules set client_handler_type = 2 WHERE id in (41400, 20400, 21200, 32500);
+-- 离线包
+update eh_service_modules set client_handler_type = 3 WHERE id in (49100, 20800, 20600);
+
+
+-- 设置应用类型
+-- 默认为园区模块
+UPDATE eh_service_modules set app_type = 1;
+-- 设置oa模块
+UPDATE eh_service_modules set app_type = 0 WHERE id in (50100,  50300, 50500, 50400, 52000, 50600, 54000, 51300, 51400, 55000, 57000, 60100, 60200, 60210, 13000, 20650, 20830, 41020, 50700, 41410, 41420, 41430, 41400);
+-- 更新应用信息
+UPDATE eh_service_module_apps a set a.app_type = IFNULL((SELECT b.app_type from eh_service_modules b where b.id = a.module_id), 1);
+
+
+-- 增加应用、项目和公司菜单
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`, `config_type`) VALUES ('15025000', '应用入口', '15000000', NULL, 'servicemodule-entry', '1', '2', '/15000000/15025000', 'zuolin', '30', NULL, '2', 'system', 'module', NULL);
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`, `config_type`) VALUES ('15030000', '应用管理', '15000000', NULL, 'application-management', '1', '2', '/15000000/15030000', 'zuolin', '40', NULL, '2', 'system', 'module', NULL);
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`, `config_type`) VALUES ('15040000', '项目管理', '15000000', NULL, 'project-management', '1', '2', '/15000000/15040000', 'zuolin', '50', NULL, '2', 'system', 'module', NULL);
+INSERT INTO `eh_web_menus` (`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`, `config_type`) VALUES ('15050000', '企业管理', '15000000', NULL, 'business-admin', '1', '2', '/15000000/15050000', 'zuolin', '60', NULL, '2', 'system', 'module', NULL);
+DELETE FROM `eh_web_menus` WHERE `id`='15030100';
+DELETE FROM `eh_web_menus` WHERE `id`='15030200';
+DELETE FROM `eh_web_menus` WHERE `id`='15030300';
+
+-- 公司门禁 员工认证 职级管理 任务管理 设置为org控制  add by yanjun 20180614
+UPDATE eh_service_modules SET module_control_type = 'org_control' WHERE id in (41020, 50500, 50300, 13000);
+UPDATE eh_service_module_apps SET module_control_type = 'org_control' WHERE module_id in (41020, 50500, 50300, 13000);
+
+-- janson 物品搬迁
+UPDATE `eh_service_modules` SET `instance_config`='{"url":"${home.url}/goods-move/build/index.html?ns=2&hideNavigationBar=1&ehnavigatorstyle=0#/home#sign_suffix"}' WHERE `id`='49200';
+UPDATE `eh_service_module_apps` SET `instance_config`='{"url":"${home.url}/goods-move/build/index.html?ns=2&hideNavigationBar=1&ehnavigatorstyle=0#/home#sign_suffix"}' WHERE `module_id`='49200';
+UPDATE `eh_service_modules` SET `client_handler_type`='2' WHERE `id`='49200';
+-- end
+
+
+-- 增加电商模块，用于运营板块 add by yanjun 201807041725
+INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`, `instance_config`, `action_type`, `update_time`, `operator_uid`, `creator_uid`, `description`, `multiple_flag`, `module_control_type`, `app_type`, `client_handler_type`, `system_app_flag`) VALUES ('92000', '电商', '0', '/92000', '1', '1', '2', '0', '2018-07-04 17:22:11', NULL, NULL, '2018-07-04 17:22:20', '0', '0', '0', '0', NULL, '1', '0', NULL);
+INSERT INTO `eh_service_modules` (`id`, `name`, `parent_id`, `path`, `type`, `level`, `status`, `default_order`, `create_time`, `instance_config`, `action_type`, `update_time`, `operator_uid`, `creator_uid`, `description`, `multiple_flag`, `module_control_type`, `app_type`, `client_handler_type`, `system_app_flag`) VALUES ('92100', '微商城', '92000', '/92000/92100', '1', '2', '2', '0', '2018-07-04 17:23:28', '{\"url\":\"${stat.biz.server.url}zl-ec/rest/service/front/logon?hideNavigationBar=1&sourceUrl=${stat.biz.server.url}nar/biz/web/app/user/index.html?clientrecommend=1#/recommend?_k=zlbiz#sign_suffix\"}', NULL, '2018-07-04 17:23:33', '0', '0', '0', '0', NULL, '1', '2', NULL);
+
+
+-- 更新企业客户管理模块
+UPDATE eh_service_modules set client_handler_type = 2 where id = 21100;
+
+
+-- 园区入驻从“敬请期待”改为“上线”
+UPDATE eh_service_modules set client_handler_type = 0, instance_config = '{}' WHERE id = 40100;
+UPDATE eh_service_module_apps set instance_config = '{}' WHERE module_id = 40100;
