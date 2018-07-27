71730a614bf7de9f8fe3df6301853e1c26db4c83
jiarui <rui.jia@zuolin.com>
2018-07-27 14:03:08 +0800

    property zuolin base

--- db/equipment-sta-delta-schema.sql
+++ db/equipment-sta-delta-schema.sql
@@ -9,3 +9,11 @@ ALTER TABLE `eh_var_field_scopes` ADD COLUMN `owner_type`  VARCHAR(1024)  NULL  
 ALTER TABLE `eh_var_field_item_scopes` ADD COLUMN `owner_type`  VARCHAR(1024)  NULL AFTER `owner_id`;
 ALTER TABLE `eh_var_field_group_scopes` ADD COLUMN `owner_type`  VARCHAR(1024)  NULL AFTER `owner_id`;
 -- end
+
+
+-- 通用脚本
+-- AUHOR:jiarui 20180727
+-- REMARK:客户管理增加ownerId ownerype
+ALTER  TABLE  `eh_enterprise_customers` ADD  COLUMN `owner_id` BIGINT(20) NOT NULL  DEFAULT  0 AFTER  `namespace_id`;
+ALTER  TABLE  `eh_enterprise_customers` ADD  COLUMN `owner_type` VARCHAR(1024) NULL AFTER  `owner_id`;
+-- end
