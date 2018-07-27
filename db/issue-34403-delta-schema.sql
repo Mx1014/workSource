d8b573e0108864a10c46211cff97e5adf99061d1
liangyanlong <yanlong.liang@zuolin.com>
2018-07-27 11:57:56 +0800

    issue-34403: 报名表增加表单支ID集合字段.

--- db/issue-34403-delta-schema.sql
+++ db/issue-34403-delta-schema.sql
@@ -1,5 +1,5 @@
 -- 通用脚本
 -- add by yanlong.liang 20180727
 -- 报名表增加表单值ID
-ALTER TABLE `eh_activity_roster` ADD COLUMN `general_form_value_id` BIGINT COMMENT '表单值ID';
+ALTER TABLE `eh_activity_roster` ADD COLUMN `general_form_value_id` VARCHAR(512) COMMENT '表单值ID集合';
 -- END
