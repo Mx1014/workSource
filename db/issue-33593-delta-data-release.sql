ce27f9886bd4f17b9aeb2204e5cf8a2f96c75b6c
liangyanlong <yanlong.liang@zuolin.com>
2018-07-30 18:36:04 +0800

    issue-33593: 修改文案.

--- db/issue-33593-delta-data-release.sql
+++ db/issue-33593-delta-data-release.sql
@@ -5,3 +5,4 @@ VALUES( 'activity.notification', 19, 'zh_CN', '活动报名人数不足最低限
 INSERT INTO eh_locale_templates(`scope`, `code`,`locale`, `description`, `text`)
 VALUES( 'activity.notification', 20, 'zh_CN', '活动报名人数不足最低限制人数，活动取消', '您发起的活动“${subject}”由于未达到最低人数，已被自动取消。');
 INSERT INTO `eh_locale_strings` (`scope`,`code`,`locale`,`text`) VALUES ('activity',28,'zh_CN','活动取消通知');
+UPDATE eh_locale_strings SET text = '来晚啦，活动已删除' WHERE scope = 'forum' and code = 10006
