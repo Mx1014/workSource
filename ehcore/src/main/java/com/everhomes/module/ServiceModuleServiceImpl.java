adc5b9953ef994b1dc320f25da9d442e96828705
yanjun <jun.yan@zuolin.com>
2018-08-01 17:17:24 +0800

    fix

--- ehcore/src/main/java/com/everhomes/module/ServiceModuleServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/module/ServiceModuleServiceImpl.java
@@ -16,6 +16,8 @@ import com.everhomes.namespace.Namespace;
 import com.everhomes.organization.*;
 import com.everhomes.organization.pm.pay.GsonUtil;
 import com.everhomes.portal.PortalPublishHandler;
+import com.everhomes.rest.namespace.ListCommunityByNamespaceCommandResponse;
+import com.everhomes.rest.organization.ListCommunitiesByOrganizationIdCommand;
 import com.everhomes.serviceModuleApp.ServiceModuleApp;
 import com.everhomes.serviceModuleApp.ServiceModuleAppProvider;
 import com.everhomes.rest.acl.*;
@@ -671,6 +673,23 @@ public class ServiceModuleServiceImpl implements ServiceModuleService {
         Integer namespaceId = UserContext.getCurrentNamespaceId();
 
 
+        //定制版的App广场item没有appId和moduleId，直接查询公司管理的项目
+        if(cmd.getModuleId() == null && cmd.getAppId() == null){
+
+            ListCommunitiesByOrganizationIdCommand listCmd = new ListCommunitiesByOrganizationIdCommand();
+            listCmd.setOrganizationId(cmd.getOrganizationId());
+            ListCommunityByNamespaceCommandResponse listRes = organizationService.listCommunityByOrganizationId(listCmd);
+
+            List<ProjectDTO> dtos = new ArrayList<>();
+            if(listRes != null && listRes.getCommunities() != null && listRes.getCommunities().size() > 0){
+                for (CommunityDTO community: listRes.getCommunities()){
+                    ProjectDTO dto = toProjectDto(community);
+                    dtos.add(dto);
+                }
+            }
+            return dtos;
+        }
+
         if(cmd.getModuleId() == null && cmd.getAppId() != null){
             ServiceModuleApp app = serviceModuleAppService.findReleaseServiceModuleAppByOriginId(cmd.getAppId());
             if(app != null){
@@ -685,6 +704,17 @@ public class ServiceModuleServiceImpl implements ServiceModuleService {
         return rolePrivilegeService.getTreeProjectCategories(namespaceId, dtos);
 
     }
+
+
+    private ProjectDTO toProjectDto(CommunityDTO communityDto){
+        ProjectDTO dto = new ProjectDTO();
+        dto.setProjectId(communityDto.getId());
+        dto.setProjectName(communityDto.getName());
+        dto.setProjectType(com.everhomes.entity.EntityType.COMMUNITY.getCode());
+        dto.setCommunityType(communityDto.getCommunityType());
+        return dto;
+    }
+
 
     @Override
     public List<ProjectDTO> listUserRelatedProjectByModuleId(ListUserRelatedProjectByModuleCommand cmd) {
