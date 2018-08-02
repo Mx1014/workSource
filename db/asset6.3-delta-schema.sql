a7ff3d4f4dff6d7773ebda90b35d4a2a3185a666
cx.yang <cx.yang@zuolin.com>
2018-08-02 17:43:59 +0800

    issue-34110 增加欠费天数字段

--- db/asset6.3-delta-schema.sql
+++ db/asset6.3-delta-schema.sql
@@ -3,6 +3,7 @@
 -- DESCRIPTION: 此SECTION放所有域空间都需要执行的脚本，包含基线、独立部署、研发数据等环境
 -- AUTHOR: 杨崇鑫
 -- REMARK: 物业缴费V6.3 （欠费时间筛选、费项进多账单组）
+ALTER TABLE `eh_payment_bills` ADD COLUMN `due_day_count` BIGINT COMMENT '欠费天数' after due_day_deadline;
 
 
 -- --------------------- SECTION END ---------------------------------------------------------
