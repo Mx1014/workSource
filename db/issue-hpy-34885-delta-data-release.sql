09eba3268c7a281d6598c508df85097a1e6b7ebd
pengyu.huang <pengyu.huang@zuolin.com>
2018-08-02 13:45:21 +0800

    导入导出数据修改

--- db/issue-hpy-34885-delta-data-release.sql
+++ db/issue-hpy-34885-delta-data-release.sql
@@ -15,5 +15,21 @@ update `eh_var_fields` set name = 'financingDemandItemId' where id = @eecid;
 UPDATE `eh_var_fields` set field_type = 'Long' where name in ('buyOrLeaseItemId','financingDemandItemId',
 'dropBox1','dropBox2','dropBox3','dropBox4','dropBox5','dropBox6','dropBox7','dropBox8','dropBox9');
 
+--end
+
+-- AUTHOR: 黄鹏宇 20180801
+-- REMARK:数据导入导出使用
+-- 购买/租赁
+UPDATE `eh_var_fields` SET NAME='dropBox1ItemId' where name = 'dropBox1';
+UPDATE `eh_var_fields` SET NAME='dropBox1ItemId' where name = 'dropBox1';
+UPDATE `eh_var_fields` SET NAME='dropBox2ItemId' where name = 'dropBox2';
+UPDATE `eh_var_fields` SET NAME='dropBox3ItemId' where name = 'dropBox3';
+UPDATE `eh_var_fields` SET NAME='dropBox4ItemId' where name = 'dropBox4';
+UPDATE `eh_var_fields` SET NAME='dropBox5ItemId' where name = 'dropBox5';
+UPDATE `eh_var_fields` SET NAME='dropBox6ItemId' where name = 'dropBox6';
+UPDATE `eh_var_fields` SET NAME='dropBox7ItemId' where name = 'dropBox7';
+UPDATE `eh_var_fields` SET NAME='dropBox8ItemId' where name = 'dropBox8';
+UPDATE `eh_var_fields` SET NAME='dropBox9ItemId' where name = 'dropBox9';
+
 -- END
 -- --------------------- SECTION END ---------------------------------------------------------
--- db/issue-hpy-34885-delta-schema.sql
+++ db/issue-hpy-34885-delta-schema.sql
@@ -60,11 +60,9 @@ ALTER TABLE `eh_enterprise_customers` MODIFY COLUMN `drop_box8` BIGINT;
 ALTER TABLE `eh_enterprise_customers` MODIFY COLUMN `drop_box9` BIGINT;
 -- end
 
--- --------------------- SECTION BEGIN -------------------------------------------------------
--- ENV: ALL
 -- DESCRIPTION: 此SECTION放所有域空间都需要执行的脚本，包含基线、独立部署、研发数据等环境
 -- AUTHOR: 黄鹏宇 20180802
--- REMARK:修复客户下拉框字段类型
+-- REMARK:修复客户下拉框字段类型0
 ALTER TABLE `eh_enterprise_customers` CHANGE `drop_box1` `drop_box1_item_id` BIGINT;
 ALTER TABLE `eh_enterprise_customers` CHANGE `drop_box2` `drop_box2_item_id` BIGINT;
 ALTER TABLE `eh_enterprise_customers` CHANGE `drop_box3` `drop_box3_item_id` BIGINT;
