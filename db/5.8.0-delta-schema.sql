e1042959f6095276206eec00ccbf918e39795568
liangyanlong <yanlong.liang@zuolin.com>
2018-08-02 15:13:27 +0800

    5.8.0

--- db/5.8.0-delta-schema.sql
+++ db/5.8.0-delta-schema.sql
@@ -33,3 +33,10 @@ CREATE TABLE `eh_payment_bill_attachments` (
 ALTER TABLE `eh_forum_posts` ADD COLUMN `min_quantity` INT(11) COMMENT '最低限制人数';
 ALTER TABLE `eh_activities` ADD COLUMN `min_quantity` INT(11) COMMENT '最低限制人数';
 -- END BY yanlong.liang
+
+-- 通用脚本
+-- add by yanlong.liang 20180719
+-- 导出中心增加阅读状态和下载次数
+ALTER TABLE `eh_tasks` ADD COLUMN `read_status` TINYINT(4) COMMENT '阅读状态';
+ALTER TABLE `eh_tasks` ADD COLUMN `download_times` INT(11) COMMENT '下载次数';
+-- end
