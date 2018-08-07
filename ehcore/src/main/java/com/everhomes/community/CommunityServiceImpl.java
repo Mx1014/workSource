f276f62754adde295fcff1ebf89c51cac554d79b
steve <cen.tang@zuolin.com>
2018-08-07 16:31:09 +0800

    issue-30696:fix bug

--- ehcore/src/main/java/com/everhomes/community/CommunityServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/community/CommunityServiceImpl.java
@@ -253,7 +253,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.web.multipart.MultipartFile;
 
