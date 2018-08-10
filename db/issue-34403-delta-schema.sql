a2038383478ca648cfac2ae833055346022755af
liangyanlong <yanlong.liang@zuolin.com>
2018-08-10 11:37:16 +0800

    issue-34403:项目和表单表字段修改.

--- db/issue-34403-delta-schema.sql
+++ db/issue-34403-delta-schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE `eh_community_general_form`(
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'id of the record',
   `namespace_id` INTEGER NOT NULL COMMENT '域空间ID',
   `community_id` BIGINT COMMENT '项目ID',
-  `form_id` BIGINT COMMENT '表单ID',
+  `form_origin_id` BIGINT COMMENT '表单formOriginID',
   `type` VARCHAR(32) COMMENT '类型',
   PRIMARY KEY (`id`)
 )ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '表单与项目关系表';
