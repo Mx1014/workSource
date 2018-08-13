4523dda54e45d4d3cc53221071ed8b182d4dc28f
pengyu.huang <pengyu.huang@zuolin.com>
2018-08-13 20:45:53 +0800

    SQL

--- db/issue-hpy-33306-delta-schema.sql
+++ db/issue-hpy-33306-delta-schema.sql
@@ -4,6 +4,7 @@
 -- AUTHOR: 黄鹏宇 2018-8-13
 -- REMARK: 增加导入错误日志表
 
+
 CREATE TABLE `eh_sync_data_errors` (
   `id` BIGINT NOT NULL,
   `namespace_id` INTEGER,
@@ -12,9 +13,7 @@ CREATE TABLE `eh_sync_data_errors` (
   `owner_id` BIGINT NOT NULL COMMENT '错误对应的id，如：contractId，对应同步的任务类型',
   `owner_type` VARCHAR(64),
   `error_message` VARCHAR(512) NOT NULL COMMENT '发生错误的信息',
-  `version` BIGINT NOT NULL COMMENT '同步版本';
-  `create_time` DATETIME,
-  `update_time` DATETIME ON UPDATE CURRENT_TIMESTAMP,
+  `task_id` BIGINT NOT NULL COMMENT '同步版本';
 
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
