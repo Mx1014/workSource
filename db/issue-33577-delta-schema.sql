f918d699a85a9d79d69c8be3f21eef73c89ca1e8
wuhan <han.wu@zuolin.com>
2018-08-15 10:07:50 +0800

    issue-33577: update_time改成update_date

--- db/issue-33577-delta-schema.sql
+++ db/issue-33577-delta-schema.sql
@@ -1,7 +1,7 @@
 -- 通用脚本
 -- ADD BY wh
 -- ISSUE-33577 增加update_time 给punch_logs表(为金蝶对接接口提供)
-ALTER TABLE eh_punch_logs ADD COLUMN `update_time` DATETIME ;
+ALTER TABLE eh_punch_logs ADD COLUMN `update_date` DATE ;
 
 
 -- ISSUE-33577 END
