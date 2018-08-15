97af9d68acfe48923c9463744e2638351032c9c7
st.zheng <st.zheng@zuolin.com>
2018-08-15 17:22:23 +0800

    园区入驻标准版

--- ehcore/src/main/java/com/everhomes/portal/LeaseProjectPublicshHandler.java
+++ ehcore/src/main/java/com/everhomes/portal/LeaseProjectPublicshHandler.java
@@ -12,6 +12,7 @@ import com.everhomes.techpark.expansion.EnterpriseLeaseIssuerProvider;
 import com.everhomes.techpark.expansion.LeaseProject;
 import com.everhomes.techpark.expansion.LeasePromotionConfig;
 import com.everhomes.util.StringHelper;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,22 +63,84 @@ public class LeaseProjectPublicshHandler implements PortalPublishHandler {
     }
 
     private void updateConfig(Integer namespaceId, LeaseProjectInstanceConfig config) {
-        if (config.getHideAddressFlag() == null)
-            config.setHideAddressFlag((byte) 0);
-        LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.HIDE_ADDRESS_FLAG.getCode(),
-                config.getCategoryId().longValue());
-        if (!(config.getHideAddressFlag() == 0 && config2 == null)) {
-            if (config2 == null){
-                LeasePromotionConfig newConfig = new LeasePromotionConfig();
-                newConfig.setNamespaceId(namespaceId);
-                newConfig.setCategoryId(config.getCategoryId().longValue());
-                newConfig.setConfigName(LeasePromotionConfigType.HIDE_ADDRESS_FLAG.getCode());
-                newConfig.setConfigValue(config.getHideAddressFlag().toString());
-                enterpriseLeaseIssuerProvider.createLeasePromotionConfig(newConfig);
-            }else {
-                config2.setConfigValue(config.getHideAddressFlag().toString());
-                enterpriseLeaseIssuerProvider.updateLeasePromotionConfig(config2);
+        if (config.getHideAddressFlag() != null) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.HIDE_ADDRESS_FLAG.getCode(),
+                    config.getCategoryId().longValue());
+            if (!(config.getHideAddressFlag() == 0 && config2 == null)) {
+                updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.HIDE_ADDRESS_FLAG.getCode(),
+                        config.getHideAddressFlag().toString(),config2);
             }
+        }
+
+        if (config.getBuildingIntroduceFlag() != null) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.BUILDING_INTRODUCE_FLAG.getCode(),
+                    config.getCategoryId().longValue());
+            if (!(config.getBuildingIntroduceFlag() == 0 && config2 == null)) {
+                updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.BUILDING_INTRODUCE_FLAG.getCode(),
+                        config.getBuildingIntroduceFlag().toString(),config2);
+            }
+        }
+
+        if (config.getRenewFlag() != null) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.RENEW_FLAG.getCode(),
+                    config.getCategoryId().longValue());
+            if (!(config.getRenewFlag() == 0 && config2 == null)) {
+                updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.RENEW_FLAG.getCode(),
+                        config.getRenewFlag().toString(),config2);
+            }
+        }
+
+        if (config.getAreaSearchFlag() != null) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.AREA_SEARCH_FLAG.getCode(),
+                    config.getCategoryId().longValue());
+            if (!(config.getAreaSearchFlag() == 0 && config2 == null)) {
+                updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.AREA_SEARCH_FLAG.getCode(),
+                        config.getAreaSearchFlag().toString(),config2);
+            }
+        }
+
+        if (config.getRentAmountFlag() != null) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.RENT_AMOUNT_FLAG.getCode(),
+                    config.getCategoryId().longValue());
+            if (!(config.getRentAmountFlag() == 0 && config2 == null)) {
+                updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.RENT_AMOUNT_FLAG.getCode(),
+                        config.getRentAmountFlag().toString(),config2);
+            }
+        }
+
+        if (!StringUtils.isBlank(config.getRentAmountUnit()) ) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.RENT_AMOUNT_UNIT.getCode(),
+                    config.getCategoryId().longValue());
+            updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.RENT_AMOUNT_UNIT.getCode(),
+                    config.getRentAmountUnit(),config2);
+        }
+
+        if (!StringUtils.isBlank(config.getDisplayNameStr()) ) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.DISPLAY_NAME_STR.getCode(),
+                    config.getCategoryId().longValue());
+            updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.DISPLAY_NAME_STR.getCode(),
+                    config.getDisplayNameStr(),config2);
+        }
+
+        if (!StringUtils.isBlank(config.getDisplayOrderStr()) ) {
+            LeasePromotionConfig config2 = enterpriseLeaseIssuerProvider.findLeasePromotionConfig(namespaceId, LeasePromotionConfigType.DISPLAY_ORDER_STR.getCode(),
+                    config.getCategoryId().longValue());
+            updatePromotionConfig(namespaceId,config.getCategoryId().longValue(),LeasePromotionConfigType.DISPLAY_ORDER_STR.getCode(),
+                    config.getDisplayOrderStr(),config2);
+        }
+    }
+
+    private void updatePromotionConfig(Integer namespaceId,Long categoryId,String configName,String configValue,LeasePromotionConfig config){
+        if (config == null) {
+            LeasePromotionConfig newConfig = new LeasePromotionConfig();
+            newConfig.setNamespaceId(namespaceId);
+            newConfig.setCategoryId(categoryId);
+            newConfig.setConfigName(configName);
+            newConfig.setConfigValue(configValue);
+            enterpriseLeaseIssuerProvider.createLeasePromotionConfig(newConfig);
+        } else {
+            config.setConfigValue(configValue);
+            enterpriseLeaseIssuerProvider.updateLeasePromotionConfig(config);
         }
     }
 
