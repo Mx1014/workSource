add8c38430941f2657dd5f101ac6d8d2ed900cfe
zhiwei.zhang <zhiwei.zhang@zuolin.com>
2018-08-15 09:48:25 +0800

    issue-33552 merge to 5.8.0

--- db/5.8.0-delta-data-release.sql
+++ db/5.8.0-delta-data-release.sql
@@ -155,7 +155,15 @@ VALUES (@zuolin_menu_id, '企业账单', @zuolin_menu_parent_id, NULL, @data_typ
 INSERT INTO `eh_web_menus`(`id`, `name`, `parent_id`, `icon_url`, `data_type`, `leaf_flag`, `status`, `path`, `type`, `sort_num`, `module_id`, `level`, `condition_type`, `category`, `config_type`) 
 VALUES (@organization_menu_id, '企业账单', @organization_menu_parent_id, NULL, @data_type, 1, 2, @organization_menu_path, 'organization', 220, @module_id, 3, 'system', 'module', NULL);
 
+-- AUTHOR: 张智伟
+-- REMARK: 企业公告分享提示文案
+SET @locale_string_id = (SELECT MAX(id) FROM `eh_locale_strings`);
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@locale_string_id := @locale_string_id + 1), 'enterprise.notice', '10000', 'zh_CN', '未设置公告分享链接');
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@locale_string_id := @locale_string_id + 1), 'enterprise.notice', '10001', 'zh_CN', '该公告设置了保密');
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@locale_string_id := @locale_string_id + 1), 'enterprise.notice', '10002', 'zh_CN', '该公告已被删除');
+INSERT INTO `eh_locale_strings` (`id`, `scope`, `code`, `locale`, `text`) VALUES ((@locale_string_id := @locale_string_id + 1), 'enterprise.notice', '10003', 'zh_CN', '分享链接已失效');
 
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('enterprise.notice.share.url', '/announcement/build/index.html?ns=%s&noticeToken=%s', '企业公告分享uri', '0', NULL, '1');
 
 -- --------------------- SECTION END ---------------------------------------------------------
 
