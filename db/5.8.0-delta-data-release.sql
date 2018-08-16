7f0271c22a5c963d1858266f6714becd96e810ac
liangyanlong <yanlong.liang@zuolin.com>
2018-08-16 11:50:39 +0800

    5.8.0: 用户认证v3.6

--- db/5.8.0-delta-data-release.sql
+++ db/5.8.0-delta-data-release.sql
@@ -368,6 +368,22 @@ ON s.owner_id=e.enterprise_id AND s.user_id=e.user_id AND s.punch_month=e.punch_
 SET s.exception_request_counts=e.exception_request_counts,s.punch_exception_request_count=e.exception_request_counts
 WHERE s.user_id>0;
 
+-- AUTHOR: 梁燕龙  20180816
+-- REMARK: ISSUE-34179: 用户认证v3.6
+-- 权限
+INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`) VALUES (42007, null, '查看审核列表', '用户认证查看审核列表', NULL);
+INSERT INTO `eh_acl_privileges` (`id`, `app_id`, `name`, `description`, `tag`) VALUES (42008, null, '审核权限', '用户认证审核权限', NULL);
+
+set @mp_id = (select MAX(id) from eh_service_module_privileges);
+INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
+VALUES (@mp_id:=@mp_id+1, '35000', '0', 42007, '查看审核列表', '0', NOW());
+INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
+VALUES (@mp_id:=@mp_id+1, '35000', '0', 42008, '审核权限', '0', NOW());
+-- 将用户认证移动到跟项目相关
+UPDATE eh_service_modules SET module_control_type = 'community_control' WHERE id =35000;
+UPDATE eh_service_module_apps SET module_control_type = 'community_control' WHERE module_id = 35000;
+UPDATE eh_authorizations SET owner_type = 'EhAll' WHERE auth_id = 35000;
+UPDATE eh_authorizations SET module_control_type = 'community_control' WHERE auth_id = 35000;
 -- --------------------- SECTION END ---------------------------------------------------------
 
 
--- db/backup/issue-34179-delta-data-release.sql
+++ db/backup/issue-34179-delta-data-release.sql
@@ -8,4 +8,9 @@ INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`,
 VALUES (@mp_id:=@mp_id+1, '35000', '0', 42007, '查看审核列表', '0', NOW());
 INSERT INTO `eh_service_module_privileges` (`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`)
 VALUES (@mp_id:=@mp_id+1, '35000', '0', 42008, '审核权限', '0', NOW());
+
+UPDATE eh_service_modules SET module_control_type = 'community_control' WHERE id =35000;
+UPDATE eh_service_module_apps SET module_control_type = 'community_control' WHERE module_id = 35000;
+UPDATE eh_authorizations SET owner_type = 'EhAll' WHERE auth_id = 35000;
+UPDATE eh_authorizations SET module_control_type = 'community_control' WHERE auth_id = 35000;
 -- END
