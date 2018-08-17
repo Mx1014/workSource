3b27a927e681f872ef128fa197f5c35bca0f4232
liangyanlong <yanlong.liang@zuolin.com>
2018-08-17 11:22:30 +0800

    issue-35386: 圳智慧url。

--- db/issue-35386-delta-data-release.sql
+++ db/issue-35386-delta-data-release.sql
@@ -1,6 +1,6 @@
 -- 圳智慧beta
 -- addy yanlong.liang 20180814
 INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`)
-VALUES ('zhenzhihui.url', 'http://120.132.117.22:8016/ZHYQ/restservices/LEAPAuthorize/attributes//query?TICKET=', '圳智慧认证url', '999931', NULL, '1');
+VALUES ('zhenzhihui.url', 'http://120.132.117.22:8016/ZHYQ/restservices/LEAPAuthorize/attributes/query?TICKET=', '圳智慧认证url', '999931', NULL, '1');
 INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`)
 VALUES ('zhenzhihui.appkey', 'c00f02aac30d0822', '圳智慧APPKEY', '999931', NULL, '1');
