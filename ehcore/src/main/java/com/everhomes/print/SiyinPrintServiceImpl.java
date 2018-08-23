8e457181249ed51a38aeb9dc89cc64a0fec7c2a3
huangmingbo <mingbo.huang@zuolin.com>
2018-08-23 19:01:41 +0800

    fix

--- ehcore/src/main/java/com/everhomes/print/SiyinPrintServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/print/SiyinPrintServiceImpl.java
@@ -754,8 +754,9 @@ public class SiyinPrintServiceImpl implements SiyinPrintService {
 //        preOrderCommand.setBusinessOrderNumber(generateBizOrderNum(accountCode,OrderType.OrderTypeEnum.PRINT_ORDER.getPycode(),order.getOrderNo()));
         preOrderCommand.setBusinessPayerType(payerType.getCode());
         preOrderCommand.setBusinessPayerId(String.valueOf(UserContext.currentUserId()));
-//        String businessPayerParams = getBusinessPayerParams(cmd);
-//        preOrderCommand.setBusinessPayerParams(businessPayerParams);
+        String businessPayerParams = getBusinessPayerParams(cmd);
+        preOrderCommand.setBusinessPayerParams(businessPayerParams);
+        
 
        // preOrderCommand.setPaymentPayeeType(billGroup.getBizPayeeType()); 不填会不会有问题?
         preOrderCommand.setPaymentPayeeId(bizPayeeId); //不知道填什么
@@ -783,7 +784,30 @@ public class SiyinPrintServiceImpl implements SiyinPrintService {
         return preOrderCommand;
     }
 	
-    private Long changePayAmount(BigDecimal amount){
+    private String getBusinessPayerParams(PayPrintOrderCommandV2 cmd) {
+
+
+        Long businessPayerId = UserContext.currentUserId();
+
+
+        UserIdentifier buyerIdentifier = userProvider.findUserIdentifiersOfUser(businessPayerId, cmd.getNamespaceId());
+        String buyerPhone = null;
+        if(buyerIdentifier != null) {
+            buyerPhone = buyerIdentifier.getIdentifierToken();
+        }
+        // 找不到手机号则默认一个
+        if(buyerPhone == null || buyerPhone.trim().length() == 0) {
+            buyerPhone = configurationProvider.getValue(UserContext.getCurrentNamespaceId(), "gorder.default.personal_bind_phone", "");
+        }
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("businessPayerPhone", buyerPhone);
+        return StringHelper.toJsonString(map);
+    
+	}
+
+
+	private Long changePayAmount(BigDecimal amount){
 
         if(amount == null){
             return 0L;
