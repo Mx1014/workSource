f6ffb357568ad4b0af13acec667408fd03aea988
yanjun <jun.yan@zuolin.com>
2018-08-23 17:29:17 +0800

    issue null

--- ehrest/src/main/java/com/everhomes/rest/launchpad/ListAllAppsResponse.java
+++ ehrest/src/main/java/com/everhomes/rest/launchpad/ListAllAppsResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * <ul>
  *     <li>defaultDtos: 首页的Items信息（默认、自定义），参考{@link AppDTO}</li>
- *     <li>categoryDtos: “全部”-各个分类的item信息汇总。“更多”-所有的信息，List的size为1，里面包括defaultItemDtos。 参考{@link LaunchPadCategoryDTO}</li>
+ *     <li>categoryDtos: “全部”-各个分类的item信息汇总。“更多”-所有的信息，List的size为1，里面包括defaultDtos。 参考{@link LaunchPadCategoryDTO}</li>
  * </ul>
  */
 public class ListAllAppsResponse {
