97c92c48604445a7abf02e45b003f1c47ed34209
nan.rong <nan.rong@zuolin.com>
2018-08-24 11:45:24 +0800

    sql release fix

--- db/workReport-1.2-delta-data-release.sql
+++ db/workReport-1.2-delta-data-release.sql
@@ -1,3 +1,7 @@
 UPDATE `eh_locale_templates` SET `text`='${applierName}给你提交了${reportName}（${reportTime}）' WHERE `scope`='work.report.notification' AND `code`='1';
-
 UPDATE `eh_locale_templates` SET `text`='${applierName}更新了Ta的${reportName}（${reportTime}）' WHERE `scope`='work.report.notification' AND `code`='2';
+
+
+UPDATE `eh_work_reports` SET `validity_setting` = '{"endTime":"10:00","endType":1,"startTime":"15:00","startType":0}' WHERE `report_type` = 0;
+UPDATE `eh_work_reports` SET `validity_setting` = '{"endMark":"1","endTime":"10:00","endType":1,"startMark":"5","startTime":"15:00","startType":0}' WHERE `report_type` = 1;
+UPDATE `eh_work_reports` SET `validity_setting` = '{"endMark":"1","endTime":"10:00","endType":1,"startMark":"31","startTime":"15:00","startType":0}' WHERE `report_type` = 2;
