d9ac26600ede749e06fda0121c5b6fc0b2b65f7d
huangmingbo <mingbo.huang@zuolin.com>
2018-08-27 11:35:52 +0800

    issue-36529 添加人性化返回信息

--- ehrest/src/main/java/com/everhomes/rest/express/ExpressServiceErrorCode.java
+++ ehrest/src/main/java/com/everhomes/rest/express/ExpressServiceErrorCode.java
@@ -11,6 +11,8 @@ public interface ExpressServiceErrorCode {
 	int EMPTY_WORK_FLOW = 180806;  //没有启用工作流
 	int ERROR_CREATE_FLOWCASE = 180807;  //创建工作流失败
 	int ERROR_CREATE_USER_ACCOUNT = 180808;  //创建个人付款方失败
+	int ERROR_CREATE_REPEAT_PAYEE_ACCOUNT = 180809; //该收款账户已存在，请勿重复添加
+	int ERROR_PAYEE_ACCOUNT_ID_NOT_EXIST = 180810; //需要更新的收款账户ID不存在
 
 	String WORK_FLOW_NAME = "workflowtitle";  //工作流名称
 
