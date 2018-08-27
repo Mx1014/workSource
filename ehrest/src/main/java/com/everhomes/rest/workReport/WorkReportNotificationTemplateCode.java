54e4dbca015692b5a302c433b74b3d2951a00a60
nan.rong <nan.rong@zuolin.com>
2018-08-27 19:01:37 +0800

    code update

--- ehrest/src/main/java/com/everhomes/rest/workReport/WorkReportNotificationTemplateCode.java
+++ ehrest/src/main/java/com/everhomes/rest/workReport/WorkReportNotificationTemplateCode.java
@@ -15,20 +15,7 @@ public interface WorkReportNotificationTemplateCode {
 	int COMMENT_MESSAGE_5 = 5; //	${评论人} 在你的 ${汇报类型}（${汇报日期}） 中 发表了评论
 
 
-
 	int POST_WORK_REPORT_VAL = 101;	//给接收人发通知：提交工作汇报
 
 	int UPDATE_WORK_REPORT_VAL = 102;   //给接收人发通知：更新工作汇报
-
-/*	int AUTHOR_REPLY_WORK_REPORT_VAL = 3;   //给读者发通知：作者回复了你
-
-	int AUTHOR_COMMENT_WORK_REPORT_VAL = 4;   //给接收人发通知：作者发表了评论
-
-	int READER_REPLY_WORK_REPORT_VAL_FOR_AUTHOR   = 5;   //给作者发通知：读者回复了你
-
-	int READER_REPLY_WORK_REPORT_VAL_FOR_READER = 6;   //给读者发通知：读者在作者的汇报中回复了你
-
-	int READER_COMMENT_WORK_REPORT_VAL = 7;   //给作者发通知：读者发表了评论*/
-
-
 }
