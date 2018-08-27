ea45dd950fd99434d8f0fc79b7c03affb4913ea5
cx.yang <cx.yang@zuolin.com>
2018-08-27 10:42:48 +0800

    issue-30655 瑞安CM对接增加数据库配置

--- db/issue-customer4.1-delta-data-release.sql
+++ db/issue-customer4.1-delta-data-release.sql
@@ -5,9 +5,9 @@
 -- REMARK: 配置客户V4.1瑞安CM对接的访问地址
 SET @id = ifnull((SELECT MAX(id) FROM `eh_configurations`),0);
 INSERT INTO `eh_configurations`(`id`, `name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) 
-VALUES (@id := @id + 1, 'RuiAnCM.sync.url', 'http://10.50.12.39/cm/WebService/OfficeApp-CM/OfficeApp_CMService.asmx', '瑞安新天地对接的第三方地址', 0, NULL, 1);
-
-
+	VALUES (@id := @id + 1, 'RuiAnCM.sync.url', 'http://10.50.12.39/cm/WebService/OfficeApp-CM/OfficeApp_CMService.asmx', '瑞安新天地对接的第三方地址', 0, NULL, 1);
+INSERT INTO `eh_configurations`(`id`, `name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) 
+	VALUES (@id:=@id+1, 'contractService', '999929', NULL, 999929, NULL, 1);
 
 
 -- --------------------- SECTION END ---------------------------------------------------------
