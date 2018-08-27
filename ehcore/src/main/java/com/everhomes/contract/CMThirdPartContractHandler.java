df2612a6d700f1b345f66c61bf626630521d565d
pengyu.huang <pengyu.huang@zuolin.com>
2018-08-27 11:31:31 +0800

    同步时将数据存入备份表

--- ehcore/src/main/java/com/everhomes/contract/CMThirdPartContractHandler.java
+++ ehcore/src/main/java/com/everhomes/contract/CMThirdPartContractHandler.java
@@ -222,7 +222,7 @@ public class CMThirdPartContractHandler implements ThirdPartContractHandler{
         CMSyncObject cmSyncObject =
                 (CMSyncObject) StringHelper.fromJsonString(enterprises, CMSyncObject.class);
 
-
+        syncData(cmSyncObject, DataType.CONTRACT.getCode(), communityIdentifier);
 
         if(SUCCESS_CODE.equals(cmSyncObject.getErrorCode())) {
 
