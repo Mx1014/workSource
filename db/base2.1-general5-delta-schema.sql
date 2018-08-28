b0e32c59038744a3648b6a447c722f6019a1d18c
shiheng.ma <shiheng.ma@zuolin.com>
2018-08-28 10:35:27 +0800

    issue-35084:工位预订通用修改

--- db/base2.1-general5-delta-schema.sql
+++ db/base2.1-general5-delta-schema.sql
@@ -120,3 +120,19 @@ MODIFY COLUMN `source_id`  bigint(20) NULL AFTER `owner_type`;
 ALTER TABLE `eh_office_cubicle_cities` ADD COLUMN `org_id` BIGINT(20) DEFAULT NULL COMMENT '所属管理公司Id';
 ALTER TABLE `eh_office_cubicle_cities` ADD COLUMN `owner_type` VARCHAR(128) DEFAULT NULL COMMENT '项目类型';
 ALTER TABLE `eh_office_cubicle_cities` ADD COLUMN `owner_id` BIGINT(20) DEFAULT NULL COMMENT '项目Id';
+
+CREATE TABLE `eh_office_cubicle_configs` (
+  `id` BIGINT NOT NULL,
+  `namespace_id` INTEGER NOT NULL DEFAULT 0,
+  `org_id` BIGINT NOT NULL DEFAULT 0 COMMENT '管理公司Id',
+  `owner_type` VARCHAR(64) NOT NULL,
+  `owner_id` BIGINT NOT NULL,
+  `customize_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '0: general configure, 1:customize configure',
+  `status` TINYINT NOT NULL DEFAULT 2 COMMENT '0: invalid, 1:waiting_for_approval, 2: valid',
+  `create_time` DATETIME(3) NOT NULL COMMENT 'record create time',
+  `creator_uid` BIGINT NOT NULL DEFAULT 0,
+  `update_time` DATETIME(3) COMMENT 'record update time',
+  `updater_uid` BIGINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+-- END 工位预订
