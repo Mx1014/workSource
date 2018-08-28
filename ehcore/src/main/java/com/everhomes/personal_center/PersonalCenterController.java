bc0fddd4c0b1860b49b0b3510491779e45349cf0
liangyanlong <yanlong.liang@zuolin.com>
2018-08-28 10:31:16 +0800

    issue-36203

--- ehcore/src/main/java/com/everhomes/personal_center/PersonalCenterController.java
+++ ehcore/src/main/java/com/everhomes/personal_center/PersonalCenterController.java
@@ -1,5 +1,13 @@
 // @formatter:off
 package com.everhomes.personal_center;
 
-public class PersonalCenterController {
+import com.everhomes.controller.ControllerBase;
+import com.everhomes.discover.RestDoc;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestDoc(value="personal center form controller", site="core")
+@RestController
+@RequestMapping("/personal_center")
+public class PersonalCenterController extends ControllerBase{
 }
