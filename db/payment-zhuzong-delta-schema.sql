ff58d8f7a77749db8d68dbd04276afdc7879d377
st.zheng <st.zheng@zuolin.com>
2018-08-28 10:52:22 +0800

    一卡通

--- db/payment-zhuzong-delta-schema.sql
+++ db/payment-zhuzong-delta-schema.sql
@@ -1,4 +1,7 @@
 -- add by st.zheng 增加一卡通账号设置
+ALTER TABLE `eh_parking_recharge_orders`
+ADD COLUMN `biz_order_no`  varchar(128) NULL AFTER `order_no`;
+
 CREATE TABLE `eh_payment_card_accounts` (
 `id`  bigint(20) NOT NULL ,
 `namespace_id`  int(11) NULL ,
@@ -8,7 +11,4 @@ CREATE TABLE `eh_payment_card_accounts` (
 `craete_time`  datetime NULL ON UPDATE CURRENT_TIMESTAMP ,
 PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `eh_parking_recharge_orders`
-ADD COLUMN `biz_order_no`  varchar(128) NULL AFTER `order_no`;
 
