6ccdc8c02f66abcbc6e40729096bd6e2ab76ad58
cx.yang <cx.yang@zuolin.com>
2018-08-30 16:02:12 +0800

    物业缴费V6.0收费项配置支持新增

--- db/asset6.0-delta-schema.sql
+++ db/asset6.0-delta-schema.sql
@@ -1,6 +1,7 @@
 -- AUTHOR: 杨崇鑫
 -- REMARK: 物业缴费V6.0 收费项配置可手动新增
 ALTER TABLE `eh_payment_charging_items` ADD COLUMN `namespace_id` INTEGER COMMENT '增加域空间ID作为标识';
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `owner_id` BIGINT COMMENT '增加园区ID作为标识';
 
 
 
