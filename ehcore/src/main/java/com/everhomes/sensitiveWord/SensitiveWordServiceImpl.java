3c1e6096457c5db58a6f854c2f9878d7c9831798
liangyanlong <yanlong.liang@zuolin.com>
2018-08-31 14:47:24 +0800

    issue-36960: 构建敏感词库失败后，置为Null,不进行敏感词校验.

--- ehcore/src/main/java/com/everhomes/sensitiveWord/SensitiveWordServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/sensitiveWord/SensitiveWordServiceImpl.java
@@ -84,6 +84,8 @@ public class SensitiveWordServiceImpl implements SensitiveWordService, Applicati
         }
         if (map.size() > 0) {
             acdat.build(map);
+        }else {
+            acdat = null;
         }
         fileUrl = cmd.getUrl();
     }
