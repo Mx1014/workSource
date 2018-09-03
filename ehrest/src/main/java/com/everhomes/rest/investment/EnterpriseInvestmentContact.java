568f423574b16542582c754bf640890acb45bf10
pengyu.huang <pengyu.huang@zuolin.com>
2018-09-03 09:56:52 +0800

    增加个command

---
+++ ehrest/src/main/java/com/everhomes/rest/investment/EnterpriseInvestmentContact.java
@@ -0,0 +1,24 @@
+package com.everhomes.rest.investment;
+
+import java.sql.Timestamp;
+
+public class EnterpriseInvestmentContact {
+
+    private Long id;
+    private Integer namespaceId;
+    private Long communityId;
+    private String name;
+    private Long phoneNumber;
+    private String email;
+    private String position;
+    private String address;
+    private Byte type;
+    private Byte originType;
+    private Byte status;
+    private Long customerId;
+    private Timestamp createTime;
+    private String createBy;
+    private Timestamp operatorTime;
+    private String operatorBy;
+
+}
