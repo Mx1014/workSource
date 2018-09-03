f94cc1d26f46c89a42cae72de62a9d4d2d469ea7
cx.yang <cx.yang@zuolin.com>
2018-09-03 15:05:06 +0800

    issue-30557 新增收费项配置增加园区类型、多应用入口字段

--- db/asset6.0-delta-schema.sql
+++ db/asset6.0-delta-schema.sql
@@ -2,6 +2,8 @@
 -- REMARK: 物业缴费V6.0 收费项配置可手动新增
 ALTER TABLE `eh_payment_charging_items` ADD COLUMN `namespace_id` INTEGER COMMENT '增加域空间ID作为标识';
 ALTER TABLE `eh_payment_charging_items` ADD COLUMN `owner_id` BIGINT COMMENT '增加园区ID作为标识';
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `owner_type` VARCHAR(64)  COMMENT '增加园区ID作为标识';
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `category_id` BIGINT COMMENT '多入口应用id';
 
 
 
