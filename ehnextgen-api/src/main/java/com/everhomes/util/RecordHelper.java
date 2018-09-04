31d7b20744bc479e6826400f18520c10119ca81a
xq.tian <xq.tian@zuolin.com>
2018-09-04 16:17:06 +0800

    record helper

--- ehnextgen-api/src/main/java/com/everhomes/util/RecordHelper.java
+++ ehnextgen-api/src/main/java/com/everhomes/util/RecordHelper.java
@@ -1,13 +1,17 @@
 package com.everhomes.util;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
 import org.jooq.Record;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class RecordHelper {
 
-	public static <T> T convert(Record record, Class<T> clazz){
+	/*public static <T> T convert1(Record record, Class<T> clazz){
 		if (record == null) {
 			return null;
 		}
@@ -30,8 +34,33 @@ public class RecordHelper {
 		} catch (InstantiationException | IllegalAccessException e) {
 			return null;
 		}
-	}
+	}*/
 	
+	public static <T> T convert(Record record, Class<T> clazz){
+		if (record == null) {
+			return null;
+		}
+
+		try {
+			T t = clazz.newInstance();
+			BeanInfo beanInfo = Introspector.getBeanInfo(clazz, Object.class);
+			PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+			if (pds != null && pds.length > 0) {
+				for (PropertyDescriptor pd : pds) {
+					Method wm = pd.getWriteMethod();
+					Object value = getValue(record, pd.getName(), pd.getPropertyType());
+					if (value != null) {
+						wm.invoke(t, value);
+					}
+				}
+			}
+			return t;
+		} catch (InstantiationException | IllegalAccessException
+				|IntrospectionException | InvocationTargetException e) {
+			return null;
+		}
+	}
+
 	private static <T> T getValue(Record record, String fieldName, Class<T> clazz){
 		String column = camelToUnderline(fieldName);
 		try {
