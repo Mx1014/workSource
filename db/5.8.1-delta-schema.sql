aa9f5243ba09d54d045e297c94fb4f5a4645a10c
liangyanlong <yanlong.liang@zuolin.com>
2018-09-04 09:51:21 +0800

    5.8.1

--- db/5.8.1-delta-schema.sql
+++ db/5.8.1-delta-schema.sql
@@ -44,4 +44,17 @@ ALTER TABLE `eh_pm_task_orders` ADD COLUMN `general_order_id` varchar(64) NULL C
 ALTER TABLE `eh_siyin_print_orders` ADD COLUMN `general_order_id` VARCHAR(64) NULL COMMENT '统一订单系统订单编号' ;
 ALTER TABLE `eh_express_orders` ADD COLUMN `general_order_id` VARCHAR(64) NULL COMMENT '统一订单系统订单编号' ;
 
+-- AUTHOR:梁燕龙 20180904
+-- REMARK:报名表增加表单ID字段
+ALTER TABLE `eh_activity_roster` ADD COLUMN `form_id` BIGINT COMMENT '表单ID';
+-- AUTHOR:梁燕龙 20180904
+-- REMARK:表单与项目关联表
+CREATE TABLE `eh_community_general_form`(
+  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL COMMENT '域空间ID',
+  `community_id` BIGINT COMMENT '项目ID',
+  `form_origin_id` BIGINT COMMENT '表单formOriginID',
+  `type` VARCHAR(32) COMMENT '类型',
+  PRIMARY KEY (`id`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '表单与项目关系表';
 -- --------------------- SECTION END ---------------------------------------------------------
