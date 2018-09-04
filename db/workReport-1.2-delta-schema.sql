8f3c9574122d8f6ccbbdb6f2ab7b3aeb76ec01fb
nan.rong <nan.rong@zuolin.com>
2018-09-04 14:41:46 +0800

    sql update

--- db/workReport-1.2-delta-schema.sql
+++ db/workReport-1.2-delta-schema.sql
@@ -1,4 +1,9 @@
 ALTER TABLE `eh_work_report_val_receiver_map` ADD COLUMN `organization_id` BIGINT DEFAULT 0 NOT NULL COMMENT 'the orgId for the user' AFTER `namespace_id`;
+ALTER TABLE `eh_work_report_val_receiver_map` ADD COLUMN `report_id` BIGINT DEFAULT 0 NOT NULL COMMENT 'the report id' AFTER `organization_id`;
+ALTER TABLE `eh_work_report_val_receiver_map` ADD COLUMN `reminder_time` DATETIME AFTER `read_status`;
+
+ALTER TABLE `eh_work_report_val_receiver_map` ADD INDEX `i_reminder_time` (`reminder_time`) ;
+
 
 ALTER TABLE `eh_work_reports` ADD COLUMN `validity_setting` VARCHAR(512) COMMENT 'the expiry date of the work report' AFTER `form_version`;
 ALTER TABLE `eh_work_reports` ADD COLUMN `receiver_msg_type` TINYINT NOT NULL DEFAULT 0 COMMENT 'the type of the receiver message settings' AFTER `validity_setting`;
@@ -11,3 +16,4 @@ ALTER TABLE `eh_work_report_vals` ADD COLUMN `receiver_avatar` VARCHAR(1024) COM
 ALTER TABLE `eh_work_report_vals` ADD COLUMN `applier_avatar` VARCHAR(1024) COMMENT 'the avatar of the author' AFTER `receiver_avatar`;
 
 ALTER TABLE `eh_work_report_vals` MODIFY COLUMN `report_time` DATE COMMENT 'the target time of the report';
+
