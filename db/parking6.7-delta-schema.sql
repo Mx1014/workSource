c80d7a5cc435602c22d628019aea8029f63b5965
Zhou.Miao <zhou.miao@zuolin.com>
2018-09-04 14:02:40 +0800

    加入用户须知字段

--- db/parking6.7-delta-schema.sql
+++ db/parking6.7-delta-schema.sql
@@ -1,3 +1,3 @@
 -- AUTHOR: 缪洲
 -- REMARK: 停车缴费V6.7，增加用户须知
-ALTER TABLE `eh_parking_lots` DROP COLUMN `summry` text COMMENT '用户须知';
+ALTER TABLE `eh_parking_lots` ADD COLUMN `summary` text COMMENT '用户须知';
