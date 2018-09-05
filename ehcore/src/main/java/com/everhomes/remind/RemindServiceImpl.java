e15677832158b16cd6ce42170e55b99349788cbf
wuhan <han.wu@zuolin.com>
2018-09-05 16:44:58 +0800

    issue-33943:fixbug 提醒重复的bug

--- ehcore/src/main/java/com/everhomes/remind/RemindServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/remind/RemindServiceImpl.java
@@ -1272,15 +1272,13 @@ public class RemindServiceImpl implements RemindService, ApplicationListener<Con
             repeatRemind.setShareCount(shareMembers.size());
         }
 
-        List<Remind> originSubscribeReminds = remindProvider.findRemindsByTrackRemindIds(Collections.singletonList(originRemind.getId()));
-
         dbProvider.execute(transactionStatus -> {
+            List<Remind> originSubscribeReminds = remindProvider.findRemindsByTrackRemindIds(Collections.singletonList(originRemind.getId()));
             remindProvider.createRemind(repeatRemind);
             remindProvider.batchCreateRemindShare(buildRemindShares(shareMembers, repeatRemind));
             //origin日程点完成后,origin日程被追踪的日程也会重复一份
-            originSubscribeReminds.stream().map(remind -> {
+            originSubscribeReminds.forEach(remind -> {
                 createSubscribeRemind(repeatRemind, remind.getContactName(), remind.getUserId());
-                return null;
             });
 
             return null;
