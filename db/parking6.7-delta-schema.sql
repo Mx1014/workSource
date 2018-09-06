93cf902ceb83dcef56c21b84d7f66efa45430c1a
Zhou.Miao <zhou.miao@zuolin.com>
2018-09-06 16:31:39 +0800

    增加选中订单号置顶字段

--- db/parking6.7-delta-schema.sql
+++ db/parking6.7-delta-schema.sql
@@ -1,3 +1,4 @@
 -- AUTHOR: 缪洲
 -- REMARK: 停车缴费V6.7，增加用户须知
 ALTER TABLE `eh_parking_lots` ADD COLUMN `summary` text COMMENT '用户须知';
+ALTER TABLE `eh_parking_recharge_orders` ADD COLUMN `invoice_top_status` TINYINT COMMENT '0:未选中订单号 1:选中订单号'；
