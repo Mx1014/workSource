e5eb063afdc12782544ad1bd16818287c5fba271
yanjun <jun.yan@zuolin.com>
2018-09-10 12:01:15 +0800

    fix scentoken token

--- ehnextgen-messaging/src/main/java/com/everhomes/user/UserServiceImpl.java
+++ ehnextgen-messaging/src/main/java/com/everhomes/user/UserServiceImpl.java
@@ -3921,7 +3921,7 @@ public class UserServiceImpl implements UserService, ApplicationListener<Context
         Long userId = user.getId();
         Integer namespaceId = UserContext.getCurrentNamespaceId();
 
-        SceneTokenDTO sceneToken = checkSceneToken(userId, cmd.getSceneToken());
+        //SceneTokenDTO sceneToken = checkSceneToken(userId, cmd.getSceneToken());
 
         //先按域空间查，ownerid和ownertype暂时不用
         ListSearchTypesBySceneReponse response = new ListSearchTypesBySceneReponse();
