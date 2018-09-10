b7efe8483cb119b30074e72565f2a47b2c9cd363
cx.yang <cx.yang@zuolin.com>
2018-09-10 20:02:34 +0800

    issue-30557 物业缴费V6.0 账单、费项表增加是否删除状态字段

--- db/asset6.0-delta-schema.sql
+++ db/asset6.0-delta-schema.sql
@@ -10,6 +10,8 @@ ALTER TABLE `eh_payment_bills` ADD COLUMN `can_delete` TINYINT DEFAULT 0 COMMENT
 ALTER TABLE `eh_payment_bills` ADD COLUMN `can_modify` TINYINT DEFAULT 0 COMMENT '0：不可编辑；1：可编辑';
 ALTER TABLE `eh_payment_bill_items` ADD COLUMN `can_delete` TINYINT DEFAULT 0 COMMENT '0：不可删除；1：可删除';
 ALTER TABLE `eh_payment_bill_items` ADD COLUMN `can_modify` TINYINT DEFAULT 0 COMMENT '0：不可编辑；1：可编辑';
-
-
+-- AUTHOR: 杨崇鑫
+-- REMARK: 物业缴费V6.0 账单、费项表增加是否删除状态字段
+ALTER TABLE `eh_payment_bills` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMENT '删除状态：0：已删除；1：正常使用';
+ALTER TABLE `eh_payment_bill_items` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMENT '删除状态：0：已删除；1：正常使用';
 
