8e66aea6ab903dfcf83cf54cfe02b6514df7d6ce
wuhan <han.wu@zuolin.com>
2018-09-12 13:49:30 +0800

    merge issue-36405: 公告1.8

--- db/5.9.0-delta-schema.sql
+++ db/5.9.0-delta-schema.sql
@@ -2,3 +2,11 @@
 -- REMARK: 用户表增加企业ID
 ALTER TABLE `eh_users` ADD COLUMN `company_id` BIGINT COMMENT '公司ID';
 -- END
+
+ 
+-- AUTHOR: 吴寒
+-- REMARK: 公告1.8 修改表结构
+ALTER TABLE `eh_enterprise_notices` ADD COLUMN `stick_flag` TINYINT NOT NULL DEFAULT 0 COMMENT '是否置顶，0-否，1-是';
+ALTER TABLE `eh_enterprise_notices` ADD COLUMN `stick_time` DATETIME;
+-- REMARK: 公告1.8 修改表结构
+-- END
