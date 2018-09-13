63e0474dea94522b9374a0bb8eacb636fc532ca2
Zhou.Miao <zhou.miao@zuolin.com>
2018-09-13 18:08:09 +0800

    parking6.7

--- db/parking6.7-delta-schema.sql
+++ db/parking6.7-delta-schema.sql
@@ -1,6 +1,6 @@
 -- AUTHOR: 缪洲
 -- REMARK: 停车缴费V6.7，增加用户须知
 
-ALTER TABLE `eh_parking_lots` DROP COLUMN `notice_contact` text COMMENT '用户须知联系电话';
+ALTER TABLE `eh_parking_lots` ADD COLUMN `notice_contact` varchar(20) COMMENT '用户须知联系电话';
 ALTER TABLE `eh_parking_lots` ADD COLUMN `summary` text COMMENT '用户须知';
 
