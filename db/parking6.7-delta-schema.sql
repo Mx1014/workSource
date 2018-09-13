ef55ce0d03301667d57af4ce0a054699503014bb
Zhou.Miao <zhou.miao@zuolin.com>
2018-09-13 16:45:49 +0800

    parking6.7

--- db/parking6.7-delta-schema.sql
+++ db/parking6.7-delta-schema.sql
@@ -1,4 +1,3 @@
 -- AUTHOR: 缪洲
 -- REMARK: 停车缴费V6.7，增加用户须知
-ALTER TABLE `eh_parking_lots` DROP COLUMN `summary` text COMMENT '用户须知';
-ALTER TABLE `eh_parking_lots` ADD COLUMN `notice_json` varchar(2048) DEFAULT NULL COMMENT '用户须知与联系电话（用json数组的方式存储）';	
+ALTER TABLE `eh_parking_lots` ADD COLUMN `notice_json` varchar(1024) DEFAULT NULL COMMENT '用户须知与联系电话（用json数组的方式存储）';	
