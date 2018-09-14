777e8bc1a52256ba099f1192eec670a301def71e
steve <cen.tang@zuolin.com>
2018-09-14 11:03:24 +0800

    issue-30701:save work 05

--- ehnextgen-api/src/main/java/com/everhomes/advertisement/InvestmentAd.java
+++ ehnextgen-api/src/main/java/com/everhomes/advertisement/InvestmentAd.java
@@ -1,9 +1,9 @@
 package com.everhomes.advertisement;
 
-import com.everhomes.server.schema.tables.pojos.EhInvestmentAdvertisement;
+import com.everhomes.server.schema.tables.pojos.EhInvestmentAdvertisements;
 import com.everhomes.util.StringHelper;
 
-public class Advertisement extends EhInvestmentAdvertisement{
+public class InvestmentAd extends EhInvestmentAdvertisements{
 
 	private static final long serialVersionUID = 1L;
 
