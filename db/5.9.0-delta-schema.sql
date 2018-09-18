0eecfe5d78c77951798ec301f751af2aac9c6057
wuhan <han.wu@zuolin.com>
2018-09-18 13:43:03 +0800

    issue-33887:文件名过长不能加索引

--- db/5.9.0-delta-schema.sql
+++ db/5.9.0-delta-schema.sql
@@ -81,7 +81,6 @@ ALTER TABLE `eh_payment_bill_items` ADD COLUMN `consume_user_id` BIGINT COMMENT 
 ALTER TABLE `eh_file_management_contents` ADD COLUMN `operator_name`  VARCHAR(256) ;
 ALTER TABLE `eh_file_management_catalogs` ADD COLUMN `operator_name`  VARCHAR(256) ;
 -- REMARK: issue-33887: 给文件表增加索引
-ALTER TABLE `eh_file_management_contents` ADD INDEX  `i_eh_content_name` (`content_name`);
 ALTER TABLE `eh_file_management_contents` ADD INDEX  `i_eh_content_catalog_id` (`catalog_id`);
 ALTER TABLE `eh_file_management_contents` ADD INDEX  `i_eh_content_parent_id` (`parent_id`);
 -- REMARK: issue-33887
