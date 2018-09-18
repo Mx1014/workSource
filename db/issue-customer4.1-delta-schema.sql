828a0ad03adf3b787f9e1398157be307690c4992
cx.yang <cx.yang@zuolin.com>
2018-09-18 16:06:27 +0800

    issue-30655 瑞安CM对接 账单表增加第三方唯一标识字段

--- db/issue-customer4.1-delta-schema.sql
+++ db/issue-customer4.1-delta-schema.sql
@@ -4,3 +4,5 @@ ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `is_default` TINYINT DEFAULT 0 C
 -- REMARK: 瑞安CM对接 账单、费项表增加是否是只读字段
 ALTER TABLE `eh_payment_bills` ADD COLUMN `is_readonly` TINYINT DEFAULT 0 COMMENT '只读状态：0：非只读；1：只读';
 ALTER TABLE `eh_payment_bill_items` ADD COLUMN `is_readonly` TINYINT DEFAULT 0 COMMENT '只读状态：0：非只读；1：只读';
+-- REMARK: 瑞安CM对接 账单表增加第三方唯一标识字段
+ALTER TABLE `eh_payment_bills` ADD COLUMN `third_bill_id` VARCHAR(1024) COMMENT '账单表增加第三方唯一标识字段';
