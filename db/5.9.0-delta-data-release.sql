1d4a428ff97aa96216aac795ec68dc4e15d975e3
liangyanlong <yanlong.liang@zuolin.com>
2018-09-18 19:03:31 +0800

    5.9.0

--- db/5.9.0-delta-data-release.sql
+++ db/5.9.0-delta-data-release.sql
@@ -112,6 +112,13 @@ UPDATE eh_work_reports SET `icon_uri`='cs://1/image/aW1hZ2UvTVRveE9UVm1aRGhsTTJR
 UPDATE eh_work_reports SET `icon_uri`='cs://1/image/aW1hZ2UvTVRwaFlqazJNVFk1WkdabFpEbGhNamc0T1RjNVltWmlOakl3TmpobE1qUXpOUQ' WHERE report_template_id = 1;
 UPDATE eh_work_reports SET `icon_uri`='cs://1/image/aW1hZ2UvTVRvMU1tRTVPV0kwWlRjeU16WmlOR05rTnpWbE9HUTFZV1ExTlRZMFpHUm1Odw' WHERE report_template_id = 2;
 UPDATE eh_work_reports SET `icon_uri`='cs://1/image/aW1hZ2UvTVRvd1lXRmtObUkwWkRaaFlXRXdPRFV5TmpjMU5qa3hNelk1TVRRNE5XUTRNdw' WHERE report_template_id = 3;
+
+-- AUTHOR: 梁燕龙
+-- REMARK: 活动报名人数不足最低限制人数自动取消活动消息推送
+INSERT INTO eh_locale_templates(`scope`, `code`,`locale`, `description`, `text`)
+VALUES( 'announcement.notification', 1, 'zh_CN', '公告消息', '${subject}');
+INSERT INTO `eh_locale_strings` (`scope`,`code`,`locale`,`text`) VALUES ('announcement',1,'zh_CN','公告消息');
+INSERT INTO `eh_locale_strings` (`scope`,`code`,`locale`,`text`) VALUES ('forum',10007,'zh_CN','来晚啦，公告已不存在');
 -- --------------------- SECTION END ---------------------------------------------------------
 -- --------------------- SECTION BEGIN -------------------------------------------------------
 -- ENV: zuolin-base
