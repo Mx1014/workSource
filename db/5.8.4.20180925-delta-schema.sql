6f7a160406a7fdc915e75d4c3520c8a94a34c948
wuhan <han.wu@zuolin.com>
2018-09-19 15:35:37 +0800

    issue-33887: 去掉name的索引

--- db/5.8.4.20180925-delta-schema.sql
+++ db/5.8.4.20180925-delta-schema.sql
@@ -44,7 +44,6 @@ ALTER TABLE `eh_enterprise_notices` ADD COLUMN `stick_time` DATETIME;
 ALTER TABLE `eh_file_management_contents` ADD COLUMN `operator_name`  VARCHAR(256) ;
 ALTER TABLE `eh_file_management_catalogs` ADD COLUMN `operator_name`  VARCHAR(256) ;
 -- REMARK: issue-33887: 给文件表增加索引
-ALTER TABLE `eh_file_management_contents` ADD INDEX  `i_eh_content_name` (`content_name`);
 ALTER TABLE `eh_file_management_contents` ADD INDEX  `i_eh_content_catalog_id` (`catalog_id`);
 ALTER TABLE `eh_file_management_contents` ADD INDEX  `i_eh_content_parent_id` (`parent_id`);
  
