53a3bcb1e866d12248c9881fa02fe324e788830c
cx.yang <cx.yang@zuolin.com>
2018-09-19 19:34:44 +0800

    issue-30557 merge 5.9.0

--- db/5.9.0-delta-schema.sql
+++ db/5.9.0-delta-schema.sql
@@ -74,6 +74,24 @@ ALTER TABLE `eh_payment_bill_items` ADD COLUMN `source_id` BIGINT COMMENT '各�
 ALTER TABLE `eh_payment_bill_items` ADD COLUMN `source_name` VARCHAR(1024) COMMENT '账单来源（如：停车缴费，缴费的新增/导入等）';
 ALTER TABLE `eh_payment_bill_items` ADD COLUMN `consume_user_id` BIGINT COMMENT '企业下面的某个人的ID';
 
+-- AUTHOR: 杨崇鑫
+-- REMARK: 物业缴费V6.0 收费项配置可手动新增
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `namespace_id` INTEGER COMMENT '增加域空间ID作为标识';
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `owner_id` BIGINT COMMENT '增加园区ID作为标识';
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `owner_type` VARCHAR(64)  COMMENT '增加园区ID作为标识';
+ALTER TABLE `eh_payment_charging_items` ADD COLUMN `category_id` BIGINT COMMENT '多入口应用id';
+-- AUTHOR: 杨崇鑫
+-- REMARK: 物业缴费V6.0 账单、费项增加是否可以删除、是否可以编辑状态字段
+ALTER TABLE `eh_payment_bills` ADD COLUMN `can_delete` TINYINT DEFAULT 0 COMMENT '0：不可删除；1：可删除';
+ALTER TABLE `eh_payment_bills` ADD COLUMN `can_modify` TINYINT DEFAULT 0 COMMENT '0：不可编辑；1：可编辑';
+ALTER TABLE `eh_payment_bill_items` ADD COLUMN `can_delete` TINYINT DEFAULT 0 COMMENT '0：不可删除；1：可删除';
+ALTER TABLE `eh_payment_bill_items` ADD COLUMN `can_modify` TINYINT DEFAULT 0 COMMENT '0：不可编辑；1：可编辑';
+-- AUTHOR: 杨崇鑫
+-- REMARK: 物业缴费V6.0 账单、费项表增加是否删除状态字段
+ALTER TABLE `eh_payment_bills` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMENT '删除状态：0：已删除；1：正常使用';
+ALTER TABLE `eh_payment_bill_items` ADD COLUMN `delete_flag` TINYINT DEFAULT 1 COMMENT '删除状态：0：已删除；1：正常使用';
+
+
 
 -- AUTHOR: 黄明波
 -- REMARK: #33683服务联盟样式列表添加排序 #37669修复
