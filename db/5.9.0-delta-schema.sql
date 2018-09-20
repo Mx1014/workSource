b0c9e4f19c612d390911e1aa2788bb7a7a81d53c
Zhou.Miao <zhou.miao@zuolin.com>
2018-09-20 21:22:35 +0800

    parking6.7

--- db/5.9.0-delta-schema.sql
+++ db/5.9.0-delta-schema.sql
@@ -462,3 +462,9 @@ ALTER TABLE `eh_rentalv2_orders`
 ADD COLUMN `account_name`  varchar(255) NULL AFTER `old_custom_object`;
 -- END
 
+-- AUTHOR: 缪洲
+-- REMARK: 停车缴费V6.7，增加用户须知
+
+ALTER TABLE `eh_parking_lots` ADD COLUMN `notice_contact` varchar(20) COMMENT '用户须知联系电话';
+ALTER TABLE `eh_parking_lots` ADD COLUMN `summary` text COMMENT '用户须知';
+-- END
