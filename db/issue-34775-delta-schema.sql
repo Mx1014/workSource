080c82f2bf3e614e0ed89f0a43bccd0f3ae88b90
wuhan <han.wu@zuolin.com>
2018-09-20 11:34:55 +0800

    issue-34775: SQL:增加日统计月统计表的外出打卡统计项

--- db/issue-34775-delta-schema.sql
+++ db/issue-34775-delta-schema.sql
@@ -1,7 +1,7 @@
  
 -- AUTHOR: 吴寒
 -- REMARK: 打卡考勤V8.2 - 支持人脸识别关联考勤；支持自动打卡
-ALTER TABLE `eh_punch_logs` ADD COLUMN `create_type` TINYINT(4) NOT NULL DEFAULT 0 COMMENT '创建类型 : 0-正常打卡创建 1-自动打卡创建 2-人脸识别打卡创建 4-其他第三方接口创建(通过第三方接口打卡没有带创建类型)' ;
+ALTER TABLE `eh_punch_logs` ADD COLUMN `create_type` TINYINT NOT NULL DEFAULT 0 COMMENT '创建类型 : 0-正常打卡创建 1-自动打卡创建 2-人脸识别打卡创建 4-其他第三方接口创建(通过第三方接口打卡没有带创建类型)' ;
 
 -- 外出打卡表
 CREATE TABLE `eh_punch_go_out_logs` (
@@ -16,15 +16,13 @@ CREATE TABLE `eh_punch_go_out_logs` (
   `img_uri` VARCHAR(2048) DEFAULT NULL COMMENT '打卡上传图片uri地址',
   `description` VARCHAR(1024) DEFAULT NULL COMMENT '备注',
   `punch_date` DATE DEFAULT NULL COMMENT 'user punch date',
-  `punch_organization_id` BIGINT DEFAULT NULL COMMENT '考勤规则id:可以为null',
-  `punch_time` DATETIME DEFAULT NULL COMMENT '打卡时间', 
+  `punch_time` DATETIME DEFAULT NULL COMMENT '打卡时间',
   `identification` VARCHAR(255) DEFAULT NULL COMMENT 'unique identification for a phone',
   `status` TINYINT(4) DEFAULT NULL COMMENT '打卡状态 0-正常 1-迟到 2-早退 3-缺勤 14-缺卡',
-  `approval_status` TINYINT(4) DEFAULT NULL COMMENT '校正后的打卡状态 0-正常 null-没有异常校准',
-  `smart_alignment` TINYINT(4) DEFAULT '0' COMMENT '只能校准状态 0-非校准 1-校准',
-  `should_punch_time` BIGINT DEFAULT NULL COMMENT '应该打卡时间(用以计算早退迟到时长)',
-  `device_change_flag` TINYINT(4) DEFAULT '0' COMMENT '0- unchange 1-changed',
   `update_date` DATE DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `i_eh_user_id` (`user_id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `eh_punch_day_logs` ADD COLUMN `go_out_punch_flag` TINYINT DEFAULT 0 COMMENT '是否外出打卡，1：是 0：否' AFTER `normal_flag`;
+ALTER TABLE `eh_punch_statistics` ADD COLUMN `go_out_punch_day_count` INT DEFAULT 0 COMMENT '当月外出打卡天数' AFTER `rest_day_count`;
