37b1ff2fa2083a0c3cc5937644834ba390f0ed36
yanjun <jun.yan@zuolin.com>
2018-09-25 16:01:41 +0800

    issue-34780 add enable enterprise_pay_flag

---
+++ db/cloudprint1.4-delta-schema.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `eh_siyin_print_orders` ADD COLUMN `general_bill_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '统一账单id' ;
+
+ALTER TABLE `eh_service_modules` ADD COLUMN `enable_enterprise_pay`  tinyint(4) NULL COMMENT '企业支付标志，0-否，1-是';
+ALTER TABLE `eh_service_module_apps` ADD COLUMN `enable_enterprise_pay`  tinyint(4) NULL COMMENT '企业支付标志，0-否，1-是';
