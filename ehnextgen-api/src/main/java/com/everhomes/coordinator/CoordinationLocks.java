9c3721ed7145cdc40061d83d3a1d9de6e3d83785
nan.rong <nan.rong@zuolin.com>
2018-09-25 14:57:23 +0800

    issue-38151  加锁避免定时任务被重复调用

--- ehnextgen-api/src/main/java/com/everhomes/coordinator/CoordinationLocks.java
+++ ehnextgen-api/src/main/java/com/everhomes/coordinator/CoordinationLocks.java
@@ -151,7 +151,7 @@ public enum CoordinationLocks {
     VISITOR_SYS_CONFIG("visitor_sys_config"),
     WORK_REPORT_RX_MSG("work_report_rx_msg"),
     WORK_REPORT_AU_BASIC_MSG("work_report_au_basic_msg"),
-	WORK_REPORT_AU_MSG("work_report_author_msg"),
+	WORK_REPORT_AU_MSG("work_report_au_msg"),
 
     VISITOR_SYS_LOCATION("visitor_sys_location"),
     ACTIVITY_SIGNUP_TIMEOUT("activity_signup_timeout"),
