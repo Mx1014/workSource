72c450174af903d40a2a3cb2721122e5d7985f18
pengyu.huang <pengyu.huang@zuolin.com>
2018-09-27 11:37:27 +0800

    增加moduleName

--- ehcore/src/main/java/com/everhomes/investment/InvitedCustomerServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/investment/InvitedCustomerServiceImpl.java
@@ -353,6 +353,7 @@ public class InvitedCustomerServiceImpl implements InvitedCustomerService {
             fieldItemCommand.setCommunityId(cmd.getCommunityId());
             // this field id menus investment enterprise levelItemId private key
             fieldItemCommand.setFieldId(5L);
+            fieldItemCommand.setModuleName("investment_promotion");
             List<FieldItemDTO> items = fieldService.listFieldItems(fieldItemCommand);
             Map<Long, FieldItemDTO> itemsMap = transferCurrentCommunityItemsMap(items);
             if (itemsMap != null && itemsMap.size() > 0) {
