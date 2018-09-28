5f46850d47a2c2d6f4efa5d0d97f0abeca4ccb2d
Zhou.Miao <zhou.miao@zuolin.com>
2018-09-28 09:59:22 +0800

    cloudprint1.4

--- db/cloudprint1.4-delta-schema.sql
+++ db/cloudprint1.4-delta-schema.sql
@@ -1,3 +1,5 @@
+-- AUTHOR: 缪洲
+-- REMARK: issue-34780 企业支付授权列表
 ALTER TABLE `eh_siyin_print_orders` ADD COLUMN `general_bill_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '统一账单id' ;
 
 ALTER TABLE `eh_service_modules` ADD COLUMN `enable_enterprise_pay_flag`  tinyint(4) NULL COMMENT '企业支付标志，0-否，1-是';
