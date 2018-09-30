5d1fb512b15819603af447841c891a60b8d85a86
shiheng.ma <shiheng.ma@zuolin.com>
2018-09-30 15:59:07 +0800

    issue-38604:企业到访确认配置（园区登记临时访客情况）

--- ehcore/src/main/java/com/everhomes/visitorsys/VisitorSysServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/visitorsys/VisitorSysServiceImpl.java
@@ -2558,7 +2558,7 @@ public class VisitorSysServiceImpl implements VisitorSysService{
 //        }
 //        园区确认预约访客，企业的关联 和 园区登记临时访客，企业关联
         if ((convert.getId() != null && convert.getVisitorType().equals(VisitorsysVisitorType.BE_INVITED.getCode())) ||
-                (convert.getId() == null && convert.getVisitorType().equals(VisitorsysVisitorType.TEMPORARY.getCode()))) {
+                convert.getVisitorType().equals(VisitorsysVisitorType.TEMPORARY.getCode())) {
             if (ownerType == VisitorsysOwnerType.COMMUNITY) {
                 VisitorSysConfiguration config = visitorSysConfigurationProvider.findVisitorSysConfigurationByOwner(convert.getNamespaceId(), VisitorsysOwnerType.ENTERPRISE.getCode(), convert.getEnterpriseId());
                 if (null != config.getBaseConfig() && TrueOrFalseFlag.FALSE.getCode().equals(config.getBaseConfig().getVisitorConfirmFlag())) {
