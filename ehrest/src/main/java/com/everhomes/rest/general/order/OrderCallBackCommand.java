2ef7cbfa378a7ebd1383e76b09e7d365d6fbe621
huangmingbo <mingbo.huang@zuolin.com>
2018-10-07 09:54:31 +0800

    fix

--- ehrest/src/main/java/com/everhomes/rest/general/order/OrderCallBackCommand.java
+++ ehrest/src/main/java/com/everhomes/rest/general/order/OrderCallBackCommand.java
@@ -1,11 +1,10 @@
 package com.everhomes.rest.general.order;
 
-import com.everhomes.rest.order.OrderType;
 import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
- * <li>callBackType : 回调类型 0-企业支付成功 1-发票</li>
+ * <li>callBackType : 回调类型 0-企业支付成功 1-发票 {@link com.everhomes.rest.general.order.OrderCallBackType}</li>
  * <li>businessType : 业务类型标识 如OrderType.PRINT_ORDER_CODE 填{@link com.everhomes.rest.order.OrderType}</li>
  * <li>businessOrderId : 业务订单id</li>
  * </ul>
@@ -22,31 +21,25 @@ public class OrderCallBackCommand {
 		return StringHelper.toJsonString(this);
 	}
 
-
 	public String getBusinessType() {
 		return businessType;
 	}
-
 
 	public void setBusinessType(String businessType) {
 		this.businessType = businessType;
 	}
 
-
 	public String getBusinessOrderId() {
 		return businessOrderId;
 	}
-
 
 	public void setBusinessOrderId(String businessOrderId) {
 		this.businessOrderId = businessOrderId;
 	}
 
-
 	public Byte getCallBackType() {
 		return callBackType;
 	}
-
 
 	public void setCallBackType(Byte callBackType) {
 		this.callBackType = callBackType;
