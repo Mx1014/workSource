39b33903752a9cd50f65cb338e6013c01e14d104
xq.tian <xq.tian@zuolin.com>
2018-10-15 10:16:05 +0800

    update bean name

--- ehcore/src/main/java/com/everhomes/flow/vars/FlowVaribleApplierNameResolver.java
+++ ehcore/src/main/java/com/everhomes/flow/vars/FlowVaribleApplierNameResolver.java
@@ -3,22 +3,23 @@ package com.everhomes.flow.vars;
 import com.everhomes.flow.FlowCaseState;
 import com.everhomes.flow.FlowService;
 import com.everhomes.flow.FlowVariableTextResolver;
-import com.everhomes.rest.user.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component(FlowVariableTextResolver.APPLIER_PHONE)
-public class FlowVaribleApplierNameResolver implements FlowVariableTextResolver {
-    @Autowired
-    FlowService flowService;
+import com.everhomes.rest.user.UserInfo;
 
-    @Override
-    public String variableTextRender(FlowCaseState ctx, String variable) {
-        UserInfo userInfo = flowService.getUserInfoInContext(ctx, ctx.getFlowCase().getApplyUserId());
-        if (userInfo == null || userInfo.getPhones() == null || userInfo.getPhones().size() == 0) {
-            return "error";//TODO use error ?
-        }
-        return userInfo.getPhones().get(0);
-    }
+@Component(FlowVariableTextResolver.APPLIER_NAME)
+public class FlowVaribleApplierNameResolver implements FlowVariableTextResolver {
+	@Autowired
+    FlowService flowService;
+	
+	@Override
+	public String variableTextRender(FlowCaseState ctx, String variable) {
+		UserInfo userInfo = flowService.getUserInfoInContext(ctx, ctx.getFlowCase().getApplyUserId());
+		if(userInfo == null) {
+			return "error";//TODO use error ?
+		}
+		return userInfo.getNickName();
+	}
 
 }
--- ehcore/src/main/java/com/everhomes/flow/vars/FlowVaribleApplierPhoneResolver.java
+++ ehcore/src/main/java/com/everhomes/flow/vars/FlowVaribleApplierPhoneResolver.java
@@ -3,23 +3,22 @@ package com.everhomes.flow.vars;
 import com.everhomes.flow.FlowCaseState;
 import com.everhomes.flow.FlowService;
 import com.everhomes.flow.FlowVariableTextResolver;
+import com.everhomes.rest.user.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.everhomes.rest.user.UserInfo;
-
-@Component(FlowVariableTextResolver.APPLIER_NAME)
+@Component(FlowVariableTextResolver.APPLIER_PHONE)
 public class FlowVaribleApplierPhoneResolver implements FlowVariableTextResolver {
-	@Autowired
+    @Autowired
     FlowService flowService;
-	
-	@Override
-	public String variableTextRender(FlowCaseState ctx, String variable) {
-		UserInfo userInfo = flowService.getUserInfoInContext(ctx, ctx.getFlowCase().getApplyUserId());
-		if(userInfo == null) {
-			return "error";//TODO use error ?
-		}
-		return userInfo.getNickName();
-	}
+
+    @Override
+    public String variableTextRender(FlowCaseState ctx, String variable) {
+        UserInfo userInfo = flowService.getUserInfoInContext(ctx, ctx.getFlowCase().getApplyUserId());
+        if (userInfo == null || userInfo.getPhones() == null || userInfo.getPhones().size() == 0) {
+            return "error";//TODO use error ?
+        }
+        return userInfo.getPhones().get(0);
+    }
 
 }
