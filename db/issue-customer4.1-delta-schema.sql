0dd2b1ace819126d08f82dc3e3871e0f92ea6cbc
dingjianmin <jm.ding@zuolin.com>
2018-10-17 11:44:31 +0800

    （瑞安合同）修改发起人字段类型

--- db/issue-customer4.1-delta-schema.sql
+++ db/issue-customer4.1-delta-schema.sql
@@ -4,4 +4,5 @@ ALTER TABLE `eh_payment_bill_groups` ADD COLUMN `is_default` TINYINT DEFAULT 0 C
 -- REMARK: 瑞安CM对接 账单、费项表增加是否是只读字段
 ALTER TABLE `eh_payment_bills` ADD COLUMN `is_readonly` TINYINT DEFAULT 0 COMMENT '只读状态：0：非只读；1：只读';
 ALTER TABLE `eh_payment_bill_items` ADD COLUMN `is_readonly` TINYINT DEFAULT 0 COMMENT '只读状态：0：非只读；1：只读';
-
+-- AUTHOR: djm
+alter table eh_contracts modify column sponsor_uid varchar(50);
