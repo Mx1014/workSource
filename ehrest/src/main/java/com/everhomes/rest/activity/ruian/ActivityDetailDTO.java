27bece656f74439908e1d7655c5cd88d16626bf0
huangliangming <liangming.huang@zuolin.com>
2018-10-17 10:34:15 +0800

    some commit

--- ehrest/src/main/java/com/everhomes/rest/activity/ruian/ActivityDetailDTO.java
+++ ehrest/src/main/java/com/everhomes/rest/activity/ruian/ActivityDetailDTO.java
@@ -1,4 +1,15 @@
 package com.everhomes.rest.activity.ruian;
 
+import java.util.List;
+
+/**
+ * 对接瑞安方的类，别人的的字段名都是大写开头的，为方便转按只能按别人的写了，不是我的锅．
+ */
 public class ActivityDetailDTO {
+
+    private Integer Code ;//返回状态
+    private ActivityModel Data;//返回数据
+    private String Message ;//返回状态描述
+
+
 }
