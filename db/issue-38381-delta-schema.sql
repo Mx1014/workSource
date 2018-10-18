4a225b5a333525498539c41b50ce8ab9da0ef77a
liangyanlong <yanlong.liang@zuolin.com>
2018-10-18 16:09:48 +0800

    issue-38381: 增加包名字段

--- db/issue-38381-delta-schema.sql
+++ db/issue-38381-delta-schema.sql
@@ -1,3 +1,4 @@
 -- AUTHOR: 梁燕龙
 -- REMARK: 微信分享配置中增加主题色字段
 ALTER TABLE `eh_app_urls` ADD COLUMN `theme_color` VARCHAR(64) COMMENT '主题色';
+ALTER TABLE `eh_app_urls` ADD COLUMN `package_name` VARCHAR(64) COMMENT '包名';
