f3fc657e3baa112747d228e102c2b8d5e91dbeef
liangyanlong <yanlong.liang@zuolin.com>
2018-10-19 16:43:04 +0800

    issue-36940: 脚本

--- db/issue-36940-delta-schema.sql
+++ db/issue-36940-delta-schema.sql
@@ -5,7 +5,9 @@ CREATE TABLE `eh_user_authentication_organizations`(
   `namespace_id` INTEGER NOT NULL  COMMENT '域空间ID',
   `community_id` BIGINT NOT NULL COMMENT '项目ID',
   `organization_id` BIGINT NOT NULL COMMENT '企业ID',
-  `creator_uid` BIGINT NOT NULL COMMENT 'assignment creator uid',
+  `auth_flag` TINYINT NOT NULL COMMENT '是否授权，0不授权，1授权',
+  `status` TINYINT NOT NULL COMMENT '状态, 1无效，2生效',
+  `creator_uid` BIGINT COMMENT 'creator uid',
   `create_time` DATETIME COMMENT 'record create time',
 
   PRIMARY KEY (`id`)
