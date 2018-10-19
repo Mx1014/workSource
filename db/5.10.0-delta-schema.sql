fe8fa0946ec8038fa3b6bc1b566f9be321aa6bcc
liangyanlong <yanlong.liang@zuolin.com>
2018-10-19 14:16:28 +0800

    5.10.0

--- db/5.10.0-delta-schema.sql
+++ db/5.10.0-delta-schema.sql
@@ -35,3 +35,15 @@ ALTER TABLE `eh_visitor_sys_office_locations` ADD COLUMN `refer_id` bigint(20) N
 -- AUTHOR: 黄明波
 -- REMARK: 服务联盟通用配置修复
 ALTER TABLE `eh_service_alliance_categories` ADD COLUMN `origin_category_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '原默认配置的服务类型id，如果是项目配置新增的，通常就是当前记录id' ;
+
+-- AUTHOR: 梁燕龙
+-- REMARK: 用户认证审核权限配置表
+CREATE TABLE `eh_user_authentication_organizations`(
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL  COMMENT '域空间ID',
+  `organization_id` BIGINT NOT NULL COMMENT '企业ID',
+  `creator_uid` BIGINT NOT NULL COMMENT 'assignment creator uid',
+  `create_time` DATETIME COMMENT 'record create time',
+
+  PRIMARY KEY (`id`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '用户认证审核权限配置表';
