e229cdef7ff7a427a9beb236f52f3a87751370ee
huangliangming <liangming.huang@zuolin.com>
2018-10-20 09:43:54 +0800

    5.10.0:merge 5.10.0-hlm-0.1 to change a column

--- db/5.10.0-delta-schema.sql
+++ db/5.10.0-delta-schema.sql
@@ -65,4 +65,4 @@ CREATE TABLE `eh_user_authentication_organizations`(
 
 -- AUTHOR: 黄良铭
 -- REMARK: 场景记录表添加字段
-ALTER TABLE eh_user_current_scene ADD COLUMN  token VARCHAR(2048);
+ALTER TABLE eh_user_current_scene ADD COLUMN  sign_token VARCHAR(2048);
