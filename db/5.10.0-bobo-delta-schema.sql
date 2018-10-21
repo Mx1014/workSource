6fc5f27f2d0be3e749dffd259d070fed1c4b80fa
huangmingbo <mingbo.huang@zuolin.com>
2018-10-21 15:45:27 +0800

    fix sql

--- db/5.10.0-bobo-delta-schema.sql
+++ db/5.10.0-bobo-delta-schema.sql
@@ -2,3 +2,4 @@
 -- REMARK: 服务联盟通用配置修复
 ALTER TABLE `eh_service_alliance_categories` ADD COLUMN `enable_provider` TINYINT NOT NULL DEFAULT '0' COMMENT '0-关闭服务商功能 1-开启' ;
 ALTER TABLE `eh_service_alliance_categories` ADD COLUMN `enable_comment` TINYINT NOT NULL DEFAULT '0' COMMENT '0-关闭评论功能 1-开启评论功能' ;
+ALTER TABLE `eh_service_alliance_categories` ADD COLUMN `description` MEDIUMTEXT NULL COMMENT '首页样式描述文字';
