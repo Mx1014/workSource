6d417aec149409a2c66e3ac8b01a9d55c3a522f9
shiheng.ma <shiheng.ma@zuolin.com>
2018-10-22 21:17:23 +0800

    issue-33684:物业报修对接国贸报错信息

--- db/pmtask-3.8-delta-data-release.sql
+++ db/pmtask-3.8-delta-data-release.sql
@@ -2,4 +2,4 @@
 -- REMARK: 物业报修3.8 对接国贸报错信息 20181022
 INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10026', 'zh_CN', '用户不存在');
 INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10027', 'zh_CN', '初始化失败');
-INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10026', 'zh_CN', '获取数据失败');
+INSERT INTO `eh_locale_strings` (`scope`, `code`, `locale`, `text`) VALUES ('pmtask', '10028', 'zh_CN', '获取数据失败');
