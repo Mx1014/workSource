cce7855441b65a2900fd7520a62287a65d9cb502
huangmingbo <mingbo.huang@zuolin.com>
2018-10-24 20:45:04 +0800

    issue-38437 fix

--- ehrest/src/main/java/com/everhomes/yellowPage/faq/GetLatestServiceStateCommand.java
+++ ehrest/src/main/java/com/everhomes/yellowPage/faq/GetLatestServiceStateCommand.java
@@ -1,6 +1,7 @@
 package com.everhomes.yellowPage.faq;
 
 import com.everhomes.rest.yellowPage.AllianceCommonCommand;
+import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
@@ -10,6 +11,46 @@ import com.everhomes.rest.yellowPage.AllianceCommonCommand;
  * <li>type: 服务联盟类型</li>
  * </ul>
  **/
-public class GetLatestServiceStateCommand extends AllianceCommonCommand{
-	
+public class GetLatestServiceStateCommand {
+	private Integer namespaceId;
+	private String ownerType;
+	private Long ownerId;
+	private Long type;
+
+	@Override
+	public String toString() {
+		return StringHelper.toJsonString(this);
+	}
+
+	public String getOwnerType() {
+		return ownerType;
+	}
+
+	public void setOwnerType(String ownerType) {
+		this.ownerType = ownerType;
+	}
+
+	public Long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public Integer getNamespaceId() {
+		return namespaceId;
+	}
+
+	public void setNamespaceId(Integer namespaceId) {
+		this.namespaceId = namespaceId;
+	}
+
+	public Long getType() {
+		return type;
+	}
+
+	public void setType(Long type) {
+		this.type = type;
+	}
 }
--- ehrest/src/main/java/com/everhomes/yellowPage/faq/GetSquareCardInfosCommand.java
+++ ehrest/src/main/java/com/everhomes/yellowPage/faq/GetSquareCardInfosCommand.java
@@ -1,6 +1,7 @@
 package com.everhomes.yellowPage.faq;
 
 import com.everhomes.rest.yellowPage.AllianceCommonCommand;
+import com.everhomes.util.StringHelper;
 /**
  * <ul>
  * <li>namespaceId: namespaceId</li>
@@ -10,8 +11,49 @@ import com.everhomes.rest.yellowPage.AllianceCommonCommand;
  * <li>serviceTypeId: 服务类型id, 不需要限定服务类型时不传</li>
  * </ul>
  **/
-public class GetSquareCardInfosCommand extends AllianceCommonCommand{	
+public class GetSquareCardInfosCommand {
+	private Integer namespaceId;
+	private String ownerType;
+	private Long ownerId;
+	private Long type;
 	private Long serviceTypeId;
+
+	@Override
+	public String toString() {
+		return StringHelper.toJsonString(this);
+	}
+
+	public String getOwnerType() {
+		return ownerType;
+	}
+
+	public void setOwnerType(String ownerType) {
+		this.ownerType = ownerType;
+	}
+
+	public Long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public Integer getNamespaceId() {
+		return namespaceId;
+	}
+
+	public void setNamespaceId(Integer namespaceId) {
+		this.namespaceId = namespaceId;
+	}
+
+	public Long getType() {
+		return type;
+	}
+
+	public void setType(Long type) {
+		this.type = type;
+	}
 
 	public Long getServiceTypeId() {
 		return serviceTypeId;
