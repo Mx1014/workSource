0478375d0341f5205fbcc343263231a8e568bab2
liangyanlong <yanlong.liang@zuolin.com>
2018-10-29 19:21:24 +0800

    issue-36037: itemGroup增加字段.

--- ehrest/src/main/java/com/everhomes/rest/portal/ItemGroupInstanceConfig.java
+++ ehrest/src/main/java/com/everhomes/rest/portal/ItemGroupInstanceConfig.java
@@ -76,6 +76,128 @@ public class ItemGroupInstanceConfig {
 
 	private String allOrMoreIconUrl;
 
+	//banner字段
+	private String itemGroup;
+
+	private Long categoryId;
+
+	private Long appId;
+
+	private String moreRouter;
+
+	private Byte backgroundType;
+
+	private String color;
+
+	private String topColor;
+
+	private String bottomColor;
+
+	private Byte autoScroll;
+
+	private Integer paddingFlag;
+
+	private Long widthRatio;
+
+	private Long heightRatio;
+	//banner字段end
+
+	public String getItemGroup() {
+		return itemGroup;
+	}
+
+	public void setItemGroup(String itemGroup) {
+		this.itemGroup = itemGroup;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Long getAppId() {
+		return appId;
+	}
+
+	public void setAppId(Long appId) {
+		this.appId = appId;
+	}
+
+	public String getMoreRouter() {
+		return moreRouter;
+	}
+
+	public void setMoreRouter(String moreRouter) {
+		this.moreRouter = moreRouter;
+	}
+
+	public Byte getBackgroundType() {
+		return backgroundType;
+	}
+
+	public void setBackgroundType(Byte backgroundType) {
+		this.backgroundType = backgroundType;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getTopColor() {
+		return topColor;
+	}
+
+	public void setTopColor(String topColor) {
+		this.topColor = topColor;
+	}
+
+	public String getBottomColor() {
+		return bottomColor;
+	}
+
+	public void setBottomColor(String bottomColor) {
+		this.bottomColor = bottomColor;
+	}
+
+	public Byte getAutoScroll() {
+		return autoScroll;
+	}
+
+	public void setAutoScroll(Byte autoScroll) {
+		this.autoScroll = autoScroll;
+	}
+
+	public Integer getPaddingFlag() {
+		return paddingFlag;
+	}
+
+	public void setPaddingFlag(Integer paddingFlag) {
+		this.paddingFlag = paddingFlag;
+	}
+
+	public Long getWidthRatio() {
+		return widthRatio;
+	}
+
+	public void setWidthRatio(Long widthRatio) {
+		this.widthRatio = widthRatio;
+	}
+
+	public Long getHeightRatio() {
+		return heightRatio;
+	}
+
+	public void setHeightRatio(Long heightRatio) {
+		this.heightRatio = heightRatio;
+	}
+
 	public Integer getColumnCount() {
 		return columnCount;
 	}
