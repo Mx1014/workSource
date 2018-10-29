2cdde340609345a6ed39e7e7acd8fea586b19280
huangliangming <liangming.huang@zuolin.com>
2018-10-29 11:34:40 +0800

    5.10.0-hlm-0.2:fix sql

--- db/5.10.0-hlm-0.2-delta-data-release.sql
+++ db/5.10.0-hlm-0.2-delta-data-release.sql
@@ -105,85 +105,85 @@
 -- AUTHOR:黄良铭  20181025
 -- REMARK: 瑞安活动对接配置项
 -- 默认
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey', 'd2NP2Z','publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey', 'a6cfff2c4aa370f8','privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid', '5b5046c988ce7e5ad49c9b10','appid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.publickey', 'd2NP2Z','publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name`)
+VALUES ( 'mall.ruian.privatekey', 'a6cfff2c4aa370f8','privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid', '5b5046c988ce7e5ad49c9b10','appid','999929','');
 
 -- 上海新天地
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey.1', 'd2NP2Z','上海新天地publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey.1', 'a6cfff2c4aa370f8','上海新天地privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid.1', '5b5046c988ce7e5ad49c9b10','上海新天地appid','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'ruian.mall.id.1', '10764','上海新天地mallid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.publickey.1', 'd2NP2Z','上海新天地publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.privatekey.1', 'a6cfff2c4aa370f8','上海新天地privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid.1', '5b5046c988ce7e5ad49c9b10','上海新天地appid','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'ruian.mall.id.1', '10764','上海新天地mallid','999929','');
 
 
 -- 重庆天地
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey.2', 'o7Oep_','重庆天地publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey.2', '7d57f43738f546e2','重庆天地privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid.2', '5b505c8688ce7e238c3c3a2a','重庆天地appid','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'ruian.mall.id.2', '10782','重庆天地mallid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name`)
+VALUES ( 'mall.ruian.publickey.2', 'o7Oep_','重庆天地publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.privatekey.2', '7d57f43738f546e2','重庆天地privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid.2', '5b505c8688ce7e238c3c3a2a','重庆天地appid','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'ruian.mall.id.2', '10782','重庆天地mallid','999929','');
 
 -- 岭南天地
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey.3', 'bOT1fy','岭南天地publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey.3', 'ef94c7e11445aebd','岭南天地privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid.3', '5b505b8e3ae74e465c93447b','岭南天地appid','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'ruian.mall.id.3', '10778','岭南天地mallid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.publickey.3', 'bOT1fy','岭南天地publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.privatekey.3', 'ef94c7e11445aebd','岭南天地privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid.3', '5b505b8e3ae74e465c93447b','岭南天地appid','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'ruian.mall.id.3', '10778','岭南天地mallid','999929','');
 
 
 -- 虹桥天地
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey.4', '6pqMSA','虹桥天地publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey.4', '761604f49636c418','虹桥天地privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid.4', '5b505ac188ce7e238c3c3a28','虹桥天地appid','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'ruian.mall.id.4', '10743','虹桥天地mallid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name`)
+VALUES ( 'mall.ruian.publickey.4', '6pqMSA','虹桥天地publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.privatekey.4', '761604f49636c418','虹桥天地privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid.4', '5b505ac188ce7e238c3c3a28','虹桥天地appid','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'ruian.mall.id.4', '10743','虹桥天地mallid','999929','');
 
 -- 创智天地
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey.5', 'XWLzKN','创智天地publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey.5', 'cfeb935979f50825','创智天地privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid.5', '5b5048333ae74e58743209f7','创智天地appid','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'ruian.mall.id.5', '10776','创智天地mallid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.publickey.5', 'XWLzKN','创智天地publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.privatekey.5', 'cfeb935979f50825','创智天地privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid.5', '5b5048333ae74e58743209f7','创智天地appid','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'ruian.mall.id.5', '10776','创智天地mallid','999929','');
 
 -- 瑞虹天地
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.publickey.6', 'ydVQ7f','瑞虹天地publickey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.privatekey.6', '24f36ef07865a906','瑞虹天地privatekey','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.appid.6', '5b5047103ae74e58743209f3','瑞虹天地appid','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'ruian.mall.id.6', '10775','瑞虹天地mallid','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.publickey.6', 'ydVQ7f','瑞虹天地publickey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.privatekey.6', '24f36ef07865a906','瑞虹天地privatekey','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.appid.6', '5b5047103ae74e58743209f3','瑞虹天地appid','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'ruian.mall.id.6', '10775','瑞虹天地mallid','999929','');
 
 
 
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'activity.butt.url.getcategorylist', 'https://openapi10.mallcoo.cn/Event/Activity/V1/GetCategoryList/','获取活动分类','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'activity.butt.url.getactivitylist', 'https://openapi10.mallcoo.cn/Event/Activity/V1/GetList/','获取活动列表','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'activity.butt.url.getactivity', ' https://openapi10.mallcoo.cn/Event/Activity/V1/GetDetail/','获取活动详情','999929','','');
-INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` ,`is_readonly`)
-VALUES ( 'mall.ruian.url.activity', 'https://m.mallcoo.cn/a/custom/10764/xtd/activitylist','瑞安活动列表面URL','999929','','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'activity.butt.url.getcategorylist', 'https://openapi10.mallcoo.cn/Event/Activity/V1/GetCategoryList/','获取活动分类','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'activity.butt.url.getactivitylist', 'https://openapi10.mallcoo.cn/Event/Activity/V1/GetList/','获取活动列表','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name`)
+VALUES ( 'activity.butt.url.getactivity', ' https://openapi10.mallcoo.cn/Event/Activity/V1/GetDetail/','获取活动详情','999929','');
+INSERT INTO `eh_configurations` ( `name`, `value`, `description`, `namespace_id`, `display_name` )
+VALUES ( 'mall.ruian.url.activity', 'https://m.mallcoo.cn/a/custom/10764/xtd/activitylist','瑞安活动列表面URL','999929','');
 -- end
 
 -- --------------------- SECTION END ruianxintiandi ------------------------------------------
