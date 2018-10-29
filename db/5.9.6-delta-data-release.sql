556bc7ba3081ab7df74172ccf140d1367bffc9cb
Zhou.Miao <zhou.miao@zuolin.com>
2018-10-29 19:44:40 +0800

    fix

--- db/5.9.6-delta-data-release.sql
+++ db/5.9.6-delta-data-release.sql
@@ -823,6 +823,10 @@ update eh_siyin_print_printers set printer_name = 'Sys_NJ_INNO_2F02' where reade
 update eh_siyin_print_printers set printer_name = 'Sys_NJ_INNO_3F01' where reader_name = 'TC101152723540';
 update eh_siyin_print_printers set printer_name = 'Sys_NJ_INNO_2F01' where reader_name = 'TC101152723478';
 
+
+-- AUTHOR: 缪洲
+-- REMARK: 停车缴费收款账号迁移
+update eh_parking_business_payee_accounts ac set ac.merchant_id = ac.payee_id ;
 -- --------------------- SECTION END ruianxintiandi ------------------------------------------
 
 
