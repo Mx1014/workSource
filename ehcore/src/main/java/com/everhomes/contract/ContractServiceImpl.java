9c95cd94c7f72cbaaa89b2b9bee800affd1ed051
cx.yang <cx.yang@zuolin.com>
2018-10-29 10:24:01 +0800

    issue-38508 合并分支

--- ehcore/src/main/java/com/everhomes/contract/ContractServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/contract/ContractServiceImpl.java
@@ -8,20 +8,5 @@ import org.springframework.stereotype.Component;
 @Component(ContractService.CONTRACT_PREFIX + "")
 public class ContractServiceImpl extends DefaultContractServiceImpl
 		implements ApplicationListener<ContextRefreshedEvent> {
-
-<<<<<<< HEAD
-}
-=======
-				BigDecimal totalAmount = assetProvider.getBillExpectanciesAmountOnContract(contract.getContractNumber(),contract.getId(), assetCategoryId, contract.getNamespaceId());
-				contract.setRent(totalAmount);
-				contractProvider.updateContract(contract);
-			}
-		}
-	}
-	
-	
-	
-	
 	
 }
->>>>>>> asset7.0
