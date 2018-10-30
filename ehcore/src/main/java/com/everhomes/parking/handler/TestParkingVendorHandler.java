7ff98cf9345e063b7f5eb4f17cde1ae4496ca25b
Zhou.Miao <zhou.miao@zuolin.com>
2018-10-30 21:18:24 +0800

    parking6.7.5

--- ehcore/src/main/java/com/everhomes/parking/handler/TestParkingVendorHandler.java
+++ ehcore/src/main/java/com/everhomes/parking/handler/TestParkingVendorHandler.java
@@ -149,9 +149,15 @@ public class TestParkingVendorHandler extends DefaultParkingVendorHandler {
     public Boolean notifyParkingRechargeOrderPayment(ParkingRechargeOrder order) {
 		boolean notifyresult = configProvider.getBooleanValue("parking.test.notifyresult", true);
 		ParkingCardRequest request;
-		if (order.getOrderType().equals(ParkingOrderType.RECHARGE.getCode()) || order.getOrderType().equals(ParkingOrderType.OPEN_CARD.getCode())) {
+		if (order.getOrderType().equals(ParkingOrderType.OPEN_CARD.getCode())) {
+			if (null != order.getCardRequestId()) {
+	            request = parkingProvider.findParkingCardRequestById(order.getCardRequestId());
+	        }else {
+	            request = getParkingCardRequestByOrder(order);
+	            order.setCardRequestId(request.getId()); //补上id
+	        }
 		        updateFlowStatus(request);
-		    }
+		    
         }
 		return notifyresult;
 
