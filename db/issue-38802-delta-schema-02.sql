3f6db0836a8ac523e9afe7a6cc6f50ea6af00376
liangyanlong <yanlong.liang@zuolin.com>
2018-10-31 10:53:11 +0800

    issue-38802

--- db/issue-38802-delta-schema-02.sql
+++ db/issue-38802-delta-schema-02.sql
@@ -840,4 +840,16 @@ ALTER TABLE `eh_organization_details` MODIFY COLUMN `avatar`  varchar(512)  DEFA
 -- REMARK: 合同添加押金状态字段
 ALTER TABLE eh_contracts ADD COLUMN `deposit_status`  tinyint(4) NULL COMMENT '押金状态, 0-未缴, 2-已缴' AFTER deposit;
 -- end
+CREATE TABLE `eh_user_authentication_organizations`(
+  `id` BIGINT NOT NULL COMMENT 'id of the record',
+  `namespace_id` INTEGER NOT NULL  COMMENT '域空间ID',
+  `community_id` BIGINT COMMENT '项目ID',
+  `organization_id` BIGINT NOT NULL COMMENT '企业ID',
+  `auth_flag` TINYINT NOT NULL COMMENT '是否授权，0不授权，1授权',
+  `status` TINYINT NOT NULL COMMENT '状态, 1无效，2生效',
+  `creator_uid` BIGINT COMMENT 'creator uid',
+  `create_time` DATETIME COMMENT 'record create time',
+
+  PRIMARY KEY (`id`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '用户认证审核权限配置表';
 -- ------------------------------------------------- 5.8.4.20180925 新增的数据脚本   end ---------------------------------
