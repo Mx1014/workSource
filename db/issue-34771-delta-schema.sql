7bcb329f488d7d1600c953fd21f73264ca30750b
qingyan <qingyan.li@zuolin.com>
2018-11-03 17:46:17 +0800

    CREATE TABLE `eh_aclink_group_doors`

--- db/issue-34771-delta-schema.sql
+++ db/issue-34771-delta-schema.sql
@@ -1468,3 +1468,21 @@ CREATE TABLE `eh_asset_dooraccess_logs` (
 -- AUTHOR: xq.tian
 -- REMARK: 删除 eh_recommendations 的外键, 因为他影响了这张表的数据操作
 ALTER TABLE eh_recommendations DROP FOREIGN KEY fk_eh_recommendations_user_idx;
+
+-- AUTHOR: 李清岩
+-- REMARK: 门禁v3.0.2 issue-34771
+-- REMARK: 增加门禁组门禁关系表
+CREATE TABLE `eh_aclink_group_doors` (
+	`id` BIGINT  NOT NULL,
+	`namespace_id` INT  DEFAULT NULL COMMENT '域空间id',
+	`owner_id` BIGINT  NOT NULL DEFAULT '0' COMMENT '门禁组所属机构id',
+	`owner_type` TINYINT DEFAULT NULL COMMENT '门禁组所属机构类型 0园区 1公司',
+	`group_id` BIGINT DEFAULT NULL COMMENT '门禁组id',
+	`door_id` BIGINT DEFAULT NULL COMMENT '门禁组所属门禁id',
+	`status` TINYINT NOT NULL DEFAULT '1' COMMENT '状态 0删除 1有效',
+	`creator_uid` BIGINT  DEFAULT NULL COMMENT '创建者id',
+	`create_time` datetime DEFAULT NULL COMMENT '创建时间',
+	`operator_uid` BIGINT  DEFAULT NULL COMMENT '修改者id',
+	`operator_time` datetime DEFAULT NULL COMMENT '修改时间',
+	PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COMMENT = '门禁组门禁关系表';
