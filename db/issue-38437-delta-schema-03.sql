57444bc84d332ad2ab30e365f235444a45409d70
huangmingbo <mingbo.huang@zuolin.com>
2018-11-03 14:26:01 +0800

    fix merger

--- db/issue-38437-delta-schema-03.sql
+++ db/issue-38437-delta-schema-03.sql
@@ -898,17 +898,7 @@ ALTER TABLE `eh_service_alliance_attachments` ADD COLUMN `owner_type` VARCHAR(50
 
 
 
--- AUTHOR: 梁燕龙
--- REMARK: 用户认证审核权限配置表
-CREATE TABLE `eh_user_authentication_organizations`(
-  `id` BIGINT NOT NULL COMMENT 'id of the record',
-  `namespace_id` INTEGER NOT NULL  COMMENT '域空间ID',
-  `organization_id` BIGINT NOT NULL COMMENT '企业ID',
-  `creator_uid` BIGINT NOT NULL COMMENT 'assignment creator uid',
-  `create_time` DATETIME COMMENT 'record create time',
 
-  PRIMARY KEY (`id`)
-)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT '用户认证审核权限配置表';
 
 
 -- AUTHOR: 黄良铭
