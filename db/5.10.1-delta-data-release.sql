d09ca55d64d763ba609fc90e5b610b3f09ac50a1
huangliangming <liangming.huang@zuolin.com>
2018-11-06 17:33:41 +0800

    5.10.1:merge issue-38802 add sql

--- db/5.10.1-delta-data-release.sql
+++ db/5.10.1-delta-data-release.sql
@@ -9,6 +9,11 @@
 -- DESCRIPTION: 此SECTION放所有域空间都需要执行的脚本，包含基线、独立部署、研发数据等环境
 -- AUTHOR:
 -- REMARK:
+
+-- AUTHOR: 梁燕龙
+-- REMARK: issue-38802
+INSERT INTO `eh_locale_strings` (scope, code, locale, text)
+    VALUES ('activity', 10033,'zh_CN','请到活动详情页扫码签到');
 -- --------------------- SECTION END ALL -----------------------------------------------------
 -- --------------------- SECTION BEGIN -------------------------------------------------------
 -- ENV: zuolin-base
