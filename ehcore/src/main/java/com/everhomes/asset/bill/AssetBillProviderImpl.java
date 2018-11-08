39cf97eb7050790928c670c03f70db732cb1d3af
cx.yang <cx.yang@zuolin.com>
2018-11-08 14:37:44 +0800

    issue-41301 批量删除合同产生的相关账单、费项明细数据（合同模块调用）

--- ehcore/src/main/java/com/everhomes/asset/bill/AssetBillProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/asset/bill/AssetBillProviderImpl.java
@@ -44,6 +44,7 @@ public class AssetBillProviderImpl implements AssetBillProvider {
                     .and(Tables.EH_PAYMENT_BILLS.OWNER_TYPE.eq(ownerType))
                     .and(Tables.EH_PAYMENT_BILLS.OWNER_ID.eq(ownerId))
                     .and(Tables.EH_PAYMENT_BILLS.STATUS.notEqual(AssetPaymentBillStatus.PAID.getCode())) //已支付的不允许删除
+                    .and(Tables.EH_PAYMENT_BILLS.DELETE_FLAG.eq(AssetPaymentBillDeleteFlag.VALID.getCode()))
                     .and(Tables.EH_PAYMENT_BILLS.ID.in(billIdList))
                     .execute();
             //删除费项（置状态）
@@ -53,6 +54,7 @@ public class AssetBillProviderImpl implements AssetBillProvider {
                 .and(Tables.EH_PAYMENT_BILL_ITEMS.OWNER_TYPE.eq(ownerType))
                 .and(Tables.EH_PAYMENT_BILL_ITEMS.OWNER_ID.eq(ownerId))
                 .and(Tables.EH_PAYMENT_BILL_ITEMS.STATUS.notEqual(AssetPaymentBillStatus.PAID.getCode())) //已支付的不允许删除
+                .and(Tables.EH_PAYMENT_BILLS.DELETE_FLAG.eq(AssetPaymentBillDeleteFlag.VALID.getCode()))
                 .and(Tables.EH_PAYMENT_BILL_ITEMS.ID.in(billIdList))
 	            .execute();
             return null;
@@ -68,6 +70,7 @@ public class AssetBillProviderImpl implements AssetBillProvider {
 				.and(r.OWNER_ID.eq(ownerId))
 				.and(r.CONTRACT_ID.eq(contractId))
 				.and(r.STATUS.eq(AssetPaymentBillStatus.PAID.getCode()))
+				.and(r.DELETE_FLAG.eq(AssetPaymentBillDeleteFlag.VALID.getCode()))
 				.fetchInto(PaymentBills.class);
         if(list != null && list.size() > 0) {
         	return AssetPaymentBillStatus.PAID.getCode();
