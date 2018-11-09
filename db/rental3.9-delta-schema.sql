06f022d6fe4300a73ff2dceda28794efcb61c85f
st.zheng <st.zheng@zuolin.com>
2018-11-09 14:47:25 +0800

    rental3.9

--- db/rental3.9-delta-schema.sql
+++ db/rental3.9-delta-schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE `eh_rentalv2_price_classification` (
 `source_type`  varchar(255) NULL ,
 `owner_id`  bigint(20) NULL ,
 `owner_type`  varchar(255) NULL ,
+`user_price_type`  tinyint(4) NULL ,
 `classification`  varchar(255) NULL ,
 `workday_price`  decimal(10,2) NULL ,
 `original_price`  decimal(10,2) NULL ,
@@ -13,6 +14,7 @@ CREATE TABLE `eh_rentalv2_price_classification` (
 `full_price`  decimal(10,2) NULL ,
 `cut_price`  decimal(10,2) NULL ,
 `discount_ratio`  double NULL ,
+`resource_type`  varchar(255) NULL
 PRIMARY KEY (`id`)
 )ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
