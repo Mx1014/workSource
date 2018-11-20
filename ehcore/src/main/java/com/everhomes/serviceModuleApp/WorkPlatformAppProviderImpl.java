f93017b9a7c0515d3a4f0f77150d36cce53c6dd3
liangyanlong <yanlong.liang@zuolin.com>
2018-11-20 11:18:51 +0800

    issue-41688

--- ehcore/src/main/java/com/everhomes/serviceModuleApp/WorkPlatformAppProviderImpl.java
+++ ehcore/src/main/java/com/everhomes/serviceModuleApp/WorkPlatformAppProviderImpl.java
@@ -40,9 +40,11 @@ public class WorkPlatformAppProviderImpl implements WorkPlatformAppProvider{
                 .and(Tables.EH_WORK_PLATFORM_APPS.SCOPE_ID.eq(scopeId))
                 .and(Tables.EH_WORK_PLATFORM_APPS.SCOPE_TYPE.eq(ScopeType.ORGANIZATION.getCode()));
 
-        step.where(condition).fetchAny().map((r) ->{
-            return ConvertHelper.convert(r, WorkPlatformApp.class);
-        });
+        if (step.where(condition).fetchAny() != null) {
+            step.where(condition).fetchAny().map((r) ->{
+                return ConvertHelper.convert(r, WorkPlatformApp.class);
+            });
+        }
         return null;
     }
 
@@ -84,10 +86,12 @@ public class WorkPlatformAppProviderImpl implements WorkPlatformAppProvider{
                 .and(Tables.EH_WORK_PLATFORM_APPS.SCOPE_ID.eq(scopeId))
                 .and(Tables.EH_WORK_PLATFORM_APPS.SCOPE_TYPE.eq(ScopeType.ORGANIZATION.getCode()))
                 .and(Tables.EH_WORK_PLATFORM_APPS.ORDER.gt(sortNum));
-        step.where(condition).orderBy(Tables.EH_WORK_PLATFORM_APPS.ORDER.asc()).fetchAny().map((r) ->{
-            list.add(ConvertHelper.convert(r, WorkPlatformApp.class));
-            return null;
-        });
+        if (step.where(condition).orderBy(Tables.EH_WORK_PLATFORM_APPS.ORDER.asc()).fetchAny() != null) {
+            step.where(condition).orderBy(Tables.EH_WORK_PLATFORM_APPS.ORDER.asc()).fetchAny().map((r) ->{
+                list.add(ConvertHelper.convert(r, WorkPlatformApp.class));
+                return null;
+            });
+        }
         return list;
     }
 
@@ -99,10 +103,12 @@ public class WorkPlatformAppProviderImpl implements WorkPlatformAppProvider{
         Condition condition = Tables.EH_WORK_PLATFORM_APPS.APP_ID.eq(appOriginId)
                 .and(Tables.EH_WORK_PLATFORM_APPS.SCOPE_ID.eq(scopeId))
                 .and(Tables.EH_WORK_PLATFORM_APPS.SCOPE_TYPE.eq(ScopeType.ORGANIZATION.getCode()));
-        step.where(condition).orderBy(Tables.EH_WORK_PLATFORM_APPS.ORDER.asc()).fetchAny().map((r) ->{
-            list.add(ConvertHelper.convert(r, WorkPlatformApp.class));
-            return null;
-        });
+        if (step.where(condition).orderBy(Tables.EH_WORK_PLATFORM_APPS.ORDER.asc()).fetchAny() != null) {
+            step.where(condition).orderBy(Tables.EH_WORK_PLATFORM_APPS.ORDER.asc()).fetchAny().map((r) ->{
+                list.add(ConvertHelper.convert(r, WorkPlatformApp.class));
+                return null;
+            });
+        }
         return list;
     }
 
