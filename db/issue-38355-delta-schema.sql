f1ce2d6569cc16f6a805434ae28ae72f4e6eebf2
huqi <qi.hu@zuolin.com>
2018-11-22 13:51:21 +0800

    huqi

--- db/issue-38355-delta-schema.sql
+++ db/issue-38355-delta-schema.sql
@@ -20,3 +20,5 @@ CREATE TABLE `eh_flow_statistics_handle_log` (
 
 ALTER TABLE eh_flow_cases ADD COLUMN origin_app_id BIGINT COMMENT '应用 id';
 
+-- REMARK:　修改工作流记录处理表的表结构，添加‘泳道周期时长’字段
+ALTER TABLE `eh_flow_statistics_handle_log` ADD COLUMN flow_lanes_cycle BIGINT(32) DEFAULT 0 COMMENT '泳道周期时长（单位为秒 s）';
