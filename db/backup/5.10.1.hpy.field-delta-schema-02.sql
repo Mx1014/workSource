064567e13f4b0b25274830035eaa72908ba22855
shiheng.ma <shiheng.ma@zuolin.com>
2018-11-23 14:15:03 +0800

    5.11.0合并5.10.2

--- db/backup/5.10.1.hpy.field-delta-schema-02.sql
+++ db/backup/5.10.1.hpy.field-delta-schema-02.sql
@@ -123,26 +123,6 @@ ALTER TABLE `eh_service_alliances`	ADD COLUMN `update_uid` BIGINT NULL COMMENT '
 ALTER TABLE `eh_service_alliance_application_records` ADD COLUMN `update_time` DATETIME NULL DEFAULT NULL COMMENT '更新时间' ;
 -- end
 
--- AUTHOR: 李清岩
--- REMARK: 门禁v3.0.2 issue-34771
--- REMARK: 增加门禁组门禁关系表
-CREATE TABLE `eh_aclink_group_doors` (
-	`id` BIGINT  NOT NULL,
-	`namespace_id` INT  DEFAULT NULL COMMENT '域空间id',
-	`owner_id` BIGINT  NOT NULL DEFAULT '0' COMMENT '门禁组所属机构id',
-	`owner_type` TINYINT DEFAULT NULL COMMENT '门禁组所属机构类型 0园区 1公司',
-	`group_id` BIGINT DEFAULT NULL COMMENT '门禁组id',
-	`door_id` BIGINT DEFAULT NULL COMMENT '门禁组所属门禁id',
-	`status` TINYINT NOT NULL DEFAULT '1' COMMENT '状态 0删除 1有效',
-	`creator_uid` BIGINT  DEFAULT NULL COMMENT '创建者id',
-	`create_time` datetime DEFAULT NULL COMMENT '创建时间',
-	`operator_uid` BIGINT  DEFAULT NULL COMMENT '修改者id',
-	`operator_time` datetime DEFAULT NULL COMMENT '修改时间',
-	PRIMARY KEY (`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COMMENT = '门禁组门禁关系表';
--- AUTHOR: 李清岩
--- REMARK: 门禁v3.0.2 issue-34771
--- REMARK: 门禁iPad自定义logo
-ALTER TABLE `eh_aclink_ipads` ADD COLUMN `logo_uri` VARCHAR(2048) DEFAULT NULL COMMENT 'logo uri' ;
-ALTER TABLE `eh_aclink_ipads` ADD COLUMN `logo_url` VARCHAR(2048) DEFAULT NULL COMMENT 'logo url' ;
-ALTER TABLE `eh_door_access` CHANGE adress_detail address_detail VARCHAR(64);
+-- AUTHOR: 黄明波
+-- REMARK: issue-41586
+ALTER TABLE `eh_siyin_print_orders` ADD COLUMN `user_notify_flag` TINYINT(4) NULL DEFAULT '0' COMMENT '0-未起定时器通知用户 1-已起定时器通知用户';
