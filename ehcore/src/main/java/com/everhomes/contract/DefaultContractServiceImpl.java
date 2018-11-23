46e994ce106cd0f5aada331f8c8d645812a3e223
dingjianmin <jm.ding@zuolin.com>
2018-11-23 16:50:39 +0800

    根据合同id,自动刷新合同账单,删除所有的合同账单信息

--- ehcore/src/main/java/com/everhomes/contract/DefaultContractServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/contract/DefaultContractServiceImpl.java
@@ -3973,43 +3973,40 @@ public class DefaultContractServiceImpl implements ContractService, ApplicationL
 		//分割合同id String
         List<Long> contractIdlist = Arrays.asList(cmd.getContractIds().split(",")).stream().map(s -> Long.parseLong(s.trim())).collect(Collectors.toList());
 
-        for (Long contractId : contractIdlist) {
-        	try {Contract contract = contractProvider.findContractById(contractId);
-        	if (contract == null) {
-				continue ;
-			}
-        	FindContractCommand command = new FindContractCommand();
-			command.setId(contractId);
-			command.setPartyAId(contract.getPartyAId());
-			command.setCommunityId(contract.getCommunityId());
-			command.setNamespaceId(contract.getNamespaceId());
-			command.setCategoryId(contract.getCategoryId());
-			ContractDetailDTO contractDetailDTO = findContract(command);
-
-			//生成正常合同清单
-			ExecutorUtil.submit(new Runnable() {
-				@Override
-				public void run() {
-					generatePaymentExpectancies(contract, contractDetailDTO.getChargingItems(), contractDetailDTO.getAdjusts(), contractDetailDTO.getFrees());
-					//判断是否为正常合同，为正常合同进行入场
-					if (contract.getStatus() == ContractStatus.ACTIVE.getCode()) {//由于合同入场的接口有Bug，导致无法更新账单，所以如果是正常合同，那么直接调用代码
-							assetService.upodateBillStatusOnContractStatusChange(contract.getId(), AssetPaymentConstants.CONTRACT_SAVE);
-						//// 合同入场
-//						EntryContractCommand entryContractCommand = new EntryContractCommand();
-//						entryContractCommand.setCategoryId(contract.getCategoryId());
-//						entryContractCommand.setCommunityId(contract.getCommunityId());
-//						entryContractCommand.setId(contract.getId());
-//						entryContractCommand.setNamespaceId(contract.getNamespaceId());
-//						entryContractCommand.setOrgId(contract.getPartyAId());
-//						entryContractCommand.setPartyAId(contract.getPartyAId());
-//						entryContract(entryContractCommand);
-					}
+		for (Long contractId : contractIdlist) {
+			try {
+				Contract contract = contractProvider.findContractById(contractId);
+				if (contract == null) {
+					continue;
 				}
-			});} catch (Exception e) {
+				// 删除所有的合同账单信息
+				assetProvider.deleteContractPaymentByContractId(contractId);
+				
+				FindContractCommand command = new FindContractCommand();
+				command.setId(contractId);
+				command.setPartyAId(contract.getPartyAId());
+				command.setCommunityId(contract.getCommunityId());
+				command.setNamespaceId(contract.getNamespaceId());
+				command.setCategoryId(contract.getCategoryId());
+				ContractDetailDTO contractDetailDTO = findContract(command);
+
+				// 生成正常合同清单
+				ExecutorUtil.submit(new Runnable() {
+					@Override
+					public void run() {
+						generatePaymentExpectancies(contract, contractDetailDTO.getChargingItems(),
+								contractDetailDTO.getAdjusts(), contractDetailDTO.getFrees());
+						// 判断是否为正常合同，为正常合同进行入场
+						if (contract.getStatus() == ContractStatus.ACTIVE.getCode()) {// 由于合同入场的接口有Bug，导致无法更新账单，所以如果是正常合同，那么直接调用代码
+							assetService.upodateBillStatusOnContractStatusChange(contract.getId(), AssetPaymentConstants.CONTRACT_SAVE);
+						}
+					}
+				});
+			} catch (Exception e) {
 				LOGGER.info("autoGeneratingBill is error , contract id " + contractId);
-				continue ;
+				continue;
 			}
-        }
+		}
 	}
 
 	public ContractTaskOperateLog initializationContract(InitializationCommand cmd) {
