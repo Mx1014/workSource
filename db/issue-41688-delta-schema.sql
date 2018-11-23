74b96000badd7f77ee98df63689e46324b0bfcb9
liangyanlong <yanlong.liang@zuolin.com>
2018-11-23 09:36:30 +0800

    issue-41688: 修改脚本

--- db/issue-41688-delta-schema.sql
+++ db/issue-41688-delta-schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE `eh_work_platform_apps` (
   `scope_id` BIGINT COMMENT '范围对象id',
   `order` INTEGER,
   `visible_flag` TINYINT COMMENT '可见性，0:不可见，1:可见',
-  `scene_type` TINYINT COMMENT '1:管理端，2：用户端',
+  `entry_id` BIGINT COMMENT '应用入口id',
   PRIMARY KEY (`id`),
   KEY `u_eh_recommend_app_scope_id` (`scope_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='工作台公司推荐应用';
