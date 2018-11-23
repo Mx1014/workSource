a03627a4848b8269d395a80f41a0f5ba3bd4dc15
shiheng.ma <shiheng.ma@zuolin.com>
2018-11-23 16:48:34 +0800

    issue-40150:金茂对接

--- db/visitorsys1.4-delta-schema.sql
+++ db/visitorsys1.4-delta-schema.sql
@@ -2,3 +2,37 @@
 -- REMARK: 访客1.4 到访类型修改
 ALTER TABLE `eh_visitor_sys_visit_reason` ADD COLUMN `reason_type`  tinyint NULL DEFAULT null COMMENT '类型 0为住宅小区,1为商业小区';
 ALTER TABLE `eh_visitor_sys_visit_reason` ADD COLUMN `visit_reason_code`  tinyint NULL DEFAULT null COMMENT '到访原因类型码';
+
+
+-- AUTHOR: 马世亨 20181122
+-- REMARK: 访客1.4 访客管理对接海康威视人员表
+CREATE TABLE `eh_visitor_sys_hkws_user` (
+`person_id` int NULL DEFAULT NULL COMMENT '人员ID',
+`person_no` varchar(256) NULL DEFAULT NULL COMMENT '人员编号',
+`person_name` varchar(256) NULL DEFAULT NULL COMMENT '姓名',
+`gender` tinyint NULL DEFAULT NULL COMMENT '性别',
+`certificate_type` int NULL DEFAULT NULL COMMENT '证件类型',
+`certificate_no` varchar(256) NULL DEFAULT NULL COMMENT '证件号码',
+`birthday` bigint NULL DEFAULT NULL COMMENT '出生日期',
+`person_pinyin` varchar(256) NULL DEFAULT NULL COMMENT '姓名拼音',
+`phone_no` varchar(256) NULL DEFAULT NULL COMMENT '联系电话',
+`address` varchar(256) NULL DEFAULT NULL COMMENT '联系地址',
+`photo` varchar(256) NULL DEFAULT NULL COMMENT '免冠照',
+`english_name` varchar(256) NULL DEFAULT NULL COMMENT '英文名',
+`email` varchar(256) NULL DEFAULT NULL COMMENT '邮箱',
+`entry_date` bigint NULL DEFAULT NULL COMMENT '入职日期',
+`leave_date` bigint NULL DEFAULT NULL COMMENT '离职日期',
+`education` varchar(256) NULL DEFAULT NULL COMMENT '学历',
+`nation` varchar(256) NULL DEFAULT NULL COMMENT '民族',
+`dept_uuid` varchar(256) NULL DEFAULT NULL COMMENT '所属部门UUID',
+`dept_no` varchar(256) NULL DEFAULT NULL COMMENT '所属部门编号',
+`dept_name` varchar(256) NULL DEFAULT NULL COMMENT '所属部门名称',
+`dept_path` varchar(256) NULL DEFAULT NULL COMMENT '所属部门路径',
+`status` tinyint NULL DEFAULT NULL COMMENT '人员状态',
+`identity_uuid` varchar(256) NULL DEFAULT NULL COMMENT '身份UUID',
+`identity_name` varchar(256) NULL DEFAULT NULL COMMENT '身份名称',
+`create_time` bigint NULL DEFAULT NULL COMMENT '创建时间',
+`update_time` bigint NULL DEFAULT NULL COMMENT '更新时间',
+`remark` varchar(256) NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='访客管理对接海康威视人员表';
--- ehcore/src/main/java/com/everhomes/visitorsys/VisitorSysHKWSUtil.java
+++ ehcore/src/main/java/com/everhomes/visitorsys/VisitorSysHKWSUtil.java
@@ -9,6 +9,11 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class VisitorSysHKWSUtil {
 
@@ -29,21 +34,47 @@ public class VisitorSysHKWSUtil {
         params.put("gender","1");
         params.put("phoneNo",visitor.getVisitorPhone());
         params.put("personNum","" + 1 + visitor.getFollowUpNumbers());
-        params.put("startTime",visitor.getVisitorName());
-        params.put("endTime",visitor.getVisitorName());
+
+        Timestamp time = visitor.getPlannedVisitTime();
+        DateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+        DateFormat edFormat = new SimpleDateFormat("yyyy-MM-dd 23:59:59");
+        String st = sdFormat.format(time);
+        String et = edFormat.format(time);
+        params.put("startTime",Timestamp.valueOf(st).getTime());
+        params.put("endTime",Timestamp.valueOf(et).getTime());
         params.put("identityType","0");//0为身份证
-        params.put("certificateNo",visitor.getVisitorName());
-        params.put("isVip",visitor.getVisitorName());
-        params.put("purpose",visitor.getVisitorName());
-        params.put("plateNo",visitor.getVisitorName());
+        params.put("certificateNo",visitor.getIdNumber());
+        params.put("isVip","0");//0 非VIP 1 VIP
+        params.put("purpose",visitor.getVisitReason());
+        params.put("plateNo",visitor.getPlateNo());
 
 
-        HKWSHandler.post(ADD_APPOINTMENT,params);
+        HkwsThirdResponse resp = HKWSHandler.post(ADD_APPOINTMENT,params);
         return "";
     }
 
     public String delAppointment(){
         return "";
+    }
+
+    public void initHKWSUsers(){
+
+        JSONObject params = new JSONObject();
+
+        params.put("pageNo",0);
+        params.put("pageSize",400);
+        params.put("opUserUuid",HKWSHandler.getOpUserUuid(false));
+        params.put("startTime",Long.MIN_VALUE);
+        params.put("endTime",Long.MIN_VALUE);
+        HkwsThirdResponse resp = HKWSHandler.post(GET_PERSON_INFO,params);
+        if(!HKWSHandler.isSuccess(resp)){
+
+        }
+
+//        while () {}
+
+
+
     }
 
 }
