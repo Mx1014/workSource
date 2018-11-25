0c603b4ba262ac641102694d0d9f705ba246dd53
mengqianxiang <qx.meng@zuolin.com>
2018-11-25 21:34:32 +0800

    交易明细与下载中心对接完成，issue-42274BUG解决

---
+++ db/5.11.0-delta-data-beta.sql
@@ -0,0 +1,25 @@
+-- AUTHOR: 孟千翔 2018-11-25
+-- REMARK: 添加交易明细导出表头及数据标签信息
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","14001","zh_CN","账单时间");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12002","zh_CN","账单组");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12003","zh_CN","收费项信息");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12004","zh_CN","客户名称");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12005","zh_CN","客户类型");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12006","zh_CN","订单状态");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12007","zh_CN","支付方式");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12008","zh_CN","实收金额");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12009","zh_CN","应收金额");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12010","zh_CN","减免");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12011","zh_CN","增收");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","13012","zh_CN","订单编号");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12013","zh_CN","缴费时间");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12014","zh_CN","缴费人电话");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","12015","zh_CN","缴费人");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","14016","zh_CN","楼栋门牌");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","20001","zh_CN","个人客户");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","20002","zh_CN","企业客户");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","20003","zh_CN","已完成");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","20004","zh_CN","微信");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","20005","zh_CN","支付宝");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","20006","zh_CN","对公转账");
+INSERT INTO eh_locale_strings (`scope`,`code`,`locale`,`text`) VALUES("export.paymentbill","30001","zh_CN","交易明细");
