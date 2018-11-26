45bbd1f9230809e12f5a8afbb5aca339ff18ecf4
Zhou.Miao <zhou.miao@zuolin.com>
2018-11-26 15:36:36 +0800

    工会预定

---
+++ ehrest/src/main/java/com/everhomes/rest/officecubicle/DeleteRoomAdminCommand.java
@@ -0,0 +1,66 @@
+// @formatter:off
+package com.everhomes.rest.officecubicle;
+
+import com.everhomes.util.StringHelper;
+
+/**
+ * <ul>
+ * <li>namespaceId: 域空间id</li>
+ * <li>ownerType: 归属的类型，{@link com.everhomes.rest.parking.ParkingOwnerType}</li>
+ * <li>ownerId: 归属的ID，如小区ID</li>
+ * <li>spaceId: 停车场ID</li>
+ * <li>roomId: 办公室id</li>
+ * </ul>
+ */
+public class DeleteRoomAdminCommand {
+    private Integer namespaceId;
+    private String ownerType;
+    private Long ownerId;
+    private Long spaceId;
+    private Long roomId;
+
+
+    public Integer getNamespaceId() {
+        return namespaceId;
+    }
+
+    public void setNamespaceId(Integer namespaceId) {
+        this.namespaceId = namespaceId;
+    }
+
+    public String getOwnerType() {
+        return ownerType;
+    }
+
+    public void setOwnerType(String ownerType) {
+        this.ownerType = ownerType;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Long getSpaceId() {
+		return spaceId;
+	}
+
+	public void setSpaceId(Long spaceId) {
+		this.spaceId = spaceId;
+	}
+	public Long getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(Long roomId) {
+		this.roomId = roomId;
+	}
+
+	@Override
+    public String toString() {
+        return StringHelper.toJsonString(this);
+    }
+}
