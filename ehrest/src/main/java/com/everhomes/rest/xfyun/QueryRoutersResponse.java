4267526e7da3cac33a123242830805d54fbdd1bf
huangmingbo <mingbo.huang@zuolin.com>
2018-11-26 12:16:36 +0800

    金茂对接 接口修改

--- ehrest/src/main/java/com/everhomes/rest/xfyun/QueryRoutersResponse.java
+++ ehrest/src/main/java/com/everhomes/rest/xfyun/QueryRoutersResponse.java
@@ -7,25 +7,24 @@ import com.everhomes.util.StringHelper;
 
 /**
  * <ul>
- * <li>routerDtos: 跳转路径，{@link com.everhomes.rest.launchpadbase.AppDTO}</li>
+ * <li>routerDtos: 跳转路径，{@link com.everhomes.rest.xfyun.RouterDTO}</li>
  * </ul>
  */
 public class QueryRoutersResponse {
-	
-	private List<AppDTO> routerDtos;
-	
+
+	private List<RouterDTO> routerDtos;
+
 	@Override
 	public String toString() {
 		return StringHelper.toJsonString(this);
 	}
 
-	public List<AppDTO> getRouterDtos() {
+	public List<RouterDTO> getRouterDtos() {
 		return routerDtos;
 	}
 
-	public void setRouterDtos(List<AppDTO> routerDtos) {
+	public void setRouterDtos(List<RouterDTO> routerDtos) {
 		this.routerDtos = routerDtos;
 	}
-
 
 }
