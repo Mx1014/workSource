a4052713bbf81a661c7793a5d5cf5841888bc488
chenhe <he.chen@zuolin.com>
2018-11-27 10:33:43 +0800

    删除无用import

--- ehcore/src/main/java/com/everhomes/openapi/zhenzhihui/ZhenzhihuiOpenApiRedirectHandler.java
+++ ehcore/src/main/java/com/everhomes/openapi/zhenzhihui/ZhenzhihuiOpenApiRedirectHandler.java
@@ -9,8 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Map;
 
