644a60d660f034a48a21ecc333f9a85d40bf41fb
Zhou.Miao <zhou.miao@zuolin.com>
2018-11-29 10:14:08 +0800

    工会预定V2.4

--- db/5.11.0-yuanqu-dev2-delta-schema.sql
+++ db/5.11.0-yuanqu-dev2-delta-schema.sql
@@ -105,6 +105,7 @@ CREATE TABLE `eh_office_cubicle_charge_users` (
   `namespace_id` INTEGER,
   `owner_type` VARCHAR(32) NOT NULL COMMENT 'community 园区或者其他类型',
   `owner_id` BIGINT NOT NULL COMMENT '园区id或者其他id',
+  `space_id` BIGINT,
   `charge_uid` BIGINT,
   `charge_name` VARCHAR(32),
   `create_time` DATETIME,
