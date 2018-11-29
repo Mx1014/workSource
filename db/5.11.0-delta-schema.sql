bddc07f09cf57f5acee98411c63c88bc0efb8915
cx.yang <cx.yang@zuolin.com>
2018-11-29 13:52:58 +0800

    issue-42424 merge sql to 5.11.0

--- db/5.11.0-delta-schema.sql
+++ db/5.11.0-delta-schema.sql
@@ -405,4 +405,13 @@ ALTER TABLE `eh_siyin_print_business_payee_accounts` MODIFY COLUMN `payee_id` BI
 -- AUTHOR: 黄鹏宇 20181128
 -- REMARK: 去除创建管理员一定要有用户的限制
 ALTER TABLE eh_enterprise_customer_admins MODIFY creator_uid BIGINT NULL;
+
+
+-- AUTHOR: 杨崇鑫 20181128
+-- REMARK: 缺陷 #42424 【智谷汇】保证金设置为固定金额，但是实际会以合同签约门牌的数量计价。实际上保证金是按照合同收费，不是按照门牌的数量进行重复计费。 给智谷汇的权限
+ALTER TABLE `eh_contract_charging_items` ADD COLUMN `one_time_bill_status` TINYINT COMMENT '是否是一次性产生费用，1：是，0：否';
+
+
+
+
 -- END
