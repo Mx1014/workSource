94119e6925bc9f7b1a822841f31c93b96eb6532a
liangyanlong <yanlong.liang@zuolin.com>
2018-12-05 20:32:50 +0800

    5.11.1-ruian: 瑞安定制

--- db/5.11.1-ruian-delta-schema-02.sql
+++ db/5.11.1-ruian-delta-schema-02.sql
@@ -225,4 +225,4 @@ CREATE TABLE `eh_contract_statistic_communities` (
 
 -- AUTHOR: 莫谋斌
 -- REMARK: eh_communities增加字段；
-ALTER TABLE eh_communities ADD COLUMN background_img_url VARCHAR DEFAULT '' COMMENT '小区或园区项目的图片链接';
+ALTER TABLE eh_communities ADD COLUMN background_img_url VARCHAR(500) DEFAULT '' COMMENT '小区或园区项目的图片链接';
