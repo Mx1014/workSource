80d2129224d4b98188314dcb7874620e9093de98
liangyanlong <yanlong.liang@zuolin.com>
2018-12-06 12:27:17 +0800

    issue-41688: 发布时，增加入口ID。识别入口

--- ehrest/src/main/java/com/everhomes/rest/portal/ItemActionData.java
+++ ehrest/src/main/java/com/everhomes/rest/portal/ItemActionData.java
@@ -7,14 +7,24 @@ import com.everhomes.util.StringHelper;
  * <ul>
  *     <li>layoutId: 门户id</li>
  *     <li>moduleAppId: 应用id</li>
+ *     <li>moduleEntryId: 入口ID</li>
  * </ul>
  */
 public class ItemActionData {
 
 	private Long layoutId;
 	private Long moduleAppId;
+    private Long moduleEntryId;
 
-	public Long getLayoutId() {
+    public Long getModuleEntryId() {
+        return moduleEntryId;
+    }
+
+    public void setModuleEntryId(Long moduleEntryId) {
+        this.moduleEntryId = moduleEntryId;
+    }
+
+    public Long getLayoutId() {
 		return layoutId;
 	}
 
