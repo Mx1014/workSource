4aaa9dd22064778bbf1252fe13376fef3562b058
steve <cen.tang@zuolin.com>
2018-12-06 11:45:40 +0800

    issue-43040：添加sql到5.11.1-delta-schema.sql文件中

--- db/5.11.1-delta-schema.sql
+++ db/5.11.1-delta-schema.sql
@@ -226,3 +226,11 @@ CREATE TABLE `eh_contract_statistic_communities` (
 -- AUTHOR: 莫谋斌
 -- REMARK: eh_communities增加字段；
 ALTER TABLE eh_communities ADD COLUMN background_img_url VARCHAR DEFAULT '' COMMENT '小区或园区项目的图片链接';
+
+-- AUTHOR: tangcen
+-- REMARK: 增加楼宇资产管理统计字段
+ALTER TABLE eh_property_statistic_community ADD COLUMN signedup_apartment_count int(11) DEFAULT 0 COMMENT '园区下的待签约房源数' AFTER saled_apartment_count;
+ALTER TABLE eh_property_statistic_community ADD COLUMN waitingroom_apartment_count int(11) DEFAULT 0 COMMENT '园区下的待接房房源数' AFTER signedup_apartment_count;
+ALTER TABLE eh_property_statistic_building ADD COLUMN signedup_apartment_count int(11) DEFAULT 0 COMMENT '楼宇内的待签约房源数' AFTER saled_apartment_count;
+ALTER TABLE eh_property_statistic_building ADD COLUMN waitingroom_apartment_count int(11) DEFAULT 0 COMMENT '楼宇内的待接房房源数' AFTER signedup_apartment_count;
+
