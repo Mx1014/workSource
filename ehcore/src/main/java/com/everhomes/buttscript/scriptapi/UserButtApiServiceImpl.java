0d2c98cf93d2e240e28e5d6a1314dbd953663a60
liangyanlong <yanlong.liang@zuolin.com>
2018-12-07 09:54:14 +0800

    5.11.1-ruian: 会员升级后，推送消息给该用户

--- ehcore/src/main/java/com/everhomes/buttscript/scriptapi/UserButtApiServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/buttscript/scriptapi/UserButtApiServiceImpl.java
@@ -1,14 +1,24 @@
 package com.everhomes.buttscript.scriptapi;
 
 import com.everhomes.flow.*;
+import com.everhomes.messaging.MessagingService;
 import com.everhomes.organization.OrganizationService;
+import com.everhomes.rest.app.AppConstants;
 import com.everhomes.rest.community.CommunityType;
 import com.everhomes.rest.flow.FlowEventType;
+import com.everhomes.rest.messaging.ChannelType;
+import com.everhomes.rest.messaging.MessageBodyType;
+import com.everhomes.rest.messaging.MessageChannel;
+import com.everhomes.rest.messaging.MessageDTO;
+import com.everhomes.rest.messaging.MessageMetaConstant;
+import com.everhomes.rest.messaging.MessagingConstants;
 import com.everhomes.rest.organization.OrganizationDTO;
 import com.everhomes.rest.organization.OrganizationGroupType;
 import com.everhomes.rest.organization.OrganizationMemberStatus;
 import com.everhomes.scriptengine.nashorn.NashornModuleApiService;
+import com.everhomes.user.User;
 import com.everhomes.user.UserService;
+import com.everhomes.util.StringHelper;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +39,9 @@ public class UserButtApiServiceImpl implements NashornModuleApiService {
 
     @Autowired
     private OrganizationService organizationService;
+
+    @Autowired
+    private MessagingService messagingService;
 
     @Override
     public String name() {
@@ -75,6 +88,20 @@ public class UserButtApiServiceImpl implements NashornModuleApiService {
         return count ;
     }
 
+    private void sendVipLevelMessageToUser(Long userId, String levelName) {
+        String notifyTextForApplicant = "您已成为" + levelName + "会员";
+        MessageDTO messageDto = new MessageDTO();
+        messageDto.setAppId(AppConstants.APPID_MESSAGING);
+        messageDto.setSenderUid(User.SYSTEM_UID);
+        messageDto.setBodyType(MessageBodyType.NOTIFY.getCode());
+        messageDto.setBody(notifyTextForApplicant);
+        messageDto.setMetaAppId(AppConstants.APPID_ENTERPRISE);
+        messageDto.setChannels(new MessageChannel(ChannelType.USER.getCode(), String.valueOf(userId)));
+        messagingService.routeMessage(User.SYSTEM_USER_LOGIN,
+                AppConstants.APPID_MESSAGING, ChannelType.USER.getCode(), String.valueOf(userId),
+                messageDto, MessagingConstants.MSG_FLAG_STORED_PUSH.getCode());
+
+    }
 
     public void testCall() {
         LOGGER.debug("this is test api call");
