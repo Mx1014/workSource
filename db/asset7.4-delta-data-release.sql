d1806cac509b9fcd4d054808b71bcc7127e17bdf
cx.yang <cx.yang@zuolin.com>
2018-12-07 19:52:23 +0800

    瑞安CM对接：开启支付权限

--- db/asset7.4-delta-data-release.sql
+++ db/asset7.4-delta-data-release.sql
@@ -68,6 +68,11 @@
 SET @id = ifnull((SELECT MAX(id) FROM `eh_payment_app_views`),0);
 INSERT INTO `eh_payment_app_views`(`id`, `namespace_id`, `community_id`, `has_view`, `view_item`, `remark1_type`, `remark1_identifier`, `remark2_type`, `remark2_identifier`, `remark3_type`, `remark3_identifier`) 
 	VALUES (@id := @id + 1, 999929, NULL, 1, 'CONFIRM', NULL, NULL, NULL, NULL, NULL, NULL);
+	
+-- AUTHOR: 杨崇鑫 2018-12-07
+-- REMARK: 瑞安CM对接：开启支付权限
+update eh_payment_app_views set has_view = 1 where namespace_id=999929 and view_item = 'PAY';	
+
 
 
 
