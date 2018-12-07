f40f948bc8ab4f14dc9ad85c13866d5f7fa4e9dd
cx.yang <cx.yang@zuolin.com>
2018-12-07 17:59:09 +0800

    asset7.4 merge 5.11.1

--- db/asset7.4-delta-data-release.sql
+++ db/asset7.4-delta-data-release.sql
@@ -65,7 +65,7 @@
 -- DESCRIPTION: 此SECTION只在上海瑞安新天地-999929执行的脚本
 -- AUTHOR: 杨崇鑫 2018-12-06
 -- REMARK: 瑞安CM对接 :APP端显示的支付状态是“已支付，待确认”。
-SET @id = ifnull((SELECT MAX(id) FROM `eh_payment_app_views`),0); 
+SET @id = ifnull((SELECT MAX(id) FROM `eh_payment_app_views`),0);
 INSERT INTO `eh_payment_app_views`(`id`, `namespace_id`, `community_id`, `has_view`, `view_item`, `remark1_type`, `remark1_identifier`, `remark2_type`, `remark2_identifier`, `remark3_type`, `remark3_identifier`) 
 	VALUES (@id := @id + 1, 999929, NULL, 1, 'CONFIRM', NULL, NULL, NULL, NULL, NULL, NULL);
 
