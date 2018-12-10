e30f21574364ce9223a6f99f9ea094ef13ee6630
dingjianmin <jm.ding@zuolin.com>
2018-12-10 17:28:55 +0800

    contract4.4:save work

--- db/contract4.4-delta-data-release.sql
+++ db/contract4.4-delta-data-release.sql
@@ -4,10 +4,10 @@
 UPDATE `eh_service_module_functions` SET  `explain`='打印' WHERE `id`='21216';
 
 SET @id = (SELECT MAX(id) from eh_service_module_privileges);
-INSERT INTO `eh_service_module_privileges`(`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) VALUES (@id:=@id+1 , 21210, 0, 21226, '生成合同文档', 0, SYSDATE());
+INSERT INTO `eh_service_module_privileges`(`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) VALUES (@id:=@id+1 , 21210, 0, 21226, '生成合同文档', 12, SYSDATE());
 INSERT INTO `eh_acl_privileges`(`id`, `app_id`, `name`, `description`, `tag`) VALUES (21226, 0, '合同管理 生成合同文档', '合同管理生成合同文档权限', NULL);
 INSERT INTO `eh_service_module_functions` (`id`, `module_id`, `privilege_id`, `explain`) VALUES (21226, '21200', '21226', '生成合同文档');
 
-INSERT INTO `eh_service_module_privileges`(`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) VALUES (@id:=@id+1 , 21210, 0, 21227, '查看合同文档权限', 0, SYSDATE());
+INSERT INTO `eh_service_module_privileges`(`id`, `module_id`, `privilege_type`, `privilege_id`, `remark`, `default_order`, `create_time`) VALUES (@id:=@id+1 , 21210, 0, 21227, '查看合同文档', 13, SYSDATE());
 INSERT INTO `eh_acl_privileges`(`id`, `app_id`, `name`, `description`, `tag`) VALUES (21227, 0, '合同管理 查看合同文档', '合同管理 查看合同文档权限', NULL);
 INSERT INTO `eh_service_module_functions` (`id`, `module_id`, `privilege_id`, `explain`) VALUES (21227, '21200', '21227', '查看合同文档');
