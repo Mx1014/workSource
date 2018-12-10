b0ac231ae6dfe01de5384d25b6ddcee783a7e3eb
st.zheng <st.zheng@zuolin.com>
2018-12-10 11:25:21 +0800

    资源预约3.8.3

--- db/rental3.8.3-delta-schema.sql
+++ db/rental3.8.3-delta-schema.sql
@@ -5,7 +5,7 @@ ADD COLUMN `detail_page_type`  tinyint(4) NULL COMMENT '资源详情样式 0:默
 ALTER TABLE `eh_rentalv2_default_rules`
 ADD COLUMN `preview_using_image_uri`  varchar(255) NULL COMMENT '资源概览 使用中图片' AFTER `file_flag`;
 ALTER TABLE `eh_rentalv2_default_rules`
-ADD COLUMN `priview_idle_image_uri`  varchar(255) NULL COMMENT '资源概览 空闲图片' AFTER `preview_using_image_uri`;
+ADD COLUMN `preview_idle_image_uri`  varchar(255) NULL COMMENT '资源概览 空闲图片' AFTER `preview_using_image_uri`;
 ALTER TABLE `eh_rentalv2_default_rules`
 ADD COLUMN `shop_name`  varchar(255) NULL COMMENT '关联商铺名' AFTER `priview_idle_image_uri`;
 ALTER TABLE `eh_rentalv2_default_rules`
