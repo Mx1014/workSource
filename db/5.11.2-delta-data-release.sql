bbb5551cd7a94837a08abaa97bbb1836dc7e09d9
shiheng.ma <shiheng.ma@zuolin.com>
2018-12-11 13:56:30 +0800

    issue-40150:鼎峰汇对接配置项

--- db/5.11.2-delta-data-release.sql
+++ db/5.11.2-delta-data-release.sql
@@ -13,6 +13,32 @@
 -- DESCRIPTION: 此SECTION放所有域空间都需要执行的脚本，包含基线、独立部署、研发数据等环境
 -- AUTHOR:
 -- REMARK:
+
+-- AUTHOR: 马世亨 20181211
+-- REMARK: 访客v1.4 添加住宅小区到访类型
+update eh_visitor_sys_visit_reason set reason_type = 1 , visit_reason_code = id;
+INSERT INTO `eh_visitor_sys_visit_reason` (`id`, `namespace_id`, `owner_type`, `owner_id`, `visit_reason`, `status`, `creator_uid`, `create_time`, `operator_uid`, `operate_time`, `reason_type`, `visit_reason_code`)
+VALUES ('11', '0', 'community', '0', '朋友', '2', '0', '2018-06-21 16:15:40', '0', '2018-06-21 16:15:40', 0, 0);
+INSERT INTO `eh_visitor_sys_visit_reason` (`id`, `namespace_id`, `owner_type`, `owner_id`, `visit_reason`, `status`, `creator_uid`, `create_time`, `operator_uid`, `operate_time`, `reason_type`, `visit_reason_code`)
+VALUES ('12', '0', 'community', '0', '外卖', '2', '0', '2018-06-21 16:15:40', '0', '2018-06-21 16:15:40', 0, 1);
+INSERT INTO `eh_visitor_sys_visit_reason` (`id`, `namespace_id`, `owner_type`, `owner_id`, `visit_reason`, `status`, `creator_uid`, `create_time`, `operator_uid`, `operate_time`, `reason_type`, `visit_reason_code`)
+VALUES ('13', '0', 'community', '0', '快递', '2', '0', '2018-06-21 16:15:40', '0', '2018-06-21 16:15:40', 0, 2);
+INSERT INTO `eh_visitor_sys_visit_reason` (`id`, `namespace_id`, `owner_type`, `owner_id`, `visit_reason`, `status`, `creator_uid`, `create_time`, `operator_uid`, `operate_time`, `reason_type`, `visit_reason_code`)
+VALUES ('14', '0', 'community', '0', '家政', '2', '0', '2018-06-21 16:15:40', '0', '2018-06-21 16:15:40', 0, 3);
+INSERT INTO `eh_visitor_sys_visit_reason` (`id`, `namespace_id`, `owner_type`, `owner_id`, `visit_reason`, `status`, `creator_uid`, `create_time`, `operator_uid`, `operate_time`, `reason_type`, `visit_reason_code`)
+VALUES ('15', '0', 'community', '0', '其它', '2', '0', '2018-06-21 16:15:40', '0', '2018-06-21 16:15:40', 0, 4);
+
+-- AUTHOR: 马世亨 20181211
+-- REMARK: 访客v1.4 访客鼎峰汇对接配置项
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.url.interface', 'http://syx.jslife.com.cn/jsaims/as', '访客鼎峰汇对接接口地址', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.cid', '880075500000001', '访客鼎峰汇对接客户号', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.version', '2', '访客鼎峰汇对接版本', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.signKey', '7ac3e2ee1075bf4bb6b816c1e80126c0', '访客鼎峰汇对接秘钥', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.url.login', 'http://syx.jslife.com.cn/jsaims/login', '访客鼎峰汇对接登录地址', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.usr', '880075500000001', '访客鼎峰汇对接客户号', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.psw', '888888', '访客鼎峰汇对接帐号', '0', NULL, '0');
+INSERT INTO `eh_configurations` (`name`, `value`, `description`, `namespace_id`, `display_name`, `is_readonly`) VALUES ('visitorsys.dingfenghui.parkCode', '0010028888', '访客鼎峰汇对接密码', '0', NULL, '0');
+
 -- --------------------- SECTION END ALL -----------------------------------------------------
 -- --------------------- SECTION BEGIN -------------------------------------------------------
 -- ENV: zuolin-base
