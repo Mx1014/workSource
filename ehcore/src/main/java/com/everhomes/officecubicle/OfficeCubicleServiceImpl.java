82c10c71d60c54d0695d6becf2faa2cbb886f44c
Zhou.Miao <zhou.miao@zuolin.com>
2018-12-12 16:21:29 +0800

    工位预定

--- ehcore/src/main/java/com/everhomes/officecubicle/OfficeCubicleServiceImpl.java
+++ ehcore/src/main/java/com/everhomes/officecubicle/OfficeCubicleServiceImpl.java
@@ -1851,7 +1851,10 @@ public class OfficeCubicleServiceImpl implements OfficeCubicleService {
 					officeCubicleProvider.updateCubicle(station);
 				}
 			}
-			room = ConvertHelper.convert(cmd, OfficeCubicleRoom.class);
+			room.setOwnerId(cmd.getOwnerId());
+			room.setOwnerType(cmd.getOwnerType());
+			room.setNamespaceId(cmd.getNamespaceId());
+			room.setSpaceId(cmd.getSpaceId());
 			room.setDescription(cmd.getDescription());
 			room.setNamespaceId(cmd.getNamespaceId());
 			room.setCoverUri(cmd.getCoverUri());
@@ -1873,7 +1876,10 @@ public class OfficeCubicleServiceImpl implements OfficeCubicleService {
 
 		this.dbProvider.execute((TransactionStatus status) -> {
 			OfficeCubicleStation station = officeCubicleProvider.getOfficeCubicleStationById(cmd.getStationId());
-			station = ConvertHelper.convert(cmd, OfficeCubicleStation.class);
+			station.setOwnerId(cmd.getOwnerId());
+			station.setOwnerType(cmd.getOwnerType());
+			station.setNamespaceId(cmd.getNamespaceId());
+			station.setSpaceId(cmd.getSpaceId());
 			station.setId(cmd.getStationId());
 			station.setDescription(cmd.getDescription());
 			station.setNamespaceId(cmd.getNamespaceId());
@@ -2629,6 +2635,7 @@ public class OfficeCubicleServiceImpl implements OfficeCubicleService {
 			dto.setRentDate(rentDate);
 			dto.setCoverUrl(this.contentServerService.parserUri(dto.getCoverUri(), EntityType.USER.getCode(),
 					UserContext.current().getUser().getId()));
+			dto.setStatus(r.getStatus());
 			return dto;
 			}).collect(Collectors.toList()));
 		}
