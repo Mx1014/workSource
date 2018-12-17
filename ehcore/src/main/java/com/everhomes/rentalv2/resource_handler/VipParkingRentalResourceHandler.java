1f9bb31c80604d77e84b799fd2a2bba82d52a5df
Zhou.Miao <zhou.miao@zuolin.com>
2018-12-17 20:47:42 +0800

    停车V6.7.7

--- ehcore/src/main/java/com/everhomes/rentalv2/resource_handler/VipParkingRentalResourceHandler.java
+++ ehcore/src/main/java/com/everhomes/rentalv2/resource_handler/VipParkingRentalResourceHandler.java
@@ -108,7 +108,7 @@ public class VipParkingRentalResourceHandler implements RentalResourceHandler {
         Integer pageSize = Integer.MAX_VALUE;
         List<RentalOrder> bills = rentalv2Provider.searchRentalOrders(cmd.getResourceTypeId(), cmd.getResourceType(),
                 cmd.getResourceId(), cmd.getBillStatus(), cmd.getStartTime(), cmd.getEndTime(),cmd.getTag1(),
-                cmd.getTag2(),null,cmd.getKeyword(), cmd.getPageAnchor(), pageSize);
+                cmd.getTag2(),null,cmd.getKeyword(), cmd.getPageAnchor(), pageSize,cmd.getPageNum());
 
         if(null == bills){
             bills = new ArrayList<>();
