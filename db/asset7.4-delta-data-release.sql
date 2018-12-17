d2b62e5f7106cd748a63eaef41b0410fe21047f1
cx.yang <cx.yang@zuolin.com>
2018-12-17 15:53:00 +0800

    瑞安项目专用、同瑞安CM系统服务账单同步失败的账单展示

---
+++ db/asset7.4-delta-data-release.sql
@@ -0,0 +1,85 @@
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: OPERATION
+-- DESCRIPTION: 此SECTION放升级相关的操作要求，如调接口、查询数据确认、修改配置文件、更新特殊程序等
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END OPERATION------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: ALL
+-- DESCRIPTION: 此SECTION放所有域空间都需要执行的脚本，包含基线、独立部署、研发数据等环境
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END ALL -----------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: zuolin-base
+-- DESCRIPTION: 此SECTION只在左邻基线（非独立署部）执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END zuolin-base ---------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: dev
+-- DESCRIPTION: 此SECTION只在开发库中执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END dev -----------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: zijing
+-- DESCRIPTION: 此SECTION只在清华信息港(紫荆)-999984执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END ---------------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: guangda
+-- DESCRIPTION: 此SECTION只在光大-999979执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END guangda -------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: szbay
+-- DESCRIPTION: 此SECTION只在深圳湾-999966执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END szbay ---------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: chuangyechang
+-- DESCRIPTION: 此SECTION只在成都创业场-999964执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END chuangyechang -------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: anbang
+-- DESCRIPTION: 此SECTION只在安邦物业-999949执行的脚本
+-- AUTHOR:
+-- REMARK:
+-- --------------------- SECTION END anbang---------------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: nanshanquzhengfu
+-- DESCRIPTION: 此SECTION只在南山区政府-999931执行的脚本
+-- --------------------- SECTION END nanshanquzhengfu ----------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: guanzhouyuekongjian
+-- DESCRIPTION: 此SECTION只在广州越空间-999930执行的脚本
+-- --------------------- SECTION END guanzhouyuekongjian -------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: ruianxintiandi
+-- DESCRIPTION: 此SECTION只在上海瑞安新天地-999929执行的脚本
+
+-- AUTHOR: 杨崇鑫
+-- REMARK: 瑞安项目专用、同瑞安CM系统服务账单同步失败的账单展示
+INSERT INTO `eh_service_module_functions`(`id`, `module_id`, `privilege_id`, `explain`) VALUES (102, 20400, 0, 'CM同步失败账单');
+-- AUTHOR: 杨崇鑫
+-- REMARK: 瑞安项目专用、同瑞安CM系统服务账单同步失败的账单展示
+set @id=(select ifnull((SELECT max(id) from eh_service_module_include_functions),1));
+INSERT INTO `eh_service_module_include_functions`(`id`, `namespace_id`, `module_id`, `community_id`, `function_id`) VALUES (@id:= @id +1, 999929, 20400, NULL, 102);
+
+
+
+-- --------------------- SECTION END ruianxintiandi ------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: wanzhihui
+-- DESCRIPTION: 此SECTION只在万智汇-999953执行的脚本
+-- --------------------- SECTION END wanzhihui ------------------------------------------
+-- --------------------- SECTION BEGIN -------------------------------------------------------
+-- ENV: jinmao
+-- DESCRIPTION: 此SECTION只在上海金茂-智臻生活 -999925执行的脚本
+-- --------------------- SECTION END jinmao ------------------------------------------
