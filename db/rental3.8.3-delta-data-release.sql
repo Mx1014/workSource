95f60a3a14c2c137b9c70da3600d180861d0801a
st.zheng <st.zheng@zuolin.com>
2018-12-19 17:57:29 +0800

    资源预约3.8.3

--- db/rental3.8.3-delta-data-release.sql
+++ db/rental3.8.3-delta-data-release.sql
@@ -1,3 +1,3 @@
 -- 资源预约3.8.3
 -- by st.zheng
-update eh_configurations set `value` = '/resource-rental/build/index.html#/resource-detail?namespaceId=%s&rentalSiteId=%s&detailPageType=%s&canRental=1&isApp=1' where name = 'rental.resource.detail.url';
+update eh_configurations set `value` = '/resource-rental/build/index.html#/resource-detail?namespaceId=%s&rentalSiteId=%s&detailPageType=%s&canRental=1&isApp=1#sign_suffix' where name = 'rental.resource.detail.url';
