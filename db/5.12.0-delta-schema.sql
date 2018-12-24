da641dc697a100bc9db17eb0c6914de425983485
liangyanlong <yanlong.liang@zuolin.com>
2018-12-24 17:54:36 +0800

    issue-45112

--- db/5.12.0-delta-schema.sql
+++ db/5.12.0-delta-schema.sql
@@ -219,3 +219,9 @@ CREATE TABLE `eh_aclink_photo_sync_result` (
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COMMENT='照片同步结果表';
 
 ALTER TABLE eh_door_auth_level ADD COLUMN `name` varchar(128) COMMENT '自动授权对象名称' AFTER `level_type`;
+
+-- AUTHOR: 梁燕龙 20181219
+-- REMARK: 动态主页签
+ALTER TABLE eh_portal_navigation_bars MODIFY config_json VARCHAR(1024);
+ALTER TABLE eh_portal_navigation_bars ADD COLUMN `top_bar_style` TINYINT COMMENT '主页签顶栏样式，1：透明渐变，2：不透明形变，3：不透明固定';
+ALTER TABLE eh_launch_pad_indexs ADD COLUMN `preview_version_id` BIGINT COMMENT '预览版本号';
